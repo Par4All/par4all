6d6bcce50434a5542f8bbfb61fce6f6f4b70014a
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1993-07-02 14:16:48 +0000

    # on 1993/07/02 14:16:48, keryell did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@40 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Passes/wpips/xv_sizes.h
@@ -0,0 +1,26 @@
+        /* We need to estimate the size of the decor added by the widow
+			manager, Y_WM_DECOR_SIZE & X_WM_DECOR_SIZE. RK, 9/10/1993. */
+
+#define Y_WM_DECOR_SIZE 30
+#define X_WM_DECOR_SIZE 4
+
+#define WPIPS_WIDTH  660
+#define WPIPS_HEIGHT 190
+
+#define EDIT_WIDTH WPIPS_WIDTH
+#define EDIT_HEIGHT 400
+
+#define DIALOG_WIDTH WPIPS_WIDTH
+#define DIALOG_HEIGHT 250
+
+#define HELP_WIDTH 420
+#define HELP_HEIGHT 180
+
+#define SINGLE_CHOICE_WIDTH 315
+#define SINGLE_CHOICE_HEIGHT 160
+
+#define MULTIPLE_CHOICE_WIDTH 400
+#define MULTIPLE_CHOICE_HEIGHT 150
+
+#define QUERY_HEIGHT 120
+#define QUERY_WIDTH 330
