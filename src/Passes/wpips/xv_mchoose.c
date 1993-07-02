bfc9475d0112da4037dd6d96d19c5b495c6461e0
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1993-07-02 14:11:57 +0000

    # on 1993/07/02 14:11:57, keryell did: Version en cours avec un panel de properties qui ne marche pas...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@29 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/xv_mchoose.c
+++ src/Passes/wpips/xv_mchoose.c
@@ -5,6 +5,7 @@
 #include <sys/resource.h>
 #include <xview/xview.h>
 #include <xview/panel.h>
+#include <xview/notice.h>
 #include <types.h>
 
 #include "genC.h"
@@ -13,6 +14,8 @@
 #include "wpips.h"
 
 static Panel_item mchoices, choices, ok, help;
+static char mchoices_notify_buffer[SMALL_BUFFER_LENGTH];
+
 static void (*apply_on_mchoices)();
 
 static void mchoose_help_notify(item, event)
@@ -27,31 +30,66 @@ Panel_item item;
 Event *event;
 {
     char *mchoices_args[ARGS_LENGTH];
+	char buffer[SMALL_BUFFER_LENGTH];
     int mchoices_length = 0;
-
-    int i, nchoices;
-
-    hide_window(mchoose_frame);
+    int i, nchoices, len;
+	int item_is_in_the_list;
+	char *p;
 
     nchoices = (int) xv_get(choices, PANEL_LIST_NROWS, 0);
     mchoices_length = 0;
 
+	/*
     for (i = 0; i < nchoices; i++) {
 	if ((int) xv_get(choices, PANEL_LIST_SELECTED, i) == TRUE) {
 	    char *s = strdup(xv_get(choices, PANEL_LIST_STRING, i));
 	    args_add(&mchoices_length, mchoices_args, s);		     
 	}
     }
+		Read the text line instead, 1 word separated by ' '.
+			RK, 19/05/1993.
+	*/
+	strcpy(mchoices_notify_buffer, (char *)xv_get(mchoices,PANEL_VALUE));
+	p = mchoices_notify_buffer;
+	while(sscanf(p,"%s%n",&buffer,&len) == 1) {
+		args_add(&mchoices_length, mchoices_args, strdup(buffer));
+		item_is_in_the_list = FALSE;
+		for(i = 0; i < nchoices; i++)
+			if (strcmp((char *)xv_get(choices, PANEL_LIST_STRING, i), buffer) == 0) {
+				item_is_in_the_list = TRUE;
+				break;
+			}
+		if (item_is_in_the_list == FALSE)
+			break;
+		p += len;
+	}
 
-    for (i = 0; i < nchoices; i++) {
-	xv_set(choices, PANEL_LIST_DELETE, 0, 0);
-    }
-
-    xv_set(mchoices, PANEL_VALUE, "", 0);
+	/*	At least on item selected, and in the list.
+			RK, 21/05/1993.
+	*/
+	if (mchoices_length == 0 || item_is_in_the_list == FALSE) {
+		int result;
+		char *s;
+		s = mchoices_length == 0 ? "You have to select at least 1 item!" :
+			"You have selected an item not in the choice list!";
+    	args_free(&mchoices_length, mchoices_args);
+		prompt_user(s);
+		return;
+	}
 
     (*apply_on_mchoices)(&mchoices_length, mchoices_args);
 
     args_free(&mchoices_length, mchoices_args);
+
+		/* Delay the graphics transformations. RK, 21/05/1993. */
+
+	for (i = 0; i < nchoices; i++) {
+	xv_set(choices, PANEL_LIST_DELETE, 0, 0);
+	}
+
+	xv_set(mchoices, PANEL_VALUE, "", 0);
+
+    hide_window(mchoose_frame);
 }
 
 
@@ -61,20 +99,23 @@ Panel_item item;
 Panel_list_op op;
 Event *event;
 {
-    char buffer[SMALL_BUFFER_LENGTH];
-
     int nchoices = (int) xv_get(choices, PANEL_LIST_NROWS, 0);
 
-    buffer[0] = '\0';
+    mchoices_notify_buffer[0] = '\0';
+
+	/* Now it is mchoices_notify_buffer which is used for the selection.
+		No size verification implemented yet... :-)
+		RK, 19/05/1993. */
 
     while (nchoices--) {
 	if ((int) xv_get(choices, PANEL_LIST_SELECTED, nchoices) == TRUE) {
-	    strcat(buffer, xv_get(choices, PANEL_LIST_STRING, nchoices));
-	    strcat(buffer, " ");
+	    strcat(mchoices_notify_buffer,
+			xv_get(choices, PANEL_LIST_STRING, nchoices));
+	    strcat(mchoices_notify_buffer, " ");
 	}
     }
 
-    xv_set(mchoices, PANEL_VALUE, buffer, 0);
+    xv_set(mchoices, PANEL_VALUE, mchoices_notify_buffer, 0);
 }
 
 
@@ -89,6 +130,7 @@ void (*f)();
     int nchoices;
 
     apply_on_mchoices = f;
+    mchoices_notify_buffer[0] = '\0';
 
     xv_set(mchoose_frame, FRAME_LABEL, title, 0);
 
@@ -111,10 +153,15 @@ void create_mchoose_window()
 {
     mchoose_panel = xv_create(mchoose_frame, PANEL, 0);
 
+/* Carriage return enable for "OK". RK, 19/05/1993. */
+
     mchoices = xv_create(mchoose_panel, PANEL_TEXT,
 			 PANEL_LABEL_STRING, "Current choices",
 			 PANEL_VALUE_DISPLAY_LENGTH, 30,
 			 PANEL_VALUE_STORED_LENGTH, 128,
+			 PANEL_NOTIFY_LEVEL,PANEL_SPECIFIED,
+			 PANEL_NOTIFY_STRING,"\n",
+			PANEL_NOTIFY_PROC, ok_notify,
 			 XV_X, 10,
 			 XV_Y, 10,
 			 0);
