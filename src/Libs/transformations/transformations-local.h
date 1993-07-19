9031f9140ea28663c56ca40d35a4f9e7013490b6
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1993-07-19 14:27:31 +0000

    # on 1993/07/19 14:27:31, keryell did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@109 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/transformations/transformations-local.h
@@ -0,0 +1,18 @@
+#define SIGN_EQ(a,b) ((((a)>0 && (b)>0) || ((a)<0 && (b)<0)) ? TRUE : FALSE)
+#define FORTRAN_DIV(n,d) (SIGN_EQ((n),(d)) ? ABS(n)/ABS(d) : -(ABS(n)/ABS(d)))
+#define FORTRAN_MOD(n,m) (SIGN_EQ((n),(m)) ? ABS(n)%ABS(m) : -(ABS(n)%ABS(m)))
+
+/*
+ * EFORMAT: the expression format used in recursiv evaluations.
+ * = ((ICOEF * EXPR) + ISHIFT)
+ * it is SIMPLER when it is interesting to replace initial expression by the
+ * one generated from eformat.
+ */
+struct eformat {
+    expression expr;
+    int icoef;
+    int ishift;
+    boolean simpler;
+};
+
+
