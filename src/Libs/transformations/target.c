8ab75ef928c8aa76465ffcd54eb084a0e05c5a8a
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1993-07-19 14:27:28 +0000

    # on 1993/07/19 14:27:28, keryell did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@107 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/transformations/target.c
@@ -0,0 +1,36 @@
+/* Summary description of the target machine.
+ *
+ * Should be merged with the wp65 description and the complexity cost model by Lei Zhou
+ *
+ * Francois Irigoin, 16 January 1993
+ */
+
+#include "genC.h"
+
+int get_cache_line_size()
+{
+    return 1;
+}
+
+int get_processor_number()
+{
+    return 16;
+}
+
+int get_vector_register_length()
+{
+    return 64;
+}
+
+int get_vector_register_number()
+{
+    return 8;
+}
+
+int get_minimal_task_size()
+{
+    /* the unit is supposed to be consistent with the complexity cost tables used
+     * that should be expressed in machine cycles
+     */
+    return 10000;
+}
