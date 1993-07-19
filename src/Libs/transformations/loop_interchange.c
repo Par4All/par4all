4b685fa72e0567f5718881052bf94efca69da405
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1993-07-19 14:27:21 +0000

    # on 1993/07/19 14:27:21, keryell did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@100 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/transformations/loop_interchange.c
@@ -0,0 +1,66 @@
+ /* interface with pipsmake */
+
+#include <stdio.h>
+extern int fprintf();
+extern int sscanf();
+#include <string.h>
+
+#include "genC.h"
+#include "constants.h"
+#include "misc.h"
+#include "ri.h"
+#include "ri-util.h"
+#include "database.h"
+#include "pipsdbm.h"
+#include "resources.h"
+
+#include "control.h"
+#include "generation.h"
+
+#include "transformations.h"
+
+entity selected_label;
+
+bool selected_loop_p(l)
+loop l;
+{
+    return loop_label(l) == selected_label;
+}
+
+void loop_interchange(module_name)
+string module_name;
+{
+    char lp_label[6];
+    entity module = local_name_to_top_level_entity(module_name);
+    statement s;
+    string resp;
+
+    debug_on("LOOP_INTERCHANGE_DEBUG_LEVEL");
+
+    pips_assert("loop_interchange", entity_module_p(module));
+
+    /* DBR_CODE will be changed: argument "pure" should take FALSE but this would be useless
+       since there is only *one* version of code; a new version will be put back in the
+       data base after exchanging the loops */
+    s = (statement) db_get_memory_resource(DBR_CODE, module_name, TRUE);
+
+    /* Get the loop label form the user */
+    resp= user_request("Which loop do you want to exchange?\n(give its label): ");
+    sscanf(resp, "%s", lp_label);
+    selected_label = find_label_entity(module_name, lp_label);
+    if (selected_label==entity_undefined) {
+	user_error("loop_interchange", "loop label `%s' does not exist\n", lp_label);
+    }
+
+    look_for_nested_loop_statements(s, interchange,selected_loop_p);
+
+    debug_off();
+
+    /* Reorder the module, because new statements have been generated. */
+    module_body_reorder(s);
+
+    DB_PUT_MEMORY_RESOURCE(DBR_CODE, 
+			   strdup(module_name), 
+			   (char*) s);
+
+}
