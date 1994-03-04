cd56c3595f27db07e5ed0c006198046d38851c34
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-03-04 17:04:44 +0000

    # on 1994/03/04 17:04:44, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@186 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/hpfc/access_description.h
@@ -0,0 +1,30 @@
+#ifndef ACCESS_DESCRIPTION_H
+#define ACCESS_DESCRIPTION_H
+
+/*
+ * just something looking like Newgen Domains for homogeneity
+ *
+ * should use normalized somewhere, somehow...
+ */
+
+#define access			int
+
+#define	access_undefined        (-1)
+#define	local_constant		 (1)
+#define local_shift		 (2)
+#define local_star		 (3)
+#define local_affine		 (4)
+#define aligned_constant	 (5)
+#define aligned_shift		 (6)
+#define aligned_star		 (7)
+#define aligned_affine		 (8)
+#define not_aligned		 (9)
+#define local_form_cst          (10)
+
+#define	access_undefined_p(a)		(a == access_undefined)
+
+#define access_tag(a)			(a)
+#define make_access(a)			(a)
+
+#endif
+
