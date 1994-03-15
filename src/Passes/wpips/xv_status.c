087a61af2c718f2ef47e9f911b2b31cf02789ab2
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-03-15 21:05:18 +0000

    # on 1994/03/15 21:05:18, keryell did: Essai de positionnement automatique des PANEL_ITEMs. Bof, car le texte des valeurs n'est plus aligne'...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@277 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/xv_status.c
+++ src/Passes/wpips/xv_status.c
@@ -87,68 +87,62 @@ string m;
 
 void create_status_subwindow()
 {
-    window_fit(main_panel);
+  window_fit(main_panel);
 
-    status_panel = (Panel) xv_create(main_frame, PANEL,
-				     WIN_X, 0,
-				     WIN_BELOW, main_panel,
-				     /* Corrige un bug de peinture en
-                                        couleur. */
-				     PANEL_ITEM_X_GAP, 200,
-				     NULL);
+  status_panel = (Panel) xv_create(main_frame, PANEL,
+				   WIN_X, 0,
+				   WIN_BELOW, main_panel,
+				   /* Corrige un bug de peinture en
+				      couleur. */
+				   PANEL_ITEM_X_GAP, 200,
+				   /*				     PANEL_LAYOUT, PANEL_VERTICAL,*/
+				   NULL);
 
-    /* PANEL_VALUE_Y used to be unset before we shifted to xview.3, Apr. 92 */
-    message = 
-	xv_create(status_panel, PANEL_TEXT, 
-		  PANEL_VALUE_X, 85,
-		  PANEL_VALUE_Y, xv_rows(status_panel, 0),
-		  PANEL_LABEL_STRING, "Message:",
-		  PANEL_READ_ONLY, TRUE,
-		  PANEL_VALUE_DISPLAY_LENGTH, 64,
-		  NULL);
+  /* PANEL_VALUE_Y used to be unset before we shifted to xview.3, Apr. 92 */
+  message = 
+    xv_create(status_panel, PANEL_TEXT, 
+	      PANEL_LABEL_STRING, "Message:",
+	      PANEL_READ_ONLY, TRUE,
+	      PANEL_VALUE_DISPLAY_LENGTH, 64,
+	      NULL);
 
-    directory_name = 
-	xv_create(status_panel, PANEL_TEXT, 
-		  PANEL_VALUE_X, 85,
-		  PANEL_VALUE_Y, xv_rows(status_panel, 1),
-		  PANEL_LABEL_STRING, "Directory:",
-		  PANEL_READ_ONLY, TRUE,
-		  PANEL_VALUE_DISPLAY_LENGTH, 64,
-		  NULL);
+  directory_name = 
+    xv_create(status_panel, PANEL_TEXT,
+	      PANEL_NEXT_ROW, -1,
+	      PANEL_LABEL_STRING, "Directory:",
+	      PANEL_READ_ONLY, TRUE,
+	      PANEL_VALUE_DISPLAY_LENGTH, 64,
+	      NULL);
 
-    program_name = 
-	xv_create(status_panel, PANEL_TEXT,
-		  PANEL_VALUE_X, 85,
-		  PANEL_VALUE_Y, xv_rows(status_panel, 2),
-		  PANEL_LABEL_STRING, "Workspace:",
-		  PANEL_VALUE_DISPLAY_LENGTH, 10,
-		  PANEL_READ_ONLY, TRUE, 
-		  NULL);
+  program_name = 
+    xv_create(status_panel, PANEL_TEXT,
+	      PANEL_NEXT_ROW, -1,
+	      PANEL_LABEL_STRING, "Workspace:",
+	      PANEL_VALUE_DISPLAY_LENGTH, 10,
+	      PANEL_READ_ONLY, TRUE, 
+	      NULL);
 
-    module_name = 
-	xv_create(status_panel, PANEL_TEXT, 
-		  PANEL_VALUE_X, 85,
-		  PANEL_VALUE_Y, xv_rows(status_panel, 3),
-		  PANEL_LABEL_STRING, "Module:",
-		  PANEL_READ_ONLY, TRUE,
-		  PANEL_VALUE_DISPLAY_LENGTH, 10,
-		  NULL);
+  module_name = 
+    xv_create(status_panel, PANEL_TEXT, 
+	      PANEL_NEXT_ROW, -1,
+	      PANEL_LABEL_STRING, "Module:",
+	      PANEL_READ_ONLY, TRUE,
+	      PANEL_VALUE_DISPLAY_LENGTH, 10,
+	      NULL);
 
-    memory_name = 
-	xv_create(status_panel, PANEL_TEXT, 
-/*		  PANEL_VALUE_X, xv_col(status_panel, 2), */
-		  PANEL_VALUE_X, 255,
-		  PANEL_VALUE_Y, xv_rows(status_panel, 3),
-		  PANEL_LABEL_STRING, "Memory:",
-		  PANEL_READ_ONLY, TRUE,
-		  PANEL_VALUE_DISPLAY_LENGTH, 10,
-		  NULL);
+  memory_name = 
+    xv_create(status_panel, PANEL_TEXT, 
+	      /*		  PANEL_VALUE_X, xv_col(status_panel, 2), */
+	      PANEL_LABEL_STRING, "Memory:",
+	      PANEL_READ_ONLY, TRUE,
+	      PANEL_VALUE_DISPLAY_LENGTH, 10,
+	      NULL);
 
-    window_fit(status_panel);
-    window_fit(main_frame);
+  window_fit(status_panel);
+  window_fit(main_frame);
 
-    show_directory();
-    show_program();
-    show_module();
+  show_directory();
+  show_program();
+  show_module();
 
 }
