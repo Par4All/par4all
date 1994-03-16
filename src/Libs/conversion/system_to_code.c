02c4e4f01b7e3480cac1ad6a1444ed2243abc46a
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-03-16 17:22:42 +0000

    # on 1994/03/16 17:22:42, coelho did: Psysteme to expression added
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@296 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/conversion/system_to_code.c
+++ src/Libs/conversion/system_to_code.c
@@ -2,7 +2,8 @@
  * HPFC module by Fabien COELHO
  *
  * SCCS stuff:
- * $RCSfile: system_to_code.c,v $ ($Date: 1994/03/16 12:04:56 $, ) version $Revision$, got on %D%, %T%
+ * $RCSfile: system_to_code.c,v $ ($Date: 1994/03/16 17:22:42 $, ) version $Revision$, 
+ * got on %D%, %T%
  * $Id$
  */
 
@@ -55,6 +56,65 @@ extern fprintf();
 /*
  * ONE ARRAY REFERENCES MODIFICATIONS
  */
+
+
+/*
+ * TEST GENERATION
+ */
+
+/*
+ * expression Psysteme_to_expression(Psysteme systeme)
+ *
+ * From a Psysteme, a logical expression that checks for
+ * the constraints is generated.
+ */
+expression Psysteme_to_expression(systeme)
+Psysteme systeme;
+{
+    entity
+	equ = local_name_to_top_level_entity(EQUAL_OPERATOR_NAME),
+	leq = local_name_to_top_level_entity(LESS_OR_EQUAL_OPERATOR_NAME);
+    list
+	conjonction = 
+	    gen_nconc
+		(Pcontrainte_to_expression_list(sc_egalites(systeme), equ),
+		 Pcontrainte_to_expression_list(sc_inegalites(systeme), leq));
+    expression
+	result = list_to_conjonction(conjonction);
+
+    gen_free_list(conjonction);
+    return(result);
+}
+
+/*
+ * 
+ */
+list Pcontrainte_to_expression_list(constraint, operator)
+Pcontrainte constraint;
+entity operator;
+{
+    list
+	result = NIL;
+    Pcontrainte
+	c = NULL;
+    expression
+	zero = make_integer_constant_expression(0);
+
+    for(c = constraint;
+	!CONTRAINTE_UNDEFINED_P(c);
+	c=c->succ)
+    {
+	result = 
+	    CONS(EXPRESSION,
+		 MakeBinaryCall(operator,
+				make_vecteur_expression(contrainte_vecteur(c)),
+				zero),
+		 result);
+    }
+
+    return(result);
+}
+
 
 
 /*
