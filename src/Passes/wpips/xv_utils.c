2c3d93182061f256cfd06b03e8916784cbbb3a19
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-03-16 01:08:22 +0000

    # on 1994/03/16 01:08:22, keryell did: Ame'lioration de la pre'sentation.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@282 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/xv_utils.c
+++ src/Passes/wpips/xv_utils.c
@@ -5,6 +5,12 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <xview/xview.h>
+#include <xview/panel.h>
+
+#include "genC.h"
+#include "misc.h"
+
+#include "wpips.h"
 
 extern char *getwd();
 
@@ -25,4 +31,19 @@ Frame frame;
 {
     xv_set(frame, FRAME_CLOSED, FALSE, NULL);
     xv_set(frame, XV_SHOW, TRUE, NULL);
+}
+
+
+/* Centre la souris sur une fene^tre : */
+void pointer_in_center_of_frame(Frame frame)
+{
+  Display *dpy;
+  Window xwindow;
+  Rect rect;
+    
+  dpy = (Display *)xv_get(main_frame, XV_DISPLAY);
+  xwindow = (Window) xv_get(frame, XV_XID);
+  frame_get_rect(frame, &rect);
+  XWarpPointer(dpy, None, xwindow, None, None, None, None, 
+	       rect.r_width/2, rect.r_height/2);
 }
