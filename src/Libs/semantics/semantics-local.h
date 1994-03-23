1e22d47643c0ce3dc5a1835c00394066e33e71cc
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-03-23 16:53:57 +0000

    # on 1994/03/23 16:53:57, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@338 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/semantics/semantics-local.h
@@ -0,0 +1,8 @@
+ /* include file for semantic analysis */
+
+#define SEMANTICS_OPTIONS "?Otcfieod-D:"
+
+#define SEQUENTIAL_TRANSFORMER_SUFFIX ".tran"
+#define USER_TRANSFORMER_SUFFIX ".utran"
+#define SEQUENTIAL_PRECONDITION_SUFFIX ".prec"
+#define USER_PRECONDITION_SUFFIX ".uprec"
