302e802519ea2c509267845489ad575c21b3c2f5
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-06-03 14:14:34 +0000

    # on 1994/06/03 14:14:34, coelho did: I/O Release:-)
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@463 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/hpfc/inits.c
+++ src/Libs/hpfc/inits.c
@@ -7,7 +7,7 @@
  * run-time resolution parameters.
  *
  * SCCS stuff:
- * $RCSfile: inits.c,v $ ($Date: 1994/04/11 11:51:45 $, ) version $Revision$,
+ * $RCSfile: inits.c,v $ ($Date: 1994/06/03 14:14:34 $, ) version $Revision$,
  * got on %D%, %T%
  * $Id$
  *
@@ -49,11 +49,14 @@ FILE* file;
     fprintf(file, "c\nc parameters\nc\n");
 
     fprintf(file, "      parameter(REALNBOFARRAYS = %d)\n", 
-	    gen_length(distributedarrays));
+	    number_of_distributed_arrays());
+
     fprintf(file, "      parameter(REALNBOFTEMPLATES = %d)\n", 
-	    gen_length(templates));
+	    number_of_templates());
+
     fprintf(file, "      parameter(REALNBOFPROCESSORS = %d)\n", 
-	    gen_length(processors));
+	    number_of_processors());
+
     fprintf(file, "      parameter(REALMAXSIZEOFPROCS = %d)\n", 
 	    max_size_of_processors());
 
@@ -67,7 +70,7 @@ FILE* file;
 	 entity
 	     array = ENTITY(CAR(ce));
 	 entity
-	     newarray = (entity) GET_ENTITY_MAPPING(oldtonewnodevar, array);
+	     newarray = load_entity_node_new(array);
 	 int
 	     i = 1;
 
@@ -80,7 +83,7 @@ FILE* file;
 	     firstline = FALSE;
 	 }
      },
-	 distributedarrays);
+	 list_of_distributed_arrays());
 
     fprintf(file, "\nc\nc bounds values\nc\n");
     
@@ -89,7 +92,7 @@ FILE* file;
 	 entity
 	     array = ENTITY(CAR(ce));
 	 entity
-	     newarray = (entity) GET_ENTITY_MAPPING(oldtonewnodevar, array);
+	     newarray = load_entity_node_new(array);
 	 int
 	     i = 1;
 
@@ -106,7 +109,7 @@ FILE* file;
 		     HpfcExpressionToInt(dimension_upper(d)));
 	 }
      },
-	 distributedarrays);
+	 list_of_distributed_arrays());
 
     fprintf(file, "c\nc end of automatically generated file\nc\n");
 }
@@ -132,7 +135,7 @@ int max_size_of_processors()
 	 current_max = max(current_max,
 			   NumberOfElements(variable_dimensions(a)));
      }, 
-	 processors);
+	 list_of_processors());
 
     return(current_max);
 }
@@ -160,20 +163,20 @@ FILE* file;
      {
 	 entity
 	     array = ENTITY(CAR(ce));
-	 int an = (int) GET_ENTITY_MAPPING(hpfnumber, array);
+	 int an = get_hpf_number(array);
 	 int nd  = NumberOfDimension(array);
 	 align
-	     al = (align) GET_ENTITY_MAPPING(hpfalign, array);
+	     al = load_entity_align(array);
 	 entity
 	     template = align_template(al);
-	 int tn = (int) GET_ENTITY_MAPPING(hpfnumber, template);
+	 int tn = get_hpf_number(template);
 	 distribute
-	     di = (distribute) GET_ENTITY_MAPPING(hpfdistribute, template);
+	     di = load_entity_distribute(template);
 	 int i;
 	 
 	 fprintf(file, "c\nc initializing array %s, number %d\nc\n",
 		 entity_local_name(array), an);
-
+	 
 	 /*
 	  * NODIMA: Number Of  DIMensions of an Array
 	  * ATOT: Array TO Template
@@ -185,172 +188,163 @@ FILE* file;
 	  * RANGEA: lower, upper, size and declaration, aso
 	  */
 	 i = 1;
-	 MAPL(cd,
-	  {
-	      dimension
-		  d = DIMENSION(CAR(cd));
-	      int lb = HpfcExpressionToInt(dimension_lower(d));
-	      int ub = HpfcExpressionToInt(dimension_upper(d));
-	      int sz = (ub-lb+1);
-	      int decl;
-	      int j;
-	      list l = (list) GET_ENTITY_MAPPING(newdeclarations, array);
-	      alignment 
-		  a = FindAlignmentOfDim(align_alignment(al), i);
-	      
-
-	      /* gen_find_ith()... */
-	      for(j=1 ; j<i ; j++) l=CDR(l);
-	      decl = INT(CAR(l));
-
-	      /*
-	       * RANGEA contents:
-	       *
-	       * 1: lower bound
-	       * 2: upper bound
-	       * 3: size, (2)-(1)+1
-	       * 4: new declaration flag
-	       */
-	      fprintf(file, "\n");
-	      fprintf(file, "      RANGEA(%d, %d, 1) = %d\n", an, i, lb);
-	      fprintf(file, "      RANGEA(%d, %d, 2) = %d\n", an, i, ub);
-	      fprintf(file, "      RANGEA(%d, %d, 3) = %d\n", an, i, sz);
-	      fprintf(file, "c\n");
-	      fprintf(file, "      RANGEA(%d, %d, 4) = %d\n", an, i, decl);
-	      
-	      switch(decl)
-	      {
-	      case NO_NEW_DECLARATION:
-		  break;
-	      case ALPHA_NEW_DECLARATION:
-		  /*
-		   * 5: 1 - lower bound
-		   */
-		  fprintf(file, "      RANGEA(%d, %d, 5) = %d\n", an, i, (1-lb));
-		  break;
-	      case BETA_NEW_DECLARATION:
-	      {
-		  int tdim = alignment_templatedim(a);
-		  int procdim = 0;
-		  distribution
-		      d = FindDistributionOfDim(distribute_distribution(di), 
-						tdim,
-						&procdim);
-		  int param = HpfcExpressionToInt(distribution_parameter(d));
-		  int rate;
-		  int shift;
-		  dimension 
-		      dim = FindIthDimension(template, tdim);
-		  
-		  pips_assert("create_init_common_param", 
-			      style_block_p(distribution_style(d)));
-		  
-		  rate = HpfcExpressionToInt(alignment_rate(a));
-		  shift = (HpfcExpressionToInt(alignment_constant(a)) -
-			   HpfcExpressionToInt(dimension_lower(dim)));
-		  /*
-		   * 5: distribution parameter n, 
-		   * 6: alignment rate a,
-		   * 7: alignment shift, b-t_{m}
-		   */
-		  fprintf(file, "      RANGEA(%d, %d, 5) = %d\n", an, i, param);
-		  fprintf(file, "      RANGEA(%d, %d, 6) = %d\n", an, i, rate);
-		  fprintf(file, "      RANGEA(%d, %d, 7) = %d\n", an, i, shift);
-	      
-		  break;
-	      }
-	      case GAMMA_NEW_DECLARATION:
-	      {
-		  int tdim = alignment_templatedim(a);
-		  int procdim = 0;
-		  distribution
-		      d = FindDistributionOfDim(distribute_distribution(di), 
-						tdim,
-						&procdim);
-		  int param = HpfcExpressionToInt(distribution_parameter(d));
-		  int sc;
-		  int no;
-		  int shift;
-		  dimension 
-		      dim = FindIthDimension(template, tdim);
-		  entity
-		      proc = distribute_processors(di);
-
-		  pips_assert("create_init_common_param", 
-			      style_cyclic_p(distribution_style(d)));
-
-		  sc = param*SizeOfIthDimension(proc, procdim);
-		  shift = (HpfcExpressionToInt(alignment_constant(a)) -
-			   HpfcExpressionToInt(dimension_lower(dim)));
-		  no = (lb + shift) / sc ;
-
-		  /*
-		   * 5: distribution parameter n,
-		   * 6: cycle length n*p,
-		   * 7: initial cycle number,
-		   * 8: alignment shift, b-t_{m}
-		   */
-		  fprintf(file, "      RANGEA(%d, %d, 5) = %d\n", an, i, param);
-		  fprintf(file, "      RANGEA(%d, %d, 6) = %d\n", an, i, sc);
-		  fprintf(file, "      RANGEA(%d, %d, 7) = %d\n", an, i, no);
-		  fprintf(file, "      RANGEA(%d, %d, 8) = %d\n", an, i, shift);
-
-		  break;
-	      }
-	      case DELTA_NEW_DECLARATION:
-	      {
-		  int tdim = alignment_templatedim(a);
-		  int rate = HpfcExpressionToInt(alignment_rate(a));
-		  int cst  = HpfcExpressionToInt(alignment_constant(a));
-		  int param = HpfcExpressionToInt(distribution_parameter(d));
-		  int procdim = 0;
-		  int sc;
-		  int no;
-		  int shift;
-		  int chuncksize;		      
-		  distribution
-		      d = FindDistributionOfDim(distribute_distribution(di), 
-						tdim,
-						&procdim);
-		  dimension 
-		      templdim = FindIthDimension(template, tdim);
-		  entity
-		      proc = distribute_processors(di);
-		  
-		  pips_assert("create_init_common_param", 
-			      style_cyclic_p(distribution_style(d)));
-		  
-		  sc = param*SizeOfIthDimension(proc, procdim);
-		  shift = (cst - HpfcExpressionToInt(dimension_lower(templdim)));
-		  no = (rate*lb + shift) / sc ;
-		  chuncksize = iceil(param, abs(rate));
-		  
-		  /*
-		   *  5: distribution parameter n
-		   *  6: cycle length n*p,
-		   *  7: initial cycle number,
-		   *  8: alignment shift, b-t_{m}
-		   *  9: alignment rate a,
-		   * 10: chunck size ceil(n,|a|)
-		   */
-		  fprintf(file, "      RANGEA(%d, %d, 5) = %d\n", an, i, param);
-		  fprintf(file, "      RANGEA(%d, %d, 6) = %d\n", an, i, sc);
-		  fprintf(file, "      RANGEA(%d, %d, 7) = %d\n", an, i, no);
-		  fprintf(file, "      RANGEA(%d, %d, 8) = %d\n", an, i, shift);
-		  fprintf(file, "      RANGEA(%d, %d, 9) = %d\n", an, i, rate);
-		  fprintf(file, "      RANGEA(%d, %d, 10) = %d\n", an, i, chuncksize);
-
-		  break;
-	      }
-	      default:
-		  pips_error("create_init_common_param", 
-			     "unexpected new declaration tag (%d)\n",
-			     decl);
-	      }
-
-	      i++;
-	  },
-	      variable_dimensions(type_variable(entity_type(array))));
+	 for (i=1; i<=nd; i++)
+	 {
+	     dimension
+		 d = entity_ith_dimension(array, i);
+	     int lb = HpfcExpressionToInt(dimension_lower(d));
+	     int ub = HpfcExpressionToInt(dimension_upper(d));
+	     int sz = (ub-lb+1);
+	     int decl = new_declaration(array, i);
+	     alignment 
+		 a = FindAlignmentOfDim(align_alignment(al), i);
+	     
+	     /*
+	      * RANGEA contents:
+	      *
+	      * 1: lower bound
+	      * 2: upper bound
+	      * 3: size, (2)-(1)+1
+	      * 4: new declaration flag
+	      */
+	     fprintf(file, "\n");
+	     fprintf(file, "      RANGEA(%d, %d, 1) = %d\n", an, i, lb);
+	     fprintf(file, "      RANGEA(%d, %d, 2) = %d\n", an, i, ub);
+	     fprintf(file, "      RANGEA(%d, %d, 3) = %d\n", an, i, sz);
+	     fprintf(file, "c\n");
+	     fprintf(file, "      RANGEA(%d, %d, 4) = %d\n", an, i, decl);
+	     
+	     switch(decl)
+	     {
+	     case NO_NEW_DECLARATION:
+		 break;
+	     case ALPHA_NEW_DECLARATION:
+		 /*
+		  * 5: 1 - lower bound
+		  */
+		 fprintf(file, "      RANGEA(%d, %d, 5) = %d\n", 
+			 an, i, (1-lb));
+		 break;
+	     case BETA_NEW_DECLARATION:
+	     {
+		 int tdim = alignment_templatedim(a);
+		 int procdim = 0;
+		 distribution
+		     d = FindDistributionOfDim(distribute_distribution(di), 
+					       tdim,
+					       &procdim);
+		 int param = HpfcExpressionToInt(distribution_parameter(d));
+		 int rate;
+		 int shift;
+		 dimension 
+		     dim = FindIthDimension(template, tdim);
+		 
+		 pips_assert("create_init_common_param", 
+			     style_block_p(distribution_style(d)));
+		 
+		 rate = HpfcExpressionToInt(alignment_rate(a));
+		 shift = (HpfcExpressionToInt(alignment_constant(a)) -
+			  HpfcExpressionToInt(dimension_lower(dim)));
+		 /*
+		  * 5: distribution parameter n, 
+		  * 6: alignment rate a,
+		  * 7: alignment shift, b-t_{m}
+		  */
+		 fprintf(file, "      RANGEA(%d, %d, 5) = %d\n", an, i, param);
+		 fprintf(file, "      RANGEA(%d, %d, 6) = %d\n", an, i, rate);
+		 fprintf(file, "      RANGEA(%d, %d, 7) = %d\n", an, i, shift);
+		 
+		 break;
+	     }
+	     case GAMMA_NEW_DECLARATION:
+	     {
+		 int tdim = alignment_templatedim(a);
+		 int procdim = 0;
+		 distribution
+		     d = FindDistributionOfDim(distribute_distribution(di), 
+					       tdim,
+					       &procdim);
+		 int param = HpfcExpressionToInt(distribution_parameter(d));
+		 int sc;
+		 int no;
+		 int shift;
+		 dimension 
+		     dim = FindIthDimension(template, tdim);
+		 entity
+		     proc = distribute_processors(di);
+		 
+		 pips_assert("create_init_common_param", 
+			     style_cyclic_p(distribution_style(d)));
+		 
+		 sc = param*SizeOfIthDimension(proc, procdim);
+		 shift = (HpfcExpressionToInt(alignment_constant(a)) -
+			  HpfcExpressionToInt(dimension_lower(dim)));
+		 no = (lb + shift) / sc ;
+		 
+		 /*
+		  * 5: distribution parameter n,
+		  * 6: cycle length n*p,
+		  * 7: initial cycle number,
+		  * 8: alignment shift, b-t_{m}
+		  */
+		 fprintf(file, "      RANGEA(%d, %d, 5) = %d\n", an, i, param);
+		 fprintf(file, "      RANGEA(%d, %d, 6) = %d\n", an, i, sc);
+		 fprintf(file, "      RANGEA(%d, %d, 7) = %d\n", an, i, no);
+		 fprintf(file, "      RANGEA(%d, %d, 8) = %d\n", an, i, shift);
+		 
+		 break;
+	     }
+	     case DELTA_NEW_DECLARATION:
+	     {
+		 int tdim = alignment_templatedim(a);
+		 int rate = HpfcExpressionToInt(alignment_rate(a));
+		 int cst  = HpfcExpressionToInt(alignment_constant(a));
+		 int param = HpfcExpressionToInt(distribution_parameter(d));
+		 int procdim = 0;
+		 int sc;
+		 int no;
+		 int shift;
+		 int chck;		      
+		 distribution
+		     d = FindDistributionOfDim(distribute_distribution(di), 
+					       tdim,
+					       &procdim);
+		 dimension 
+		     templdim = FindIthDimension(template, tdim);
+		 entity
+		     proc = distribute_processors(di);
+		 
+		 pips_assert("create_init_common_param", 
+			     style_cyclic_p(distribution_style(d)));
+		 
+		 sc = param*SizeOfIthDimension(proc, procdim);
+		 shift = (cst - HpfcExpressionToInt(dimension_lower(templdim)));
+		 no = (rate*lb + shift) / sc ;
+		 chck = iceil(param, abs(rate));
+		 
+		 /*
+		  *  5: distribution parameter n
+		  *  6: cycle length n*p,
+		  *  7: initial cycle number,
+		  *  8: alignment shift, b-t_{m}
+		  *  9: alignment rate a,
+		  * 10: chunck size ceil(n,|a|)
+		  */
+		 fprintf(file, "      RANGEA(%d, %d, 5) = %d\n", an, i, param);
+		 fprintf(file, "      RANGEA(%d, %d, 6) = %d\n", an, i, sc);
+		 fprintf(file, "      RANGEA(%d, %d, 7) = %d\n", an, i, no);
+		 fprintf(file, "      RANGEA(%d, %d, 8) = %d\n", an, i, shift);
+		 fprintf(file, "      RANGEA(%d, %d, 9) = %d\n", an, i, rate);
+		 fprintf(file, "      RANGEA(%d, %d, 10) = %d\n", an, i, chck);
+		 
+		 break;
+	     }
+	     default:
+		 pips_error("create_init_common_param", 
+			    "unexpected new declaration tag (%d)\n",
+			    decl);
+	     }
+	 }
 
 	 fprintf(file, "\n");
 
@@ -385,7 +379,7 @@ FILE* file;
 	     }
 	  }
      },
-	 distributedarrays);
+	 list_of_distributed_arrays());
 
     /*
      * Templates
@@ -396,13 +390,13 @@ FILE* file;
      {
 	 entity
 	     template = ENTITY(CAR(ce));
-	 int tn = (int) GET_ENTITY_MAPPING(hpfnumber, template);
+	 int tn = get_hpf_number(template);
 	 int nd  = NumberOfDimension(template);
 	 distribute
-	     di = (distribute) GET_ENTITY_MAPPING(hpfdistribute, template);
+	     di = load_entity_distribute(template);
 	 entity
 	     proc = distribute_processors(di);
-	 int pn = (int) GET_ENTITY_MAPPING(hpfnumber, proc);
+	 int pn = get_hpf_number(proc);
 	 int procdim = 1;
 	 int tempdim = 1;
 	 
@@ -474,18 +468,18 @@ FILE* file;
 	  },
 	      distribute_distribution(di));
      },
-	 templates);
+	 list_of_templates());
 
     /*
      * Processors
      */
     fprintf(file, "c\nc Processors Initializations\nc\n");
 
-MAPL(ce,
+    MAPL(ce,
      {
 	 entity
 	     proc = ENTITY(CAR(ce));
-	 int pn = (int) GET_ENTITY_MAPPING(hpfnumber, proc);
+	 int pn = get_hpf_number(proc);
 	 int nd  = NumberOfDimension(proc);
 	 int procdim = 1;
 	 
@@ -517,9 +511,10 @@ MAPL(ce,
 	  },
 	      variable_dimensions(type_variable(entity_type(proc))));
      },
-	 processors);
+	 list_of_processors());
 
     fprintf(file, "      end\n");
-    fprintf(file, "c\nc end of automatically generated fortran subroutine\nc\n");
+    fprintf(file, 
+	    "c\nc end of automatically generated fortran subroutine\nc\n");
 }
 
