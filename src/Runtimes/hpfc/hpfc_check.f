3a39905884a25f183b4d827baba1a21fa77774ab
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-08-29 13:56:27 +0000

    # on 1994/08/29 13:56:27, coelho did: m4 version
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@520 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/hpfc/hpfc_check.f
+++ src/Runtimes/hpfc/hpfc_check.f
@@ -1,14 +1,14 @@
 c
 c Checks run-time library version
 c
-c $RCSfile: hpfc_check.f,v $ ($Date: 1994/05/13 14:48:29 $, )
+c $RCSfile: hpfc_check.f,v $ ($Date: 1994/08/29 13:56:27 $, )
 c version $Revision$
 c got on %D%, %T%
 c $Id$
 c
 c
       subroutine hpfc_check()
-      include 'parameters.h'
+      include 'real_parameters.h'
       include 'hpfc_commons.h'
       if  (REALNBOFARRAYS.GT.MAXNBOFARRAYS.OR.
      $     REALNBOFTEMPLATES.GT.MAXNBOFTEMPLATES.OR.
@@ -16,7 +16,7 @@ c
      $     REALMAXSIZEOFPROCS.GT.MAXMAXSIZEOFPROCS) then
          print *, 
      $        'HPFC run-time library',
-     $        '  must recompiled with larger parameters'
+     $        '  must be recompiled with larger parameters'
          stop
       endif
 c
