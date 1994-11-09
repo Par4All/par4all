e0d347838042d986b8d2e2960efa56be6d6e6160
pips <pips@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-11-09 12:15:06 +0000

    # on 1994/11/09 12:15:06, pips did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@643 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/pip/sol.c
@@ -0,0 +1,233 @@
+#include "type.h"
+
+#include <stdio.h>
+
+extern long int cross_product, limit;
+extern int verbose;
+extern FILE *dump;
+
+struct S
+    {int flags;
+     Entier param1, param2;
+    };
+
+#define Nil  1
+#define If   2
+#define List 3
+#define Form 4
+#define New  5
+#define Div  6
+#define Val  7
+
+struct S sol_space[SOL_SIZE];
+static int sol_free;
+
+Entier mod();
+
+Entier pgcd(x,y)
+Entier x, y;
+{Entier r;
+ while(y)
+     {r = mod(x, y);
+      x = y;
+      y = r;
+     }
+ return(x>= 0? x : -x);
+}
+
+void sol_init()
+{
+ sol_free = 0;
+}
+
+int sol_hwm()
+{
+ return(sol_free);
+}
+
+void sol_reset(p)
+int p;
+{
+ if(p<0 || p>=SOL_SIZE)
+     {fprintf(stderr, "salades de saison .... : sol\n");
+      exit(40);
+     }
+ sol_free = p;
+}
+
+struct S *sol_alloc()
+{struct S *r;
+ r = sol_space + sol_free;
+ sol_free++;
+ if(sol_free >= SOL_SIZE)
+     {fprintf(stderr, "grosse solution!!! : sol\n");
+      exit(26);
+     }
+ return(r);
+}
+
+void sol_nil()
+{
+ struct S * r;
+ r = sol_alloc();
+ r -> flags = Nil;
+ if(verbose > 0)
+   {fprintf(dump, "sol_nil\n");
+    fflush(dump);
+  }
+}
+
+int is_not_Nil(p)
+int p;
+{
+ return(sol_space[p].flags != Nil);
+}
+
+void sol_if()
+{
+ struct S *r;
+ r = sol_alloc();
+ r -> flags = If;
+}
+
+void sol_list(n)
+int n;
+{struct S * r;
+ r = sol_alloc();
+ r->flags = List;
+ r->param1 = n;
+}
+
+void sol_forme(l)
+int l;
+{
+ struct S *r;
+ r = sol_alloc();
+ r -> flags = Form;
+ r -> param1 = l;
+}
+
+void sol_new(k)
+int k;
+{
+ struct S *r;
+ r = sol_alloc();
+ r -> flags = New;
+ r -> param1 = k;
+}
+
+void sol_div()
+{
+ struct S *r;
+ r = sol_alloc();
+ r -> flags = Div;
+}
+
+void sol_val(n, d)
+Entier n, d;
+{
+ struct S *r;
+ r = sol_alloc();
+ r -> flags = Val;
+ r -> param1 = n;
+ r -> param2 = d;
+}
+
+int sol_edit(foo, i)
+FILE *foo;
+int i;
+{int j, n;
+ struct S *p;
+ Entier N, D, d;
+ debut : p = sol_space + i;
+ switch(p->flags)
+    {case Nil : fprintf(foo, "()\n");
+                if(verbose>0)fprintf(dump, "()\n");
+                i++; break;
+     case If  : fprintf(foo, "(if ");
+                if(verbose>0)fprintf(dump, "(if ");
+                i = sol_edit(foo, ++i);
+		i = sol_edit(foo, i);
+		i = sol_edit(foo, i);
+		fprintf(foo, ")\n");
+                if(verbose>0)fprintf(dump, ")\n");
+		break;
+     case List: fprintf(foo, "(list ");
+                if(verbose>0)fprintf(dump, "(list ");
+                n = p->param1;
+                i++;
+                while(n--) i = sol_edit(foo, i);
+		fprintf(foo, ")\n");
+                if(verbose>0)fprintf(dump, ")\n");
+		break;
+     case Form: fprintf(foo, "#[");
+                if(verbose>0)fprintf(dump, "#[");
+                n = p->param1;
+                for(j = 0; j<n; j++)
+                   {i++; p++;
+                    N = p->param1; D = p->param2;
+                    d = pgcd(N, D);
+	            if(d == D){putc(' ', foo);
+		               fprintf(foo, FORMAT, N/d);
+			       if(verbose>0){
+				 putc(' ', dump);
+				 fprintf(dump, FORMAT, N/d);
+			       }
+			      }
+                    else{putc(' ', foo);
+		         fprintf(foo,FORMAT,N/d);
+		         fprintf(foo,"/");
+			 fprintf(foo,FORMAT, D/d);
+			 if(verbose>0)
+			   {putc(' ', dump);
+			    fprintf(dump,FORMAT,N/d);
+			    fprintf(dump,"/");
+			    fprintf(dump,FORMAT, D/d);
+			  }
+			}
+                   }
+		fprintf(foo, "]\n");
+	        if(verbose>0)fprintf(dump, "]\n");
+		i++;
+		break;
+     case New : n = p->param1;
+                fprintf(foo, "(newparm %d ", n);
+		if(verbose>0)fprintf(dump, "(newparm %d ", n);
+                i = sol_edit(foo, ++i);
+		fprintf(foo, ")\n");
+		if(verbose>0)fprintf(dump, ")\n");
+		goto debut;
+     case Div : fprintf(foo, "(div ");
+	        if(verbose>0)fprintf(dump, "(div ");
+                i = sol_edit(foo, ++i);
+		i = sol_edit(foo, i);
+		fprintf(foo, ")\n");
+		if(verbose>0)fprintf(dump, ")\n");
+		break;
+     case Val : N = p->param1; D = p->param2;
+                d = pgcd(N, D);
+		if(d == D){putc(' ', foo);
+		           fprintf(foo, FORMAT, N/d);
+			   if(verbose>0)
+			     {putc(' ', dump);
+			      fprintf(dump, FORMAT, N/d);
+			    }
+			  }
+		else{putc(' ', foo);
+		     fprintf(foo, FORMAT, N/d);
+		     fprintf(foo, "/");
+		     fprintf(foo, FORMAT, D/d);
+		     if(verbose>0)
+		       {putc(' ', dump);
+			fprintf(dump, FORMAT, N/d);
+			fprintf(dump, "/");
+			fprintf(dump, FORMAT, D/d);
+		      }
+		    }
+		i++;
+		break;
+     default  : fprintf(foo, "Inconnu : sol\n");
+		if(verbose>0)fprintf(dump, "Inconnu : sol\n");
+    }
+    return(i);
+}
