ce31dc662e7d40c5cf0b39ab20abb88094766ef7
pips <pips@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-11-09 12:14:57 +0000

    # on 1994/11/09 12:14:57, pips did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@634 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/pip/pip-local.h
@@ -0,0 +1,22 @@
+
+#define PIP_SOLVE_MIN 0
+#define PIP_SOLVE_MAX 1
+#define PIP_SOLVE_INTEGER 1
+#define PIP_SOLVE_RATIONAL 0
+
+#define DFG_MODULE_NAME "DFG"
+#define MAPPING_MODULE_NAME "MAPPING"
+#define DIFF_PREFIX "DIFF"
+#define COEFF_PREFIX "COEFF"
+
+#define POSITIVE 1
+#define NEGATIVE 0
+
+#define ADD_ELEMENT_TO_LIST( _list, _type, _element) \
+    (_list = gen_nconc( _list, CONS( _type, _element, NIL)))
+
+/* PIP includes */
+#include "type.h"
+#include "sol.h"
+#include "tab.h"
+
