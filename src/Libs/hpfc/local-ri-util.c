5be76c8b6a6894fc612741946c3daa04c7dbeb83
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-11-17 14:19:13 +0000

    # on 1994/11/17 14:19:13, coelho did: ++
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@682 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/hpfc/local-ri-util.c
+++ src/Libs/hpfc/local-ri-util.c
@@ -1,6 +1,7 @@
-
-/*
- * What could be in ri-util, or even in ri.h!
+/* SCCS Stuff:
+ * $RCSfile: local-ri-util.c,v $ ($Date: 1994/11/17 14:19:13 $, )
+ * version $Revision$
+ * got on %D%, %T%
  *
  * Fabien Coelho May 1993
  */
@@ -39,75 +40,6 @@ type t;
     }
     else
 	return(t); /* !!! means sharing */
-}
-
-/*
- * perfectly_nested_parallel_loop_to_body
- *
- * FC 930722: modified from perfectly_nested_loop_to_body, ri-util,
- */
-statement perfectly_nested_parallel_loop_to_body(loop_nest, plloop)
-statement loop_nest;
-list *plloop;
-{
-    instruction 
-	ins = statement_instruction(loop_nest);
-
-    switch(instruction_tag(ins)) 
-    {
-    case is_instruction_block: 
-    {
-	list 
-	    lb = instruction_block(ins);
-	statement 
-	    first_s = STATEMENT(CAR(lb));
-	instruction 
-	    first_i = statement_instruction(first_s);
-	
-	if (instruction_loop_p(first_i) && (gen_length(lb)==1)) /* perfect ? */
-	{
-	    if (execution_parallel_p(loop_execution(instruction_loop(first_i))))
-	    {
-		*plloop = gen_nconc(*plloop, 
-			 CONS(LOOP, instruction_loop(first_i), 
-				    NIL));
-		return(perfectly_nested_parallel_loop_to_body(first_s, plloop));
-	    }
-	    else 
-		return(loop_nest);
-	}
-	else
-	    return(loop_nest);
-
-	break;
-    }
-    case is_instruction_loop: 
-    {
-	statement 
-	    sbody = loop_body(instruction_loop(ins));
-
-	if (execution_parallel_p(loop_execution(instruction_loop(ins))))
-	{
-	    *plloop = gen_nconc(*plloop, 
-		     CONS(LOOP, instruction_loop(ins), 
-			  NIL));
-	    return(perfectly_nested_parallel_loop_to_body(sbody, plloop));
-	}
-	else
-	    return(loop_nest);
-	break;
-    }
-    case is_instruction_test:
-    case is_instruction_call:
-    case is_instruction_unstructured:
-    case is_instruction_goto:
-	return(loop_nest);
-    default:
-	pips_error("perfectly_nested_parallel_loop_to_body", "illegal tag\n");
-	break;
-    }
-    
-    return(loop_nest); /* just to avoid a gcc warning */
 }
 
 /*
