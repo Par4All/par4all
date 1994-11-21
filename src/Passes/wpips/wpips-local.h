e042fedf0f5934edd53f90ab08b41bd97d7e5922
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1994-11-21 18:48:34 +0000

    # on 1994/11/21 18:48:34, keryell did: ++
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@696 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/wpips-local.h
+++ src/Passes/wpips/wpips-local.h
@@ -38,12 +38,17 @@
 #define MESSAGE_BUFFER_LENGTH 128
 #define TEXT_BUFFER_LENGTH 1024
 
+/* How many display wondows can be opened : */
+#define MAX_NUMBER_OF_WPIPS_WINDOWS 9
+#define INITIAL_NUMBER_OF_WPIPS_WINDOWS 2
+extern int number_of_wpips_windows;
+
 
 extern Frame main_frame, 
     schoose_frame, 
     mchoose_frame, 
     log_frame, 
-    edit_frame[2], 
+    edit_frame[MAX_NUMBER_OF_WPIPS_WINDOWS], 
     help_frame, 
     query_frame,
 	properties_frame;
@@ -54,6 +59,9 @@ extern Panel main_panel,
     mchoose_panel,
     schoose_panel,
     help_panel;
+
+typedef enum {PIPS_ICON, ICFG_ICON, WP65_PE_ICON, WP65_bank_ICON, callgraph_ICON,
+		parallel_ICON, sequential_ICON, user_ICON, LAST_ICON} icon_list;
 
 
 extern char *strdup(), *re_comp();
