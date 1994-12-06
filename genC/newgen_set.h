8fe92b9e1cdc8298fb161ba9a4a3251579e6b0b9
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1994-12-06 14:13:26 +0000

    # on 1994/12/06 14:13:26, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@26 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

---
+++ genC/newgen_set.h
@@ -0,0 +1,64 @@
+/*
+
+	-- NewGen Project
+
+	The NewGen software has been designed by Remi Triolet and Pierre
+	Jouvelot (Ecole des Mines de Paris). This prototype implementation
+	has been written by Pierre Jouvelot.
+
+	This software is provided as is, and no guarantee whatsoever is
+	provided regarding its appropriate behavior. Any request or comment
+	should be sent to newgen@isatis.ensmp.fr.
+
+	(C) Copyright Ecole des Mines de Paris, 1989
+
+*/
+
+
+#ifndef SET_INCLUDED
+#define SET_INCLUDED
+/* - set.h
+   
+   Pierre Jouvelot (3 Avril 1989)
+
+   Set (of CHAR *) package interface. 
+
+   WARNING: You aren't allowed to use = or == between sets. Always use 
+   SET_ASSIGN and SET_EQUAL. */
+
+#include "newgen_types.h"
+#include "newgen_hash.h"
+
+typedef enum {
+    set_string = hash_string ,
+    set_int = hash_int,
+    set_pointer = hash_pointer
+} set_type ;
+
+typedef struct set {
+    hash_table table ;
+    set_type type ;
+} *set ;
+
+#define set_undefined ((struct set *)(-16))
+#define set_undefined_p(s) ((s)==set_undefined)
+
+#define SET_MAP(element,code,set) \
+    { HASH_MAP(_set_map_key, element, code, (set)->table); }
+
+/* functions declared in set.c */
+
+extern set set_add_element GEN_PROTO (( set, set, char *)) ;
+extern set set_del_element GEN_PROTO(( set, set, char *)) ;
+extern set set_assign GEN_PROTO(( set, set )) ;
+extern bool set_belong_p GEN_PROTO(( set, char *)) ;
+extern void set_clear GEN_PROTO(( set )) ;
+extern set set_difference GEN_PROTO(( set, set, set )) ;
+extern bool set_equal GEN_PROTO(( set, set )) ;
+extern void set_free GEN_PROTO(( set )) ;
+extern set set_intersection GEN_PROTO(( set, set, set )) ;
+extern set set_make GEN_PROTO(( set_type )) ;
+extern set set_singleton GEN_PROTO(( set_type, char * )) ;
+extern set set_union GEN_PROTO(( set, set, set )) ;
+
+#endif
