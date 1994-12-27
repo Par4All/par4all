05201eda2c10dca311796ddf3e85f53461f75d92
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1994-12-27 08:35:17 +0000

    # on 1994/12/27 08:35:17, coelho did: quicker gen_recurse
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@38 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- genC/newgen_generic_stack.h
+++ genC/newgen_generic_stack.h
@@ -2,54 +2,68 @@
  * Fabien COELHO, 05/12/94
  */
 
-#define DEFINE_STATIC_STACK(name, type) \
+#define DEFINE_STACK(PREFIX, name, type) \
 \
 static gen_stack name##_stack = gen_stack_undefined; \
 \
-void make_##name##_stack() \
+PREFIX void make_##name##_stack() \
 {\
   assert(name##_stack==gen_stack_undefined);\
   name##_stack = gen_stack_make(type##_domain, 0);\
 }\
 \
-void free_##name##_stack() \
+PREFIX void free_##name##_stack() \
 {\
   gen_stack_free(name##_stack); \
   name##_stack = gen_stack_undefined; \
 }\
 \
-gen_stack get_##name##_stack() \
+PREFIX gen_stack get_##name##_stack() \
 {\
   return(name##_stack);\
 }\
 \
-void set_##name##_stack(s) \
+PREFIX void set_##name##_stack(s) \
 gen_stack s; \
 {\
   assert(name##_stack==gen_stack_undefined);\
   name##_stack = s;\
 }\
 \
-void reset_##name##_stack()\
+PREFIX void reset_##name##_stack()\
 {\
   name##_stack = gen_stack_undefined; \
 }\
 \
-void name##_push(i)\
+PREFIX void name##_push(i)\
 type i;\
 {\
   gen_push(i, name##_stack);\
 }\
+PREFIX type name##_replace(i)\
+type i;\
+{\
+  return(gen_replace(i, name##_stack));\
+}\
 \
-type name##_pop()\
+PREFIX type name##_pop()\
 {\
   return(gen_pop(name##_stack));\
 }\
 \
-type name##_head()\
+PREFIX type name##_head()\
 {\
   return(gen_head(name##_stack));\
+}\
+\
+PREFIX bool name##_empty_p()\
+{\
+  assert(name##_stack!=gen_stack_undefined);\
+  return(gen_stack_empty_p(name##_stack));\
 }
+
+#define DEFINE_LOCAL_STACK(name, type) DEFINE_STACK(static, name, type)
+#define DEFINE_GLOBAL_STACK(name, type) DEFINE_STACK(/**/, name, type)
 
 /*  That is all
  */
