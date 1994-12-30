8c7e5a86833237f310dfdaca0645f239401af99b
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1994-12-30 13:58:47 +0000

    # on 1994/12/30 13:58:47, coelho did: chunk -> gen_chunk
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@78 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- genC/newgen_stack.h
+++ genC/newgen_stack.h
@@ -1,11 +1,11 @@
 /*  STACK MANAGEMENT -- headers
  *
- *  - a stack is declared with type gen_stack
- *  - it is allocated with gen_stack_make(newgen domain, bulk size)
- *  - it is freed with gen_stack_free(stack)
- *  - gen_stack_size(stack) returns the size
- *  - gen_stack_empty_p(stack) tells whether the stack is empty or not
- *  - gen_push, pop, head, replace do what you may expect from them
+ *  - a stack is declared with type stack
+ *  - it is allocated with stack_make(newgen domain, bulk size)
+ *  - it is freed with stack_free(stack)
+ *  - stack_size(stack) returns the size
+ *  - stack_empty_p(stack) tells whether the stack is empty or not
+ *  - stack_{push,pop,head,replace} do what you may expect from them
  *
  *  Fabien COELHO 05/12/94
  */
@@ -13,41 +13,45 @@
 #ifndef STACK_INCLUDED
 #define STACK_INCLUDED
 
-typedef struct gen_stack_bulk
+/* the stack bulks, that is arrays containing the elements
+ */
+typedef struct _stack_bulk
 {
   int n_item;                 /* next available item in the bulk */
   int max_items;              /* the maximun number of items of this bulk */
-  chunk **items;              /* the items (only pointers at the moment) */
-  struct gen_stack_bulk *succ;/* the next bulk */
+  char **items;               /* the items (only pointers at the moment) */
+  struct _stack_bulk *succ;   /* the next bulk */
 }
-  gen_stack_bulk, *gen_stack_ptr;
+  _stack_bulk, *_stack_ptr;
 
+/*  the stack head
+ */
 typedef struct 
 {
   int size;
-  int type; /* as BASIC, LIST, EXTERNAL and so on */
+  int type; /* as BASIC, LIST, EXTERNAL, CHUNK and so on */
   int max_extent;
-  gen_stack_ptr stack;
-  gen_stack_ptr available;
+  _stack_ptr stack;
+  _stack_ptr available;
 }
-  gen_stack_head, *gen_stack;
+  _stack_head, *stack;
 
-#define GEN_STACK_NULL ((gen_stack) NULL)
-#define GEN_STACK_NULL_P(s) ((s)==GEN_STACK_NULL)
+#define STACK_NULL ((stack) NULL)
+#define STACK_NULL_P(s) ((s)==STACK_NULL)
 
-#define gen_stack_undefined  ((gen_stack)-14)
-#define gen_stack_undefined_p(s) ((s)==gen_stack_undefined)
+#define stack_undefined  ((stack)-14)
+#define stack_undefined_p(s) ((s)==stack_undefined)
 
-extern gen_stack gen_stack_make GEN_PROTO((int, int));
-extern void gen_stack_free GEN_PROTO((gen_stack));
+extern stack stack_make GEN_PROTO((int, int));
+extern void stack_free GEN_PROTO((stack));
 
-extern bool gen_stack_empty_p GEN_PROTO((gen_stack));
-extern int gen_stack_size GEN_PROTO((gen_stack));
+extern bool stack_empty_p GEN_PROTO((stack));
+extern int stack_size GEN_PROTO((stack));
 
-extern void gen_push GEN_PROTO((chunk*, gen_stack));
-extern chunk *gen_pop GEN_PROTO((gen_stack));
-extern chunk *gen_head GEN_PROTO((gen_stack));
-extern chunk *gen_replace GEN_PROTO((chunk*, gen_stack));
+extern void stack_push GEN_PROTO((char*, stack));
+extern char *stack_pop GEN_PROTO((stack));
+extern char *stack_head GEN_PROTO((stack));
+extern char *stack_replace GEN_PROTO((char*, stack));
 
 #endif
 
