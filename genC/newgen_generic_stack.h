a9d8e24899f8e0c729e5eb8894fc224dbbc29529
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1994-12-30 13:58:45 +0000

    # on 1994/12/30 13:58:45, coelho did: chunk -> gen_chunk
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@73 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- genC/newgen_generic_stack.h
+++ genC/newgen_generic_stack.h
@@ -4,75 +4,73 @@
 
 #define DEFINE_STACK(PREFIX, name, type) \
 \
-static gen_stack name##_stack = gen_stack_undefined; \
+static stack name##_stack = stack_undefined; \
 \
 PREFIX void make_##name##_stack() \
 {\
-  assert(name##_stack==gen_stack_undefined);\
-  name##_stack = gen_stack_make(type##_domain, 0);\
+  assert(name##_stack==stack_undefined);\
+  name##_stack = stack_make(type##_domain, 0);\
 }\
 \
 PREFIX void free_##name##_stack() \
 {\
-  gen_stack_free(name##_stack); \
-  name##_stack = gen_stack_undefined; \
+  stack_free(name##_stack); \
+  name##_stack = stack_undefined; \
 }\
 \
-PREFIX gen_stack get_##name##_stack() \
+PREFIX stack get_##name##_stack() \
 {\
   return(name##_stack);\
 }\
 \
 PREFIX void set_##name##_stack(s) \
-gen_stack s; \
+stack s; \
 {\
-  assert(name##_stack==gen_stack_undefined);\
+  assert(name##_stack==stack_undefined);\
   name##_stack = s;\
 }\
 \
 PREFIX void reset_##name##_stack()\
 {\
-  name##_stack = gen_stack_undefined; \
+  name##_stack = stack_undefined; \
 }\
 \
 PREFIX void name##_push(i)\
 type i;\
 {\
-  gen_push(i, name##_stack);\
+  stack_push(i, name##_stack);\
 }\
 PREFIX type name##_replace(i)\
 type i;\
 {\
-  return(gen_replace(i, name##_stack));\
+  return(stack_replace(i, name##_stack));\
 }\
 \
 PREFIX type name##_pop()\
 {\
-  return(gen_pop(name##_stack));\
+  return(stack_pop(name##_stack));\
 }\
 \
 PREFIX type name##_head()\
 {\
-  return(gen_head(name##_stack));\
+  return(stack_head(name##_stack));\
 }\
 \
 PREFIX bool name##_empty_p()\
 {\
-  assert(name##_stack!=gen_stack_undefined);\
-  return(gen_stack_empty_p(name##_stack));\
+  return(stack_empty_p(name##_stack));\
 }\
 \
 PREFIX int name##_size()\
 {\
-  assert(name##_stack!=gen_stack_undefined);\
-  return(gen_stack_size(name##_stack));\
+  return(stack_size(name##_stack));\
 }\
 \
-static void check_##name()\
+static void check_##name##_stack()\
 {\
-  gen_stack s = get_##name##_stack();\
+  stack s = get_##name##_stack();\
   chunk\
-     *item_1 = (chunk *) check_##name,\
+     *item_1 = (chunk *) check_##name##_stack,\
      *item_2 = (chunk *) get_##name##_stack;\
   \
   reset_##name##_stack();\
