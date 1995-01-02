be392fd028fb6e58f2a3fe9f0576d67c414a0b9a
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1995-01-02 12:49:37 +0000

    # on 1995/01/02 12:49:37, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@57 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/sc/sc_oppose.c
@@ -0,0 +1,37 @@
+ /* package sc */
+
+#include <stdio.h>
+#include <string.h>
+extern   int fprintf();
+#include "boolean.h"
+#include "arithmetique.h"
+#include "vecteur.h"
+#include "contrainte.h"
+#include "sc.h"
+
+/* Psysteme sc_oppose(Psysteme ps):
+ * calcul pour un systeme de contraintes sans egalites du systeme de
+ * contraintes dont les inegalites sont les negations des inegalites
+ * originelles; attention, cela ne calcule pas le polyedre complementaire!
+ *
+ * Un systeme non trivial risque fort d'etre transforme en systeme non
+ * faisable.
+ *
+ * pour chaque inegalite donnee AX <= B, on construit une inegalites "opposee"
+ * AX > B ie -AX <= -B
+ */
+Psysteme sc_oppose(ps)
+Psysteme ps;
+{
+    Pcontrainte eq;
+
+    if (ps->nb_eq != 0) {
+	(void) fprintf(stderr,"sc_oppose: systeme contenant des egalites\n");
+	abort();
+    }
+
+    for (eq = ps->inegalites; eq != (Pcontrainte )NULL; eq = eq->succ)
+	(void) vect_multiply(eq->vecteur,-1);
+
+    return(ps);
+}
