bd242a393ad2dd7f8d6499e2c1d345688d178c58
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1995-01-02 11:46:44 +0000

    # on 1995/01/02 11:46:44, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@15 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/arithmetique/abs.c
@@ -0,0 +1,45 @@
+ /* package arithmetique */
+
+/*LINTLIBRARY*/
+
+#include <stdio.h>
+extern int fprintf();
+#include <values.h>
+#include <setjmp.h>
+
+#include "arithmetique.h"
+#include "assert.h"
+
+/* int absval(int i): absolute value of i (SUN version)
+ */
+int absval(i)
+int	i;
+{
+    return( abs_ofl_ctrl(i, 0));
+}
+
+/* int absval_ofl(int i): absolute value of i (SUN version)
+ * Overflow control is made and returns to the last setjmp(overflow_error). 
+ */
+int absval_ofl(i)
+int	i;
+{
+    return( abs_ofl_ctrl(i, 1));
+
+}
+
+
+int abs_ofl_ctrl(i, ofl_ctrl)
+int i;
+int ofl_ctrl;
+{
+
+    extern jmp_buf overflow_error;
+    
+    if ((ofl_ctrl == 1) && (i == -MAXINT-1))   
+	longjmp(overflow_error, 5);
+        
+    assert(i != -MAXINT-1);
+    return (i>0) ? i: -i;    
+    
+}
