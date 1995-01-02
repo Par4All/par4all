07483cf79997f456bca3aada142541f2e2142e94
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1995-01-02 11:49:41 +0000

    # on 1995/01/02 11:49:41, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@24 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/contrainte/error.c
@@ -0,0 +1,38 @@
+ /* package contrainte */
+
+/*LINTLIBRARY*/
+
+#include <stdio.h>
+#include <varargs.h>
+	
+/* void contrainte_error(va_dcl va_alist) should be called to terminate
+ * execution and to core dump when data structures are corrupted or when
+ * an undefined operation is requested (zero divide for instance).
+ * CONTRAINTE_ERROR should be called as:
+ * 
+ *   CONTRAINTE_ERROR(function_name, format, expression-list)
+ * 
+ * where function_name is a string containing the name of the function
+ * calling CONTRAINTE_ERROR, and where format and expression-list are passed as
+ * arguments to vprintf. CONTRAINTE_ERROR terminates execution with abort.
+ */
+/*VARARGS0*/
+void contrainte_error(va_alist)
+va_dcl
+{
+    va_list args;
+    char *fmt;
+
+    va_start(args);
+
+    /* print name of function causing error */
+    (void) fprintf(stderr, "contrainte error in %s: ", va_arg(args, char *));
+    fmt = va_arg(args, char *);
+
+    /* print out remainder of message */
+    (void) vfprintf(stderr, fmt, args);
+    va_end(args);
+
+    /* create a core file for debug */
+    (void) abort();
+}
