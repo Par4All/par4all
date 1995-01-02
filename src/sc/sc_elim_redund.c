a90f2d30c9a5f05405f5a0c9e3c91cdd3ff45de2
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1995-01-02 12:07:04 +0000

    # on 1995/01/02 12:07:04, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@54 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/sc/sc_elim_redund.c
@@ -0,0 +1,86 @@
+ /* package sc */
+
+#include <stdio.h>
+#include <string.h>
+#include <setjmp.h>
+#include "arithmetique.h"
+#include "boolean.h"
+#include "vecteur.h"
+#include "contrainte.h"
+#include "sc.h"
+
+
+
+/* Psysteme sc_elim_redund(Psysteme ps):
+ * elimination des contraintes lineaires redondantes dans le systeme par test
+ * de faisabilite
+ *
+ * Remarque: il ne faut pas appliquer de normalisation du systeme apres
+ *  inversion de la contrainte et avant le test de faisabilite en RATIONELS, 
+ * car l'elimination des redondances n'est alors pas  necessairement 
+ * correcte en entiers.
+ *
+ *  resultat retourne par la fonction :
+ *
+ *  Psysteme	    : Le systeme initial est modifie. Il est egal a NULL si 
+ *		      le systeme initial est non faisable.
+ *
+ *  Les parametres de la fonction :
+ *
+ *  Psysteme ps    : systeme lineaire 
+ *
+ */
+
+Psysteme sc_inequations_elim_redund(ps)
+Psysteme ps;
+{
+    Pcontrainte eq, eq1;
+
+    for (eq = ps->inegalites;eq != NULL; eq = eq1) {
+	eq1 = eq->succ;
+	contrainte_reverse(eq);	
+	if (sc_rational_feasibility_ofl_ctrl(ps, OFL_CTRL,TRUE))
+	    contrainte_reverse(eq);
+	else  { 
+	    eq_set_vect_nul(eq);
+	    sc_rm_empty_constraints(ps,0);
+	}
+    }
+   
+    return(ps);
+}
+
+/* Psysteme sc_elim_redund(Psysteme ps):
+ * elimination des contraintes lineaires redondantes dans le systeme par test
+ * de faisabilite. Les tests de faisabilite sont appliques sur tout le 
+ * systeme. L'elimination des redondances est donc totale.
+ *
+ *  resultat retourne par la fonction :
+ *
+ *  Psysteme	    : Le systeme initial est modifie. Il est egal a NULL si 
+ *		      le systeme initial est non faisable.
+ *
+ *  Les parametres de la fonction :
+ *
+ *  Psysteme ps    : systeme lineaire 
+ *
+ */
+
+Psysteme sc_elim_redund(ps)
+Psysteme ps;
+{
+    Pcontrainte eq;
+  
+    for (eq = ps->egalites; eq != NULL; eq=eq->succ)
+	vect_normalize(eq->vecteur);
+    ps = sc_kill_db_eg(ps);
+
+    if (SC_UNDEFINED_P(ps)) return(ps);
+    if (!sc_rational_feasibility_ofl_ctrl(ps,OFL_CTRL,TRUE))
+    {
+	sc_rm(ps);
+	return(NULL);
+    }
+    return(sc_inequations_elim_redund(ps));
+}
+
