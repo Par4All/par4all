85a256fdab01bc8d2610684c8a624864b88da6dd
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1995-02-03 09:49:00 +0000

    # on 1995/02/03 09:49:00, coelho did: ++
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@101 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- genC/newgen_stack.h
+++ genC/newgen_stack.h
@@ -1,7 +1,7 @@
 /*  STACK MANAGEMENT -- headers
  *  
  * $RCSfile: newgen_stack.h,v $ version $Revision$
- * $Date: 1995/02/02 18:10:38 $, 
+ * $Date: 1995/02/03 09:49:00 $, 
  * got on %D%, %T%
  *
  *  - a stack is declared with type stack (internals not visible from here!)
@@ -13,7 +13,7 @@
  *  - stack_{push,pop,head,replace} do what you may expect from them
  *  - stack_info gives informations about the stack
  *  - stack_map applies the function on all the items in stack.
- *  - stack_iterator_{init,next,end_p,clean} to iterate.
+ *  - stack_iterator_{init,next_and_go,end} to iterate.
  *  - see STACK_MAP for instance.
  *
  *  Fabien COELHO 05/12/94
@@ -21,6 +21,12 @@
 
 #ifndef STACK_INCLUDED
 #define STACK_INCLUDED
+
+#ifdef __STRICT_ANSI__
+#define _PROTO(x) x
+#else
+#define _PROTO(x) ()
+#endif
 
 /*  encapsulated types
  */
@@ -37,24 +43,36 @@ typedef struct __stack_iterator *stack_iterator;
 #define stack_undefined  ((stack)-14)
 #define stack_undefined_p(s) ((s)==stack_undefined)
 
+#define STACK_CHECK(s)\
+  message_assert("stack null or undefined",\
+                 !STACK_NULL_P(s) && !stack_undefined_p(s))
+
 /*   allocation
  */
-extern stack stack_make GEN_PROTO((int, int)); /* type, size */
-extern void stack_free GEN_PROTO((stack));
+extern stack stack_make _PROTO((int, int, int)); /* type, bulk_size, policy */
+extern void stack_free _PROTO((stack));
+
+/*   observers
+ */
+extern int stack_size _PROTO((stack));
+extern int stack_type _PROTO((stack));
+extern int stack_bulk_size _PROTO((stack));
+extern int stack_policy _PROTO((stack));
+extern int stack_max_extent _PROTO((stack));
 
 /*   miscellaneous
  */
-extern void stack_map GEN_PROTO((stack, void(*)()));
-extern int stack_empty_p GEN_PROTO((stack));
-extern int stack_size GEN_PROTO((stack));
-extern void stack_info GEN_PROTO((FILE*, stack));
+extern int stack_consistent_p _PROTO((stack));
+extern int stack_empty_p _PROTO((stack));
+extern void stack_info _PROTO((FILE*, stack));
+extern void stack_map _PROTO((stack, void(*)()));
 
 /*   stack use
  */
-extern void stack_push GEN_PROTO((char*, stack));
-extern char *stack_pop GEN_PROTO((stack));
-extern char *stack_head GEN_PROTO((stack));
-extern char *stack_replace GEN_PROTO((char*, stack));
+extern void stack_push _PROTO((char*, stack));
+extern char *stack_pop _PROTO((stack));
+extern char *stack_head _PROTO((stack));
+extern char *stack_replace _PROTO((char*, stack));
 
 /*   stack iterator
  *
@@ -62,25 +80,22 @@ extern char *stack_replace GEN_PROTO((char*, stack));
  *   it is not very efficient, due to the many function calls.
  *   Consider gen_map first which has a very small overhead.
  */
-extern stack_iterator stack_iterator_init GEN_PROTO((stack));
-extern char *stack_iterator_next GEN_PROTO((stack_iterator));
-extern int stack_iterator_end_p GEN_PROTO((stack_iterator));
-extern void stack_iterator_clean GEN_PROTO((stack_iterator*));
+extern stack_iterator stack_iterator_init _PROTO((stack, int)); /* X-ward */
+extern int stack_iterator_next_and_go _PROTO((stack_iterator, char**));
+extern void stack_iterator_end _PROTO((stack_iterator*));
+extern int stack_iterator_end_p _PROTO((stack_iterator)); /* not needed */
 
 /* applies _code on the items of _stack downward , with _item of _itemtype.
  */
 #define STACK_MAP(_item, _itemtype, _code, _stack) \
 {\
-    stack_iterator _i = stack_iterator_init(_stack);\
+    stack_iterator _i = stack_iterator_init(_stack, 1);\
     _itemtype _item;\
-    while(!(stack_iterator_end_p(_i)))\
-    {\
-	_item = (_itemtype) stack_iterator_next(_i);\
-	_code;\
-    }\
-    stack_iterator_clean(&_i);\
+    while(stack_iterator_next_and_go(_i, (char**)&_item)) _code;\
+    stack_iterator_end(&_i);\
 }
 
+#undef _PROTO
 #endif
 
 /*  That is all
