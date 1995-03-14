9f540d9a0e456fc1e5023ef5c1496fa04f1e5e40
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1995-03-14 18:12:23 +0000

    # on 1995/03/14 18:12:23, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@119 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- genC/newgen_generic_function.h
+++ genC/newgen_generic_function.h
@@ -1,4 +1,4 @@
-/* $RCSfile: newgen_generic_function.h,v $ ($Date: 1995/03/14 18:06:56 $, )
+/* $RCSfile: newgen_generic_function.h,v $ ($Date: 1995/03/14 18:12:23 $, )
  * version $Revision$
  * got on %D%, %T%
  */
@@ -17,15 +17,15 @@
 
 #define GENERIC_FUNCTION(PREFIX, name, type)\
 static type name;\
-PREFIX bool name##_undefined_p() { return(name==type##_undefined_p); }
-PREFIX void reset_##name(){ name = type##_undefined; }\
-PREFIX void set_##name(v) type v; { assert(name##_undefined_p()); name = v; }\
-PREFIX type get_##name() { return(name); }\
-PREFIX void init_##name() { name = make_##type(); }\
-PREFIX void close_##name() { free_##type(name); }\
-PREFIX void extend_##name(k,v) char *k,*v;{ extend_##type(name, k, v); }\
-PREFIX void update_##name(k,v) char *k,*v;{ update_##type(name, k, v); }\
-PREFIX char *apply_##name(k) char *k; { apply_##type(name, k); }
+PREFIX bool name##_undefined_p() { return(name==type##_undefined_p);}\
+PREFIX void reset_##name(){ name = type##_undefined;}\
+PREFIX void set_##name(v) type v; { assert(name##_undefined_p()); name = v;}\
+PREFIX type get_##name() { return(name);}\
+PREFIX void init_##name() { name = make_##type();}\
+PREFIX void close_##name() { free_##type(name);}\
+PREFIX void extend_##name(k,v) char *k,*v;{ extend_##type(name, k, v);}\
+PREFIX void update_##name(k,v) char *k,*v;{ update_##type(name, k, v);}\
+PREFIX char *apply_##name(k) char *k; { apply_##type(name, k);}
 
 #define GENERIC_LOCAL_FUNCTION(name, type)\
         GENERIC_FUNCTION(static, name, type)
