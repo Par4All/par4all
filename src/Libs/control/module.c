7e1b1706104d161b490e4596188bbc51ac2f0a5d
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-03-22 11:31:23 +0000

    # on 1995/03/22 11:31:23, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@922 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/control/module.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <strings.h>
+#include <string.h>
+extern int fprintf();
+
+#include "genC.h"
+#include "ri.h"
+#include "database.h"
+
+#include "ri-util.h"
+#include "control.h"
+#include "pipsdbm.h"
+
+#include "resources.h"
+#include "constants.h"
+
+/* interface with pipsdbm and pipsmake */
+
+void controlizer(module_name)
+string module_name;
+{
+   
+    statement module_stat;
+    
+    module_stat = 
+	    copy_statement((statement)
+			   db_get_memory_resource(DBR_PARSED_CODE, 
+						  module_name, 
+						  TRUE)) ;
+
+    /* *module_stat can be re-used because control_graph reallocates
+       statements; do not show that to any student! */
+    statement_instruction(module_stat) =
+	make_instruction(is_instruction_unstructured,
+			 control_graph(module_stat));
+    statement_ordering(module_stat) = MAKE_ORDERING(0,1) ;
+
+    DB_PUT_MEMORY_RESOURCE(DBR_CODE, 
+			   strdup(module_name), 
+			   module_stat);
+}
+
+
+
+
+
+
+
