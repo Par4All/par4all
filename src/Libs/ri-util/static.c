999f25ed8b8d6f38140ea7ce424d17a6599fc1e1
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-04-04 08:29:03 +0000

    # on 1995/04/04 08:29:03, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1018 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/ri-util/static.c
@@ -0,0 +1,78 @@
+/* static variables and related access functions concerning the current module
+ *
+ * Be'atrice Apvrille, august 27, 1993
+ */
+
+
+
+/* used to store the summary transformer ? 
+   to retrieve intraprocedural effects ? */
+
+#include <stdio.h>
+extern int fprintf();
+#include <string.h>
+
+#include "genC.h"
+
+#include "ri.h"
+#include "ri-util.h"
+#include "misc.h"
+
+static entity current_module_entity = entity_undefined;
+
+void set_current_module_entity(e)
+entity e;
+{
+    pips_assert("set_current_module_entity", entity_module_p(e));
+    /* FI: I should perform some kind of memorization for all static variables
+       including the value maps (especially them) */
+
+    pips_assert("set_current_module_entity", 
+		current_module_entity == entity_undefined); 
+
+    current_module_entity = e;
+}
+
+entity get_current_module_entity()
+{
+    return current_module_entity;
+}
+
+void reset_current_module_entity()
+{
+     current_module_entity = entity_undefined;
+}
+
+
+/* used to retrieve the intraprocedural effects of the current module */
+
+static statement current_module_statement = statement_undefined;
+
+void set_current_module_statement(s)
+statement s;
+{
+    pips_assert("set_current_module_statement", 
+		current_module_statement == statement_undefined);
+    current_module_statement = s;
+}
+
+
+statement get_current_module_statement()
+{
+    return current_module_statement;
+}
+
+
+void reset_current_module_statement()
+{
+    current_module_statement = statement_undefined;
+}
+
+
+
+
+
+
+
+
+
