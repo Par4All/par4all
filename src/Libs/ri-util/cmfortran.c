8fc792b7b5af4429431ad3a8be49e95ee993407b
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-04-04 08:28:57 +0000

    # on 1995/04/04 08:28:57, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1004 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/ri-util/cmfortran.c
@@ -0,0 +1,92 @@
+/* Prettyprinter for CM FORTRAN loops.
+
+   There are memory leaks here since a new expression is constructed.
+
+*/
+
+#include <stdio.h>
+extern int fprintf();
+#include <string.h>
+
+#include "genC.h"
+#include "list.h"
+#include "hash.h"
+#include "mapping.h"
+
+#include "text.h"
+#include "text-util.h"
+#include "ri.h"
+#include "misc.h"
+
+#include "ri-util.h"
+
+/*======================================================================*/
+cons *words_cmf_loop_range(obj)
+range obj;
+{
+    cons *pc;
+    call c = syntax_call(expression_syntax(range_increment(obj)));
+
+    pc = words_subexpression(range_lower(obj), 0);
+    pc = CHAIN_SWORD(pc,":");
+    pc = gen_nconc(pc, words_subexpression(range_upper(obj), 0));
+    if (/*  expression_constant_p(range_increment(obj)) && */
+	 strcmp( entity_local_name(call_function(c)), "1") == 0 )
+	return(pc);
+    pc = CHAIN_SWORD(pc,":");
+    pc = gen_nconc(pc, words_expression(range_increment(obj)));
+
+    return(pc);
+}
+
+
+/*======================================================================*/
+text text_loop_cmf(module, label, margin, obj, n, lr, lidx)
+     entity module;
+     string label;
+     int margin;
+     loop obj;
+     int n;
+     list lr, lidx;
+{
+  text result_text = text_undefined;
+  instruction i;
+  entity idx;
+  range r;
+
+  i = statement_instruction(loop_body(obj));
+  idx = loop_index(obj);
+  r = loop_range(obj);
+
+  lr = gen_nconc(lr, CONS(RANGE, r, NIL));
+  lidx = gen_nconc(lidx, CONS(ENTITY, idx, NIL));
+
+  if(!instruction_assign_p(i)) {
+    if(instruction_loop_p(i)) {
+      result_text = text_loop_cmf(module, label, margin,
+				  instruction_loop(i), n, lr, lidx);
+    }
+  }
+  else {
+    list pc, lli, llr;
+    unformatted u;
+    
+    pc = CHAIN_SWORD(NIL, "FORALL(");
+    for(lli = lidx, llr = lr; !ENDP(lli); POP(lli), POP(llr)) {
+      pc = CHAIN_SWORD(pc, entity_local_name(ENTITY(CAR(lli))));
+      pc = CHAIN_SWORD(pc, " = ");
+      pc = gen_nconc(pc, words_cmf_loop_range(RANGE(CAR(llr))));
+      if(CDR(lli) != NIL)
+	pc = CHAIN_SWORD(pc, ", ");
+    }
+    pc = CHAIN_SWORD(pc, ") ");
+    pc = gen_nconc(pc, words_call(instruction_call(i), 0));
+    u = make_unformatted(strdup(label), n, margin, pc) ;
+    result_text = make_text(CONS(SENTENCE,
+				 make_sentence(is_sentence_unformatted,
+					       u),
+				 NIL));
+  }
+  return(result_text);
+}
+
