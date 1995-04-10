9042ce7b7e491f2de734de402addea6695b65ed5
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-04-10 18:49:44 +0000

    # on 1995/04/10 18:49:44, coelho did: lighter includes
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1050 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/hpfc/hpfc-local.h
+++ src/Libs/hpfc/hpfc-local.h
@@ -1,23 +1,10 @@
 /* local definitions
  * 
- * $RCSfile: hpfc-local.h,v $ ($Date: 1995/03/27 16:22:35 $, )
- * version $Revision$,
+ * $RCSfile: hpfc-local.h,v $ ($Date: 1995/04/10 18:49:44 $, )
+ * version $Revision$
  */
-
-/* hmmm... shouldn't be necessary
- */
-#include <ctype.h>
-#include "genC.h"
-#include "ri.h"
-#include "text-util.h"
-#include "hpf.h"
-#include "hpf_private.h"
-#include "message.h"
 
 #define HPFC_PACKAGE "HPFC-PACKAGE"
-
-#define hpfc_warning \
-    if (!get_bool_property("HPFC_NO_WARNING")) user_warning
 
 extern entity /* in compiler.c */
     host_module,
