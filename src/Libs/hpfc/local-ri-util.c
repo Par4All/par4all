e0efe589c15697eaf9c0565ab86739014ee5f9b1
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-04-10 18:49:37 +0000

    # on 1995/04/10 18:49:37, coelho did: lighter includes
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1043 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/hpfc/local-ri-util.c
+++ src/Libs/hpfc/local-ri-util.c
@@ -1,29 +1,10 @@
-/* SCCS Stuff:
- * $RCSfile: local-ri-util.c,v $ ($Date: 1995/04/04 10:47:21 $, )
+/* $RCSfile: local-ri-util.c,v $ ($Date: 1995/04/10 18:49:37 $, )
  * version $Revision$
- * got on %D%, %T%
  *
- * Fabien Coelho May 1993
+ * HPFC (c) Fabien Coelho May 1993
  */
 
-
-#include <stdio.h>
-#include <string.h>
-
-extern int fprintf();
-
-#include "genC.h"
-
-#include "ri.h"
-#include "hpf.h"
-#include "hpf_private.h"
-
-#include "ri-util.h"
-#include "misc.h"
-#include "text-util.h"
-#include "hpfc.h"
 #include "defines-local.h"
-#include "properties.h"
 
 type type_variable_dup(t)
 type t;
@@ -57,7 +38,7 @@ entity e;
     for (s=fortran_library; *s!=(string) NULL; s++)
 	if (same_string_p(*s, name)) return(TRUE);
 
-    return(FALSE);
+    return(FALSE); /* else not found */
 }
 
 /* that is all
