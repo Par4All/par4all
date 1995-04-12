6867268252c232dfadd0d282545b4983c9687542
oget <oget@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1995-04-12 10:33:24 +0000

    # on 1995/04/12 10:33:24, oget did: complete hide of data structures and new HASH_MAP
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@163 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- genC/newgen_hash.h
+++ genC/newgen_hash.h
@@ -14,37 +14,36 @@
 
 */
 
-/* $RCSfile: newgen_hash.h,v $ ($Date: 1995/04/06 17:12:31 $, )
+/* $RCSfile: newgen_hash.h,v $ ($Date: 1995/04/12 10:33:24 $, )
  * version $Revision$
  * got on %D%, %T%
  */
 
 #ifndef HASH_INCLUDED
 #define HASH_INCLUDED
-#define HASH_DEFAULT_SIZE 17
 
-/* Some predefined values for the key 
- */
-#define HASH_ENTRY_FREE (char *) 0
-#define HASH_ENTRY_FREE_FOR_PUT (char *) -1
+#define HASH_DEFAULT_SIZE 17
 
 typedef enum hash_key_type { 
     hash_string, hash_int, hash_pointer, hash_chunk } hash_key_type;
 
-typedef struct hash_entry {
-    char *key;
-    char *val;
-} hash_entry, *hash_entry_pointer;
 
-typedef struct hash_table {
-    hash_key_type hash_type;
-    int hash_size;
-    int hash_entry_number;
-    int (*hash_rank)();
-    int (*hash_equal)();
-    hash_entry *hash_array;
-    int hash_size_limit;
-} *hash_table;
+/* Define hash_table structure which is hided.
+ * The only thing we know about it is that the entries are in an array
+ * pointed to by hash_table_array(htp), it contains hash_table_size(htp)
+ * elements. These elements can be read with hash_entry_val(...) or
+ * hash_entry_key(...). If the key is HASH_ENTRY_FREE or
+ * HASH_ENTRY_FREE_FOR_PUT then the slot is empty. 
+ */
+
+struct __hash_entry;
+typedef struct __hash_entry *hash_entry_pointer;
+
+struct __hash_table;
+typedef struct __hash_table *hash_table;
+
+/* Value of an undefined hash_table 
+ */
 
 #define hash_table_undefined ((hash_table)gen_chunk_undefined)  
 
@@ -54,20 +53,15 @@ typedef struct hash_table {
 
 #define HASH_UNDEFINED_VALUE ((char *) gen_chunk_undefined)
 
-#define hash_table_empty_p(htp) (hash_table_count(htp) == 0)
+#define hash_table_empty_p(htp) (hash_table_entry_count(htp) == 0)
 
 #define HASH_MAP(k,v,code,h) \
     {\
-    hash_table _map_hash_h = (h) ;\
-    register hash_entry_pointer _map_hash_p = hash_table_array(_map_hash_h) ;\
-    hash_entry_pointer _map_hash_end = \
-	    hash_table_array(_map_hash_h)+hash_table_size(_map_hash_h) ;\
-    for( ; _map_hash_p<_map_hash_end ; _map_hash_p++ ) { \
-	if( hash_entry_key(_map_hash_p) !=HASH_ENTRY_FREE && \
-            hash_entry_key(_map_hash_p) !=HASH_ENTRY_FREE_FOR_PUT) { \
-	    char *k = hash_entry_key(_map_hash_p) ; \
-	    char *v = hash_entry_val(_map_hash_p) ; \
-            code ; }}}
+    hash_table _map_hash_h = (h) ; \
+    register hash_entry_pointer _map_hash_p = NULL; \
+    char *k, *v; \
+    while ((_map_hash_p = hash_table_scan(_map_hash_h,_map_hash_p,&k,&v))) { \
+            code ; }}
 #endif
 
 /* Let's define a new version of
@@ -112,9 +106,16 @@ extern int hash_table_entry_count GEN_PROTO((hash_table));
 extern int hash_table_size GEN_PROTO((hash_table));
 extern hash_key_type hash_table_type GEN_PROTO((hash_table));
 extern hash_entry_pointer hash_table_array GEN_PROTO((hash_table));
+#if 0
 extern char *hash_entry_val GEN_PROTO((hash_entry_pointer));
 extern char *hash_entry_key GEN_PROTO((hash_entry_pointer));
-
+extern hash_entry_pointer hash_entry_succ GEN_PROTO((hash_entry_pointer));
+extern hash_entry_pointer hash_entry_number GEN_PROTO((hash_entry_pointer,int));
+#endif
+extern hash_entry_pointer hash_table_scan GEN_PROTO((hash_table,
+						     hash_entry_pointer,
+						     char **,
+						     char **));
 
 /*  that is all
  */
