de9a9dfd41746a296e02a715a4df7c9d3b20637e
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1995-04-21 10:19:43 +0000

    # on 1995/04/21 10:19:43, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@90 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/sc/sc_transformation.c
+++ src/sc/sc_transformation.c
@@ -13,30 +13,28 @@
 
 /* Transform the two constraints A.x <= b and -A.x <= -b of system sc into 
  * an equality  A.x == b.
-*/
-
+ */
 void sc_transform_ineg_in_eg(sc)
 Psysteme sc;
 {
-    Pcontrainte
-	pc1, pc2, pc1_succ;
-    boolean 
-	found;
+    Pcontrainte pc1, pc2, pc1_succ, c_tmp;
+    boolean found;
 
-    for (pc1 = sc->inegalites, pc1_succ = pc1==NULL ? NULL : pc1->succ; 
+    for (pc1 = sc->inegalites, pc1_succ = (pc1==NULL ? NULL : pc1->succ); 
 	 !CONTRAINTE_UNDEFINED_P(pc1);
-	 pc1 = pc1_succ, pc1_succ = pc1==NULL ? NULL : pc1->succ)
+	 pc1 = pc1_succ, pc1_succ = (pc1==NULL ? NULL : pc1->succ))
     {
 	for (pc2 = pc1->succ, found=FALSE; 
 	     !CONTRAINTE_UNDEFINED_P(pc2) && !found; 
 	     pc2 = pc2->succ)
 	{
-	    Pvecteur
-		pv = vect_add(pc1->vecteur, pc2->vecteur); 
+	    Pvecteur pv = vect_add(pc1->vecteur, pc2->vecteur); 
 	    
 	    if (VECTEUR_NUL_P(pv)) /* True if the constraints are opposed. */
 	    {
-		sc_add_eg(sc,contrainte_dup(pc1));
+		c_tmp = contrainte_dup(pc1);
+
+		sc_add_eg(sc, c_tmp);
 		eq_set_vect_nul(pc2);
 		eq_set_vect_nul(pc1);
 		found = TRUE;
@@ -76,5 +74,3 @@ Psysteme sc;
     sc = sc_elim_db_constraints(sc); 
     /* ??? humm, the result is *not* returned */
 }
-
-
