a08bf023e9859f2e34b65e48999bbe67927d19b8
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-05-11 11:15:37 +0000

    # on 1995/05/11 11:15:37, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1185 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/misc/dialogue.c
@@ -0,0 +1,39 @@
+/* 
+  these versions of functions 'dialogue', 'message', ... are used to replaced
+  XView versions when a batch version of pips is compiled.
+*/
+
+#include <stdio.h>
+#include <varargs.h>
+
+/*VARARGS0*/
+void dialogue(va_alist)
+va_dcl
+{
+    va_list args;
+    char *fmt;
+
+    va_start(args);
+
+    fmt = va_arg(args, char *);
+
+    (void) vfprintf(stderr, fmt, args);
+
+    va_end(args);
+}
+
+/*VARARGS0*/
+void show_message(va_alist)
+va_dcl
+{
+    va_list args;
+    char *fmt;
+
+    va_start(args);
+
+    fmt = va_arg(args, char *);
+
+    (void) vfprintf(stderr, fmt, args);
+
+    va_end(args);
+}
