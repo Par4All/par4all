84e0d01ab22bf458e171d3c67e485bd542cde845
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-05-11 11:14:36 +0000

    # on 1995/05/11 11:14:36, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1180 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/misc/misc-local.h
@@ -0,0 +1,40 @@
+#include <varargs.h>
+
+#define ifdebug(l) if(the_current_debug_level>=(l))
+
+/* a debug macro that generates automatically the function name if available.
+ */
+#ifdef __GNUC__
+#define pips_debug(level, format, args...)\
+ ifdebug(level) { fprintf(stderr, "[%s] ", __FUNCTION__); \
+		  fprintf(stderr, format, ##args); }
+#else
+#define pips_debug(level, args...) debug(level, "unknown", args)
+#endif
+
+#define same_string_p(s1, s2) (strcmp((s1), (s2)) == 0)
+
+/* Constant used to dimension arrays in wpips and pipsmake */
+#define ARGS_LENGTH 512
+
+/* MAXPATHLEN is defined in <sys/param.h> for SunOS... but not for all OS! */
+#ifndef MAXPATHLEN
+#define MAXPATHLEN 1024
+#endif
+
+extern char *re_comp();
+extern int re_exec();
+
+/*VARARGS2*/
+extern void debug();
+extern void pips_error();
+extern void user_warning();
+extern void user_log();
+extern void user_error();
+
+/*VARARGS0*/
+extern char * concatenate();
+
+extern void (* pips_log_handler)(char * fmt, va_list args);
+extern void (* pips_warning_handler)();
+extern void (* pips_error_handler)();
