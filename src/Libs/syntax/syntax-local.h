1c933edb2acd013f054d26749fd1670e0dae3040
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-06-07 13:22:37 +0000

    # on 1995/06/07 13:22:37, irigoin did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1205 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/syntax/syntax-local.h
@@ -0,0 +1,65 @@
+/*
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+
+#include "debug.h"
+#include "genC.h"
+#include "ri.h"
+#include "parser_private.h"
+
+#include "ri-util.h"
+#include "misc.h"
+*/
+
+/* Legal characters to start a comment line */
+#define START_COMMENT_LINE "CcDd*!"
+
+/* definition of extern variables. see comments and declarations in file
+parser.c */
+
+extern char *LogFN;
+extern FILE *LogFD;
+extern char *ModulesFN;
+extern char *CurrentFN;
+extern int debugging_level;
+/* extern entity CurrentFunction; */
+extern char *CurrentPackage;
+extern entity DynamicArea;
+extern entity StaticArea;
+extern cons * FormalParameters;
+extern char lab_I[];
+extern char FormatValue[];
+extern char sstext[]; /* interface between the scanner and the parser */
+extern int ssleng; /* the length of the string in sstext  */
+extern FILE * ssin; /* the file read in by the scanner */
+extern int line_b_I, line_e_I, line_b_C, line_e_C;
+extern char Comm[]; /* the current comment */
+extern char PrevComm[]; /* the previous comment */
+extern int iComm;
+extern int iPrevComm;
+
+
+/* definition of implementation dependent constants */
+
+#include "constants.h"
+#define HASH_SIZE 1013
+#define FORMATLENGTH (4096)
+#define LOCAL static
+
+#define abs(v) (((v) < 0) ? -(v) : (v))
+
+extern char * getenv();
+
+
+
+#define Warning(f,m) \
+(user_warning(f,"Warning between lines %d and %d\n%s\n",line_b_I,line_e_I,m) )
+
+#define FatalError(f,m) \
+(pips_error(f,"Fatal error between lines %d and %d\n%s\n",line_b_I,line_e_I,m))
+
+#define ParserError(f,m) \
+(user_error(f,"Parser error between lines %d and %d\n%s\n",line_b_I,line_e_I,m))
+
