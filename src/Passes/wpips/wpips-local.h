87a03484f2cbfde8ac809080e27bd6f213a9b395
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-07-21 14:22:26 +0000

    # on 1995/07/21 14:22:26, keryell did: properties_frame is now options_frame.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1319 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/wpips-local.h
+++ src/Passes/wpips/wpips-local.h
@@ -10,21 +10,22 @@
 extern int number_of_wpips_windows;
 
 
+/* If we are in the Emacs mode, the log_frame is no longer really used: */
 extern Frame main_frame, 
-    schoose_frame, 
-    mchoose_frame, 
-    log_frame, 
-    edit_frame[MAX_NUMBER_OF_WPIPS_WINDOWS], 
-    help_frame, 
-    query_frame,
-	properties_frame;
+   schoose_frame, 
+   mchoose_frame, 
+   log_frame, 
+   edit_frame[MAX_NUMBER_OF_WPIPS_WINDOWS], 
+   help_frame, 
+   query_frame,
+   options_frame;
 
 extern Panel main_panel,
-    status_panel,
-    query_panel,
-    mchoose_panel,
-    schoose_panel,
-    help_panel;
+   status_panel,
+   query_panel,
+   mchoose_panel,
+   schoose_panel,
+   help_panel;
 
 typedef enum {PIPS_ICON, ICFG_ICON, WP65_PE_ICON, WP65_bank_ICON, callgraph_ICON,
 		parallel_ICON, sequential_ICON, user_ICON, LAST_ICON} icon_list;
