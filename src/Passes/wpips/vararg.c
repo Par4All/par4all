54a5e11296e7c762533c24db5acce8bbe8f91d50
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-07-31 17:30:06 +0000

    # on 1995/07/31 17:30:06, keryell did: Now in ANSI-C. Should be merged in xv_log.c.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1391 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/vararg.c
+++ src/Passes/wpips/vararg.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
-#include <varargs.h>
-#include <setjmp.h>
+#include <stdarg.h>
+#include <string.h>
 
 #define SMALL_BUFFER_LENGTH 256
 
 /*VARARGS0*/
-void wpips_user_error(args)
-va_list * args;
+void
+wpips_user_error(char * calling_function_name,
+                 char * a_message_format,
+                 va_list * some_arguments)
 {
-    char *fmt;
-    char error_buffer[SMALL_BUFFER_LENGTH];
-    char * perror_buffer = &error_buffer[0];
-    extern jmp_buf pips_top_level;
+   char error_buffer[SMALL_BUFFER_LENGTH];
 
-    /* print name of function causing error */
-    (void) sprintf(perror_buffer, "user error in %s: ", 
-		    va_arg(* args, char *));
-    fmt = va_arg(* args, char *);
+   /* print name of function causing error */
+   (void) sprintf(error_buffer, "user error in %s: ", 
+                  calling_function_name);
 
-    /* print out remainder of message */
-    (void) vsprintf(perror_buffer+strlen(perror_buffer), fmt, *args);
-    /* va_end(args); */
+   /* print out remainder of message */
+   (void) vsprintf(error_buffer + strlen(error_buffer),
+                   a_message_format, *some_arguments);
 
-    wpips_user_error_message(error_buffer);
+   wpips_user_error_message(error_buffer);
 }
 
 
 /*VARARGS0*/
-void wpips_user_warning(args)
-va_list * args;
+void
+wpips_user_warning(char * calling_function_name,
+                   char * a_message_format,
+                   va_list * some_arguments)
 {
-    char *fmt;
-    char warning_buffer[SMALL_BUFFER_LENGTH];
+   char warning_buffer[SMALL_BUFFER_LENGTH];
 
-    /* print name of function causing warning */
-    (void) sprintf(warning_buffer, "user warning in %s: ", 
-		    va_arg(* args, char *));
-    fmt = va_arg(* args, char *);
+   /* print name of function causing warning */
+   (void) sprintf(warning_buffer, "user warning in %s: ", 
+                  calling_function_name);
 
-    /* print out remainder of message */
-    (void) vsprintf(warning_buffer+strlen(warning_buffer), fmt, *args);
-    /* va_end(args); */
+   /* print out remainder of message */
+   (void) vsprintf(warning_buffer+strlen(warning_buffer),
+                   a_message_format, *some_arguments);
 
-    wpips_user_warning_message(warning_buffer);
+   wpips_user_warning_message(warning_buffer);
 }
