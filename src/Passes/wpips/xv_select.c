e0730d4a6697853a3614aa25322886fde4df966e
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-07-31 17:35:59 +0000

    # on 1995/07/31 17:35:59, keryell did: Some stuff added for the main panel "Workspace:" and "Module:": menu generation procedures, select procedures and so on.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1396 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/xv_select.c
+++ src/Passes/wpips/xv_select.c
@@ -90,25 +90,25 @@ void start_directory_notify(menu, menu_item)
 
 static Menu_item create_pgm, open_pgm, close_pgm, module_item;
 
-void start_create_program_notify(menu, menu_item)
-Menu menu;
-Menu_item menu_item;
+void
+start_create_program_notify(Menu menu,
+                            Menu_item menu_item)
 {
-    /* It is not coherent to change directory while creating a program. 
-     * Here should the menu item be set to MENU_INACTIVE
-     */
-    start_query("Create Workspace", 
-		"Enter workspace name: ", 
-		"CreateWorkspace",
-		continue_create_program_notify,
-		/* Pas la peine de faire quelque chose si on appuie
-                   sur cancel : */
-		cancel_create_program_notify);
+   /* It is not coherent to change directory while creating a program. 
+    * Here should the menu item be set to MENU_INACTIVE
+    */
+   start_query("Create Workspace", 
+               "Enter workspace name: ", 
+               "CreateWorkspace",
+               continue_create_program_notify,
+               /* Pas la peine de faire quelque chose si on appuie
+                  sur cancel : */
+               cancel_create_program_notify);
 }
 
-success cancel_create_program_notify(item, event)
-Panel_item item;
-Event *event;
+void
+cancel_create_program_notify(Panel_item item,
+                             Event * event)
 {
   /* Re'tablit le droit d'ouvrir ou de cre'er un autre worspace : */
   xv_set(create_pgm, MENU_INACTIVE, FALSE, 0);
@@ -168,6 +168,7 @@ continue_create_program_notify(char * name)
 
          db_create_program(name);
          open_log_file();
+         display_memory_usage();
          mchoose("Create Workspace", 
                  fortran_list_length, fortran_list, 
                  end_create_program_notify);
@@ -189,30 +190,32 @@ end_create_program_notify(int * pargc, char * argv[])
 
    show_program();
    select_a_module_by_default();
+   display_memory_usage();
 }
 
 
-void end_open_program_notify(name)
-     string name;
+void
+end_open_program_notify(string name)
 {
-    schoose_close();
+   schoose_close();
 
-    /* Around a bug in schoose... */
-    if (db_get_current_program_name() != NULL
-	&& strcmp(db_get_current_program_name(), name) == 0)
+   /* Around a bug in schoose... */
+   if (db_get_current_program_name() != NULL
+       && strcmp(db_get_current_program_name(), name) == 0)
       return;
     
-    if ( open_program(name) ) {
+   if ( open_program(name) ) {
       open_log_file();
       xv_set(close_pgm, MENU_INACTIVE, FALSE, 0);
       xv_set(module_item, MENU_INACTIVE, FALSE, 0);
       show_program();
       select_a_module_by_default();
-    }
-    else {
+   }
+   else {
       xv_set(create_pgm, MENU_INACTIVE, FALSE, 0);
       xv_set(open_pgm, MENU_INACTIVE, FALSE, 0);
-    }
+   }
+   display_memory_usage();
 }
 
 void cancel_open_program_notify()
@@ -254,34 +257,108 @@ Menu_item menu_item;
 
 
 
-void close_program_notify(menu, menu_item)
-Menu menu;
-Menu_item menu_item;
+void
+close_program_notify(Menu menu,
+                     Menu_item menu_item)
 {
-    close_program();
+   close_program();
 
-    /* FI: according to cproto and gcc */
-    /* edit_close_notify(); */
-    edit_close_notify(menu, menu_item);
+   /* FI: according to cproto and gcc */
+   /* edit_close_notify(); */
+   edit_close_notify(menu, menu_item);
 
-    show_program();
-    show_module();
+   show_program();
+   show_module();
 
-    xv_set(create_pgm, MENU_INACTIVE, FALSE, 0);
-    xv_set(open_pgm, MENU_INACTIVE, FALSE, 0);
-    xv_set(close_pgm, MENU_INACTIVE, TRUE, 0);
+   xv_set(create_pgm, MENU_INACTIVE, FALSE, 0);
+   xv_set(open_pgm, MENU_INACTIVE, FALSE, 0);
+   xv_set(close_pgm, MENU_INACTIVE, TRUE, 0);
 
-    xv_set(module_item, MENU_INACTIVE, TRUE, 0);
-    hide_window(schoose_frame);
+   xv_set(module_item, MENU_INACTIVE, TRUE, 0);
+   hide_window(schoose_frame);
+   display_memory_usage();
 }
 
 
-void end_select_module_notify(name)
-string name;
+/* To be used with schoose_create_abbrev_menu_with_text from the main
+   panel: */
+void
+open_or_create_workspace(char * workspace_name)
 {
-    lazy_open_module(name);
+   int i;
+   char *program_list[ARGS_LENGTH];
+   int  program_list_length = 0;
 
-    show_module();
+   if (db_get_current_program_name() != NULL)
+      /* There is an open workspace: close it first: */
+      close_program_notify((Menu) NULL, (Menu_item) NULL);
+
+   /* To choose between open or create, look for the an existing
+      workspace with the same name: */
+   pips_get_program_list(&program_list_length, program_list);
+
+   for(i = 0; i < program_list_length; i++)
+      if (strcmp(workspace_name, program_list[i]) == 0) {
+         /* OK, the workspace exist, open it: */
+         end_open_program_notify(workspace_name);
+         return;
+      }
+   
+   /* The workspace does not exist, create it: */
+   (void) continue_create_program_notify(workspace_name);
+}
+
+
+/* To use with schoose_create_abbrev_menu_with_text: */
+Menu
+generate_workspace_menu()
+{
+   Menu menu;
+   int i;
+   char *program_list[ARGS_LENGTH];
+   int  program_list_length = 0;
+
+   pips_get_program_list(&program_list_length, program_list);
+
+   menu = xv_create(NULL, MENU,
+                    MENU_TITLE_ITEM, " Select in the workspace list: ",
+                    NULL);
+   if (program_list_length == 0) {
+      xv_set(menu, MENU_APPEND_ITEM,
+             xv_create(XV_NULL, MENUITEM,
+                       MENU_STRING,
+                       "* No workspace available in this directory *",
+                       MENU_RELEASE,
+                       MENU_INACTIVE, TRUE,
+                       NULL),
+             NULL);
+   }
+   else {
+      for(i = 0; i < program_list_length; i++)
+         xv_set(menu, MENU_APPEND_ITEM,
+                xv_create(XV_NULL, MENUITEM,
+                          MENU_STRING, strdup(program_list[i]),
+                          MENU_RELEASE,
+                          /* The strdup'ed string will also be
+                             freed when the menu is discarded: */
+                          MENU_RELEASE_IMAGE,
+                          NULL),
+                NULL);
+   }
+   args_free(&program_list_length,
+             program_list);
+   
+   return menu;
+}
+
+
+void
+end_select_module_notify(string name)
+{
+   lazy_open_module(name);
+
+   show_module();
+   display_memory_usage();
 }
 
 void cancel_select_module_notify()
@@ -316,6 +393,60 @@ Menu_item menu_item;
     args_free(&module_list_length, module_list);
 }
 
+
+/* To use with schoose_create_abbrev_menu_with_text: */
+Menu
+generate_module_menu()
+{
+   Menu menu;
+   int i;
+   char *module_list[ARGS_LENGTH];
+   int  module_list_length = 0;
+
+   menu = xv_create(NULL, MENU,
+                    MENU_TITLE_ITEM, " Select in the module list: ",
+                    NULL);
+
+   if (db_get_current_program_name() == NULL) {
+      xv_set(menu, MENU_APPEND_ITEM,
+             xv_create(XV_NULL, MENUITEM,
+                       MENU_STRING, "* No workspace yet! *",
+                       MENU_RELEASE,
+                       MENU_INACTIVE, TRUE,
+                       NULL),
+             NULL);
+   }
+   else {
+      db_get_module_list(&module_list_length, module_list);
+   
+      if (module_list_length == 0) {
+         xv_set(menu, MENU_APPEND_ITEM,
+                xv_create(XV_NULL, MENUITEM,
+                          MENU_STRING,
+                          "* No module available in this workspace *",
+                          MENU_RELEASE,
+                          MENU_INACTIVE, TRUE,
+                          NULL),
+                NULL);
+      }
+      else {
+         for(i = 0; i < module_list_length; i++)
+            xv_set(menu, MENU_APPEND_ITEM,
+                   xv_create(XV_NULL, MENUITEM,
+                             MENU_STRING, strdup(module_list[i]),
+                             MENU_RELEASE,
+                             /* The strdup'ed string will also be
+                                freed when the menu is discarded: */
+                             MENU_RELEASE_IMAGE,
+                             NULL),
+                   NULL);
+      }
+      args_free(&module_list_length,
+                module_list);
+   }
+   
+   return menu;
+}
 
 
 void create_select_menu()
