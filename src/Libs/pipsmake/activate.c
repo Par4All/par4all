552c830d101d1272dbc37f98a8c6a75bf92f162d
oget <oget@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-07-31 15:09:10 +0000

    # on 1995/07/31 15:09:10, oget did: unused variable ...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1373 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/pipsmake/activate.c
+++ src/Libs/pipsmake/activate.c
@@ -13,6 +13,7 @@ extern int fprintf();
 
 #include "misc.h"
 
+static void delete_derived_resources();
 
 bool active_phase_p(phase)
 string phase;
@@ -51,6 +52,11 @@ string phase;
     string vrn;
     string old_phase;
     makefile current_makefile = parse_makefile();
+    bool rule_cyclic_p = TRUE;
+
+    debug_on("PIPSMAKE_DEBUG_LEVEL");
+    debug(1, "activate", "%s - requested\n", phase);
+
 
     /* find rule that describes phase */
     r = find_rule_by_phase(phase);
@@ -60,66 +66,177 @@ string phase;
     }
 
     /* complete simple cases */
-    if (active_phase_p(phase))
+    if (active_phase_p(phase)) {
+	debug( 7, "activate", "Rule `%s' already active\n", phase);
 	return (phase);
-    if (gen_length(rule_produced(r))!=1)
-	if (gen_length(rule_produced(r))>1){
-		pips_error("activate", 
-			   "Phase %s : rule that produces more than one resource : inconsistency risks \n", phase);
-	}
-	else
-	    pips_error("activate", 
-		       "Phase %s produces no resource\n", phase);
+    }
+    if (!gen_length(rule_produced(r)))
+        pips_error("activate", 
+		   "Phase %s produces no resource\n", phase);
 
-    /* find resource res that is produced by phase */
-    res = VIRTUAL_RESOURCE(CAR(rule_produced(r)));
-    vrn = virtual_resource_name(res);
+    /* GO: for many produced resources we loop over them
+       with the same 'old' code */
+    MAPL(pvrp, {
+	bool require_produced_rule_p = FALSE;
 
-    /* Check if selected phase produces a resource it uses */
-    MAPL(pvr, {
-        virtual_resource 	vr = VIRTUAL_RESOURCE(CAR(pvr));
-        string 			vrn2 = virtual_resource_name(vr);
-        owner 			vro = virtual_resource_owner(vr);
+	/* find resource res that is produced by phase */
+	res = VIRTUAL_RESOURCE(CAR(pvrp));
+	vrn = virtual_resource_name(res);
 
-        /* We do not check callers and callees */
-        if ( owner_callers_p(vro) || owner_callees_p(vro) ) {}
+	MAPL(pvr, {
+	    virtual_resource vr = VIRTUAL_RESOURCE(CAR(pvr));
+	    string vrn2 = virtual_resource_name(vr);
+	    owner vro = virtual_resource_owner(vr);
 
-        /* We can't select a phase that uses a resource it produces */
-        else if (strcmp(vrn, vrn2) == 0) {
-            pips_error("activate",
-                      "Selected phase %s requires a resource it produces\n",
-                      phase);
-        }
-        else debug(9, "activate",
-             "OK : Required resource %s is not produced by selected phase %s\n",
-	      vrn2, phase);
-    }, (list) rule_required( r ) );
+	    /* We do not check callers and callees */
+	    if ( owner_callers_p(vro) || owner_callees_p(vro) ) {}
+	    else if (same_string_p(vrn, vrn2))
+		require_produced_rule_p = TRUE;
 
+	}, (list) rule_required( r ) );
 
-    /* find current active phase old_phase that produces res */
-    old_phase = rule_phase(find_rule_by_resource(vrn));
+	/* If the current produced resource is not required
+	   by the new rule */
+	if (!require_produced_rule_p) {
+	    rule_cyclic_p = FALSE;
+	    /* find current active phase old_phase that produces res */
+	    old_phase = rule_phase(find_rule_by_resource(vrn));
 
-    /* replace old_phase by phase in active phase list */
-    if (old_phase != NULL) {
+	    /* replace old_phase by phase in active phase list */
+	    if (old_phase != NULL) {
 
-	MAPL(pa, {
-	    string s = STRING(CAR(pa));
+		MAPL(pa, {
+		    string s = STRING(CAR(pa));
 	    
-	    if (strcmp(s, old_phase) == 0) {
-		free(STRING(CAR(pa)));
-		STRING(CAR(pa)) = strdup(phase);
+		    if (strcmp(s, old_phase) == 0) {
+			free(STRING(CAR(pa)));
+			STRING(CAR(pa)) = strdup(phase);
+		    }
+		}, makefile_active_phases(current_makefile));
 	    }
-	}, makefile_active_phases(current_makefile));
+
+	    if (activate_del_derived_ressources_p() == TRUE)
+		delete_derived_resources (res);
+	    else
+		if (db_get_current_program() != database_undefined) {
+		    /* remove resources with the same name as res to maintain 
+		       consistency in the database */
+		    db_unput_resources(vrn);
+		}
+	}
+    }, rule_produced(r));
+
+    if (rule_cyclic_p == TRUE) {
+	pips_error("activate",
+		   "Phase %s is cyclic\n",
+		   phase);
     }
 
-    if (db_get_current_program() != database_undefined) {
-	/* remove resources with the same name as res to maintain 
-	   consistency in the database */
-	db_unput_resources(vrn);
-    }
-
+    debug_off();
     return (phase);
 }
+
+/*
+ * get the set of resources being derived from a given one 
+ */
+static void get_more_derived_resources (vrn, set_of_res)
+string vrn;
+set set_of_res;
+{
+    makefile m = parse_makefile();
+    rule r;
 
+    /* If the given resource is not in the set */
+    if (set_belong_p (set_of_res, (char *) vrn))
+	return;
 
+    /* put it into the set  */
+    set_add_element (set_of_res, set_of_res, (char *) vrn);
 
+    debug(8, "get_more_derived_resources",
+	  "got %s\n",
+	  vrn);
+	
+    /* For all active  phases*/
+    MAPL(pa, {
+
+	r = find_rule_by_phase(STRING(CAR(pa)));
+	
+	if (rule_use_resource_produced(r) == TRUE)
+	    debug(9, "get_more_derived_resources",
+		  "Don't scan cycling phase %s\n",STRING(CAR(pa)));
+	else
+	{
+	    debug(9, "get_more_derived_resources",
+		  "Scan phase %s\n",STRING(CAR(pa)));
+
+	    /* Search in the required rules */
+	    MAPL(pvr, {
+		virtual_resource res2 = VIRTUAL_RESOURCE(CAR(pvr));
+		string vrn2 = virtual_resource_name(res2);
+
+		/* If the resource names are equal */
+		if (same_string_p(vrn, vrn2)) {
+		
+		    debug(9, "get_more_derived_resources",
+			  "Resource %s is required by phase %s\n",
+			  vrn, STRING(CAR(pa)));
+
+		    /* make a recursion for all the produced rules */
+		    MAPL(pvr3, {
+			virtual_resource res3 = VIRTUAL_RESOURCE(CAR(pvr3));
+			string vrn3 = virtual_resource_name(res3);
+			/* Here, there is no infinite loop problem
+			   with rule producing a ressource they require,
+			   because the resource has already been had 
+			   to the set */
+			get_more_derived_resources(vrn3, set_of_res);
+		    }, (list) rule_produced ( r ) );
+		    break;
+		}
+	    }, (list) rule_required( r ) );
+	}
+    }, makefile_active_phases(m));
+}
+
+/* Test if a rule uses a resource it produces */
+bool rule_use_resource_produced(r)
+rule r;
+{
+    MAPL(pvrp, {
+	/* find resource res that is produced by phase */
+	virtual_resource res = VIRTUAL_RESOURCE(CAR(pvrp));
+	string vrn = virtual_resource_name(res);
+
+	MAPL(pvr, {
+	    virtual_resource vr = VIRTUAL_RESOURCE(CAR(pvr));
+	    string vrn2 = virtual_resource_name(vr);
+	    owner vro = virtual_resource_owner(vr);
+
+	    /* We do not check callers and callees */
+	    if ( owner_callers_p(vro) || owner_callees_p(vro) ) {}
+	    else if (same_string_p(vrn, vrn2))
+		return TRUE;
+	}, (list) rule_required( r ) );
+
+    }, (list) rule_produced( r ) );
+
+    return FALSE;
+}
+
+/*
+ * Delete the resources derived from a given one 
+ */
+static void delete_derived_resources (res)
+virtual_resource res;
+{
+    set s = set_make (set_pointer);
+    string vrn = virtual_resource_name(res);
+
+    /* Get the set of virtual resource to destroy */
+    get_more_derived_resources (vrn, s);
+
+    SET_MAP(se, {
+	delete_named_resources((string) se);
+    }, s);
+}
