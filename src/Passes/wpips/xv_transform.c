4783697f429a30bd83264bfe3a5c31c8adc47b49
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-07-31 17:36:53 +0000

    # on 1995/07/31 17:36:53, keryell did: Transforms are now (manually!) sorted.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1397 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/xv_transform.c
+++ src/Passes/wpips/xv_transform.c
@@ -24,77 +24,85 @@
 #include "phases.h"
 
 
-void transform_notify(menu, menu_item)
-Menu menu;
-Menu_item menu_item;
+void
+transform_notify(Menu menu,
+                 Menu_item menu_item)
 {
-    char *label = (char *) xv_get(menu_item, MENU_STRING);
+   char *label = (char *) xv_get(menu_item, MENU_STRING);
 
-    char *modulename = db_get_current_module_name();
+   char *modulename = db_get_current_module_name();
 
-    /* FI: borrowed from edit_notify() */
-    if (modulename == NULL) {
-	prompt_user("No module selected");
-	return;
-    }
+   /* FI: borrowed from edit_notify() */
+   if (modulename == NULL) {
+      prompt_user("No module selected");
+      return;
+   }
 
-    if (modulename != NULL) {
-	if (strcmp(label, PRIVATIZE_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_PRIVATIZE_MODULE, modulename);
-	}
-	else if (strcmp(label, DISTRIBUTE_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_DISTRIBUTER, modulename);
-	}
-	else if (strcmp(label, PARTIAL_EVAL_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_PARTIAL_EVAL, modulename);
-	}
-	else if (strcmp(label, UNROLL_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_UNROLL, modulename);
-	}
-	else if (strcmp(label,STRIP_MINE_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_STRIP_MINE, modulename);
-	}
-	else if (strcmp(label,LOOP_INTERCHANGE_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_LOOP_INTERCHANGE, modulename);
-	}
-	else if (strcmp(label, SUPPRESS_DEAD_CODE_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_SUPPRESS_DEAD_CODE, modulename);
-	}
-	else if (strcmp(label, ATOMIZER_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_ATOMIZER, modulename);
-	}
-	else if (strcmp(label, NEW_ATOMIZER_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_NEW_ATOMIZER, modulename);
-	}
-	else if (strcmp(label, REDUCTIONS_TRANSFORM) == 0) {
-	    safe_apply(BUILDER_REDUCTIONS, modulename);
-	}
-	else {
-	    pips_error("transform_notify", "Bad choice");
-	}
-    }
+   if (modulename != NULL) {
+      if (strcmp(label, PRIVATIZE_TRANSFORM) == 0) {
+         safe_apply(BUILDER_PRIVATIZE_MODULE, modulename);
+      }
+      else if (strcmp(label, DISTRIBUTE_TRANSFORM) == 0) {
+         safe_apply(BUILDER_DISTRIBUTER, modulename);
+      }
+      else if (strcmp(label, PARTIAL_EVAL_TRANSFORM) == 0) {
+         safe_apply(BUILDER_PARTIAL_EVAL, modulename);
+      }
+      else if (strcmp(label, UNROLL_TRANSFORM) == 0) {
+         safe_apply(BUILDER_UNROLL, modulename);
+      }
+      else if (strcmp(label,STRIP_MINE_TRANSFORM) == 0) {
+         safe_apply(BUILDER_STRIP_MINE, modulename);
+      }
+      else if (strcmp(label,LOOP_INTERCHANGE_TRANSFORM) == 0) {
+         safe_apply(BUILDER_LOOP_INTERCHANGE, modulename);
+      }
+      else if (strcmp(label, SUPPRESS_DEAD_CODE_TRANSFORM) == 0) {
+         safe_apply(BUILDER_SUPPRESS_DEAD_CODE, modulename);
+      }
+      else if (strcmp(label, UNSPAGHETTIFY_TRANSFORM) == 0) {
+         safe_apply(BUILDER_UNSPAGHETTIFY, modulename);
+      }
+      else if (strcmp(label, ATOMIZER_TRANSFORM) == 0) {
+         safe_apply(BUILDER_ATOMIZER, modulename);
+      }
+      else if (strcmp(label, NEW_ATOMIZER_TRANSFORM) == 0) {
+         safe_apply(BUILDER_NEW_ATOMIZER, modulename);
+      }
+      else if (strcmp(label, REDUCTIONS_TRANSFORM) == 0) {
+         safe_apply(BUILDER_REDUCTIONS, modulename);
+      }
+      else {
+         pips_error("transform_notify", "Bad choice");
+      }
+   }
+
+   display_memory_usage();
 }
 
-void create_transform_menu()
+
+void
+create_transform_menu()
 {
-    Menu menu;
+   Menu menu;
 
-    menu = xv_create(XV_NULL, MENU_COMMAND_MENU, 
-			 MENU_GEN_PIN_WINDOW, main_frame, "Transform Menu",
-		     MENU_ACTION_ITEM, PRIVATIZE_TRANSFORM, transform_notify,
-		     MENU_ACTION_ITEM, DISTRIBUTE_TRANSFORM, transform_notify,
-		     MENU_ACTION_ITEM, PARTIAL_EVAL_TRANSFORM, transform_notify,
-		     MENU_ACTION_ITEM, UNROLL_TRANSFORM, transform_notify,
-		     MENU_ACTION_ITEM, STRIP_MINE_TRANSFORM, transform_notify,
-		     MENU_ACTION_ITEM, LOOP_INTERCHANGE_TRANSFORM, transform_notify,
-		     MENU_ACTION_ITEM, SUPPRESS_DEAD_CODE_TRANSFORM, transform_notify,
-		     MENU_ACTION_ITEM, ATOMIZER_TRANSFORM, transform_notify,
-		     MENU_ACTION_ITEM, NEW_ATOMIZER_TRANSFORM, transform_notify,
-		     MENU_ACTION_ITEM, REDUCTIONS_TRANSFORM, transform_notify,
-		     NULL);
+   menu = xv_create(XV_NULL, MENU_COMMAND_MENU, 
+                    MENU_GEN_PIN_WINDOW, main_frame, "Transform Menu",
+                    MENU_ACTION_ITEM, ATOMIZER_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, SUPPRESS_DEAD_CODE_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, DISTRIBUTE_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, LOOP_INTERCHANGE_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, NEW_ATOMIZER_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, PARTIAL_EVAL_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, PRIVATIZE_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, REDUCTIONS_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, STRIP_MINE_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, UNROLL_TRANSFORM, transform_notify,
+                    MENU_ACTION_ITEM, UNSPAGHETTIFY_TRANSFORM, transform_notify,
+                    NULL);
 
-    (void) xv_create(main_panel, PANEL_BUTTON,
-		     PANEL_LABEL_STRING, "Transform",
-		     PANEL_ITEM_MENU, menu,
-		     0);
+   (void) xv_create(main_panel, PANEL_BUTTON,
+                    PANEL_LABEL_STRING, "Transform",
+                    PANEL_ITEM_MENU, menu,
+                    0);
 }
