1982ea4d44e954f6f0c26145f66594b0c1e2b8ac
oget <oget@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-08-03 10:20:12 +0000

    # on 1995/08/03 10:20:12, oget did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1427 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/icfg/icfg-local.h
@@ -0,0 +1,22 @@
+/* 
+   icfg.h -- acronym
+   include file for Interprocedural Control Flow Graph
+ */
+
+#ifndef ICFG_INCLUDED
+#define ICFG_INCLUDED
+
+#define ICFG_NOT_FOUND NULL
+
+#define ICFG_OPTIONS "tcdDIFl"
+
+#define ICFG_CALLEES_TOPO_SORT "ICFG_CALLEES_TOPO_SORT"
+#define ICFG_DRAW "ICFG_DRAW"
+#define ICFG_DEBUG "ICFG_DEBUG"
+#define ICFG_DEBUG_LEVEL "ICFG_DEBUG_LEVEL"
+#define ICFG_DOs "ICFG_DOs"
+#define ICFG_IFs "ICFG_IFs"
+#define ICFG_FLOATs "ICFG_FLOATs"
+#define ICFG_SHORT_NAMES "ICFG_SHORT_NAMES"
+
+#endif ICFG_INCLUDED
