7f1e30914077139718ba1c5d2f727091f6f0e514
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-08-04 09:03:23 +0000

    # on 1995/08/04 09:03:23, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1460 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/atomizer/control.c
@@ -0,0 +1,120 @@
+/* -- control.c
+ *
+ * package atomizer :  Alexis Platonoff, juillet 91
+ * --
+ *
+ * Functions for the manipulations and modifications of the control graph.
+ */
+
+#include <stdio.h>
+extern int fprintf();
+#include <string.h>
+
+#include "genC.h"
+#include "ri.h"
+#include "graph.h"
+#include "dg.h"
+/* #include "database.h"*/
+
+#include "ri-util.h"
+#include "constants.h"
+#include "misc.h"
+#include "list.h"
+
+#include "atomizer.h"
+
+/*============================================================================*/
+/* void modify_blocks(control c): Adds a node to the control graph when the
+ * statement of "c" is not an "instruction_block".
+ *
+ * This node is added just before "c". "c" predecessor became the new node.
+ * The latter's predecessors are those of "c", its successor is "c".
+ *
+ * "l_inst" keep the reference to the instructions for which a new node is
+ * added. It is a global variable that will be used when we will generate
+ * new statements (see atomizer_of_statement(), in atomizer.c).
+ *
+ * Called functions :
+ *       _ make_empty_statement() : ri-util/statement.c
+ */
+void modify_blocks(c)
+control c;
+{
+extern list l_inst;
+
+control nc;
+instruction inst = statement_instruction(control_statement(c));
+
+if (instruction_tag(inst) != is_instruction_block)
+  {
+  if (! instruction_in_list_p(inst, l_inst))
+    {
+    nc = make_control(control_statement(c), NIL, NIL);
+    control_statement(c) = make_empty_statement();
+
+    control_successors(nc) = control_successors(c);
+    control_predecessors(nc) = CONS(CONTROL, c, NIL);
+
+    control_successors(c) = CONS(CONTROL, nc, NIL);
+
+    l_inst = CONS(INSTRUCTION, inst, l_inst);
+    }
+  }
+}
+
+
+
+/*============================================================================*/
+/* void atom_get_blocs(control c, cons **l): Almost the get_blocs() of 
+ * the file control/control.h; the only modification is the call to
+ * modify_blocks() which adds a node to the control graph that will contain
+ * the instructions created by the translation of the statement contained
+ * in "c".
+ */
+void atom_get_blocs( c, l )
+control c ;
+cons **l ;
+{
+MAPL( cs, {if( CONTROL( CAR( cs )) == c ) return ;}, *l ) ;
+*l = CONS( CONTROL, c, *l ) ;
+
+modify_blocks(c);
+
+MAPL( cs, {atom_get_blocs( CONTROL( CAR( cs )), l );},
+      control_successors( c )) ;
+
+MAPL( ps, {atom_get_blocs( CONTROL( CAR( ps )), l );},
+      control_predecessors( c )) ;
+}
+
+
+
+/*============================================================================*/
+/* control find_control_block(control c): Returns the current control found
+ * in the static variable "cont_block".
+ *
+ * The use of this function is: 
+ *      _ first, in order to initialize the static "cont_block" this function is
+ *        called with the control we want to memorize. 
+ *      _ then, when we need to get this control, this function is called
+ *        with the argument "control_undefined".
+ *
+ * This is used by atomizer_of_statement() when we generate new statements.
+ */
+control find_control_block(c)
+control c;
+{
+static control cont_block;
+
+if(c != control_undefined)
+  {
+  instruction inst = statement_instruction(control_statement(c));
+  if(instruction_tag(inst) != is_instruction_block)
+    cont_block = CONTROL(CAR(control_predecessors(c)));
+  else
+    cont_block = control_undefined;
+  }
+
+return(cont_block);
+}
+
