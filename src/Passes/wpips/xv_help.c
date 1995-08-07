cf691bfc127d08a9bae413c918b81fe9914255e7
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-08-07 10:46:50 +0000

    # on 1995/08/07 10:46:50, keryell did: Help menu: Internet documentation through XMosaic or Netscape.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1526 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/xv_help.c
+++ src/Passes/wpips/xv_help.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include <sys/time.h>
@@ -9,6 +10,7 @@
 #include <types.h>
 
 #include "genC.h"
+#include "top-level.h"
 #include "wpips.h"
 #include "xv_sizes.h"
 
@@ -18,92 +20,122 @@
 #define min(a,b) ((a) < (b) ? (a) : (b))
 #define HELP_LINES 8
 
-
+/* The URL of the PIPS documentation at the École des Mines de Paris: */
+#define PIPS_DOCUMENTATION_URL "http://www.cri.ensmp.fr/~pips"
 
 static Panel_item lines[HELP_LINES];
 static char *help_list[ARGS_LENGTH];
 static int help_list_length;
 
-void display_help(topic)
-char *topic;
+void
+display_help(char * topic)
 {
-    int i;
+   int i;
 
-    if (help_list_length != 0)
-	/* help window is already opened */
-	args_free(&help_list_length, help_list);
+   if (help_list_length != 0)
+      /* help window is already opened */
+      args_free(&help_list_length, help_list);
 
-    get_help_topic(topic, &help_list_length, help_list);
+   get_help_topic(topic, &help_list_length, help_list);
 
-    for (i = 0; i < min(HELP_LINES, help_list_length); i++) {
-	xv_set(lines[i], PANEL_LABEL_STRING, help_list[i], 0);
-    }
+   for (i = 0; i < min(HELP_LINES, help_list_length); i++) {
+      xv_set(lines[i], PANEL_LABEL_STRING, help_list[i], 0);
+   }
 
-    for (i = min(HELP_LINES, help_list_length); i < HELP_LINES; i++) {
-	xv_set(lines[i], PANEL_LABEL_STRING, "", 0);
-    }
+   for (i = min(HELP_LINES, help_list_length); i < HELP_LINES; i++) {
+      xv_set(lines[i], PANEL_LABEL_STRING, "", 0);
+   }
 
-    unhide_window(help_frame);
+   unhide_window(help_frame);
 }
 
 
 
-void close_help_notify(item, event)
-Panel_item item;
-Event *event;
+static void
+close_help_notify(Panel_item item,
+                  Event * event)
 {
-    args_free(&help_list_length, help_list);
+   args_free(&help_list_length, help_list);
 
-    hide_window(help_frame);
+   hide_window(help_frame);
 }
 
 
 
-void create_help_window()
+void
+create_help_window()
 {
-    int i;
-    Panel help_panel;
+   int i;
+   Panel help_panel;
 
-    help_panel = xv_create(help_frame, PANEL,
-			   NULL);
+   help_panel = xv_create(help_frame, PANEL,
+                          NULL);
 
-    for (i = 0; i < HELP_LINES; i++) {
-	lines[i] = xv_create(help_panel, PANEL_MESSAGE, 
-			     XV_X, 15,
-			     XV_Y, 15*(i+1),
-			     0);
-    }
+   for (i = 0; i < HELP_LINES; i++) {
+      lines[i] = xv_create(help_panel, PANEL_MESSAGE, 
+                           XV_X, 15,
+                           XV_Y, 15*(i+1),
+                           0);
+   }
 
-    (void) xv_create(help_panel, PANEL_BUTTON,
-		     PANEL_LABEL_STRING, "CLOSE",
-		     XV_X, HELP_WIDTH/2-28,
-		     XV_Y, 15*(HELP_LINES+1),
-		     PANEL_NOTIFY_PROC, close_help_notify,
-		     0);
+   (void) xv_create(help_panel, PANEL_BUTTON,
+                    PANEL_LABEL_STRING, "CLOSE",
+                    XV_X, HELP_WIDTH/2-28,
+                    XV_Y, 15*(HELP_LINES+1),
+                    PANEL_NOTIFY_PROC, close_help_notify,
+                    0);
 
 
 }
 
-void help_notify(menu, menu_item)
-Menu menu;
-Menu_item menu_item;
+static void
+help_notify(Menu menu,
+            Menu_item menu_item)
 {
-    display_help((char *) xv_get(menu_item, MENU_STRING));
+   display_help((char *) xv_get(menu_item, MENU_STRING));
 }
 
 
-
-void create_help_menu()
+static void
+help_launch_pips_netscape(Menu menu,
+                          Menu_item menu_item)
 {
-    Menu menu;
+   system("netscape " PIPS_DOCUMENTATION_URL " &");
+}
 
-    menu = xv_create(XV_NULL, MENU_COMMAND_MENU, 
-		     MENU_ACTION_ITEM, "Workspace", help_notify,
-		     MENU_ACTION_ITEM, "Module", help_notify,
-		     NULL);
 
-    (void) xv_create(main_panel, PANEL_BUTTON,
-		     PANEL_LABEL_STRING, "Help ",
-		     PANEL_ITEM_MENU, menu,
-		     0);
+static void
+help_launch_pips_xmosaic(Menu menu,
+                          Menu_item menu_item)
+{
+   system("xmosaic " PIPS_DOCUMENTATION_URL " &");
+}
+
+
+void
+create_help_menu()
+{
+   Menu menu;
+
+   menu = xv_create(XV_NULL, MENU_COMMAND_MENU, 
+                    MENU_TITLE_ITEM,
+                    "The PIPS documentation",
+                    MENU_GEN_PIN_WINDOW, main_frame, "Help Menu",
+                    MENU_ACTION_ITEM, "Few words about \"Workspace\"...",
+                    help_notify,
+                    MENU_ACTION_ITEM, "Few words about \"Module\"...",
+                    help_notify,
+                    /* Just a separator: */
+                    MENU_ITEM, MENU_STRING, "--------", MENU_INACTIVE, TRUE,
+                    NULL,
+                    MENU_ACTION_ITEM, "The PIPS documentation on Internet with Netscape...",
+                    help_launch_pips_netscape,
+                    MENU_ACTION_ITEM, "The PIPS documentation on Internet with XMosaic...",
+                    help_launch_pips_xmosaic,
+                    NULL);
+
+   (void) xv_create(main_panel, PANEL_BUTTON,
+                    PANEL_LABEL_STRING, "Help ",
+                    PANEL_ITEM_MENU, menu,
+                    0);
 }
