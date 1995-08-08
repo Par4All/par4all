b9095195a7e884491f39ec100c157e982d6acb62
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-08-08 15:02:09 +0000

    # on 1995/08/08 15:02:09, coelho did: longer but cleaner version.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1559 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/hpfc/hpfc_interactive.c
+++ src/Scripts/hpfc/hpfc_interactive.c
@@ -1,10 +1,10 @@
 /* $RCSfile: hpfc_interactive.c,v $ (version $Revision$)
- * $Date: 1995/08/08 10:54:37 $, 
+ * $Date: 1995/08/08 15:02:09 $, 
  *
  * interactive interface to hpfc,
  * based on the GNU READLINE library for interaction,
  * and the associated HISTORY library for history.
- * taken from bash.
+ * taken from bash. it's definitely great.
  */
 
 #include <stdio.h>
@@ -19,7 +19,7 @@ extern int chdir();
 #include "readline/history.h"
 
 #define HPFC_PROMPT "hpfc> " 		/* prompt for readline  */
-#define HPFC_PREFIX "hpfc"   		/* forked shell script  */
+#define HPFC_SHELL "hpfc"   		/* forked shell script  */
 #define HPFC_HISTENV "HPFC_HISTORY"	/* history file env variable */
 #define HPFC_HISTORY_LENGTH 100		/* max length of history file */
 #define HIST ".hpfc.history" 		/* default history file */
@@ -27,10 +27,6 @@ extern int chdir();
 #define SHELL_ESCAPE "\\" 		/* ! used for history reference */
 #define CHANGE_DIR   "cd "
 #define QUIT         "quit"
-
-/* the lexer is quite simple:-)
- */
-#define PREFIX_EQUAL_P(str, prf) (strncmp(str, prf, strlen(prf))==0)
 
 /*  returns the full hpfc history file name, i.e.
  *  - $HPFC_HISTORY (if any)
@@ -42,19 +38,17 @@ static char *default_hist_file_name()
 
     if (hist) return hist;
 
-    /* else builds the default name.
+    /* else builds the default name. memory leak.
      */
     home = getenv("HOME");
     return sprintf((char*) malloc(sizeof(char)*(strlen(home)+strlen(HIST)+2)),
 		   "%s/%s", home, HIST);
 }
 
-/* main: interactive loop and history management.
- */
-int main()
+static char * initialize_hpfc_history()
 {
     HIST_ENTRY * last_entry;
-    char *last, *line, *file_name = default_hist_file_name();
+    char *file_name = default_hist_file_name();
     
     /*  initialize history: 
      *  read the history file, then point to the last entry.
@@ -67,40 +61,87 @@ int main()
     /* last points to the last history line of any.
      * used to avoid to put twice the same line.
      */
-    last = last_entry ? last_entry->line : NULL ;
+    return last_entry ? last_entry->line : NULL ;
+}
+
+
+/* Handlers
+ */
+void cdir_handler(char * line)
+{
+    if (chdir(line+strlen(CHANGE_DIR)))
+	fprintf(stderr, "error while changing directory\n");
+}
+
+void shell_handler(char * line)
+{
+    system(line+strlen(SHELL_ESCAPE));
+}
+
+void quit_handler(char * line)
+{
+    char *file_name = default_hist_file_name();
+
+    /*   close history: truncate list and write history file
+     */
+    stifle_history(HPFC_HISTORY_LENGTH);
+    write_history(file_name);
     
+    exit(0);
+}
+
+void default_handler(char * line)
+{
+    char *shll = (char*)
+	malloc(sizeof(char)*(strlen(HPFC_SHELL)+strlen(line)+2));
+    
+    system(sprintf(shll, "%s %s", HPFC_SHELL, line));
+
+    free(shll);
+}
+
+struct t_handler 
+{
+    char * name;
+    void (*function)(char *);
+} ;
+
+static struct t_handler handlers[] =
+{
+  { QUIT,		quit_handler },
+  { CHANGE_DIR, 	cdir_handler },
+  { SHELL_ESCAPE, 	shell_handler },
+  { (char *) NULL, 	default_handler}
+};
+
+/* the lexer is quite simple:-)
+ */
+#define PREFIX_EQUAL_P(str, prf) (strncmp(str, prf, strlen(prf))==0)
+
+static void (*find_handler(char* line))(char *)
+{
+    struct t_handler * x = handlers;
+    while ((x->name) && !PREFIX_EQUAL_P(line, x->name)) x++;
+    return x->function;
+}
+
+/* MAIN: interactive loop and history management.
+ */
+int main()
+{
+    char *last, *line;
+
+    last = initialize_hpfc_history();
+
     /*  interactive loop
      */
     while ((line = readline(HPFC_PROMPT)))
     {
-	if (PREFIX_EQUAL_P(line, QUIT)) 
-	    break;
-	else if (PREFIX_EQUAL_P(line, CHANGE_DIR))
-	{
-	    if (chdir(line+strlen(CHANGE_DIR)))
-		fprintf(stderr, "error while changing directory\n");
-	}
-	else if (PREFIX_EQUAL_P(line, SHELL_ESCAPE))
-	{
-	    /*   the shell escape is directly executed. That easy!
-	     */
-	    system(line+strlen(SHELL_ESCAPE));
-	}
-	else
-	{
-	    /*   calls a script:-)
-	     *   All this stuff could be taken care of in C, but shell
-	     *   scripts are much easier to develop:-) 
-	     */
-	    char *shll = (char*)
-		malloc(sizeof(char)*(strlen(HPFC_PREFIX)+strlen(line)+2));
+	/*   calls the appropriate handler.
+	 */
+	(find_handler(line))(line);
 
-	    system(sprintf(shll, "%s %s", HPFC_PREFIX, line));
-
-	    free(shll);
-	}
-
-	/*   add to history if not the same as the last one
+	/*   add to history if not the same as the last one.
 	 */
 	if (line && *line && ((last && strcmp(last, line)!=0) || (!last)))
 	    add_history(line), last = line; 
@@ -108,14 +149,9 @@ int main()
 	    line = (char*) ((line) ? (free(line), NULL) : NULL);
     }
 
-    if (!line) fprintf(stdout, "\n"); /* for Ctrl-D terminations */
-
-    /*   close history: truncate list and write history file
-     */
-    stifle_history(HPFC_HISTORY_LENGTH);
-    write_history(file_name);
-
-    return 0;
+    fprintf(stdout, "\n"); /* for Ctrl-D terminations */
+    quit_handler(line);
+    return 0; 
 }
 
 /*   that is all
