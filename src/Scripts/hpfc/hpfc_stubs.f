ce07f2ad94815ef1394dd7d2372031ced130aaaa
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-09-05 13:41:45 +0000

    # on 1995/09/05 13:41:45, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@1857 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/hpfc/hpfc_stubs.f
+++ src/Scripts/hpfc/hpfc_stubs.f
@@ -1,5 +1,11 @@
 c
-c Fake sources for PIPS to deal with special FC directives
+c Fake sources for PIPS to deal with special FC directives. 
+c functions with side effects, just in case. 
+c
+c (c) Fabien COELHO, 09/95
+c
+c $RCSfile: hpfc_stubs.f,v $ (version $Revision$)
+c ($Date: 1995/09/05 13:41:45 $, )
 c      
       subroutine hpfc1()
       print *, 'hpf1: '
