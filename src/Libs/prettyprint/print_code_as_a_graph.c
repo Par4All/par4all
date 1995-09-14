1b86e19a3982b9b543450db086cc55a00411e9f6
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-09-14 10:12:37 +0000

    # on 1995/09/14 10:12:37, keryell did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2025 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/prettyprint/print_code_as_a_graph.c
@@ -0,0 +1,111 @@
+/* Define all the entry points to generate the various graph
+   sequential view to be viewed later with a graph viewer such as
+   daVinci.
+
+   Ronan.Keryell@cri.ensmp.fr, 5/09/1995. */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "genC.h"
+#include "ri.h"
+#include "text.h"
+#include "database.h"
+
+#include "misc.h"
+#include "properties.h"
+#include "top-level.h"
+#include "text-util.h"
+#include "ri-util.h"
+#include "pipsdbm.h"
+#include "complexity_ri.h"
+#include "complexity.h"
+#include "effects.h"
+#include "regions.h"
+#include "semantics.h"
+#include "prettyprint.h"
+
+
+void
+print_code_as_a_graph(char *mod_name)
+{
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", TRUE);
+  print_code(mod_name);
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", FALSE);
+}
+
+
+void
+print_code_as_a_graph_transformers(char *mod_name)
+{
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", TRUE);
+  print_code_transformers(mod_name);
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", FALSE);
+}
+
+
+void
+print_code_as_a_graph_complexities(char *mod_name)
+{
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", TRUE);
+  print_code_complexities(mod_name);
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", FALSE);
+}
+
+
+void
+print_code_as_a_graph_preconditions(char *mod_name)
+{
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", TRUE);
+  print_code_preconditions(mod_name);
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", FALSE);
+}
+
+
+void
+print_code_as_a_graph_regions(char *mod_name)
+{
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", TRUE);
+  user_warning("print_code_as_a_graph_regions", "To be done...");
+  print_code/*_regions*/(mod_name);
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", FALSE);
+}
+
+
+void
+print_code_as_a_graph_in_regions(char *mod_name)
+{
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", TRUE);
+  user_warning("print_code_as_a_graph_regions", "To be done...");
+  print_code/*_in_regions*/(mod_name);
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", FALSE);
+}
+
+
+void
+print_code_as_a_graph_out_regions(char *mod_name)
+{
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", TRUE);
+  user_warning("print_code_as_a_graph_regions", "To be done...");
+  print_code/*_out_regions*/(mod_name);
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", FALSE);
+}
+
+
+void
+print_code_as_a_graph_proper_effects(char *mod_name)
+{
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", TRUE);
+  print_code_proper_effects(mod_name);
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", FALSE);
+}
+
+
+void
+print_code_as_a_graph_cumulated_effects(char *mod_name)
+{
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", TRUE);
+  print_code_cumulated_effects(mod_name);
+  set_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH", FALSE);
+}
