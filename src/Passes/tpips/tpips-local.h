6ff32ec40532f9e28607a099e1c1f615f5012f7e
oget <oget@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-09-14 10:29:12 +0000

    # on 1995/09/14 10:29:12, oget did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2034 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/tpips/tpips-local.h
+++ src/Passes/tpips/tpips-local.h
@@ -1,4 +1,11 @@
 
+#define FILE_LIST_MAX_LENGTH 10
+
+typedef struct _t_file_list {
+	int argc;
+	char *argv[FILE_LIST_MAX_LENGTH];
+}t_file_list;
+
 typedef struct _res_or_rule {
 	list the_owners;
 	string the_name;
@@ -6,6 +13,7 @@ typedef struct _res_or_rule {
 
 extern int tp_lex();
 extern int tp_parse();
+extern void tp_error();
 extern void tp_init_lex();
 extern void tp_begin_key();
 extern void tp_begin_fname();
