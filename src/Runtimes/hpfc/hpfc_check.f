905c61a4927babe3387a46eff3246bc112cc56ba
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-09-15 15:57:02 +0000

    # on 1995/09/15 15:57:02, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2090 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/hpfc/hpfc_check.f
+++ src/Runtimes/hpfc/hpfc_check.f
@@ -1,7 +1,7 @@
 c
 c Checks run-time library version
 c
-c $RCSfile: hpfc_check.f,v $ ($Date: 1994/09/21 17:23:02 $, )
+c $RCSfile: hpfc_check.f,v $ ($Date: 1995/09/15 15:57:02 $, )
 c version $Revision$
 c got on %D%, %T%
 c $Id$
@@ -13,7 +13,8 @@ c
       if  (REALNBOFARRAYS.GT.MAXNBOFARRAYS.OR.
      $     REALNBOFTEMPLATES.GT.MAXNBOFTEMPLATES.OR.
      $     REALNBOFPROCESSORS.GT.MAXNBOFPROCESSORS.OR.
-     $     REALMAXSIZEOFPROCS.GT.MAXMAXSIZEOFPROCS) then
+     $     REALMAXSIZEOFPROCS.GT.MAXMAXSIZEOFPROCS.OR.
+     $     REALMAXSIZEOFBUFFER.GT.MAXMAXSIZEOFBUFFER) then
          print *, 
      $        'HPFC run-time library',
      $        '  must be recompiled with larger parameters'
@@ -26,6 +27,10 @@ c
       NBOFTEMPLATES = REALNBOFTEMPLATES
       NBOFPROCESSORS = REALNBOFPROCESSORS
       MAXSIZEOFPROCS = REALMAXSIZEOFPROCS
+c
+c and hpfc_buffers
+c
+      SIZEOFBUFFER = REALMAXSIZEOFBUFFER
       end
 c
 c that s all
