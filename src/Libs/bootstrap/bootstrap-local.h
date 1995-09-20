f752d5e5b4f4923af1b9f8e002a75af213fde567
oget <oget@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-09-20 13:50:58 +0000

    # on 1995/09/20 13:50:58, oget did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2208 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/bootstrap/bootstrap-local.h
@@ -0,0 +1,9 @@
+
+/* GO: package name for io effects */
+#define IO_EFFECTS_PACKAGE_NAME "__IO_EFFECTS"
+
+/* GO: corresponding array name */
+#define IO_EFFECTS_ARRAY_NAME "LUNS"
+
+/* GO: size of the unit specifier */
+#define IO_EFFECTS_UNIT_SPECIFIER_LENGTH 4
