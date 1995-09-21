aee352fbeae5373c9e6fa6fdfafe7291ab2324ba
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-09-21 13:07:32 +0000

    # on 1995/09/21 13:07:32, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2228 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/misc/system.c
+++ src/Libs/misc/system.c
@@ -1,5 +1,5 @@
 /* $RCSfile: system.c,v $ version $Revision$
- * ($Date: 1995/09/18 13:16:56 $, )
+ * ($Date: 1995/09/21 13:07:32 $, )
  *
  * a safe system call. abort if fails.
  * FC 09/95
@@ -11,15 +11,27 @@
 #include "genC.h"
 #include "misc.h"
 
-int safe_system(string command) /* the command to be executed */
+void
+safe_system(
+    string command) /* the command to be executed */
+{
+    int status = system(command);
+    
+    if (status)
+	user_error("safe_system", "Failed (sig: %d, ret: %d) for %s\n", 
+		   (status/0x100) & 0xff, status & 0xff, command);
+}
+
+int
+safe_system_no_abort(
+    string command) /* the command to be executed */
 {
     int status = system(command);
     
     if (status == 127)
-		pips_error("safe_system", "Could not execute : %s\n", command);
+	pips_error("safe_system", "Could not execute : %s\n", command);
 
     return (status / 256) & 255;
 }
-
 /* that is all
  */
