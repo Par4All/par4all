1ee2f6ebe42fe924263981e55009450de154f1c2
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-09-27 08:52:47 +0000

    # on 1995/09/27 08:52:47, irigoin did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2339 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/transformer/transformer_main.c
@@ -0,0 +1,25 @@
+ /* main for transformer package
+  *
+  * Tests link-editing, no more
+  *
+  * Francois Irigoin, 21 April 1990
+  */
+
+#include <setjmp.h>
+
+#include "genC.h"
+#include "ri.h"
+
+jmp_buf pips_top_level;
+
+#include "transformer.h"
+
+main()
+{
+    transformer t1 = transformer_undefined;
+    transformer t2 = transformer_undefined;
+    transformer t3;
+    
+    t3 = transformer_convex_hull(t1, t2);
+    (void) print_transformer(t3);
+}
