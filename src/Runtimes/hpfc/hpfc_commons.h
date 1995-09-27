01aad7fce4ff83a551814551a3a5bbdf921e7d0f
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-09-27 17:08:01 +0000

    # on 1995/09/27 17:08:01, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2363 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/hpfc/hpfc_commons.h
+++ src/Runtimes/hpfc/hpfc_commons.h
@@ -1,10 +1,8 @@
 c
 c inclusions for run-time support functions
 c
-c $RCSfile: hpfc_commons.h,v $ ($Date: 1995/09/07 11:25:14 $, )
-c version $Revision$
-c got on %D%, %T%
-c $Id$
+c $RCSfile: hpfc_commons.h,v $ version $Revision$
+c ($Date: 1995/09/27 17:08:01 $, )
 c
 c
       include 'hpfc_parameters.h'
@@ -12,8 +10,9 @@ c
       include 'hpfc_globs.h'
       include 'hpfc_procs.h'
       include 'hpfc_buffers.h'
+      include 'hpfc_counts.h'
 c
-c parameters value to be define at the beginning
+c parameters value to be defined when starting a program.
 c
       integer
      $     NBOFARRAYS,
