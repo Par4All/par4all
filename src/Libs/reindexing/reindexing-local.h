2a4d7a8925071c06eba7676a6bdba70e69ad4226
platonof <platonof@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-02 17:20:16 +0000

    # on 1995/10/02 17:20:16, platonof did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2506 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/reindexing/reindexing-local.h
@@ -0,0 +1,31 @@
+/*#define INFINITY 2147483647*/ /* 2^31 - 1 */
+#define INFINITY 32767 /* 2^15 - 1 */
+
+#define RE_MODULE_NAME "REINDEX"
+
+#define STAT_SYM "S"
+
+#define IS_TEMP 1
+
+#define IS_NEW_ARRAY 2
+
+#define ENTITY_MIN_P(e) (strcmp(entity_local_name(e), \
+				MIN_OPERATOR_NAME) == 0)
+#define ENTITY_MAX_P(e) (strcmp(entity_local_name(e), \
+				MAX_OPERATOR_NAME) == 0)
+#define ENTITY_MIN_OR_MAX_P(e) (ENTITY_MIN_P(e) || \
+				 ENTITY_MAX_P(e) )
+
+#define IS_LOOP_BOUNDS 0
+#define IS_ARRAY_BOUNDS 1
+
+#define MIN_OPERATOR_NAME "MIN"
+#define MAX_OPERATOR_NAME "MAX"
+
+#define SAI "INS_"
+#define SAT "TEMP_"
+#define SA_MODULE_NAME "SA"
+#define SPACE " "
+#define COMA  ","
+#define LEPA  "("
+#define RIPA  ")"
