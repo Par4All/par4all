c3cad6d7d7722e24029249f70874180e1d87a889
platonof <platonof@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-02 16:56:27 +0000

    # on 1995/10/02 16:56:27, platonof did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2499 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/array_dfg/array_dfg-local.h
@@ -0,0 +1,24 @@
+/* local definitions of this library */
+#define RETURN( _debug, _function, _ret_obj ) \
+   { debug((_debug), (_function), "returning \n"); return( (_ret_obj) ); }
+#define ADFG_MODULE_NAME        "ADFG"
+#define EXPRESSION_PVECTEUR(e)  normalized_linear(NORMALIZE_EXPRESSION( e ))
+#define ENTRY_ORDER     300000  /* Hard to have a non illegal number for hash_put !!! */
+#define EXIT_ORDER      100000
+#define TAKE_LAST	TRUE
+#define TAKE_FIRST	FALSE
+#define BASE_NODE_NUMBER 1000
+
+/* Structure for return of a possible source */
+typedef struct Sposs_source {
+        quast   *qua;
+        Ppath   pat;
+} Sposs_source, *Pposs_source;
+
+
+/* Structure to list wich node read or write an effect */
+typedef struct Sentity_vertices {
+        entity ent;
+        list lis;    /* list of vertices */
+} Sentity_vertices, *Pentity_vertices;
+
