9a10a5c9850214681b732c203ee9851ff55a82a8
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-03 09:05:40 +0000

    # on 1995/10/03 09:05:40, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2517 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/conversion/system_to_code.c
+++ src/Libs/conversion/system_to_code.c
@@ -3,20 +3,18 @@
  *    moved to conversion on 15 May 94
  *
  * SCCS stuff:
- * $RCSfile: system_to_code.c,v $ ($Date: 1995/10/02 19:42:05 $, ) version $Revision$, 
+ * $RCSfile: system_to_code.c,v $ ($Date: 1995/10/03 09:05:40 $, ) version $Revision$, 
  * got on %D%, %T%
  * $Id$
  */
 
-/*
- * Standard includes
+/* Standard includes
  */
  
 #include <stdio.h>
 #include <string.h> 
 
-/*
- * Psystems stuff
+/* Psystems stuff
  */
 
 #include "types.h"
@@ -25,16 +23,14 @@
 #include "contrainte.h"
 #include "sc.h"
 
-/*
- * Newgen stuff
+/* Newgen stuff
  */
 
 #include "genC.h"
 
 #include "ri.h" 
 
-/*
- * PIPS stuff
+/* PIPS stuff
  */
 
 #include "ri-util.h" 
@@ -44,23 +40,20 @@
 #include "semantics.h"
 #include "effects.h"
 
-/* in syntax.h */
-entity CreateIntrinsic(string name);
+entity CreateIntrinsic(string name);/* in syntax.h */
 
 #include "conversion.h"
 
-/*
- * TEST GENERATION
- */
+/********************************************************** TEST GENERATION */
 
-/*
- * expression Psysteme_to_expression(Psysteme systeme)
+/* expression Psysteme_to_expression(Psysteme systeme)
  *
  * From a Psysteme, a logical expression that checks for
  * the constraints is generated.
  */
-expression Psysteme_to_expression(systeme)
-Psysteme systeme;
+expression 
+Psysteme_to_expression(
+    Psysteme systeme)
 {
     entity
 	equ = local_name_to_top_level_entity(EQUAL_OPERATOR_NAME),
@@ -76,38 +69,30 @@ Psysteme systeme;
     result = expression_list_to_conjonction(conjonction);
 
     gen_free_list(conjonction);
-    return(result);
+    return result;
 }
 
-/*
- * 
- */
-list Pcontrainte_to_expression_list(constraint, operator)
-Pcontrainte constraint;
-entity operator;
+list 
+Pcontrainte_to_expression_list(
+    Pcontrainte constraint,
+    entity operator)
 {
-    list
-	result = NIL;
-    Pcontrainte
-	c = NULL;
-    Pvecteur
-      vneg = VECTEUR_NUL,
-      vpos = VECTEUR_NUL;
+    list result = NIL;
+    Pcontrainte c = NULL;
+    Pvecteur vneg = VECTEUR_NUL, vpos = VECTEUR_NUL;
 
-    for(c=constraint;
-	!CONTRAINTE_UNDEFINED_P(c);
-	c=c->succ)
+    for(c=constraint; c; c=c->succ)
       if (entity_undefined_p(operator))
-	/* 
-	 * a simple expression is generated.
+
+	/* a simple expression is generated.
 	 */
 	result = 
 	    CONS(EXPRESSION, 
 	       make_vecteur_expression(contrainte_vecteur(c)),
 	       result);
       else
-	/*
-	 * a binary operator should be given, as eg, ne, lt, le, gt, ge.
+
+	/* a binary operator should be given, as eg, ne, lt, le, gt, ge.
 	 */
 	Pvecteur_separate_on_sign(contrainte_vecteur(c), &vpos, &vneg),
 	result = 
@@ -119,27 +104,23 @@ entity operator;
 	vect_rm(vpos), vpos=VECTEUR_NUL,
         vect_rm(vneg), vneg=VECTEUR_NUL;
     
-    return(result);
+    return result;
 }
 
 /* expression constraints_to_loop_bound(c, var, is_lower)
- * Pcontrainte c;
- * Variable var;
- * bool is_lower;
- * entity divide;
  * 
  * the is_lower (lower/upper) loop bound for variable var relative
  * to Pcontrainte c is generated. All the constraints in c are used,
  * and they must be ok. 
  */
-expression constraints_to_loop_bound(c, var, is_lower, divide)
-Pcontrainte c;
-Variable var;
-bool is_lower;
-entity divide;
+expression 
+constraints_to_loop_bound(
+    Pcontrainte c,  /* the constraints of the bound */
+    Variable var,   /* the index variable */
+    bool is_lower,  /* lower or upper bound */
+    entity divide)  /* integer division to be called */
 {
   int len = 0, sign = is_lower? -1: +1;
-  entity operator = is_lower? CreateIntrinsic("MAX"): CreateIntrinsic("MIN");
   expression result;
   list le = NIL;
   Psysteme s;
@@ -153,7 +134,7 @@ entity divide;
       inegalites_fprint(stderr, c, entity_local_name);
   }
 
-  assert(!CONTRAINTE_UNDEFINED_P(c));
+  message_assert("some constraints", !CONTRAINTE_UNDEFINED_P(c));
 
   /*  the constraints are sorted first, to ensure a deterministic result
    *  ??? the sorting criterion is rather strange:-)
@@ -162,18 +143,15 @@ entity divide;
   vect_sort(sc_base(s), compare_Pvecteur);
   sc_sort_constraints(s, sc_base(s));
 
-  /*  each contraint is considered in turn to generate the bound
+  /*  each constraint is considered in turn to generate a bound
    */
-  for(c=sc_inegalites(s);
-      c!=(Pcontrainte) NULL;
-      c=c->succ)
-    {
+  for(c=sc_inegalites(s); c; c=c->succ)
+  {
       Value val = vect_coeff(var, c->vecteur);
-      Pvecteur vdiv = vect_del_var(c->vecteur, var),
-               vadd = VECTEUR_NUL, v;
+      Pvecteur vdiv = vect_del_var(c->vecteur, var), vadd = VECTEUR_NUL, v;
       expression ediv, eadd, e;
 
-      assert(sign*val>0);
+      message_assert("coherent value and sign", sign*val>0);
 
       if (val>0) 
 	  vect_chg_sgn(vdiv);
@@ -183,30 +161,36 @@ entity divide;
 	  val=-val, vect_add_elem(&vdiv, TCST, val-1);
 
       /* extract coefficients that are dividable by val...
+       * x = (ay+bz)/a -> x = y + bz/a
        */
       for (v=vdiv; v; v=v->succ)
       {
 	  Variable va = var_of(v);
 	  Value vl = val_of(v);
 
-	  if (vl%val==0) 
-	      vect_add_elem(&vadd, va, vl/val);
+	  if (vl%val==0) vect_add_elem(&vadd, va, vl/val);
       }
 
       for (v=vadd; v; v=v->succ)
-      {
 	  vect_erase_var(&vdiv, var_of(v));
-      }
 
       /* assert. no 2i=0 should have reached this function...
        */
       message_assert("some expression", vdiv || vadd);
 
+      /* I should perform some other optimizations here, by looking at
+       * the extent of the numerator, that may result in a constant after
+       * division by the denominator. For instance, x = y/a and 0 <= y < a
+       * would lead to x = 0, which is quite simpler... I would need a hook
+       * from the callers of this function to retrieve the constant lower
+       * and upper bounds of each variable in order to perform this.
+       */
+
       if (vdiv)
       {
 	  ediv = make_vecteur_expression(vdiv);
 	  
-	  if (val!=1) 
+	  if (val!=1) /* (I guess I should notice this case earlier:-) */
 	      ediv = MakeBinaryCall(divide, ediv, int_to_expression(val));
 	  
 	  if (vadd)
@@ -216,50 +200,47 @@ entity divide;
 				 eadd, ediv);
 	  }
 	  else
-	  {
 	      e = ediv;
-	  }
       }
       else 
-      {
 	  e = make_vecteur_expression(vadd);
-      }
 
       le = CONS(EXPRESSION, e, le);
-    }
+  }
 
-  /* final operation
+  /* final operation: MAX or MIN if more than one bound
    */
-  len = gen_length(le);  assert(len!=0);
+  len = gen_length(le);  message_assert("some expressions", len!=0);
 
-  /* NO, MIN and MAX should take any number of arguments in fortran!
-   * // result = expression_list_to_binay_operator_call(le, operator); 
-   */
-  result = len==1? EXPRESSION(CAR(le)): make_call_expression(operator, le);
+  if (len==1)
+  {
+      result = EXPRESSION(CAR(le));
+      gen_free_list(le);
+  }
+  else
+  {
+      entity operator = is_lower ?
+	  CreateIntrinsic("MAX"): CreateIntrinsic("MIN");
 
-  if (len==1) gen_free_list(le);
+      result = make_call_expression(operator, le);
+  }
+
   sc_rm(s);
-
-  return(result);
+  return result;
 }
 
 /* this function checks whether the lower and upper constraints
  * are going to generate the same bound on variable var.
  */
-bool bounds_equal_p(var, lower, upper)
-Variable var;
-Pcontrainte lower, upper;
+bool 
+bounds_equal_p(
+    Variable var,
+    Pcontrainte lower,
+    Pcontrainte upper)
 {
-    Pvecteur
-	v_lower,
-	v_upper,
-	sum;
-    Value
-	val_lower, 
-	val_upper,
-	the_ppcm;
-    bool
-	result;
+    Pvecteur v_lower, v_upper, sum;
+    Value val_lower, val_upper, the_ppcm;
+    bool result;
 
     if (nb_elems_list(lower)!=1 || nb_elems_list(upper)!=1) return(FALSE);
 
@@ -268,7 +249,7 @@ Pcontrainte lower, upper;
     
     /* ??? the arithmetic ppcm version is on int instead of values 
      */
-    the_ppcm = ppcm(-val_lower,val_upper);
+    the_ppcm = ppcm(-val_lower, val_upper);
 
     v_lower = vect_dup(lower->vecteur);
     v_lower = vect_multiply(v_lower, -the_ppcm/val_lower);
@@ -285,7 +266,7 @@ Pcontrainte lower, upper;
 
     vect_rm(v_lower), vect_rm(v_upper), vect_rm(sum);
 
-    return(result);
+    return result;
 }
 
 /* sc is used to generate the loop nest bounds for variables vars.
@@ -293,20 +274,20 @@ Pcontrainte lower, upper;
  *
  * sc is not touched...
  */
-statement systeme_to_loop_nest(sc, vars, body, divide)
-Psysteme sc;
-list vars;
-statement body;
-entity divide; /* I have to give the divide entity to be called */
+statement 
+systeme_to_loop_nest(
+    Psysteme sc,
+    list /* of entity */ vars,
+    statement body,
+    entity divide) /* I have to give the divide entity to be called */
 {
     range rg;
-    Variable var;
     Pcontrainte	c, lower, upper;
     list reverse;
     statement assign, current = body;
     Psysteme s;
     
-    if (ENDP(vars)) return(body);
+    if (ENDP(vars)) return body;
 
     s = sc_dup(sc);
     sc_transform_eg_in_ineg(s);  /* ??? could do a better job with = */
@@ -314,11 +295,11 @@ entity divide; /* I have to give the divide entity to be called */
 
     reverse = gen_nreverse(gen_copy_seq(vars));
 
-    assert(sc_nbre_egalites(s)==0);
+    message_assert("no equalities, now", sc_nbre_egalites(s)==0);
     
     MAP(ENTITY, e,
     {
-	var = (Variable) e;
+	Variable var = (Variable) e;
 	
 	pips_debug(5, "variable %s loop\n", entity_name((entity) var));
 	
@@ -371,28 +352,27 @@ entity divide; /* I have to give the divide entity to be called */
     gen_free_list(reverse);
     sc_inegalites(s)=c, sc_rm(s);
 
-    return(current);
+    return current;
 }
 
-/*
- * statement generate_optional_if(sc, stat)
+/* statement generate_optional_if(sc, stat)
  *
  * if sc is Z^n then no if is required,
  * if sc is empty, then statement is nop,
  * else an if is required
  */
-statement generate_optional_if(sc, stat)
-Psysteme sc;
-statement stat;
+statement 
+generate_optional_if(
+    Psysteme sc,
+    statement stat)
 {
     if (sc_rn_p(sc)) return(stat);
     if (sc_empty_p(sc)) return(make_empty_statement());
 
-    return(st_make_nice_test(Psysteme_to_expression(sc),
+    return st_make_nice_test(Psysteme_to_expression(sc),
 			     CONS(STATEMENT, stat, NIL),
-			     NIL));
+			     NIL);
 }
 
-/*
- * that's all
+/* that is all
  */
