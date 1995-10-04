d88252725801a0676f29bccddd63d126e90ba47d
platonof <platonof@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-04 15:07:07 +0000

    # on 1995/10/04 15:07:07, platonof did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2600 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/paf-util/paf-util-local.h
@@ -0,0 +1,19 @@
+
+#define PIP_SOLVE_MIN 0
+#define PIP_SOLVE_MAX 1
+
+#define IS_MIN 0
+#define IS_MAX 1
+
+#define PAF_UTIL_MODULE_NAME "PAFUTIL"
+#define DFG_MODULE_NAME "DFG"
+#define MAPPING_MODULE_NAME "MAPPING"
+#define DIFF_PREFIX "DIFF"
+#define COEFF_PREFIX "COEFF"
+
+#define POSITIVE 1
+#define NEGATIVE 0
+
+#define ADD_ELEMENT_TO_LIST( _list, _type, _element) \
+    (_list = gen_nconc( _list, CONS( _type, _element, NIL)))
+
