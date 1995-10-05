5b1db276ae3d5a0c1a311dff0b6dbedb6459ecc3
oget <oget@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-05 11:42:42 +0000

    # on 1995/10/05 11:42:42, oget did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2663 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/pipsmake/pipsmake-local.h
+++ src/Libs/pipsmake/pipsmake-local.h
@@ -1,6 +1,7 @@
 /* From readmakefile.y sunce the .y is not passed through cproto (RK): */
 extern makefile parse_makefile();
 extern rule find_rule_by_phase(string pname);
-
+extern bool close_makefile();
+extern makefile open_makefile();
 typedef bool (*pipsmake_callback_handler_type)(void);
 
