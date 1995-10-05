f7dd1e8a05a1c0b653c5b840d1d4545bf3a0d55f
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-05 10:09:42 +0000

    # on 1995/10/05 10:09:42, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2634 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/misc/misc-local.h
+++ src/Libs/misc/misc-local.h
@@ -9,21 +9,42 @@
 
 #define ifdebug(l) if(the_current_debug_level>=(l))
 
-/* a debug macro that generates automatically the function name if available.
+/* these macros use the GNU extensions that allow to know the 
+ * function name thru the special __FUNCTION__ macro. Also variable
+ * number of arguments macros are used.
+ * if not available, other macros or function calls are generated.
+ * Fabien.
  */
 #ifdef __GNUC__
 #define pips_debug(level, format, args...)\
  ifdebug(level) fprintf(stderr, "[" __FUNCTION__  "] " format, ##args);
 #define pips_user_warning(format, args...)\
   user_warning(__FUNCTION__, format, ##args)
+#define pips_user_error(format, args...)\
+  user_error(__FUNCTION__, format, ##args)
+#define pips_assert(what, predicate)\
+  if(!(predicate)){\
+    (void) fprintf(stderr, \
+    "[%s] (%s:%d) assertion failed\n", __FUNCTION__, __FILE__, __LINE__);\
+    (void) fprintf(stderr, "\n %s not verified\n\n", what); abort();}
+#define pips_internal_error(format, args...)\
+  pips_error(__FUNCTION__, format, ##args)
 #else
 #define pips_debug pips_debug_function
 #define pips_user_warning pips_user_warning_function
+#define pips_user_error pips_user_error_function
+#define pips_internal_error pips_internal_error_function
+#define pips_assert(what, predicate)\
+  if(!(predicate)){\
+    (void) fprintf(stderr, \
+    "(%s:%d) assertion failed\n", __FILE__, __LINE__);\
+    (void) fprintf(stderr, "\n %s not verified\n\n", what); abort();}
 #endif
 
 /* USE message_assert or assert instead, defined in newgen.
  */
-#define pips_assert(f, p) pips_assert_function((f), (p), __LINE__, __FILE__)
+/* #define pips_assert(f, p) pips_assert_function((f), (p), __LINE__, __FILE__)
+ */
 
 #define same_string_p(s1, s2) (strcmp((s1), (s2)) == 0)
 
