dbdda88e35db437610ea8354cea82a6aa9e0fc39
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-10 19:45:29 +0000

    # on 1995/10/10 19:45:29, irigoin did: Euh...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2834 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/semantics/prettyprint.c
+++ src/Libs/semantics/prettyprint.c
@@ -1,7 +1,7 @@
-/* 	%A% ($Date: 1995/10/05 23:14:23 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.	 */
+/* 	%A% ($Date: 1995/10/10 19:45:29 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.	 */
 
 #ifndef lint
-static char vcid[] = "%A% ($Date: 1995/10/05 23:14:23 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.";
+static char vcid[] = "%A% ($Date: 1995/10/10 19:45:29 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.";
 #endif /* lint */
 
  /* package semantics - prettyprint interface */
@@ -165,7 +165,7 @@ bool give_code_p;
 	ifdebug(1) {
 	    print_number_to_statement(nts);
 	}
-	debug_off();
+	/* debug_off(); */
     }
 
 
