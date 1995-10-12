08d968f413bc6209053f96f25e9d5d8740d5669a
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-12 14:02:51 +0000

    # on 1995/10/12 14:02:51, irigoin did: Split into reorder.c and control.c. New module in reorder.c.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2856 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/control/control.c
+++ src/Libs/control/control.c
@@ -782,84 +782,6 @@ control top, bottom, res;
     return(u);
 }
 
-/* These functions compute the statement_ordering of their arguments. 
-   U_NUMBER is the current unstructured number and *S_NUMBER the current
-   statement number, in the current U_NUMBER. */
-
-static int u_number;
-#define RESET_UNSTRUCTURED_NUMBER {u_number = 1;}
-#define NEW_UNSTRUCTURED_NUMBER (u_number++)
-
-static int statement_reorder(st, un, sn)
-statement st;
-int un, sn;
-{
-    instruction i = statement_instruction(st);
-    void unstructured_reorder();
-
-    debug(5, "statement_reorder", "entering for %d : (%d,%d)\n",
-	  statement_number(st), un, sn);
-
-    statement_ordering(st) = MAKE_ORDERING(un, sn);
-
-    sn += 1;
-
-    switch (instruction_tag(i)) {
-      case is_instruction_block:
-	debug(5, "statement_reorder", "block\n");
-	MAPL(sts, {
-	    sn = statement_reorder(STATEMENT(CAR(sts)), un, sn);
-	}, instruction_block(i));
-	break;
-      case is_instruction_test:
-	debug(5, "statement_reorder", "test\n");
-	sn = statement_reorder(test_true(instruction_test(i)), un, sn);
-	sn = statement_reorder(test_false(instruction_test(i)), un, sn);
-	break;
-      case is_instruction_loop:
-	debug(5, "statement_reorder", "loop\n");
-	sn = statement_reorder(loop_body(instruction_loop(i)), un, sn);
-	break;
-      case is_instruction_goto:
-      case is_instruction_call:
-	debug(5, "statement_reorder", "goto or call\n");
-	break;
-      case is_instruction_unstructured: 
-	debug(5, "statement_reorder", "unstructured\n");
-	unstructured_reorder(instruction_unstructured(i));
-	break;
-      default:
-	pips_error("statement_reorder", "Unknown tag %d\n", 
-		   instruction_tag(i));
-    }
-
-    debug(5, "statement_reorder", "exiting %d\n", sn);
-
-    return(sn);
-}
-
-void unstructured_reorder(u)
-unstructured u;
-{
-    cons *blocs = NIL;
-
-    debug(5, "unstructured_reorder", "entering\n");
-
-    CONTROL_MAP(ctl, {
-	statement st = control_statement(ctl);
-	int un = NEW_UNSTRUCTURED_NUMBER;
-
-	debug(5, "unstructured_reorder", "will reorder %d %d\n",
-	      statement_number(st), un);
-
-	statement_reorder(st, un, 1);
-    }, unstructured_control(u), blocs);
-
-    gen_free_list(blocs);
-
-    debug(5, "unstructured_reorder", "exiting\n");
-}
-
 /* CONTROL_GRAPH returns the control graph of the statement ST. */
 
 unstructured control_graph(st) 
@@ -906,26 +828,10 @@ statement st;
 	    fprintf(stderr, "%x, ", (unsigned int) ctl) ;
 	}, unstructured_control(u), blocs);
     }
-    RESET_UNSTRUCTURED_NUMBER;
+    reset_unstructured_number();
     unstructured_reorder(u);
 
     debug_off();
 
     return(u);
-}
-
-void module_body_reorder(body)
-statement body;
-{
-    RESET_UNSTRUCTURED_NUMBER;
-
-    debug_on("CONTROL_DEBUG_LEVEL");
-
-    /* FI: I do not understand why unstructured numbering is not
-     * restarted from 0.
-     * statement_reorder(body, NEW_UNSTRUCTURED_NUMBER, 1);
-     */
-    statement_reorder(body, 0, 1);
-
-    debug_off();
 }
