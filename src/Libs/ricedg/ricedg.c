7b67d6027347cb720cd23f9526654fd5f67d4e3c
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-13 15:50:42 +0000

    # on 1995/10/13 15:50:42, irigoin did: Restructuring of ricedg.c into ricedg.c, quick_privatize.c and prettyprint.c
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2906 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ricedg/ricedg.c
+++ src/Libs/ricedg/ricedg.c
@@ -1,7 +1,7 @@
 
 /* Dependence Graph computation for Allen & Kennedy algorithm
  *
- * Remi Triolet
+ * Remi Triolet, Yi-qing Yang
  *
  * Modifications:
  *  - new option to use semantics analysis results (Francois Irigoin, 
@@ -17,6 +17,9 @@
  *
  *  - TestDependence split into different procedures for more readability.
  *    (BC, August 1995).
+ *
+ *  - creation of quick_privatize.c and prettyprint.c to reduce 
+ *    the size of ricedg.c (FI, Oct. 1995)
  *
  * Notes:
  *  - Many values seem to be assigned to StatementToContext and never be
@@ -141,7 +144,7 @@ static bool PRINT_RSDG = FALSE;
 #define DG_FULL 2
 #define DG_SEMANTICS 3
 
-static dg_type = DG_FAST;
+static int dg_type = DG_FAST;
 
     
 /*********************************************************************************/
@@ -212,14 +215,9 @@ static void dependence_system_add_lci_and_di(Psysteme */*psc_dep*/,
 static list TestDiVariables(Psysteme /*ps*/, int /*cl*/, statement /*s1*/, 
 			     effect /*ef1*/, statement /*s2*/, effect /*ef2*/);
 static Ptsg dependence_cone_positive(Psysteme /*dep_sc*/);
-static void quick_privatize_graph(graph /*dep_graph*/);
-static bool quick_privatize_loop(statement /*stat*/, list /*successors*/);
-static bool quick_privatize_statement_pair(statement /*s1*/, statement /*s2*/, 
-					   list /*conflicts*/);
 static list loop_variant_list(statement /*stat*/);
 static boolean TestDiCnst(Psysteme /*ps*/, int /*cl*/, statement /*s1*/, 
 			  effect /*ef1*/, statement /*s2*/, effect /*ef2*/);
-static void writeresult(char */*mod_name*/);
 
 
 
@@ -1884,148 +1882,6 @@ Psysteme dep_sc;
 }
 
 
-/*********************************************************************************/
-/* QUICK PRIVATIZATION                                                           */
-/*********************************************************************************/
-
-static void quick_privatize_graph(dep_graph)
-graph dep_graph;
-{
-
-    /* we analyze arcs exiting from loop statements */
-    MAP(VERTEX, v1, 
-    {
-	statement s1 = vertex_to_statement(v1);
-	list successors = vertex_successors(v1);
-	
-	if (statement_loop_p(s1)) 
-	{
-	    loop l = statement_loop(s1);
-	    list locals = loop_locals(l);
-	    entity ind = loop_index(l);
-	    
-	    if (gen_find_eq(ind, locals) == entity_undefined) 
-	    {
-		if (quick_privatize_loop(s1, successors)) 
-		{
-		    debug(8, "quick_privatize_graph", 
-			  "loop %d privatized\n", statement_number(s1));
-		    
-		    loop_locals(l) = CONS(ENTITY, ind, locals);
-		}
-		else 
-		{
-		    debug(1, "quick_privatize_graph", 
-			  "could not privatize loop %d\n", statement_number(s1));
-		}
-	    }
-	}
-    },
-	graph_vertices(dep_graph) );
-}
-
-
-
-static bool quick_privatize_loop(stat, successors)
-statement stat;
-list successors;
-{
-
-    debug(3, "quick_privatize_loop", "arcs from %d\n", statement_number(stat));
-
-    MAP(SUCCESSOR, su,
-    {
-	dg_arc_label dal = (dg_arc_label) successor_arc_label(su);
-	statement st = vertex_to_statement(successor_vertex(su));
-	
-	debug(3, "quick_privatize_loop", "arcs to %d\n", statement_number(st));
-	
-	if (! quick_privatize_statement_pair(stat, st, 
-					     dg_arc_label_conflicts(dal)))
-	    return(FALSE);
-    },
-	successors );
-    
-    return(TRUE);
-}
-
-
-
-static bool quick_privatize_statement_pair(s1, s2, conflicts)
-statement s1, s2;
-list conflicts;
-{
-    loop l1 = statement_loop(s1);
-    entity ind1 = loop_index(l1);
-
-    MAP(CONFLICT, c,
-    {
-	effect f1 = conflict_source(c);
-	reference r1 = effect_reference(f1);
-	entity e1 = reference_variable(r1);
-
-	effect f2 = conflict_sink(c);
-	reference r2 = effect_reference(f2);
-	entity e2 = reference_variable(r2);
-
-	debug(2, "quick_privatize_statement_pair", 
-	      "conflict between %s & %s\n", entity_name(e1), entity_name(e2));
-
-	/* equivalence or conflict not created by loop index. I give up ! */
-	if (e1 != ind1)  
-	    continue; 
-	
-	if (action_write_p(effect_action(f1)) && 
-	    action_read_p(effect_action(f2))) 
-	{
-	    /* we must know where this read effect come from. if it
-	       comes from the loop body, the arc may be ignored. */
-
-	    list loops = load_statement_enclosing_loops(s2);
-
-	    if (gen_find_eq(s1, loops) == entity_undefined) 
-	    {
-		loop l2;
-		entity ind2;
-		list range_effects;
-
-		debug(3, "quick_privatize_statement_pair", 
-		      "the arc goes outside the loop body.\n");
-
-		if (! statement_loop_p(s2)) 
-		{
-		    debug(3, "quick_privatize_statement_pair", "s2 not a loop\n"); 
-		    return(FALSE);
-		}
-
-		/* s2 is a loop. if there are no read effet in the range
-		   part, ignore this conflict. */
-		l2 = statement_loop(s2);
-		ind2 = loop_index(l2);
-		range_effects = proper_effects_of_range(loop_range(l2), 
-							is_action_read);
-
-		MAP(EFFECT, e, 
-		{
-		    if (reference_variable(effect_reference(e)) == ind2 &&
-			action_read_p(effect_action(e))) 
-		    {
-			
-			debug(3, "quick_privatize_statement_pair", 
-			      "index read in range expressions\n"); 
-			
-			free_effects(make_effects(range_effects));
-			return(FALSE);
-		    }
-		}, range_effects);
-		free_effects(make_effects(range_effects));
-	    }
-	}
-    },
-	conflicts );
-    
-    return(TRUE);
-}
 
 
 
@@ -2094,84 +1950,10 @@ effect ef1,ef2;
 
 
 
-/*********************************************************************************/
-/* DG PRINTING FUNCTIONS                                                         */
-/*********************************************************************************/
-
-bool print_whole_dependence_graph(mod_name)
-string mod_name;
-{
-    set_bool_property("PRINT_DEPENDENCE_GRAPH_WITHOUT_PRIVATIZED_DEPS",
-		      FALSE);
-    set_bool_property("PRINT_DEPENDENCE_GRAPH_WITHOUT_NOLOOPCARRIED_DEPS",
-		      FALSE);
-    return print_dependence_graph(mod_name);
-}
-
-bool print_effective_dependence_graph(mod_name)
-string mod_name;
-{
-    set_bool_property("PRINT_DEPENDENCE_GRAPH_WITHOUT_PRIVATIZED_DEPS",
-		      TRUE);
-    set_bool_property("PRINT_DEPENDENCE_GRAPH_WITHOUT_NOLOOPCARRIED_DEPS",
-		      FALSE);
-    return print_dependence_graph(mod_name);
-}
-
-bool print_loop_carried_dependence_graph(mod_name)
-string mod_name;
-{
-    set_bool_property("PRINT_DEPENDENCE_GRAPH_WITHOUT_PRIVATIZED_DEPS",
-		      TRUE);
-    set_bool_property("PRINT_DEPENDENCE_GRAPH_WITHOUT_NOLOOPCARRIED_DEPS",
-		      TRUE);
-    return print_dependence_graph(mod_name);
-}
-
-bool print_dependence_graph(mod_name)
-string mod_name;
-{
-    string dg_name = NULL;
-    string local_dg_name = NULL;
-    graph dg;
-    FILE *fp;
-    statement mod_stat;
-
-    set_current_module_entity(local_name_to_top_level_entity(mod_name));
-    set_current_module_statement( (statement)
-	db_get_memory_resource(DBR_CODE, mod_name, TRUE) );
-    mod_stat = get_current_module_statement();
-
-    dg = (graph) db_get_memory_resource(DBR_DG, mod_name, TRUE);
-
-    debug_on("RICEDG_DEBUG_LEVEL");
-
-    local_dg_name = (string) strdup(concatenate(mod_name, ".dg", NULL));
-    dg_name = (string) strdup(concatenate(db_get_current_workspace_directory(), "/", 
-					  local_dg_name, NULL));
-    fp = safe_fopen(dg_name, "w");
-    if (get_bool_property("PRINT_DEPENDENCE_GRAPH_WITHOUT_PRIVATIZED_DEPS") || 
-	get_bool_property("PRINT_DEPENDENCE_GRAPH_WITHOUT_NOLOOPCARRIED_DEPS")) {
-	print_graph_with_reduction(fp, mod_stat, dg);
-    }
-    else  
-	print_graph(fp, mod_stat, dg);
-    safe_fclose(fp, dg_name);
-
-    debug_off();
-    
-    DB_PUT_FILE_RESOURCE(DBR_DG_FILE, strdup(mod_name), 
-			 local_dg_name);
-
-    reset_current_module_statement();
-    reset_current_module_entity();
-    free(dg_name);
-    return TRUE;
-}
 
 
 
-static void writeresult(mod_name)
+void writeresult(mod_name)
 char *mod_name;
 {
     FILE *fp;
