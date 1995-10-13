c987b6d0330240b2e4fe724b1812578380f2942c
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-13 15:51:37 +0000

    # on 1995/10/13 15:51:37, irigoin did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2909 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/ricedg/quick_privatize.c
@@ -0,0 +1,183 @@
+/*********************************************************************************/
+/* QUICK PRIVATIZATION                                                           */
+/*********************************************************************************/
+
+#include <stdio.h>
+/* #include <values.h> */
+
+#include "genC.h"
+#include "text.h"
+#include "ri.h"
+#include "graph.h"
+#include "dg.h"
+
+/* instantiation of the dependence graph */
+typedef dg_arc_label arc_label;
+typedef dg_vertex_label vertex_label;
+
+/* #include "database.h" */
+
+#include "misc.h"
+/* #include "text-util.h" */
+
+#include "ri-util.h" /* linear.h is included in */
+#include "effects.h"
+/*
+#include "control.h"
+#include "pipsdbm.h"
+#include "semantics.h"
+
+#include "constants.h"
+#include "properties.h"
+*/
+
+/* includes pour system generateur */
+#include "ray_dte.h"
+#include "sommet.h"
+#include "sg.h"
+#include "polyedre.h"
+
+#include "ricedg.h"
+
+static bool quick_privatize_loop(statement /*stat*/, list /*successors*/);
+static bool quick_privatize_statement_pair(statement /*s1*/, statement /*s2*/, 
+					   list /*conflicts*/);
+
+void quick_privatize_graph(dep_graph)
+graph dep_graph;
+{
+
+    /* we analyze arcs exiting from loop statements */
+    MAP(VERTEX, v1, 
+    {
+	statement s1 = vertex_to_statement(v1);
+	list successors = vertex_successors(v1);
+	
+	if (statement_loop_p(s1)) 
+	{
+	    loop l = statement_loop(s1);
+	    list locals = loop_locals(l);
+	    entity ind = loop_index(l);
+	    
+	    if (gen_find_eq(ind, locals) == entity_undefined) 
+	    {
+		if (quick_privatize_loop(s1, successors)) 
+		{
+		    debug(8, "quick_privatize_graph", 
+			  "loop %d privatized\n", statement_number(s1));
+		    
+		    loop_locals(l) = CONS(ENTITY, ind, locals);
+		}
+		else 
+		{
+		    debug(1, "quick_privatize_graph", 
+			  "could not privatize loop %d\n", statement_number(s1));
+		}
+	    }
+	}
+    },
+	graph_vertices(dep_graph) );
+}
+
+
+
+static bool quick_privatize_loop(stat, successors)
+statement stat;
+list successors;
+{
+    debug(3, "quick_privatize_loop", "arcs from %d\n", statement_number(stat));
+
+    MAP(SUCCESSOR, su,
+    {
+	dg_arc_label dal =  (dg_arc_label) successor_arc_label(su);
+	statement st = vertex_to_statement(successor_vertex(su));
+	
+	debug(3, "quick_privatize_loop", "arcs to %d\n", statement_number(st));
+	
+	if (! quick_privatize_statement_pair(stat, st, 
+					     dg_arc_label_conflicts(dal)))
+	    return(FALSE);
+    },
+	successors );
+
+    return(TRUE);
+}
+
+
+
+static bool quick_privatize_statement_pair(s1, s2, conflicts)
+statement s1, s2;
+list conflicts;
+{
+    loop l1 = statement_loop(s1);
+    entity ind1 = loop_index(l1);
+
+    MAP(CONFLICT, c,
+    {
+	effect f1 = conflict_source(c);
+	reference r1 = effect_reference(f1);
+	entity e1 = reference_variable(r1);
+
+	effect f2 = conflict_sink(c);
+	reference r2 = effect_reference(f2);
+	entity e2 = reference_variable(r2);
+
+	debug(2, "quick_privatize_statement_pair", 
+	      "conflict between %s & %s\n", entity_name(e1), entity_name(e2));
+
+	/* equivalence or conflict not created by loop index. I give up ! */
+	if (e1 != ind1)  
+	    continue; 
+	
+	if (action_write_p(effect_action(f1)) && 
+	    action_read_p(effect_action(f2))) 
+	{
+	    /* we must know where this read effect come from. if it
+	       comes from the loop body, the arc may be ignored. */
+
+	    list loops = load_statement_enclosing_loops(s2);
+
+	    if (gen_find_eq(s1, loops) == entity_undefined) 
+	    {
+		loop l2;
+		entity ind2;
+		list range_effects;
+
+		debug(3, "quick_privatize_statement_pair", 
+		      "the arc goes outside the loop body.\n");
+
+		if (! statement_loop_p(s2)) 
+		{
+		    debug(3, "quick_privatize_statement_pair", "s2 not a loop\n"); 
+		    return(FALSE);
+		}
+
+		/* s2 is a loop. if there are no read effet in the range
+		   part, ignore this conflict. */
+		l2 = statement_loop(s2);
+		ind2 = loop_index(l2);
+		range_effects = proper_effects_of_range(loop_range(l2), 
+							is_action_read);
+
+		MAP(EFFECT, e, 
+		{
+		    if (reference_variable(effect_reference(e)) == ind2 &&
+			action_read_p(effect_action(e))) 
+		    {
+			
+			debug(3, "quick_privatize_statement_pair", 
+			      "index read in range expressions\n"); 
+			
+			free_effects(make_effects(range_effects));
+			return(FALSE);
+		    }
+		}, range_effects);
+		free_effects(make_effects(range_effects));
+	    }
+	}
+    },
+	conflicts );
+    
+    return(TRUE);
+}
+
