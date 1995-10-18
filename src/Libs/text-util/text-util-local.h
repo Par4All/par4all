e675eabd852d9438c38699fc3d4cdd3e11f67f55
oget <oget@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-18 15:28:20 +0000

    # on 1995/10/18 15:28:20, oget did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2976 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/text-util/text-util-local.h
@@ -0,0 +1,27 @@
+#define PIPS_COMMENT_PREFIX "C$PIPS"
+
+#define MAKE_SWORD(s) strdup(s)
+#define MAKE_IWORD(i) i2a(i)
+#define MAKE_FWORD(f) f2a(f)
+
+#define CHAIN_SWORD(l,s) gen_nconc(l,CONS(STRING, MAKE_SWORD(s), NIL))
+#define CHAIN_IWORD(l,i) gen_nconc(l,CONS(STRING, MAKE_IWORD(i), NIL))
+#define CHAIN_FWORD(l,f) gen_nconc(l,CONS(STRING, MAKE_FWORD(f), NIL))
+
+#define MAKE_ONE_WORD_SENTENCE(m, s) \
+    make_sentence(is_sentence_unformatted, \
+		  make_unformatted((char *) NULL, 0, m, CHAIN_SWORD(NIL, s)))
+
+#define ADD_SENTENCE_TO_TEXT(t,p) { \
+       text _t_ = (t); \
+       text_sentences(_t_) = \
+	   gen_nconc(text_sentences(_t_), CONS(SENTENCE, (p), NIL));\
+       }
+
+#define MERGE_TEXTS(r,t) { \
+       text _r_ = (r), _t_ = (t); \
+       text_sentences(_r_) = \
+	   gen_nconc(text_sentences(_r_), text_sentences(_t_)); \
+       text_sentences(_t_) = NIL; \
+       gen_free(_t_); \
+       }
