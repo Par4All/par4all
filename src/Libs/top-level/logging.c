5705f8bcfbea727f7b49dea356016ef8b1dc1ad9
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-23 11:07:57 +0000

    # on 1995/10/23 11:07:57, irigoin did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@2996 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/top-level/logging.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#include "genC.h"
+#include "misc.h"
+#include "database.h"
+
+#define LOG_FILE "LOGFILE"
+/* The log file is closed by default */
+static FILE *log_file = NULL;
+/* Used in information messages */
+char log_file_name[MAXPATHLEN];
+
+
+
+void
+close_log_file()
+{
+   if (log_file != NULL && get_bool_property("USER_LOG_P") == TRUE)
+      if (fclose(log_file) != 0) {
+         perror("close_log_file");
+         abort();
+      }
+   log_file = NULL;
+}
+
+
+void
+open_log_file()
+{
+   if (log_file != NULL)
+      close_log_file();
+
+   if (get_bool_property("USER_LOG_P") == TRUE) {
+      sprintf(log_file_name, "%s/%s",
+              db_get_current_workspace_directory(),
+              LOG_FILE);
+      
+      if ((log_file = fopen(log_file_name, "a")) == NULL) {
+         perror("open_log_file");
+         abort();
+      }
+   }
+}
+
+
+void
+log_on_file(char chaine[])
+{
+   if (log_file != NULL && get_bool_property("USER_LOG_P") == TRUE) {
+      if (fprintf(log_file, "%s", chaine) <= 0) {
+         perror("log_on_file");
+         abort();
+      }
+      else
+         fflush(log_file);
+   }
+}
