c888aca87a7ae3d426e439cd92d9ee7f3f73f11f
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-24 11:24:44 +0000

    # on 1995/10/24 11:24:44, keryell did: Added the functions bool statement_write_argument_of_module_effect(statement s,                                           entity module,                                           statement_mapping effects_list_map) bool statement_io_effect_p(statement_mapping effects_list_map,                       statement s)
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3026 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/effects.c
+++ src/Libs/ri-util/effects.c
@@ -52,3 +52,60 @@ statement_mapping efs_map;
     return l_map;
 }
 
+
+
+/* Return TRUE if the statement has a write effect on at least one of
+   the argument of the module. */
+bool
+statement_write_argument_of_module_effect(statement s,
+                                          entity module,
+                                          statement_mapping effects_list_map)
+{
+   bool write_effect_on_a_module_argument_found = FALSE;
+   list effects_list = (list) GET_STATEMENT_MAPPING(effects_list_map, s);
+
+   MAP(EFFECT, an_effect,
+       {
+          entity a_variable = reference_variable(effect_reference(an_effect));
+          
+          if (action_write_p(effect_action(an_effect))
+              && variable_in_module_p(a_variable, module)) {
+             write_effect_on_a_module_argument_found = TRUE;
+             break;
+          }
+       },
+       effects_list);
+
+   return write_effect_on_a_module_argument_found;
+
+}
+
+
+/* Return true if a statement has an I/O effect in the effects
+   list. */
+bool
+statement_io_effect_p(statement_mapping effects_list_map,
+                      statement s)
+{
+   bool io_effect_found = FALSE;
+   list effects_list = (list) GET_STATEMENT_MAPPING(effects_list_map, s);
+   /* If there is an I/O effects, the following entity should
+      exist. If it does not exist, statement_io_effect_p() will return
+      FALSE anyway. */
+   entity private_io_entity =
+      global_name_to_entity(TOP_LEVEL_MODULE_NAME,
+                            IO_EFFECTS_ARRAY_NAME);
+
+   MAP(EFFECT, an_effect,
+       {
+          entity a_touched_variable =
+             reference_variable(effect_reference(an_effect));
+          if (a_touched_variable == private_io_entity) {
+             io_effect_found = TRUE;
+             break;
+          }
+       },
+       effects_list);
+
+   return io_effect_found;
+}
