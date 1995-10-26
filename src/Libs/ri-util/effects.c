76ade69d1a1ac0d034e496e2f96b0e33afe700d1
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-10-26 11:03:11 +0000

    # on 1995/10/26 11:03:11, keryell did: statement_write_argument_of_module_effect() renamed statement_write_argument_of_module_effect_p().
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3047 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/effects.c
+++ src/Libs/ri-util/effects.c
@@ -57,9 +57,9 @@ statement_mapping efs_map;
 /* Return TRUE if the statement has a write effect on at least one of
    the argument of the module. */
 bool
-statement_write_argument_of_module_effect(statement s,
-                                          entity module,
-                                          statement_mapping effects_list_map)
+statement_write_argument_of_module_effect_p(statement s,
+                                            entity module,
+                                            statement_mapping effects_list_map)
 {
    bool write_effect_on_a_module_argument_found = FALSE;
    list effects_list = (list) GET_STATEMENT_MAPPING(effects_list_map, s);
