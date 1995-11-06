df56e60d8bb634662cce358b23783ffc7d01b2b7
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-11-06 23:09:57 +0000

    # on 1995/11/06 23:09:57, irigoin did: Improved comments. New special name for the SEMANTICS package, with stars to avoid any possible confusion.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3082 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformer/transformer-local.h
+++ src/Libs/transformer/transformer-local.h
@@ -3,15 +3,19 @@
 /* prefix used for value entity names; no conflict should occur with user
  * function names as long as they are restricted to 6 characters
  */
-#define SEMANTICS_MODULE_NAME "SEMANTICS"
+#define SEMANTICS_MODULE_NAME "*SEMANTICS*"
 
+/* Must be used in suffixes and prefixes below */
 #define SEMANTICS_SEPARATOR '#'
 
 /* internal entity names (FI: I should have used suffixes to be consistent with external
  * suffixes */
 #define OLD_VALUE_PREFIX "o#"
 #define INTERMEDIATE_VALUE_PREFIX "i#"
-/* external suffixes */
+
+/* external suffixes (NEW_VALUE_SUFFIX is not used, new values are represented
+ * by the variable itself, i.e. new value suffix is the empty string "")
+ */
 #define NEW_VALUE_SUFFIX "#new"
 #define OLD_VALUE_SUFFIX "#init"
 #define INTERMEDIATE_VALUE_SUFFIX "#int"
