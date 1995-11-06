859e1b02809f3ba7e557e95a69293c8ec427778c
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-11-06 23:50:22 +0000

    # on 1995/11/06 23:50:22, keryell did: Some code to name the RI objects added.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3085 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/attachment_pretty_print.c
+++ src/Libs/ri-util/attachment_pretty_print.c
@@ -3,6 +3,7 @@
 
 #include "genC.h"
 #include "ri.h"
+#include "misc.h"
 #include "word_attachment.h"
 
 /* By default, do not pretty print with emacs (no properties attached
@@ -14,6 +15,62 @@ bool is_attachment_prettyprint = FALSE;
 /* The hash table to associate an Emacs property list to some output
    text: */
 static hash_table word_text_attachment_mapping;
+
+
+/* Mapping from an object to a name and from a name to an object: */
+static hash_table names_of_almost_everything_in_a_module = NULL;
+static hash_table names_to_almost_everything_in_a_module = NULL;
+
+
+/* The unique (well, I hope so...) integer to name an object in PIPS
+   from the extern world. It is not reset since it could be useful to
+   name various instance of the same module for example. I hope that
+   the 2^32 wrap around will not occur too quickly... :-( */
+static int current_name_of_something = 0;
+
+bool
+name_something(chunk * something)
+{
+   hash_put(names_of_almost_everything_in_a_module,
+            (char *) something,
+            (char *) current_name_of_something);
+
+   hash_put(names_to_almost_everything_in_a_module,
+            (char *) current_name_of_something,
+            (char *) something);
+
+   current_name_of_something ++;
+   
+   /* Go on in the RI: */
+   return TRUE;
+}
+
+
+void
+name_almost_everything_in_a_module(statement module)
+{
+   pips_assert("names_of_almost_everything_in_a_module not NULL",
+               names_of_almost_everything_in_a_module == NULL);
+   pips_assert("names_to_almost_everything_in_a_module not NULL",
+               names_to_almost_everything_in_a_module == NULL);
+
+   names_of_almost_everything_in_a_module = hash_table_make(hash_pointer, 0);
+   names_to_almost_everything_in_a_module = hash_table_make(hash_int, 0);
+   
+   gen_multi_recurse(module,
+                     statement_domain, name_something, gen_null,
+                     NULL);
+}
+
+
+void
+free_names_of_almost_everything_in_a_module()
+{
+   hash_table_free(names_of_almost_everything_in_a_module);
+   names_of_almost_everything_in_a_module = NULL;
+   hash_table_free(names_to_almost_everything_in_a_module);
+   names_to_almost_everything_in_a_module = NULL;
+}
 
 
 /* Initialize some things related with the attachment of properties: */
@@ -58,9 +115,9 @@ attach_a_property_to_a_word_text_pointer(void * a_pointer,
 {
    if (is_attachment_prettyprint) {
       attach_a_property_list_to_a_word_text_pointer(a_pointer,
-                                                    CONS(ATTACHMENT,
-                                                         an_attachment,
-                                                         NIL));
+                                                    make_attachments(CONS(ATTACHMENT,
+                                                                          an_attachment,
+                                                                          NIL)));
    }
 }
 
