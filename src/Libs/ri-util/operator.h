181b050aae0e9002d90856d80a7506f7d0de11a7
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-11-14 18:21:22 +0000

    # on 1995/11/14 18:21:22, irigoin did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3147 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/ri-util/operator.h
@@ -0,0 +1,26 @@
+/* FI: these values were taken from syntax/tokyacc.h but I do not 
+ * think they matter.
+ *
+ * MOD was an initial exception. So are MINIMUM and MAXIMUM
+ */
+
+#define AND     55
+#define EQ     56
+#define EQV     57
+#define GE     58
+#define GT     59
+#define LE     60
+#define LT     61
+#define NE     62
+#define NEQV     63
+#define NOT     64
+#define OR     65
+#define MINUS     73
+#define PLUS     74
+#define SLASH     75
+#define STAR     76
+#define POWER     77
+#define MOD       78  /* not evaluated, but later added in IsBinaryOperator*/
+#define CONCAT     84
+#define MINIMUM 85
+#define MAXIMUM 86
