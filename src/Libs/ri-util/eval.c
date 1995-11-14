a42b5a7f65ec5f2fcff611e9bcb8612263d770a9
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-11-14 18:23:51 +0000

    # on 1995/11/14 18:23:51, irigoin did: Processing of modulo added. Constant definitions moved into operator.h to share them with normalize.c.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3150 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/eval.c
+++ src/Libs/ri-util/eval.c
@@ -24,32 +24,7 @@ range: a range is not evaluated.
 
 #include "ri-util.h"
 
-/* FI: these values were taken from synatx/tokyacc.h but I do not 
- * think they matter.
- *
- * MOD was an initial exception. So are MINIMUM and MAXIMUM
- */
-
-#define AND     55
-#define EQ     56
-#define EQV     57
-#define GE     58
-#define GT     59
-#define LE     60
-#define LT     61
-#define NE     62
-#define NEQV     63
-#define NOT     64
-#define OR     65
-#define MINUS     73
-#define PLUS     74
-#define SLASH     75
-#define STAR     76
-#define POWER     77
-#define MOD       78  /* not evaluated, but later added in IsBinaryOperator*/
-#define CONCAT     84
-#define MINIMUM 85
-#define MAXIMUM 86
+#include "operator.h"
 
 value EvalExpression(e)
 expression e;
@@ -220,6 +195,14 @@ cons *la;
 	    constant_int(value_constant(v)) = argl/argr;
 	else {
 	    fprintf(stderr, "[EvalBinaryOp] zero divide\n");
+	    abort();
+	}	
+	break;
+      case MOD:
+	if (argr != 0)
+	    constant_int(value_constant(v)) = argl%argr;
+	else {
+	    fprintf(stderr, "[EvalBinaryOp] zero divide in modulo\n");
 	    abort();
 	}	
 	break;
