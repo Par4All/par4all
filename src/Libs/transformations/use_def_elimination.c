74be5ab27f8da0b0c7531e53e95707e938411ce5
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-11-27 09:46:27 +0000

    # on 1995/11/27 09:46:27, keryell did: Added a guard to avoid putting a label on empty block.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3227 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformations/use_def_elimination.c
+++ src/Libs/transformations/use_def_elimination.c
@@ -400,6 +400,8 @@ remove_this_statement_if_useless(statement s)
    if (! set_belong_p(the_useful_statements, (char *) s)) {
       gen_free(statement_instruction(s));
       statement_instruction(s) = make_instruction_block(NIL);
+      /* Since the RI need to have no label on instruction block: */
+      insert_a_continue_if_a_block_statement_has_a_label(s);
       if (get_debug_level() >= 6)
          fprintf(stderr, "remove_this_statement_if_useless removes statement %#x (%#x).\n", (int) s, statement_ordering(s));
    }
