5adb851f598862f695441b4e42b06a2a3733457b
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-11-27 16:02:40 +0000

    # on 1995/11/27 16:02:40, keryell did: Added a directory menu and a CPU usage display.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3231 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/wpips/xv_status.c
+++ src/Passes/wpips/xv_status.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -20,12 +22,19 @@
 
 #include "wpips.h"
 
-#define DECALAGE_STATUS 100
+enum {DECALAGE_STATUS = 100
+};
+
+/* Max number of digits displayed in the status panel: */
+enum {CPU_USAGE_LENGTH = 5
+};
+
 
 Panel_item directory_name_panel_item;
 Panel_item workspace_name_panel_item,
 memory_name, message, window_number;
 Panel_item module_name_panel_item;
+Panel_item cpu_usage_item;
 
 Server_image status_window_pips_image;
 
@@ -36,7 +45,9 @@ void
 display_memory_usage()
 {
    char memory_string[17];
-
+   char cpu_string[CPU_USAGE_LENGTH + 1];
+   struct rusage an_rusage;
+   
    /* Well, the memory usage is quite approximate in my brain... */
    sprintf(memory_string, "%10.3f", (sbrk(0) - etext)/(double)(1 << 20));
    
@@ -45,6 +56,21 @@ display_memory_usage()
    xv_set(memory_name,
           PANEL_VALUE, memory_string,
           NULL);
+
+   if (getrusage(RUSAGE_SELF, &an_rusage) == 0) {
+      double the_cpu_time =
+         an_rusage.ru_utime.tv_sec + an_rusage.ru_stime.tv_sec
+         + (an_rusage.ru_utime.tv_usec + an_rusage.ru_stime.tv_usec)*1e-6;
+      sprintf(cpu_string, "%*.*g",
+              CPU_USAGE_LENGTH, CPU_USAGE_LENGTH,the_cpu_time);
+   }
+   else
+      /* getrusage() failed: */
+      sprintf(cpu_string, "%*s", CPU_USAGE_LENGTH, "?");
+   
+   xv_set(cpu_usage_item,
+          PANEL_VALUE, cpu_string,
+          NULL);
 }
 
 
@@ -52,6 +78,10 @@ void
 window_number_notify(Panel_item item, int value, Event *event)
 {
    number_of_wpips_windows = (int) xv_get(item, PANEL_VALUE);
+   
+   if (wpips_emacs_mode)
+      send_window_number_to_emacs(number_of_wpips_windows);
+   
    display_memory_usage();
 }
 
@@ -150,7 +180,7 @@ create_status_subwindow()
                 PANEL_VALUE_DISPLAY_LENGTH, 64,
                 PANEL_VALUE_STORED_LENGTH, 1000,
                 NULL);
-
+/*
    directory_name_panel_item = 
       xv_create(main_panel, PANEL_TEXT, 
                 PANEL_VALUE_X, DECALAGE_STATUS,
@@ -161,7 +191,20 @@ create_status_subwindow()
                 PANEL_VALUE_DISPLAY_LENGTH, 64,
                 PANEL_VALUE_STORED_LENGTH, 256,
                 NULL);
+   */
+   directory_name_panel_item =
+      schoose_create_abbrev_menu_with_text(main_panel,
+                                           "Directory:",
+                                           61,
+                                           DECALAGE_STATUS,
+                                           xv_rows(main_panel, 2),
+                                           generate_directory_menu,
+                                           (void (*)(char *)) end_directory_notify);
 
+   xv_set(directory_name_panel_item,
+          PANEL_VALUE_STORED_LENGTH, 256,
+          NULL);
+   
    workspace_name_panel_item =
       schoose_create_abbrev_menu_with_text(main_panel,
                                            "Workspace:",
@@ -190,6 +233,14 @@ create_status_subwindow()
                 PANEL_READ_ONLY, TRUE,
                 NULL);
 
+   cpu_usage_item = xv_create(main_panel, PANEL_TEXT,
+                             PANEL_LABEL_STRING, "CPU (s):", 
+                              PANEL_VALUE_DISPLAY_LENGTH, CPU_USAGE_LENGTH,
+                              PANEL_READ_ONLY, TRUE,
+                              PANEL_VALUE_X, xv_col(main_panel, 68),
+                              PANEL_VALUE_Y, xv_rows(main_panel, 3),
+                              NULL);
+   
    window_number = 
       xv_create(main_panel, PANEL_NUMERIC_TEXT,
                 /*PANEL_ITEM_X_GAP, DECALAGE_STATUS,
@@ -213,7 +264,7 @@ create_status_subwindow()
                 PANEL_NOTIFY_PROC, wpips_interrupt_pipsmake,
                                 /* Put the Pixmap above the Help button: */
                 XV_X, xv_get(quit_button, XV_X) + (xv_get(quit_button, XV_WIDTH) - xv_get(pips_icon_server_image, XV_WIDTH))/2,
-                XV_Y, xv_rows(main_panel, 3) + 10,
+                XV_Y, xv_rows(main_panel, 3) + 20,
                 NULL);
    
    window_fit(main_panel);
