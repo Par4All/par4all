b0f2d8ac7a49a6a7cb5a739339f11f1390a49fb6
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-12-01 13:24:08 +0000

    # on 1995/12/01 13:24:08, irigoin did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3321 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/movements/operation.c
@@ -0,0 +1,34 @@
+/*
+ * PACKAGE MOVEMENTS
+ *
+ * Corinne Ancourt  - 1995
+ */
+
+
+#include <stdio.h>
+#include "genC.h"
+#include "ri.h"
+#include "ri-util.h"
+#include "constants.h"
+#include "misc.h"
+
+expression
+make_div_expression(expression ex1,  cons * ex2)
+{ 
+    entity div = local_name_to_top_level_entity("idiv");
+
+    return(make_expression(
+			   make_syntax(is_syntax_call,
+				       make_call(div,
+						 CONS(EXPRESSION,
+						      ex1,ex2))
+				       ),normalized_undefined));
+}
+
+expression
+make_op_expression(entity op, cons * ex2)
+{
+ return(make_expression(make_syntax(is_syntax_call,
+				    make_call(op,ex2)), 
+			normalized_undefined));
+}
