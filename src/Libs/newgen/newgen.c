264b18c9675a33dc84c576594c27fc9cc7bae169
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1995-12-14 18:03:04 +0000

    # on 1995/12/14 18:03:04, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3383 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/newgen/newgen.c
+++ src/Libs/newgen/newgen.c
@@ -1,8 +1,8 @@
-/* 	%A% ($Date: 1995/08/10 14:12:52 $, ) version $Revision$, got on %D%, %T% [%P%].
+/* 	%A% ($Date: 1995/12/14 18:03:04 $, ) version $Revision$, got on %D%, %T% [%P%].
         Copyright (c) - École des Mines de Paris Proprietary.	 */
 
 #ifndef lint
-static char vcid[] = "%A% ($Date: 1995/08/10 14:12:52 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.";
+char top_level_newgen_c_vcid[] = "%A% ($Date: 1995/12/14 18:03:04 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.";
 #endif /* lint */
 #include <stdio.h>
 
@@ -32,23 +32,42 @@ void initialize_newgen()
     gen_read_spec(ALL_SPECS);
       
     /* initialisation des fonctions d'entrees-sorties pour les types
-       de donnees non geres par NewGen */  
+     * de donnees non geres par NewGen (ou presque:-)
+     */  
     gen_init_external(ARC_LABEL, 
-		      (char *(*)()) gen_read, gen_write, gen_free, 
-		      (char *(*)()) gen_copy_tree);
+		      (char *(*)()) gen_read, 
+		      (void (*)()) gen_write,
+		      (void (*)()) gen_free, 
+		      (char *(*)()) gen_copy_tree,
+		      (int (*)()) gen_allocated_memory);
     gen_init_external(VERTEX_LABEL, 
-		      (char *(*)()) gen_read, gen_write, gen_free,
-		      (char *(*)()) gen_copy_tree);
+		      (char *(*)()) gen_read, 
+		      (void (*)()) gen_write,
+		      (void (*)()) gen_free, 
+		      (char *(*)()) gen_copy_tree,
+		      (int (*)()) gen_allocated_memory);
     gen_init_external(PPOLYNOME, 
-		      (char *(*)()) polynome_gen_read, polynome_gen_write, 
-		      polynome_gen_free, (char *(*)()) polynome_gen_copy_tree);
+		      (char *(*)()) polynome_gen_read, 
+		      (void (*)()) polynome_gen_write, 
+		      (void (*)()) polynome_gen_free,
+		      (char *(*)()) polynome_gen_copy_tree,
+		      (int (*)()) polynome_gen_allocated_memory);
     gen_init_external(PVECTEUR, 
-		      (char *(*)()) vect_gen_read, vect_gen_write, 
-		      vect_gen_free, (char *(*)()) vect_gen_copy_tree);
+		      (char *(*)()) vect_gen_read, 
+		      (void (*)()) vect_gen_write, 
+		      (void (*)()) vect_gen_free,
+		      (char *(*)()) vect_gen_copy_tree,
+		      (int (*)()) vect_gen_allocated_memory);
     gen_init_external(PSYSTEME, 
-		      (char *(*)()) sc_gen_read, sc_gen_write, 
-		      sc_gen_free, (char *(*)()) sc_gen_copy_tree);
+		      (char *(*)()) sc_gen_read, 
+		      (void (*)()) sc_gen_write, 
+		      (void (*)()) sc_gen_free,
+		      (char *(*)()) sc_gen_copy_tree,
+		      (int (*)()) sc_gen_allocated_memory);
     gen_init_external(MATRICE, 
-		      (char *(*)()) matrice_gen_read, matrice_gen_write, 
-		      matrice_gen_free, (char *(*)()) matrice_gen_copy_tree);
+		      (char *(*)()) matrice_gen_read,
+		      (void (*)()) matrice_gen_write, 
+		      (void (*)()) matrice_gen_free,
+		      (char *(*)()) matrice_gen_copy_tree,
+		      (int (*)()) NULL); /* can't get the size... FC */
 }
