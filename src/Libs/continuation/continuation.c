4321463899b786f4ffb8fc7932cfaeb8c4f2275a
creusil <creusil@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-02-26 16:00:30 +0000

    # on 1996/02/26 16:00:30, creusil did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3522 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/continuation/continuation.c
@@ -0,0 +1,35 @@
+/* package continuaiton :  Be'atrice Creusillet, 1996
+ *
+ * This File contains the functions computing continuation conditions 
+ * of a module (over- and under-approximations.
+ *
+ *
+ */
+#include <stdio.h>
+#include <string.h>
+
+#include "genC.h"
+#include "ri.h"
+#include "database.h"
+
+#include "ri-util.h"
+#include "control.h"
+#include "constants.h"
+#include "misc.h"
+#include "text-util.h"
+#include "text.h"
+#include "properties.h"
+
+#include "transformer.h"
+#include "semantics.h"
+
+#include "pipsdbm.h"
+#include "resources.h"
+
+
+
+bool continuation_conditions(char *module_name)
+{
+    return(TRUE);
+}
+
