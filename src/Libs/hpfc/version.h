599c23d1f912b8a148f0996623b5652d94e8700a
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-02-26 11:26:50 +0000

    # on 1996/02/26 11:26:50, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3519 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/hpfc/version.h
@@ -0,0 +1,22 @@
+/* This file is part of HPFC within PIPS.
+ *
+ * $RCSfile: version.h,v $ version $Revision$
+ * %D%, $Date: 1996/02/26 11:26:50 $
+ *
+ * (c) Fabien COELHO
+ */
+
+/* Rough history of HPFC versions:
+ * 
+ * 1.0 (Jun 93): Runtime resolution
+ * 2.0 (Sep 93): Overlaps
+ * 3.0 (Aug 94): IOs
+ * 4.0 (Dec 94): commons/preocedures
+ * 5.0 (Jun 95): remappings
+ * 5.1 (Feb 96): dynamic live copies
+ */
+
+#define HPFC_CURRENT_VERSION "5.1"
+
+/* that is all
+ */
