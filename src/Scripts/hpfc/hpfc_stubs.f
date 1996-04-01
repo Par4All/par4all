19594dbf004322d8640ee16765ed5bb47ca3492e
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-04-01 11:01:37 +0000

    # on 1996/04/01 11:01:37, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3630 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/hpfc/hpfc_stubs.f
+++ src/Scripts/hpfc/hpfc_stubs.f
@@ -5,17 +5,24 @@ c
 c (c) Fabien COELHO, 09/95
 c
 c $RCSfile: hpfc_stubs.f,v $ (version $Revision$)
-c ($Date: 1995/09/05 13:41:45 $, )
-c      
+c ($Date: 1996/04/01 11:01:37 $, )
+c     
+c synchronization 
       subroutine hpfc1()
       print *, 'hpf1: '
       end
+c timer on
       subroutine hpfc2()
       print *, 'hpf2: '
       end
+c timer off
       subroutine hpfc3(comment)
       character comment*(*)
       print *, 'hpf3: ', comment
+      end
+c io/host section marker
+      subroutine hpfc7()
+      print *, 'hpfc7: '
       end
 c
 c That is all
