f8c93dd6a58409ff5c818b2463e07ce82efcb074
creusil <creusil@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-04-02 13:23:08 +0000

    # on 1996/04/02 13:23:08, creusil did: nothing. bc.
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@261 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/sc/sc_new_loop_bound.c
+++ src/sc/sc_new_loop_bound.c
@@ -197,7 +197,7 @@ algorithm_row_echelon(
      */
     ps_tmp =  sc_dup(scn);
     sc_transform_eg_in_ineg(ps_tmp);
-    ps_interm = sc_fusion(ps_interm, ps_tmp);
+    ps_interm = sc_fusion(ps_interm, ps_tmp); 
     ps_interm = sc_triang_elim_redund(ps_interm, base_index); 
 
     *pcondition = get_other_constraints(&ps_interm, base_index);
