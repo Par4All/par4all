85c971f952a26ea76e6c4f4a59125c8bfdbb95c2
creusil <creusil@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-04-19 15:37:02 +0000

    # on 1996/04/19 15:37:02, creusil did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3694 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/comp_sections/comp_sections.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "genC.h"
+#include "ri.h"
+#include "database.h"
+
+#include "ri-util.h"
+#include "control.h"
+#include "constants.h"
+#include "misc.h"
+#include "text-util.h"
+#include "text.h"
+
+
+#include "properties.h"
+
+#include "transformer.h"
+#include "semantics.h"
+
+#include "effects.h"
+#include "regions.h"
+
+#include "pipsdbm.h"
+#include "resources.h"
+
+
+bool summary_complementary_sections(char *module_name)
+{
+    return(TRUE);
+}
+
+bool complementary_sections(char *module_name)
+{
+    return(TRUE);
+}
+
+bool print_complementary_sections(char *module_name)
+{
+    return(TRUE);
+}
+
