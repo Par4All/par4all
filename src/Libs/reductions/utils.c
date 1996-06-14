751420c718e0887514ae5fdbcb35c51ae1c718d6
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-14 16:20:54 +0000

    # on 1996/06/14 16:20:54, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3835 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/reductions/utils.c
@@ -0,0 +1,12 @@
+/* $RCSfile: utils.c,v $ (version $Revision$)
+ * $Date: 1996/06/14 16:20:54 $, 
+ *
+ * utilities for reductions.
+ *
+ * FC, June 1996.
+ */
+
+#include "local-header.h"
+
+/* end of $RCSfile: utils.c,v $
+ */
