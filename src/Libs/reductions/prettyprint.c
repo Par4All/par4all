3711b66cacc20760d18f840eac1818dbecf90879
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-14 16:25:31 +0000

    # on 1996/06/14 16:25:31, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3842 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/reductions/prettyprint.c
+++ src/Libs/reductions/prettyprint.c
@@ -1,5 +1,5 @@
 /* $RCSfile: prettyprint.c,v $ (version $Revision$)
- * $Date: 1996/06/14 16:20:54 $, 
+ * $Date: 1996/06/14 16:25:31 $, 
  *
  * prettyprint of reductions.
  *
@@ -12,16 +12,22 @@ bool print_code_proper_reductions(string module)
 {
     debug_on("REDUCTIONS_DEBUG_LEVEL");
     pips_debug(1, "considering module %s\n", module);
+
     pips_user_warning("not implemented yet\n");
+
     debug_off();
+    return TRUE;
 }
 
 bool print_code_cumulated_reductions(string module)
 {
     debug_on("REDUCTIONS_DEBUG_LEVEL");
     pips_debug(1, "considering module %s\n", module);
+
     pips_user_warning("not implemented yet\n");
+
     debug_off();
+    return TRUE;
 }
 
 /* end of $RCSfile: prettyprint.c,v $
