a18df5a9ace18f760ec5e091ef1cb5b3d9494cf1
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-14 14:29:32 +0000

    # on 1996/06/14 14:29:32, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3832 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/reductions/local-header.h
@@ -0,0 +1,21 @@
+/* $RCSfile: local-header.h,v $ (version $Revision$)
+ * $Date: 1996/06/14 14:29:32 $, 
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "genC.h"
+#include "ri.h"
+#include "database.h"
+#include "resources.h"
+
+#include "misc.h"
+#include "ri-util.h"
+#include "pipsdbm.h"
+
+#include "reduction_private.h"
+#include "reductions.h"
+
+/* end of $RCSfile: local-header.h,v $
+ */
