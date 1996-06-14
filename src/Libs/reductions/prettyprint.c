0dfd331da34b2f6928176faaae598a309e6fe812
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-14 16:20:54 +0000

    # on 1996/06/14 16:20:54, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3834 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/reductions/prettyprint.c
@@ -0,0 +1,28 @@
+/* $RCSfile: prettyprint.c,v $ (version $Revision$)
+ * $Date: 1996/06/14 16:20:54 $, 
+ *
+ * prettyprint of reductions.
+ *
+ * FC, June 1996.
+ */
+
+#include "local-header.h"
+
+bool print_code_proper_reductions(string module)
+{
+    debug_on("REDUCTIONS_DEBUG_LEVEL");
+    pips_debug(1, "considering module %s\n", module);
+    pips_user_warning("not implemented yet\n");
+    debug_off();
+}
+
+bool print_code_cumulated_reductions(string module)
+{
+    debug_on("REDUCTIONS_DEBUG_LEVEL");
+    pips_debug(1, "considering module %s\n", module);
+    pips_user_warning("not implemented yet\n");
+    debug_off();
+}
+
+/* end of $RCSfile: prettyprint.c,v $
+ */
