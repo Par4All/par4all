d2edb9a77e224fc302a41ae7d07b661600b69dd4
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-14 16:21:26 +0000

    # on 1996/06/14 16:21:26, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3836 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/reductions/reductions.c
@@ -0,0 +1,37 @@
+/* $RCSfile: reductions.c,v $ (version $Revision$)
+ * $Date: 1996/06/14 16:21:26 $, 
+ *
+ * detection of simple reductions.
+ * debug driven by REDUCTIONS_DEBUG_LEVEL
+ *
+ * FC, June 1996.
+ */
+
+#include "local-header.h"
+
+bool summary_reductions(string module)
+{
+    debug_on("REDUCTIONS_DEBUG_LEVEL");
+    pips_debug(1, "considering module %s\n", module);
+    pips_user_warning("not implemented yet\n");
+    debug_off();
+}
+
+bool proper_reductions(string module)
+{
+    debug_on("REDUCTIONS_DEBUG_LEVEL");
+    pips_debug(1, "considering module %s\n", module);
+    pips_user_warning("not implemented yet\n");
+    debug_off();
+}
+
+bool cumulated_reductions(string module)
+{
+    debug_on("REDUCTIONS_DEBUG_LEVEL");
+    pips_debug(1, "considering module %s\n", module);
+    pips_user_warning("not implemented yet\n");
+    debug_off();
+}
+
+/* end of $RCSfile: reductions.c,v $
+ */
