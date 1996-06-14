567607f7e8e96a48ef01ea1c4fd2d9e1c58dfd8e
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-14 16:20:53 +0000

    # on 1996/06/14 16:20:53, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3833 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/reductions/transformation.c
@@ -0,0 +1,20 @@
+/* $RCSfile: transformation.c,v $ (version $Revision$)
+ * $Date: 1996/06/14 16:20:53 $, 
+ *
+ * code transformationa for reductions.
+ *
+ * FC, June 1996.
+ */
+
+#include "local-header.h"
+
+bool loop_reductions(string module)
+{
+    debug_on("REDUCTIONS_DEBUG_LEVEL");
+    pips_debug(1, "considering module %s\n", module);
+    pips_user_warning("not implemented yet\n");
+    debug_off();
+}
+
+/* end of $RCSfile: transformation.c,v $
+ */
