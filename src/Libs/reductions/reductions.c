2a484405a8e23f96f16a9dfbbbd906e3e069f3bc
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-14 16:24:39 +0000

    # on 1996/06/14 16:24:39, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3840 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/reductions/reductions.c
+++ src/Libs/reductions/reductions.c
@@ -1,5 +1,5 @@
 /* $RCSfile: reductions.c,v $ (version $Revision$)
- * $Date: 1996/06/14 16:21:26 $, 
+ * $Date: 1996/06/14 16:24:39 $, 
  *
  * detection of simple reductions.
  * debug driven by REDUCTIONS_DEBUG_LEVEL
@@ -13,24 +13,33 @@ bool summary_reductions(string module)
 {
     debug_on("REDUCTIONS_DEBUG_LEVEL");
     pips_debug(1, "considering module %s\n", module);
+
     pips_user_warning("not implemented yet\n");
+
     debug_off();
+    return TRUE;
 }
 
 bool proper_reductions(string module)
 {
     debug_on("REDUCTIONS_DEBUG_LEVEL");
     pips_debug(1, "considering module %s\n", module);
+
     pips_user_warning("not implemented yet\n");
+
     debug_off();
+    return TRUE;
 }
 
 bool cumulated_reductions(string module)
 {
     debug_on("REDUCTIONS_DEBUG_LEVEL");
     pips_debug(1, "considering module %s\n", module);
+
     pips_user_warning("not implemented yet\n");
+
     debug_off();
+    return TRUE;
 }
 
 /* end of $RCSfile: reductions.c,v $
