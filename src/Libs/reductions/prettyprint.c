8fab3fad21a2554d6710fee094ff4c74161449ba
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-15 18:11:18 +0000

    # on 1996/06/15 18:11:18, coelho did: some first version...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3875 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/reductions/prettyprint.c
+++ src/Libs/reductions/prettyprint.c
@@ -1,7 +1,7 @@
 /* $RCSfile: prettyprint.c,v $ (version $Revision$)
- * $Date: 1996/06/15 13:22:08 $, 
+ * $Date: 1996/06/15 18:11:18 $, 
  *
- * prettyprint of reductions.
+ * (pretty)print of reductions.
  *
  * FC, June 1996.
  */
@@ -10,6 +10,8 @@
 #include "text.h"
 #include "semantics.h"
 #include "prettyprint.h"
+
+#define REDUCTION_PREFIX "!"
 
 #define PROP_SUFFIX ".preds"
 #define CUMU_SUFFIX ".creds"
@@ -20,25 +22,28 @@
 GENERIC_LOCAL_FUNCTION(printed_reductions, pstatement_reductions)
 static string reduction_decoration = NULL;
 
-/* allocates and returns the name of the operator
- */
-static string reduction_operator_name(reduction_operator o)
+string reduction_operator_tag_name(tag t)
 {
-    string name = NULL;
-
-    switch(reduction_operator_tag(o))
+    switch(t)
     {
-    case is_reduction_operator_none: name = "none"; break;
-    case is_reduction_operator_sum:  name = "sum";  break;
-    case is_reduction_operator_prod: name = "prod"; break;
-    case is_reduction_operator_min:  name = "min";  break;
-    case is_reduction_operator_max:  name = "max";  break;
-    case is_reduction_operator_and:  name = "and";  break;
-    case is_reduction_operator_or:   name = "or";   break;
+    case is_reduction_operator_none: return "none"; 
+    case is_reduction_operator_sum:  return "sum";  
+    case is_reduction_operator_prod: return "prod"; 
+    case is_reduction_operator_min:  return "min";  
+    case is_reduction_operator_max:  return "max";  
+    case is_reduction_operator_and:  return "and";  
+    case is_reduction_operator_or:   return "or";   
     default: pips_internal_error("unexpected reduction operator tag!");
     }
 
-    return strdup(name);
+    return NULL;
+}
+
+/* allocates and returns the name of the operator
+ */
+string reduction_operator_name(reduction_operator o)
+{
+    return strdup(reduction_operator_tag_name(reduction_operator_tag(o)));
 }
 
 static list /* of string */ words_reduction(reduction r)
@@ -49,23 +54,58 @@ static list /* of string */ words_reduction(reduction r)
 	   CONS(STRING, "],", NIL))));
 }
 
-static list /* of string */ words_reductions(reductions rs)
+static list /* of string */ words_reductions(string note, reductions rs)
 {
     list /* of string */ ls = NIL;
     MAP(REDUCTION, r, 
 	ls = gen_nconc(words_reduction(r), ls),
 	reductions_list(rs));
-    return ls;
+    
+    return ls? CONS(STRING, strdup(note), ls): NIL;
 }
 
-static text text_reductions(statement s)
+static string note_for_statement(statement s)
 {
-    if (!bound_printed_reductions_p(s)) /* unreachable statements */
-	return make_text(NIL);
-    /* else */
+    instruction i = statement_instruction(s);
+    switch (instruction_tag(i))
+    {
+    case is_instruction_sequence: return "seq  ";
+    case is_instruction_loop: return "loop ";
+    case is_instruction_test: return "test ";
+    case is_instruction_call: return "call ";
+    case is_instruction_unstructured: return "unst ";
+    default: pips_internal_error("unexpected instruction tag");
+    }
+    return "";
+}
 
-    return words_predicate_to_commentary
-	(words_reductions(load_printed_reductions(s)), "!");
+static text text_reductions(entity module, int margin, statement s)
+{
+    text t;
+
+    debug_on("REDUCTIONS_DEBUG_LEVEL");
+    pips_debug(1, "considering statement 0x%x\n", (unsigned int) s);
+    
+    t = bound_printed_reductions_p(s)? /* unreachable statements? */
+	words_predicate_to_commentary
+	    (words_reductions(note_for_statement(s),
+			      load_printed_reductions(s)), 
+	     REDUCTION_PREFIX):
+		make_text(NIL);
+
+    debug_off();
+    return t;
+}
+
+static text text_code_reductions(statement s)
+{
+    text t;
+    debug_on("PRETTYPRINT_DEBUG_LEVEL");
+    init_prettyprint(text_reductions);
+    t = text_module(get_current_module_entity(), s);
+    close_prettyprint();
+    debug_off();
+    return t;
 }
 
 static void 
@@ -77,24 +117,19 @@ print_reductions(
 {
     text t;
 
-    set_current_module_entity
-	(local_name_to_top_level_entity(module_name));
+    set_current_module_entity(local_name_to_top_level_entity(module_name));
     set_printed_reductions((pstatement_reductions) 
 	 db_get_memory_resource(resource_name, module_name, TRUE));
-    set_current_module_statement
-	((statement) db_get_memory_resource(DBR_CODE, module_name, TRUE));
+    set_current_module_statement((statement) 
+	 db_get_memory_resource(DBR_CODE, module_name, TRUE));
     reduction_decoration = decoration_name;
 
-    init_prettyprint(text_reductions);
-
-    t = text_module(get_current_module_entity(),
-		    get_current_module_statement());
-
+    t = text_code_reductions(get_current_module_statement());
     (void) make_text_resource(module_name, DBR_PRINTED_FILE, file_suffix, t);
-
     free_text(t);
-    close_prettyprint();
 
+    reset_current_module_entity();
+    reset_current_module_statement();
     reset_printed_reductions();
     reduction_decoration = NULL;
 }
