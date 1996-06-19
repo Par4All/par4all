4ca885370ae4b0d97bcb9b75670e619e42f66c7f
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-19 13:58:34 +0000

    # on 1996/06/19 13:58:34, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3912 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/reductions/call.c
+++ src/Libs/reductions/call.c
@@ -1,5 +1,5 @@
 /* $RCSfile: call.c,v $ (version $Revision$)
- * $Date: 1996/06/18 22:48:49 $, 
+ * $Date: 1996/06/19 13:58:34 $, 
  *
  * Fabien COELHO
  */
@@ -14,108 +14,47 @@
  * Fabien.
  */
 
-static list /* of entity */
-translate_into_local_entities(
-    entity module,
-    call c,
-    entity var)
-{
-    storage st = entity_storage(var);
-
-    pips_debug(7, "entity %s\n", entity_name(var));
-
-    if (storage_formal_p(st))
-    {
-	int offset = formal_offset(storage_formal(st));
-	syntax s = expression_syntax
-	    (EXPRESSION(gen_nth(offset-1, call_arguments(c))));
-	if (syntax_reference_p(s))
-	    return CONS(ENTITY, reference_variable(syntax_reference(s)), NIL);
-	/* else might be binded to some call */
-    }
-    else if (storage_ram_p(st))
-    {
-	effect ef = make_effect(make_reference(var, NIL),
-				make_action(is_action_read, UU),
-				make_approximation(is_approximation_must, UU),
-				make_transformer(NIL, make_predicate(NULL)));
-	list lef = global_effect_translation(ef, call_function(c));
-	list /* of entity */ lent = NIL;
-	
-	MAP(EFFECT, e, lent = gen_once(effect_variable(e), lent), lef);
-
-	gen_map(gen_free, lef); 
-	gen_free_list(lef);
-
-	return lent;
-    }
-    /* else what? */
-    
-    return NIL;
-}
-
-static entity 
-translate_into_one_local_entity(
-    entity module, /* the module we're in */
-    call c,        /* the called function */
-    entity var)    /* the entity to be translated into the module space... */
-{
-    list le = translate_into_local_entities(module, c, var);
-    if (gen_length(le)!=1)
-    {
-	gen_free_list(le);
-	return entity_undefined;
-    }
-    else
-    {
-	entity e = ENTITY(CAR(le));
-	gen_free_list(le);
-	return e;
-    }
-}
-
-/* translate formel_ref under called(real_args) into some new reference.
- * the formal reference may be arbitrary, A(B(I)) for instance...
- * thus reshapings must be managed with great care...
- * I'll do very basic things to begin with, translating only direct
- * one level references.
+/* translation of a reference, based on effect translations...
+ * such a interface should be available?
  */
-bool /* whether ok */
-translate_reference(
+list /* of reference */ 
+summary_to_proper_reference(
     call c,
-    reference external_ref,
-    reference *presult)
+    reference r)
 {
-    entity var =
-	translate_into_one_local_entity(get_current_module_entity(),
-					c, reference_variable(external_ref));
-
-    if (entity_undefined_p(var))
-	return FALSE;
-
-    *presult = make_reference(var, NIL);
-    return TRUE;	
+    effect e = make_effect(r, /* persistent! */
+			   make_action(is_action_read, UU),
+			   make_approximation(is_approximation_must, UU),
+			   make_transformer(NIL, make_predicate(NULL)));
+    list /* of effect */ le = CONS(EFFECT, e, NIL), lef, lref = NIL;
+    
+    lef = summary_to_proper_effects(call_function(c), call_arguments(c), le);
+    
+    MAP(EFFECT, ef, lref = CONS(REFERENCE, effect_reference(ef), lref), lef);
+    
+    gen_map(gen_free, lef); gen_free_list(lef);
+    return lref;
 }
 
-static bool /* whether ok */
+static list /* of reduction */
 translate_reduction(
     call c,
-    reduction external_red,
-    reduction *pred)
+    reduction external_red)
 {
-    reference ref;
-    if (translate_reference(c, reduction_reference(external_red), &ref))
+    reference ref = reduction_reference(external_red);
+    list /* of reference */ lref = summary_to_proper_reference(c, ref),
+         /* of reduction */ lrds = NIL;
+
+    MAP(REFERENCE, r, 
     {
-	*pred = copy_reduction(external_red);
-	free_reference(reduction_reference(*pred));
-	reduction_reference(*pred) = ref;
-	gen_free_list(reduction_dependences(*pred));
-	reduction_dependences(*pred) = NIL;
+	reduction red = copy_reduction(external_red);
+	reduction_reference(red) = r;
+	lrds = CONS(REDUCTION, red, lrds);
+    },
+	lref);
 
-	return TRUE;
-    }
-
-    return FALSE;
+    gen_free_list(lref); /* just the backbone, refs are pointed to now */
+    return lrds;
 }
 
 list /* of reduction */
@@ -128,11 +67,9 @@ translate_reductions(
     {
 	reductions rs = load_summary_reductions(fun);
 	list lr = NIL;
-	reduction translated;
 
-	MAP(REDUCTION, r,
-	    if (translate_reduction(c, r, &translated))
-	        lr = CONS(REDUCTION, translated, lr),
+	MAP(REDUCTION, r, 
+	    lr = gen_nconc(translate_reduction(c, r), lr),
 	    reductions_list(rs));
 	
 	return lr;
