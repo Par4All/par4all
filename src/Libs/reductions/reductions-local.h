327365794f660fc44bb3e26b913d09cbce423524
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-06-21 17:50:09 +0000

    # on 1996/06/21 17:50:09, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@3921 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/reductions/reductions-local.h
+++ src/Libs/reductions/reductions-local.h
@@ -1,16 +1,10 @@
 /* $RCSfile: reductions-local.h,v $ (version $Revision$)
- * $Date: 1996/06/17 18:11:50 $, 
+ * $Date: 1996/06/21 17:50:09 $, 
  */
 /* shorthands for REDUCTION:
  */
 #define reduction_variable(r) reference_variable(reduction_reference(r))
 #define reduction_none_p(r) reduction_operator_none_p(reduction_op(r))
-
-/* shorthands for EFFECT:
- */
-#define effect_variable(e) reference_variable(effect_reference(e))
-#define effect_write_p(e) action_write_p(effect_action(e))
-#define effect_read_p(e) action_read_p(effect_action(e))
 
 /* quick debug macros
  */
