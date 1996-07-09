e9ef5a1c180827f272ea87d3eb768c10f036800a
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-07-09 16:53:01 +0000

    # on 1996/07/09 16:53:01, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@322 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/plint/plint-local.h
@@ -0,0 +1,37 @@
+/* package plint: programmation lineaire en nombres entiers
+ *
+ * Corinne Ancourt
+ *
+ * utilise les packages:
+ *  arithmetique.h
+ *  vecteur.h
+ *  contrainte.h
+ *  sc.h
+ *  ray_dte.h
+ *  sommet.h
+ *  sg.h
+ *  polyedre.h (indirectement, pour les fonctions de conversion de sc en
+ *              liste de sommets et reciproquement)
+ *  matrice.h 
+ */
+
+/*
+ * Representation d'une solution d'un systeme lineaire
+ *
+ * Pourrais-tu etre plus precise, Corinne? FI
+ */
+
+/* constante associee a une solution (Comment est-elle associee? FI)    */
+#define SOLUTION 0
+
+typedef struct Ssolution{
+    /* variable du systeme */
+    Variable var;
+    /* valeur de la variable */
+    int val;
+    /* denominateur de la valeur de la variable */
+    int denominateur;
+    /* pointeur vers la variable suivante */
+    struct Ssolution *succ;
+} *Psolution,Ssolution;
+
