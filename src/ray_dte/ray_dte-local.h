8227cbc1070b16ae6236ecaffb72d09cf5eb98aa
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-07-09 08:55:46 +0000

    # on 1996/07/09 08:55:46, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@285 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/ray_dte/ray_dte-local.h
@@ -0,0 +1,35 @@
+/* package ray_dte: structure de donnees representant les rayons et les
+ * droites d'un systeme generateur; elle contient le vecteur correspondant,
+ * un eventuel tableau de saturation, et le chainage vers les autres rayons
+ * ou droites.
+ *
+ * Francois Irigoin, Mai 1989
+ *
+ * Voir poly.h
+ *
+ * A terme, poly.h devrait exploser et la definition de ray_dte etre remise
+ * dans ce fichier; a moins qu'on ne mette plutot ray_dte.h dans sg.h
+ * pour eviter une explosion des .h
+ */
+
+#ifndef RAY_DTE
+/* numero du type de donnees */
+#define RAY_DTE 105
+
+typedef struct rdte   {
+	int *eq_sat;
+	struct	Svecteur *vecteur;
+	struct rdte   *succ;
+	} *Pray_dte,Sray_dte;
+
+#define print_rd(s) ray_dte_fprint(stdout,s)
+
+#define print_lray_dte(lv) fprint_lray_dte(stdout,lv)
+
+#ifndef VERSION_FINALE
+#define RAY_DTE_RM(rd,f) dbg_ray_dte_rm(rd,f)
+#else
+#define RAY_DTE_RM(rd,f) ray_dte_rm(rd)
+#endif
+
+#endif /* RAY_DTE */
