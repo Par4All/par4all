f8c3bfdabadc7b18c36288a41859a980b0f562c8
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-07-09 21:50:54 +0000

    # on 1996/07/09 21:50:54, coelho did: none
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@4056 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/pip/pip_interface.c
+++ src/Libs/pip/pip_interface.c
@@ -13,11 +13,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include "types.h"
 
 /* Newgen includes 	*/
 #include "genC.h"
-#include "list.h"
 
 /* C3 includes 		*/
 #include "boolean.h"
