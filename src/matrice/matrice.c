fc6f8fab1f64e394c9efc72ec9f0ea3f1d84558a
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-07-09 13:44:12 +0000

    # on 1996/07/09 13:44:12, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@302 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/matrice/matrice.c
@@ -0,0 +1,518 @@
+ /* package matrice */
+
+#include <stdio.h>
+#include <sys/stdtypes.h> /*for debug with dbmalloc */
+#include <malloc.h>
+
+#include "assert.h"
+
+#include "boolean.h"
+#include "arithmetique.h"
+
+#include "matrice.h"
+
+/* void matrice_transpose(matrice a, matrice a_t, int n, int m):
+ * transpose an (nxm) rational matrix a into a (mxn) rational matrix a_t
+ *
+ *         t
+ * a_t := a ;
+ */
+matrice_transpose(a,a_t,n,m)
+int	a[];
+int	a_t[];
+int	n;
+int	m;
+{
+    int	loop1,loop2;
+
+    /* verification step */
+    assert(n >= 0 && m >= 0);
+
+    /* copy from a to a_t */
+    DENOMINATOR(a_t) = DENOMINATOR(a);
+    for(loop1=1; loop1<=n; loop1++)
+	for(loop2=1; loop2<=m; loop2++) 
+	    ACCESS(a_t,m,loop2,loop1) = ACCESS(a,n,loop1,loop2);
+}
+
+/* void matrice_multiply(matrice a, matrice b, matrice c, int p, int q, int r):
+ * multiply rational matrix a by rational matrix b and store result in matrix c
+ *
+ *	a is a (pxq) matrix, b a (qxr) and c a (pxr)
+ *
+ *      c := a x b ;
+ *
+ * Algorithm used is directly from definition, and space has to be
+ * provided for output matrix c by caller. Matrix c is not necessarily
+ * normalized: its denominator may divide all its elements
+ * (see matrice_normalize()).
+ *
+ * Precondition:	p > 0; q > 0; r > 0;
+ *                      c != a; c != b; -- aliasing between c and a or b
+ *                                      -- is not supported
+ */
+void matrice_multiply(a,b,c,p,q,r)
+matrice	a;			/* input */
+matrice b;			/* input */
+matrice	c;			/* output */
+int	p;			/* input */
+int	q;			/* input */
+int	r;			/* input */
+{
+    int	loop1,loop2,loop3;
+    int	i;
+
+    /* validate dimensions */
+    assert(p > 0 && q > 0 && r > 0);
+    /* simplified aliasing test */
+    assert(c != a && c != b);
+
+    /* set denominator */
+    DENOMINATOR(c) = DENOMINATOR(a) * DENOMINATOR(b);
+
+    /* use ordinary school book algorithm */
+    for(loop1=1; loop1<=p; loop1++)
+	for(loop2=1; loop2<=r; loop2++) {
+	    i = 0;
+	    for(loop3=1; loop3<=q; loop3++) 
+		i = i + ACCESS(a,p,loop1,loop3)*ACCESS(b,q,loop3,loop2); 
+	    ACCESS(c,p,loop1,loop2) = i;
+	}
+}
+
+/* void matrice_normalize(matrice a, int n, int m)
+ *
+ *	A rational matrix is stored as an integer one with one extra
+ *	integer, the denominator for all the elements. To normalise the
+ *	matrix in this sense means to reduce this denominator to the 
+ *	smallest positive number possible. All elements are also reduced
+ *      to their smallest possible value.
+ *
+ * Precondition: DENOMINATOR(a)!=0
+ */
+void matrice_normalize(a,n,m)
+int	a[];			/* input */
+int	n;			/* input */
+int	m;			/* output */
+{
+    int	loop1,loop2;
+    int	factor;
+
+    assert(DENOMINATOR(a) != 0);
+
+    /* we must find the GCD of all elements of matrix */
+    factor = DENOMINATOR(a);
+
+    for(loop1=1; loop1<=n; loop1++)
+	for(loop2=1; loop2<=m; loop2++) 
+	    factor = pgcd(factor,ACCESS(a,n,loop1,loop2));
+
+    /* factor out */
+    if (factor != 1) {
+	for(loop1=1; loop1<=n; loop1++)
+	    for(loop2=1; loop2<=m; loop2++)
+		ACCESS(a,n,loop1,loop2) = 
+		    ACCESS(a,n,loop1,loop2) / factor;
+	DENOMINATOR(a) = DENOMINATOR(a) / factor;
+    }
+
+    /* ensure denominator is positive */
+    /* FI: this code is useless because pgcd()always return a positive integer,
+       even if a is the null matrix; its denominator CANNOT be 0 */
+    assert(DENOMINATOR(a) > 0);
+    if(DENOMINATOR(a) < 0) {
+	DENOMINATOR(a) = DENOMINATOR(a)*-1;
+	for(loop1=1; loop1<=n; loop1++)
+	    for(loop2=1; loop2<=m; loop2++)
+		ACCESS(a,n,loop1,loop2) = 
+		    -1 * ACCESS(a,n,loop1,loop2);
+    }
+}
+
+/* void matrice_normalizec(matrice MAT, int n, int m):
+ *  Normalisation des coefficients de la matrice MAT, i.e. division des 
+ *  coefficients de la matrice MAT et de son denominateur par leur pgcd
+ *
+ *  La matrice est modifiee.
+ *
+ *  Les parametres de la fonction :
+ *
+ *!int MAT[]	: matrice  de dimension (n,m) 
+ * int  n	: nombre de lignes de la matrice
+ * int  m 	: nombre de colonnes de la matrice
+ */
+void matrice_normalizec(MAT,n,m)
+int MAT[];
+int n,m;
+{
+    int i,a;
+
+    /* FI What an awful test! It should be an assert() */
+    if (n && m) {
+	a = MAT[0];
+	for (i = 1;(i<=n*m) && (a >1);i++)
+	    a = pgcd(a,MAT[i]);
+
+	if (a>1) {
+	    for (i = 0;i<=n*m;i++)
+		MAT[i] /= a;
+	}
+    }
+}
+
+/* void matrice_swap_columns(matrice matrix, int n, int m, int c1, int c2):
+ * exchange columns c1,c2 of an (nxm) rational matrix
+ *
+ *	Precondition:	n > 0; m > 0; 0 < c1 <= m; 0 < c2 <= m; 
+ */
+void matrice_swap_columns(matrix,n,m,c1,c2)
+int	matrix[];		/* input and output matrix */
+int	n;			/* number of rows */
+int	m;			/* number of columns */
+int	c1,c2;			/* column numbers */
+{
+    int	loop1;
+    int	temp;
+
+    /* validation step */
+    /*
+     * if ((n < 1) || (m < 1))   return(-208);
+     * if ((c1 > m) || (c2 > m)) return(-209);
+     */
+    assert(n > 0 && m > 0);
+    assert(0 < c1 && c1 <= m);
+    assert(0 < c2 && c2 <= m);
+
+    for(loop1=1; loop1<=n; loop1++) {
+	temp = ACCESS(matrix,n,loop1,c1);
+	ACCESS(matrix,n,loop1,c1) = ACCESS(matrix,n,loop1,c2);
+	ACCESS(matrix,n,loop1,c2) = temp;
+    }
+}	
+
+/* void matrice_swap_rows(matrice a, int n, int m, int r1, int r2):
+ * exchange rows r1 and r2 of an (nxm) rational matrix a
+ *
+ * Precondition: n > 0; m > 0; 1 <= r1 <= n; 1 <= r2 <= n
+ */
+void matrice_swap_rows(a,n,m,r1,r2)
+int	a[];		/* input and output matrix */
+int	n;			/* number of columns */
+int	m;			/* number of rows */
+int	r1,r2;			/* row numbers */
+{
+    int	loop1;
+    int	temp;
+
+    /* validation */
+    assert(n > 0);
+    assert(m > 0);
+    assert(0 < r1 && r1 <= n);
+    assert(0 < r2 && r2 <= n);
+
+    for(loop1=1; loop1<=m; loop1++) {
+	temp = ACCESS(a,n,r1,loop1);
+	ACCESS(a,n,r1,loop1) = ACCESS(a,n,r2,loop1);
+	ACCESS(a,n,r2,loop1) = temp;
+    }
+}
+
+/* void matrice_assign(matrice A, matrice B, int n, int m)
+ * Copie de la matrice A dans la matrice B
+ *
+ *  Les parametres de la fonction :
+ *
+ * int 	A[]	:  matrice
+ *!int 	B[]	:  matrice
+ * int  n	: nombre de lignes de la matrice
+ * int  m 	: nombre de colonnes de la matrice
+ *
+ * Ancien nom: mat_dup
+ */
+void matrice_assign(A,B,n,m)
+int A[],B[];
+int n,m;
+{
+    int i;
+    for (i = 0 ;i<= n*m;i++)
+	B[i] = A[i];
+}
+
+/* boolean matrice_egalite(matrice A, matrice B, int n, int m)
+ * test de l'egalite de deux matrices A et B; elles doivent avoir
+ * ete normalisees au prealable pour que le test soit mathematiquement
+ * exact
+ *
+ *  Les parametres de la fonction :
+ *
+ * int 	A[]	:  matrice
+ * int 	B[]	:  matrice
+ * int  n	: nombre de lignes de la matrice
+ * int  m 	: nombre de colonnes de la matrice
+ */
+boolean matrice_egalite(A,B,n,m)
+int A[],B[];
+int n,m;
+{
+    int i;
+    for (i = 0 ;i<= n*m;i++)
+	if(B[i] != A[i])
+	    return(FALSE);
+    return(TRUE);
+}
+
+/* void matrice_nulle(matrice Z, int n, int m):
+ * Initialisation de la matrice Z a la valeur matrice nulle
+ *
+ * Post-condition: 
+ *
+ * QQ i dans [1..n]
+ * QQ j dans [1..n]
+ *    Z(i,j) == 0
+ *
+ *  Les parametres de la fonction :
+ *
+ *!int 	Z[]	:  matrice
+ * int  n	: nombre de lignes de la matrice
+ * int  m 	: nombre de colonnes de la matrice
+ */
+void matrice_nulle(Z,n,m)
+int Z[];
+int n,m;
+{
+    int i,j;
+
+    for (i=1;i<=n;i++)
+	for (j=1;j<=m;j++)
+	    ACCESS(Z,n,i,j)=0;
+    DENOMINATOR(Z) = 1;
+}
+
+/* boolean matrice_nulle_p(matrice Z, int n, int m):
+ * test de nullite de la matrice Z
+ *
+ * QQ i dans [1..n]
+ * QQ j dans [1..n]
+ *    Z(i,j) == 0
+ *
+ *  Les parametres de la fonction :
+ *
+ * int 	Z[]	:  matrice
+ * int  n	: nombre de lignes de la matrice
+ * int  m 	: nombre de colonnes de la matrice
+ */
+boolean matrice_nulle_p(Z,n,m)
+matrice Z;
+int n,m;
+{
+    int i,j;
+
+    for (i=1;i<=n;i++)
+	for (j=1;j<=m;j++)
+	    if(ACCESS(Z,n,i,j)!=0)
+		return(FALSE);
+    return(TRUE);
+}
+
+/* boolean matrice_diagonale_p(matrice Z, int n, int m):
+ * test de nullite de la matrice Z
+ *
+ * QQ i dans [1..n]
+ * QQ j dans [1..n]
+ *    Z(i,j) == 0 && i != j || i == j
+ *
+ *  Les parametres de la fonction :
+ *
+ * int 	Z[]	:  matrice
+ * int  n	: nombre de lignes de la matrice
+ * int  m 	: nombre de colonnes de la matrice
+ */
+boolean matrice_diagonale_p(Z,n,m)
+matrice Z;
+int n,m;
+{
+    int i,j;
+
+    for (i=1;i<=n;i++)
+	for (j=1;j<=m;j++)
+	    if(i!=j && ACCESS(Z,n,i,j)!=0)
+		return(FALSE);
+    return(TRUE);
+}
+
+/* boolean matrice_triangulaire_p(matrice Z, int n, int m, boolean inferieure):
+ * test de triangularite de la matrice Z
+ *
+ * si inferieure == TRUE
+ * QQ i dans [1..n]
+ * QQ j dans [i+1..m]
+ *    Z(i,j) == 0
+ *
+ * si inferieure == FALSE (triangulaire superieure)
+ * QQ i dans [1..n]
+ * QQ j dans [1..i-1]
+ *    Z(i,j) == 0
+ *
+ *  Les parametres de la fonction :
+ *
+ * int 	Z[]	:  matrice
+ * int  n	: nombre de lignes de la matrice
+ * int  m 	: nombre de colonnes de la matrice
+ */
+boolean matrice_triangulaire_p(Z,n,m,inferieure)
+matrice Z;
+int n,m;
+boolean inferieure;
+{
+    int i,j;
+
+    for (i=1; i <= n; i++)
+	if(inferieure)
+	    for (j=i+1; j <= m; j++)
+		if(ACCESS(Z,n,i,j)!=0)
+		    return(FALSE);
+		else
+		    ;
+	else
+	    for (j=1; j <= i-1; j++)
+		if(ACCESS(Z,n,i,j)!=0)
+		    return(FALSE);
+		else
+		    ;
+    return(TRUE);
+}
+
+/* boolean matrice_triangulaire_unimodulaire_p(matrice Z, int n, boolean inferieure)
+ * test de la triangulaire et unimodulaire de la matrice Z.
+ * si inferieure == TRUE
+ * QQ i dans [1..n]
+ * QQ j dans [i+1..n]
+ *    Z(i,j) == 0
+ * i dans [1..n]
+ *   Z(i,i) == 1
+ *
+ * si inferieure == FALSE (triangulaire superieure)
+ * QQ i dans [1..n]
+ * QQ j dans [1..i-1]
+ *    Z(i,j) == 0
+ * i dans [1..n]
+ *   Z(i,i) == 1
+ * 
+ * les parametres de la fonction :
+ * matrice Z : la matrice entre
+ * int n     : la dimension de la martice caree
+ */
+boolean matrice_triangulaire_unimodulaire_p(Z,n,inferieure)
+matrice Z;
+int n;
+boolean inferieure;
+{
+    boolean triangulaire;
+    int i;
+    triangulaire = matrice_triangulaire_p(Z,n,n,inferieure);
+    if (triangulaire == FALSE)
+	return(FALSE);
+    else{
+	for(i=1; i<=n; i++)
+	    if (ACCESS(Z,n,i,i) != 1)
+		return(FALSE);
+	return(TRUE);
+    }
+}	    
+
+/* void matrice_substract(matrice a, matrice b, matrice c, int n, int m):
+ * substract rational matrix c from rational matrix b and store result
+ * in matrix a
+ *
+ *	a is a (nxm) matrix, b a (nxm) and c a (nxm)
+ *
+ *      a = b - c ;
+ *
+ * Algorithm used is directly from definition, and space has to be
+ * provided for output matrix a by caller. Matrix a is not necessarily
+ * normalized: its denominator may divide all its elements
+ * (see matrice_normalize()).
+ *
+ * Precondition:	n > 0; m > 0;
+ * Note: aliasing between a and b or c is supported
+ */
+void matrice_substract(a,b,c,n,m)
+matrice a;         /* output */
+matrice b, c;      /* input */
+int n,m;           /* input */
+{
+    int d1,d2;   /* denominators of b, c */
+    int lcm;     /* ppcm of b,c */
+    int i,j;
+
+    /* precondition */
+    assert(n>0 && m>0);
+    assert(DENOMINATOR(b) > 0);
+    assert(DENOMINATOR(c) > 0);
+
+    d1 = DENOMINATOR(b);
+    d2 = DENOMINATOR(c);
+    if (d1 == d2){
+	for (i=1; i<=n; i++)
+	    for (j=1; j<=m; j++)
+		ACCESS(a,n,i,j) = ACCESS(b,n,i,j) - ACCESS(c,n,i,j);
+	DENOMINATOR(a) = d1;
+    }
+    else{
+	lcm = ppcm(d1,d2);
+	d1 = lcm/d1;
+	d2 = lcm/d2;
+	for (i=1; i<=n; i++)
+	    for (j=1; j<=m; j++)
+		ACCESS(a,n,i,j) = ACCESS(b,n,i,j)*d1 - ACCESS(c,n,i,j)*d2;
+	DENOMINATOR(a) = lcm;
+    }
+}
+
+/* void matrice_soustraction_colonne(matrice MAT,int n,int m,int c1,int c2,int x):
+ * Soustrait x fois la colonne c2 de la colonne c1
+ * Precondition: n > 0; m > 0; 0 < c1, c2 < m;
+ * Effet: c1[0..n-1] = c1[0..n-1] - x*c2[0..n-1].
+ *
+ *  Les parametres de la fonction :
+ *
+ * int  MAT[]     :  matrice
+ * int  n         :  nombre de lignes de la matrice
+ * int  m         :  nombre de colonnes de la matrice
+ * int  c1        :  numero du colonne
+ * int  c2        :  numero du colonne
+ * int  x         : 
+ */
+void matrice_soustraction_colonne(MAT,n,m,c1,c2,x)
+int MAT[];        
+int n,m; 
+int c1,c2,x;
+{
+    int i;
+    for (i=1; i<=n; i++)
+	ACCESS(MAT,n,i,c1) = ACCESS(MAT,n,i,c1) - x * ACCESS(MAT,n,i,c2);
+}
+
+/* void matrice_soustraction_ligne(matrice MAT,int n,int m,int r1,int r2,int x):
+ * Soustrait x fois la ligne r2 de la ligne r1 
+ * Precondition: n > 0; m > 0; 0 < r1, r2 < n;
+ * Effet: r1[0..m-1] = r1[0..m-1] - x*r2[0..m-1]
+ *
+ *  Les parametres de la fonction :
+ *
+ * int  MAT[]     :  matrice
+ * int  n         :  nombre de lignes de la matrice
+ * int  m         :  nombre de colonnes de la matrice
+ * int  r1        :  numero du ligne
+ * int  r2        :  numero du ligne
+ * int  x         : 
+ */
+void matrice_soustraction_ligne(MAT,n,m,r1,r2,x)
+int MAT[];        
+int n,m; 
+int r1,r2,x;
+{
+    int i;
+    for (i=1; i<=m; i++)
+	ACCESS(MAT,n,r1,i) = ACCESS(MAT,n,r1,i) - x * ACCESS(MAT,n,r2,i);
+}
