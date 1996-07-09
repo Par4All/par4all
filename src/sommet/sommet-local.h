b1ababc3bae1c1c5509fa9fa22c772793612251e
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-07-09 10:26:38 +0000

    # on 1996/07/09 10:26:38, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@287 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/sommet/sommet-local.h
@@ -0,0 +1,65 @@
+/* package sommet: structure de donnees representant les sommets
+ * d'un systeme generateur; elle contient:
+ *  - le vecteur correspondant, a un coefficient multiplicatif pret qui le
+ *    rend entier,
+ *  - son denominateur,
+ *  - un eventuel tableau de saturation (par rapport a une liste implicite
+ *    de contraintes),
+ *  - le chainage vers les autres rayons ou droites.
+ *
+ * Francois Irigoin, Mai 1989
+ *
+ * Cette structure de donnees est aussi utilisee dans plint.dir pour
+ * representer la fonction economique, les contraintes lineaires et
+ * les systemes de contraintes lineaires.
+ *
+ * FI: commentaires a completer par Corinne, declaration d'un type
+ * synonyme pour eviter les conflits?
+ */
+
+#ifndef SOMMET
+
+/* valeur numerique utilise pour flagger les structures de donnees de type
+ * sommet */
+#define SOMMET 1004
+
+/* structure de donnees Sommet
+ *  - eq_sat: eventuel tableau des saturations du sommets par rapport a un
+ *    eventuel systeme de contraintes; inutilisable quand on ne connait pas
+ *    le nombre de contraintes, egalites ou inegalites
+ *  - vecteur: coordonnees entieres du sommet, a un coefficient multiplicatif
+ *    pres; l'inverse de ce coefficient est donne par le champ suivant
+ *  - denominateur: coefficient permettant de garder les coordonnees du
+ *    sommet sous forme rationnelle; les numerateurs des coordonnees se
+ *    trouvent dans "vecteur"; le denominateur est unique, pour le sommet,
+ *    i.e. c'est le PGCD des coordonnees; le denominateur doit toujours
+ *    etre strictement positif
+ *  - succ: pointeur vers le sommet suivant; on s'interesse a l'ensemble
+ *    des sommets du systeme generateur plutot qu'a un sommet particulier
+ */
+typedef struct typ_som {
+    int *eq_sat;
+    Pvecteur vecteur;
+    int denominateur;
+    struct typ_som *succ;
+} *Psommet,Ssommet;
+
+#define print_som(s) sommet_fprint(stdout,s)
+
+#define print_lsom(ls) fprint_lsom(stdout,ls)
+
+#define VERSION_FINALE
+#ifdef VERSION_FINALE
+#define SOMMET_RM(s,function_name) sommet_rm(s)
+#else
+#define SOMMET_RM(s,function_name) dbg_sommet_rm(s,function_name)
+#endif
+
+/* macros d'acces */
+
+/* int sommet_denominateur(Psommet): denominateur des coordonnees d'un
+ * sommet; ex den_of()
+ */
+#define sommet_denominateur(s) ((s)->denominateur)
+
+#endif /* SOMMET */
