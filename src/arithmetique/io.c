6c9d2b78ba27397dcdbbf5153bde3b5a7330fd2c
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-07-13 16:02:32 +0000

    # on 1996/07/13 16:02:32, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@375 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/arithmetique/io.c
+++ src/arithmetique/io.c
@@ -1,12 +1,44 @@
 /* package arithmetic
  *
  * $RCSfile: io.c,v $ (version $Revision$)
- * $Date: 1996/07/13 15:57:13 $, 
+ * $Date: 1996/07/13 16:02:32 $, 
+ *
+ * IO on a Value
  */
 #include <stdio.h>
 #include <strings.h>
 
 #include "arithmetique.h"
+
+void print_Value(Value v)
+{
+    (void) printf(VALUE_FMT, v);
+}
+
+void fprint_Value(FILE *f, Value v)
+{
+    (void) fprintf(f, VALUE_FMT, v);
+}
+
+char *sprint_Value(char *s, Value v)
+{
+    return sprintf(s, VALUE_FMT, v);
+}
+
+int fscan_Value(FILE *f, Value *pv)
+{
+    return fscanf(f, VALUE_FMT, pv);
+}
+
+int scan_Value(Value *pv)
+{
+    return scanf(VALUE_FMT, pv);
+}
+
+int sscan_Value(char *s, Value *pv)
+{
+    return sscanf(s, VALUE_FMT, pv);
+}
 
 /* this seems a reasonnable upperbound
  */
@@ -16,26 +48,6 @@ char * Value_to_string(Value v)
 {
     static char buf[BUFFER_SIZE];
     return sprintf(VALUE_FMT "\0", buf, v);
-}
-
-void fprint_Value(FILE *f, Value v)
-{
-    (void) fprintf(f, VALUE_FMT, v);
-}
-
-char * sprint_Value(char *s, Value v)
-{
-    return sprintf(s, VALUE_FMT, v);
-}
-
-void fscan_Value(FILE *f, Value *pv)
-{
-    (void) fscanf(f, VALUE_FMT, pv);
-}
-
-void scan_Value(Value *pv)
-{
-    (void) scanf(VALUE_FMT, pv);
 }
 
 /* end of $RCSfile: io.c,v $
