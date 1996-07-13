8a3a157e66fb54ea9b032db89800b0d3850c893b
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-07-13 15:55:25 +0000

    # on 1996/07/13 15:55:25, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@373 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/arithmetique/io.c
+++ src/arithmetique/io.c
@@ -1,106 +1,37 @@
 /* package arithmetic
  *
  * $RCSfile: io.c,v $ (version $Revision$)
- * $Date: 1996/07/13 14:31:44 $, 
+ * $Date: 1996/07/13 15:55:25 $, 
  */
 #include <stdio.h>
 #include <strings.h>
 
-#include "assert.h"        /* abort version */
 #include "arithmetique.h"
 
-#define BUFFER_SIZE 21
-
-#ifdef VALUE_IS_LONGLONG
-#define BASE '9' /* last digit of the base, must be >= '1' */
-#define STRING_ZERO "\00000000000000000000\0"
-#define STRING_ONE  "\00000000000000000001\0"
-
-/* adds two chars for BASE, and tells whether an increment is needed */
-static char addchar(char a1, char a2, int *ret)
-{
-    char res = a1+a2-'0';
-    if (res>BASE)
-	res-=(BASE-'0'+1), *ret=1;
-    else
-	*ret = 0;
-    return res;
-}
-
-/* a++ (for incrementing, starting from a and going to the left)
+/* this seems a reasonnable upperbound
  */
-static void addone(char *a)
-{
-    int ret=1;
-    while(ret && *a)
-	*a--=addchar(*a,'1',&ret);
-    assert(!ret);
-}
+#define BUFFER_SIZE 50
 
-/* a+=b */
-static void add(char *a, char *b)
-{
-    int i, ret;
-    assert(a!=b);
-    for (i=BUFFER_SIZE-1; i>=0; i--)
-    {
-	addchar(*(a+i), *(b+i), &ret);
-	if (ret) addone(a+i);
-    }
-}
-
-/* s*=2 */
-static void timestwo(char *s)
-{
-    char buf[BUFFER_SIZE];
-    (void) strncpy(buf, s, BUFFER_SIZE);
-    add(s, buf);
-}
-
-/* returns a static pointer, thus must be strdup if necessary...
- * some format could be specified? "x" -> BASE is 'f', "d" -> '9'...
- */
-char * Value_to_string(Value v)
-{
-   static char buf[BUFFER_SIZE]; 
-   char pow[BUFFER_SIZE];
-   char *pb;
-   int negative = 0;
-   Value power=VALUE_ONE;
-   int iter;
-   
-   if (VALUE_NEG_P(v)) 
-       v=-v, negative=1;
-
-   (void) strncpy(buf, STRING_ZERO, BUFFER_SIZE);   /* buf = 0 */
-   (void) strncpy(pow, STRING_ONE,  BUFFER_SIZE);   /* pow = 1 */
-
-   /* compute the string value of v 
-    */
-   for (iter=(sizeof(Value)*8-1); iter>0; iter--)
-   {
-       if (power&v) add(buf, pow); /* buf += pow */
-       timestwo(pow);              /* pow *= 2 */
-       power<<=1;
-   }
-   
-   /* remove leading zeros but the last
-    */
-   for (pb=buf+1; *pb=='0' && *(pb+1); pb++);
-
-   /* add the sign of needed 
-    */
-   if (negative) pb--, *pb='-';
-
-   return pb;
-}
-#else
 char * Value_to_string(Value v)
 {
     static char buf[BUFFER_SIZE];
-    return sprintf("%ld\0", buf, v);
+    return sprintf(VALUE_FMT "\0", buf, v);
 }
-#endif
+
+void fprint_Value(FILE *f, Value v)
+{
+    (void) fprintf(f, VALUE_FMT, v);
+}
+
+char * sprint_Value(char *s, Value v)
+{
+    return sprintf(s, VALUE_FMT, v);
+}
+
+void fscan_Value(FILE *f, Value *pv)
+{
+    (void) fscanf(f, VALUE_FMT, pv);
+}
 
 /* end of $RCSfile: io.c,v $
  */
