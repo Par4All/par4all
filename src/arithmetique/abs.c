9485f553e9fa163699454fc846bc4fa37341d38e
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-07-13 11:58:52 +0000

    # on 1996/07/13 11:58:52, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@355 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/arithmetique/abs.c
+++ src/arithmetique/abs.c
@@ -1,6 +1,6 @@
- /* package arithmetique */
-
-/*LINTLIBRARY*/
+/* $RCSfile: abs.c,v $ (version $Revision$)
+ * $Date: 1996/07/13 11:58:52 $, 
+ */
 
 #include <stdio.h>
 /* #include <values.h> */
@@ -12,34 +12,27 @@
 
 /* int absval(int i): absolute value of i (SUN version)
  */
-int absval(i)
-int	i;
+Value absval(Value i)
 {
-    return( abs_ofl_ctrl(i, 0));
+    return abs_ofl_ctrl(i, 0);
 }
 
 /* int absval_ofl(int i): absolute value of i (SUN version)
  * Overflow control is made and returns to the last setjmp(overflow_error). 
  */
-int absval_ofl(i)
-int	i;
+Value absval_ofl(Value i)
 {
-    return( abs_ofl_ctrl(i, 1));
-
+    return abs_ofl_ctrl(i, 1);
 }
 
 
-int abs_ofl_ctrl(i, ofl_ctrl)
-int i;
-int ofl_ctrl;
+Value abs_ofl_ctrl(Value i, int ofl_ctrl)
 {
-
     extern jmp_buf overflow_error;
     
-    if ((ofl_ctrl == 1) && (i == INT_MIN))   
+    if ((ofl_ctrl == 1) && (i == VALUE_MIN))   
 	longjmp(overflow_error, 5);
         
-    assert(i != INT_MIN);
+    assert(i != VALUE_MIN);
     return (i>0) ? i: -i;    
-    
 }
