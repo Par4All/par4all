e9e2d9dd3a8959a350777a64608f0acc6bd67cb2
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-07-25 08:54:39 +0000

    # on 1996/07/25 08:54:39, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@4448 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/movements/movements-local.h
@@ -0,0 +1,10 @@
+/* Package movements   */
+
+#include "matrice.h"
+
+#define COLUMN_MAJOR TRUE
+
+#define SUFFIX_FOR_TEMP_VAR1_IN_INNER_LOOP          "_1"
+#define SUFFIX_FOR_TEMP_VAR2_IN_INNER_LOOP          "_2"
+
+ 
