a8f7bcba06771f2d0e8a4f198f15a0bc81ac1151
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-08-06 10:15:10 +0000

    # on 1996/08/06 10:15:10, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@707 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/Tests/sc_to_sg_test.c
@@ -0,0 +1,64 @@
+/* test de la conversion d'un systeme d'equations et d'inequations en
+ * un systeme generateur
+ *
+ * Francois Irigoin, Decembre 1989
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "boolean.h"
+#include "vecteur.h"
+#include "contrainte.h"
+#include "sc.h"
+
+#include "ray_dte.h"
+#include "sommet.h"
+#include "sg.h"
+
+#include "polyedre.h"
+
+extern int sc_to_sg_debug_level;
+
+main(argc,argv)
+int argc;
+char * argv[];
+{
+    Psysteme s;
+    /* systeme generateur sg */
+    Ptsg sg; 
+    FILE * f;
+    char * filename = "stdin";
+
+    if(argc==1) {
+	f = stdin;
+	fprintf(stderr,"Lecture sur stdin\n");
+    }
+    else if(argc==2) {
+	filename = strdup(argv[1]);
+	if((f = fopen(filename,"r")) == NULL) {
+	    fprintf(stderr,"Ouverture du fichier %s impossible\n",
+		    filename);
+	    exit(4);
+	}
+    }
+    else {
+	fprintf(stderr,"Usage: sc_fscan_print [filename]\n");
+	exit(1);
+    }
+
+    /* lecture du systeme */
+    if(sc_fscan(f,&s)) {
+	fprintf(stderr,"syntaxe correcte dans %s\n",filename);
+	sc_fprint(stdout,s, variable_default_name);
+    }
+    else {
+	fprintf(stderr,"erreur de syntaxe dans %s\n",filename);
+	exit(1);
+    }
+
+    /* conversion */
+    sc_to_sg_debug_level = 9;
+    sg = sc_to_sg(s);
+    sg_fprint(stdout, sg, variable_default_name);
+}
