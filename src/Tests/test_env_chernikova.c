20fc3d7611d33e2eadbefcdbb23bae061ddb6358
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1996-08-07 20:01:36 +0000

    # on 1996/08/07 20:01:36, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@763 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/Tests/test_env_chernikova.c
+++ src/Tests/test_env_chernikova.c
@@ -9,85 +9,68 @@
 
 #include <stdio.h>
 #include <malloc.h>
-extern int fprintf();
-extern int printf();
-extern char * strdup();
 
 #include "boolean.h"
 #include "arithmetique.h"
-#include"assert.h"
+#include "assert.h"
 #include "vecteur.h"
 #include "contrainte.h"
 #include "ray_dte.h"
 #include "sommet.h"
 #include "sc.h"
 #include "sg.h"
-#include "types.h"
 #include "polyedre.h"
 
-
-main(argc, argv)
-     int argc;
-     char **argv;
+Psysteme read_syst_from_file(char * name)
 {
-    FILE * f1;
-    FILE * f2;
-    char * filename = "stdin";
-    Psysteme sc1=sc_new(); 
-    Psysteme sc2=sc_new();
-    Psysteme sc=sc_new();
-    Ptsg sg = sg_new();
-    Ptsg sg1,sg2 ;
-    Matrix *a;
-    Polyhedron *A;
+    FILE * f;
+    Psysteme s = sc_new();
+    
+    if((f = fopen(name,"r")) == NULL) {
+	fprintf(stderr,"Ouverture du fichier %s impossible\n",name);
+	exit(2);
+    }
+
+    if(sc_fscan(f,&s)) {
+	fprintf(stderr,"syntaxe correcte dans %s\n",name);
+	/* sc_fprint(stderr, s, *variable_default_name); */
+    }
+    else {
+	fprintf(stderr,"erreur de syntaxe dans %s\n",name);
+	exit(3);
+    }
+    
+    return s;
+}
+
+int main(int argc, char **argv)
+{
+    Psysteme sc1, sc2, sc;
+
     if(argc!=3) {
 	fprintf(stdout,"Usage: %s sc1 sc2\n",argv[0]);
 	exit(1);
     }
 
-    if((f1 = fopen(argv[1],"r")) == NULL) {
-	fprintf(stdout,"Ouverture du fichier %s impossible\n",
-		argv[1]);
-	exit(4);
-    }
-
-    if((f2 = fopen(argv[2],"r")) == NULL) {
-	fprintf(stdout,"Ouverture du fichier %s impossible\n",
-		argv[2]);
-	exit(4);
-    }
-
-    /* lecture du premier systeme */
-    if(sc_fscan(f1,&sc1)) {
-	fprintf(stderr,"syntaxe correcte dans %s\n",argv[1]);
-	sc_fprint(stdout, sc1, *variable_default_name);
-    }
-    else {
-	fprintf(stderr,"erreur de syntaxe dans %s\n",argv[1]);
-	exit(1);
-    }
-
-
-    /* lecture du premier systeme */
-    if(sc_fscan(f2,&sc2)) {
- 	fprintf(stderr,"syntaxe correcte dans %s\n",argv[2]);
-	sc_fprint(stdout, sc2, *variable_default_name);
-    }
-    else {
-	fprintf(stderr,"erreur de syntaxe dans %s\n",argv[2]);
-	exit(1);
-    }
+    sc1 = read_syst_from_file(argv[1]);
+    sc2 = read_syst_from_file(argv[2]);
 
     sc1 = sc_normalize(sc1);
     sc2 = sc_normalize(sc2);
+
     sc_fprint(stdout, sc1, *variable_default_name);
     sc_fprint(stdout, sc2, *variable_default_name);
 
     assert(vect_size(sc1->base) == vect_size(sc2->base));
 
-    sc2 = sc_translate(sc2,sc1->base,  *variable_default_name);
-    sc=sc_convex_hull(sc1,sc2); 
+    sc2 = sc_translate(sc2, sc1->base, *variable_default_name);
+    sc = sc_convex_hull(sc1,sc2); 
 
     printf("systeme correspondant \n");
     sc_fprint(stdout, sc, *variable_default_name);
-} /* main */
+
+    return 0;
+}
+
+/* that is all
+ */
