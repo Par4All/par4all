9303623dc6ecbea89d2e946b4dbb7338b8988abb
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-08-10 12:44:32 +0000

    # on 1996/08/10 12:44:32, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@4624 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/syntax/util.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "genC.h"
+#include "ri.h"
+#include "constants.h"
+
+extern char *strndup();
+
+/* a few simple functions to build or access internal representation */
+
+/*value MakeValueLitteral()*/
+/*{*/
+/*    return(make_value(is_value_constant, */
+/*		      make_constant(is_constant_litteral, NIL)));*/
+/*}*/
+
+/* FI: these functions were moved in ri-util/type.c and ri-util/entity.c */
