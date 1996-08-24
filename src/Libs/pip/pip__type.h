f7f3182bdf262ac38ea942ac70fdb015f5cc6e9a
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-08-24 10:33:03 +0000

    # on 1996/08/24 10:33:03, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@4978 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/pip/pip__type.h
@@ -0,0 +1,20 @@
+#define Entier int
+#define FORMAT "%d"
+#define UN 1
+
+/* Modification by Alexis Platonoff: we need a greater solution size */
+/*#define SOL_SIZE 4096*/
+#define SOL_SIZE 16384
+
+#define True 1
+#define False 0
+
+#ifdef TC
+#define DEBUG 8
+#endif
+
+#define Q if(cross_product>=limit)
+
+#define MAXCOL 1024
+#define MAXPARM 100
+
