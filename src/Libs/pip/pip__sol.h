e6ac42861748be4ae7e97536696d9af364f41446
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-08-24 10:33:04 +0000

    # on 1996/08/24 10:33:04, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@4979 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/pip/pip__sol.h
@@ -0,0 +1,14 @@
+int sol_pgcd();
+int sol_ppcm();
+void sol_init();
+int sol_hwm();
+void sol_reset();
+void sol_nil();
+void sol_if();
+void sol_list();
+void sol_form();
+void sol_new();
+void sol_div();
+void sol_val();
+int sol_edit();
+int is_not_Nil();
