35edf3f2daec892a2b59885950d743e7cc369a8a
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-08-30 17:41:38 +0000

    # on 1996/08/30 17:41:38, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5033 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/xpomp/fractal.f
+++ src/Runtimes/xpomp/fractal.f
@@ -1,30 +1,31 @@
       program fractal
 
+      implicit none
+
       include 'xpomp_graphic_F.h'
       
-c     Algorithm parameters:
+!     Algorithm parameters:
       integer n_iteration
       parameter (n_iteration = 256)
       real*8 big_value
       parameter (big_value = 100)
-c     distorsion = 2 to have normal Mandelbrot:
+!     distorsion = 2 to have normal Mandelbrot:
       real*8 distorsion
       parameter (distorsion = 2)
 
-c     Size of the iteration space:
+!     Size of the iteration space:
       integer x_size, y_size
       parameter(x_size = 200)     
       parameter(y_size = 200)
 
-c     The zooming ratio to display this iteration space:      
+!     The zooming ratio to display this iteration space:      
       integer x_display_zoom, y_display_zoom
       parameter(x_display_zoom = 2)     
       parameter(y_display_zoom = 2)
 
-c     The zooming factor used when zooming with the button:
+!     The zooming factor used when zooming with the button:
       real*8 zooming_factor
       parameter (zooming_factor = 4)
-
 
       integer x_display_size, y_display_size
       parameter(x_display_size = x_size*x_display_zoom)     
@@ -35,14 +36,16 @@ c     The zooming factor used when zooming with the button:
       character image(0:x_size - 1, 0:y_size - 1)
       
       real*8 zoom, xcenter, ycenter
-      real*8 zr, zrp, zi, cr, ci
-      integer status
+      real*8 zr, zrp, zi, cr, ci, d
+      integer status, button
       
-c     Some HPF parallelism description:
-chpf$ processor pe(2, 2)
-chpf$ template space(0:x_size - 1, 0:y_size - 1)
-chpf$ distribute space(block, block)
-chpf$ align image with space
+! Some HPF distributions:
+! cyclic would make more sense as far as load balancing is concerned
+! However HPFC would not be very good at it...
+!hpf$ processor pe(2, 2)
+!hpf$ template space(0:x_size - 1, 0:y_size - 1)
+!hpf$ distribute space(block, block) onto pe
+!hpf$ align image with space
       
       display = xpomp_open_display(x_display_size, y_display_size)
       status = xpomp_set_color_map(display, 1, 1, 0, 0)
@@ -56,15 +59,15 @@ chpf$ align image with space
       print *, 'Use mouse button 1 to zoom in, button 2 to recenter'
       print *, '    and button 3 to zoom out'
 
-c     Main loop:
+!     Main loop:
  10   continue
 
-c     Compute a fractal image:
+!     Compute a fractal image:
 chpf$ independent, new(cr,ci,zr,zi,d,zrp)
-      do 1 x = 0, x_size - 1
+      do x = 0, x_size - 1
          cr = xcenter + (x - x_size/2)*zoom/x_size
 chpf$ independent
-         do 2 y = 0, y_size - 1      
+         do y = 0, y_size - 1      
             ci = ycenter + (y - y_size/2)*zoom/y_size
             zr = cr
             zi = ci
@@ -74,11 +77,11 @@ chpf$ independent
                zi = distorsion*zr*zi + ci
                zr = zrp
                if (d .gt. big_value) goto 300
- 3          enddo
+            enddo
  300        continue
             image(x, y) = CHAR(k)
- 2       continue
- 1    continue
+         enddo
+      enddo
 
 c     Display the image:
       status = xpomp_flash(display, image, x_size, y_size, 
