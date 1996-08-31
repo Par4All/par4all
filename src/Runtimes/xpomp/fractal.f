efeb62309ba72424997ef7fe4c45f6a180c9be74
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-08-31 13:16:17 +0000

    # on 1996/08/31 13:16:17, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5069 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/xpomp/fractal.f
+++ src/Runtimes/xpomp/fractal.f
@@ -1,33 +1,38 @@
-! testing Xpomp display facilities
+! testing Xpomp display facilities in fortran
+!
+! (c) Ronan.Keryell@cri.ensmp.fr 1996
+!
 ! $RCSfile: fractal.f,v $ (version $Revision$)
-! $Date: 1996/08/30 19:58:48 $, 
+! $Date: 1996/08/31 13:16:17 $, 
 !
       program fractal
 
       implicit none
 
       include 'xpomp_graphic_F.h'
+
+!ldf$ -L$(XPOMP_RUNTIME) -L$(PIPS_ROOT)/Runtime/xpomp -lxpomp
       
-!     Algorithm parameters:
+! Algorithm parameters:
       integer n_iteration
       parameter (n_iteration = 256)
       real*8 big_value
       parameter (big_value = 100)
-!     distorsion = 2 to have normal Mandelbrot:
+! distorsion = 2 to have normal Mandelbrot:
       real*8 distorsion
       parameter (distorsion = 2)
 
-!     Size of the iteration space:
+! Size of the iteration space:
       integer x_size, y_size
       parameter(x_size = 200)     
       parameter(y_size = 200)
 
-!     The zooming ratio to display this iteration space:      
+! The zooming ratio to display this iteration space:      
       integer x_display_zoom, y_display_zoom
       parameter(x_display_zoom = 2)     
       parameter(y_display_zoom = 2)
 
-!     The zooming factor used when zooming with the button:
+! The zooming factor used when zooming with the button:
       real*8 zooming_factor
       parameter (zooming_factor = 4)
 
@@ -35,6 +40,7 @@
       parameter(x_display_size = x_size*x_display_zoom)     
       parameter(y_display_size = y_size*y_display_zoom)
       
+      integer counter
       integer k, x, y
       integer display
       character image(0:x_size - 1, 0:y_size - 1)
@@ -51,8 +57,8 @@
 !hpf$ distribute space(block, block) onto pe
 !hpf$ align image with space
       
-      display = xpomp_open_display(x_display_size, y_display_size)
-      status = xpomp_set_color_map(display, 1, 1, 0, 0)
+      call xpomp_open_display(x_display_size, y_display_size, display)
+      call xpomp_set_color_map(display, 1, 1, 0, 0, status)
       
       xcenter = 0
       ycenter = 0
@@ -63,14 +69,15 @@
       print *, 'Use mouse button 1 to zoom in, button 2 to recenter'
       print *, '    and button 3 to zoom out'
 
-!     Main loop:
+! Main loop:
+      counter = 0
  10   continue
 
-!     Compute a fractal image:
-chpf$ independent, new(cr,ci,zr,zi,d,zrp)
+! Compute a fractal image:
+!hpf$ independent, new(cr,ci,zr,zi,d,zrp)
       do x = 0, x_size - 1
          cr = xcenter + (x - x_size/2)*zoom/x_size
-chpf$ independent
+!hpf$    independent
          do y = 0, y_size - 1      
             ci = ycenter + (y - y_size/2)*zoom/y_size
             zr = cr
@@ -87,24 +94,30 @@ chpf$ independent
          enddo
       enddo
 
-c     Display the image:
-      status = xpomp_flash(display, image, x_size, y_size, 
-     &     0, 0, x_display_zoom, y_display_zoom)
+! Display the image:
+      call xpomp_flash(display, image, x_size, y_size, 
+     &     0, 0, x_display_zoom, y_display_zoom, status)
 
-c     Wait for user interaction:
-      button = xpomp_wait_mouse(display, x, y)
+! Wait for user interaction:
+      call xpomp_wait_mouse(display, x, y, button)
       xcenter = xcenter + (x/x_display_zoom - x_size/2)*zoom/x_size
       ycenter = ycenter + (y/y_display_zoom - y_size/2)*zoom/y_size
       
       if (button.eq.1) then
-       zoom = zoom/zooming_factor
+         zoom = zoom/zooming_factor
       else if (button.eq.3) then
-       zoom = zoom*zooming_factor
+         zoom = zoom*zooming_factor
       endif
 
       print *, 'Position (', xcenter, ',', ycenter, '), zoom =', zoom
-      goto 10
 
+! hpfc does not like infinite loop without an exit point...
+! some bug related to the management of unstructured to be investigated
+! I added this counter and exit as a temporary fix - FC.
+      counter = counter+1
+      if (counter.gt.100000) goto 20
+      goto 10
+ 20   continue
       end
 
 ! end of $RCSfile: fractal.f,v $
