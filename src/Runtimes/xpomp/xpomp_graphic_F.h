1e9b5241ccfc178b77075f4e4756e73a5b82ff67
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-08-31 12:15:59 +0000

    # on 1996/08/31 12:15:59, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5066 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/xpomp/xpomp_graphic_F.h
+++ src/Runtimes/xpomp/xpomp_graphic_F.h
@@ -1,66 +1,66 @@
 ! $RCSfile: xpomp_graphic_F.h,v $ (version $Revision$)
-! $Date: 1996/08/30 22:01:02 $, 
+! $Date: 1996/08/31 12:15:59 $, 
 !
-!  The fortran headers for the HPFC graphical library.
-
-c     Have a look to show_fxpomp_graphic.h to have 
-c     a more precise description.
-c
-c     Ronan.Keryell@cri.ensmp.fr
-c
-c     To open a new display:
+!  The fortran headers for the XPOMP graphical library.
+!
+!  Have a look to show_fxpomp_graphic.h for a more precise description.
+!  The FC IO directives tells HPFC that the directive is an IO...
+!
+! (c) Ronan.Keryell@cri.ensmp.fr 1996
+!     Centre de Recherche en Informatique,
+!     École des mines de Paris.
+!
+! To open a new display:
 !fcd$ io xpomp_open_display
       integer xpomp_open_display
       external xpomp_open_display
 
-c     The user function to close a display window:
-c     xpomp_close_display
+! The user function to close a display window:
 !fcd$ io xpomp_close_display
 
-c     Get the current default HPFC display:
+! Get the current default HPFC display:
 !fcd$ io xpomp_get_current_default_display
       integer xpomp_get_current_default_display
       external xpomp_get_current_default_display
 
-c     Set the current default HPFC display and return the old one:
+! Set the current default HPFC display and return the old one:
 !fcd$ io xpomp_set_current_default_display
       integer xpomp_set_current_default_display
       external xpomp_set_current_default_display
 
-c     Get the pixel bit width of a display window:
+! Get the pixel bit width of a display window:
 !fcd$ io xpomp_get_depth
       integer xpomp_get_depth
       external xpomp_get_depth
 
-c     Select the color map
+! Select the color map
 !fcd$ io xpomp_set_color_map
       integer xpomp_set_color_map
       external xpomp_set_color_map
 
-c     Load a user defined colormap and select it:
+! Load a user defined colormap and select it:
 !fcd$ io xpomp_set_user_color_map
       integer xpomp_set_user_color_map
       external xpomp_set_user_color_map
 
-c     Wait for a mouse click:
+! Wait for a mouse click:
 !fcd$ io xpomp_wait_mouse
       integer xpomp_wait_mouse
       external xpomp_wait_mouse
 
-c     Just test for a mouse click:
+! Just test for a mouse click:
 !fcd$ io xpomp_is_mouse
       integer xpomp_is_mouse
       external xpomp_is_mouse
 
-c     The user interface to show something uncooked. 
-c     Return -1 if it fails, 0 if not:
+! The user interface to show something uncooked. 
+! Return -1 if it fails, 0 if not:
 !fcd$ io xpomp_flash
       integer xpomp_flash
       external xpomp_flash
 
-c     Show a float/double array and interpolate color between min_value
-c     tomax_value. If min_value = max_value = 0, 
-c     automatically scale the colors:
+! Show a float/double array and interpolate color between min_value and
+! max_value. If min_value = max_value = 0, automatically scale the colors:
 !fcd$ io xpomp_show_real4
       integer xpomp_show_real4
       external xpomp_show_real4
@@ -69,8 +69,7 @@ c     automatically scale the colors:
       integer xpomp_show_real8
       external xpomp_show_real8
 
-c      Print out a small help about keyboard usage in xPOMP:
-c     xpomp_show_usage
+! Print out a small help about keyboard usage in xPOMP:
 !fcd$ io xpomp_show_usage
 
 !
