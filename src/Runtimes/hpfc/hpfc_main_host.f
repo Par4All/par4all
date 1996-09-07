94368c72fdd2d5496533bf558477b54e6fd062f3
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-09-07 15:51:24 +0000

    # on 1996/09/07 15:51:24, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5257 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/hpfc/hpfc_main_host.f
+++ src/Runtimes/hpfc/hpfc_main_host.f
@@ -1,10 +1,10 @@
 ! $RCSfile: hpfc_main_host.f,v $ (version $Revision$)
-! $Date: 1996/09/07 14:49:30 $, 
+! $Date: 1996/09/07 15:51:24 $, 
 !
 ! the main for the HPFC host program
 !
-      program main_host
-      call hpfc_init_host_node
+      program main host
+      call hpfc init host node
       call host
       end
 !
