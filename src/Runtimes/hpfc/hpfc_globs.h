d84296cda5db74a535165ab8a850d5a3257a7705
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-09-07 16:22:33 +0000

    # on 1996/09/07 16:22:33, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5268 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/hpfc/hpfc_globs.h
+++ src/Runtimes/hpfc/hpfc_globs.h
@@ -1,39 +1,39 @@
-c
-c COMMON /HPFC_GLOBS/
-c
-c $RCSfile: hpfc_globs.h,v $ ($Date: 1994/09/21 17:23:03 $, )
-c version $Revision$
-c got on %D%, %T%
-c $Id$
-c
-c
-      common /HPFC_GLOBS/
-     $     OWNERTID,
+!
+! COMMON /HPFC GLOBS/
+!
+! $RCSfile: hpfc_globs.h,v $ ($Date: 1996/09/07 16:22:33 $, )
+! version $Revision$
+! got on %D%, %T%
+! $Id$
+!
+!
+      common /HPFC GLOBS/
+     $     OWNER TID,
      $     OLID,
      $     OPN,
      $     OINDP(7),
      $     OREPLICATED,
-     $     SENDERTID,
+     $     SENDER TID,
      $     SLID,
-     $     COMPUTERTID,
+     $     COMPUTER TID,
      $     CLID,
      $     CPN,
      $     CINDP(7),
      $     CPOS(7),
      $     CPOSPN,
-     $     CPOSCOMPUTED,
+     $     CPOS COMPUTED,
      $     NLID,
      $     NTID
-c
+!
       integer 
-     $     OWNERTID,    OLID, OPN, OINDP,
-     $     SENDERTID,   SLID,
-     $     COMPUTERTID, CLID, CPN, CINDP, CPOS, CPOSPN,
-     $     NLID, NTID
-c
+     $     OWNER TID,    OLID, OPN, OINDP,
+     $     SENDER TID,   SLID,
+     $     COMPUTER TID, CLID, CPN, CINDP, CPOS, CPOSPN,
+     $     N LID, N TID
+!
       logical
-     $     OREPLICATED, CPOSCOMPUTED
-c
-c
-c that s all
-c
+     $     O REPLICATED, CPOS COMPUTED
+!
+!
+! that s all
+!
