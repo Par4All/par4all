46e428f726afcdf62295096f6155c10c1d968708
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-09-07 14:49:30 +0000

    # on 1996/09/07 14:49:30, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5247 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Runtimes/hpfc/hpfc_main_host.f
@@ -0,0 +1,12 @@
+! $RCSfile: hpfc_main_host.f,v $ (version $Revision$)
+! $Date: 1996/09/07 14:49:30 $, 
+!
+! the main for the HPFC host program
+!
+      program main_host
+      call hpfc_init_host_node
+      call host
+      end
+!
+! end of $RCSfile: hpfc_main_host.f,v $
+!
