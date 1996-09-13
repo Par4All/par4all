d4f4fb5fd04d9dd8b4c8e349df016ffd27b2ba9a
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-09-13 17:55:24 +0000

    # on 1996/09/13 17:55:24, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5372 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/hpfc/hpfc_stubs.f
+++ src/Scripts/hpfc/hpfc_stubs.f
@@ -5,32 +5,32 @@
 ! (c) Fabien COELHO, 09/95
 !
 ! $RCSfile: hpfc_stubs.f,v $ (version $Revision$)
-! ($Date: 1996/09/13 17:23:45 $, )
+! ($Date: 1996/09/13 17:55:24 $, )
 !     
 ! synchronization 
       subroutine hpfc1
-!fcd$ fake
+!!fcd$ fake
       print *, 'hpfc1: '
       end
 ! timer on
       subroutine hpfc2
-!fcd$ fake
+!!fcd$ fake
       print *, 'hpfc2: '
       end
 ! timer off
       subroutine hpfc3(comment)
-!fcd$ fake
+!!fcd$ fake
       character comment*(*)
       print *, 'hpfc3: ', comment
       end
 ! io/host section marker
       subroutine hpfc7
-!fcd$ fake
+!!fcd$ fake
       print *, 'hpfc7: '
       end
 ! dead FC directive. one argument, why not...
       subroutine hpfc8(x)
-!fcd$ fake
+!!fcd$ fake
       integer x
       print *, 'hpfc8: ', x
       end
