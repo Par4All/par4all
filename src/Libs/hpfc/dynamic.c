565614055faad1a19a0d970b8113844c1e229640
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1996-12-26 11:14:21 +0000

    # on 1996/12/26 11:14:21, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5794 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/hpfc/dynamic.c
+++ src/Libs/hpfc/dynamic.c
@@ -6,7 +6,7 @@
  * to deal with them in HPFC.
  *
  * $RCSfile: dynamic.c,v $ version $Revision$
- * ($Date: 1996/12/26 10:41:29 $, )
+ * ($Date: 1996/12/26 11:14:21 $, )
  */
 
 #include "defines-local.h"
@@ -128,7 +128,6 @@ static void add_dynamic_synonym(
     entities_list(es) = CONS(ENTITY, new_e, entities_list(es));
     store_dynamic_hpf(new_e, es);
     store_primary_entity(new_e, load_primary_entity(e));
-
 }
 
 void 
@@ -139,11 +138,13 @@ set_similar_mappings_for_updates(void)
 
     MAP(ENTITY, array,
     {
-	if (dynamic_entity_p(array))
+	if (dynamic_entity_p(array) && bound_similar_mapping_p(array))
 	{
 	    entity n = load_new_node(array);
 	    entity s = load_similar_mapping(array);
 	    entity ns = load_new_node(s);
+
+	    pips_debug(7, "array %s\n", entity_name(array));
 	    
 	    store_new_node_variable(ns, n);
 	    store_new_node_variable(ns, array);
