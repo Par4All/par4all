6800f470185ceb135d34db095f18799381660012
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-01-02 18:41:25 +0000

    # on 1997/01/02 18:41:25, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5885 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/hpfc/hpfc_commons.h
+++ src/Runtimes/hpfc/hpfc_commons.h
@@ -1,19 +1,19 @@
-c
-c inclusions for run-time support functions
-c
-c $RCSfile: hpfc_commons.h,v $ version $Revision$
-c ($Date: 1997/01/02 17:52:24 $, )
-c
-c
+!
+! inclusions for run-time support functions
+!
+! $RCSfile: hpfc_commons.h,v $ version $Revision$
+! ($Date: 1997/01/02 18:41:25 $, )
+!
+!
       include 'hpfc_parameters.h'
       include 'hpfc_param.h'
       include 'hpfc_globs.h'
       include 'hpfc_procs.h'
       include 'hpfc_buffers.h'
       include 'hpfc_misc.h'
-c
-c parameters value to be defined when starting a program.
-c
+!
+! parameters value to be defined when starting a program.
+!
       integer
      $     NBOFARRAYS,
      $     NBOFTEMPLATES,
@@ -26,6 +26,6 @@ c
      $     NBOFPROCESSORS,
      $     MAXSIZEOFPROCS,
      $     SIZEOFBUFFER
-c
-c that s all
-c
+!
+! that s all
+!
