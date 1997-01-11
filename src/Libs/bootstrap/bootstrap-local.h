be0671242711828549cfaf0d84c974051ae8f27a
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-01-11 22:54:51 +0000

    # on 1997/01/11 22:54:51, irigoin did: New names for handlings errors and end-of-file conditions in IO statements.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5965 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/bootstrap/bootstrap-local.h
+++ src/Libs/bootstrap/bootstrap-local.h
@@ -1,9 +1,14 @@
-
-/* GO: package name for io effects */
+/* GO: package name for io routines */
 #define IO_EFFECTS_PACKAGE_NAME "__IO_EFFECTS"
 
-/* GO: corresponding array name */
+/* GO: array of Logical UNits; it is more or less handled as the current file pointer */
 #define IO_EFFECTS_ARRAY_NAME "LUNS"
+
+/* GO: array of end of file codes */
+#define IO_EOF_ARRAY_NAME "END_LUNS"
+
+/* GO: array of error codes for LUNs */
+#define IO_ERROR_ARRAY_NAME "ERR_LUNS"
 
 /* GO: size of the unit specifier */
 #define IO_EFFECTS_UNIT_SPECIFIER_LENGTH 4
