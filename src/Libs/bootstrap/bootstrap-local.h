afa85cf1ea5ee2408709433852a50351de27d1d8
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-01-13 22:00:25 +0000

    # on 1997/01/13 22:00:25, irigoin did: Definitions moved from bootstrap-local.h into ri-util-local.h
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@5985 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/bootstrap/bootstrap-local.h
+++ src/Libs/bootstrap/bootstrap-local.h
@@ -1,14 +1,1 @@
-/* GO: package name for io routines */
-#define IO_EFFECTS_PACKAGE_NAME "__IO_EFFECTS"
-
-/* GO: array of Logical UNits; it is more or less handled as the current file pointer */
-#define IO_EFFECTS_ARRAY_NAME "LUNS"
-
-/* GO: array of end of file codes */
-#define IO_EOF_ARRAY_NAME "END_LUNS"
-
-/* GO: array of error codes for LUNs */
-#define IO_ERROR_ARRAY_NAME "ERR_LUNS"
-
-/* GO: size of the unit specifier */
-#define IO_EFFECTS_UNIT_SPECIFIER_LENGTH 4
+/* Empty bootstrap-local.h */
