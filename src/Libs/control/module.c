5680ed703b97d2daccd789a57d7eeeb9887fcdbe
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-01-24 15:44:10 +0000

    # on 1997/01/24 15:44:10, keryell did: Now the controlizer can use unspaghettify.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6066 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/control/module.c
+++ src/Libs/control/module.c
@@ -11,6 +11,7 @@
 #include "pipsdbm.h"
 
 #include "resources.h"
+#include "properties.h"
 #include "constants.h"
 
 /* interface with pipsdbm and pipsmake */
@@ -22,13 +23,13 @@ string module_name;
     statement module_stat;
     
     module_stat = 
-	    copy_statement((statement)
-			   db_get_memory_resource(DBR_PARSED_CODE, 
-						  module_name, 
-						  TRUE)) ;
-
-    /* *module_stat can be re-used because control_graph reallocates
-       statements; do not show that to any student! */
+	copy_statement((statement)
+		       db_get_memory_resource(DBR_PARSED_CODE, 
+					      module_name, 
+					      TRUE)) ;
+    
+       /* *module_stat can be re-used because control_graph reallocates
+	  statements; do not show that to any student! */
     statement_instruction(module_stat) =
 	make_instruction(is_instruction_unstructured,
 			 control_graph(module_stat));
@@ -37,10 +38,16 @@ string module_name;
     /* By setting this property, we try to unspaghettify the control
        graph of the module: */
     if (get_bool_property("UNSPAGHETTIFY_IN_CONTROLIZER")) {
+	/* To have the debug in unspaghettify_statement() working: */
+	set_current_module_statement(module_stat);
+	set_current_module_entity(local_name_to_top_level_entity(module_name));
+       
 	unspaghettify_statement(module_stat);
-	/* Reorder the module, because new statementsthe control graph
-	   may have been generatchanged. */
-	   module_reorder(module_stat);
+	/* Reorder the module, because statements may have been
+           changed. */
+	module_reorder(module_stat);
+	reset_current_module_statement();
+	reset_current_module_entity();
     }
     
     DB_PUT_MEMORY_RESOURCE(DBR_CODE, 
