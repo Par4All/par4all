b523eeacbcb2fa119e28cf308a420c8d7d0db1ce
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-02-04 18:31:04 +0000

    # on 1997/02/04 18:31:04, irigoin did: CONTROL_MAP moved from ri-util
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6134 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/ri-util-local.h
+++ src/Libs/ri-util/ri-util-local.h
@@ -489,7 +489,36 @@ entity get_ith_##lname##_prime(int i)\
 #define IO_EFFECTS_UNIT_SPECIFIER_LENGTH 4
 
 /* Empty comments (i.e. default comments) */
+
 #define empty_comments string_undefined
+
+/* Macro (and function) to walk through unstructured */
+
+static void get_blocs( c, l )
+control c ;
+cons **l ;
+{
+    MAPL( cs, {if( CONTROL( CAR( cs )) == c ) return ;}, *l ) ;
+    *l = CONS( CONTROL, c, *l ) ;
+    MAPL( cs, {get_blocs( CONTROL( CAR( cs )), l );}, control_successors( c )) ;
+    MAPL( ps, {get_blocs( CONTROL( CAR( ps )), l );}, control_predecessors( c )) ;
+}
+
+#define CONTROL_MAP( ctl, code, c, list ) \
+{ \
+    cons *_cm_list_init = (list) ; \
+    cons *_cm_list = _cm_list_init ; \
+    if( _cm_list == NIL ) {\
+         get_blocs( c, &_cm_list ) ; \
+         _cm_list = gen_nreverse( _cm_list ) ; \
+    }\
+    MAPL( _cm_ctls, {control ctl = CONTROL( CAR( _cm_ctls )) ; \
+ \
+		 code ;}, \
+	  _cm_list ) ; \
+   if( _cm_list_init == NIL ) \
+        list = _cm_list ; \
+}
 
 /* that is all for $RCSfile: ri-util-local.h,v $
  */
