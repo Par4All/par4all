bd1d68edffe8db4a0ac5a658f5fdf3e4be7c3dd8
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-02-04 18:37:18 +0000

    # on 1997/02/04 18:37:18, irigoin did: CONTROL_MAP moved into ri-util-local.h
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6136 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/control/control-local.h
+++ src/Libs/control/control-local.h
@@ -3,30 +3,4 @@
 #ifndef CONTROL_INCLUDED
 #define CONTROL_INCLUDED
 
-static void get_blocs( c, l )
-control c ;
-cons **l ;
-{
-    MAPL( cs, {if( CONTROL( CAR( cs )) == c ) return ;}, *l ) ;
-    *l = CONS( CONTROL, c, *l ) ;
-    MAPL( cs, {get_blocs( CONTROL( CAR( cs )), l );}, control_successors( c )) ;
-    MAPL( ps, {get_blocs( CONTROL( CAR( ps )), l );}, control_predecessors( c )) ;
-}
-
-#define CONTROL_MAP( ctl, code, c, list ) \
-{ \
-    cons *_cm_list_init = (list) ; \
-    cons *_cm_list = _cm_list_init ; \
-    if( _cm_list == NIL ) {\
-         get_blocs( c, &_cm_list ) ; \
-         _cm_list = gen_nreverse( _cm_list ) ; \
-    }\
-    MAPL( _cm_ctls, {control ctl = CONTROL( CAR( _cm_ctls )) ; \
- \
-		 code ;}, \
-	  _cm_list ) ; \
-   if( _cm_list_init == NIL ) \
-        list = _cm_list ; \
-}
-
 #endif
