489905bd13004a49a11e084923adc47a733f8502
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-03-07 14:14:07 +0000

    # on 1997/03/07 14:14:07, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6227 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/tpips/tpips-local.h
+++ src/Passes/tpips/tpips-local.h
@@ -1,5 +1,5 @@
 /* $RCSfile: tpips-local.h,v $ (version $Revision$)
- * $Date: 1997/01/01 20:50:39 $, 
+ * $Date: 1997/03/07 14:14:07 $, 
  */
 
 /* FI: temporary storage of file names could be avoided with better yacc rules
@@ -29,6 +29,26 @@ extern FILE * tp_in;
 #ifdef FLEX_SCANNER
 extern void tp_restart(FILE *);
 #endif
+
+#define TPIPS_PRIMARY_PROMPT 	"tpips> " /* prompt for readline  */
+#define TPIPS_SECONDARY_PROMPT 	"> "
+#define TPIPS_CONTINUATION_CHAR '\\'
+
+#define TPIPS_HISTENV "TPIPS_HISTORY"	/* history file env variable */
+#define TPIPS_HISTORY_LENGTH 100	/* max length of history file */
+#define TPIPS_COMMENT_PREFIX '#'	/* comment prefix */
+#define TPIPS_HISTORY ".tpips.history" 	/* default history file */
+#define TPIPS_REQUEST_BUFFER_LENGTH 100
+
+#define SHELL_ESCAPE "shell" 		/* ! used for history reference */
+#define CHANGE_DIR   "cd "
+#define SET_ENV	     "setenv "
+#define SET_PROP     "setproperty "
+
+#define GET_PROP     "getproperty"
+#define QUIT         "quit"
+#define HELP         "help"
+#define ECHO         "echo"
 
 /* end of $RCSfile: tpips-local.h,v $ 
  */
