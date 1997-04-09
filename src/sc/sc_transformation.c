dc13adfced9ce876097a5568810e101d32a906df
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1997-04-09 19:25:19 +0000

    # on 1997/04/09 19:25:19, coelho did: == extraction implemented (+- Maslov)
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@809 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/sc/sc_transformation.c
+++ src/sc/sc_transformation.c
@@ -1,4 +1,7 @@
-/* Package sc */
+/* Package sc 
+ * $RCSfile: sc_transformation.c,v $ (version $Revision$)
+ * $Date: 1997/04/09 19:25:19 $, .
+ */
 
 #include <string.h>
 #include <stdio.h>
@@ -9,6 +12,33 @@
 #include "contrainte.h"
 #include "sc.h"
 #include "malloc.h"
+
+/* Transform each equality in two inequalities
+ */
+
+void sc_transform_eg_in_ineg(sc)
+Psysteme sc;
+{
+
+    Pcontrainte eg,pc1,pc2;
+
+    for (eg = sc->egalites; 
+	 !CONTRAINTE_UNDEFINED_P(eg); 
+	 eg=eg->succ) 
+    {
+	pc1 = contrainte_dup(eg);
+	pc2 = contrainte_dup(eg);
+	vect_chg_sgn(pc2->vecteur);
+	sc_add_ineg(sc,pc1);
+	sc_add_ineg(sc,pc2);
+    }
+    
+    sc->egalites = contraintes_free(sc->egalites);
+    sc->nb_eq = 0;
+
+    sc = sc_elim_db_constraints(sc); 
+    /* ??? humm, the result is *not* returned */
+}
 
 
 /* Transform the two constraints A.x <= b and -A.x <= -b of system sc into 
@@ -48,29 +78,175 @@ Psysteme sc;
     /* ??? humm, the result is *not* returned */
 }
 
-
-/* Transform each equality in two inequalities */
-
-void sc_transform_eg_in_ineg(sc)
-Psysteme sc;
+/* sc_find_equalities(Psysteme * ps)
+ *
+ * what: updates *ps to an equivalent system with more/better equalities.
+ * in/out: *ps
+ * how:   (1) Maslov PLDI'92;   (2) (a <= x <= a) => (x == a)
+ *
+ * (1) Inspired by Vadim Maslov, PLDI'92 "delinearization". It is based on
+ * the following property (a 1 page theorem and proof in his paper):
+ * 
+ * Prop: forall a,n,m in Z, a>0, |m|<a, (an+m=0  <=>  n=0, m=0)
+ * Proof: [<=] trivial; [=>] a|n|=|m|<a => |n|<1 => n=0 => m=0;
+ *
+ * From this property, typical of dependence tests on linearized accesses,
+ * two simple equations are derived from a bigger one and inequalities.
+ * Our purpose is to identify the simpler equalities and to substitute
+ * them in the system, not to perform an actual dependence test.
+ *
+ * The issue is to identify candidates n and m in equalities, so as to test
+ * the bounds on m and match the property conditions. We will not assume
+ * cartesian constraints on the variables, but rather compute it by projection.
+ * We will use the gcd condition before projecting to find m bounds. 
+ * By doing so, we may lose some instances if a variable is in fact a constant.
+ *
+ * FC, Apr 09 1997.
+ */
+static int abscmp(Pvecteur * pv1, Pvecteur * pv2)
 {
-
-    Pcontrainte eg,pc1,pc2;
-
-    for (eg = sc->egalites; 
-	 !CONTRAINTE_UNDEFINED_P(eg); 
-	 eg=eg->succ) 
-    {
-	pc1 = contrainte_dup(eg);
-	pc2 = contrainte_dup(eg);
-	vect_chg_sgn(pc2->vecteur);
-	sc_add_ineg(sc,pc1);
-	sc_add_ineg(sc,pc2);
-    }
-    
-    sc->egalites = contraintes_free(sc->egalites);
-    sc->nb_eq = 0;
-
-    sc = sc_elim_db_constraints(sc); 
-    /* ??? humm, the result is *not* returned */
+    Value v1 = value_abs(val_of(*pv1)), v2 = value_abs(val_of(*pv2));
+    return value_compare(v1, v2);
 }
+
+void sc_find_equalities(Psysteme * ps)
+{
+    Variable vtmp = (Variable) "local temporary variable";
+    Pcontrainte eq;
+
+    /* first round
+     */
+
+    /* FOR EACH EQUALITY
+     */
+    for (eq = sc_egalites(*ps); eq; eq = eq->succ)
+    {
+	Pvecteur m, an;
+	Value c, a;
+	
+	an = vect_dup(contrainte_vecteur(eq));
+	m = vect_new(vtmp, VALUE_MONE);
+
+	c = vect_coeff(TCST, an);
+	vect_erase_var(&an, TCST);
+
+	vect_sort_in_place(&an, abscmp);
+	
+	/* BUILD POSSIBLE AN AND M (AN + M + C == 0)
+	 */
+	while (!VECTEUR_NUL_P(an))
+	{
+	    Value ref = value_abs(val_of(an)), coeff;
+	    Value vmin, vmax, d, r, dp, rp, delta, ma;
+	    Psysteme ns;
+
+	    do
+	    {
+		Pvecteur v = m; /* insert head(an) in front of m */
+		m = an;
+		an = an->succ;
+		m->succ = v;
+		coeff = VECTEUR_NUL_P(an)? VALUE_ZERO: value_abs(val_of(an));
+	    } 
+	    while (value_eq(coeff, ref));
+
+	    /* WITH AN, COMPUTES A AND CHECK THE GCD CONDITION 
+	     */
+	    a = vect_pgcd_all(an);
+	    if (value_one_p(a)) 
+		continue;
+
+	    /* COMPUTE M BOUNDS, IF ANY
+	     */
+	    ns = sc_dup(*ps);
+	    sc_add_egalite(ns, contrainte_make(vect_dup(m)));
+	    base_rm(sc_base(ns));
+	    sc_creer_base(ns);
+
+	    if (!sc_minmax_of_variable(ns, vtmp, &vmin, &vmax))
+	    {
+		/* the system is not feasible. */
+		vect_rm(m); 
+		vect_rm(an);
+		sc_rm(*ps);
+		*ps = sc_empty(BASE_NULLE);
+		return;
+	    }
+
+	    if (value_min_p(vmin) || value_max_p(vmax))
+		/* well, if it is not bounded, the latter tests won't 
+		 * be bounded either, thus we could shorten the loop
+		 * for this eq???
+		 */
+		continue;
+
+	    /* now, we must compute the shifts...
+	     *
+	     * an + m + c == 0,   vmin <= m <= vmax ;
+	     * c = a d + r,  0 <= r < a, 
+	     * a (n+d) + (m+r) == 0,   vmin+r <= m+r <= vmax+r,
+	     * (vmax+r) = a d' + r', 0 <= r' < a,
+	     * vmin+r-ad' <= m+r-ad' <= r' < a
+	     * 
+	     * question: -a < vmin+r-ad ?
+	     * if YES: m+r-ad'==0 and n+d+d'==0
+	     * assert: vmin+r-ad<=0 (or not feasible!)
+	     */
+
+	    /* c = a d + r 
+	     */
+	    d = value_pdiv(c, a); 
+	    r = value_pmod(c, a); 
+
+	    value_addto(vmax, r);
+	    value_addto(vmin, r);
+	    
+	    /* vmax' = a dp + rp
+	     */
+	    dp = value_pdiv(vmax, a); 
+	    rp = value_pmod(vmax, a);
+
+	    /* delta = -ad'
+	     */
+	    delta = value_minus(rp, vmax); 
+	    vmax = rp;
+	    value_addto(vmin, delta);
+	    ma = value_uminus(a);
+	    
+	    if (value_lt(ma, vmin)) /* CONDITION |m|<a */
+	    {
+		Value n_shift, m_shift;
+
+		Pvecteur n = vect_div(an, a); /* an is lost */
+		n_shift = value_plus(d,dp);
+		vect_add_elem(&n, TCST, n_shift);
+		
+		vect_erase_var(&m, vtmp);
+		m_shift = value_plus(r, delta);
+		vect_add_elem(&m, TCST, m_shift);
+
+		/* INSERT m==0 and n==0 [in place of the old one]
+		 */
+		sc_add_egalite(*ps, contrainte_make(m)); /* m pointed to */
+		
+		vect_rm(contrainte_vecteur(eq));
+		contrainte_vecteur(eq) = vect_dup(n);
+
+		/* KEEPS ON WITH N AND resetted M
+		 */
+		m = vect_new(vtmp, VALUE_MONE);
+		an = n;
+	    }
+	}
+	
+	vect_rm(m), m=VECTEUR_NUL;
+	vect_rm(an), an=VECTEUR_NUL;
+    }
+
+    /* second round
+     */
+    sc_transform_ineg_in_eg(*ps);
+}
+
+/* that is all
+ */
