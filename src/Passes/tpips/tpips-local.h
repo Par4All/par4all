42ca3639e8dd7ba52faeba0468a1996a0f752f89
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-04-11 16:18:24 +0000

    # on 1997/04/11 16:18:24, coelho did: GET_ENV added, and cleaned.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6483 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/tpips/tpips-local.h
+++ src/Passes/tpips/tpips-local.h
@@ -1,5 +1,5 @@
 /* $RCSfile: tpips-local.h,v $ (version $Revision$)
- * $Date: 1997/04/10 16:41:25 $, 
+ * $Date: 1997/04/11 16:18:24 $, 
  */
 
 /* FI: temporary storage of file names could be avoided with better yacc rules
@@ -31,22 +31,24 @@ extern void tp_restart(FILE *);
 #endif
 
 #define TPIPS_PRIMARY_PROMPT 	"tpips> " /* prompt for readline  */
+#define TPIPS_REQUEST_PROMPT    "tpips-request> "
 #define TPIPS_SECONDARY_PROMPT 	"> "
 #define TPIPS_CONTINUATION_CHAR '\\'
-#define TPIPS_REQUEST_PROMPT    "tpips-request> "
 
 #define TPIPS_HISTENV "TPIPS_HISTORY"	/* history file env variable */
 #define TPIPS_HISTORY_LENGTH 100	/* max length of history file */
 #define TPIPS_COMMENT_PREFIX '#'	/* comment prefix */
 #define TPIPS_HISTORY ".tpips.history" 	/* default history file */
-#define TPIPS_REQUEST_BUFFER_LENGTH 100
 
 #define SHELL_ESCAPE "shell" 		/* ! used for history reference */
 #define CHANGE_DIR   "cd "
-#define SET_ENV	     "setenv "
-#define SET_PROP     "setproperty "
 
+#define SET_ENV	     "setenv "
+#define GET_ENV	     "getenv "
+
+#define SET_PROP     "setproperty "
 #define GET_PROP     "getproperty"
+
 #define QUIT         "quit"
 #define HELP         "help"
 #define ECHO         "echo"
