5b25278813ac3aa1b1f27801dd7ba489dd9d71d8
creusil <creusil@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-04-16 11:27:51 +0000

    # on 1997/04/16 11:27:51, creusil did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6543 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/comp_sections/all.h
@@ -0,0 +1,68 @@
+#ifndef _ALL
+#define ALL
+
+#include <stdio.h>
+#include <string.h>
+
+/*{{{  newgen includes*/
+#include "genC.h"
+#include "ri.h"
+#include "database.h"
+#include "newgen_hash.h"
+
+/*}}}*/
+
+/* C3 includes 		*/
+#include "boolean.h"
+#include "arithmetique.h"
+#include "vecteur.h"
+#include "contrainte.h"
+#include "ray_dte.h"
+#include "sommet.h"
+#include "sg.h"
+#include "sc.h"
+#include "polyedre.h"
+#include "matrix.h"
+
+/* Pips includes 	*/
+#include "ri.h"
+#include "ri-util.h"
+#include "constants.h"
+#include "graph.h"
+#include "dg.h"
+#include "paf_ri.h"
+#include "text.h"
+#include "text-util.h"
+#include "misc.h"
+#include "paf-util.h"
+#include "static_controlize.h"
+
+
+#include "control.h"
+#include "constants.h"
+#include "misc.h"
+#include "text-util.h"
+#include "properties.h"
+
+#include "transformer.h"
+#include "semantics.h"
+
+#include "effects.h"
+#include "regions.h"
+
+#include "pipsdbm.h"
+#include "resources.h"
+#include "compsec.h"
+#include "bootstrap.h"
+
+/* all typedef's and declarations will appear in this file
+ * this is PIPS policy!
+ */
+
+#include "comp_sections.h"
+
+
+#endif
+
+
+
