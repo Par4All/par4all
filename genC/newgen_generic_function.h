fa997fcaa6190f51cc0aebc1bc6ea3cfc0e54dbb
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1997-04-17 17:58:44 +0000

    # on 1997/04/17 17:58:44, coelho did: store_or_update added, plus ansi declarations.
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@407 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- genC/newgen_generic_function.h
+++ genC/newgen_generic_function.h
@@ -1,4 +1,4 @@
-/* $RCSfile: newgen_generic_function.h,v $ ($Date: 1996/09/21 15:42:31 $, )
+/* $RCSfile: newgen_generic_function.h,v $ ($Date: 1997/04/17 17:58:44 $, )
  * version $Revision$
  * got on %D%, %T%
  */
@@ -10,27 +10,27 @@
  */
 #define GENERIC_STATIC_OBJECT(PREFIX, name, type)\
 static type name##_object = type##_undefined;\
-PREFIX bool name##_undefined_p() { return name##_object==type##_undefined;}\
-PREFIX void reset_##name() \
+PREFIX bool name##_undefined_p(void){ return name##_object==type##_undefined;}\
+PREFIX void reset_##name(void) \
 { message_assert("must reset sg defined", !name##_undefined_p());\
   name##_object=type##_undefined;}\
 PREFIX void set_##name(type o)\
 { message_assert("must set sg undefined", name##_undefined_p());\
   name##_object=o;}\
-PREFIX type get_##name() \
+PREFIX type get_##name(void) \
 { message_assert("must get sg defined", !name##_undefined_p());\
   return name##_object;}\
-static int name##_generic_static_status_hack()\
+static int name##_generic_static_status_hack(void)\
 { return (int) reset_##name & (int) set_##name & \
       (int) reset_##name & (int) get_##name & \
       (int) name##_generic_static_status_hack;}
 
 #define GENERIC_STATIC_STATUS(PREFIX, name, type, init, cloze)\
 GENERIC_STATIC_OBJECT(PREFIX, name, type)\
-PREFIX void init_##name() \
+PREFIX void init_##name(void) \
 { message_assert("must initialize sg undefined", name##_undefined_p());\
   name##_object = init;}\
-PREFIX void close_##name()\
+PREFIX void close_##name(void)\
 { message_assert("must close sg defined", !name##_undefined_p());\
   cloze(name##_object); name##_object = type##_undefined;}
 
@@ -49,28 +49,31 @@ PREFIX void close_##name()\
 
 #define GENERIC_FUNCTION(PREFIX, name, type)\
 GENERIC_STATIC_STATUS(PREFIX, name, type, make_##type(), free_##type)\
-PREFIX void store_##name(k,v) type##_key_type k; type##_value_type v;\
+PREFIX void store_##name(type##_key_type k, type##_value_type v)\
        { extend_##type(name##_object, k, v);}\
-PREFIX void update_##name(k,v) type##_key_type k; type##_value_type v;\
+PREFIX void update_##name(type##_key_type k, type##_value_type v)\
        { update_##type(name##_object, k, v);}\
-PREFIX type##_value_type load_##name(k) type##_key_type k;\
+PREFIX type##_value_type load_##name(type##_key_type k)\
        { return(apply_##type(name##_object, k));}\
-PREFIX type##_value_type delete_##name(k) type##_key_type k;\
+PREFIX type##_value_type delete_##name(type##_key_type k)\
        { return(delete_##type(name##_object, k));}\
-PREFIX bool bound_##name##_p(k) type##_key_type k; \
-       { return(bound_##type##_p(name##_object, k));}
+PREFIX bool bound_##name##_p(type##_key_type k) \
+       { return(bound_##type##_p(name##_object, k));} \
+PREFIX void store_or_update_##name(type##_key_type k, type##_value_type v)\
+       { if(bound_##name##_p(k)) update_##name(k, v); else store_##name(k, v);}
 
 /* plus a non good looking hack to avoid gcc warnings about undefined statics.
  */
 #define GENERIC_LOCAL_FUNCTION(name, type)\
         GENERIC_FUNCTION(static, name, type)\
-static int name##_generic_local_function_hack()\
+static int name##_generic_local_function_hack(void)\
 { return (int) name##_undefined_p & (int) reset_##name & \
 	 (int) set_##name & (int) get_##name & \
          (int) init_##name & (int) close_##name & \
 	 (int) update_##name & (int) load_##name & \
 	 (int) bound_##name##_p & (int) delete_##name & \
-         (int) store_##name & (int) name##_generic_local_function_hack;}
+         (int) store_##name & (int) store_or_update_##name & \
+         (int) name##_generic_local_function_hack;}
 
 #define GENERIC_GLOBAL_FUNCTION(name, type)\
         GENERIC_FUNCTION(extern, name, type)
