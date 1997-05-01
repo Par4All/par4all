e148d896cbd7b147d06db9e01542ba79368c6bbc
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-05-01 09:39:08 +0000

    # on 1997/05/01 09:39:08, irigoin did: Quotes added around command names to improve the readability of error messages.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6686 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/misc/system.c
+++ src/Libs/misc/system.c
@@ -1,5 +1,5 @@
 /* $RCSfile: system.c,v $ version $Revision$
- * ($Date: 1997/04/29 10:47:58 $, )
+ * ($Date: 1997/05/01 09:39:08 $, )
  *
  * a safe system call. abort if fails.
  * FC 09/95
@@ -29,10 +29,10 @@ safe_system_no_abort(
     int status = system(command);
     
     if (status == 127)
-	pips_error("safe_system", "Could not execute : %s\n", command);
+	pips_error("safe_system", "Could not execute : '%s'\n", command);
 
     if (status)
-	pips_user_warning("Failed (ret: %d, sig: %d) for %s\n", 
+	pips_user_warning("Failed (ret: %d, sig: %d) for '%s'\n", 
 			  (status/0x100) & 0xff, status & 0xff, command);
 
     return (status / 256) & 255;
