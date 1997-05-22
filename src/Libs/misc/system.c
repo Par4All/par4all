5dae0ed0eb1a583b419ed09d31a177a8eec4391b
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-05-22 11:16:34 +0000

    # on 1997/05/22 11:16:34, irigoin did: call to pips_user_warning() replaced by call to user_warning()
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6791 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/misc/system.c
+++ src/Libs/misc/system.c
@@ -1,5 +1,5 @@
 /* $RCSfile: system.c,v $ version $Revision$
- * ($Date: 1997/05/01 09:39:08 $, )
+ * ($Date: 1997/05/22 11:16:34 $, )
  *
  * a safe system call. abort if fails.
  * FC 09/95
@@ -31,9 +31,12 @@ safe_system_no_abort(
     if (status == 127)
 	pips_error("safe_system", "Could not execute : '%s'\n", command);
 
-    if (status)
-	pips_user_warning("Failed (ret: %d, sig: %d) for '%s'\n", 
-			  (status/0x100) & 0xff, status & 0xff, command);
+    if (status) {
+	/* For portability reasons, do not use pips_user_warning() here */
+	user_warning("safe_system_no_abort", 
+		     "Failed (ret: %d, sig: %d) for '%s'\n", 
+		     (status/0x100) & 0xff, status & 0xff, command);
+    }
 
     return (status / 256) & 255;
 }
