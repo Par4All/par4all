02dff58a9fec6d178dc162cf475669a04ae95af0
zory <zory@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-06-03 15:16:02 +0000

    # on 1997/06/03 15:16:02, zory did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6947 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/hpfc/hpfc_globs.h
+++ src/Runtimes/hpfc/hpfc_globs.h
@@ -3,33 +3,36 @@
 !
 ! $Id$
 ! $Log: hpfc_globs.h,v $
+! Revision 1.5  1997/06/03 15:16:02  zory
+! *** empty log message ***
+!
 ! Revision 1.4  1997/03/20 07:51:13  coelho
 ! better RCS headers.
 !
 !
       common /HPFC GLOBS/
      $     OWNER TID,
-     $     OLID,
+     $     OWNERLID,
      $     OPN,
      $     OINDP(7),
      $     OREPLICATED,
      $     SENDER TID,
-     $     SLID,
+     $     SENDERLID,
      $     COMPUTER TID,
-     $     CLID,
+     $     COMPUTERLID,
      $     CPN,
      $     CINDP(7),
      $     CPOS(7),
      $     CPOSPN,
      $     CPOS COMPUTED,
-     $     NLID,
+     $     NEIGHBORLID,
      $     NTID
 !
       integer 
-     $     OWNER TID,    OLID, OPN, OINDP,
-     $     SENDER TID,   SLID,
-     $     COMPUTER TID, CLID, CPN, CINDP, CPOS, CPOSPN,
-     $     N LID, N TID
+     $     OWNER TID,    OWNERLID, OPN, OINDP,
+     $     SENDER TID,   SENDERLID,
+     $     COMPUTER TID, COMPUTERLID, CPN, CINDP, CPOS, CPOSPN,
+     $     NEIGHBORLID, N TID
 !
       logical
      $     O REPLICATED, CPOS COMPUTED
