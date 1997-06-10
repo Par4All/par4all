666feb3c755e761690c10a0c70097b0ccbc81586
zory <zory@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-06-10 07:57:03 +0000

    # on 1997/06/10 07:57:03, zory did: specific declarations for pvm
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6981 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Runtimes/hpfc/hpfc_specific_pvm.h
@@ -0,0 +1,29 @@
+!
+! $Id$
+!
+! $Log: hpfc_specific_pvm.h,v $
+! Revision 1.1  1997/06/10 07:57:03  zory
+! Initial revision
+!
+!
+
+!
+! PVM encoding to use.
+!
+
+      common /HPFC PVM ENCODING/
+     $     HPFC BUFFER ENCODING(2)
+      integer HPFC BUFFER ENCODING
+
+!
+! PVM type to use
+!
+     
+      common /HPFC PVM TYPES/
+     $     HPFC TYPE PVM(10)
+      integer HPFC TYPE PVM
+
+
+!
+! that's all 
+!
