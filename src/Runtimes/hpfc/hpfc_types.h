c998caa17fd840afef4a823c2430157dff3ef29f
zory <zory@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-06-10 07:55:57 +0000

    # on 1997/06/10 07:55:57, zory did: hpfc specific declaration of types
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6980 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Runtimes/hpfc/hpfc_types.h
@@ -0,0 +1,31 @@
+!
+! $Id$
+!
+! $Log: hpfc_types.h,v $
+! Revision 1.1  1997/06/10 07:55:57  zory
+! Initial revision
+!
+!
+
+! 
+! HPFC Types definition 
+!
+
+      integer 
+     $     HPFC INTEGER4,
+     $     HPFC INTEGER8,
+     $     HPFC REAL4,
+     $     HPFC REAL8,
+     $     HPFC CHARACTER
+
+
+      parameter(HPFC INTEGER = 1) 
+      parameter(HPFC INTEGER4 = 2)
+      parameter(HPFC REAL4 = 3)
+      parameter(HPFC REAL8 = 4)
+      parameter(HPFC CHARACTER = 5)
+
+
+!
+! that's all
+!
