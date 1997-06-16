bc61a9ace5fb58090ac3ad9f6fa37b1913de24dc
zory <zory@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-06-16 12:55:03 +0000

    # on 1997/06/16 12:55:03, zory did: specific MPI includes
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6993 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Runtimes/hpfc/hpfc_commons_mpi.h
@@ -0,0 +1,21 @@
+!
+! $Id$
+!
+! $Log: hpfc_commons_mpi.h,v $
+! Revision 1.1  1997/06/16 12:55:03  zory
+! Initial revision
+!
+!
+
+      include "hpfc_commons.h"
+!
+! PVM specific
+!
+      include "mpif.h"
+      include "hpfc_specific_mpi.h"
+
+
+
+!
+! that's all 
+!
