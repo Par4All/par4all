192991c7a5ff2c218d75d12a57364c0250892d5a
zory <zory@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-06-16 12:52:20 +0000

    # on 1997/06/16 12:52:20, zory did: MPI specific declarations
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@6992 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Runtimes/hpfc/hpfc_specific_mpi.h
@@ -0,0 +1,23 @@
+!
+! $Id$
+!
+! $Log: hpfc_specific_mpi.h,v $
+! Revision 1.1  1997/06/16 12:52:20  zory
+! Initial revision
+!
+!
+
+
+!
+! MPI type to use
+!
+     
+      common /HPFC MPI TYPES/
+     $     HPFC TYPE MPI(8)
+      integer HPFC TYPE MPI
+
+
+
+!
+! that's all 
+!
