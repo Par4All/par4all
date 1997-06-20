5a9de8a404806993605276a415d39d14f338d30d
creusil <creusil@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-06-20 08:45:32 +0000

    # on 1997/06/20 08:45:32, creusil did: added library alias-classes. Nicky.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7009 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/dev/define_libraries.sh
+++ src/Scripts/dev/define_libraries.sh
@@ -2,6 +2,9 @@
 # $Id$
 #
 # $Log: define_libraries.sh,v $
+# Revision 1.14  1997/06/20 08:45:32  creusil
+# added library alias-classes. Nicky.
+#
 # Revision 1.13  1997/04/19 07:46:21  coelho
 # *** empty log message ***
 #
@@ -46,10 +49,10 @@ LINEAR_LIBS='-lmatrice -lunion -lpolyedre -lsparse_sc -lsc -lcontrainte -lsg -ls
 # many doc dirs are skipped (would be long?)
 PIPS_DOCS='Documentation constants help manl newgen'
 
-PIPS_ORDERED_LIBS='xpomp text-util properties misc ri-util conversion movements pipsdbm comp_sections transformer bootstrap control hyperplane flint syntax prettyprint static_controlize effects semantics complexity continuation reductions regions callgraph icfg paf-util pip ricedg array_dfg prgm_mapping scheduling reindexing chains rice transformations hpfc atomizer wp65 pipsmake top-level pips tpips wpips'
+PIPS_ORDERED_LIBS='xpomp text-util properties misc ri-util conversion movements pipsdbm comp_sections transformer bootstrap control hyperplane flint syntax prettyprint static_controlize effects semantics complexity continuation reductions regions alias-classes callgraph icfg paf-util pip ricedg array_dfg prgm_mapping scheduling reindexing chains rice transformations hpfc atomizer wp65 pipsmake top-level pips tpips wpips'
 
 # all libraires for pips
-PIPS_LIBS='-ltop-level -lpipsmake -lwp65 -lhpfc -ltransformations -lmovements -lbootstrap -lcallgraph -licfg -lchains -lcomplexity -lconversion -lprettyprint -latomizer -lprgm_mapping -lscheduling -lreindexing -larray_dfg -lpaf-util -lstatic_controlize -lsyntax -lregions -lcomp_sections -lcontrol -lsemantics -lcontinuation -lrice -lricedg -leffects -lpipsdbm -ltransformer -lpip -lri-util -lproperties -ltext-util -lmisc -lproperties -lreductions -lflint $(NEWGEN_LIBS) $(LINEAR_LIBS) -lm -lrx'
+PIPS_LIBS='-ltop-level -lpipsmake -lwp65 -lhpfc -ltransformations -lmovements -lbootstrap -lcallgraph -licfg -lchains -lcomplexity -lconversion -lprettyprint -latomizer -lprgm_mapping -lscheduling -lreindexing -larray_dfg -lpaf-util -lstatic_controlize -lsyntax -lregions -lalias-classes -lcomp_sections -lcontrol -lsemantics -lcontinuation -lrice -lricedg -leffects -lpipsdbm -ltransformer -lpip -lri-util -lproperties -ltext-util -lmisc -lproperties -lreductions -lflint $(NEWGEN_LIBS) $(LINEAR_LIBS) -lm -lrx'
 
 #
 # X11
@@ -74,3 +77,8 @@ TPIPS_ADDED_LIBS='-lreadline -ltermcap'
 
 # end of it!
 #
+
+
+
+
+
