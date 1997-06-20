52e901522cde5cb21d6a0d796b37119feb5a12c2
creusil <creusil@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-06-20 14:44:38 +0000

    # on 1997/06/20 14:44:38, creusil did: fill in function bodies for everything except treatment of COMMONs. Nicky.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7011 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/alias-classes/alias_pairs.c
+++ src/Libs/alias-classes/alias_pairs.c
@@ -20,8 +20,191 @@
 #include "pipsdbm.h"
 #include "resources.h"
 
+static entity current_callee;
+static list list_regions_callee;
+static statement current_caller_stmt = statement_undefined;
+static list list_pairs;
+
+static bool stmt_filter(s)
+statement s;
+{
+    pips_debug(1, "statement %03d\n", statement_number(s));
+    
+    current_caller_stmt = s;
+    return(TRUE);
+}
+
+/* static list formal_regions_backward_translation(entity func, list real_args, 
+ *                                   func_regions, transformer context)
+ * input    : an external function func, its real arguments at call site (real_args),
+ *            its summary regions (with formal args), and the calling context. 
+ * output   : the translated formal regions.
+ * modifies : ?
+ * comment  :	
+ *
+ * Algorithm :
+ * -----------
+ *    let list_regions_callee be the list of the regions on variables of
+ *    current_callee
+ *    let real_regions be the list of the translated regions
+ *
+ *    real_regions = empty
+ *    FOR each expression real_exp IN real_args
+ *        arg_num = number in the list of the function real arguments
+ *        FOR each callee_region IN list_regions_callee
+ *            callee_ent = entity of the region callee_region
+ *            IF callee_ent is the formal parameter numbered arg_num
+ *                IF real_exp is an lhs (expression with one entity)
+ *                    real_region = translation of the region callee_region
+ *                    pairs = pairs + <callee_region,real_region>
+ *                ENDIF
+ *            ENDIF
+ *        ENDFOR
+ *    ENDFOR
+ */
+static void
+add_parameter_aliases_for_this_call_site(call call_site, transformer context)
+{
+    list real_regions = NIL, r_args;
+    int arg_num;
+    list real_args;
+
+    pips_debug(8, "begin\n");
+
+    real_args = call_arguments(call_site);
+
+    for (r_args = real_args, arg_num = 1; r_args != NIL;
+	 r_args = CDR(r_args), arg_num++) 
+    {
+	MAP(EFFECT, callee_region,
+	 {
+	     entity callee_ent = region_entity(callee_region);
+	     
+	     /* If the formal parameter corresponds to the real argument then
+	      * we perform the translation.
+	      */
+	     if (ith_parameter_p(current_callee, callee_ent, arg_num))
+	     {
+		 expression real_exp = EXPRESSION(CAR(r_args));
+		 syntax real_syn = expression_syntax(real_exp);
+		 
+		 /* If the real argument is a reference to an entity, then we
+		  * translate the regions of the corresponding formal parameter
+		  */
+		 if (syntax_reference_p(real_syn)) 
+		 {
+		    reference real_ref = syntax_reference(real_syn);
+		    list real_inds = reference_indices(real_ref);
+		    entity real_ent = reference_variable(real_ref);
+		    region real_reg;
+		    list pair;
+
+		    real_reg =
+			region_translation(callee_region, current_callee, reference_undefined,
+				  real_ent, get_current_module_entity(), real_ref,
+				  VALUE_ZERO, BACKWARD);
+		    
+		    pair = CONS(EFFECT,region_dup(callee_region),NIL);
+		    pair = gen_nconc(pair,CONS(EFFECT,real_reg,NIL));
+		    list_pairs = gen_nconc(list_pairs,CONS(LIST,pair,NIL));
+		}
+	     }
+	 }, list_regions_callee);
+    }
+}
+
+
+static bool
+call_site_to_alias_pairs(call call_site)
+{
+    transformer context;
+
+    if (call_function(call_site) != current_callee) return TRUE;
+
+    context = load_statement_precondition(current_stmt);
+
+    set_interprocedural_translation_context_sc(current_callee,real_args);
+    set_backward_arguments_to_eliminate(current_callee);
+
+    add_parameter_aliases_for_this_call_site(call_site,context);
+
+    reset_translation_context_sc();
+    reset_arguments_to_eliminate();
+
+    return TRUE;
+}
+
+static void
+caller_to_alias_pairs( entity caller, entity callee)
+{
+    char *caller_name;
+    statement caller_statement;
+
+    reset_current_module_entity();
+    set_current_module_entity(caller);
+    caller_name = module_local_name(caller);
+    pips_debug(2, "begin for caller: %s\n", caller_name);
+    
+    /* All we need to perform the translation */
+    set_current_module_statement( (statement)
+	db_get_memory_resource(DBR_CODE, caller_name, TRUE) );
+    set_cumulated_rw_effects((statement_effects)
+	db_get_memory_resource(DBR_CUMULATED_EFFECTS, caller_name, TRUE));
+    module_to_value_mappings(caller);
+    set_precondition_map( (statement_mapping) 
+        db_get_memory_resource(DBR_PRECONDITIONS, caller_name, TRUE));
+
+    caller_statement = get_current_module_statement();
+
+    gen_multi_recurse(caller_statement,
+		      statement_domain, stmt_filter, gen_null,
+		      call_domain, call_site_to_alias_pairs, gen_null,
+		      NULL);
+  
+    reset_current_module_entity();
+    set_current_module_entity(callee);    
+}
+
+static bool
+alias_pairs( string module_name, list l_reg )
+{
+
+    callees callers;
+    list l_pairs = NIL;
+
+    set_current_module_entity( local_name_to_top_level_entity(module_name) );
+    module = get_current_module_entity();
+    current_callee = module;
+    list_regions_callee = l_reg;
+
+    /* we need the callers of the current module  */
+    callers = (callees) db_get_memory_resource(DBR_CALLERS,
+					       module_name,
+					       TRUE);
+
+    /* we scan the callers to find the call sites */
+    MAP(STRING, caller_name,
+    {
+	entity caller = local_name_to_top_level_entity(caller_name);
+	list_pairs = NIL;
+	caller_to_alias_pairs(caller, module);
+    },
+	callees_callees(callers));
+
+
+    reset_current_module_entity();
+    return TRUE;
+}
+
 bool 
 in_alias_pairs( string module_name )
 {
-    return TRUE;
+    list l_reg;
+
+    /* we need the IN summary regions*/
+    l_reg = (list) db_get_memory_resource(DBR_IN_SUMMARY_REGIONS,
+					  module_name,
+					  TRUE);
+    return alias_pairs(module_name, l_reg);
 }
+
