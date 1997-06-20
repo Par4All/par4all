2707d996de348bf44a2ee162e4c8605190450dd4
creusil <creusil@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-06-20 08:17:25 +0000

    # on 1997/06/20 08:17:25, creusil did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7006 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/alias-classes/alias_pairs.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#include <setjmp.h>
+
+#include "genC.h"
+#include "ri.h"
+#include "database.h"
+
+#include "ri-util.h"
+#include "constants.h"
+#include "control.h"
+#include "misc.h"
+#include "text.h"
+
+#include "effects.h"
+#include "regions.h"
+
+#include "pipsdbm.h"
+#include "resources.h"
