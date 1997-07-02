d2d0d2bb93dc4963205912473b2f700a6f00746c
williams <williams@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-07-02 15:05:34 +0000

    # on 1997/07/02 15:05:34, williams did: for installation. bc.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7045 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/alias-classes/prettyprint.c
+++ src/Libs/alias-classes/prettyprint.c
@@ -13,6 +13,7 @@
 #include "control.h"
 #include "misc.h"
 #include "text.h"
+#include "text-util.h"
 
 #include "effects.h"
 #include "regions.h"
@@ -20,15 +21,74 @@
 
 #include "pipsdbm.h"
 #include "resources.h"
+#include "prettyprint.h"
 
 #define BACKWARD TRUE
 #define FORWARD FALSE
 
+
+static text alias_pairs_text(string module_name,string resource_name)
+{
+    list l_pairs;
+    entity module;
+    text txt = make_text(NIL);
+
+/*    debug_on("REGIONS_DEBUG_LEVEL"); */
+
+    l_pairs = (list) db_get_memory_resource(resource_name, module_name, TRUE);    
+
+    set_current_module_entity( local_name_to_top_level_entity(module_name));
+    module = get_current_module_entity();
+
+    /* To set up the hash table to translate value into value names */       
+    set_cumulated_rw_effects((statement_effects)
+	  db_get_memory_resource(DBR_CUMULATED_EFFECTS, module_name, TRUE));
+    module_to_value_mappings(module);
+
+    MAP(LIST,pair,
+	{
+	    /* MERGE_TEXTS(txt, text_array_regions(pair)); */
+    ADD_SENTENCE_TO_TEXT(txt,make_sentence(is_sentence_formatted,strdup("\n")));
+	},
+    l_pairs);
+
+/*    debug_off(); */
+
+    reset_current_module_entity();
+    reset_cumulated_rw_effects();
+
+    return txt;
+}
+
+
+static bool
+print_alias_pairs( string module_name, string resource_name, string file_extn )
+{
+    char *file_resource_name;
+    bool success = TRUE;
+
+    file_resource_name = DBR_ALIAS_FILE;
+
+    /*
+    begin_attachment_prettyprint();
+    */  
+    success = make_text_resource(module_name, file_resource_name,
+				 file_extn,
+				 alias_pairs_text(module_name,
+				    resource_name));
+    /*
+    end_attachment_prettyprint();
+    */
+    return(TRUE);
+}
+
+
 bool
 print_in_alias_pairs( string module_name )
 {
-return(TRUE);
+return print_alias_pairs(module_name,DBR_IN_ALIAS_PAIRS,".in_alias");
 }
+
 
 bool
 print_out_alias_pairs( string module_name )
