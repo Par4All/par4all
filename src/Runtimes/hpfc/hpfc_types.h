1d106b0ccf65cf3b60f34997dea8617f30783ba8
zory <zory@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-07-03 11:11:54 +0000

    # on 1997/07/03 11:11:54, zory did: type manipulation is pvm/mpi independent
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7067 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Runtimes/hpfc/hpfc_types.h
+++ src/Runtimes/hpfc/hpfc_types.h
@@ -2,6 +2,9 @@
 ! $Id$
 !
 ! $Log: hpfc_types.h,v $
+! Revision 1.2  1997/07/03 11:11:54  zory
+! type manipulation is pvm/mpi independent
+!
 ! Revision 1.1  1997/06/10 07:55:57  zory
 ! Initial revision
 !
@@ -12,18 +15,24 @@
 !
 
       integer 
+     $     HPFC INTEGER2,
      $     HPFC INTEGER4,
-     $     HPFC INTEGER8,
      $     HPFC REAL4,
      $     HPFC REAL8,
-     $     HPFC CHARACTER
+     $     HPFC STRING,
+     $     HPFC BYTE1,
+     $     HPFC COMPLEX8,
+     $     HPFC COMPLEX16
 
 
-      parameter(HPFC INTEGER = 1) 
+      parameter(HPFC INTEGER2 = 1) 
       parameter(HPFC INTEGER4 = 2)
       parameter(HPFC REAL4 = 3)
       parameter(HPFC REAL8 = 4)
-      parameter(HPFC CHARACTER = 5)
+      parameter(HPFC STRING = 5)
+      parameter(HPFC BYTE1 = 6)
+      parameter(HPFC COMPLEX8 = 7)
+      parameter(HPFC COMPLEX16 = 8)
 
 
 !
