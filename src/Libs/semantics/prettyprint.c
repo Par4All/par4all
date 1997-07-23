0c19069a2e407b1dfb4b0f17422722b53604dbd8
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-07-23 15:02:19 +0000

    # on 1997/07/23 15:02:19, keryell did: syst_debug(s) changed to sc_syst_debug(s) to avoid conflict with bison output of sc+gram.y... :-(
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7248 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/semantics/prettyprint.c
+++ src/Libs/semantics/prettyprint.c
@@ -1,7 +1,7 @@
-/* 	%A% ($Date: 1997/07/21 12:26:22 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.	 */
+/* 	%A% ($Date: 1997/07/23 15:02:19 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.	 */
 
 #ifndef lint
-char vcid_semantics_prettyprint[] = "%A% ($Date: 1997/07/21 12:26:22 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.";
+char vcid_semantics_prettyprint[] = "%A% ($Date: 1997/07/23 15:02:19 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.";
 #endif /* lint */
 
  /* package semantics - prettyprint interface */
@@ -511,7 +511,7 @@ text text_transformer(transformer tran)
 	  
 	  ifdebug(7) {
 	      pips_debug(7, "sys %p\n", ps);
-	      syst_debug(ps);
+	      sc_syst_debug(ps);
 	  }
 	  
 	  if (ps != NULL) {
