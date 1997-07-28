9443fcc4b17943079bf7b96c2aee07d44663ef81
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-07-28 17:32:30 +0000

    # on 1997/07/28 17:32:30, coelho did: new effects-* libraries added.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7355 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/dev/define_libraries.sh
+++ src/Scripts/dev/define_libraries.sh
@@ -2,6 +2,9 @@
 # $Id$
 #
 # $Log: define_libraries.sh,v $
+# Revision 1.15  1997/07/28 17:32:30  coelho
+# new effects-* libraries added.
+#
 # Revision 1.14  1997/06/20 08:45:32  creusil
 # added library alias-classes. Nicky.
 #
@@ -49,7 +52,7 @@ LINEAR_LIBS='-lmatrice -lunion -lpolyedre -lsparse_sc -lsc -lcontrainte -lsg -ls
 # many doc dirs are skipped (would be long?)
 PIPS_DOCS='Documentation constants help manl newgen'
 
-PIPS_ORDERED_LIBS='xpomp text-util properties misc ri-util conversion movements pipsdbm comp_sections transformer bootstrap control hyperplane flint syntax prettyprint static_controlize effects semantics complexity continuation reductions regions alias-classes callgraph icfg paf-util pip ricedg array_dfg prgm_mapping scheduling reindexing chains rice transformations hpfc atomizer wp65 pipsmake top-level pips tpips wpips'
+PIPS_ORDERED_LIBS='xpomp text-util properties misc ri-util conversion movements pipsdbm comp_sections transformer bootstrap control hyperplane flint syntax prettyprint static_controlize effects effects-generic effects-simple semantics complexity continuation reductions regions effects-convex alias-classes callgraph icfg paf-util pip ricedg array_dfg prgm_mapping scheduling reindexing chains rice transformations hpfc atomizer wp65 pipsmake top-level pips tpips wpips'
 
 # all libraires for pips
 PIPS_LIBS='-ltop-level -lpipsmake -lwp65 -lhpfc -ltransformations -lmovements -lbootstrap -lcallgraph -licfg -lchains -lcomplexity -lconversion -lprettyprint -latomizer -lprgm_mapping -lscheduling -lreindexing -larray_dfg -lpaf-util -lstatic_controlize -lsyntax -lregions -lalias-classes -lcomp_sections -lcontrol -lsemantics -lcontinuation -lrice -lricedg -leffects -lpipsdbm -ltransformer -lpip -lri-util -lproperties -ltext-util -lmisc -lproperties -lreductions -lflint $(NEWGEN_LIBS) $(LINEAR_LIBS) -lm -lrx'
