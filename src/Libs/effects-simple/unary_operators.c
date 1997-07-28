1ac8cac57b1e8f2a88dad236af95e7ff4a344901
creusil <creusil@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-07-28 14:01:04 +0000

    # on 1997/07/28 14:01:04, creusil did: bye...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7322 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/effects-simple/unary_operators.c
+++ src/Libs/effects-simple/unary_operators.c
@@ -19,19 +19,57 @@
 #include "effects-simple.h"
 
 
+/*********************************************************************************/
+/* REFERENCE EFFECTS                                                             */
+/*********************************************************************************/
+
+effect
+reference_effect_dup(effect eff)
+{
+    return (copy_effect(eff));
+}
+
+void
+reference_effect_free(effect eff)
+{
+    free_effect(eff);
+}
+
 effect
 reference_to_simple_effect(reference ref, action ac)
 {
 /* It shoulb be this one, but it does not work. Maybe there is a clash with
-   old uses of make_simple_effects.
+   old uses of make_simple_effects. Or persistancy is not properly handled? - bc.
    */
 /*    cell cell_ref = make_cell(is_cell_reference, copy_reference(ref)); */
-    cell cell_ref = make_cell(is_cell_preference, make_preference(ref));
+    cell cell_ref = make_cell(is_cell_preference, 
+			      make_preference(reference_dup(ref)));
     approximation ap = make_approximation(is_approximation_must, UU);
     effect eff;
 
     eff = make_effect(cell_ref, ac, ap, make_descriptor(is_descriptor_none,UU));  
     return(eff);
+}
+
+/*********************************************************************************/
+/* SIMPLE EFFECTS                                                                */
+/*********************************************************************************/
+
+effect
+simple_effect_dup(effect eff)
+{
+    effect new_eff;
+    new_eff = copy_effect(eff);
+    effect_reference(new_eff) = reference_dup(effect_reference(new_eff));
+    return(new_eff);
+}
+
+void
+simple_effect_free(effect eff)
+{
+    free_reference(effect_reference(eff));
+    effect_reference(eff) = reference_undefined;
+    free_effect(eff);
 }
 
 effect
@@ -44,6 +82,7 @@ reference_to_reference_effect(reference ref, action ac)
     eff = make_effect(cell_ref, ac, ap, make_descriptor(is_descriptor_none,UU));  
     return(eff);
 }
+
 
 list 
 simple_effects_union_over_range(list l_eff, entity i, range r, descriptor d)
@@ -76,4 +115,10 @@ effect_to_sdfi_list(effect eff)
 	effect_approximation_tag(eff) = is_approximation_may;
     }
     return(CONS(EFFECT,eff,NIL));
+}
+
+void
+simple_effects_descriptor_normalize(list l_eff)
+{
+    return;
 }
