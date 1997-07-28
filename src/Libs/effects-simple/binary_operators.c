5fc5c0c17ce9eb4ff9e107a0471e7ad46740dfc5
creusil <creusil@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-07-28 14:01:17 +0000

    # on 1997/07/28 14:01:17, creusil did: bye
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7323 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/effects-simple/binary_operators.c
+++ src/Libs/effects-simple/binary_operators.c
@@ -24,10 +24,31 @@
 #include "transformer.h"
 #include "semantics.h"
 
-#include "effects.h"
+#include "effects-generic.h"
+#include "effects-simple.h"
 
 #include "pipsdbm.h"
 #include "resources.h"
+
+
+list
+ReferenceUnion(list l1, list l2,
+		     boolean (*union_combinable_p)(effect, effect))
+{
+    return gen_nconc(l1,l2);
+}
+
+list
+ReferenceTestUnion(list l1, list l2,
+		     boolean (*union_combinable_p)(effect, effect))
+{
+    list l_res;
+
+    /* somthing more clever should be done here - bc. */
+    l_res = gen_nconc(l1,l2);
+    effects_to_may_effects(l_res);
+    return l_res;
+}
 
 
 
@@ -37,7 +58,8 @@
  * modifies : l1 and l2 and their effects. Effects that are not reused in
  *            the output list of effects are freed.nothing (no sharing introduced).
  */
-list EffectsMayUnion(list l1, list l2,
+list 
+EffectsMayUnion(list l1, list l2,
 		     boolean (*union_combinable_p)(effect, effect))
 {
     list lr;
@@ -57,7 +79,8 @@ list EffectsMayUnion(list l1, list l2,
  * modifies : l1 and l2 and their effects. Effects that are not reused in
  *            the output list of effects are freed.
  */
-list EffectsMustUnion(list l1, list l2,
+list 
+EffectsMustUnion(list l1, list l2,
 		      boolean (*union_combinable_p)(effect, effect)) 
 {
     list lr;
@@ -70,21 +93,24 @@ list EffectsMustUnion(list l1, list l2,
     return(lr);
 }
 
-list effects_may_union(effect eff1, effect eff2)
+list 
+effects_may_union(effect eff1, effect eff2)
 {
     list l_res = NIL;
     l_res = CONS(EFFECT, effect_may_union(eff1,eff2), NIL);
     return(l_res);
 }
 
-list effects_must_union(effect eff1, effect eff2)
+list 
+effects_must_union(effect eff1, effect eff2)
 {
     list l_res = NIL;
     l_res = CONS(EFFECT, effect_must_union(eff1,eff2), NIL);
     return(l_res);
 }
 
-effect effect_may_union(effect eff1, effect eff2)
+effect 
+effect_may_union(effect eff1, effect eff2)
 {
     effect eff;
     
@@ -106,7 +132,8 @@ effect effect_may_union(effect eff1, effect eff2)
     return(eff);
 }
 
-effect effect_must_union(effect eff1, effect eff2)
+effect 
+effect_must_union(effect eff1, effect eff2)
 {
     effect eff;
     if (effect_scalar_p(eff1))
@@ -128,29 +155,82 @@ effect effect_must_union(effect eff1, effect eff2)
 }
 
 
-
-/*********************************************************************************/
-/* PROPER EFFECTS TO SUMMARY EFFECTS                                             */
-/*********************************************************************************/
-
-/* list proper_to_summary_effects(list l_effects)
- * input    : a list of proper effects: there may be several effects for 
- *            a given array with a given action.
- * output   : a list of summary effects, with the property that is only
- *            one effect for each array per action.
- * modifies : the input list. Some effects are freed. 
- * comment  : The computation scans the first list (the "base" list);
- *            From this element, the next elements are scanned (the "current"
- *            list). If the current effect is combinable with the base effect,
- *            they are combined. Both original effects are freed, and the base
- *            is replaced by the union. 
- */
-list proper_to_summary_effects(list l_effects)
+static list
+effect_sup_difference(effect eff1, effect eff2)
 {
-        return(proper_effects_combine(l_effects, FALSE));
+    list l_res = NIL;
+    if (effect_must_p(eff2))
+	l_res = NIL;
+    else
+	l_res = effect_to_may_effect_list(eff1);
+    return(l_res);
 }
 
-effect proper_to_summary_effect(effect eff)
+static list
+effect_inf_difference(effect eff1, effect eff2)
+{
+    list l_res = NIL;
+    return(l_res);
+}
+
+
+
+/* list EffectsSupDifference(list l1, l2)
+ * input    : two lists of effects
+ * output   : a list of effect, representing the sup_difference of the 
+ *            initial effects.
+ * modifies : the effects of l2 may be freed.
+ * comment  : we keep the effects of l1 that are not combinable with those
+ *            of l2, but we don't keep the effects of l2 that are not 
+ *            combinable with those of l_reg1.	
+ */
+list 
+EffectsSupDifference(list l1, list l2,
+			  boolean (*difference_combinable_p)(effect, effect))
+{
+    list l_res = NIL;
+
+    debug(3, "EffectsSupDifference", "begin\n");
+    l_res = list_of_effects_generic_binary_op(l1, l2,
+					   difference_combinable_p,
+					   effect_sup_difference,
+					   effect_to_list,
+					   effect_to_nil_list);
+    debug(3, "EffectsSupDifference", "end\n");
+
+    return l_res;
+}
+
+/* list EffectsInfDifference(list l1, l2)
+ * input    : two lists of effects
+ * output   : a list of effect, representing the inf_difference of the 
+ *            initial effects.
+ * modifies : the effects of l2 may be freed.
+ * comment  : we keep the effects of l1 that are not combinable with those
+ *            of l2, but we don't keep the effects of l2 that are not 
+ *            combinable with those of l_reg1.	
+ */
+list 
+EffectsInfDifference(list l1, list l2,
+			  boolean (*difference_combinable_p)(effect, effect))
+{
+    list l_res = NIL;
+
+    debug(3, "EffectsInfDifference", "begin\n");
+    l_res = list_of_effects_generic_binary_op(l1, l2,
+					   difference_combinable_p,
+					   effect_inf_difference,
+					   effect_to_list,
+					   effect_to_nil_list);
+    debug(3, "EffectsInfDifference", "end\n");
+
+    return l_res;
+}
+
+
+
+effect 
+proper_to_summary_simple_effect(effect eff)
 {
     if (!effect_scalar_p(eff))
     {
@@ -158,93 +238,4 @@ effect proper_to_summary_effect(effect eff)
 	effect_approximation_tag(eff) = is_approximation_may;
     }
     return(eff);
-}
-
-/* list proper_effects_contract(list l_effects)
- * input    : a list of proper effects
- * output   : a list of proper effects in which there is no two identical 
- *            scalar effects. 
- * modifies : the input list. 
- * comment  : This is used to reduce the number of dependence tests.
- */
-
-list proper_effects_contract(list l_effects)
-{
-    return(proper_effects_combine(l_effects, TRUE));
-}
-
-
-/* list proper_effects_combine(list l_effects, bool scalars_only_p)
- * input    : a list of proper effects, and a boolean to know on which
- *            elements to perform the union.
- * output   : a list of effects, in which the selected elements have been merged.
- * modifies : the input list.
- * comment  :
- */
-list proper_effects_combine(list l_effects, bool scalars_only_p)
-{
-    list base, current, pred;
-
-    ifdebug(6)
-    {
-	pips_debug(6, "proper effects: \n");
-	print_effects(l_effects);	
-    }
-
-    base = l_effects;
-    /* scan the list of effects */
-    while(!ENDP(base) )
-    {	
-	/* scan the next elements to find effects combinable
-	 * with the effects of the base element.
-	 */
-	current = CDR(base);
-	pred = base;
-	while (!ENDP(current))
-	{
-	    effect eff_base = EFFECT(CAR(base));
-	    effect eff_current = EFFECT(CAR(current));
-	    
-
-	    /* Both effects are about the same scalar variable, 
-	       with the same action 
-	       */ 
-	    if ((!scalars_only_p || effect_scalar_p(eff_base)) &&
-		effects_same_action_p(eff_base, eff_current) )  
-	    {
-		list tmp;
-		effect new_eff_base;
-				
-		/* compute their union */
-		new_eff_base = effect_must_union
-		    (proper_to_summary_effect(eff_base),
-		     proper_to_summary_effect(eff_current)); 
-		/* free the original effects: no memory leak */
-		free_effect(eff_base);
-		free_effect(eff_current);
-		
-		/* replace the base effect by the new effect */
-		EFFECT(CAR(base)) = new_eff_base;
-		
-		/* remove the current list element from the global list */
-		tmp = current;	    
-		current = CDR(current);
-		CDR(pred) = current;
-		free(tmp);	    
-	    }
-	    else
-	    {
-		pred = current;
-		current = CDR(current);
-	    }
-	}
-	base = CDR(base);
-    }
-
-    ifdebug(6)
-    {
-	pips_debug(6, "summary effects: \n");
-	print_effects(l_effects);	
-    }
-    return(l_effects);    
 }
