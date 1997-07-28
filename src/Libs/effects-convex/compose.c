e8d34a2ee0040d17e893f37c331db93e3aa1fb9a
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-07-28 14:44:04 +0000

    # on 1997/07/28 14:44:04, coelho did: hop.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7336 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/effects-convex/compose.c
@@ -0,0 +1,58 @@
+/* package convex effects :  Be'atrice Creusillet 6/97
+ *
+ * $Id$
+ *
+ * File: compose.c
+ * ~~~~~~~~~~~~~~~
+ *
+ * This File contains the intanciation of the generic functions necessary 
+ * for the composition of convex effects with transformers or preconditions
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "genC.h"
+
+#include "effects-generic.h"
+#include "effects-convex.h"
+
+list
+convex_regions_transformer_compose(list l_reg, transformer trans)
+{
+    project_regions_with_transformer_inverse(l_reg, trans, NIL);
+    return l_reg;
+}
+
+list
+convex_regions_inverse_transformer_compose(list l_reg, transformer trans)
+{
+    project_regions_with_transformer(l_reg, trans, NIL);
+    return l_reg;
+}
+
+list 
+convex_regions_precondition_compose(list l_reg, transformer context)
+{
+    list l_res = NIL;
+    Psysteme sc_context = predicate_system(transformer_relation(context));
+    
+    ifdebug(8)
+	{
+	    pips_debug(8, "context: \n");
+	    sc_syst_debug(sc_context);
+	}
+	
+    MAP(EFFECT, reg,
+	{
+	    if (! effect_scalar_p(reg) )
+		region_sc_append(reg, sc_context, FALSE);
+
+	    if (!region_empty_p(reg))
+		l_res = CONS(EFFECT, reg, l_res);    
+	},
+	l_reg);
+
+    return l_res;
+}
