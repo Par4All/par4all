b91e77eabbed3f09a9cba05d73a537d931ea33e5
creusil <creusil@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-07-28 15:31:39 +0000

    # on 1997/07/28 15:31:39, creusil did: BC -> FC
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7340 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/effects-generic/interprocedural.c
@@ -0,0 +1,10 @@
+/* package generic effects :  Be'atrice Creusillet 5/97
+ *
+ * File: interprocedural.c
+ * ~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * This File contains the generic functions necessary for the interprocedural 
+ * computation of all types of in effects.
+ *
+ */
+
