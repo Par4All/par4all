1d1f20c078e01594dd6e50c0cc386726a8f85c7e
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-07-29 17:57:55 +0000

    # on 1997/07/29 17:57:55, coelho did: test for undefined context first...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7374 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/effects-convex/utils.c
+++ src/Libs/effects-convex/utils.c
@@ -19,6 +19,23 @@
 bool 
 empty_convex_context_p(transformer context)
 {
-    Psysteme sc_context = predicate_system(transformer_relation(context));
+    Psysteme sc_context;
+
+    if (transformer_undefined_p(context))
+
+	/* this happens to the CONTINUE statement of the exit node
+	 * even if unreachable. Thus transformer are not computed,
+	 * orderings are not set... however gen_multi_recurse goes there.
+	 * I just store NIL, what seems reasonnable an answer.
+	 * It seems to be sufficient for other passes. 
+	 * I should check that it is indeed the exit node?
+	 * FC.
+	 */
+	return TRUE;
+
+    /* else 
+     */
+
+    sc_context = predicate_system(transformer_relation(context));
     return sc_empty_p(sc_context);
 }
