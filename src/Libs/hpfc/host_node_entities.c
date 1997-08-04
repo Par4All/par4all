c8c7b92d9a60f624a0345fa50afa7bf7ee4ae882
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-04 13:55:09 +0000

    # on 1997/08/04 13:55:09, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7475 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/hpfc/host_node_entities.c
+++ src/Libs/hpfc/host_node_entities.c
@@ -2,6 +2,9 @@
  *
  * $Id$
  * $Log: host_node_entities.c,v $
+ * Revision 1.25  1997/08/04 13:55:09  coelho
+ * *** empty log message ***
+ *
  * Revision 1.24  1997/07/21 14:00:58  keryell
  * Replaced %x format by %p.
  *
@@ -16,9 +19,10 @@
 #include "defines-local.h"
 
 #include "control.h"
-#include "regions.h"
 #include "semantics.h"
-#include "effects.h"
+#include "effects-generic.h"
+#include "effects-simple.h"
+#include "effects-convex.h"
 
 /*      HOST AND NODE ENTITIES MANAGEMENT
  */
