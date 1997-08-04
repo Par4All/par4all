a54fcb8699890d8f55bc63b436490fd76cca011d
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-04 09:57:30 +0000

    # on 1997/08/04 09:57:30, coelho did: generic prettyprint engine used.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7441 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformations/array_privatization.c
+++ src/Libs/transformations/array_privatization.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <string.h>
+
 #include "genC.h"
 #include "ri.h"
 #include "database.h"
@@ -230,7 +231,8 @@ statement module_stat;
     l_cand = RegionsInfDifference(l_write, l_in, w_r_combinable_p);
 
     pips_debug(2, "OUT_CAND = CAND inter OUT\n");
-    l_out_cand = RegionsIntersection(regions_dup(l_cand), l_out, w_w_combinable_p);
+    l_out_cand = 
+	RegionsIntersection(regions_dup(l_cand), l_out, w_w_combinable_p);
 
     pips_debug(2, "PRIV = CAND -inf OUT_CAND\n");
     l_priv = RegionsInfDifference(l_cand, l_out_cand, w_w_combinable_p);
@@ -263,10 +265,11 @@ static void private_regions_of_module_loops(statement module_stat)
 
     pips_debug(1,"begin\n");
     
-    gen_multi_recurse(module_stat, 
-		      statement_domain, stmt_filter, private_regions_of_statement,
-		      loop_domain, private_regions_of_loop, gen_null,
-		      NULL); 
+    gen_multi_recurse(
+	module_stat, 
+	statement_domain, stmt_filter, private_regions_of_statement,
+	loop_domain, private_regions_of_loop, gen_null,
+	NULL); 
     
 
     pips_debug(1,"end\n");
@@ -323,8 +326,10 @@ loop l;
 
 
     /* first get the write, in and out regions, invariant if available */
-    l_write = regions_dup(regions_write_regions(load_statement_inv_regions(b)));
-    l_in = regions_to_write_regions(regions_dup(load_statement_inv_in_regions(b)));
+    l_write = 
+	regions_dup(regions_write_regions(load_statement_inv_regions(b)));
+    l_in = 
+      regions_to_write_regions(regions_dup(load_statement_inv_in_regions(b)));
     l_out = regions_dup(load_statement_out_regions(b));
 
     project_regions_with_transformer_inverse(l_out, 
@@ -370,7 +375,8 @@ loop l;
     if (get_bool_property("ARRAY_PRIV_FALSE_DEP_ONLY"))
     {
 	/* Keep only arrays that induce false dependences between iterations   
-	 * that is to say arrays such that LOC(i) inter LOC(i', i'<i) != empty_set 
+	 * that is to say arrays such that 
+	 * LOC(i) inter LOC(i', i'<i) != empty_set 
 	 */
 	sc_loop_prec = sc_loop_proper_precondition(l);
 	
@@ -385,22 +391,24 @@ loop l;
 	sc_add_inegalite(sc_loop_prec, contrainte_dup(contrainte));
 	sc_loop_prec->base = BASE_NULLE;
 	sc_creer_base(sc_loop_prec);
-	l_loc_i_prime = array_regions_sc_append(l_loc_i_prime, sc_loop_prec, FALSE);
+	l_loc_i_prime = 
+	    array_regions_sc_append(l_loc_i_prime, sc_loop_prec, FALSE);
 	l_loc_i = array_regions_sc_append(l_loc_i, sc_loop_prec, FALSE);
 	sc_rm(sc_loop_prec);
 	
 	/* LOC_I(i) = LOC(i, i'<i) inter LOC(i', i'<i) */
-	l_loc_i = RegionsIntersection(l_loc_i, l_loc_i_prime, w_w_combinable_p);
+	l_loc_i = 
+	    RegionsIntersection(l_loc_i, l_loc_i_prime, w_w_combinable_p);
 	
-	/* We keep in Loc(i) only the regions that correspond to arrays in l_loc_i,
-	 * that is to say arrays that induce false dependences
+	/* We keep in Loc(i) only the regions that correspond to arrays in
+	 * l_loc_i, that is to say arrays that induce false dependences
 	 */
 	l_loc = RegionsEntitiesIntersection(l_loc, l_loc_i, w_w_combinable_p);
 
 	ifdebug(3)
 	{
-	    pips_debug(3,
-		       "regions on arrays that really induce false dependences:\n");
+	    pips_debug(3, "regions on arrays that really induce "
+		       "false dependences:\n");
 	    print_regions(l_loc);
 	}
     }
@@ -446,8 +454,8 @@ loop l;
 	    print_regions(l_out_priv);
 	}
 
-	/* keep only good candidates (those for which the copy-out is either empty
-	 * or exactly determined). */
+	/* keep only good candidates (those for which the copy-out is either
+	 * empty or exactly determined). */
 	
 	l_out_priv_tmp = l_out_priv;
 	while(!ENDP(l_out_priv_tmp))
@@ -466,7 +474,8 @@ loop l;
 		{
 		    region reg_cand = EFFECT(CAR(l_cand_tmp));
 		    l_cand_tmp = CDR(l_cand_tmp);
-		    if (same_entity_p(region_entity(reg_cand) , region_entity(reg)))
+		    if (same_entity_p(region_entity(reg_cand),
+				      region_entity(reg)))
 		    {
 			/* a` optimiser */
 			gen_remove(&l_cand, reg_cand);
@@ -516,24 +525,25 @@ loop l;
 	    ccommon = ram_section(storage_ram(entity_storage(reg_ent)));
 	    l_com_ent = area_layout(type_area(entity_type(ccommon)));
 	    
-	    for( l_tmp = l_com_ent; !ENDP(l_tmp) && !found; l_tmp = CDR(l_tmp) )
+	    for(l_tmp = l_com_ent; !ENDP(l_tmp) && !found; l_tmp = CDR(l_tmp))
 	    {
 		entity com_ent = ENTITY(CAR(l_tmp));
 		if (strcmp(entity_module_name(com_ent),
-			   module_local_name(get_current_module_entity())) == 0)
+			   module_local_name(get_current_module_entity()))==0)
 		{
 		    found = TRUE;
 		}
 	    }
 	    if (!found)	    
 	    {
-		pips_user_warning("\nCOMMON %s not declared in subroutine %s:\n\t"
-				  "variable %s cannot be privatized in loop "
-				  "number %03d\n",
-				  entity_name(ccommon),
-				  module_local_name(get_current_module_entity()),
-				  entity_name(reg_ent),
-			      statement_number(current_stmt_head()));
+		pips_user_warning
+		    ("\nCOMMON %s not declared in subroutine %s:\n\t"
+		     "variable %s cannot be privatized in loop "
+		     "number %03d\n",
+		     entity_name(ccommon),
+		     module_local_name(get_current_module_entity()),
+		     entity_name(reg_ent),
+		     statement_number(current_stmt_head()));
 		gen_remove(&l_cand, reg);
 		
 	    }
@@ -550,7 +560,7 @@ loop l;
 	l_locals = NIL;
 	MAP(EFFECT, reg,
 	    {		
-		l_locals = CONS(ENTITY, region_entity(reg), l_locals);		
+		l_locals = CONS(ENTITY, region_entity(reg), l_locals);
 	    },
 	    l_cand);
 	loop_locals(l) = l_locals;   
@@ -588,228 +598,50 @@ loop l;
 }
 
 
-/************************* PRETTYPRINT OF PRIVATIZED REGIONS (AND COPY-OUT) */
-static boolean is_user_view_p = FALSE;
-static hash_table nts = hash_table_undefined;
-static text text_statement_privatized_array_regions(entity module,
-						    int margin,
-						    statement stat);
-static text get_privatized_regions_text(string module_name,
-					bool give_code_p);
-static text text_privatized_array_regions(list l_priv, list l_out);
+/*************************** PRETTYPRINT OF PRIVATIZED AND COPY-OUT REGIONS */
 
-/* bool print_code_privatized_sections(string module_name, list summ_regions)
- * input    : the name of the current module, the name of the region and
- *            summary region resources and the file suffix
- *            the regions are in the global variable local_regions_map.
- * modifies : nothing.
- * comment  : prints the source code with the corresponding privatized and
- *            copy-out regions.	
- */
-bool print_code_privatized_regions(string module_name)
+bool
+print_code_privatized_regions(string module_name)
 {
-    bool blocks_tmp, priv_tmp, success;
-    string file_name, file_resource_name;
+    bool ok, blks_tmp, priv_tmp, scal_tmp;
 
-    blocks_tmp = get_bool_property("PRETTYPRINT_BLOCKS");
+    /* PUSH properties (should be done from the script???)
+     */
+    blks_tmp = get_bool_property("PRETTYPRINT_BLOCKS");
     priv_tmp = get_bool_property("PRETTYPRINT_ALL_PRIVATE_VARIABLES");
+    scal_tmp = get_bool_property("PRETTYPRINT_SCALAR_REGIONS");
 
-    file_name = strdup(concatenate
-	       (get_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH") ? 
-		GRAPH_FILE_EXT : "", NULL));
-
-    file_resource_name = 
-	get_bool_property("PRETTYPRINT_UNSTRUCTURED_AS_A_GRAPH") ?
-	DBR_GRAPH_PRINTED_FILE : 
-	(is_user_view_p ? DBR_PARSED_PRINTED_FILE : DBR_PRINTED_FILE);
-    
     set_bool_property("PRETTYPRINT_BLOCKS", TRUE);
     set_bool_property("PRETTYPRINT_ALL_PRIVATE_VARIABLES", TRUE);
+    set_bool_property("PRETTYPRINT_SCALAR_REGIONS", TRUE);
 
-    /* set_action_interpretation(ACTION_IN, ACTION_OUT); */
+    /* DO IT!
+     */
+    add_a_generic_prettyprint(DBR_COPY_OUT_REGIONS, FALSE,
+			      text_copyinout_array_regions,
+			      print_copyinout_regions,
+			      (generic_attachment_function) abort);
 
-    success = make_text_resource
-	(module_name, file_resource_name, file_name,
-	 get_privatized_regions_text(module_name, TRUE));
+    add_a_generic_prettyprint(DBR_PRIVATIZED_REGIONS, FALSE,
+			      text_private_array_regions,
+			      print_private_regions,
+			      (generic_attachment_function) abort);
 
-    set_bool_property("PRETTYPRINT_BLOCKS", blocks_tmp);
+    ok = print_source_or_code_effects_engine(module_name, ".privreg");
+
+    reset_generic_prettyprints();
+
+    /* POP properties
+     */
+    set_bool_property("PRETTYPRINT_BLOCKS", blks_tmp);
     set_bool_property("PRETTYPRINT_ALL_PRIVATE_VARIABLES", priv_tmp);
+    set_bool_property("PRETTYPRINT_SCALAR_REGIONS", scal_tmp);
 
-    free(file_name);
-
-    return success;
-}
-
-static text get_privatized_regions_text(string module_name,
-					bool give_code_p)
-{
-    entity module;
-    statement module_stat, user_stat = statement_undefined;
-    text txt = make_text(NIL);
-
-    debug_on("ARRAY_PRIVATIZATION_DEBUG_LEVEL");
-
-    set_private_effects((statement_effects) 
-	db_get_memory_resource(DBR_PRIVATIZED_REGIONS, module_name, TRUE));
-
-    set_copy_out_effects( (statement_effects) 
-	db_get_memory_resource(DBR_COPY_OUT_REGIONS, module_name, TRUE) );
-
-
-    set_current_module_entity( local_name_to_top_level_entity(module_name));
-    module = get_current_module_entity();
-
-    set_current_module_statement((statement) db_get_memory_resource
-				 (DBR_CODE, module_name, TRUE));
-
-    module_stat = get_current_module_statement();
-
-    /* To set up the hash table to translate value into value names */       
-    set_cumulated_rw_effects((statement_effects)
-	  db_get_memory_resource(DBR_CUMULATED_EFFECTS, module_name, TRUE));
-    module_to_value_mappings(module);
-
-
-    if(is_user_view_p) 
-    {
-	user_stat =  (statement)
-	    db_get_memory_resource(DBR_PARSED_CODE, module_name, TRUE);
-
-	nts = allocate_number_to_statement();
-	nts = build_number_to_statement(nts, module_stat);
-
-	ifdebug(1)
-	{
-	    print_number_to_statement(nts);
-	}
-    }
-
-    /* prepare the prettyprinting */
-    init_prettyprint(text_statement_privatized_array_regions);
-    set_in_out_regions_p_variable(TRUE);
-
-    if (give_code_p)
-	/* then code with regions, using text_array_regions */
-	MERGE_TEXTS(txt, text_module(module,
-				     is_user_view_p? user_stat : module_stat));
-
-    if(is_user_view_p)
-    {
-	hash_table_free(nts);
-	nts = hash_table_undefined;
-    }
-
-    close_prettyprint();
-
-    debug_off();
-
-    reset_private_effects();
-    reset_copy_out_effects();
-    reset_current_module_entity();
-    reset_current_module_statement();
-    reset_cumulated_rw_effects();
-
-    return txt;
+    return ok;
 }
 
 
-/* static text text_statement_privatized_array_regions
- *               (entity module, int margin, statement stat)
- * output   : a text representing the list of array regions associated with the
- *            statement stat.
- * comment  : if the number of array regions is not nul, then empty lines are
- *            added before and after the text of the list of regions.
- */
-static text text_statement_privatized_array_regions(module, margin, stat)
-entity module;
-int margin;
-statement stat;
-{
-    list l_priv = NIL, l_out = NIL;
-    statement s;
-
-    s = is_user_view_p? 
-	(statement) hash_get(nts, (char *) statement_number(stat)) :
-	stat;
-    
-    if (s != (statement) HASH_UNDEFINED_VALUE)
-    {
-	l_priv = effects_effects(load_private_effects(s));
-	l_out = effects_effects(load_copy_out_effects(s));
-    }
-    else
-    {
-	l_priv = (list) HASH_UNDEFINED_VALUE;
-	l_out = (list) HASH_UNDEFINED_VALUE;
-    }
-    
-    return text_privatized_array_regions(l_priv, l_out);
-}
-
-
-/* static text text_privatized_array_regions(list l_priv, list l_out)
- * input    : a list of regions
- * output   : a text representing this list of regions.
- * comment  : if the number of array regions is not nul, and if 
- *            PRETTYPRINT_LOOSE is TRUE, then empty lines are
- *            added before and after the text of the list of regions.
- */
-static text text_privatized_array_regions(list l_priv, list l_out)
-{
-#define PIPS_NORMAL_PREFIX "C"
-
-    text reg_text = make_text(NIL);
-    /* in case of loose_prettyprint, at least one region to print? */
-    boolean loose_p = get_bool_property("PRETTYPRINT_LOOSE");
-
-    /* First: private sections */
-    if (l_priv !=(list) HASH_UNDEFINED_VALUE &&  !ENDP(l_priv))
-    {
-	if (loose_p)
-	{
-	    ADD_SENTENCE_TO_TEXT(reg_text, 
-				 make_sentence(is_sentence_formatted, 
-					       strdup("\n")));
-	}
-	ADD_SENTENCE_TO_TEXT(reg_text, 
-			     make_pred_commentary_sentence
-			     (strdup("PRIVATE REGIONS:"),PIPS_NORMAL_PREFIX));
-	MERGE_TEXTS(reg_text,text_all_regions(l_priv));
-
-	if (loose_p)
-	    ADD_SENTENCE_TO_TEXT(reg_text, 
-				 make_sentence(is_sentence_formatted, 
-					       strdup("\n")));
-    }
-
-    /* Then: copy-out sections */
-    if (l_out !=(list) HASH_UNDEFINED_VALUE &&  !ENDP(l_out))
-    {
-	if (loose_p)
-	{
-	    ADD_SENTENCE_TO_TEXT(reg_text, 
-				 make_sentence(is_sentence_formatted, 
-					       strdup("\n")));
-	}
-	ADD_SENTENCE_TO_TEXT(reg_text, 
-			     make_pred_commentary_sentence
-			     (strdup("COPY-OUT REGIONS:"),PIPS_NORMAL_PREFIX));
-	MERGE_TEXTS(reg_text,text_all_regions(l_out));
-
-	if (loose_p)
-	    ADD_SENTENCE_TO_TEXT(reg_text, 
-				 make_sentence(is_sentence_formatted, 
-					       strdup("\n")));
-    }
-
-    return(reg_text);
-}
-
-/*********************************************************************************/
-/*********************************************************************************/
-/* 2-  MODULE PRIVATISATION (VARIABLE REDECLARATION)                             */
-/*********************************************************************************/
-/*********************************************************************************/
+/************************ 2-  MODULE PRIVATISATION (VARIABLE REDECLARATION) */
 
 static entity dynamic_area = entity_undefined;
 
@@ -884,19 +716,20 @@ privatize_entity(entity ent)
 			      copy_value(entity_initial(ent)));
 			      
 	
-	new_ent_storage = make_storage(is_storage_ram,
-				       make_ram(module,
-						dynamic_area,
-						CurrentOffsetOfArea(dynamic_area,
-								    new_ent),
-						NIL));
+	new_ent_storage = make_storage
+	    (is_storage_ram,
+	     make_ram(module,
+		      dynamic_area,
+		      CurrentOffsetOfArea(dynamic_area,
+					  new_ent),
+		      NIL));
 	entity_storage(new_ent) = new_ent_storage;
 
 	/* add this entity to the declarations of the module */
 	AddEntityToDeclarations(new_ent, module);
 	
-	/* replace all references to this entity by a reference to the new entity 
-	 */
+	/* replace all references to this entity by a reference to the
+	 * new entity */
 	set_current_entities(ent, new_ent);
 	gen_multi_recurse(get_current_module_statement(),
 			  reference_domain, reference_filter, gen_null, NULL);
@@ -913,8 +746,8 @@ declarations_privatizer(char *mod_name)
 
     if (get_bool_property("ARRAY_SECTION_PRIV_COPY_OUT"))
     {
-	pips_user_warning("property ARRAY_SECTION_PRIV_COPY_OUT set to TRUE ; " 
-			" COPY OUT not implemented.\n" ); 
+	pips_user_warning("property ARRAY_SECTION_PRIV_COPY_OUT set to TRUE ;" 
+			  " COPY OUT not implemented.\n" ); 
     }
 
     if (!same_string_p(rule_phase(find_rule_by_resource("REGIONS")),
@@ -952,9 +785,12 @@ declarations_privatizer(char *mod_name)
     /* For the moment, we only want to privatize whole variables;
      * Thus we only keep those variables which are absolutely not imported 
      * nor exported. 
-     * If we want to privatize regions, we can use the ressource PRIVATE_REGIONS.
+     * If we want to privatize regions, we can use the ressource 
+     * PRIVATE_REGIONS.
      */
-     /* Get the READ, WRITE, IN and OUT regions of the module */
+
+     /* Get the READ, WRITE, IN and OUT regions of the module
+      */
     set_rw_effects((statement_effects) 
 	db_get_memory_resource(DBR_REGIONS, mod_name, TRUE));
     set_in_effects((statement_effects) 
