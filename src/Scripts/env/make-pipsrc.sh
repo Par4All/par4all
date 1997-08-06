818ea09a6587d9c4e8ae0c926e7b9b85c54e720c
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-06 08:05:45 +0000

    # on 1997/08/06 08:05:45, coelho did: smaller.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7554 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/env/make-pipsrc.sh
+++ src/Scripts/env/make-pipsrc.sh
@@ -1,29 +1,13 @@
 #!/bin/sh
 #
-# SCCS Stuff
-# $RCSfile: make-pipsrc.sh,v $ ($Date: 1996/07/15 19:40:51 $, ) version $Revision$
+# $Id$
 #
 # to derive the Shell version of pipsrc.ref
 #
 
 
 {
-  cat <<-!
-	#	   --------------------------------------------------------
-	#	   --------------------------------------------------------
-	#
-	#				    WARNING
-	#
-	#		  THIS FILE HAS BEEN AUTOMATICALLY GENERATED
-	#
-	#			       DO NOT MODIFY IT
-	#
-	#	   --------------------------------------------------------
-	#	   --------------------------------------------------------
-	!
-
-   cat pipsrc.ref
-
+   cat ${PIPS_ROOT}/Include/auto-number.h pipsrc.ref
    echo export `sed -n '/^[^\[{}#=]*=/s/\([^ \t=]*\)[ \t]*=.*/\1/p' pipsrc.ref | sort -u | tr '\012' ' '` 
 
 } > pipsrc.sh
