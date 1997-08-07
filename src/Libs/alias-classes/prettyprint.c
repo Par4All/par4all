72ec0d79a3ce0c59738c85b26bc4c2e5f7bbb5ed
williams <williams@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-07 14:06:24 +0000

    # on 1997/08/07 14:06:24, williams did: debugged (I hope!). Nicky
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7601 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/alias-classes/prettyprint.c
+++ src/Libs/alias-classes/prettyprint.c
@@ -38,14 +38,14 @@ static text alias_pairs_text(string module_name,string resource_name)
     text txt = make_text(NIL);
     text txt_reg = make_text(NIL);
 
-    pips_debug(8,"module %s resource %s\n",module_name,resource_name);
+    pips_debug(4,"module %s resource %s\n",module_name,resource_name);
 
 /*    l_pairs = (list) db_get_memory_resource(resource_name, module_name, TRUE); */
 
     l_pairs = effects_to_list((effects)
 			      db_get_memory_resource(resource_name, module_name, TRUE));
 
-    pips_debug(8,"got pairs\n");
+    pips_debug(9,"got pairs\n");
 
     /* ATTENTION: all this is necessary to call module_to_value_mappings
      * to set up the hash table to translate value into value names
@@ -62,61 +62,30 @@ static text alias_pairs_text(string module_name,string resource_name)
 
     module_to_value_mappings(module);
 
-    pips_debug(8,"hash table set up\n");
+    pips_debug(9,"hash table set up\n");
 
     set_action_interpretation(ACTION_IN,ACTION_OUT);
 
     MAP(LIST,pair,
 	{
-	    pips_debug(9,"start 1st map\n");
+	    pips_debug(9,"make text for pair\n");
 
 	    if (pair != (list) HASH_UNDEFINED_VALUE && pair != list_undefined) 
 	    {
-/* was
-		MAP(EFFECT, reg,
-		    {
-			txt_reg = text_region(reg);
-			MERGE_TEXTS(txt, text_region(reg));
-		    },
-			pair);
-			*/
-
 		txt_reg = text_region(EFFECT(CAR(pair)));
-
-		pips_debug(9,"done text_region\n");
-
 		MERGE_TEXTS(txt,txt_reg);
-
-		pips_debug(9,"done MERGE_TEXTS\n");
-
 		txt_reg = text_region(EFFECT(CAR(CDR(pair))));
-
-		pips_debug(9,"done text_region\n");
-
 		MERGE_TEXTS(txt,txt_reg);
-
-		pips_debug(9,"done MERGE_TEXTS\n");
-
-		ADD_SENTENCE_TO_TEXT(
-		    txt,
-		    make_sentence(
-			is_sentence_formatted,
-			strdup("can't print translation?\n"))
-		    );
-
-		pips_debug(9,"done ADD_SENTENCE_TO_TEXT\n");
-
 		ADD_SENTENCE_TO_TEXT(
 		    txt,
 		    make_sentence(is_sentence_formatted,strdup("\n"))
 		    );
-
-		pips_debug(9,"done ADD_SENTENCE_TO_TEXT\n");
+		pips_debug(9,"made text for pair\n");
 	    }
 	},
 	    l_pairs);
 
-    pips_debug(8,"made text\n");
+    pips_debug(4,"end\n");
 
     reset_action_interpretation();
 
@@ -134,7 +103,7 @@ print_alias_pairs( string module_name, string resource_name, string file_extn )
     char *file_resource_name;
     bool success = TRUE;
 
-    pips_debug(8,"module %s resource %s file extn %s\n",
+    pips_debug(4,"module %s resource %s file extn %s\n",
 	       module_name,resource_name,file_extn);
 
     file_resource_name = DBR_ALIAS_FILE;
@@ -143,6 +112,9 @@ print_alias_pairs( string module_name, string resource_name, string file_extn )
 				 file_resource_name,
 				 file_extn,
 				 alias_pairs_text(module_name,resource_name));
+
+    pips_debug(4,"end\n");
+
     return(success);
 }
 
@@ -153,10 +125,11 @@ print_in_alias_pairs( string module_name )
     bool success = TRUE;
 
     debug_on("ALIAS_DEBUG_LEVEL");
-    pips_debug(8,"module %s\n",module_name);
+    pips_debug(4,"module %s\n",module_name);
 
     success = print_alias_pairs(module_name,DBR_IN_ALIAS_PAIRS,".in_alias");
 
+    pips_debug(4,"end\n");
     debug_off();
 
     return(TRUE);
@@ -169,10 +142,11 @@ print_out_alias_pairs( string module_name )
     bool success = TRUE;
 
     debug_on("ALIAS_DEBUG_LEVEL");
-    pips_debug(8,"module %s\n",module_name);
+    pips_debug(4,"module %s\n",module_name);
 
     success = print_alias_pairs(module_name,DBR_OUT_ALIAS_PAIRS,".out_alias");
 
+    pips_debug(4,"end\n");
     debug_off();
 
     return(TRUE);
