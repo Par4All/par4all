f0ce111a4d62c7504975aa12a28cb740396fb3db
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-18 08:18:02 +0000

    # on 1997/08/18 08:18:02, coelho did: main for pips.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7609 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Passes/pips/main_pips.c
@@ -0,0 +1,15 @@
+/* 
+ * $Id$
+ *
+ * This file contains the main for pips.
+ * Please, do not change anything! do any change to pips_main().
+ *
+ * FC.
+ */
+
+extern int pips_main(int, char**);
+
+int main(int argc, char ** argv)
+{
+    return pips_main(argc, argv);
+}
