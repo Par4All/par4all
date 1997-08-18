0128313b1b38ec7fb1d3a5d7906e39fe5c1b2861
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-18 08:50:31 +0000

    # on 1997/08/18 08:50:31, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7620 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/tpips/tpips.c
+++ src/Passes/tpips/tpips.c
@@ -826,7 +826,7 @@ static void parse_arguments(int argc, char * argv[])
 
 /* MAIN: interactive loop and history management.
  */
-int pips_main(int argc, char * argv[])
+int tpips_main(int argc, char * argv[])
 {
     debug_on("TPIPS_DEBUG_LEVEL");
 
