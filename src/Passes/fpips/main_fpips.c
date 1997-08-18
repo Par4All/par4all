be13d8703ea25acc950dbd0f9f8e1cfee4591fa7
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-18 09:40:12 +0000

    # on 1997/08/18 09:40:12, coelho did: main().
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7624 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Passes/fpips/main_fpips.c
@@ -0,0 +1,15 @@
+/* 
+ * $Id$
+ *
+ * This file contains the main for fpips.
+ * Please, do not change anything! do any change to fpips_main().
+ *
+ * FC.
+ */
+
+extern int fpips_main(int, char**);
+
+int main(int argc, char ** argv)
+{
+    return fpips_main(argc, argv);
+}
