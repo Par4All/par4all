7c48e6e4692cb37c53caaf82b3ef7050202bbf2f
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-18 08:39:08 +0000

    # on 1997/08/18 08:39:08, coelho did: main for tpips. ,
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7616 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Passes/tpips/main_tpips.c
@@ -0,0 +1,15 @@
+/* 
+ * $Id$
+ *
+ * This file contains the main for tpips.
+ * Please, do not change anything! do any change to tpips_main().
+ *
+ * FC.
+ */
+
+extern int tpips_main(int, char**);
+
+int main(int argc, char ** argv)
+{
+    return tpips_main(argc, argv);
+}
