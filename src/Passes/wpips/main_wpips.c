189b3edd4b4352dc19a547bb5d8793402b80f3fe
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-18 08:55:20 +0000

    # on 1997/08/18 08:55:20, coelho did: main() for wpips.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7622 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Passes/wpips/main_wpips.c
@@ -0,0 +1,15 @@
+/* 
+ * $Id$
+ *
+ * This file contains the main for wpips.
+ * Please, do not change anything! do any change to wpips_main().
+ *
+ * FC.
+ */
+
+extern int wpips_main(int, char**);
+
+int main(int argc, char ** argv)
+{
+    return wpips_main(argc, argv);
+}
