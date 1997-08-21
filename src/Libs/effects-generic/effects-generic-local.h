9240f20dedcf2fd07be17e12e37ed19571f0cb64
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-08-21 14:39:08 +0000

    # on 1997/08/21 14:39:08, coelho did: defines useful to both simple and convex effect methods.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7696 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/effects-generic/effects-generic-local.h
+++ src/Libs/effects-generic/effects-generic-local.h
@@ -1,6 +1,7 @@
 /* $Id$
  */
 
+
 /* some useful SHORTHANDS for EFFECT:
  */
 #define effect_entity(e) reference_variable(effect_reference(e))
@@ -18,6 +19,7 @@
 #define effect_exact_p(eff) \
         (approximation_tag(effect_approximation(eff)) ==is_approximation_exact)
 
+
 /* some string constants for prettyprints...
  */
 #define ACTION_UNDEFINED 	string_undefined
@@ -29,12 +31,46 @@
 #define ACTION_COPYOUT		"COPYOUT"
 #define ACTION_PRIVATE		"PRIVATE"
 
+
 /* prettyprint function types:
  */
 #include "text.h" /* hum... */
 typedef text (*generic_text_function)(list /* of effect */);
 typedef void (*generic_prettyprint_function)(list /* of effect */);
 typedef void (*generic_attachment_function)(text);
+
+
+/* for db_* functions 
+ */
+#define DB_GET_SE(name, NAME)				\
+static statement_effects db_get_##name(char * modname)	\
+{ return (statement_effects)				\
+  db_get_memory_resource(DBR_##NAME, modname, TRUE);}
+
+#define DB_GET_LS(name, NAME)				\
+static list db_get_##name(char * modname)		\
+{ return effects_to_list((effects)			\
+  db_get_memory_resource(DBR_##NAME, modname, TRUE));}
+
+#define DB_PUT_SE(name, NAME)						\
+static void db_put_##name(char * modname, statement_effects se)		\
+{ DB_PUT_MEMORY_RESOURCE(DBR_##NAME, strdup(modname), (char*) se);}
+
+#define DB_PUT_LS(name, NAME)				\
+static void db_put_##name(char * modname, list l)	\
+{DB_PUT_MEMORY_RESOURCE(DBR_##NAME,strdup(modname),(char*)list_to_effects(l));}
+
+#define DB_NOPUT_SE(name)\
+static void db_put_##name(char *m, statement_effects se) { return;}
+
+#define DB_NOPUT_LS(name)\
+static void db_put_##name(char *m, list l) { return;}
+
+#define DB_GETPUT_SE(name, NAME) DB_GET_SE(name, NAME) DB_PUT_SE(name, NAME)
+#define DB_GETNOPUT_SE(name, NAME) DB_GET_SE(name, NAME) DB_NOPUT_SE(name)
+#define DB_GETPUT_LS(name, NAME) DB_GET_LS(name, NAME) DB_PUT_LS(name, NAME)
+#define DB_GETNOPUT_LS(name, NAME) DB_GET_LS(name, NAME)DB_NOPUT_LS(name)
+
 
 /* For COMPATIBILITY purpose only - DO NOT USE anymore
  */
