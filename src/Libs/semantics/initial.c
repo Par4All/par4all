5f0402268c9de65c4e16a5a8d3b907e1850b791f
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-09-08 08:45:50 +0000

    # on 1997/09/08 08:45:50, coelho did: data transformer.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7761 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/semantics/initial.c
@@ -0,0 +1,67 @@
+/*
+ * $Id$
+ *
+ * $Log: initial.c,v $
+ * Revision 1.1  1997/09/08 08:45:50  coelho
+ * Initial revision
+ *
+ *
+ * Computation of initial transformers that allow to collect
+ * global initializations of BLOCK DATA and so.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "genC.h"
+
+#include "ri.h"
+#include "ri-util.h"
+
+#include "database.h"
+#include "pipsdbm.h"
+#include "resources.h"
+
+#include "misc.h"
+
+#include "effects-generic.h"
+#include "effects-simple.h"
+
+#include "transformer.h"
+
+#include "semantics.h"
+
+/******************************************************** PIPSMAKE INTERFACE */
+
+/* Compute an initial transformer.
+ */
+bool 
+initial_transformer(string name)
+{
+    pips_internal_error("not implemented yet");
+    return FALSE;
+}
+
+bool
+program_transformer(string name)
+{
+    pips_internal_error("not implemented yet");
+    return FALSE;
+}
+
+/*********************************************************** PRETTY PRINTERS */
+
+bool 
+print_code_initial_transformer(string name)
+{
+    pips_internal_error("not implemented yet");
+    return FALSE;
+}
+
+bool 
+print_code_program_transformer(string name)
+{
+    pips_internal_error("not implemented yet");
+    return FALSE;
+}
+
