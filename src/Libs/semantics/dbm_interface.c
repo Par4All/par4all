da52d40cf70948d4840a1dcdc3ead4578d61e6fc
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-09-08 19:15:50 +0000

    # on 1997/09/08 19:15:50, coelho did: use computed program preconditions.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@7781 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/semantics/dbm_interface.c
+++ src/Libs/semantics/dbm_interface.c
@@ -413,7 +413,8 @@ char *module_name;
     /* if(call_site_count( get_current_module_entity() )<=1) */
     /* Let's assume static initializations (FI, 14 September 1993) */
     if(entity_main_module_p(get_current_module_entity()))
-	pre = data_to_precondition( get_current_module_entity() );
+	pre = /* data_to_precondition( get_current_module_entity() ); */
+	    db_get_memory_resource(DBR_PROGRAM_PRECONDITION, "", FALSE);
     else
 	pre = transformer_identity();
 
