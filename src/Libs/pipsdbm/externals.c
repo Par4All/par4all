f249cdb4b188663959ef083348a2f2cfc59e3e12
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-09-25 12:50:41 +0000

    # on 1997/09/25 12:50:41, coelho did: cleaned version. set of functions for statement_mapping and statement ->.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8080 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/pipsdbm/externals.c
+++ src/Libs/pipsdbm/externals.c
@@ -18,6 +18,37 @@
 #include "paf_ri.h"
 #include "compsec.h"
 
+/********************************************************************* UTILS */
+
+/* reads an int while sharing file and buffers with newgen...
+ */
+extern int genread_input(void);
+extern FILE * genread_in;
+
+static int 
+lire_int(FILE * fd)
+{
+    int c, i = 0, sign = 1;
+    genread_in = fd;
+
+    while (isspace(c = genread_input())) ; /* trim */
+
+    if (c == '-') sign = -1;
+    else if (isdigit(c))
+	i = c-'0';
+    else if(c==EOF)
+	pips_internal_error("Unexpected EOF, corrupted resource file\n");
+    else
+	pips_internal_error("digit or '-' expected : %c %x\n", c, c);
+
+    while (isdigit(c = genread_input())) 
+	i = 10*i + (c-'0');
+
+    return sign*i;
+}
+
+/*********************************************************** ENTITY WRAPPERS */
+
 char *
 pipsdbm_read_entities(FILE * fd)
 {
@@ -32,161 +63,193 @@ pipsdbm_free_entities(char * p)
     gen_free_tabulated(entity_domain);
 }
 
-/* methods_io.c */
+/**************************************************** OLD STATEMENT MAPPING */
 
-/* statement without a proper ordering are not saved on disk */
-static int effective_number_of_statements(statement_mapping map)
+/* the old statement_mapping is an hash_table managed directly.
+ * this table associates statement to any newgen type. 
+ * now explicit newgen functions ("->") should be prefered.
+ * the storing of the mapping is based on the statement ordering for
+ * latter reconstruction with the CODE. 
+ * tabulating statements would help, but is that desirable?
+ *
+ * MY opinion is that all newgen objects should have a unique id
+ * associated to it, as an addditional hidden field, to support 
+ * persistence. more easily. Well, there would be some problemes, too.
+ *
+ * FC.
+ */
+static int
+statement_mapping_count(statement_mapping h)
 {
-  int n_records = 0;
-
-    STATEMENT_MAPPING_MAP(s, val, {
-	statement stat = (statement) s;
-	int ordering_of_stat = statement_ordering(stat);
-	if(ordering_of_stat != STATEMENT_ORDERING_UNDEFINED) {
-	  n_records++;
-	}
-    }, map);
-
-    pips_assert("write_effects_mapping", 
-		STATEMENT_MAPPING_COUNT(map)>=n_records);
-
-    return n_records;
-}
-
-extern int genread_input();
-extern FILE *genread_in;
-
-static int lire_int(fd)
-FILE *fd;
-{
-    int c;
-    int i = 0, sign = 1;
-
-    genread_in = fd;
-
-    while (isspace(c = genread_input())) ;
-
-    if (c == '-') {
-	sign = -1;
-    }
-    else if (isdigit(c)) {
-	i = c-'0';
-    }
-    else {
-      if(c==EOF)
-	pips_error("lire_int",
-		   "Unexpected end of file, corrupted resource file\n");
-      else
-	pips_error("lire_int", "digit or '-' expected : %c %x\n", c, c);
-    }
-
-    while (isdigit(c = genread_input())) {
-	i = 10*i + (c-'0');
-    }
-
-    return(sign*i);
-}
-
-/* statement_effects mappings */
-int 
-statement_effects_length(statement_effects map)
-{
-    int n_records = 0;
-
-    STATEMENT_EFFECTS_MAP(s, val, {
-	statement stat = (statement) s;
-	int ordering_of_stat = statement_ordering(stat);
-	if(ordering_of_stat != STATEMENT_ORDERING_UNDEFINED)
-	    n_records++;
-    }, map);
-
-    return n_records;
+    int n = 0;
+    STATEMENT_MAPPING_MAP(s, k,
+	if (statement_ordering((statement) s)!=STATEMENT_ORDERING_UNDEFINED) 
+	    n++,
+	h);
+    return n;
 }
 
 void
-pipsdbm_write_statement_effects(FILE *fd, statement_effects map)
+pipsdbm_write_statement_mapping(
+    FILE * fd, /* file to write to */
+    statement_mapping h /* hash table to dump */)
 {
-    fprintf(fd, "%d\n", statement_effects_length(map));
-
-    STATEMENT_EFFECTS_MAP(s, effs, {
-	statement stat = (statement) s;
-	int ordering_of_stat = statement_ordering(stat);
-	if(ordering_of_stat != STATEMENT_ORDERING_UNDEFINED)
-	{
-	    fprintf(fd, "%d\n",ordering_of_stat );
-	    write_effects(fd, effs);
-	}
-	else {
-	    user_warning("write_effects_mapping",
-			 "Statement with illegal ordering, "
-			 "effects not stored on disk!\n");
-	}
-    }, map);
+    fprintf(fd, "%d\n", statement_mapping_count(h));
+    STATEMENT_MAPPING_MAP(s, v,
+    {
+	statement key = (statement) s;
+	gen_chunkp val = (gen_chunkp) v;
+	int order = statement_ordering(key);
+	if (order!=STATEMENT_ORDERING_UNDEFINED) { /* save it! */
+	    fprintf(fd, "%d\n", order);
+	    gen_write(fd, (gen_chunkp) val);
+	} 
+	else pips_user_warning("statement with illegal ordering\n");
+    },
+	h);
 }
 
-statement_effects
-pipsdbm_read_statement_effects(FILE *fd)
+hash_table
+pipsdbm_read_statement_mapping(FILE * fd)
 {
-    statement module_stat;
-    statement_effects map;
-    int ne;
+    statement stat;
+    hash_table result = hash_table_make(hash_pointer, 0);
+    int n;
 
-    /* as for writing, loading ordering should be handled at a higher
-       level, via tables */
     pips_assert("some current module name", db_get_current_module_name());
-    module_stat = (statement) 
+    pips_debug(3, "statement -> ??? for %s\n", db_get_current_module_name());
+    stat = (statement) /* hope it is the right module... */
 	db_get_memory_resource(DBR_CODE, db_get_current_module_name(), TRUE);
 
-    initialize_ordering_to_statement(module_stat);
-    map = make_statement_effects();
+    initialize_ordering_to_statement(stat);
 
-    ne = lire_int(fd);
-    while (ne-- > 0) {
-	int ns;
-	if((ns = lire_int(fd))==STATEMENT_ORDERING_UNDEFINED)
-	    pips_internal_error("Undefined statement ordering\n");
+    /* get meta data.
+     */
+    n = lire_int(fd);
 
-	extend_statement_effects(map, ordering_to_statement(ns), 
-				 read_effects(fd));
+    while (n-->0) {
+	int so = lire_int(fd);
+	pips_assert("valid ordering", so!=STATEMENT_ORDERING_UNDEFINED);
+	hash_put(result,(char*)ordering_to_statement(so),(char*)gen_read(fd));
     }
-    
-    return(map);
+
+    return result;
+}
+
+/* a little bit partial, because domain are not checked.
+ */
+bool
+pipsdbm_check_statement_mapping(statement_mapping h)
+{
+    STATEMENT_MAPPING_MAP(k, v, {
+	pips_assert("consistent statement", gen_consistent_p((statement) k));
+	pips_assert("consistent val", gen_consistent_p((gen_chunkp) v));
+    },
+	h);
+    return TRUE;
 }
 
 void
-pipsdbm_free_statement_effects(statement_effects map)
+pipsdbm_free_statement_mapping(statement_mapping h)
 {
-    STATEMENT_EFFECTS_MAP(s, val, {
-	free_effects((effects) val);
-    }, map);
+    STATEMENT_MAPPING_MAP(k, v, gen_free((gen_chunkp) v), h);
+    FREE_STATEMENT_MAPPING(h);
 }
 
-void write_static_control_mapping(fd, map)
-FILE *fd;
-statement_mapping map;
-{
-    fprintf(fd, "%d\n", effective_number_of_statements(map));
+/**************************************** STATEMENT FUNCTIONS: STATEMENT -> */
 
-    STATEMENT_MAPPING_MAP(s, val, {
-        statement stat = (statement) s;
-        int ordering_of_stat = statement_ordering(stat);
-	if(ordering_of_stat != STATEMENT_ORDERING_UNDEFINED) {
-	  fprintf(fd, "%d\n",ordering_of_stat );
-	  write_static_control(fd, (static_control) val );
-	} else 
-	  pips_user_warning("Statement with illegal ordering\n");
-    }, map);
+/* this piece of code makes low level assumptions about newgen internals.
+ * see also the comments about statement mappings above.
+ */
+
+#define STATEMENT_FUNCTION_MAP(k, v, code, _map_hash_h)			\
+    { hash_entry_pointer _map_hash_p = NULL;				\
+      char *_map_k; char *_map_v;					\
+      while ((_map_hash_p =						\
+	   hash_table_scan(_map_hash_h,_map_hash_p,&_map_k,&_map_v))) {	\
+        statement k = ((gen_chunk*)_map_k)->p ;				\
+        gen_chunkp v = ((gen_chunk*)_map_v)->p;				\
+        code ; }}
+
+/* count the number of statements with a valid ordering. */
+static int 
+number_of_ordered_statements(hash_table h)
+{
+    int n = 0;
+    STATEMENT_FUNCTION_MAP(s, x, 
+        if (statement_ordering(s)!=STATEMENT_ORDERING_UNDEFINED) n++, h);
+    return n;
 }
 
-/* Modification Dec 11 1995: ne pas utiliser free_static_control */
-/* car il libere des champs qui appartiennent a d'autres structures */
-/* que celles controlees par static_controlize...(champs d'origine) */
-/* Les liberation de ces champs par un autre transformer (use_def_elim) */
-/* entrainait alors un core dump au niveau de cette procedure. */
-/* On fait a la place des gen_free_list en detail --DB */
- 
-void free_static_control_mapping(map)
-statement_mapping map;
+/* the stored stuff need be based on the ordering...
+ * because newgen wont regenerate pointers...
+ */
+void
+pipsdbm_write_statement_function(
+    FILE * fd, /* file to write to */
+    gen_chunkp map /* statement function */)
+{
+    hash_table h = (map+1)->h;
+    fprintf(fd, "%d\n%d\n", map->i, number_of_ordered_statements(h));
+    STATEMENT_FUNCTION_MAP(s, x, 
+    {
+	int order = statement_ordering(s);
+	if (order!=STATEMENT_ORDERING_UNDEFINED) { /* save it! */
+	    fprintf(fd, "%d\n", order);
+	    gen_write(fd, x);
+	} 
+	else pips_user_warning("Statement with illegal ordering, lost data\n");
+    },
+	h);
+}
+
+gen_chunkp
+pipsdbm_read_statement_function(FILE * fd /* file to read from */)
+{
+    statement stat; 
+    int domain, n;
+    gen_chunkp result;
+    hash_table h;
+    
+    pips_assert("some current module name", db_get_current_module_name());
+    pips_debug(3, "statement -> ??? for %s\n", db_get_current_module_name());
+    stat = (statement) /* hope it is the right module... */
+	db_get_memory_resource(DBR_CODE, db_get_current_module_name(), TRUE);
+
+    initialize_ordering_to_statement(stat);
+
+    /* get meta data.
+     */
+    domain = lire_int(fd);
+    n = lire_int(fd);
+
+    /* allocate the statement function. 
+     */
+    result = gen_alloc(2*sizeof(gen_chunk), GEN_CHECK_ALLOC, domain);
+    h = (result+1)->h;
+
+    /* now reads each couple and rebuild the function.
+     */
+    while(n-->0) {
+	int so = lire_int(fd);
+	pips_assert("valid ordering", so!=STATEMENT_ORDERING_UNDEFINED);
+	HASH_EXTEND(p, p, h, ordering_to_statement(so), gen_read(fd));
+    }
+
+    return result;
+}
+
+/********************************************************************** MISC */
+
+/* Modification Dec 11 1995: ne pas utiliser free_static_control
+ * car il libere des champs qui appartiennent a d'autres structures
+ * que celles controlees par static_controlize...(champs d'origine)
+ * Les liberation de ces champs par un autre transformer (use_def_elim)
+ * entrainait alors un core dump au niveau de cette procedure.
+ * On fait a la place des gen_free_list en detail --DB ]
+ */
+ void 
+free_static_control_mapping(statement_mapping map)
 {
     STATEMENT_MAPPING_MAP(s, val, {
         gen_free_list(static_control_loops((static_control) val));
@@ -198,292 +261,4 @@ statement_mapping map;
     }, map);
 
     FREE_STATEMENT_MAPPING(map);
-}
-
-statement_mapping read_static_control_mapping(fd)
-FILE *fd;
-{
-    statement module_stat;
-    statement_mapping map;
-    int ne;
-    string module_name = db_get_current_module_name();
-
-    pips_assert("current module name", module_name);
-    module_stat = (statement)
-        db_get_memory_resource(DBR_CODE, module_name, TRUE);
-    initialize_ordering_to_statement(module_stat);
-    map = MAKE_STATEMENT_MAPPING();
-
-    ne = lire_int(fd);
-
-    while (ne-- > 0) {
-        int ns;
-
-	if((ns = lire_int(fd))==STATEMENT_ORDERING_UNDEFINED) {
-	  pips_error("read_static_control_mapping",
-		     "Undefined statement ordering\n");
-	}
-
-        SET_STATEMENT_MAPPING(map,
-                              ordering_to_statement(ns),
-                              read_static_control(fd));
-    }
-    return(map);
-}
-
-bool check_static_control_mapping(map)
-statement_mapping map;
-{
-    STATEMENT_MAPPING_MAP(s, val, {
-        pips_assert("Statement in static control mapping",
-		    gen_consistent_p((statement)s));
-        pips_assert("Static control mapping ok",
-		    gen_consistent_p( (static_control) val));
-    }, map);
-    return TRUE;
-}
-
-
-void write_transformer_mapping(fd, map)
-FILE *fd;
-statement_mapping map;
-{
-    fprintf(fd, "%d\n", effective_number_of_statements(map));
-
-    STATEMENT_MAPPING_MAP(s, val, {
-	statement stat = (statement) s;
-	int ordering_of_stat = statement_ordering(stat);
-	if(ordering_of_stat != STATEMENT_ORDERING_UNDEFINED) {
-	  fprintf(fd, "%d\n", ordering_of_stat);
-	  write_transformer(fd, (transformer) val);
-	} else
-	    pips_user_warning("Statement with illegal ordering\n");
-    }, map);
-}
-
-void print_transformer_mapping(map)
-statement_mapping map;
-{
-    STATEMENT_MAPPING_MAP(s, val, {
-
-	/* The key may be obsolete because the module code
-	 * may have been removed, e.g. a transformation was
-	 * applied 
-	 */
-	/* gen_check((statement) s, statement_domain); */
-	gen_check((transformer) val, transformer_domain);
-	fprintf(stderr,"\t%p\t%p\n", s, val);
-    }, map);
-}
-
-void free_transformer_mapping(map)
-statement_mapping map;
-{
-    /* print_transformer_mapping(map); */
-
-    STATEMENT_MAPPING_MAP(s, val, {
-
-	/* The key may be obsolete because the module code
-	 * may have been removed, e.g. a transformation was
-	 * applied 
-	 */
-	/* gen_check((statement) s, statement_domain); */
-
-	gen_check((transformer) val, transformer_domain);
-	free_transformer ((transformer) val);
-    }, map);
-
-    FREE_STATEMENT_MAPPING(map);
-}
-
-bool check_transformer_mapping(map)
-statement_mapping map;
-{
-    bool consistent_p = TRUE;
-    /* Warning: gen_check() aborts on mistake!
-     * The following piece of code behaves strangely,
-     * but provides key variables to the debugger.
-     */
-
-    STATEMENT_MAPPING_MAP(key, val, {
-	statement s = (statement) key;
-	transformer t = (transformer) val;
-
-	if(!gen_check(s, statement_domain)) {
-	    pips_internal_error("Non consistent transformer mapping"
-				" for statement key %p\n", s);
-	}
-
-	if(!gen_check(t, transformer_domain)) {
-	    pips_internal_error("Non consistent transformer mapping"
-				" for transformer value %p\n", t);
-	}
-
-    }, map);
-
-    return consistent_p;
-}
-
-statement_mapping read_transformer_mapping(fd)
-FILE *fd;
-{
-    statement module_stat;
-    statement_mapping map;
-    string module_name = db_get_current_module_name();
-    int ne;
-
-    /* as for writing, loading ordering should be handled at a higher
-       level, via tables */
-    pips_assert("some current module name", module_name);
-    module_stat = (statement) 
-	db_get_memory_resource(DBR_CODE, module_name, TRUE);
-
-    initialize_ordering_to_statement(module_stat);
-    map = MAKE_STATEMENT_MAPPING();
-    ne = lire_int(fd);
-
-    while (ne-- > 0) {
-	int ns;
-	if((ns = lire_int(fd))==STATEMENT_ORDERING_UNDEFINED)
-	    pips_internal_error("Undefined statement ordering\n");
-
-	SET_STATEMENT_MAPPING(map, ordering_to_statement(ns), 
-			      read_transformer(fd));
-    }
-
-    return(map);
-}
-
-void write_complexity_mapping(fd, map)
-FILE *fd;
-statement_mapping map;
-{
-    fprintf(fd, "%d\n", effective_number_of_statements(map));
-
-    STATEMENT_MAPPING_MAP(s, val, {
-	statement stat = (statement) s;
-	int ordering_of_stat = statement_ordering(stat);
-	if(ordering_of_stat != STATEMENT_ORDERING_UNDEFINED) {
-	  fprintf(fd, "%d\n",ordering_of_stat );
-	  write_complexity(fd, (complexity) val);
-	} else
-	    pips_user_warning("Statement with illegal ordering\n");
-    }, map);
-}
-
-void free_complexity_mapping(map)
-statement_mapping map;
-{
-    STATEMENT_MAPPING_MAP(s, val, {
-	free_complexity((complexity) val);
-    }, map);
-
-    FREE_STATEMENT_MAPPING(map);
-}
-
-statement_mapping read_complexity_mapping(fd)
-FILE *fd;
-{
-    statement module_stat;
-    statement_mapping map;
-    string module_name = db_get_current_module_name();
-    int ne;
-
-    /* as for writing, loading ordering should be handled at a higher
-       level, via tables */
-    pips_assert("some current module name", module_name);
-    module_stat = (statement) 
-	db_get_memory_resource(DBR_CODE, module_name, TRUE);
-
-    initialize_ordering_to_statement(module_stat);
-
-    map = MAKE_STATEMENT_MAPPING();
-
-    ne = lire_int(fd);
-
-    while (ne-- > 0) {
-	int ns = lire_int(fd);
-	SET_STATEMENT_MAPPING(map, 
-			      ordering_to_statement(ns), 
-			      read_complexity(fd));
-    }
-
-    return(map);
-}
-
-void write_compsec_mapping(fd, map)
-FILE *fd;
-statement_mapping map;
-{
-    fprintf(fd, "%d\n", effective_number_of_statements(map));
-
-    STATEMENT_MAPPING_MAP(s, val, {
-	statement stat = (statement) s;
-	int ordering_of_stat = statement_ordering(stat);
-	if(ordering_of_stat != STATEMENT_ORDERING_UNDEFINED) {
-	  fprintf(fd, "%d\n",ordering_of_stat );
-	  write_comp_desc_set(fd, val);
-	}
-	else {
-	    pips_user_warning("Statement with illegal ordering\n");
-	}
-    }, map);
-}
-
-void free_compsec_mapping(map)
-statement_mapping map;
-{
-    STATEMENT_MAPPING_MAP(s, val, {
-	free_comp_desc_set((comp_desc_set) val);
-    }, map);
-
-    FREE_STATEMENT_MAPPING(map);
-}
-
-statement_mapping read_compsec_mapping(fd)
-FILE *fd;
-{
-    statement module_stat;
-    statement_mapping map;
-    string module_name = db_get_current_module_name();
-    int ne;
-
-    /* as for writing, loading ordering should be handled at a higher
-       level, via tables */
-    pips_assert("some current module name", module_name);
-    module_stat = (statement) 
-	db_get_memory_resource(DBR_CODE, module_name, TRUE);
-
-    initialize_ordering_to_statement(module_stat);
-
-    map = MAKE_STATEMENT_MAPPING();
-
-    ne = lire_int(fd);
-
-    while (ne-- > 0) {
-	int ns;
-
-	if((ns = lire_int(fd))==STATEMENT_ORDERING_UNDEFINED) {
-	  pips_error("read_compsec_mapping",
-		     "Undefined statement ordering\n");
-	}
-
-	SET_STATEMENT_MAPPING(map, 
-			      ordering_to_statement(ns), 
-			      read_comp_desc_set(fd));
-    }
-
-    return(map);
-}
-
-bool check_compsec_mapping(map)
-statement_mapping map;
-{
-    STATEMENT_MAPPING_MAP(s, val, 
-      {
-	  assert(statement_consistent_p((statement) s) &&
-		 comp_desc_set_consistent_p((comp_desc_set)val));
-      }, map);
-    
-    return(TRUE);
 }
