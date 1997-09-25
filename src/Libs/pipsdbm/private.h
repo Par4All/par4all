0a425380315ab99e3a02d608c40aacd6372266ae
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-09-25 09:43:48 +0000

    # on 1997/09/25 09:43:48, coelho did: includes needed by pipsdbm files.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8075 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/pipsdbm/private.h
@@ -0,0 +1,24 @@
+/*
+ * $Id$
+ *
+ * stuff to include, local to pipsdbm.
+ * SHOULD NOT include ri-util.h.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "resources.h"
+
+#include "genC.h"
+#include "ri.h"
+
+#include "misc.h"
+#include "properties.h"
+
+#include "ri-util.h" /* needed for statement_mapping */
+
+#include "database.h" /* for obsolete functions... */
+#include "pipsdbm.h"
