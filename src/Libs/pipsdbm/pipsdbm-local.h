53bfb32552c40ec93f2db430cd56a0aa5bbf038f
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-09-25 09:43:48 +0000

    # on 1997/09/25 09:43:48, coelho did: defines to switch from old to new interface.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8071 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/pipsdbm/pipsdbm-local.h
@@ -0,0 +1,18 @@
+/*
+ * $Id$
+ */
+
+/* conform to old interface.
+ */
+#define DB_PUT_MEMORY_RESOURCE(res_name, own_name, res_val) \
+  db_put_or_update_memory_resource(res_name, own_name, (char*) res_val, TRUE)
+
+/* put a resource which is a file. it is just a resource as any other.
+ */
+#define DB_PUT_FILE_RESOURCE DB_PUT_MEMORY_RESOURCE
+#define DB_PUT_NEW_FILE_RESOURCE(res_name, own_name, res_val) \
+  db_put_or_update_memory_resource(res_name, own_name, (char*) res_val, FALSE)
+
+#define db_get_file_resource db_get_memory_resource
+#define db_unput_a_resource(r,o) (db_delete_resource(r,o), TRUE)
+#define build_pgmwd db_get_workspace_directory_name
