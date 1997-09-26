1cb0c2d5ee1a8dd8271961b93e3f06ba181aeb71
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-09-26 14:51:23 +0000

    # on 1997/09/26 14:51:23, coelho did: old stuff back temporarilly.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8118 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/misc/args.c
+++ src/Libs/misc/args.c
@@ -42,3 +42,91 @@ sort_list_of_strings(list l)
     update_list_from_arg(l, a);
     gen_array_free(a);
 }
+
+/************************************************************ OBSOLETE STUFF */
+
+/* 
+ * Quite a poor code in my opinion. FC.
+ */
+
+/* given li build argv, update *argc and free li */
+void
+list_to_arg(list li,
+            int * pargc,
+            char * argv[])
+{
+   list l2=li;
+   int index=0;
+
+   pips_assert("not too long", gen_length(li)<ARGS_LENGTH);
+
+   while(l2!=NIL) {
+       argv[index++] = STRING(CAR(l2));
+       l2= CDR(l2);
+   }
+
+   gen_free_list(li), li= NIL;
+   (*pargc) = index;
+}
+
+
+/* Just modify the strings in a list from an array of strings. The
+   array of string must have at least as much as strings as in the
+   list. No free is done. */
+void
+update_list_from_arg(
+    list l,
+    char ** the_strings)
+{
+    int i = 0;
+    pips_assert("long enough", gen_length(l)<ARGS_LENGTH);
+    MAPL(scons, STRING(CAR(scons)) = the_strings[i++], l);
+}
+
+
+void 
+args_free(
+    int *pargc,
+    char *argv[])
+{
+    int i;
+    pips_assert("length ok", *pargc<ARGS_LENGTH);
+
+    for (i = 0; i < *pargc; i++)
+	free(argv[i]);
+    (*pargc) = 0;
+}
+
+
+
+void 
+args_add(
+    int *pargc,
+    char *argv[],
+    char *arg)
+{
+    pips_assert("not too many", *pargc<ARGS_LENGTH);
+    argv[*pargc] = arg;
+    (*pargc) ++;
+}
+
+
+int
+compare_args_string_for_qsort(const void *x,
+                              const void *y)
+{
+   return strcmp(* (char **) x, * (char **) y);
+
+}
+
+
+/* Sort an arg list: */
+void
+args_sort(int argc,
+          char *argv[])
+{
+   qsort((char *) argv,
+         argc,
+         sizeof(char *),
+         compare_args_string_for_qsort);
+}
