6a747b2ca03256e1ec9e4b761b30b493854cf872
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-09-26 14:16:08 +0000

    # on 1997/09/26 14:16:08, coelho did: static sized args dropped out.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8112 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/misc/args.c
+++ src/Libs/misc/args.c
@@ -1,5 +1,7 @@
-/* 
- * Quite a poor code in my opinion. FC.
+/* $Id$
+ *
+ * there was some static buffer management here. 
+ * now it relies on dynamic arrays.
  */
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,104 +10,35 @@
 #include "genC.h"
 #include "misc.h"
 
-
-/* given li build argv, update *argc and free li */
 void
-list_to_arg(list li,
-            int * pargc,
-            char * argv[])
+list_to_array(list l, gen_array_t a)
 {
-   list l2=li;
-   int index=0;
-
-   pips_assert("not too long", gen_length(li)<ARGS_LENGTH);
-
-   while(l2!=NIL) {
-       argv[index++] = STRING(CAR(l2));
-       l2= CDR(l2);
-   }
-
-   gen_free_list(li), li= NIL;
-   (*pargc) = index;
+    int index = 0;
+    MAP(STRING, s, gen_array_addto(a, index++, s), l);
+    gen_free_list(l);
 }
-
 
 /* Just modify the strings in a list from an array of strings. The
    array of string must have at least as much as strings as in the
    list. No free is done. */
 void
-update_list_from_arg(
-    list l,
-    char ** the_strings)
+update_list_from_arg(list l, gen_array_t a)
 {
-    int i = 0;
-    pips_assert("long enough", gen_length(l)<ARGS_LENGTH);
-    MAPL(scons, STRING(CAR(scons)) = the_strings[i++], l);
+    int index = 0;
+    MAPL(scons, STRING(CAR(scons)) = gen_array_item(a, index++), l);
 }
 
 
-void 
-args_free(
-    int *pargc,
-    char *argv[])
-{
-    int i;
-    pips_assert("length ok", *pargc<ARGS_LENGTH);
-
-    for (i = 0; i < *pargc; i++)
-	free(argv[i]);
-    (*pargc) = 0;
-}
-
-
-
-void 
-args_add(
-    int *pargc,
-    char *argv[],
-    char *arg)
-{
-    pips_assert("not too many", *pargc<ARGS_LENGTH);
-    argv[*pargc] = arg;
-    (*pargc) ++;
-}
-
-
-int
-compare_args_string_for_qsort(const void *x,
-                              const void *y)
-{
-   return strcmp(* (char **) x, * (char **) y);
-
-}
-
-
-/* Sort an arg list: */
-void
-args_sort(int argc,
-          char *argv[])
-{
-   qsort((char *) argv,
-         argc,
-         sizeof(char *),
-         compare_args_string_for_qsort);
-}
-
-
-/* Sort a list of string: */
+/* Sort a list of strings.
+ */
 void
 sort_list_of_strings(list l)
 {
-    int arg_number = 0;
     int number_of_strings = gen_length(l);
-    char * argsv[ARGS_LENGTH];
-
-    pips_assert("not too long", number_of_strings<ARGS_LENGTH);
-    list_to_arg(l, &arg_number, argsv);
-
-    pips_assert("The size of the args and the list should be the same",
-		arg_number == number_of_strings);
-
-    args_sort(arg_number, argsv);
+    gen_array_t a = gen_array_make(number_of_strings); 
+    list_to_array(l, a);
+    pips_assert("same length", number_of_strings==gen_array_nitems(a));
+    gen_array_sort(a);
     update_list_from_arg(l, argsv);
+    gen_array_free(a);
 }
