8afe95e1671b52f664faef7d65aa87f3b8355624
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1997-09-26 12:55:03 +0000

    # on 1997/09/26 12:55:03, coelho did: dynamic array utils.
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@461 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

---
+++ genC/newgen_array.h
@@ -0,0 +1,20 @@
+/* $Id$
+ */
+
+struct _gen_array_chunk_t;
+typedef struct _gen_array_chunk_t * gen_array_t;
+
+#define gen_array_undefined ((gen_array_t) -12)
+#define gen_array_undefined_p(a) ((a)==gen_array_undefined)
+
+/* declarations...
+ */
+gen_array_t gen_array_make(int);
+void gen_array_free(gen_array_t);
+void gen_array_full_free(gen_array_t);
+void gen_array_addto(gen_array_t, int, char *);
+void gen_array_dupaddto(gen_array_t, int, char *);
+char **gen_array_pointer(gen_array_t);
+int gen_array_nitems(gen_array_t);
+int gen_array_size(gen_array_t);
+void gen_array_sort(gen_array_t);
