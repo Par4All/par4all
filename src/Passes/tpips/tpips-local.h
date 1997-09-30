d8c93eefc6c3481116ddecb1b692431d77dd04f6
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-09-30 06:54:13 +0000

    # on 1997/09/30 06:54:13, coelho did: typedef for array removed. gen_array_t used instead.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8144 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/tpips/tpips-local.h
+++ src/Passes/tpips/tpips-local.h
@@ -1,16 +1,5 @@
-/* $RCSfile: tpips-local.h,v $ (version $Revision$)
- * $Date: 1997/04/11 16:18:24 $, 
+/* $Id$
  */
-
-/* FI: temporary storage of file names could be avoided with better yacc rules
- * 272 files in Zebulon
- */
-#define FILE_LIST_MAX_LENGTH 300
-
-typedef struct _t_file_list {
-	int argc;
-	char *argv[FILE_LIST_MAX_LENGTH];
-} t_file_list;
 
 typedef struct _res_or_rule {
 	list the_owners;
