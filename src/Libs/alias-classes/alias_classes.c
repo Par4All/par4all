66f0cc559cbe2f008a4470ad89683d2288b35233
williams <williams@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-10-03 13:48:02 +0000

    # on 1997/10/03 13:48:02, williams did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8227 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/alias-classes/alias_classes.c
+++ src/Libs/alias-classes/alias_classes.c
@@ -16,16 +16,56 @@
 #include "misc.h"
 #include "text.h"
 
+#include "boolean.h"
+#include "vecteur.h"
+#include "contrainte.h"
+#include "sc.h"
+#include "sommet.h"
+#include "ray_dte.h"
+#include "sg.h"
+#include "polyedre.h"
+#include "union.h"
+
 #include "effects-generic.h"
 #include "effects-simple.h"
 #include "effects-convex.h"
 
+#include "semantics.h"
+#include "transformer.h"
+
 #include "pipsdbm.h"
 #include "resources.h"
 
-bool
-alias_equivalence_classes ( string name )
+static list l_alias_lists = NIL;
+static list l_alias_classes = NIL;
+
+/*
+static void
+add_classes_callees(string module_name, list alias_lists)
 {
+}
+*/
+
+bool
+alias_classes( string module_name )
+{
+/*
+    list alias_lists;
+
+    pips_debug(4,"begin for module %s\n",module_name);
+
+    alias_lists = effects_to_list((effects)
+				  db_get_memory_resource(DBR_ALIAS_LISTS,
+							 module_name,
+							 TRUE));
+    MAP(LIST,alias_list,
+    {
+    l_alias_lists = gen_nconc(CONS(LIST,regions_dup(alias_list),NIL),l_alias_lists);
+    },alias_lists);
+
+    add_classes_callees(module_name,alias_lists);
+    */
+
     return TRUE;
 }
 
