5a4652e60b1d88429aa1b35bbe38fd4f25aab130
ancourt <ancourt@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-11-21 17:48:52 +0000

    # on 1997/11/21 17:48:52, ancourt did: line management added...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8551 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/text-util/util.c
+++ src/Libs/text-util/util.c
@@ -1,8 +1,6 @@
-/* 	%A% ($Date: 1996/06/12 16:38:24 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.	 */
-
-#ifndef lint
-char vcid_text_util[] = "%A% ($Date: 1996/06/12 16:38:24 $, ) version $Revision$, got on %D%, %T% [%P%].\n Copyright (c) École des Mines de Paris Proprietary.";
-#endif /* lint */
+/*
+ * $Id$
+ */
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -11,6 +9,7 @@ char vcid_text_util[] = "%A% ($Date: 1996/06/12 16:38:24 $, ) version $Revision$
 #include "text.h"
 #include "text-util.h"
 #include "misc.h"
+#include "properties.h"
 
 char *
 i2a(int i)
@@ -89,4 +88,85 @@ last_word_of_sentence(sentence s)
 	pips_assert("s should be formatted or unformatted...", 0);
 
     return end;
+}
+
+/******************************************************** LINE MANAGEMENT */
+
+#define FORESYS_CONTINUATION_PREFIX "C$&"
+
+#define LINE_SUFFIX "\n"
+#define MAX_LINE_LENGTH 70
+
+static int
+last_comma_or_clopar(string s )
+{
+    int last = 0;
+    int i = strlen(s)-1;
+    for (; i>0 && !last; i--)
+	if (s[i] == ',' || s[i]==')') 
+	    last = i;
+
+    return last;
+}
+
+bool 
+add_to_current_line(
+    string crt_line,   /* current line being processed. */
+    string add_string, /* string to add to this line. */
+    string str_prefix, /* prefix when breaking a line. */
+    text txt,          /* where to append complete lines. */
+    bool first_line    /* rather useless??? */)
+{
+    int lcrt_line=0;
+    int ladd_string=0;
+    int last_virg=0;
+    bool divide=FALSE;
+
+    if((lcrt_line = strlen(crt_line)) + (ladd_string=strlen(add_string)) 
+       > MAX_LINE_LENGTH-2)
+    {   
+	char tmp[MAX_LINE_LENGTH];
+
+	if(first_line)
+	{
+	    first_line = FALSE;
+	    if(get_bool_property("PRETTYPRINT_FOR_FORESYS")) /* ??? */
+	    {
+		str_prefix = FORESYS_CONTINUATION_PREFIX; /* ??? */
+	    }
+	}
+
+	last_virg = last_comma_or_clopar(crt_line);
+	divide = (last_virg > 0)
+	    && (last_virg !=lcrt_line-1) 
+	    &&  (lcrt_line -last_virg +ladd_string < MAX_LINE_LENGTH-2);
+
+	if (divide) 
+	{
+	    /* only to remain coherent with the last validation. CA.
+	     */
+	    if (crt_line[last_virg+1]==' ')
+		last_virg++;
+
+	    strcpy(tmp, crt_line+last_virg+1); /* save the end of the line */
+	    crt_line[last_virg+1] = '\0';      /* trunc! */
+	}
+	
+	strcat(crt_line, LINE_SUFFIX);
+	ADD_SENTENCE_TO_TEXT
+	    (txt, make_sentence(is_sentence_formatted, strdup(crt_line)));
+
+	/* now regenera the beginning of the line */
+	strcpy(crt_line, str_prefix);
+	strcat(crt_line, "    "); /* ??? should be in str_prefix... */
+	if (divide) 
+	    strcat(crt_line, tmp); /* get back saved part */
+    }
+
+    if(strlen(crt_line) + strlen(add_string) > MAX_LINE_LENGTH-2)
+	/* this may happen the truncation is not large enough for add_string? */
+	pips_internal_error("line buffer too small");
+
+    strcat(crt_line, add_string);
+    return first_line;
 }
