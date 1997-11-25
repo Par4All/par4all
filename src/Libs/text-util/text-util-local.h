f6bca88a4b40b1189cc0a3f9d8aa3eb845902fcf
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-11-25 08:22:56 +0000

    # on 1997/11/25 08:22:56, coelho did: cleaner...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8587 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/text-util/text-util-local.h
+++ src/Libs/text-util/text-util-local.h
@@ -2,6 +2,7 @@
  * $Id$
  */
 #define PIPS_COMMENT_PREFIX "C$PIPS"
+#define FORESYS_CONTINUATION_PREFIX "C$&"
 
 #define MAKE_SWORD(s) strdup(s)
 #define MAKE_IWORD(i) i2a(i)
@@ -15,15 +16,11 @@
     make_sentence(is_sentence_unformatted, \
 		  make_unformatted((char *) NULL, 0, m, CHAIN_SWORD(NIL, s)))
 
-#define ADD_SENTENCE_TO_TEXT(t,p) { \
-       text _t_ = (t); \
-       text_sentences(_t_) = \
-	   gen_nconc(text_sentences(_t_), CONS(SENTENCE, (p), NIL));\
-       }
+#define ADD_SENTENCE_TO_TEXT(t,p) { text _t_ = (t); \
+ text_sentences(_t_)=gen_nconc(text_sentences(_t_), CONS(SENTENCE, (p), NIL));}
 
 #define MERGE_TEXTS(r,t) { \
-       text _r_ = (r); \
-       text _t_ = (t); \
+       text _r_ = (r); text _t_ = (t); \
        text_sentences(_r_) = \
 	   gen_nconc(text_sentences(_r_), text_sentences(_t_)); \
        text_sentences(_t_) = NIL; \
@@ -31,7 +28,6 @@
        }
 
 /* maximum length of a line when prettyprinting...
+ * from 0 to 69, i.e. 70 chars, plus "\n\0" 
  */
-#define MAX_LINE_LENGTH 71 /* from 0 to 69, i.e. 70 chars, plus '\0' */
-
-#define FORESYS_CONTINUATION_PREFIX "C$&"
+#define MAX_LINE_LENGTH 72
