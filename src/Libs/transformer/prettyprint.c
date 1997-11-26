5dd243d903690efca90b1915683a8fcf28cff572
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-11-26 10:52:59 +0000

    # on 1997/11/26 10:52:59, coelho did: obsolete prettyprint functions partially dropped.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8605 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformer/prettyprint.c
+++ src/Libs/transformer/prettyprint.c
@@ -22,75 +22,19 @@
 #include "properties.h"
 
 #include "transformer.h"
-extern void qsort();
 
-
-/* This function is not declred any where else.
- * It is used to sort arguments preconditions in 
- * arguments_to_string  ( qsort).
- */
-static int wordcmp(s1,s2)
-char **s1, **s2;
+string transformer_to_string(transformer tf)
 {
-    return strcmp(*s1,*s2);
+    pips_internal_error("not implemenented anymore\n");
+    return string_undefined;
 }
-
-
-
-
-string transformer_to_string(tf)
-transformer tf;
-{
-    static char buffer[2048];
-    boolean a_la_fortran = get_bool_property("PRETTYPRINT_FOR_FORESYS");
-
-    buffer[0] = '\0';
-
-    if(tf==transformer_undefined)
-	(void) strcat(buffer, " TRANSFORMER: TRANSFORMER_UNDEFINED\n");
-    else {
-	(void) strcat(buffer, "T");
-	(void) arguments_to_string(buffer, transformer_arguments(tf));
-	if (a_la_fortran)
-	    (void) strcat(buffer, ",");
-	(void) strcat(buffer, " ");
-	(void) relation_to_string(buffer,
-				  (Psysteme) predicate_system(transformer_relation(tf)),
-				  pips_user_value_name);
-
-    }
-    pips_assert("transformer_to_string", strlen(buffer)<2047);
-    
-    return strdup(buffer);
-}
-
-
 
 string precondition_to_string(pre)
 transformer pre;
 {
-    static char buffer[2048];
-    boolean a_la_fortran = get_bool_property("PRETTYPRINT_FOR_FORESYS");
-
-    buffer[0] = '\0';
-
-    if(pre==transformer_undefined)
-	(void) strcat(buffer, " PRECONDITION: TRANSFORMER_UNDEFINED\n");
-    else {
-	(void) strcat(buffer, "P");
-	(void) arguments_to_string(buffer, transformer_arguments(pre));
-	if (a_la_fortran)
-	    (void) strcat(buffer, ",");
-	(void) strcat(buffer, " ");
-	(void) relation_to_string(buffer,
-				  (Psysteme) predicate_system(transformer_relation(pre)),
-				  pips_user_value_name);
-    };
-    
-    pips_assert("precondition_to_string", strlen(buffer)<2047);
-    return strdup(buffer);
+    pips_internal_error("not implemenented anymore\n");
+    return string_undefined;
 }
-
 
 /*
  * Modification : 92 08 26  Arnauld
@@ -98,142 +42,23 @@ transformer pre;
  * Now, the function also sorts entity_local_name .
  *
  */
-
-string arguments_to_string(s, args)
-string s;
-cons * args;
+string arguments_to_string(string s, list args)
 {
-    int j=0, provi_length = 1;
-    char *provi[100];
-
-    strcat(s, "(");
-    if(ENDP(args));
-    else {
-	MAPL(c, {entity e = ENTITY(CAR(c));
-		 if (e==entity_undefined) 
-			provi[j] = (char*) "entity_undefined";
-		 else
-			provi[j] = (char*) entity_local_name(e);
-		 j++;
-	     },
-	     args);
-	provi_length = j;
-    
-	qsort(provi, provi_length, sizeof provi[0], wordcmp);
-	if ( provi_length > 1 ) {
-	    for (j=0; j < provi_length-1; j++) {
-		strcat(s, provi[j]);
-		strcat(s,",");
-	    }
-	};
-	strcat(s, provi[provi_length-1]);
-    }
-    strcat(s, ")");
-    return s;
+    pips_internal_error("not implemenented anymore\n");
+    return string_undefined;
 }
 
-
-
-static string (*transformer_variable_name)(entity) = NULL;
-
-/* The strange argument type is required by qsort(), deep down in the calls */
-static int is_inferior_pvarval(Pvecteur * pvarval1, Pvecteur * pvarval2)
+string 
+relation_to_string(
+    string s,
+    Psysteme ps,
+    char * (*variable_name)(entity))
 {
-    /* The constant term is given the highest weight to push constant
-       terms at the end of the constraints and to make those easy
-       to compare. If not, constant 0 will be handled differently from
-       other constants. However, it would be nice to give constant terms
-       the lowest weight to print simple constraints first...
-
-       Either I define two comparison functions, or I cheat somewhere else.
-       Let's cheat? */
-    int is_equal = 0;
-    
-    if (term_cst(*pvarval1) && !term_cst(*pvarval2))
-	is_equal = 1;
-    else if (term_cst(*pvarval1) && term_cst(*pvarval2))
-	is_equal = 0;
-    else if(term_cst(*pvarval2))
-	is_equal = -1;
-    else
-	is_equal = 
-	    strcmp(transformer_variable_name((entity) vecteur_var(*pvarval1)),
-		   transformer_variable_name((entity) vecteur_var(*pvarval2)));
-
-    return is_equal; 
+    pips_internal_error("not implemenented anymore\n");
+    return string_undefined;
 }
 
-
-
-string relation_to_string(s, ps, variable_name)
-string s;
-Psysteme ps;
-char * (*variable_name)(entity);
-{
-    Pcontrainte peq;
-    boolean a_la_fortran = get_bool_property("PRETTYPRINT_FOR_FORESYS");
-    
-    transformer_variable_name = variable_name;
-
-    if (ps != NULL) {
-	bool first = TRUE;
-
-	sc_lexicographic_sort(ps, is_inferior_pvarval);
-
-	if (!a_la_fortran)
-	    (void) strcat(s, "{");
-
-	for (peq = ps->egalites;peq!=NULL; peq=peq->succ) {
-	    if(first)
-		first = FALSE;
-	    else
-		switch (a_la_fortran) {
-		case FALSE :
-		    (void) sprintf(s+strlen(s),", ");
-		    break;
-		case TRUE : 
-		    (void) sprintf(s+strlen(s),".AND.");
-		    break;
-		}
-	    if (a_la_fortran)
-		(void) sprintf(s+strlen(s),"(");
-	    egalite_sprint_format(s,peq,variable_name, a_la_fortran);
-	    if (a_la_fortran)
-		(void) sprintf(s+strlen(s),")");
-	}
-
-	for (peq = ps->inegalites;peq!=NULL; peq=peq->succ) {
-	    if(first)
-		first = FALSE;
-	    else
-		switch (a_la_fortran) {
-		case FALSE :
-		    (void) sprintf(s+strlen(s),", ");
-		    break;
-		case TRUE : 
-		    (void) sprintf(s+strlen(s),".AND.");
-		    break;
-		}
-	    if (a_la_fortran)
-		(void) sprintf(s+strlen(s),"(");
-	    inegalite_sprint_format(s,peq,variable_name, a_la_fortran);
-	    if (a_la_fortran)
-		(void) sprintf(s+strlen(s),")");
-	}
-
-	if (!a_la_fortran)
-	    (void) strcat(s,"}");
-    }
-    else
-	(void) strcat(s,"SC_UNDEFINED");
-    
-    transformer_variable_name = NULL;
-
-    return s;
-}
-
-char * pips_user_value_name(e)
-entity e;
+char * pips_user_value_name(entity e)
 {
     if(e == (entity) TCST) {
 	return "";
