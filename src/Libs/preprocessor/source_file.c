c89400f5b2f7f61834fa5dfdd701970184fd5894
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1997-12-05 11:05:55 +0000

    # on 1997/12/05 11:05:55, coelho did: some functions moved back to top-level.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@8675 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/preprocessor/source_file.c
+++ src/Libs/preprocessor/source_file.c
@@ -35,10 +35,10 @@
 #include "phases.h"
 
 #include "property.h"
-#include "pipsmake.h"
+/* #include "pipsmake.h" */
 #include "pipsdbm.h"
 
-#include "top-level.h"
+#include "preprocessor.h"
 
 #define skip_line_p(s) \
   ((*(s))=='\0' || (*(s))=='!' || (*(s))=='*' || (*(s))=='c' || (*(s))=='C')
@@ -128,49 +128,6 @@ pips_change_directory(char *dir)
     }
 
     return NULL;
-}
-
-/* returns the allocated full path name 
- */
-static char *
-get_view_file(char * print_type, bool displayable)
-{
-   char * module_name = db_get_current_module_name();
-
-   if(displayable && !displayable_file_p(print_type)) {
-       pips_user_error("resource %s cannot be displayed\n", print_type);
-   }
-
-   if(module_name != NULL)
-   {
-      if ( safe_make(print_type, module_name) ) 
-      {
-         char *file_name = db_get_file_resource(print_type, module_name, TRUE);
-	 char *pgm_wd = build_pgmwd(db_get_current_workspace_name());
-	 char *file_name_in_database = strdup(
-	     concatenate(pgm_wd, "/", file_name, NULL));
-
-	 free(pgm_wd); 
-         return file_name_in_database;
-      }
-   }
-   else {
-      /* should be show_message */
-      user_log("No current module; select a module\n");
-   }
-   return NULL;
-}
-
-char *
-build_view_file(char * print_type)
-{
-    return get_view_file(print_type, TRUE);
-}
-
-char *
-get_dont_build_view_file(char * print_type)
-{
-    return get_view_file(print_type, FALSE);
 }
 
 /********************************************************* PIPS SOURCE PATH */
