96c9bed2bd08077a3135edcf782299cc559d79cf
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1998-04-11 13:06:09 +0000

    # on 1998/04/11 13:06:09, coelho did: inclusion of typical C3/Linear header files.
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@907 8716bff9-3cf5-0310-8878-b27a67f92df8

---
+++ src/arithmetique/linear.h
@@ -0,0 +1,20 @@
+/*
+ * $Id$
+ *
+ * includes what is typically needed from the C3/Linear library.
+ *
+ * $Log: linear.h,v $
+ * Revision 1.1  1998/04/11 13:06:09  coelho
+ * Initial revision
+ *
+ */
+
+#if !defined(LINEAR_H_INCLUDED)
+#define LINEAR_H_INCLUDED
+
+#include "boolean.h"
+#include "vecteur.h"
+#include "contrainte.h"
+#include "sc.h"
+
+#endif /* LINEAR_H_INCLUDED */
