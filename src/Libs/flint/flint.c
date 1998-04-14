24cd1041123e894eef900820594ccf28d0a4f79a
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-04-14 20:26:56 +0000

    # on 1998/04/14 20:26:56, coelho did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9129 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/flint/flint.c
+++ src/Libs/flint/flint.c
@@ -10,25 +10,8 @@
  */
 
 /*************************************************************/
-/* system includes */
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
-#include <stdlib.h>
 
-/* PiPs specific headers */
-#include "genC.h"
-#include "linear.h"
-#include "ri.h"
-#include "database.h"
-
-#include "misc.h"
-#include "ri-util.h"
-#include "graph.h"
-#include "flint.h"
-#include "effects-generic.h"
-#include "pipsdbm.h"
-#include "resources.h"
+#include "local.h"
 
 /* internal variables */
 static FILE    *flint_messages_file = NULL;
