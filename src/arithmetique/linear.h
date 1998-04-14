31e4f479b04577ee3ddd5ecd2a04b15616c2dc59
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
1998-04-14 08:55:39 +0000

    # on 1998/04/14 08:55:39, coelho did: more includes.
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@909 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/arithmetique/linear.h
+++ src/arithmetique/linear.h
@@ -4,17 +4,28 @@
  * includes what is typically needed from the C3/Linear library.
  *
  * $Log: linear.h,v $
+ * Revision 1.2  1998/04/14 08:55:39  coelho
+ * more includes.
+ *
  * Revision 1.1  1998/04/11 13:06:09  coelho
  * Initial revision
  *
  */
 
-#if !defined(LINEAR_H_INCLUDED)
-#define LINEAR_H_INCLUDED
+#if !defined(C3_LINEAR_H_INCLUDED)
+#define C3_LINEAR_H_INCLUDED
 
 #include "boolean.h"
+#include "arithmetique.h"
 #include "vecteur.h"
 #include "contrainte.h"
 #include "sc.h"
+#include "sommet.h"
+#include "ray_dte.h"
+#include "matrix.h"
+#include "sg.h"
+#include "polyedre.h"
+#include "union.h"
+#include "polynome.h"
 
-#endif /* LINEAR_H_INCLUDED */
+#endif /* C3_LINEAR_H_INCLUDED */
