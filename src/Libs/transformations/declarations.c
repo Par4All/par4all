e1d062c9bc04a2ccaf7a865dbae2075a146d0d78
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-04-14 21:30:42 +0000

    # on 1998/04/14 21:30:42, coelho did: linear.h
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9214 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformations/declarations.c
+++ src/Libs/transformations/declarations.c
@@ -2,6 +2,9 @@
  * $Id$
  * 
  * $Log: declarations.c,v $
+ * Revision 1.4  1998/04/14 21:30:42  coelho
+ * linear.h
+ *
  * Revision 1.3  1997/10/24 16:38:15  coelho
  * set/reset current module entity.
  *
@@ -26,6 +29,7 @@
 
 #include <stdio.h>
 #include "genC.h"
+#include "linear.h"
 #include "ri.h"
 #include "ri-util.h"
 #include "resources.h"
