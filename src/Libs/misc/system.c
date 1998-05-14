2c5f3624f2b81aa5617acff0feeedea8c695fee1
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-05-14 11:44:57 +0000

    # on 1998/05/14 11:44:57, coelho did: as little bit nicer.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9367 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/misc/system.c
+++ src/Libs/misc/system.c
@@ -1,5 +1,5 @@
 /* $RCSfile: system.c,v $ version $Revision$
- * ($Date: 1997/05/22 11:16:34 $, )
+ * ($Date: 1998/05/14 11:44:57 $, )
  *
  * a safe system call. abort if fails.
  * FC 09/95
@@ -11,9 +11,7 @@
 #include "genC.h"
 #include "misc.h"
 
-void
-safe_system(
-    string command) /* the command to be executed */
+void safe_system(string command) /* the command to be executed */
 {
     int status = system(command);
     
@@ -22,23 +20,21 @@ safe_system(
 			    (status/0x100) & 0xff, status & 0xff, command);
 }
 
-int
-safe_system_no_abort(
-    string command) /* the command to be executed */
+int safe_system_no_abort(string command) /* the command to be executed */
 {
     int status = system(command);
     
     if (status == 127)
-	pips_error("safe_system", "Could not execute : '%s'\n", command);
+	pips_internal_error("Could not execute : '%s'\n", command);
 
     if (status) {
 	/* For portability reasons, do not use pips_user_warning() here */
-	user_warning("safe_system_no_abort", 
-		     "Failed (ret: %d, sig: %d) for '%s'\n", 
-		     (status/0x100) & 0xff, status & 0xff, command);
+	pips_user_warning("Failed (ret: %d, sig: %d) for '%s'\n", 
+			  (status/0x100) & 0xff, status & 0xff, command);
     }
 
     return (status / 256) & 255;
 }
+
 /* that is all
  */
