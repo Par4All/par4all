8a219fcc0452202cf1ee7061cedc7d5c93712519
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-26 08:10:25 +0000

    # on 1998/06/26 08:10:25, coelho did: flush added.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9463 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/tpips/wrapper.c
+++ src/Passes/tpips/wrapper.c
@@ -26,6 +26,9 @@
  * @author Fabien Coelho (with some advises from Ronan Keryell)
  *
  * $Log: wrapper.c,v $
+ * Revision 1.4  1998/06/26 08:10:25  coelho
+ * flush added.
+ *
  * Revision 1.3  1998/06/26 08:01:01  coelho
  * additionnal message added.
  *
@@ -131,6 +134,7 @@ static void tpw_sig_handler(int sn /* signal number */)
     case SIGCHLD: /* tpips stopped. */
     case SIGPIPE: /* idem? */
 	fprintf(stderr, WRAPPER "killing tpips...\n");
+	fflush(stderr);
 	KILL(tpips_pid, SIGKILL);
 	exit(2);
 	break;
