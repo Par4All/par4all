6c613bca78bbf0f1da047e2ae9b43bb4105e299e
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-30 17:35:33 +0000

    # on 1998/06/30 17:35:33, coelho did: a graphical and logical component in jpips.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9491 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/JPipsComponent.java
+++ src/Scripts/jpips/JPipsComponent.java
@@ -1,13 +1,16 @@
-/**
-  * $Id$ 
-  *
-  * $Log: JPipsComponent.java,v $
-  * Revision 1.2  1998/06/30 16:22:26  coelho
-  * cleaner (wrt my standards).
-  *
-  * Revision 1.1  1998/06/09 07:28:28  didry
-  * Initial revision
-  */
+/*
+ * $Id$ 
+ *
+ * $Log: JPipsComponent.java,v $
+ * Revision 1.3  1998/06/30 17:35:33  coelho
+ * a graphical and logical component in jpips.
+ *
+ * Revision 1.2  1998/06/30 16:22:26  coelho
+ * cleaner (wrt my standards).
+ *
+ * Revision 1.1  1998/06/09 07:28:28  didry
+ * Initial revision
+ */
 
 package JPips;
 
@@ -15,12 +18,13 @@ import JPips.Pawt.*;
 import java.awt.*;
 
 /** An interface that defines methods for JPips components.
+  *
   * @author Francois Didry
   */
 public interface JPipsComponent
+  extends Activatable
 {
   public Component getComponent();
   public PMenu getMenu();
-  public void setActivated(boolean yes);
   public void reset();
 }
