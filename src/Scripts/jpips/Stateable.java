e47b66334f3e26fc2c2e9adb5aa02eda36fb6bbd
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-30 17:34:50 +0000

    # on 1998/06/30 17:34:50, coelho did: something with an internal state.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9482 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Scripts/jpips/Stateable.java
@@ -0,0 +1,19 @@
+/**
+  * $Id$
+  * 
+  * A component which has a state field
+  *
+  * $Log: Stateable.java,v $
+  * Revision 1.1  1998/06/30 17:34:50  coelho
+  * Initial revision
+  *
+  */
+
+package JPips;
+
+import java.util.Vector;
+
+public interface Stateable
+{
+    public Vector getState();
+}
