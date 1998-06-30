e9b04372b7d9560c8c5ef0dbac0e47f960e94378
didry <didry@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-30 14:57:56 +0000

    # on 1998/06/30 14:57:56, didry did: do not manage the panel anymore
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9470 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/Displayer.java
+++ src/Scripts/jpips/Displayer.java
@@ -2,6 +2,9 @@
 
 /** $Id$
   * $Log: Displayer.java,v $
+  * Revision 1.2  1998/06/30 14:57:56  didry
+  * do not manage the panel anymore
+  *
   * Revision 1.1  1998/06/09 07:28:28  didry
   * Initial revision
   *
@@ -11,23 +14,25 @@
 package JPips;
 
 
+import java.awt.swing.*;
 import java.io.*;
 import java.awt.*;
 import JPips.Pawt.*;
 import java.awt.swing.border.*;
+import java.util.*;
 
 
 /** A window manager.
   * It manages the displaying of windows on the screen.
   * @author Francois Didry
   */
-abstract class Displayer
+abstract class Displayer implements JPipsComponent
 {
 
 
-  private int		noWindows;	//number of displayed windows
-  private PPanel	panel;		//panel of the displayer
-  private PLabel	labelNo;	//
+  public  Vector	frameVector;	//contains the displayed windows
+  public  int		noWindows;	//number of displayed windows
+  public  PPanel	panel;		//panel of the displayer
 
 
   /** Sets the number of displayed windows to 0.
@@ -36,30 +41,34 @@ abstract class Displayer
   public Displayer()
     {
       noWindows = 0;
-      panel = new PPanel(new BorderLayout());
-      panel.setPreferredSize(new Dimension(80,40));
-      panel.setBorder(new TitledBorder("Windows"));
-      labelNo = new PLabel();
-      updatePanel();
-      panel.add(labelNo, BorderLayout.CENTER);
+      frameVector = new Vector();
+      buildPanel();
     }
   
   
-  /** Adds a window to the count and updates the textfield.
+  /** Builds the panel for JPips.
+    */
+  public void buildPanel()
+    {
+      panel = new PPanel(new BorderLayout());
+      panel.setPreferredSize(new Dimension(300,150));      
+      panel.setBorder(new TitledBorder("Windows"));
+    }
+
+
+  /** Adds a window to the count.
     */
   public void incNoWindows()
     {
       noWindows++;
-      updatePanel();
     }
 
 
-  /** Removes a window from the count and updates the textfield.
+  /** Removes a window from the count.
     */
   public void decNoWindows()
     {
       noWindows--;
-      updatePanel();
     }
 
 
@@ -71,26 +80,24 @@ abstract class Displayer
     }
 
 
-  /** @return the panel for JPips
+  /** @return the displayer panel for JPips
     */
-  public PPanel getPanel()
+  public Component getComponent()
     {
       return panel;
     }
 
 
-  /** Updates the number of displayed windows in the panel.
-    */
-  public void updatePanel()
-    {
-      labelNo.setText(noWindows+"");
-    }
-
-
-  abstract void display(File file, boolean locked, boolean writable);
+  abstract boolean display(File file, boolean locked, boolean writable);
 
   abstract void display(String name, String string,
                         boolean locked, boolean writable);
+
+  public PMenu getMenu() { return null; }
+  
+  public void setActivated(boolean yes) {}
+
+  public void reset() {}
 
 
 }
