f0c1d9769bee89d4fb2968d377f51e5c4a27b25e
didry <didry@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-30 15:03:02 +0000

    # on 1998/06/30 15:03:02, didry did: manages a new panel
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9472 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/TextDisplayer.java
+++ src/Scripts/jpips/TextDisplayer.java
@@ -2,6 +2,9 @@
 
 /** $Id$
   * $Log: TextDisplayer.java,v $
+  * Revision 1.2  1998/06/30 15:03:02  didry
+  * manages a new panel
+  *
   * Revision 1.1  1998/06/09 07:28:28  didry
   * Initial revision
   *
@@ -10,6 +13,8 @@
 
 package JPips;
 
+import java.applet.*;
+import java.net.*;
 import java.util.*;
 import java.io.*;
 import JPips.Pawt.*;
@@ -26,40 +31,77 @@ public class TextDisplayer extends Displayer
 {
 
 
-public	Vector	frameVector;
+  public	Vector	infos;		//contains the extensions
+  public	PFrame	frame;		//frame of jpips
+  
 
-
-/** Sets the maximum number of displayed windows.
-  * Sets the number of displayed windows to 0.
-  */
-  public TextDisplayer()
+  /** Sets the number of displayed windows to 0.
+    * Creates the vector of windows.
+    */
+  public TextDisplayer(PFrame frame)
     {
       super();
-      frameVector = new Vector();
+      this.frame = frame;
+      setInfos();
     }
 
 
-  /** Displays a frame that contains a text file.
+  /** Sets the extension info table.
     */
-  public void display(File file, boolean locked, boolean writable)
+  public void setInfos()
     {
-      PTextFrame f = getFreeFrame();
-      if(f != null)
-        {
-	  f.setTitle(file.getName());
-	  f.ta.setText(getText(file));
-	  f.locked = locked;
-	  f.writable = writable;
-	}
-      else
-        {
-	  f = new PTextFrame(file.getName(), getText(file), locked, writable);
-          register(f);
-          f.setVisible(true);
-	}
+      infos = new Vector();
+      infos.addElement(new Extension("pref","basic sequential view"));
+      infos.addElement(new Extension("tran","transformers sequential view"));
+      infos.addElement(new Extension("prec","preconditions sequential view"));
+      infos.addElement(new Extension("reg","regions sequential view"));
+      infos.addElement(new Extension("parf","parallel view"));
+      infos.addElement(new Extension("icfg","basic ICFG view"));
+      infos.addElement(new Extension("icfgl","loops ICFG view"));
+      infos.addElement(new Extension("icfgc","control ICFG view"));
     }
 
 
+  /** Displays a text frame from a text file.
+    */
+  public boolean display(File file, boolean locked, boolean writable)
+    {
+      String title = getInfo(file)+" : "+file.getName();
+      for(int i=0; i<frameVector.size(); i++)
+        {
+	  PTextFrame f = (PTextFrame)frameVector.elementAt(i);
+	  if(title.equals(f.getTitle()))
+	    {
+	      f.setVisible(true);
+	      f.toFront();
+	      return false;
+	    }
+	}
+      PTextFrame f = getFreeFrame();
+      String text = getText(file);
+      if(text != null)
+        {
+          if(f != null)
+            {
+	      f.setTitle(title);
+	      f.ta.setText(text);
+	      f.panelButton.setText(title);
+	      f.locked = locked;
+	      f.writable = writable;
+	    }
+          else
+            {
+	      f = new PTextFrame(title, text, locked, writable);
+              register(f);
+              f.setVisible(true);
+	    }
+        }
+      return true;
+    }
+
+
+  /** Displays a text frame from a string.
+    */
   public void display(String title, String text,
   		      boolean locked, boolean writable)
     {
@@ -68,6 +110,7 @@ public	Vector	frameVector;
         {
 	  f.setTitle(title);
 	  f.ta.setText(text);
+	  f.panelButton.setText(title);
 	  f.locked = locked;
 	  f.writable = writable;
 	}
@@ -80,14 +123,33 @@ public	Vector	frameVector;
     }
 
 
+  /** @return info from the specified file
+    */
+  public String getInfo(File file)
+    {
+      String name = file.getName();
+      int index = name.indexOf(".");
+      if(index == -1) return "Unknown file";
+      String extension = name.substring(index+1);
+      for(int i=0; i<infos.size(); i++)
+        {
+	  Extension e = (Extension)infos.elementAt(i);
+	  if(e.getExtension().equals(extension)) return e.getInfo();
+	}
+      return "Unknown file";
+    }
+
+
+  /** Adds a windowlistener to a window.
+    */
   public void register(PTextFrame f)
     {
       frameVector.addElement(f);
       WindowListener w = new WindowListener()
         {
-	  public void windowClosing(WindowEvent e) { decNoWindows(); }
-	  public void windowOpened(WindowEvent e) { incNoWindows(); }
-	  public void windowClosed(WindowEvent e) {}
+	  public void windowClosing(WindowEvent e) { removeWindow(e); }
+	  public void windowOpened(WindowEvent e) { addWindow(e); }
+	  public void windowClosed(WindowEvent e) { removeWindow(e); }
 	  public void windowActivated(WindowEvent e) {}
 	  public void windowDeactivated(WindowEvent e) {}
 	  public void windowIconified(WindowEvent e) {}
@@ -97,6 +159,29 @@ public	Vector	frameVector;
     }
 
 
+  /** Adds a window.
+    */
+  public void addWindow(WindowEvent e)
+    {
+      PTextFrame f = (PTextFrame)e.getSource();
+      incNoWindows();
+      updatePanel();
+    }
+
+
+  /** Removes a window.
+    */
+  public void removeWindow(WindowEvent e)
+    {
+      PTextFrame f = (PTextFrame)e.getSource();
+      decNoWindows();
+      frameVector.removeElement(f);
+      updatePanel();
+    }
+
+
+  /** @return a free window if possible
+    */
   public PTextFrame getFreeFrame()
     {
       for(int i=0; i<frameVector.size(); i++)
@@ -108,33 +193,105 @@ public	Vector	frameVector;
     }
 
 
-  public String getText(File f)
+  /** Closes all the windows.
+    */
+  public void closeAll()
     {
-      if(f.canRead())
+      for(int i=0; i<frameVector.size(); i++)
         {
-	  try
+	  PTextFrame f = (PTextFrame)frameVector.elementAt(i);
+	  f.dispose();
+	}
+    }
+
+
+  /** @return the text of a text file
+    */
+  public static String getText(File f)
+    {
+      String s = new String();
+      char[] buff = new char[50000];
+      InputStream is;
+      InputStreamReader reader;
+      try
+        {
+	  reader = new FileReader(f);
+	  int nch;
+	  while ((nch = reader.read(buff, 0, buff.length)) != -1)
 	    {
-	      RandomAccessFile raf = new RandomAccessFile(f,"r");
-	      String text = "";
-	      String l = raf.readLine();
-	      while(l != null)
-	        {
-	          text = text + l +"\n";
-		  l = raf.readLine();
-	        }
-              return text;
+	      s = s + new String(buff, 0, nch);
 	    }
-	  catch(FileNotFoundException e)
-	    {
-	      System.out.println(e);
-	    }
-	  catch(IOException e)
-	    {
-	      System.out.println(e);
-	    }
-        }
+	  return s;
+	}
+      catch (IOException e)
+        {
+	  System.out.println("Could not load file: " + f);
+	}
       return null;
     }
+
+
+  /** Updates the panel.
+    */
+  public void updatePanel()
+    {
+      panel.removeAll();
+      PPanel p = new PPanel(new GridBagLayout());
+      GridBagConstraints c = new GridBagConstraints();
+      c.gridx = 0;
+      c.insets = new Insets(0,0,0,0);
+      c.fill = GridBagConstraints.HORIZONTAL;
+      c.anchor = GridBagConstraints.NORTH;
+      for(int i=0; i<frameVector.size(); i++)
+        {
+	  c.gridy = i;
+          PTextFrame f = (PTextFrame)frameVector.elementAt(i);
+          p.add(f.panelButton,c);
+	}
+      PScrollPanel scrollPanel = new PScrollPanel((Component)p);
+      panel.add(scrollPanel);
+      frame.repaint();
+      frame.pack();
+    }
+
+
+/** A link between an extension and its representative string..
+  * @author Francois Didry
+  */
+static public class Extension
+{
+
+
+  public String		extension,	//extension 
+  			info;		//information about the extension
+
+
+  /** Sets the extension and its info..
+    */
+  public Extension(String extension, String info)
+    {
+      this.extension = extension;
+      this.info = info;
+    }
+
+
+  /** @return the extension.
+    */
+  public String getExtension()
+    {
+      return extension;
+    }
+
+
+  /** @return the info.
+    */
+  public String getInfo()
+    {
+      return info;
+    }
+
+
+}
 
 
 }
