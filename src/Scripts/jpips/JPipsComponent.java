433d0adcf18ecb8d9eeb168f196ebab0c4acc18b
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-30 16:22:26 +0000

    # on 1998/06/30 16:22:26, coelho did: cleaner (wrt my standards).
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9475 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/JPipsComponent.java
+++ src/Scripts/jpips/JPipsComponent.java
@@ -1,32 +1,26 @@
-
-
-/** $Id$ 
+/**
+  * $Id$ 
+  *
   * $Log: JPipsComponent.java,v $
+  * Revision 1.2  1998/06/30 16:22:26  coelho
+  * cleaner (wrt my standards).
+  *
   * Revision 1.1  1998/06/09 07:28:28  didry
   * Initial revision
-  *
   */
-
 
 package JPips;
 
-
 import JPips.Pawt.*;
 import java.awt.*;
-
 
 /** An interface that defines methods for JPips components.
   * @author Francois Didry
   */
 public interface JPipsComponent
 {
-
   public Component getComponent();
-
   public PMenu getMenu();
-  
   public void setActivated(boolean yes);
-
   public void reset();
-
 }
