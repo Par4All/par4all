dc7279510cadd42e7d4a23612b8bc751cff289ad
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-30 17:34:50 +0000

    # on 1998/06/30 17:34:50, coelho did: something that can be activated or not.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9481 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Scripts/jpips/Activatable.java
@@ -0,0 +1,17 @@
+/**
+  * $Id$
+  * 
+  * A component which can be activated or de-activated.
+  *
+  * $Log: Activatable.java,v $
+  * Revision 1.1  1998/06/30 17:34:50  coelho
+  * Initial revision
+  *
+  */
+
+package JPips;
+
+public interface Activatable
+{
+  public void setActivated(boolean yes);
+}
