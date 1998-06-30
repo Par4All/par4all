116f3e7b550ade21ad8574b3a988348022181a1c
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-30 16:28:36 +0000

    # on 1998/06/30 16:28:36, coelho did: cleaner (wrt my standards).
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9476 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/Resetable.java
+++ src/Scripts/jpips/Resetable.java
@@ -1,29 +1,20 @@
-
-
-/** $Id$ 
+/**
+  * $Id$ 
+  *
   * $Log: Resetable.java,v $
+  * Revision 1.2  1998/06/30 16:28:36  coelho
+  * cleaner (wrt my standards).
+  *
   * Revision 1.1  1998/06/30 14:56:43  didry
   * Initial revision
-  *
-  * Revision 1.1  1998/06/09 07:28:28  didry
-  * Initial revision
-  *
   */
 
-
 package JPips;
-
-
-import JPips.Pawt.*;
-import java.awt.*;
-
 
 /** An interface that that defines the reset of jpips.
   * @author Francois Didry
   */
 public interface Resetable
 {
-
   public void reset();
-
 }
