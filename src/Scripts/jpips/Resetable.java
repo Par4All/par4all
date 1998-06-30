267b1d6b6a50e283341a6a55dc0bf25ee35e4495
didry <didry@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-30 14:56:43 +0000

    # on 1998/06/30 14:56:43, didry did: abstract class for the components of JPips
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9469 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Scripts/jpips/Resetable.java
@@ -0,0 +1,29 @@
+
+
+/** $Id$ 
+  * $Log: Resetable.java,v $
+  * Revision 1.1  1998/06/30 14:56:43  didry
+  * Initial revision
+  *
+  * Revision 1.1  1998/06/09 07:28:28  didry
+  * Initial revision
+  *
+  */
+
+
+package JPips;
+
+
+import JPips.Pawt.*;
+import java.awt.*;
+
+
+/** An interface that that defines the reset of jpips.
+  * @author Francois Didry
+  */
+public interface Resetable
+{
+
+  public void reset();
+
+}
