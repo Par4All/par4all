3d73be24f7ed5658e6ff4a7270bec01743214c95
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-06-30 16:40:14 +0000

    # on 1998/06/30 16:40:14, coelho did: awt abstraction for JPips.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9477 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Scripts/jpips/Pawt.java
@@ -0,0 +1,289 @@
+/**
+  * $Id$
+  * 
+  * $Log: Pawt.java,v $
+  * Revision 1.1  1998/06/30 16:40:14  coelho
+  * Initial revision
+  *
+  */
+
+package JPips;
+
+import java.util.*;
+import java.awt.*;
+import java.awt.swing.*;
+import java.awt.event.*;
+import java.awt.swing.event.*;
+
+/** A graphical package for JPips
+  * All components extends from swing components.
+  * Some components contain informations to manage their Events.
+  * 
+  * @author Francois Didry
+  */
+interface Pawt
+{
+  class PComponent extends java.awt.swing.JComponent
+    { public String checking; PComponent(){ super(); } }
+
+  class PFrame extends java.awt.swing.JFrame
+    { 
+      public Vector optionVector;
+      PFrame(String name){ super(name); }
+      
+      /** Disables or enables the Option objects of JPips.
+       * @param yes true means disable
+       */
+      public void lock(boolean yes)
+        {
+          for(int i=1; i<optionVector.size(); i++)
+	      ((Option)optionVector.elementAt(i)).setActivated(!yes);
+        }
+    }
+
+  class PDialog extends java.awt.swing.JDialog
+    { PDialog(Frame f, String s, boolean b){ super(f, s, b); } }
+
+  class PPanel extends java.awt.swing.JPanel
+    {
+      PPanel(){ super(); }
+      PPanel(LayoutManager l){ super(l); }
+    }
+
+  class PMenuBar extends java.awt.swing.JMenuBar
+    { PMenuBar(){ super(); } }
+
+  class PMenu extends java.awt.swing.JMenu
+    { PMenu(){ super(); }
+      PMenu(String name){ super(name); }
+    }
+
+  class PMenuItem extends java.awt.swing.JMenuItem
+    {
+      public String command;
+      public PTextField tf;
+      PMenuItem(String name){ super(name); }
+      PMenuItem(String name,String command)
+        { super(name); this.command = command; }
+      PMenuItem(String name,String command,PTextField tf)
+        { super(name); this.command = command; this.tf = tf; }
+    }
+
+  class PCheckBox extends java.awt.swing.JCheckBox
+    {
+      public String command,checking;
+      public PCheckBoxMenuItem cbmi;
+      public PTextFrame frame;
+      PCheckBox(String name){ super(name); }	
+      PCheckBox(String name,String command,String checking)
+        { super(name); this.command = command; this.checking = checking; }	
+      PCheckBox
+        (String name,String command,String checking,PCheckBoxMenuItem cbmi)
+        { super(name); this.command = command; this.checking = checking;
+	  this.cbmi = cbmi; }	
+      PCheckBox(String name,PTextFrame frame)
+        { super(name); this.frame = frame; }	
+    }
+
+  class PCheckBoxMenuItem extends java.awt.swing.JCheckBoxMenuItem
+    {
+      public String command,checking;
+      public PCheckBox cb;
+
+      PCheckBoxMenuItem(String name){ super(name); }
+
+      PCheckBoxMenuItem(String name,String command,String checking)
+      { 
+	super(name);
+	this.command = command;
+	this.checking = checking; 
+      }
+
+      PCheckBoxMenuItem(String name, String command, 
+			String checking, PCheckBox cb)
+      {
+	super(name); 
+	this.command = command; 
+	this.checking = checking;
+	this.cb = cb; 
+      }
+    }
+
+  class PRadioButtonMenuItem extends java.awt.swing.JRadioButtonMenuItem
+    {
+      public String command,
+                    checking;
+      public PLabel label;
+      public PComboBox cob;
+      public Object o;
+      public PRadioButtonMenuItem rbmi;
+      PRadioButtonMenuItem(String name){ super(name); }
+      PRadioButtonMenuItem(String name, String command)
+        { super(name); this.command = command; }
+      PRadioButtonMenuItem(String name, String command, PLabel label,
+                           PRadioButtonMenuItem rbmi)
+        { super(name); this.command = command; this.label = label;
+	  this.rbmi = rbmi; }
+      PRadioButtonMenuItem(String name, String command, PLabel label,
+                           PRadioButtonMenuItem rbmi, String checking)
+        { super(name); this.command = command; this.label = label;
+	  this.rbmi = rbmi; this.checking = checking; }
+      PRadioButtonMenuItem(String name, String command,
+                           PComboBox cob, Object o)
+        { super(name); this.command = command; this.cob = cob; this.o = o; }
+      PRadioButtonMenuItem(String name, String command,
+                           PComboBox cob, Object o, String checking)
+        { super(name); this.command = command; this.cob = cob; this.o = o;
+	  this.checking = checking; }
+    }
+
+
+  class PSeparator extends java.awt.swing.JSeparator
+    { PSeparator(){ super(); } }
+
+
+  class PButton extends java.awt.swing.JButton
+    {
+      public String command;
+      public PTextField tf;
+      public PTextFrame frame;
+      PButton(){ super(); }      
+      PButton(String name){ super(name); setMargin(new Insets(0,0,0,0)); }
+      PButton(String name, PTextFrame frame)
+        { super(name); setMargin(new Insets(0,0,0,0)); this.frame = frame; }
+      PButton(String name, String command)
+        { super(name); setMargin(new Insets(0,0,0,0)); this.command = command; }
+      PButton(String name, String command, PTextField tf)
+        { super(name); setMargin(new Insets(0,0,0,0)); this.command = command;
+	  this.tf = tf; }
+    }
+
+    
+  class PLabel extends java.awt.swing.JLabel
+    {
+      PLabel(){ super(); }
+      PLabel(String s){ super(s); }
+    }
+
+    
+  class PTextField extends java.awt.swing.JTextField
+    {
+      PTextField(){ super(); }
+      PTextField(String s){ super(s); }
+    }
+
+    
+  class PList extends java.awt.swing.JList
+    {
+      PList(){ super(); }
+      PList(Vector v){ super(v); }
+      PList(DefaultListModel l){ super(l); }
+    }
+
+    
+  class PComboBox extends java.awt.swing.JComboBox
+    {
+      public String checking,
+      		    marker;
+      public Vector vCommand = new Vector(),
+                    vRbmi = new Vector(),
+		    vChecking = new Vector();
+      PComboBox(){ super(); }
+      PComboBox(String ch){ super(); checking = ch; }
+      PComboBox(String ch, String ma){ super(); checking = ch; marker = ma; }
+    }
+
+
+  class PScrollPanel extends java.awt.swing.JScrollPane
+    {
+      PScrollPanel(){ super(); } 
+      PScrollPanel(Component c){ super(c); } 
+    }
+        
+        
+  class PButtonGroup extends java.awt.swing.ButtonGroup
+    {
+      String checking;
+      PButtonGroup(String ch){ super(); checking = ch; }
+    }
+        
+        
+  class PTextArea extends java.awt.TextArea
+    {
+      PTextArea(){ super(); }
+      PTextArea(String s){ super(s); }
+    }
+        
+        
+  class PTextFrame extends java.awt.swing.JFrame
+    {
+      boolean locked;
+      boolean writable;
+      PTextArea ta;
+      PButton panelButton;
+            
+      PTextFrame(String name, String text, boolean locked, boolean writable)
+        {
+	  //window
+	  super(name);
+	  this.locked = locked;
+	  this.writable = writable;
+          PButton b;
+	  getContentPane().setLayout(new BorderLayout());
+	  ta = new PTextArea(text);
+          ta.setFont(new Font("Monospaced", Font.PLAIN, 12));
+          ta.setEnabled(writable);
+	  PScrollPanel s = new PScrollPanel((Component)ta);
+	  getContentPane().add(s, BorderLayout.CENTER);
+	  
+	  PPanel p = new PPanel(new GridLayout(1,3));
+	  PCheckBox cb = new PCheckBox("Locked",this);
+	  cb.setSelected(locked);
+	  cb.addActionListener(new ActionListener()
+            {
+	      public void actionPerformed(ActionEvent e)
+	        {
+		  PCheckBox check = (PCheckBox)e.getSource();
+		  check.frame.locked = check.isSelected();
+		}
+	    });
+	  p.add(cb);
+	  b = new PButton("Hide",this);
+	  b.addActionListener(new ActionListener()
+            {
+	      public void actionPerformed(ActionEvent e)
+	        {
+		  PButton button = (PButton)e.getSource();
+		  button.frame.setVisible(false);
+		}
+	    });
+	  p.add(b);
+	  b = new PButton("Close",this);
+	  b.addActionListener(new ActionListener()
+            {
+	      public void actionPerformed(ActionEvent e)
+	        {
+		  PButton button = (PButton)e.getSource();
+		  button.frame.dispose();
+		}
+	    });
+	  p.add(b);
+	  getContentPane().add(p, BorderLayout.SOUTH);
+	  
+	  //jpips button
+	  panelButton = new PButton(name,this);
+	  panelButton.addActionListener(new ActionListener()
+            {
+	      public void actionPerformed(ActionEvent e)
+	        {
+		  PButton button = (PButton)e.getSource();
+		  button.frame.setVisible(true);
+		  button.frame.toFront();
+		}
+	    });
+
+	  pack();
+	  setSize(new Dimension(600,400));
+	}
+    }
+}
