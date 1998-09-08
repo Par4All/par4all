6f8f0a81600a6b75c09959ffe2f9526e52572b82
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-09-08 18:28:48 +0000

    # on 1998/09/08 18:28:48, irigoin did: Several quick bug fixes. Not a really consistent state for the Fortran 90 output but we want to make the Production version of PIPS consistent for most outputs.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9631 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/fortran90.c
+++ src/Libs/ri-util/fortran90.c
@@ -18,21 +18,42 @@
 
 static set vectors ;
 
-expression update_range( f, r, lw, up, in ) 
+/* f is supposed to be a binary operator, not always commutative. left
+   means that range r appears on the left of f, i.e. as first operator. */
+
+expression update_range(f, r, lw, up, in, left)
 entity f ;
 range r ;
 expression lw, up, in ;
+bool left;
 {
     range new_r = make_range(range_lower(r),
 			     range_upper(r),
 			     range_increment(r)) ;
-    range_lower( new_r ) = MakeBinaryCall(f, lw, range_lower(r)) ;
-    range_upper( new_r ) = MakeBinaryCall(f, up, range_upper(r)) ;
+
+    if(left) {
+	range_lower( new_r ) = MakeBinaryCall(f, range_lower(r), lw);
+	range_upper( new_r ) = MakeBinaryCall(f, range_upper(r), up);
+    }
+    else {
+	range_lower( new_r ) = MakeBinaryCall(f, lw, range_lower(r));
+	range_upper( new_r ) = MakeBinaryCall(f, up, range_upper(r));
+    }
 	
     if( strcmp( entity_local_name( f ), MULTIPLY_OPERATOR_NAME ) == 0 ) {
+	/* expression "in" must be integer constant 1 */
+	pips_assert("This is a scalar value", lw==up);
 	range_increment( new_r ) = 
-		MakeBinaryCall(f, in, range_increment(r)) ;
+		MakeBinaryCall(f, in, range_lower(r)) ;
     }
+	
+    if( !left && (strcmp( entity_local_name( f ), MINUS_OPERATOR_NAME ) == 0) ) {
+	entity um = entity_intrinsic(UNARY_MINUS_OPERATOR_NAME);
+
+	range_increment( new_r ) = 
+		MakeUnaryCall(um, range_increment(r)) ;
+    }
+
     return( make_expression(make_syntax(is_syntax_range,new_r),
 			    normalized_undefined) ) ;
 }
@@ -44,65 +65,69 @@ list args ;
 {
     expression new_e ;
     bool vector_op = 
-	    (strcmp( entity_local_name( f ), PLUS_OPERATOR_NAME ) == 0 ||
-	     strcmp( entity_local_name( f ), MINUS_OPERATOR_NAME ) == 0 ||
-	     strcmp( entity_local_name( f ), DIVIDE_OPERATOR_NAME ) == 0 ||
-	     strcmp( entity_local_name( f ), MULTIPLY_OPERATOR_NAME ) == 0 ) ;
+	(strcmp( entity_local_name( f ), PLUS_OPERATOR_NAME ) == 0 ||
+	 strcmp( entity_local_name( f ), MINUS_OPERATOR_NAME ) == 0 ||
+	 /* strcmp( entity_local_name( f ), DIVIDE_OPERATOR_NAME ) == 0 || */
+	 strcmp( entity_local_name( f ), MULTIPLY_OPERATOR_NAME ) == 0 ) ;
 
     if( !vector_op ) {
 	return( make_expression(make_syntax(is_syntax_call,
 					    make_call( f, args )),
 				normalized_undefined)) ;
     }
-{
-    expression lhs = EXPRESSION(CAR(args)) ;
-    expression rhs = EXPRESSION(CAR(CDR(args))) ;
-    syntax ls = expression_syntax( lhs ) ;
-    syntax rs = expression_syntax( rhs ) ;
+    {
+	expression lhs = EXPRESSION(CAR(args)) ;
+	expression rhs = EXPRESSION(CAR(CDR(args))) ;
+	syntax ls = expression_syntax( lhs ) ;
+	syntax rs = expression_syntax( rhs ) ;
 
-    if(set_belong_p( vectors, (char *)lhs ) &&
-       set_belong_p( vectors, (char *)rhs )) {
-	if( syntax_range_p( ls ) && syntax_range_p( rs )) {
-	    range rl = syntax_range( ls ) ;
-	    range rr = syntax_range( rs ) ;
+	if(set_belong_p( vectors, (char *)lhs ) &&
+	   set_belong_p( vectors, (char *)rhs )) {
+	    if( syntax_range_p( ls ) && syntax_range_p( rs )) {
+		range rl = syntax_range( ls ) ;
+		range rr = syntax_range( rs ) ;
 
-	    new_e = update_range(f, rl, 
-				 range_lower(rr), range_upper(rr),
-				 range_increment(rr)) ;
+		new_e = update_range(f, rl, 
+				     range_lower(rr), range_upper(rr),
+				     range_increment(rr), TRUE) ;
+	    }
+	    else {
+		new_e = MakeBinaryCall( f, lhs, rhs ) ;
+	    }
+	    set_add_element( vectors, vectors, (char *)new_e ) ;
+	    return( new_e ) ;
 	}
-	else {
-	    new_e = MakeBinaryCall( f, lhs, rhs ) ;
+	else if( set_belong_p( vectors, (char *)lhs )) {
+	    if( syntax_range_p( ls )) {
+		range rl = syntax_range( ls ) ;
+
+		new_e = update_range(f, rl, rhs, rhs, make_expression_1(), TRUE) ;
+	    }
+	    else {
+		new_e = MakeBinaryCall( f, lhs, rhs ) ;
+	    }
+	    set_add_element( vectors, vectors, (char *)new_e ) ;
+	    return( new_e ) ;
 	}
-	set_add_element( vectors, vectors, (char *)new_e ) ;
-	return( new_e ) ;
+	else if( set_belong_p( vectors, (char *)rhs )) {
+	    if( syntax_range_p( rs )) {
+		range rr = syntax_range( rs ) ;
+
+		new_e = update_range(f, rr, lhs, lhs, make_expression_1(), FALSE) ;
+	    }
+	    else {
+		new_e = MakeBinaryCall( f, lhs, rhs ) ;
+	    }
+	    set_add_element( vectors, vectors, (char *)new_e ) ;
+	    return( new_e ) ;
+	}
+	return( e ) ;
     }
-    else if( set_belong_p( vectors, (char *)lhs )) {
-	if( syntax_range_p( ls )) {
-	    range rl = syntax_range( ls ) ;
-
-	    new_e = update_range(f, rl, rhs, rhs, make_expression_1()) ;
-	}
-	else {
-	    new_e = MakeBinaryCall( f, lhs, rhs ) ;
-	}
-	set_add_element( vectors, vectors, (char *)new_e ) ;
-	return( new_e ) ;
-    }
-    else if( set_belong_p( vectors, (char *)rhs )) {
-	if( syntax_range_p( rs )) {
-	    range rr = syntax_range( rs ) ;
-
-	    new_e = update_range(f, rr, lhs, lhs, make_expression_1()) ;
-	}
-	else {
-	    new_e = MakeBinaryCall( f, lhs, rhs ) ;
-	}
-	set_add_element( vectors, vectors, (char *)new_e ) ;
-	return( new_e ) ;
-    }
-    return( e ) ;
 }
-}
+
+/* A reference cannot always be expanded. Subscript expression coupling as
+   in A(I,I) prevent coupling. Non-affine expressions such as A(I**2)
+   cannot be transformed into ranges. */
 
 expression expand_reference( s, e, i, r )
 syntax s ;
@@ -121,7 +146,7 @@ range r ;
 	return( new_e ) ;
     }
     else {
-	bool vectorp = FALSE ;
+	int dim = 0 ;
 	cons *new_args = NIL ;
 	reference new_r ;
 
@@ -129,11 +154,17 @@ range r ;
 	    expression arg = EXPRESSION( CAR( args )) ;
 
 	    new_e = expand_expression( arg, i, r ) ;
-	    vectorp |= set_belong_p( vectors, (char *)new_e ) ;
+
+	    if(expression_undefined_p(new_e))
+		return new_e;
+
+	    if(set_belong_p( vectors, (char *)new_e ))
+		dim++;
+
 	    new_args = CONS(EXPRESSION, new_e, new_args ) ;
 	}, reference_indices( rf )) ;
 
-	if( vectorp ) {
+	if( dim==1 ) {
 	    new_r = make_reference(reference_variable(rf),
 				   gen_nreverse(new_args)) ;
 	    new_e = make_expression(make_syntax(is_syntax_reference,new_r),
@@ -141,12 +172,22 @@ range r ;
 	    set_add_element( vectors, vectors, (char *)new_e ) ;
 	    return( new_e ) ;
 	}
+	else if(dim > 1) {
+	    /* If dim is greater than 1, subscript expressions are coupled
+	     * as in A(I,I+1).
+	     */
+	    return expression_undefined;
+	}
 	else {
 	    gen_free_list( new_args ) ;
 	    return( e ) ;
 	}
     }
 }
+
+
+/* Expression with a non-expandable sub-expression, e.g. a non-expandable
+   reference, cannot be expanded */
 
 expression expand_expression( e, i, r )
 expression e ;
@@ -164,13 +205,23 @@ range r ;
 	expression new_e ;
 	cons *new_args = NIL ;
 	entity f = call_function( c ) ;
-	bool vectorp = FALSE ;
+	int dim = 0 ;
 	
 	MAPL( args, {
 	    expression arg = EXPRESSION( CAR( args )) ;
 	    
 	    new_e = expand_expression( arg, i, r ) ;
-	    vectorp |= set_belong_p( vectors, (char *)new_e ) ;
+
+	    if(expression_undefined_p(new_e)) {
+		return new_e;
+	    }
+
+	    /* FI: I do not know why dim is computed. Old cut-and-paste
+               from expand_reference()? */
+	    if(set_belong_p( vectors, (char *)new_e )) {
+		dim++;
+	    }
+
 	    new_args = CONS(EXPRESSION, new_e, new_args ) ;
 	}, call_arguments( c )) ;
 	
@@ -194,22 +245,38 @@ int margin;
 loop obj;
 int n ;
 {
+    /* text_loop_90() only is called if the loop is parallel and if its
+     * body is a unique assignment statement.
+     */
     instruction i = statement_instruction( loop_body( obj )) ;
     entity idx = loop_index( obj ) ;
     range r = loop_range( obj ) ;
     expression lhs =
-	    EXPRESSION( CAR( call_arguments( instruction_call( i )))) ;
+	EXPRESSION( CAR( call_arguments( instruction_call( i )))) ;
     expression rhs = 
-	    EXPRESSION( CAR( CDR( call_arguments( instruction_call( i ))))) ;
+	EXPRESSION( CAR( CDR( call_arguments( instruction_call( i ))))) ;
+
+    expression new_lhs = expression_undefined;
+    expression new_rhs = expression_undefined;
     
     vectors = set_make( set_pointer ) ;
-{
-    expression new_lhs = expand_expression( lhs, idx, r ) ;
-    expression new_rhs = expand_expression( rhs, idx, r ) ;
+
+    new_lhs = expand_expression( lhs, idx, r ) ;
+    new_rhs = expand_expression( rhs, idx, r ) ;
     
     set_free( vectors ) ;
-    return( text_statement(module, margin, 
-			   make_assign_statement( new_lhs, new_rhs ))) ;
-}
-}
 
+    if(!expression_undefined_p(new_lhs) && !expression_undefined_p(new_rhs)) {
+	statement new_s = make_assign_statement( new_lhs, new_rhs );
+
+	/*
+	statement_number(new_s) = statement_number(loop_body(obj));
+	statement_comments(new_s) = statement_comments(loop_body(obj));
+	*/
+	return( text_statement(module, margin, new_s)) ;
+    }
+    else {
+	return( text_loop(module, label, margin, obj, n));
+    }
+
+}
