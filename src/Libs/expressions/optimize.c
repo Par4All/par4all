1dafbe33dca9d0d807d0829c2fe75a28c9ab942f
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-09-14 12:34:11 +0000

    # on 1998/09/14 12:34:11, coelho did: added import from eole and substitution in module code.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9644 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/expressions/optimize.c
+++ src/Libs/expressions/optimize.c
@@ -2,6 +2,9 @@
  * $Id$
  *
  * $Log: optimize.c,v $
+ * Revision 1.4  1998/09/14 12:34:11  coelho
+ * added import from eole and substitution in module code.
+ *
  * Revision 1.3  1998/09/11 12:18:39  coelho
  * new version thru a reference (suggested by PJ).
  *
@@ -82,30 +85,79 @@ static void write_list_of_rhs(FILE * out, list /* of expression */ le)
     free_reference(astuce);
 }
 
-#define FILE_NAME "toeole"
-
 /* export expressions to eole thru the newgen format.
  * both entities and rhs expressions are exported. 
  */
-static void write_to_eole(string module, statement s)
+static void write_to_eole(string module, list le, string file_name)
 {
-    FILE * toeole = safe_fopen(FILE_NAME, "w");
-    list /* of expression */ lc = get_list_of_rhs(s);
+    FILE * toeole;
 
     pips_debug(3, "writing to eole for module %s\n", module);
 
+    toeole = safe_fopen(file_name, "w");
     write_tabulated_entity(toeole);
-    write_list_of_rhs(toeole, lc);
+    write_list_of_rhs(toeole, le);
 
-    safe_fclose(toeole, FILE_NAME);
-    gen_free_list(lc);
+    safe_fclose(toeole, file_name);
 }
+
+/* import expressions from eole.
+ */
+static list /* of expression */
+read_from_eole(string module, string file_name)
+{
+    FILE * fromeole;
+    reference astuce;
+    list result;
+
+    pips_debug(3, "reading from eole for module %s\n", module);
+    
+    fromeole = safe_fopen(file_name, "r");
+
+    /* read entites to create... 
+     */
+
+    astuce = read_reference(fromeole);
+    result = reference_indices(astuce);
+    reference_indices(astuce) = NIL;
+    free_reference(astuce);
+
+    return result;
+}
+
+/* swap term to term syntax field in expression list, as a side effect...
+ */
+static void 
+swap_syntax_in_expression(
+    list /* of expression */ lcode,
+    list /* of expression */ lnew)
+{
+    pips_assert("equal length lists", gen_length(lcode)==gen_length(lnew));
+
+    for(; lcode; lcode=CDR(lcode), lnew=CDR(lnew))
+    {
+	expression 
+	    old = EXPRESSION(CAR(lcode)),
+	    new = EXPRESSION(CAR(lnew));
+	syntax tmp = expression_syntax(old);
+	expression_syntax(old) = expression_syntax(new);
+	expression_syntax(new) = tmp;	
+    }
+}
+
+#define OUT_FILE_NAME 	"/tmp/pips_to_eole"
+#define IN_FILE_NAME	"/tmp/eole_to_pips"
+
+#define PIPS_EOLE	"newgen_eole"
+#define PIPS_EOLE_FLAGS	"-nfmd"
 
 /* pipsmake interface.
  */
 bool optimize_expressions(string module_name)
 {
     statement s;
+    list /* of expression */ le, ln;
+    string in, out, cmd;
 
     debug_on(DEBUG_NAME);
 
@@ -119,8 +171,35 @@ bool optimize_expressions(string module_name)
 
     /* do something here.
      */
-    /*pips_user_warning("not implemented yet!\n");*/
-    write_to_eole(module_name, s);
+    in = safe_new_tmp_file(IN_FILE_NAME);
+    out = safe_new_tmp_file(OUT_FILE_NAME);
+
+    le = get_list_of_rhs(s);
+    write_to_eole(module_name, le, out);
+
+    /* run eole (Evaluation Optimization for Loops and Expressions) 
+     * as a separate process.
+     */
+    cmd = strdup(concatenate(
+	PIPS_EOLE " " PIPS_EOLE_FLAGS " -o ", in, " ", out, NULL));
+
+    pips_debug(2, "executing %s\n", cmd);
+    safe_system(cmd);
+
+    ln = read_from_eole(module_name, in);
+    swap_syntax_in_expression(le, ln);
+    gen_free_list(ln), ln=NIL;
+
+    /* remove temorary files.
+     */
+    safe_unlink(out);
+    safe_unlink(in);
+    free(out), out = NULL;
+    free(in), in = NULL;
+    free(cmd), cmd = NULL;
+
+    /* Should perform more optimizations here...
+     */
 
     /* return result.
      */
