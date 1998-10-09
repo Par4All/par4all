98b7e65c244fc63c4a8e2048507064495199bb99
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-10-09 15:50:23 +0000

    # on 1998/10/09 15:50:23, irigoin did: scanf() replaced by user_request()
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9721 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/hyperplane/hyperplane_direction.c
+++ src/Libs/hyperplane/hyperplane_direction.c
@@ -1,3 +1,13 @@
+/* Find out a legal hyperplane direction. In a first phase, trust the user blindly!
+ *
+ * $Id$
+ *
+ * $Log: hyperplane_direction.c,v $
+ * Revision 1.2  1998/10/09 15:50:23  irigoin
+ * scanf() replaced by user_request()
+ *
+ */
+
 #include <stdio.h>
 
 #include "boolean.h"
@@ -6,18 +16,49 @@
 #include "genC.h"
 #include "misc.h"
 
-void hyperplane_direction(h,n)
-int *h;
-int n;
+bool
+interactive_hyperplane_direction(Value * h, int n)
 {
     int i;
     int n_read;
+    string resp = string_undefined;
+    bool return_status = FALSE;
 
-    /*lecteure du h */
-    assert(n>=1);
-    debug(8,"hyperplane_direction","\nLecteur du h");
-    printf("Vecteur de direction :\n");
+    /* Query the user for h's coordinates */
+    pips_assert("hyperplane_direction", n>=1);
+    debug(8,"hyperplane_direction","Reading h\n");
+    resp = user_request("Hyperplane direction vector?\n"
+			"(give all its integer coordinates on one line): ");
+    if (resp[0] == '\0') {
+	user_log("Hyperplane loop transformation has been cancelled.\n");
+	return_status = FALSE;
+    }
+    else {    
+	return_status = TRUE;
+	for( i = 0; i<n; i++) {
+	    n_read = sscanf(resp," " VALUE_FMT, h+i);
+	    if(n_read!=1) {
+		user_log("Not enough coordinates. "
+			 "Hyperplane loop transformation has been cancelled.\n");
+		return_status = FALSE;
+		break;
+	    }
+	}
+    }
 
-    for( i = 0; i<n; i++)
-	n_read = fscanf(stdin," %d",h+i);
+    ifdebug(8) {
+	if(return_status) {
+	    pips_debug(8, "Hyperplane direction vector:\n");
+	    for( i = 0; i<n; i++) {
+		(void) fprintf(stderr," " VALUE_FMT, *(h+i));
+	    }
+	    (void) fprintf(stderr,"\n");
+	    pips_debug(8, "End\n");
+	}
+	else {
+	    pips_debug(8, "Ends with failure\n");
+	}
+    }
+
+    return return_status;
 }
