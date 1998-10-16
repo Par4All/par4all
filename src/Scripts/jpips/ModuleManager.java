05142a8bc6b3298ec0f978651b7374a96b81a240
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-10-16 17:16:40 +0000

    # on 1998/10/16 17:16:40, coelho did: attempt to port to jdk1.2b4
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9763 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/ModuleManager.java
+++ src/Scripts/jpips/ModuleManager.java
@@ -1,113 +1,117 @@
+/*
+  $Id$
 
-/** $Id$
-  * $Log: ModuleManager.java,v $
-  * Revision 1.1  1998/06/30 17:35:33  coelho
-  * Initial revision
-  *
-  */
+  $Log: ModuleManager.java,v $
+  Revision 1.2  1998/10/16 17:16:40  coelho
+  attempt to port to jdk1.2b4
+
+  Revision 1.1  1998/06/30 17:35:33  coelho
+  Initial revision
+*/
 
 
 package JPips;
 
-
 import java.lang.*;
 import java.util.*;
 import java.io.*;
-import java.awt.swing.*;
+
 import JPips.Pawt.*;
+
 import java.awt.*;
-import java.awt.swing.border.*;
 import java.awt.event.*;
 
+import com.sun.java.swing.*;
+import com.sun.java.swing.border.*;
 
-/** A module manager for TPips  
+
+/** A module manager for TPips.
   * @author Francois Didry
   */  
 public class ModuleManager implements JPipsComponent
 {
+    public	TPips		tpips;		//tpips instance
+    public	PList		list;		//contains the modules
+    public	PPanel		panel;		//jpips module panel
 
 
-  public	TPips		tpips;		//tpips instance
-  public	PList		list;		//contains the modules
-  public	PPanel		panel;		//jpips module panel
-
-
-  /** Sets the model of the list containing the modules names.
-    * Sets the tpips instance.
+    /** Sets the model of the list containing the modules names.
+	Sets the tpips instance.
     */  
-  public ModuleManager(TPips tpips)
+    public ModuleManager(TPips tpips)
     {
-      this.tpips = tpips;
-      buildPanel();
+	this.tpips = tpips;
+	buildPanel();
     }
+    
 
-
-  /** Creates the module panel for jpips.
-    */  
-  public void buildPanel()
+    /** Creates the module panel for jpips.
+     */  
+    public void buildPanel()
     {
-      panel = new PPanel(new BorderLayout());
-      panel.setBorder(new TitledBorder("Modules"));
-      list = new PList(new DefaultListModel());
-      list.setSelectionMode(2);
-      PScrollPanel scrollPanel = new PScrollPanel((Component)list);
-      scrollPanel.setPreferredSize(new Dimension(200,100));
-      panel.add(scrollPanel,BorderLayout.WEST);
-      PButton b = new PButton("Select All");
-      ActionListener a = new ActionListener()
+	panel = new PPanel(new BorderLayout());
+	panel.setBorder(new TitledBorder("Modules"));
+	list = new PList(new DefaultListModel());
+	list.setSelectionMode(2);
+	PScrollPanel scrollPanel = new PScrollPanel((Component)list);
+	scrollPanel.setPreferredSize(new Dimension(200,100));
+	panel.add(scrollPanel,BorderLayout.WEST);
+	PButton b = new PButton("Select All");
+	ActionListener a = new ActionListener()
         {
-	  public void actionPerformed(ActionEvent e) { setAllSelected(); }
+	    public void actionPerformed(ActionEvent e) { setAllSelected(); }
 	};
-      b.addActionListener(a);
-      panel.add(b,BorderLayout.SOUTH);
-    }
-
-
-  /** @return the module panel for JPips
-    */  
-  public Component getComponent()
-    {
-      return (Component)panel;
-    }
-
-
-
-  /** Sets as selected all the modules of the list.
-    */  
-  public void setAllSelected()
-    {
-      DefaultListModel dlm = (DefaultListModel) list.getModel();
-      int tab[] = new int[dlm.size()];
-      for(int i=0; i<dlm.size(); i++) tab[i] = i;
-      list.setSelectedIndices(tab);
+	b.addActionListener(a);
+	panel.add(b,BorderLayout.SOUTH);
     }
     
     
-  /** @return the selected modules
-    */  
-  public Object[] getSelectedModules()
+    /** @return the module panel for JPips
+     */  
+    public Component getComponent()
     {
-      return list.getSelectedValues();
+	return (Component)panel;
+    }
+    
+    
+    
+    /** Sets as selected all the modules of the list.
+     */  
+    public void setAllSelected()
+    {
+	DefaultListModel dlm = (DefaultListModel) list.getModel();
+	int tab[] = new int[dlm.size()];
+	for(int i=0; i<dlm.size(); i++) tab[i] = i;
+	list.setSelectedIndices(tab);
+    }
+    
+    
+    /** @return the selected modules
+     */  
+    public Object[] getSelectedModules()
+    {
+	return list.getSelectedValues();
     }
     
     
   /** Sets the modules of tpips in the modules list.
     */
   public void setModules()
+  {
+    DefaultListModel dlm = (DefaultListModel) list.getModel();
+    String modules = tpips.sendCommand("info modules");
+    if (modules != null)
     {
-      DefaultListModel dlm = (DefaultListModel) list.getModel();
-      String s = tpips.sendCommand("info modules");
-      if(s != null)
-        {
-	  StringTokenizer tok = new StringTokenizer(s," ",false);
-          String response;
-          while(tok.hasMoreTokens())
-            {
-	      response = tok.nextToken();
-	      dlm.addElement(response);
-	    }
-	}
+      StringTokenizer tok = new StringTokenizer(modules, " ", false);
+      String module;
+      while(tok.hasMoreTokens())
+      {
+	module = tok.nextToken();
+	System.err.println("token: " + module);
+	dlm.addElement(module);
+      }
     }
+  }
 
 
   /** Clears the modules list.
