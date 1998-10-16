9c8082dd0a94cca4e2ec0cbcc04913f9be5842f5
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-10-16 14:23:42 +0000

    # on 1998/10/16 14:23:42, coelho did: re-indented.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9754 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/Watcher.java
+++ src/Scripts/jpips/Watcher.java
@@ -1,49 +1,44 @@
+/*
+  $Id$
 
-/** $Id$
-  * $Log: Watcher.java,v $
-  * Revision 1.1  1998/06/30 15:03:42  didry
-  * Initial revision
-  *
-  */
+  $Log: Watcher.java,v $
+  Revision 1.2  1998/10/16 14:23:42  coelho
+  re-indented.
 
+  Revision 1.1  1998/06/30 15:03:42  didry
+  Initial revision
+*/
 
 package JPips;
-
 
 import java.lang.*;
 import java.util.*;
 import java.io.*;
 
-
-/** A class that s the tpips output stream.
+/** A class that checks for the tpips output stream.
   * @author Francois Didry
   */  
-public class Watcher implements Runnable
+public class Watcher 
+  implements Runnable
 {
-
-
-  public	Process		tpips;		//tpips instance
-  
+  public Process tpips;		//tpips instance
   
   public Watcher(Process tpips)
-    {
-      this.tpips = tpips;
-    }
-
+  {
+    this.tpips = tpips;
+  }
 
   public void run()
+  {
+    try
     {
-      try
-        {
-	  System.out.println("Tpips watcher running");
-          tpips.waitFor();
-          System.out.println("tpips down");
-	}
-      catch(InterruptedException e)
-        {
-	  System.out.println(e);
-	}
+      System.out.println("Tpips watcher running");
+      tpips.waitFor();
+      System.out.println("tpips down");
     }
-
-
+    catch(InterruptedException e)
+    {
+      System.out.println(e);
+    }
+  }
 }
