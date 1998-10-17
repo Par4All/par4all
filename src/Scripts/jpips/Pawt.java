d890b2001bb5ea91e56a9716574c57168b15989b
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-10-17 12:19:54 +0000

    # on 1998/10/17 12:19:54, coelho did: re-indentation. colors fixed. new factory to set default colors.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9780 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/Pawt.java
+++ src/Scripts/jpips/Pawt.java
@@ -1,61 +1,91 @@
 /*
- * $Id$
- * 
- * $Log: Pawt.java,v $
- * Revision 1.7  1998/10/17 09:50:08  coelho
- * white background color.
- *
- * Revision 1.6  1998/10/16 16:53:06  coelho
- * swing package updated.
- *
- * Revision 1.5  1998/10/16 13:55:59  coelho
- * import fixed.
- *
- * Revision 1.4  1998/07/03 16:36:51  coelho
- * PComboBox includes a direct field.
- *
- * Revision 1.3  1998/07/03 08:13:33  coelho
- * simpler constructors. tips added to labels and buttons.
- *
- * Revision 1.2  1998/06/30 17:35:33  coelho
- * abstarct wt for jpips.
- *
- * Revision 1.1  1998/06/30 16:40:14  coelho
- * Initial revision
- *
- */
+  $Id$
+  
+  $Log: Pawt.java,v $
+  Revision 1.8  1998/10/17 12:19:54  coelho
+  re-indentation.
+  colors fixed.
+  new factory to set default colors.
+
+  Revision 1.7  1998/10/17 09:50:08  coelho
+  white background color.
+ 
+  Revision 1.6  1998/10/16 16:53:06  coelho
+  swing package updated.
+ 
+  Revision 1.5  1998/10/16 13:55:59  coelho
+  import fixed.
+ 
+  Revision 1.4  1998/07/03 16:36:51  coelho
+  PComboBox includes a direct field.
+ 
+  Revision 1.3  1998/07/03 08:13:33  coelho
+  simpler constructors. tips added to labels and buttons.
+ 
+  Revision 1.2  1998/06/30 17:35:33  coelho
+  abstarct wt for jpips.
+ 
+  Revision 1.1  1998/06/30 16:40:14  coelho
+  Initial revision
+*/
 
 package JPips;
 
 import java.util.*;
+import java.io.*;
 
 import java.awt.*;
 import java.awt.event.*;
 
 import com.sun.java.swing.*;
+import com.sun.java.swing.border.*;
 import com.sun.java.swing.event.*;
 
-/** A graphical package for JPips
-  * All components extends from swing components.
-  * Some components contain informations to manage their Events.
-  * 
-  * @author Francois Didry
-  */
-interface Pawt
+/**
+   A graphical package for JPips
+   All components extends from swing components.
+   Some components contain informations to manage their Events.
+   
+   @author Francois Didry
+*/
+public abstract class Pawt
 {
-  class PComponent extends com.sun.java.swing.JComponent
+  /** Default display colors.
+   */
+  static public Color text_bg = Color.white;
+  static public Color text_fg = Color.black;
+  static public Color comp_bg = new Color(200,200,255);
+  static public Color comp_fg = Color.blue;
+
+  static class PComponent extends com.sun.java.swing.JComponent
   { 
     public String checking; 
-    PComponent() { super(); } 
+    PComponent() 
+    { 
+      super(); 
+      
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+    } 
   }
   
-  class PFrame extends com.sun.java.swing.JFrame
+  static class PFrame extends com.sun.java.swing.JFrame
   { 
     public Vector optionVector;
-    PFrame(String name){ super(name); }
+    PFrame(String name)
+    {
+      super(name); 
+      
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+
+      Container pane = getContentPane();
+      pane.setBackground(comp_bg);
+      pane.setForeground(comp_fg);
+    }
     
     /** Disables or enables the Option objects of JPips.
-     * @param yes true means disable
+	@param yes true means disable
      */
     public void lock(boolean yes)
     {
@@ -64,35 +94,89 @@ interface Pawt
     }
   }
 
-  class PDialog extends com.sun.java.swing.JDialog
-    { PDialog(Frame f, String s, boolean b){ super(f, s, b); } }
+  static class PDialog extends com.sun.java.swing.JDialog
+  { 
+    PDialog(Frame f, String s, boolean b)
+    { 
+      super(f, s, b); 
+      
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+
+      Container pane = getContentPane();
+      pane.setBackground(comp_bg);
+      pane.setForeground(comp_fg);
+    }
+  }
   
-  class PPanel extends com.sun.java.swing.JPanel
+  static class PPanel extends com.sun.java.swing.JPanel
   {
-    PPanel(){ super(); }
-    PPanel(LayoutManager l){ super(l); }
+    PPanel(LayoutManager l)
+    {
+      super(l); 
+      
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+    }
+
+    PPanel()
+    { 
+      this(null); 
+    }
   }
   
-  class PMenuBar extends com.sun.java.swing.JMenuBar
-  { PMenuBar(){ super(); } }
-  
-  class PMenu extends com.sun.java.swing.JMenu
-  { PMenu(){ super(); }
-    PMenu(String name){ super(name); }
+  static class PMenuBar extends com.sun.java.swing.JMenuBar
+  { 
+    PMenuBar()
+    { 
+      super(); 
+      
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+    } 
   }
   
-  class PMenuItem extends com.sun.java.swing.JMenuItem
+  static class PMenu extends com.sun.java.swing.JMenu
+  { 
+    PMenu(String name)
+    { 
+      super(name); 
+      
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+    }
+
+    PMenu(){ this(null); }
+  }
+  
+  static  class PMenuItem extends com.sun.java.swing.JMenuItem
   {
     public String command;
     public PTextField tf;
-    PMenuItem(String name){ super(name); }
-    PMenuItem(String name,String command)
-    { super(name); this.command = command; }
+
     PMenuItem(String name,String command,PTextField tf)
-    { super(name); this.command = command; this.tf = tf; }
+    { 
+      super(name); 
+
+      this.command = command; 
+      this.tf = tf; 
+      
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+    }
+
+    PMenuItem(String name)
+    {
+      this(name, null, null);
+    }
+
+    PMenuItem(String name,String command)
+    { 
+      this(name, command, null);
+    }
   }
   
-  class PCheckBox 
+  static class PCheckBox 
     extends com.sun.java.swing.JCheckBox
   {
     public String command,checking;
@@ -103,11 +187,16 @@ interface Pawt
 	      PCheckBoxMenuItem cbmi, PTextFrame frame)
     { 
       super(name); 
+
       if (tip!=null) setToolTipText(tip);
+
       this.command = command; 
       this.checking = checking;
       this.cbmi = cbmi; 
       this.frame = frame;
+
+      setBackground(comp_bg);
+      setForeground(comp_fg);
     }	
     
     PCheckBox(String name)
@@ -120,31 +209,37 @@ interface Pawt
     { this(name, null, null, null, null, frame); }
   }
   
-  class PCheckBoxMenuItem extends com.sun.java.swing.JCheckBoxMenuItem
+  static class PCheckBoxMenuItem extends com.sun.java.swing.JCheckBoxMenuItem
   {
-    public String command,checking;
+    public String command, checking;
     public PCheckBox cb;
     
-    PCheckBoxMenuItem(String name){ super(name); }
-    
-    PCheckBoxMenuItem(String name,String command,String checking)
-    { 
-      super(name);
-      this.command = command;
-      this.checking = checking; 
-    }
-    
+    /** main constructor. */
     PCheckBoxMenuItem(String name, String command, 
 		      String checking, PCheckBox cb)
     {
       super(name); 
+
       this.command = command; 
       this.checking = checking;
       this.cb = cb; 
+
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+    }
+
+    PCheckBoxMenuItem(String name)
+    {
+      this(name, null, null, null);
+    }
+    
+    PCheckBoxMenuItem(String name, String command, String checking)
+    { 
+      this(name, command, checking, null);
     }
   }
 
-  class PRadioButtonMenuItem 
+  static class PRadioButtonMenuItem 
     extends com.sun.java.swing.JRadioButtonMenuItem
   {
     public String command, checking;
@@ -153,35 +248,85 @@ interface Pawt
     public Object o;
     public PRadioButtonMenuItem rbmi;
 
-    PRadioButtonMenuItem(String name, String command, 
-			 PComboBox cob, Object o, String checking)
-    { super(name); this.command = command; this.cob = cob; this.o = o;
-    this.checking = checking; }
+    /** main constructor. */
+    PRadioButtonMenuItem(String name, 
+			 String command, PLabel label,
+			 PComboBox cob, PRadioButtonMenuItem rbmi,
+			 Object o, String checking)
+    { 
+      super(name); 
+
+      this.command = command; 
+      this.label = label;
+      this.cob = cob;
+      this.rbmi = rbmi;
+      this.o = o;
+      this.checking = checking;
+
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+    }
     
-    PRadioButtonMenuItem(String name){ super(name); }
+    PRadioButtonMenuItem(String name)
+    { 
+      this(name, null, null, null, null, null, null); 
+    }
+
     PRadioButtonMenuItem(String name, String command)
-    { super(name); this.command = command; }
-    PRadioButtonMenuItem(String name, String command, PLabel label,
+    {
+      this(name, command, null, null, null, null, null);
+    }
+
+    PRadioButtonMenuItem(String name, 
+			 String command, PLabel label,
 			 PRadioButtonMenuItem rbmi)
-    { super(name); this.command = command; this.label = label;
-    this.rbmi = rbmi; }
+    {
+      this(name, command, label, null, rbmi, null, null);
+    }
+
     PRadioButtonMenuItem(String name, String command, PLabel label,
 			 PRadioButtonMenuItem rbmi, String checking)
-    { super(name); this.command = command; this.label = label;
-	  this.rbmi = rbmi; this.checking = checking; }
+    { 
+      this(name, command, label, null, rbmi, null, checking);
+    }
+
     PRadioButtonMenuItem(String name, String command,
 			 PComboBox cob, Object o)
-    { super(name); this.command = command; this.cob = cob; this.o = o; }
+    { 
+      this(name, command, null, cob, null, o, null);
+    }
+
+    PRadioButtonMenuItem(String name, String command,
+			 PComboBox cob, Object o, String checking)
+    { 
+      this(name, command, null, cob, null, o, checking);
+    }
   }
 
+  /** create a border with a title.
+      @param name the border title.
+      @return the titled border.
+   */
+  static public TitledBorder createTitledBorder(String name)
+  {
+    TitledBorder t = BorderFactory.createTitledBorder(name);
+    t.setTitleColor(comp_fg);
+    return t;
+  }
 
-  class PSeparator 
+  static class PSeparator 
     extends com.sun.java.swing.JSeparator
   { 
-    PSeparator(){ super(); } 
+    PSeparator()
+    {
+      super(); 
+
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+    } 
   }
 
-  class PButton 
+  static class PButton 
     extends com.sun.java.swing.JButton
   {
     public String 	command;
@@ -192,11 +337,16 @@ interface Pawt
 	    PTextField tf, PTextFrame frame)
       { 
 	super(name); 
+
 	setMargin(new Insets(0,0,0,0));
 	if (tip!=null) setToolTipText(tip);
+
 	this.command = command;
 	this.tf = tf; 
 	this.frame = frame;
+
+	setBackground(comp_bg);
+	setForeground(comp_fg);
       }
 
     PButton(String name) 
@@ -209,25 +359,30 @@ interface Pawt
       {	this(name, command, tip, null, null); }
   }
 
-  class PLabel 
+  static class PLabel 
     extends com.sun.java.swing.JLabel
   {
     PLabel(String s, String tip)
-      { 
-	super(s); 
-	if (tip!=null) setToolTipText(tip);
-      }
-
+    { 
+      super(s); 
+      if (tip!=null) setToolTipText(tip);
+      
+      setBackground(comp_bg);
+      setForeground(comp_fg);
+    }
+    
     PLabel(String s) { this(s, null); }
-    PLabel() { super(); }
+    PLabel() { this(null, null); }
   }
     
-  class PTextField extends com.sun.java.swing.JTextField
+  static class PTextField extends com.sun.java.swing.JTextField
   {
     PTextField(String s)
     {
       super(s); 
-      setBackground(Color.white); 
+
+      setBackground(text_bg); 
+      setForeground(text_fg);
     }
 
     PTextField()
@@ -236,7 +391,7 @@ interface Pawt
     }
   }
     
-  class PList extends com.sun.java.swing.JList
+  static class PList extends com.sun.java.swing.JList
   {
     PList() { super(); }
     PList(Vector v) { super(v); }
@@ -246,45 +401,84 @@ interface Pawt
   /** A PComboBox can send directly its command on selections,
     * or wait for some specific action to do the job (e.g. a button pressed).
     */
-  class PComboBox extends com.sun.java.swing.JComboBox
+  static class PComboBox extends com.sun.java.swing.JComboBox
   {
     public String checking, marker;
     public boolean direct;
-    public Vector vCommand = new Vector(),
-      vRbmi = new Vector(),
-      vChecking = new Vector();
+    public Vector vCommand, vRbmi, vChecking;
     
     PComboBox(String ch, String ma, boolean direct)
     { 
       super(); 
+
       checking = ch; 
       marker = ma; 
       this.direct = direct;
+
+      vCommand = new Vector();
+      vRbmi = new Vector();
+      vChecking = new Vector();
+
+      setBackground(comp_bg);
+      setForeground(comp_fg);
     }
     
-    PComboBox(){ this(null, null, true); }
-    PComboBox(String ch){ this(ch, null, true); }
-    PComboBox(String ch, String ma) { this(ch, ma, true); }
+    PComboBox()
+    { 
+      this(null, null, true); 
+    }
+
+    PComboBox(String ch)
+    {
+      this(ch, null, true); 
+    }
+
+    PComboBox(String ch, String ma) 
+    { 
+      this(ch, ma, true); 
+    }
   }
 
-  class PScrollPanel extends com.sun.java.swing.JScrollPane
+  static class PScrollPanel extends com.sun.java.swing.JScrollPane
   {
-    PScrollPanel(){ super(); } 
-    PScrollPanel(Component c){ super(c); } 
+    PScrollPanel(Component c)
+    {
+      super(c); 
+
+      setBackground(comp_bg);
+      setForeground(comp_fg);      
+    } 
+
+    PScrollPanel()
+    {
+      this(null);
+    } 
   }
         
-  class PButtonGroup extends com.sun.java.swing.ButtonGroup
+  static class PButtonGroup extends com.sun.java.swing.ButtonGroup
   {
     public String checking;
-    PButtonGroup(String ch){ super(); checking = ch; }
+
+    PButtonGroup(String ch)
+    { 
+      super(); 
+      checking = ch;     
+    }
+
+    PButtonGroup()
+    {
+      this(null);
+    }
   }
         
-  class PTextArea extends java.awt.TextArea
+  static class PTextArea extends java.awt.TextArea
   {
     PTextArea(String s)
     { 
       super(s);  
-      setBackground(Color.white); 
+
+      setBackground(text_bg);
+      setForeground(text_fg);
     }
     
     PTextArea()
@@ -293,7 +487,7 @@ interface Pawt
     }
   }
         
-  class PTextFrame extends com.sun.java.swing.JFrame
+  static class PTextFrame extends com.sun.java.swing.JFrame
   {
     boolean locked;
     boolean writable;
@@ -355,5 +549,18 @@ interface Pawt
       pack();
       setSize(new Dimension(600,400));
     }
+  }
+
+  static public JFileChooser createFileChooser(File dir)
+  {
+    JFileChooser chooser = new JFileChooser(dir);
+    chooser.setBackground(comp_bg);
+    chooser.setForeground(comp_fg);
+    return chooser;
+  }
+
+  static public JFileChooser createFileChooser(String dir)
+  {
+    return createFileChooser(new File(dir));
   }
 }
