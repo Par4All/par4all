35d4f8201607e6be757a943caa08c3b838696592
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-11-17 22:08:06 +0000

    # on 1998/11/17 22:08:06, coelho did: try to display the file.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9863 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/EmacsDisplayer.java
+++ src/Scripts/jpips/EmacsDisplayer.java
@@ -4,6 +4,9 @@
   Send displays to emacs.
 
   $Log: EmacsDisplayer.java,v $
+  Revision 1.3  1998/11/17 22:08:06  coelho
+  try to display the file.
+
   Revision 1.2  1998/11/17 21:41:52  ancourt
   *** empty log message ***
 
@@ -43,38 +46,39 @@ public class EmacsDisplayer
     display(new File(name), locked, writable);
   }
  
-    boolean graphdisplay(File file, boolean locked, boolean writable)
+  boolean graphdisplay(File file, boolean locked, boolean writable)
   {
-
-      String filename= file.getPath();
-      String pstring = "pips_graph2daVinci" + " " + filename; 
+    String filename = file.getPath();
+    try
+    {
+      String ps = "pips_graph2daVinci -launch_daVinci " + filename;
+      System.err.println("running: " + ps);
+      g2davinci = Runtime.getRuntime().exec(ps);
+      System.err.println("done");
+    }
+    catch (Exception e)
+    {
+      System.err.println("EmacsDisplayer.graphdisplay Exception :" + e);
+    }
+    
+    /*
       int index = filename.lastIndexOf('-');
       String ft = filename.substring(1,index);
       String davinciname = ft + "-daVinci";
       String pstring1 =  "daVinci " + davinciname;
-      // if (! davinciname
       try
-		{
-		  g2davinci = Runtime.getRuntime().exec(pstring);
-		}
-	    catch (Exception e)
-		{
-		    System.err.println("EmacsDisplayer.graphdisplay Exception :" + e);
-		}
-	    
-     try
-		{
-		  davinci = Runtime.getRuntime().exec(pstring1);
-		}
-	    catch (Exception e)
-		{
-		    System.err.println("EmacsDisplayer.graphdisplay Exception :" + e);
-		}
-	    
+      {
+	davinci = Runtime.getRuntime().exec(pstring1);
+      }
+      catch (Exception e)
+      {
+	System.err.println("EmacsDisplayer.graphdisplay Exception :" + e);
+      }
+    */      
       
-      /*      to_emacs.print(BEGIN + "WINDOW_NUMBER:" + 4 + END +
-		   BEGIN + "VIEW_DAVINCI_GRAPH:" + filename + END);
-		   to_emacs.flush(); */
+    //to_emacs.print(BEGIN + "VIEW_DAVINCI_GRAPH:" + filename + END);
+    //to_emacs.flush(); 
+    
     return true;
   }
 }
