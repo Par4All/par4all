f36ae9f4a717e5d759a4c44923531655fbb78899
ancourt <ancourt@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1998-11-17 21:41:52 +0000

    # on 1998/11/17 21:41:52, ancourt did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@9860 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/jpips/EmacsDisplayer.java
+++ src/Scripts/jpips/EmacsDisplayer.java
@@ -4,6 +4,9 @@
   Send displays to emacs.
 
   $Log: EmacsDisplayer.java,v $
+  Revision 1.2  1998/11/17 21:41:52  ancourt
+  *** empty log message ***
+
   Revision 1.1  1998/11/12 17:20:51  coelho
   Initial revision
 
@@ -18,7 +21,8 @@ public class EmacsDisplayer
 {
   static public final String BEGIN = "\200";
   static public final String END = "\201";
-
+  public Process	g2davinci;	
+  public Process	davinci;	
   PrintStream to_emacs;
 
   public EmacsDisplayer(PrintStream out)
@@ -37,5 +41,40 @@ public class EmacsDisplayer
   void display(String name, String string, boolean locked, boolean writable)
   {
     display(new File(name), locked, writable);
+  }
+ 
+    boolean graphdisplay(File file, boolean locked, boolean writable)
+  {
+
+      String filename= file.getPath();
+      String pstring = "pips_graph2daVinci" + " " + filename; 
+      int index = filename.lastIndexOf('-');
+      String ft = filename.substring(1,index);
+      String davinciname = ft + "-daVinci";
+      String pstring1 =  "daVinci " + davinciname;
+      // if (! davinciname
+      try
+		{
+		  g2davinci = Runtime.getRuntime().exec(pstring);
+		}
+	    catch (Exception e)
+		{
+		    System.err.println("EmacsDisplayer.graphdisplay Exception :" + e);
+		}
+	    
+     try
+		{
+		  davinci = Runtime.getRuntime().exec(pstring1);
+		}
+	    catch (Exception e)
+		{
+		    System.err.println("EmacsDisplayer.graphdisplay Exception :" + e);
+		}
+	    
+      
+      /*      to_emacs.print(BEGIN + "WINDOW_NUMBER:" + 4 + END +
+		   BEGIN + "VIEW_DAVINCI_GRAPH:" + filename + END);
+		   to_emacs.flush(); */
+    return true;
   }
 }
