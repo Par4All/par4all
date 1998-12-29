45c613f3667a9bdf05dc15a1dada3df172e9b9df
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
1998-12-29 16:00:21 +0000

    # on 1998/12/29 16:00:21, coelho did: char * -> void *
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@551 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- genC/newgen_array.h
+++ genC/newgen_array.h
@@ -2,6 +2,12 @@
  * $Id$
  *
  * $Log: newgen_array.h,v $
+ * Revision 1.6  1998/12/29 16:00:21  coelho
+ * char * -> void *
+ *
+ * Revision 1.5  1997/12/05 12:18:32  coelho
+ * list <-> array
+ *
  * Revision 1.4  1997/12/04 17:24:14  coelho
  * GEN_ARRAY_MAP added.
  *
@@ -18,15 +24,17 @@ typedef struct _gen_array_chunk_t * gen_array_t;
 gen_array_t gen_array_make(int);
 void gen_array_free(gen_array_t);
 void gen_array_full_free(gen_array_t);
-void gen_array_addto(gen_array_t, int, char *);
-void gen_array_dupaddto(gen_array_t, int, char *);
-void gen_array_append(gen_array_t, char *);
-void gen_array_dupappend(gen_array_t, char *);
-char **gen_array_pointer(gen_array_t);
+void gen_array_addto(gen_array_t, int, void *);
+void gen_array_dupaddto(gen_array_t, int, void *);
+void gen_array_append(gen_array_t, void *);
+void gen_array_dupappend(gen_array_t, void *);
+char ** gen_array_pointer(gen_array_t);
 int gen_array_nitems(gen_array_t);
 int gen_array_size(gen_array_t);
-char * gen_array_item(gen_array_t, int);
+void * gen_array_item(gen_array_t, int);
 void gen_array_sort(gen_array_t);
+gen_array_t gen_array_from_list(list);
+list list_from_gen_array(gen_array_t);
 
 #define GEN_ARRAY_MAP(s, code, array)			\
   {							\
