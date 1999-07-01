1f95dccff195325733d69e715eba6d1968f77b69
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
1999-07-01 11:26:49 +0000

    # on 1999/07/01 11:26:49, coelho did: a basic ri to c dumper. very limited, this is not f2c!!!
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10153 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/prettyprint/cprettyprinter.c
@@ -0,0 +1,13 @@
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+
+bool cprinted_file(string module_name)
+{
+	return FALSE;
+}
+
+bool cindent_file(string module_name)
+{
+        return FALSE;
+}
