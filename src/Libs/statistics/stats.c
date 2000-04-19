cc489a6c548398446598afeacfe9c2173591f04a
boucheba <boucheba@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-04-19 08:32:09 +0000

    # on 2000/04/19 08:32:09, boucheba did: ok for first installation.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10217 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/statistics/stats.c
+++ src/Libs/statistics/stats.c
@@ -2,6 +2,20 @@
  * $Id$
  */
 
+#include <stdio.h>
+
+#include "genC.h"
+#include "linear.h"
+
+#include "ri.h"
+#include "database.h"
+
+#include "misc.h"
+#include "ri-util.h"
+#include "pipsdbm.h"
+
+#include "statistics.h"
+
 int loop_statistics(string name)
 {
   debug_on("STATISTICS_DEBUG_LEVEL");
