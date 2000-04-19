dd379da9e61de653ab6d7052815bd5a7382ebd1b
boucheba <boucheba@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-04-19 09:43:17 +0000

    # on 2000/04/19 09:43:17, boucheba did: au travai!
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10220 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/statistics/stats.c
+++ src/Libs/statistics/stats.c
@@ -10,18 +10,42 @@
 #include "ri.h"
 #include "database.h"
 
+#include "resources.h"
 #include "misc.h"
 #include "ri-util.h"
 #include "pipsdbm.h"
 
 #include "statistics.h"
 
+static bool loop_flt(loop l)
+{
+  fprintf(stderr, "IN loop on %s\n", entity_name(loop_index(l)));
+  return TRUE;
+}
+
+static void loop_rwt(loop l)
+{
+  fprintf(stderr, "OUT loop on %s\n", entity_name(loop_index(l)));
+}
+
+
 int loop_statistics(string name)
 {
+  statement stat;
+
   debug_on("STATISTICS_DEBUG_LEVEL");
   pips_debug(1, "considering module %s\n", name);
 
-  /* ... */
+  set_current_module_entity(local_name_to_top_level_entity(name));
+ 
+  stat = (statement) db_get_memory_resource(DBR_CODE, name, TRUE); /* ??? */
+
+  gen_context_multi_recurse
+    (stat, NULL,
+     loop_domain, loop_flt, loop_rwt,
+     NULL);
+
+  DB_PUT_FILE_RESOURCE(DBR_STATS_FILE, strdup(name), NULL);
 
   pips_debug(1, "done.\n");
   debug_off();
