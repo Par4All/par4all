9e7bf5a1416446bbab12b65e8aca1ba7e221ce49
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-04-21 06:47:17 +0000

    # on 2000/04/21 06:47:17, coelho did: dates fixed.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10226 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/top-level/logging.c
+++ src/Libs/top-level/logging.c
@@ -76,9 +76,9 @@ log_on_file(char chaine[])
 
 #define PIPS_THANKS_STRING						\
   "%s (ARCH=" SOFT_ARCH ")\n  running as %s\n\n"			\
-  "  (c) 1988-1998 Centre de Recherche en Informatique,\n"		\
+  "  (c) 1988-200 Centre de Recherche en Informatique,\n"		\
   "                École des mines de Paris, France.\n\n"		\
-  "  URL: http://www.cri.ensmp.fr/pips\n"				\
+  "  URL: http://www.cri.ensmp.fr/pips/\n"				\
   "  E-MAIL: pipsgroup@cri.ensmp.fr\n\n"				\
   "  This software is provided as is, under the terms of the GPL.\n"	\
   "  It includes software from GNU (readline, rx) and Berkeley (fsplit).\n\n"
