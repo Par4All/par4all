940fd4f958e22d98693fb2952c3c64b156d29301
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
2000-04-25 11:54:30 +0000

    # on 2000/04/25 11:54:30, coelho did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@676 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

---
+++ scripts/tabulated_count.pl
@@ -0,0 +1,13 @@
+#! /usr/local/bin/perl -wn
+#
+# $Id$ 
+#
+# reports the number of tabulated definitions in a file
+#
+
+@l = /DT\d+ /g;
+$n += @l;
+
+END {
+  print "number of tabulated definitions: $n\n";
+}
