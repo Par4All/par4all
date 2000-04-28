f7857b1747aa2e0fe8e0aae6e927918f2afd2d92
boucheba <boucheba@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-04-28 09:51:19 +0000

    # on 2000/04/28 09:51:19, boucheba did: comments added.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10239 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/stats/cumulate_statistics.pl
+++ src/Scripts/stats/cumulate_statistics.pl
@@ -2,6 +2,13 @@
 #
 # $Id$
 #
+# Accumulate counts reported in a file of format: 
+# "<what is counted...>: <number>\n"
+#
+# Exemple of usage:
+#
+# cumulate_statistics.pl y.database/*/*.loop_stats
+#
 
 ($nom, $nombre) = split /: /;
 $stats{$nom} += $nombre;
