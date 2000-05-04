97a3b801cc80724fe9e4218556683ee7dc962641
boucheba <boucheba@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-05-04 08:18:56 +0000

    # on 2000/05/04 08:18:56, boucheba did: no modification
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10252 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/statistics/stats.c
+++ src/Libs/statistics/stats.c
@@ -16,7 +16,7 @@
 
 static int depth, nbr_perfectly_nested_loop, nbr_no_perfectly_nested_loop, 
 nbr_interested_loop,cpt, nbr_nested_loop, nbr_perf_nest_loop_of_depth[100],
-  nbr_no_perf_nest_loop_of_depth[100], max_depth_perfect, nbr_loop,s,i,
+  nbr_no_perf_nest_loop_of_depth[100], max_depth_perfect, nbr_loop,som,ind,
 max_depth_no_perfect;
 static bool perfectly_nested ,first_turn ;
  
@@ -68,7 +68,7 @@ static bool loop_flt( loop l)
               list l =sequence_statements(seq); 
               MAP(STATEMENT,s, {k++;if
               ( instruction_loop_p(statement_instruction(s)))
-              position=k ; };, l); 
+              position=k ;    ; };, l); 
 	      if (position!=0 ){ 
                    k=0; 
 	           MAP(STATEMENT,s, 
@@ -91,14 +91,20 @@ static bool loop_flt( loop l)
                        return FALSE; 
                    } 
                    else return TRUE;
-	       };
+	      }
+              else { k=0; 
+                   MAP(STATEMENT,s, {if
+                   ( instruction_test_p(statement_instruction(s)))
+                    { };    ; };, l); 
+              };
+                    
 	      if (cpt ==1){
-                 cpt++; loop_rwt();
-              }; 
+                   cpt++; loop_rwt();
+                };  
 	      return FALSE;
            
 	 };     
-         if (cpt ==1) {
+         if (cpt ==1) { 
              cpt++; loop_rwt();
          }; 
          return FALSE;
@@ -189,7 +195,7 @@ static void initialize ()
    nbr_perfectly_nested_loop=0; nbr_no_perfectly_nested_loop=0;
    nbr_interested_loop=0;
    cpt=0; nbr_nested_loop=0 ; perfectly_nested =TRUE, max_depth_perfect=0  ;
-   nbr_loop=0;max_depth_no_perfect=0;first_turn=TRUE,s=0;
+   nbr_loop=0;max_depth_no_perfect=0;first_turn=TRUE,som=0;
 } 
 static void put_result(string filename)
 {
@@ -215,41 +221,114 @@ static void put_result(string filename)
   safe_fclose(file, filename);
 }
 
+typedef enum { is_unknown, is_a_loop, is_a_call } contenu_t;
+
+typedef struct {
+  hash_table contenu;
+  stack statement_stack;
+  // ...
+} * context_p, context_t;
+
+
+static bool stmt_flt(statement s, context_p context)
+{
+  stack_push(context->statement_stack, s);
+  return TRUE;
+}
+
+
+static void stmt_rwt(statement s, context_p context)
+{
+  stack_pop(context->statement_stack);
+}
+
+/*
+static void seq_rwt(sequence sq, context_p context)
+{
+  contenu_t contenu = is_unknown;
+
+  MAP(STATEMENT, s,
+  {
+    contenu = ...;
+  },
+      sequence_statements(sq));
+
+  hash_put(context->contenu,
+	   stack_head(context->statement_stack), 
+	   contenu);
+} 
+
+static void uns_rwt(unstructured u, context_p context)
+{
+  hash_put(context->contenu,
+	   stack_head(context->statement_stack), 
+	   is_bad... );
+} */
+
 int loop_statistics(string name)
 {
   statement stat;
   string filename, localfilename; 
+  context_t context;
+
   debug_on("STATISTICS_DEBUG_LEVEL");
   pips_debug(1, "considering module %s\n", name);
   set_current_module_entity(local_name_to_top_level_entity(name));
   initialize() ;
   stat = (statement) db_get_memory_resource(DBR_CODE, name, TRUE); /* ??? */
+
+  context.contenu = hash_table_make(hash_pointer, 0); /* statement -> etat */
+  context.statement_stack = stack_make(stack_pointer, 0, 0);
+
+   gen_context_multi_recurse
+     (stat, &context,loop_domain, loop_flt, loop_rwt, NULL);
+    
+    gen_context_multi_recurse
+    (stat, & context, 
+     statement_domain, stmt_flt, stmt_rwt,
+  
+     NULL)
+ 
   gen_context_multi_recurse
-  (stat, NULL,loop_domain, loop_flt, loop_rwt,NULL);
-  gen_context_multi_recurse
-  (stat, NULL,loop_domain, loop_flt1, loop_rwt1,NULL);
+    (stat, & context, 
+     statement_domain, stmt_flt, stmt_rwt,
+     sequence_domain, gen_true, seq_rwt,
+     test_domain, gen_true, test_rwt,
+     loop_domain, loop_flt, loop_rwt,
+     whileloop_domain, gen_true, wl_rwt,
+     unstructured_domain, gen_true, uns_rwt,
+     call_domain, gen_true, call_rwt,
+     expression_domain, gen_false, gen_null,
+     NULL); 
+  
+  hash_table_free(context.contenu);
+  stack_free(context.statement_stack);
+
   localfilename = db_build_file_resource_name(DBR_STATS_FILE,
 					      name, ".loop_stats");
   filename = strdup(concatenate(db_get_current_workspace_directory(), 
 				"/", localfilename, NULL));
-
-  /*  s=0;
-  for (i=1;i<=20;i++)
-    s=s+nbr_no_perf_nest_loop_of_depth[i];
-    if (s != nbr_no_perfectly_nested_loop) 
-    {
-      printf(" erreur \n %d %d", s,nbr_no_perfectly_nested_loop);
-      getchar();
-      };  */ 
+ 
   put_result(filename);
   free(filename);
   DB_PUT_FILE_RESOURCE(DBR_STATS_FILE, name, localfilename);
   reset_current_module_entity();
-  pips_debug(1, "done.\n");
+  
+
+
+pips_debug(1, "done.\n");
   debug_off();
    
   return TRUE;
 }
+
+
+
+
+
+
+
+
 
 
 
