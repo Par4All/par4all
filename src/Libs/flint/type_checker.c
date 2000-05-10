47602e475c44d17fe89a1d74fa668eaded5b7208
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-05-10 13:08:14 +0000

    # on 2000/05/10 13:08:14, coelho did: cleaner...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10280 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/flint/type_checker.c
+++ src/Libs/flint/type_checker.c
@@ -12,24 +12,27 @@
 
 #include "ri.h"
 #include "ri-util.h"
-
 #include "misc.h"
-
 #include "pipsdbm.h"
-#include "resources.h"   
+#include "resources.h"
+
+#include "bootstrap.h" /* type of intrinsics stuff... */
 
 bool type_checker(string name)
 {
-  statement stat = (statement) db_get_memory_resource(DBR_CODE, name, TRUE);
-  set_current_module_statement(stat);
-
+  statement stat;
   debug_on("TYPE_CHECKER_DEBUG_LEVEL");
+  pips_debug(1, "considering module %s\n", name);
+
+  stat = (statement) db_get_memory_resource(DBR_CODE, name, TRUE);
+  set_current_module_statement(stat);
 
   /* do the job. */
 
   DB_PUT_MEMORY_RESOURCE(DBR_CODE, name, stat);
   reset_current_module_statement();
 
+  pips_debug(1, "done");
   debug_off();
   return TRUE;
 }
