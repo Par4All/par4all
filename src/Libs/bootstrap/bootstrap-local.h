a820d0f03cc19b8bc4643505761cffba2b73229c
phamdinh <phamdinh@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-05-11 09:40:24 +0000

    # on 2000/05/11 09:40:24, phamdinh did: type defs added.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10284 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/bootstrap/bootstrap-local.h
+++ src/Libs/bootstrap/bootstrap-local.h
@@ -1,1 +1,19 @@
-/* Empty bootstrap-local.h */
+/* 
+ * $Id$
+ */
+
+/* context for type checking. */
+typedef struct 
+{
+    hash_table types;
+    stack stats;
+    int number_of_error;
+    int number_of_conversion;
+    int number_of_simplication;
+} type_context_t, * type_context_p;
+
+typedef basic (*typing_function_t)(call, type_context_p);
+
+typedef void (*switch_name_function)(call, type_context_p);
+
+
