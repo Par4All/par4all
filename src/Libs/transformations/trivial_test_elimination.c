dceeadf4d4d7a6ab69648b585d3d1b84617ed022
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-05-29 13:24:09 +0000

    # on 2000/05/29 13:24:09, coelho did: remove trivial tests...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10330 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/transformations/trivial_test_elimination.c
@@ -0,0 +1,301 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+#include "ri-util.h"
+#include "text-util.h"
+#include "database.h"
+#include "misc.h"
+#include "pipsdbm.h"
+#include "resources.h"
+#include "properties.h"
+
+#include "effects-generic.h"
+#include "effects-simple.h"
+#include "transformer.h"
+#include "semantics.h"
+#include "control.h"
+
+#include "transformations.h"
+
+/* Statistic stuff: */
+static int trivial_test_removed;
+static int trivial_test_unstructured_removed;
+
+
+static void
+display_trivial_test_statistics()
+{
+  if (get_bool_property("TRIVIAL_TEST_DISPLAY_STATISTICS")) {	
+    int elimination_count = trivial_test_removed + trivial_test_unstructured_removed ;
+    if (elimination_count > 0) {
+      user_log("* %d trivial test part%s %s been discarded. *\n",
+	       elimination_count,
+	       elimination_count > 1 ? "s" : "",
+	       elimination_count > 1 ? "have" : "has");
+      user_log("Structured trivial tests: %d\n" ,
+	       trivial_test_removed);
+	  
+      user_log("Unstructured trivial tests: %d\n" ,
+	       trivial_test_unstructured_removed);
+      /* Display also the statistics about clean_up_sequences
+	 that is called in suppress_trivial_test: */
+      display_clean_up_sequences_statistics();
+    }
+  }
+}
+
+
+static expression
+MakeInvertExpression(expression e)
+{
+  expression nc = expression_undefined;
+  expression tmpc = expression_undefined;
+
+  if (logical_expression_p(e)) {
+      entity op = call_function(syntax_call(expression_syntax(e)));
+      list args = call_arguments(syntax_call(expression_syntax(e)));
+      expression e1 = expression_undefined;
+      expression e2 = expression_undefined;
+      if (!ENDP(args)) {
+	e1 =  EXPRESSION(CAR(args));
+	if (!ENDP(CDR(args)))  
+	  e2 = EXPRESSION(CAR(CDR(args)));
+      }
+ 
+      if (relational_expression_p(e)) {
+	normalized n1 = NORMALIZE_EXPRESSION(e1);
+	normalized n2 = NORMALIZE_EXPRESSION(e2);
+	  
+	if (normalized_linear_p(n1) && normalized_linear_p(n2)) {
+
+	  if (ENTITY_NON_EQUAL_P(op)) {
+	    nc = eq_expression(e1, e2);
+	  }		    
+          else if (ENTITY_EQUAL_P(op)) {
+	    nc = ne_expression(e1, e2);
+	  }
+	  else if (ENTITY_GREATER_OR_EQUAL_P(op)) {
+	    nc = lt_expression(e1, e2);
+	  }
+	  else if (ENTITY_LESS_OR_EQUAL_P(op)) {
+	    nc = gt_expression(e1, e2);
+	  }
+	  else if (ENTITY_LESS_THAN_P(op)) {
+	    nc = ge_expression(e1, e2);
+	  }
+	  else if (ENTITY_GREATER_THAN_P(op)) {
+	    nc = le_expression(e1, e2);
+	  }
+	}
+      }
+      else if (logical_operator_expression_p(e)) {
+	if (ENTITY_NOT_P(op)) {
+	  tmpc = copy_expression(e1);
+	}
+	else if (ENTITY_AND_P(op)) {
+	  expression tmpc1 = MakeInvertExpression(e1);
+	  expression tmpc2 = MakeInvertExpression(e2);
+          tmpc = or_expression(tmpc1, tmpc2);
+	}
+	else if (ENTITY_OR_P(op)) {
+	  expression tmpc1 = MakeInvertExpression(e1);
+	  expression tmpc2 = MakeInvertExpression(e2);
+          tmpc = and_expression(tmpc1, tmpc2);
+	}
+	else if (ENTITY_EQUIV_P(op)) {
+	  // expression tmpc1 = MakeInvertExpression(e1);
+	  // expression tmpc2 = MakeInvertExpression(e2);
+	  tmpc = binary_intrinsic_expression(NON_EQUIV_OPERATOR_NAME, e1, e2);
+	}	  
+	else if (ENTITY_NON_EQUIV_P(op)) {
+	  // expression tmpc1 = MakeInvertExpression(e1);
+	  // expression tmpc2 = MakeInvertExpression(e2);
+	  tmpc = binary_intrinsic_expression(EQUIV_OPERATOR_NAME, e1, e2);
+	}
+
+	if (true_expression_p(tmpc))
+	  nc =  make_false_expression();
+	else if (false_expression_p(tmpc))
+	  nc = make_true_expression();
+	else 
+	  nc = copy_expression(tmpc);
+      }
+  }
+  return (nc) ;
+}
+
+
+static void
+trivial_test_deal_with_test(statement s) 
+{  
+  instruction i = statement_instruction(s);
+  test t = instruction_test(i);
+  expression cond = test_condition(t);
+  expression ncond = MakeInvertExpression(cond);
+
+  test  new = make_test(copy_expression(ncond),
+			copy_statement(test_false(t)),
+			make_block_statement(NIL));
+  statement_instruction(s) = make_instruction(is_instruction_test, new);
+
+  free_instruction(i);    
+  trivial_test_removed++;
+}
+
+
+static void
+trivial_test_deal_with_unstructured(persistant_statement_to_control m, statement s) 
+{
+  instruction i = statement_instruction(s);
+  test t = instruction_test(i);
+  expression cond = test_condition(t);
+  expression ncond = MakeInvertExpression(cond);
+
+  control c = apply_persistant_statement_to_control(m,s);
+  control true = CONTROL(CAR(control_successors(c)));
+  // control false = CONTROL(CAR(CDR(control_successors(c))));
+ 
+  // control tmp = copy_control(false);
+  // false = copy_control(true);
+  // true = copy_control(tmp);
+  // free_control(tmp);
+
+  // update_control_lists(c, m);
+  // UPDATE_CONTROL(c, s, control_predecessors(c), control_successors(c));
+  
+  // statement new_true = control_statement(false);
+ 
+  // make new test with control
+  // test  new = make_test(copy_expression(ncond),
+  //		 make_continue_statement(statement_label(new_true)),
+  //		 copy_statement(new_true),
+  //             make_block_statement(NIL));
+
+
+  // make new test with test_false()
+
+  test  new = make_test(copy_expression(ncond),
+			copy_statement(test_false(t)),
+			make_block_statement(NIL));
+
+  gen_remove(&control_successors(c), true);
+  gen_remove(&control_predecessors(true), c);
+   
+  statement_instruction(s) = make_instruction(is_instruction_test, new);
+
+  free_instruction(i);
+  trivial_test_unstructured_removed++;
+}
+
+
+static void
+trivial_test_statement_rewrite(statement s, persistant_statement_to_control m)
+{
+   instruction i = statement_instruction(s);
+   tag t = instruction_tag(i);
+
+   switch(t) {
+   case is_instruction_test:
+   {
+     test t ;
+     statement true ;
+
+     debug(2, "trivial_test_statement_rewrite", "is_instruction_test\n", "\n");
+     ifdebug(9) {
+       print_statement(s);
+     }
+
+     t = instruction_test(i) ;
+     true = test_true(t) ;
+     if (empty_statement_or_continue_p(true)) { 
+       if (bound_persistant_statement_to_control_p(m, s))
+	 trivial_test_deal_with_unstructured(m, s);        // unstructured case       
+       else 
+	 trivial_test_deal_with_test(s);        // structured case
+     }
+     break;
+   }
+   case is_instruction_call:
+   case is_instruction_whileloop:
+   case is_instruction_sequence:
+   case is_instruction_loop:
+   case is_instruction_unstructured:
+     break;
+   default:
+     pips_error("", "Unexpected instruction tag %d\n", t);
+     break;
+   }
+}
+
+
+static bool store_mapping(control c, persistant_statement_to_control map)
+{
+  extend_persistant_statement_to_control(map, control_statement(c), c);
+  return TRUE;
+}
+
+
+/* Suppress the test vide of the given module: */
+
+void
+suppress_trivial_test_statement(statement mod_stmt)
+{
+  persistant_statement_to_control map;
+  
+  map = make_persistant_statement_to_control();
+  gen_context_multi_recurse(mod_stmt, map, control_domain, store_mapping, gen_null,
+			    statement_domain, gen_true, trivial_test_statement_rewrite, NULL);
+  free_persistant_statement_to_control(map);
+}
+
+
+/*
+ * Trivial test elimination     
+ * mod_name : MODule NAME, nom du programme Fortran
+ * mod_stmt : MODule STateMenT
+ */
+
+bool
+suppress_trivial_test(char * mod_name)  
+{
+  /* Get the true ressource, not a copy. */
+  statement mod_stmt;
+  set_current_module_entity(local_name_to_top_level_entity(mod_name));
+  mod_stmt= (statement) db_get_memory_resource(DBR_CODE, mod_name, TRUE);
+  set_current_module_statement(mod_stmt);
+  initialize_ordering_to_statement(mod_stmt);	
+
+  debug_on("TRIVIAL_TEST_DEBUG_LEVEL");
+
+  ifdebug(1) {
+      debug(1,"trivial_test_elimination", "Begin for %s\n", mod_name);
+      pips_assert("Statements inconsistants...", statement_consistent_p(mod_stmt));
+  }
+  
+  trivial_test_removed = 0 ;
+  trivial_test_unstructured_removed = 0;
+
+  suppress_trivial_test_statement(mod_stmt);
+  display_trivial_test_statistics();
+
+  debug_off();
+
+  /* Reorder the module, because new statements have been generated. */
+  module_reorder(mod_stmt);
+
+  DB_PUT_MEMORY_RESOURCE(DBR_CODE, strdup(mod_name), mod_stmt);
+
+  reset_current_module_statement();
+  reset_current_module_entity();
+
+  debug(1,"trivial_test_elimination", "End for %s\n", mod_name);
+  
+  ifdebug(1)
+      pips_assert("Statements inconsistants...", statement_consistent_p(mod_stmt));
+  
+  return TRUE;
+}
