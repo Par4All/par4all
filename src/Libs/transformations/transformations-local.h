89fd5bbdcc264912b38edb79ddf7a20a3b0c05a2
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-07-03 13:05:02 +0000

    # on 2000/07/03 13:05:02, coelho did: eformat moved to expressions.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10392 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformations/transformations-local.h
+++ src/Libs/transformations/transformations-local.h
@@ -2,21 +2,6 @@
 #define FORTRAN_DIV(n,d) (SIGN_EQ((n),(d)) ? ABS(n)/ABS(d) : -(ABS(n)/ABS(d)))
 #define FORTRAN_MOD(n,m) (SIGN_EQ((n),(m)) ? ABS(n)%ABS(m) : -(ABS(n)%ABS(m)))
 
-
-/*
- * EFORMAT: the expression format used in recursiv evaluations.
- * = ((ICOEF * EXPR) + ISHIFT)
- * it is SIMPLER when it is interesting to replace initial expression by the
- * one generated from eformat.
- */
-struct eformat {
-    expression expr;
-    int icoef;
-    int ishift;
-    boolean simpler;
-};
-
-
 /* What is returned by dead_test_filter : */
 enum dead_test { nothing_about_test, then_is_dead, else_is_dead };
 typedef enum dead_test dead_test;
