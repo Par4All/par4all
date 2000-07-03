84d52d4fa78a8f24db917b47a7a72d324fdd59ef
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-07-03 13:04:52 +0000

    # on 2000/07/03 13:04:52, coelho did: eformat moved here.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10391 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/expressions/expressions-local.h
+++ src/Libs/expressions/expressions-local.h
@@ -1,3 +1,19 @@
 /*
  * $Id$
  */
+
+/*
+ * for partial_eval.c:
+ *
+ * EFORMAT: the expression format used in recursiv evaluations. 
+ * = ((ICOEF * EXPR) + ISHIFT)
+ * it is SIMPLER when it is interesting to replace initial expression by the
+ * one generated from eformat.
+ */
+struct eformat {
+    expression expr;
+    int icoef;
+    int ishift;
+    boolean simpler;
+};
+
