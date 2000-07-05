1ccf15364a81735a45432b9273af10aee3b45cee
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
2000-07-05 15:51:58 +0000

    # on 2000/07/05 15:51:58, coelho did: new sc_feasibility version which deal with == before chosing between FM and SIMPLEX.
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@968 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/sc/sc_feasibility.c
+++ src/sc/sc_feasibility.c
@@ -64,6 +64,7 @@ boolean ofl_res;
 /*
  * LOW LEVEL FUNCTIONS 
  */
+
 /*  just a test to improve the Simplex/FM decision.
  * c is a list of constraints, equalities or inequalities
  * pc is the number of constraints in the list
@@ -90,6 +91,177 @@ int *pc, *pv, weight;
     }
 }
 
+/* chose the next variable in base b for projection in system s.
+ * tries to avoid Fourier potential explosions when combining inequalities.
+ * - if there are equalities, chose the var with the min |coeff| (not null)
+ * - if there are only inequalities, chose the var that will generate the
+ *   minimum number of constraints with pairwise combinations.
+ * - if ineq is TRUE, consider variables even if no equalities.
+ *
+ * (c) FC 21 July 1995
+ */
+static Variable
+chose_variable_to_project_for_feasability(Psysteme s, Pbase b, boolean ineq)
+{
+  Pcontrainte c = sc_egalites(s);
+  Pvecteur v;
+  Variable var = NULL;
+  Value val;
+  int size = vect_size(b);
+
+  ifscdebug(8)
+    {
+      fprintf(stderr, "[chose_variable_to_project_for_feasability] b/s:\n");
+      vect_fprint(stderr, b, default_variable_to_string);
+      sc_fprint(stderr, s, default_variable_to_string);
+    }
+
+  if (size==1) return var_of(b);
+  assert(size>1);
+    
+  if (c)
+  {
+    /* find the lowest coeff 
+     */
+    Variable minvar = TCST;
+    Value minval = VALUE_ZERO;
+
+    for (; c; c=c->succ)
+    {
+      for (v = contrainte_vecteur(c); v; v=v->succ)
+      {
+	var = var_of(v);
+
+	if (var!=TCST)
+	{
+	  val = value_abs(val_of(v));
+	  if ((value_notzero_p(minval) && value_lt(val,minval))
+	      || value_zero_p(minval)) 
+	    minval = val, minvar = var;
+		     
+	  if (value_one_p(minval)) return minvar;
+	}
+      }
+    }
+
+    /* shouldn't find empty equalities ?? */
+    /* assert(minvar!=TCST); */
+    var = minvar;
+  }
+  
+  if (!var && ineq)
+  {
+    /* only inequalities, reduce the explosion
+     */
+    int i;
+    two_int_infop t = (two_int_infop) malloc(2*size*sizeof(int));
+    Pbase tmp;
+    int min_new;
+
+    c = sc_inegalites(s);
+
+    /* initialize t
+     */
+    for (i=0; i<size; i++) t[i][0]=0, t[i][1]=0;
+
+    /* t[x][0 (resp. 1)] = number of negative (resp. positive) coeff
+     */
+    for (; c; c=c->succ)
+    {
+      for (v = contrainte_vecteur(c); v; v=v->succ)
+      {
+	var = var_of(v); 
+	if (var!=TCST)
+	{
+	  ifscdebug(9) 
+	    fprintf(stderr, "%s\n", default_variable_to_string(var));
+
+	  for (i=0, tmp=b; tmp && var_of(tmp)!=var; 
+	       i++, tmp=tmp->succ);
+	  assert(tmp);
+		    
+	  t[i][value_posz_p(val_of(v))]++;
+	}
+      }
+    }
+
+    /* t[x][0] = number of combinations, i.e. new created constraints.
+     */
+    for (i=0; i<size; i++) t[i][0] *= t[i][1];
+
+    for (tmp=b->succ, var=var_of(b), min_new=t[0][0], i=1;
+	 min_new && i<size; 
+	 i++, tmp=tmp->succ) {
+      if (t[i][0]<min_new)
+	min_new = t[i][0], var=var_of(tmp); 
+    }
+
+    free(t);
+  }
+
+  ifscdebug(8)
+    fprintf(stderr, "[chose_variable_to_project_for_feasability] "
+	    "suggesting %s\n", default_variable_to_string(var));
+
+  return var;
+}
+
+/* project in s1 (which is modified) 
+ * using equalities or both equalities and inequalities.
+*/
+static boolean sc_fm_project_variables
+(Psysteme s1, boolean integer_p, boolean use_eq_only, int ofl_ctrl)
+{
+  Pbase b = base_dup(sc_base(s1));
+  Variable var;
+  boolean faisable = TRUE;
+    
+  while (b && faisable)
+  {
+    var = chose_variable_to_project_for_feasability(s1, b, !use_eq_only);
+
+    /* if use_eq_only */
+    if (!var) break;
+
+    vect_erase_var(&b, var);
+
+    ifscdebug(8)
+      {
+	fprintf(stderr, 
+		"[sc_fm_project_variables] system before %s projection:\n", 
+		var);
+	sc_fprint(stderr, s1, default_variable_to_string);
+      }
+	    
+    sc_projection_along_variable_ofl_ctrl(&s1, var, ofl_ctrl);
+
+    ifscdebug(8)
+    {
+      fprintf(stderr, 
+	      "[sc_fm_project_variables] system after projection:\n");
+      sc_fprint(stderr, s1, default_variable_to_string);
+    }
+	    
+    if (sc_empty_p(s1))
+    {
+      faisable = FALSE;
+      break;
+    }
+
+    if (integer_p) {
+      s1 = sc_normalize(s1);
+      if (SC_EMPTY_P(s1)) 
+      { 
+	faisable = FALSE; 
+	break;
+      }
+    }
+  }  
+
+  base_rm(b);
+  return faisable;
+}
+
 boolean 
 sc_feasibility_ofl_ctrl(sc, integer_p, ofl_ctrl, ofl_res)
 Psysteme sc;
@@ -97,193 +269,92 @@ boolean integer_p;
 int ofl_ctrl;
 boolean ofl_res;
 {
-    int
-      n_var = sc->dimension,
-      n_cont_eq = 0, n_ref_eq = 0,
-      n_cont_in = 0, n_ref_in = 0;
-    boolean 
-      ok = FALSE,
-      use_simplex = FALSE, 
-      catch_performed = FALSE;
+  int
+    n_var = sc->dimension,
+    n_cont_eq = 0, n_ref_eq = 0,
+    n_cont_in = 0, n_ref_in = 0;
+  boolean 
+    ok = FALSE,
+    use_simplex = FALSE, 
+    catch_performed = FALSE;
+  Psysteme sw = NULL;
 
-    decision_data(sc_egalites(sc), &n_cont_eq, &n_ref_eq, 2);
-    decision_data(sc_inegalites(sc), &n_cont_in, &n_ref_in, 1);
+  if (sc_rn_p(sc)) 
+    return TRUE;
 
-    /* a little discussion about what to decide (FC, 05/07/2000)
-     *
-     * - FM is good at handling equalities which are simply projected, 
-     *   but may explode with many inequalities when they are combined.
-     *   it is quite fast with few inequalities anyway.
-     *
-     * - SIMPLEX switches every eq to 2 inequalities, adding hyperplanes.
-     *   thus it is not that good with equalities.
-     *   it is quite slow with small systems because of the dense matrix
-     *   to build and manipulate.
-     *
-     * suggestion to be implemented and tested : 
-     *  1/ project equalities as much as possible.
-     *  2/ chose between FM and Simplex after that?
-     */
-    use_simplex = (n_cont_in >= NB_CONSTRAINTS_MAX_FOR_FM || 
-		   (n_cont_in>=10 && n_ref_in>2*n_cont_in));
-
-    if (sc_rn_p(sc)) 
-	return TRUE;
-
-    /* else
-     */
-    switch (ofl_ctrl) 
-    {
-    case OFL_CTRL :
-	ofl_ctrl = FWD_OFL_CTRL;
-	catch_performed = TRUE;
-	CATCH(overflow_error) 
-	{
-	    ok = ofl_res;
-	    /* 
-	     *   PLEASE do not remove this warning.
-	     *
-	     *   FC 30/01/95
-	     */
-	    fprintf(stderr, "[sc_feasibility_ofl_ctrl] "
-		    "arithmetic error (%s[%d,%deq/%dref,%din/%dref]) -> %s\n",
-		    use_simplex ? "Simplex" : "Fourier-Motzkin", 
-		    n_var, n_cont_eq, n_ref_eq, n_cont_in, n_ref_in,
-		    ofl_res ? "TRUE" : "FALSE");
-	    break;
-	}		
-    default: 
-    {
-	if (use_simplex)
-	{
-	    ok = sc_simplexe_feasibility_ofl_ctrl(sc, ofl_ctrl);
-	}
-	else 
-	{
-	    ok = sc_fourier_motzkin_feasibility_ofl_ctrl(sc, integer_p, 
-							 ofl_ctrl);
-	}
-	if (catch_performed)
-	    UNCATCH(overflow_error);
-    }
-    }
-
-    return ok;
-}
-
-/* chose the next variable in base b for projection in system s.
- * tries to avoid Fourier potential explosions when combining inequalities.
- * - if there are equalities, chose the var with the min |coeff| (not null)
- * - if there are only inequalities, chose the var that will generate the
- *   minimum number of constraints with pairwise combinations.
- *
- * (c) FC 21 July 1995
- */
-static Variable
-chose_variable_to_project_for_feasability(s, b)
-Psysteme s;
-Pbase b;
-{
-    Pcontrainte c = sc_egalites(s);
-    Pvecteur v;
-    Variable var;
-    Value val;
-    int size = vect_size(b);
-
-    ifscdebug(8)
-    {
-	fprintf(stderr, "[chose_variable_to_project_for_feasability] b/s:\n");
-	vect_fprint(stderr, b, default_variable_to_string);
-	sc_fprint(stderr, s, default_variable_to_string);
-    }
-
-    if (size==1) return var_of(b);
-    assert(size>1);
-    
-    if (c)
-    {
-	/* find the lowest coeff 
+  /* else
+   */
+  switch (ofl_ctrl) 
+  {
+  case OFL_CTRL :
+    ofl_ctrl = FWD_OFL_CTRL;
+    catch_performed = TRUE;
+    CATCH(overflow_error) 
+      {
+	ok = ofl_res;
+	/* 
+	 *   PLEASE do not remove this warning.
+	 *
+	 *   FC 30/01/95
 	 */
-	Variable minvar = TCST;
-	Value minval = VALUE_ZERO;
-
-	for (; c; c=c->succ)
-	{
-	    for (v = contrainte_vecteur(c); v; v=v->succ)
-	    {
-		 var = var_of(v);
-
-		 if (var!=TCST)
-		 {
-		     val = value_abs(val_of(v));
-		     if ((value_notzero_p(minval) && value_lt(val,minval))
-			 || value_zero_p(minval)) 
-			 minval = val, minvar = var;
-		     
-		     if (value_one_p(minval)) return minvar;
-		 }
-	    }
-	}
-
-	assert(minvar!=TCST); /* shouldn't find empty equalities ?? */
-	var = minvar;
-    }
-    else
+	fprintf(stderr, "[sc_feasibility_ofl_ctrl] "
+		"arithmetic error (%s[%d,%deq/%dref,%din/%dref]) -> %s\n",
+		use_simplex ? "Simplex" : "Fourier-Motzkin", 
+		n_var, n_cont_eq, n_ref_eq, n_cont_in, n_ref_in,
+		ofl_res ? "TRUE" : "FALSE");
+	break;
+      }		
+  default: /* anyway, try */
     {
-	/* only inequalities, reduce the explosion
-	 */
-	int i;
-	two_int_infop t = (two_int_infop) malloc(2*size*sizeof(int));
-	Pbase tmp;
-	int min_new;
+      /* a little discussion about what to decide (FC, 05/07/2000)
+       *
+       * - FM is good at handling equalities which are simply projected, 
+       *   but may explode with many inequalities when they are combined.
+       *   it is quite fast with few inequalities anyway.
+       *
+       * - SIMPLEX switches every eq to 2 inequalities, adding hyperplanes.
+       *   thus it is not that good with equalities.
+       *   it is quite slow with small systems because of the dense matrix
+       *   to build and manipulate.
+       *
+       * suggestion implemented here: 
+       *  1/ project equalities as much as possible.
+       *  2/ chose between FM and SIMPLEX **after** that?
+       */
 
-	c = sc_inegalites(s);
+      sw = sc_dup(sc);
 
-	/* initialize t
-	 */
-	for (i=0; i<size; i++) t[i][0]=0, t[i][1]=0;
+      ok = sc_fm_project_variables(sw, integer_p, TRUE, ofl_ctrl);
 
-	/* t[x][0 (resp. 1)] = number of negative (resp. positive) coeff
-	 */
-	for (; c; c=c->succ)
-	{
-	    for (v = contrainte_vecteur(c); v; v=v->succ)
-	    {
-		var = var_of(v); 
-		if (var!=TCST)
-		{
-		    ifscdebug(9) 
-			fprintf(stderr, "%s\n", 
-				default_variable_to_string(var));
+      if (!ok) break;
 
-		    for (i=0, tmp=b; tmp && var_of(tmp)!=var; 
-			 i++, tmp=tmp->succ);
-		    assert(tmp);
-		    
-		    t[i][value_posz_p(val_of(v))]++;
-		}
-	    }
-	}
+      /* else it looks feasible as far as equalities are concerned, 
+       * let us deal with inequalities now.
+       */
+	
+      decision_data(sc_egalites(sw), &n_cont_eq, &n_ref_eq, 2);
+      decision_data(sc_inegalites(sw), &n_cont_in, &n_ref_in, 1);
 
-	/* t[x][0] = number of combinations, i.e. new created constraints.
-	 */
-	for (i=0; i<size; i++) t[i][0] *= t[i][1];
+      use_simplex = (n_cont_in >= NB_CONSTRAINTS_MAX_FOR_FM || 
+		     (n_cont_in>=10 && n_ref_in>2*n_cont_in));
 
-	for (tmp=b->succ, var=var_of(b), min_new=t[0][0], i=1;
-	     min_new && i<size; 
-	     i++, tmp=tmp->succ) {
-	    if (t[i][0]<min_new)
-		min_new = t[i][0], var=var_of(tmp); 
-	}
-
-	free(t);
+      if (use_simplex)
+      {
+	ok = sc_simplexe_feasibility_ofl_ctrl(sw, ofl_ctrl);
+      }
+      else 
+      {
+	ok = sc_fourier_motzkin_feasibility_ofl_ctrl(sw, integer_p, ofl_ctrl);
+      }
     }
+  }
 
-    ifscdebug(8)
-	fprintf(stderr, "[chose_variable_to_project_for_feasability] "
-		"suggesting %s\n", default_variable_to_string(var));
-
-    return var;
+  if (sw) sc_rm(sw);
+  
+  if (catch_performed)
+    UNCATCH(overflow_error);
+  
+  return ok;
 }
 
 /* boolean sc_fourier_motzkin_faisabilite_ofl(Psysteme s):
@@ -308,81 +379,35 @@ Psysteme s;
 boolean integer_p;
 int ofl_ctrl;
 {
-    Psysteme s1;
-    boolean faisable = TRUE;
+  Psysteme s1;
+  boolean faisable = TRUE;
 
-    if (s == NULL) return TRUE;
-    s1 = sc_dup(s);
+  if (s == NULL) return TRUE;
+  s1 = sc_dup(s);
 
-    ifscdebug(8)
+  ifscdebug(8)
     {
-	fprintf(stderr, "[sc_fourier_motzkin_feasibility_ofl_ctrl] system:\n");
-	sc_fprint(stderr, s1, default_variable_to_string);
+      fprintf(stderr, "[sc_fourier_motzkin_feasibility_ofl_ctrl] system:\n");
+      sc_fprint(stderr, s1, default_variable_to_string);
     }
 
-    s1 = sc_elim_db_constraints(s1);
+  s1 = sc_elim_db_constraints(s1);
 
+  if (s1 != NULL)
+  {
+    /* a small basis if possible... (FC).
+     */
+    base_rm(sc_base(s1));
+    sc_creer_base(s1);
 
-    if (s1 != NULL)
-    {
+    faisable = sc_fm_project_variables(s1, integer_p, FALSE, ofl_ctrl);
 	
-       /* projection successive selon les  variables du systeme
-	*/
-	Variable var;
-	Pbase b = BASE_NULLE;
-        /* a small basis if possible... (FC).
-	 */
-	base_rm(sc_base(s1));
-	sc_creer_base(s1);
-	b = base_dup(sc_base(s1));
-	
-	while (b && faisable)
-	{
-	    var = chose_variable_to_project_for_feasability(s1, b);
-	    vect_erase_var(&b, var);
+    sc_rm(s1);
+  }
+  else 
+    /* sc_kill_db_eg a de'sallouer s1 a` la detection de 
+       sa non-faisabilite */
+    faisable = FALSE;
 
-	    ifscdebug(8)
-	    {
-		fprintf(stderr, "sc_fourier_motzkin_feasibility_ofl_ctrl]"
-			" system before %s projection:\n", var);
-		sc_fprint(stderr, s1, default_variable_to_string);
-	    }
-	    
-	    sc_projection_along_variable_ofl_ctrl(&s1, var, ofl_ctrl);
-
-	    ifscdebug(8)
-	    {
-		fprintf(stderr, "sc_fourier_motzkin_feasibility_ofl_ctrl]"
-			" system after projection:\n");
-		sc_fprint(stderr, s1, default_variable_to_string);
-	    }
-	    
-	    if (sc_empty_p(s1))
-	    {
-		faisable = FALSE;
-		break;
-	    }
-
-	    if (integer_p) {
-		s1 = sc_normalize(s1);
-		if (SC_EMPTY_P(s1)) 
-		{ 
-		    faisable = FALSE; 
-		    break;
-		}
-	    }
-	}
-	
-	sc_rm(s1);
-	base_rm(b);
-    }
-    else 
-	/* sc_kill_db_eg a de'sallouer s1 a` la detection de 
-	   sa non-faisabilite */
-	faisable = FALSE;
-
-    return faisable;
+  return faisable;
 }
-
-
-
