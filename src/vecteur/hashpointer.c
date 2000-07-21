d3310646839b4f78a6e15d54c465ea2d09011b35
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
2000-07-21 12:58:31 +0000

    # on 2000/07/21 12:58:31, coelho did: new version with only one malloc.
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@1011 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/vecteur/hashpointer.c
+++ src/vecteur/hashpointer.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <assert.h>
 
-/* expected headers:
+/* expected headers: the internal structure does not need to be available!
 struct linear_hashtable_st;
 typedef struct linear_hashtable_st * linear_hashtable_pt;
  */
@@ -29,14 +29,18 @@ typedef enum { false, true } boolean;
 #define FREE_CHUNK	((void *) 0)
 #define EMPTIED_CHUNK	((void *) -1)
 
+typedef struct {
+  void * key;
+  void * val;
+} paire;
+
 /* hidden structure to store the hashtable.
  */
 typedef struct linear_hashtable_st 
 {
-  int nitems;	/* number of association stored */
-  int size;	/* size of internal array */
-  void ** keys; /* array for keys */
-  void ** vals; /* array for vals */
+  int nitems;	    /* number of association stored */
+  int size;	    /* size of internal array */
+  paire * contents; /* array storing key&value paires */
 }
   * linear_hashtable_pt;
 
@@ -46,17 +50,20 @@ static int key_location(linear_hashtable_pt h, void * k, boolean toget)
 {
   register int index = ((((int)k)&(0x7fffffff))%(h->size)), loop = h->size;
 
-  while (loop-- && !(h->keys[index]==FREE_CHUNK ||
-		   h->keys[index]==k ||
-		   (toget && h->keys[index]==EMPTIED_CHUNK)))
+  while (loop-- && !(h->contents[index].key==FREE_CHUNK ||
+		   h->contents[index].key==k ||
+		   (toget && h->contents[index].key==EMPTIED_CHUNK)))
     index = (index+1) % h->size;
 
   assert(!(!loop && !toget)); /* should not loop to put! */
 
+  /* if !loop and toget, the initial index is returned.
+   * it is checked against the expected key before returning the value.
+   */
   debug_assert(index>=0 && index<h->size &&
-	       (h->keys[index]==FREE_CHUNK ||
-		h->keys[index]==EMPTIED_CHUNK ||
-		h->keys[index]==k));
+	       (h->contents[index].key==FREE_CHUNK ||
+		h->contents[index].key==EMPTIED_CHUNK ||
+		(h->contents[index].key==k || (!loop && toget))));
 
   return index;
 }
@@ -72,11 +79,11 @@ static void linear_hashtable_print(FILE * file, linear_hashtable_pt h)
   
   for (i=0; i<h->size; i++)
   {
-    register void * k = h->keys[i];
+    register void * k = h->contents[i].key;
     fprintf(file, "%d (%d): 0x%p -> 0x%p\n", 
 	    i, 
 	    (k!=FREE_CHUNK && k!=EMPTIED_CHUNK)? key_location(h, k, true): -1,
-	    k, h->vals[i]);
+	    k, h->contents[i].val);
   }
 
   fprintf(file, "[linear_hashtable_dump] done.\n");
@@ -100,7 +107,7 @@ boolean linear_hashtable_coherent_p(linear_hashtable_pt h)
   /* check number of item stored. */
   for(i=0, n=0; i<h->size; i++)
   {
-    register void * k = h->keys[i];
+    register void * k = h->contents[i].key;
     if (k!=FREE_CHUNK && k!=EMPTIED_CHUNK)
     {
       /* check key index */
@@ -136,14 +143,13 @@ linear_hashtable_pt linear_hashtable_make(void)
 
   h->size = size;
   h->nitems = 0;
-  h->keys = (void **) malloc(sizeof(void *)*size);
-  h->vals = (void **) malloc(sizeof(void *)*size);
+  h->contents = (paire*) malloc(sizeof(paire)*size);
 
-  assert(h->keys && h->vals); /* check malloc */
+  assert(h->contents); /* check malloc */
 
   for (i=0; i<size; i++)
-    h->keys[i] = FREE_CHUNK, 
-      h->vals[i] = FREE_CHUNK;
+    h->contents[i].key = FREE_CHUNK, 
+      h->contents[i].val = FREE_CHUNK;
 
   debug_assert_coherent(h);
 
@@ -155,47 +161,43 @@ void linear_hashtable_free(linear_hashtable_pt h)
 {
   debug_assert_coherent(h);
   
-  free(h->keys);
-  free(h->vals);
+  free(h->contents);
   free(h);
 }
 
 static void linear_hashtable_extend(linear_hashtable_pt h)
 {
-  register void ** oldkeys, ** oldvals;
+  register paire * oldcontents;
   register int i, oldsize, moved_nitems;
 
   debug_assert_coherent(h);
 
   moved_nitems = h->nitems;
+  oldcontents = h->contents;
   oldsize = h->size;
-  oldkeys = h->keys;
-  oldvals = h->vals;
 
   h->size = 2*oldsize + 1;
-  h->keys = (void**) malloc(sizeof(void *)*h->size);
-  h->vals = (void**) malloc(sizeof(void *)*h->size);
-  assert(h->keys && h->vals); /* check malloc */
+  h->contents = (paire*) malloc(sizeof(paire)*h->size);
+  assert(h->contents); /* check malloc */
 
   for (i=0; i<h->size; i++)
-    h->keys[i] = FREE_CHUNK,
-      h->vals[i] = FREE_CHUNK;
+    h->contents[i].key = FREE_CHUNK,
+      h->contents[i].val = FREE_CHUNK;
 
   for (i=0; i<oldsize; i++)
   {
-    register void * k = oldkeys[i];
+    register void * k = oldcontents[i].key;
     if (k!=FREE_CHUNK && k!=EMPTIED_CHUNK)
     {
       register int index = key_location(h, k, false);
-      h->keys[index] = k;
-      h->vals[index] = oldvals[i];
+      h->contents[index].key = k;
+      h->contents[index].val = oldcontents[i].val;
       moved_nitems--;
     }
   }
 
-  assert(moved_nitems==0);
-  free(oldkeys);
-  free(oldvals);
+  assert(moved_nitems==0); /* the expected number of items was moved. */
+  free(oldcontents);
 
   debug_assert_coherent(h);
 }
@@ -207,24 +209,24 @@ static void linear_hashtable_internal_put
 {
   register int index;
 
-  assert(k!=FREE_CHUNK && k!=EMPTIED_CHUNK);
+  assert(k!=FREE_CHUNK && k!=EMPTIED_CHUNK); /* no special values! */
   debug_assert_coherent(h);
 
-  if ((h->nitems*2) > h->size) { /* 50% */
+  if ((h->nitems*2) > h->size) { /* 50% limit to extend */
     linear_hashtable_extend(h);
   }
 
   index = key_location(h, k, true); /* is it already in? */
-  if (h->keys[index]!=k) /* no */
-    index = key_location(h, k, false);
+  if (h->contents[index].key!=k) /* no */
+    index = key_location(h, k, false); /* where should it be put? */
   else 
-    assert(!once);
+    assert(!once); /* if it is in, the once option must not be set */
 
-  if (h->keys[index]!=k)
+  if (h->contents[index].key!=k) /* update number of stored items */
     h->nitems++;
 
-  h->keys[index] = k;
-  h->vals[index] = v;
+  h->contents[index].key = k;
+  h->contents[index].val = v;
 
   debug_assert_coherent(h);
 }
@@ -241,17 +243,17 @@ void linear_hashtable_put_once(linear_hashtable_pt h, void * k, void * v)
 
 boolean linear_hashtable_isin(linear_hashtable_pt h, void * k)
 {
-  return h->keys[key_location(h, k, true)]==k;
+  return h->contents[key_location(h, k, true)].key==k;
 }
 
 boolean linear_hashtable_remove(linear_hashtable_pt h, void * k)
 {
   register int index = key_location(h, k, true);
 
-  if (h->keys[index]==k) 
+  if (h->contents[index].key==k) 
   {
-    h->keys[index] = EMPTIED_CHUNK;
-    h->vals[index] = FREE_CHUNK;
+    h->contents[index].key = EMPTIED_CHUNK;
+    h->contents[index].val = FREE_CHUNK;
     h->nitems--;
     return true;
   }
@@ -262,7 +264,7 @@ boolean linear_hashtable_remove(linear_hashtable_pt h, void * k)
 void * linear_hashtable_get(linear_hashtable_pt h, void * k)
 {
   register int index = key_location(h, k, true);
-  return h->keys[index]==k ? h->vals[index]: FREE_CHUNK;
+  return h->contents[index].key==k ? h->contents[index].val: FREE_CHUNK;
 }
 
 int linear_hashtable_nitems(linear_hashtable_pt h)
