dfe4483fe3dcd2e0d6f691e95799bf3503d7437d
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
2000-07-26 09:46:31 +0000

    # on 2000/07/26 09:46:31, coelho did: TRY/CATCH...
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@1024 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/polyedre/chernikova.c
+++ src/polyedre/chernikova.c
@@ -1,4 +1,6 @@
-
+/*
+ * $Id$
+ */
 #include <malloc.h> 
 #include <stdio.h>
 #include <string.h>
@@ -15,7 +17,7 @@
 #include "sg.h"
 #include "polyedre.h"
 
-/* IRISA  data structures.
+/* IRISA/POLYLIB data structures.
  */
 #include "polylib/polylib.h"
 
@@ -38,23 +40,9 @@
 #define VALUE_TO_IRINT(val) (val)
 #define IRINT_TO_VALUE(i) (i)
 
-/* should be ANSI C headers...
- */
-/*
-extern Matrix * Matrix_Alloc();
-extern void Matrix_Print();
-extern void Matrix_Free();
-extern void Polyhedron_Print();
-extern Polyhedron * Constraints2Polyhedron();
-extern Polyhedron * Rays2Polyhedron();
-extern Matrix * Polyhedron2Constraints();
-extern void Polyhedron_Free();
-*/
-
-/*  Fonctions de conversion traduisant une ligne de la structure 
+/* Fonctions de conversion traduisant une ligne de la structure 
  * Matrix de l'IRISA en un Pvecteur
  */
-
 static Pvecteur matrix_ligne_to_vecteur(mat,i,base,dim)
 Matrix *mat;
 int i;
@@ -66,23 +54,23 @@ int dim;
     boolean NEWPV = TRUE;
 
     for (j=1,pv=base ;j<dim;j++,pv=pv->succ) {
-	if (mat->p[i][j]) 
-	    if (NEWPV) { pvnew= vect_new(vecteur_var(pv),
-					 IRINT_TO_VALUE(mat->p[i][j]));
-			 NEWPV =FALSE;  }		
-	    else vect_add_elem(&pvnew,vecteur_var(pv),
-			       IRINT_TO_VALUE(mat->p[i][j]));
+      if (mat->p[i][j]) {
+	if (NEWPV) { 
+	  pvnew= vect_new(vecteur_var(pv),
+			  IRINT_TO_VALUE(mat->p[i][j]));
+	  NEWPV =FALSE; 
+	}		
+	else 
+	  vect_add_elem(&pvnew,vecteur_var(pv),
+			IRINT_TO_VALUE(mat->p[i][j]));
+      }
     }
-    return(pvnew);
+    return pvnew;
 }
 
-
-
-/*  Fonctions de conversion traduisant une ligne de la structure 
+/* Fonctions de conversion traduisant une ligne de la structure 
  * Matrix de l'IRISA en une Pcontrainte
-*/
-
-
+ */
 static Pcontrainte matrix_ligne_to_contrainte(mat,i,base)
 Matrix * mat;
 int i;
@@ -101,9 +89,7 @@ Pbase base;
 
 /*  Fonctions de conversion traduisant une ligne de la structure 
  *  Polyhedron de l'IRISA en un Pvecteur
-*/
-
-
+ */
 static Pvecteur polyhedron_ligne_to_vecteur(pol,i,base,dim)
 Polyhedron *pol;
 int i;
@@ -115,18 +101,18 @@ int dim;
     boolean NEWPV = TRUE;
 
     for (j=1,pv=base ;j<dim;j++,pv=pv->succ) {
-	if (pol->Ray[i][j]) 
+      if (pol->Ray[i][j]) {
 	    if (NEWPV) { pvnew= vect_new(vecteur_var(pv),
 					 IRINT_TO_VALUE(pol->Ray[i][j]));
 			 NEWPV =FALSE;  }		
 	    else vect_add_elem(&pvnew,vecteur_var(pv),
 			       IRINT_TO_VALUE(pol->Ray[i][j]));
+      }
     }
-    return(pvnew);
+    return pvnew;
 }
 
-
-/*  Fonctions de conversion traduisant une Pray_dte en une 
+/* Fonctions de conversion traduisant une Pray_dte en une 
  * ligne de la structure matrix de l'IRISA 
  */ 
 static void ray_to_matrix_ligne(pr,mat,i,base)
@@ -136,11 +122,11 @@ int i;
 Pbase base;
 {
     Pvecteur pb;
-    int j;
+    unsigned int j;
     Value v;
 
     for (pb = base, j=1; 
-	 !VECTEUR_NUL_P(pb) && j< mat->NbColumns-1; 
+	 !VECTEUR_NUL_P(pb) && j<mat->NbColumns-1; 
 	 pb = pb->succ,j++)
     {
 	v = vect_coeff(vecteur_var(pb),pr->vecteur);
@@ -149,7 +135,7 @@ Pbase base;
 }
 
 
-/*  Fonctions de conversion traduisant une Pcontrainte en une 
+/* Fonctions de conversion traduisant une Pcontrainte en une 
  * ligne de la structure matrix de l'IRISA 
  */
 static void contrainte_to_matrix_ligne(pc,mat,i,base)
@@ -172,17 +158,12 @@ Pbase base;
     mat->p[i][j]= VALUE_TO_IRINT(v);
 }
 
-
-
 /* Passage du systeme lineaire sc a une matrice matrix (structure Irisa)
  * Cette fonction de conversion est utilisee par la fonction 
  * sc_to_sg_chernikova
  */ 
-static void sc_to_matrix(sc,mat)
-Psysteme sc;
-Matrix *mat;
+static void sc_to_matrix(Psysteme sc, Matrix *mat)
 {
-
     int nbrows, nbcolumns, i, j;
     Pcontrainte peq;
     Pvecteur pv;
@@ -216,14 +197,9 @@ Matrix *mat;
  * structure  de Polyhedron de l'Irisa.
  * Cette fonction de conversion est utilisee par la fonction 
  * sg_to_sc_chernikova
-*/ 
-
-
-static void sg_to_polyhedron(sg,mat)
-Ptsg sg;
-Matrix * mat;
+ */ 
+static void sg_to_polyhedron(Ptsg sg, Matrix * mat)
 {
-
     Pray_dte pr;
     Psommet ps;
     int  nbC=0;
@@ -264,9 +240,7 @@ Matrix * mat;
  * sous forme d'un syste`me lineaire sc. Cette fonction est 
  * utilisee paar la fonction sg_to_sc_chernikova 
  */ 
-static void matrix_to_sc(mat,sc)
-Matrix *mat;
-Psysteme sc;
+static void matrix_to_sc(Matrix *mat, Psysteme sc)
 {
     Pcontrainte pce=NULL;
     Pcontrainte pci=NULL;
@@ -321,20 +295,15 @@ Psysteme sc;
  * sous forme d'un syste`me ge'ne'rateur. Cette fonction est 
  * utilisee paar la fonction sc_to_sg_chernikova 
  */ 
-
-
-static void polyhedron_to_sg(pol,sg)
-Polyhedron  *pol;
-Ptsg sg;
+static void polyhedron_to_sg(Polyhedron  *pol, Ptsg sg)
 {
-
     Pray_dte ldtes_tmp=NULL,ldtes = NULL;
     Pray_dte lray_tmp=NULL,lray = NULL;
     Psommet lsommet_tmp=NULL,lsommet=NULL;
     Stsg_vects dtes,rays;
     Stsg_soms sommets;
     Pvecteur pvnew;
-    int i;
+    unsigned int i;
     int nbsommets =0;
     int  nbrays=0;
     int dim = vect_size(sg->base) +1;
@@ -408,61 +377,68 @@ Ptsg sg;
 
 /* Fonction de conversion d'un systeme lineaire a un systeme generateur
  * par chenikova
-*/
-
-Ptsg  sc_to_sg_chernikova(sc)
-Psysteme sc;
+ */
+Ptsg  sc_to_sg_chernikova(Psysteme sc)
 {
-    Matrix *a;
+    Matrix *a = NULL;
     int nbrows = 0;
     int nbcolumns = 0;
-    Ptsg sg = sg_new();
-    Polyhedron *A;
-
-    /* mem_spy_begin(); */
+    Ptsg sg = NULL;
+    Polyhedron *A = NULL;
 
     assert(!SC_UNDEFINED_P(sc) && (sc_dimension(sc) != 0));
 
+    CATCH(any_exception_error)
+    {
+      if (A) Polyhedron_Free(A);
+      if (a) Matrix_Free(a);
+
+      RETHROW();
+    }
+    TRY 
+    {
     nbrows = sc->nb_eq + sc->nb_ineq + 1;
     nbcolumns = sc->dimension +2;
     sg->base = base_dup(sc->base);
     a = Matrix_Alloc(nbrows, nbcolumns);
     sc_to_matrix(sc,a);
     
-    /*  printf("\na =");
-      Matrix_Print(stderr, "%4d", a); */
-      
     A = Constraints2Polyhedron(a, MAX_NB_RAYS);
-    Matrix_Free(a);
+    Matrix_Free(a), a = NULL;
 
-/*    Polyhedron_Print(stderr, "%4d",A);
-*/
+    sg = sg_new();
     polyhedron_to_sg(A,sg);
-    Polyhedron_Free(A);
-    /*   printf(" systeme generateur\n");
-	 sg_fprint(stdout,sg,variable_dump_name);
-	 */
+    Polyhedron_Free(A), A = NULL;
+    } /* end TRY */
 
-    /* mem_spy_end("sc_to_sg_chernikova"); */
+    UNCATCH(any_exception_error);
 
-    return(sg);
+    return sg;
 }
 
 /* Fonction de conversion d'un systeme generateur a un systeme lineaire.
  * par chernikova
-*/
-
-Psysteme sg_to_sc_chernikova(sg)
-Ptsg sg;
+ */
+Psysteme sg_to_sc_chernikova(Ptsg sg)
 {
-
-
     int nbrows = sg_nbre_droites(sg)+ sg_nbre_rayons(sg)+sg_nbre_sommets(sg);
     int nbcolumns = base_dimension(sg->base)+2;
-    Matrix *a;
-    Psysteme sc= sc_new();
-    Polyhedron *A;
+    Matrix *a = NULL;
+    Psysteme sc = NULL;
+    Polyhedron *A = NULL;
 
+    CATCH(any_exception_error)
+    {
+      if (sc) sc_rm(sc);
+      if (a) Matrix_Free(a);
+      if (A) Polyhedron_Free(A);
+
+      RETHROW();
+    }
+    TRY
+    {
+
+    sc = sc_new();
     sc->base = base_dup(sg->base);
     sc->dimension = vect_size(sc->base);
 
@@ -471,21 +447,14 @@ Ptsg sg;
 	a = Matrix_Alloc(nbrows, nbcolumns);
 	sg_to_polyhedron(sg,a);	 
    
-	/* printf("\na =");
-	Matrix_Print(stderr, "%4d,", a); */
-
 	A = Rays2Polyhedron(a, MAX_NB_RAYS);
-	Matrix_Free(a);
+	Matrix_Free(a), a = NULL;
 
-/*	Polyhedron_Print(stderr, "%4d",A);
-*/
 	a= Polyhedron2Constraints(A);
-	Polyhedron_Free(A);
-	/* printf("\na =");
-		Matrix_Print(stderr, "%4d", a); 	*/
+	Polyhedron_Free(A), A = NULL;
 
 	matrix_to_sc(a,sc);
-	Matrix_Free(a);
+	Matrix_Free(a), a = NULL;
 	sc=sc_normalize(sc);
 	if (sc == NULL) {
 	    Pcontrainte pc = contrainte_make(vect_new(TCST, VALUE_ONE));
@@ -498,29 +467,26 @@ Ptsg sg;
 	sc->egalites = contrainte_make(vect_new(TCST,VALUE_ONE));
 	sc->nb_eq ++;
     }	
+    } /* end TRY */
 
+    UNCATCH(any_exception_error);
 
-    /*  printf(" impression du systeme \n"); 
-	sc_dump(sc); */
-
-    return(sc);
+    return sc;
 }
 
 
-Psysteme sc_convex_hull(sc1,sc2)
-Psysteme sc1,sc2;
+Psysteme sc_convex_hull(Psysteme sc1, Psysteme sc2)
 {
-
-    Matrix *a1,*a2;
+    Matrix *a1 = NULL,*a2 = NULL;
     int nbrows1 = 0;
     int nbcolumns1 = 0;
     int nbrows2 = 0;
     int nbcolumns2 = 0;
-    Polyhedron *A1,*A2;
-    Matrix *a;
-    Psysteme sc= sc_new();
-    Polyhedron *A;
-    int i1,i2,j;
+    Polyhedron *A1 = NULL,*A2 = NULL;
+    Matrix *a = NULL;
+    Psysteme sc = sc_new();
+    Polyhedron *A = NULL;
+    unsigned int i1,i2,j;
     int Dimension,cp;
 
     /* mem_spy_begin(); */
@@ -537,7 +503,7 @@ Psysteme sc1,sc2;
     /* comme on il ne faut pas que les structures irisa 
        apparaissent dans le fichier polyedre.h, une sous-routine 
        renvoyant un polyhedron n'est pas envisageable.
-       Le code est duplique*/
+       Le code est duplique */
 
     nbrows1 = sc1->nb_eq + sc1->nb_ineq + 1;
     nbcolumns1 = sc1->dimension +2;
@@ -556,10 +522,24 @@ Psysteme sc1,sc2;
 	Matrix_Print(stderr, "%4d",a2);
     }
     
+    CATCH(any_exception_error)
+    {
+      if (a) Matrix_Free(a);
+      if (a1) Matrix_Free(a1);
+      if (a2) Matrix_Free(a2);
+      if (A) Polyhedron_Free(A);
+      if (A1) Polyhedron_Free(A1);
+      if (A2) Polyhedron_Free(A2);
+      
+      RETHROW();
+    }
+    TRY 
+    {
+
     A1 = Constraints2Polyhedron(a1, MAX_NB_RAYS);
-    Matrix_Free(a1); 
+    Matrix_Free(a1), a1 = NULL; 
     A2 = Constraints2Polyhedron(a2, MAX_NB_RAYS);
-    Matrix_Free(a2);
+    Matrix_Free(a2), a2 = NULL;
 
     ifscdebug(8) {
 	fprintf(stderr, "[sc_convex_hull]\nA1 =");
@@ -593,22 +573,7 @@ Psysteme sc1,sc2;
 		a->p[cp][j] = A1->Ray[i1][j]; 
 	    cp++; i1++; 
 	}
-	/*
-	while (i2 < A2->NbRays && A2->Ray[i2][0] ==0) {
-	    boolean equal_rays = FALSE;
-	    for(i1p = 0, cpp=0; i1p < A1->NbRays && A1->Ray[i1p][0] ==0
-		&& !equal_rays; i1p++, cpp++) {
-		equal_rays = TRUE;
-		for (j=0 ; j < Dimension && equal_rays ; j++) {
-		    equal_rays = (a->p[cpp][j] == A2->Ray[i2][j]);
-		}
-	    }
-	    if(!equal_rays) {
-		for (j=0 ; j < Dimension ; j++) 
-		    a->p[cp][j] = A2->Ray[i2][j];
-		cp++; i2++; }
-	}
-	*/
+
 	while (i2 < A2->NbRays && A2->Ray[i2][0] ==0) {
 	    for (j=0 ; j < Dimension ; j++) 
 		a->p[cp][j] = A2->Ray[i2][j];
@@ -624,26 +589,6 @@ Psysteme sc1,sc2;
 	    cp++; i1++; 
 	}
 
-	/*
-	while (i2 < A2->NbRays && A2->Ray[i2][0] == 1 
-	       && A2->Ray[i2][Dimension-1]==0) {
-	    boolean equal_rays = FALSE;
-	    for(; i1p < A1->NbRays && A1->Ray[i1p][0] == 1 
-		&& A1->Ray[i1p][Dimension-1]==0
-		&& !equal_rays; i1p++, cpp++) {
-		equal_rays = TRUE;
-		for (j=0 ; j < Dimension && equal_rays ; j++) {
-		    equal_rays = (a->p[cpp][j] == A2->Ray[i2][j]);
-		}
-	    }
-
-	    if(!equal_rays) {
-		for (j=0; j < Dimension ; j++) 
-		    a->p[cp][j] = A2->Ray[i2][j]; 
-		cp++; i2++;
-	    }
-	}
-	*/
 	while (i2 < A2->NbRays && A2->Ray[i2][0] == 1 
 	       && A2->Ray[i2][Dimension-1]==0) {
 	    for (j=0; j < Dimension ; j++) 
@@ -659,27 +604,7 @@ Psysteme sc1,sc2;
 		a->p[cp][j] = A1->Ray[i1][j]; 
 	    cp++; i1++; 
 	}
-	/*
-	while (i2 < A2->NbRays && A2->Ray[i2][0] == 1 
-	       && A2->Ray[i2][Dimension-1]!=0) {
 
-	    boolean equal_rays = FALSE;
-	    for(; i1p < A1->NbRays && A1->Ray[i1p][0] == 1 
-		&& A1->Ray[i1p][Dimension-1]!=0
-		&& !equal_rays; i1p++, cpp++) {
-		equal_rays = TRUE;
-		for (j=0 ; j < Dimension && equal_rays ; j++) {
-		    equal_rays = (a->p[cpp][j] == A2->Ray[i2][j]);
-		}
-	    }
-
-	    if(!equal_rays) {
-		for (j=0; j < Dimension ; j++)  
-		    a->p[cp][j] = A2->Ray[i2][j]; 
-		cp++;  i2++;
-	    }
-	}
-	*/
 	while (i2 < A2->NbRays && A2->Ray[i2][0] == 1 
 	       && A2->Ray[i2][Dimension-1]!=0) {
 	    for (j=0; j < Dimension ; j++)  
@@ -687,39 +612,34 @@ Psysteme sc1,sc2;
 	    cp++;  i2++;
 	}
   
-	Polyhedron_Free(A1);
-	Polyhedron_Free(A2);
+	Polyhedron_Free(A1), A1 = NULL;
+	Polyhedron_Free(A2), A2 = NULL;
 	
-	/*    printf("\na =");
-		Matrix_Print(stderr, "%4d,",a); */
-
 	A = Rays2Polyhedron(a, MAX_NB_RAYS);
-/*	Polyhedron_Print(stderr, "%4d",A);
-*/
 
-	Matrix_Free(a);
+	Matrix_Free(a), a = NULL;
 	a = Polyhedron2Constraints(A);    
-	Polyhedron_Free(A);
+	Polyhedron_Free(A), A = NULL;
     }
 
-   /*    printf("\na =");
-	 Matrix_Print(stderr, "%4d", a); 	*/
     matrix_to_sc(a,sc);
     Matrix_Free(a);
-    sc=sc_normalize(sc);
 
+    sc = sc_normalize(sc);
+
+    } /* end TRY */
+
+    UNCATCH(any_exception_error);
 
     if (sc == NULL) {
 	Pcontrainte pc = contrainte_make(vect_new(TCST, VALUE_ONE));
 	sc = sc_make(pc, CONTRAINTE_UNDEFINED);
 	sc->base = base_dup(sc1->base);
 	sc->dimension = vect_size(sc->base);
-	}
-
-    /*    printf(" impression du systeme \n"); 
-	  sc_dump(sc); */
+    }
 
     /* mem_spy_end("sc_convex_hull"); */
 
-    return(sc);
+    return sc;
 }
+
