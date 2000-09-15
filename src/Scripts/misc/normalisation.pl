6e8a01682ae069cb4773c28cabaedc4ae70ee9a8
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-09-15 12:06:08 +0000

    # on 2000/09/15 12:06:08, coelho did: options added. some bug fixes.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10457 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/misc/normalisation.pl
+++ src/Scripts/misc/normalisation.pl
@@ -1,108 +1,137 @@
-#! /usr/local/bin/perl -w -n
+#! /usr/local/bin/perl -w
 #
 # $Id$
 #
 
-BEGIN {
-    $current_file = '';
-    %seen = ();
+use Getopt::Long;
+
+$opt_directory = '';
+$opt_suffix = 'old';
+$opt_help = '';
+
+GetOptions("directory=s", "suffix=s", "help")
+    or die $!;
+
+if ($opt_help) {
+    print STDERR
+	"Usage: normalisation.pl [-d dir] [-s suf] [-h] Logfile\n" .
+	"\t-d dir: directory sources\n" .
+	"\t-s suffix: suffix for old file (default 'old')\n" .
+	"\t-h: this help\n";
+    exit;
 }
 
-next if not /^\$DEC/;
+$current_file = '';
+%seen = ();
 
-chomp;
-($file, $module, $array, $ndim, $new, $old) = (split /\t/)[1..6];
-
-# FIX file
-$file = $module;
-$file =~ tr/A-Z/a-z/;
-if ( -f "$file.F" ) {
-    $file .= '.F';
-} elsif ( -f "$file.f" ) {
-    $file .= '.f';
-} else {
-    print STDERR "ERROR no file found for $module\n";
-    $file = '';
-}
-
-print STDERR "CONSIDERING $file $module $array $ndim $new $old\n";
-
-if (exists $seen{"$module:$array"})
+while (<>)
 {
-    print STDERR "ERROR several declarations for $module:$array\n";
-    next;
-}
+    # keep logfile lines with this prefix
+    next if not /^\$DEC/;
 
-$seen{"$module:$array"} = 1;
+    chomp;
 
-next if $old !~ /:1\)/;
-next if $new ne '*' and $old =~ /:$new\)/;
+    ($file, $module, $array, $ndim, $new, $old) = (split /\t/)[1..6];
+    
+    # fix file with source directory if appropriate
+    $file = $opt_directory . $file;
 
-if ($file ne $current_file)
-{
-    #print STDERR "SWITCHING $current_file to $file";
-    &save;
-
-    # tmp get actual file if found...
-    $current_file = $file;
-
-    # read file 
-    if ($current_file) {
-	print "OPENING $current_file\n";
-
-	open FILE, "< $current_file"
-	    or die "cannot open file $current_file for reading ($!)";
-	@fortran = <FILE>;
-	close FILE or die $!;
+    # FIX file, to be commented out...
+    $file = $module;
+    $file =~ tr/A-Z/a-z/;
+    $file = $opt_directory . $file;
+    if ( -f "$file.F" ) {
+	$file .= '.F';
+    } elsif ( -f "$file.f" ) {
+	$file .= '.f';
     } else {
-	@fortran = ();
+	error("no file found for $module\n");
+	$file = '';
     }
-}
 
-print STDERR "DOING $file $module $array $ndim $new $old\n";
-
-$n = @fortran;
-$done = 0;
-$insub = 0;
-
-for ($i = 0; $i < $n and not $done; $i++)
-{
-    $ligne = $fortran[$i];
-
-    # insure that we're in the right routine
-    $insub = 0 if $ligne =~ /^[^Cc\*!].*(function|subroutine)/i;
-    $insub = 1 if $ligne =~ /^[^Cc\*!].*(function|subroutine)[ \t]*$module/i;
-
-    next if not $insub;
-
-    # first occurence of ARRAY( is supposed to be its dimension declaration!
-    if ($ligne =~ /[ ,]$array\(/i)
+    print STDERR "CONSIDERING $file $module $array $ndim $new $old\n";
+    
+    if (exists $seen{"$module:$array"})
     {
-	print STDERR "? $array(in $module): $ligne";
-	$done = 1;
-	
-	if ($ndim>1) 
-	{
-	    if ($ligne !~ s/($array\([^()]*),[^,()]*\)/$1,$new\)/i) {
-		failed('cannot fix last dim', $ligne);
-	    }
-	} else {
-	    if ($ligne !~ s/($array\()[^()]*\)/$1$new\)/i) {
-		failed('cannot fix dim', $ligne);
-	    }
-	}
-
-	print STDERR "! $array(in $module): $ligne";
-
-	$fortran[$i] = $ligne;
+	error("several declarations for $module:$array\n");
+	next;
     }
+    
+    $seen{"$module:$array"} = 1;
+    
+    next if $old !~ /:[\*1]\)/;
+    next if $new ne '*' and $old =~ /:$new\)/;
+    
+    if ($file ne $current_file)
+    {
+	#print STDERR "SWITCHING $current_file to $file";
+	&save;
+	
+	# tmp get actual file if found...
+	$current_file = $file;
+	
+	# read file 
+	if ($current_file) {
+	    # print "OPENING $current_file\n";
+	    
+	    open FILE, "< $current_file"
+		or die "cannot open file $current_file for reading ($!)";
+	    @fortran = <FILE>;
+	    close FILE or die $!;
+	} else {
+	    @fortran = ();
+	}
+    }
+    
+    print STDERR "DOING $file $module $array $ndim $new $old\n";
+    
+    $n = @fortran;
+    $done = 0;
+    $insub = 0;
+    
+    for ($i = 0; $i < $n and not $done; $i++)
+    {
+	$ligne = $fortran[$i];
+	
+	next if $ligne =~ /^[Cc!\*]/; # skip comments.
+
+	# insure that we're in the right routine
+	$insub = 0
+	    if $ligne =~ /^[^Cc\*!].*(function|subroutine)/i;
+	$insub = 1 
+	    if $ligne =~ /^[^Cc\*!].*(function|subroutine)[ \t]*$module/i;
+	
+	next if not $insub;
+	
+	# first occurence of ARRAY( 
+	#   is supposed to be its dimension declaration!
+	if ($ligne =~ /[ ,]$array\(/i)
+	{
+	    print STDERR "? $array(in $module): $ligne";
+	    $done = 1;
+	    
+	    if ($ndim>1) 
+	    {
+		if ($ligne !~ s/($array\([^()]*),[^,()]*\)/$1,$new\)/i) {
+		    failed('cannot fix last dim', $ligne);
+		}
+	    } else {
+		if ($ligne !~ s/($array\()[^()]*\)/$1$new\)/i) {
+		    failed('cannot fix dim', $ligne);
+		}
+	    }
+	    
+	    print STDERR "! $array(in $module): $ligne";
+	    
+	    $fortran[$i] = $ligne;
+	}
+    }
+    
+    failed('declaration not found', '') if not $done;
 }
 
-failed('declaration not found', '') if not $done;
-
-END {
-    &save;
-}
+# save last file
+&save;
 
 sub save
 {
@@ -114,7 +143,7 @@ sub save
 	{
 	    # skip comments
 	    next if $fortran[$i] =~ /^[Cc!\*]/;
-
+	    
 	    $len = length($fortran[$i]);
 	    if ($len > 73) {
 		$fortran[$i] =
@@ -124,11 +153,19 @@ sub save
 	    }
 	}
 
-	print "CLOSING $current_file\n";
+	# print "CLOSING $current_file\n";
 	print STDERR "SAVING $current_file\n";
 
+	# chose old source file name 
+	$old_file = "$current_file.$opt_suffix";
+	if (-f $old_file) {
+	    $i = 1;
+	    while (-f "$old_file.$i") { $i++; }
+	    $old_file .= $i;
+	}
+	
 	# rename initial file
-	rename "$current_file", "$current_file.old"
+	rename "$current_file", "$old_file"
 	    or die "cannot rename file $current_file ($!)";
 	# save to initial file
 	open FILE, "> $current_file"
@@ -138,8 +175,14 @@ sub save
     }
 }
 
+sub error
+{
+    print STDERR "ERROR ", @_;
+    print "ERROR ", @_;
+}
+
 sub failed
 {
     my ($n, $l) = @_;
-    print STDERR "ERROR ($n) $file $module $array $ndim $new $old\n$l";
+    error "($n) $file $module $array $ndim $new $old\n$l";
 }
