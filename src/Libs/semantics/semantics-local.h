e936e9ff91d759037da172dcdfe2b8fd97be8894
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2000-11-23 17:20:48 +0000

    # on 2000/11/23 17:20:48, irigoin did: Two constants added as a heuristics to analyze CFG
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10529 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/semantics/semantics-local.h
+++ src/Libs/semantics/semantics-local.h
@@ -6,3 +6,8 @@
 #define USER_TRANSFORMER_SUFFIX ".utran"
 #define SEQUENTIAL_PRECONDITION_SUFFIX ".prec"
 #define USER_PRECONDITION_SUFFIX ".uprec"
+
+/* Maximum number of nodes in a CFG/unstructured for an accurate analysis */
+#define SEMANTICS_MAX_CFG_SIZE1 (20)
+/* Too big, even for a simple convex union: let's use effects */
+#define SEMANTICS_MAX_CFG_SIZE2 (30)
