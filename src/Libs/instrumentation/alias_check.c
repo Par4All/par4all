cb0689b03f9fe84103306382385e038b48f76d9f
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2001-05-25 09:57:08 +0000

    # on 2001/05/25 09:57:08, nguyen did: Initial version
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10597 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/instrumentation/alias_check.c
+++ src/Libs/instrumentation/alias_check.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "genC.h"
+
+#include "linear.h"
+
+#include "ri.h"
+#include "ri-util.h"
+#include "database.h"
+#include "pipsdbm.h"
+#include "resources.h"
+#include "misc.h"
+#include "control.h"
+#include "properties.h"
+#include "semantics.h"
+
+#include "instrumentation.h"
+
 bool alias_check(char * module_name)
 {
   return TRUE;
