22dd242e26780a9dfcb4da2ce60715990176efce
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2001-05-28 08:42:05 +0000

    # on 2001/05/28 08:42:05, nguyen did: Rename : bottom_up_array_bound_check to array_bound_check_bottom_up
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10611 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/instrumentation/array_bound_check_bottom_up.c
+++ src/Libs/instrumentation/array_bound_check_bottom_up.c
@@ -1023,7 +1023,7 @@ static void bottom_up_abc_statement(statement module_statement)
 
 
 
-bool bottom_up_array_bound_check(char *module_name)
+bool array_bound_check_bottom_up(char *module_name)
 { 
   statement module_statement;  
   set_current_module_entity(local_name_to_top_level_entity(module_name));
