2f22dc9ee53f67e042eba9f057fd31d9c3cd34db
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2001-05-28 07:23:37 +0000

    # on 2001/05/28 07:23:37, nguyen did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10608 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/instrumentation/alias_propagation.c
+++ src/Libs/instrumentation/alias_propagation.c
@@ -1,7 +1,25 @@
+/******************************************************************
+ *
+ *		     ALIAS PROPAGATION
+ *
+ *
+*******************************************************************/
+
+/* Aliasing occurs when two or more variables refer to the same
+storage location at the same program point.
+
+This phase tries to compute as precise as possible the
+interprocedural alias information in a whole program.
+
+The basic idea for computing interprocedural aliases is to follow all the
+possible chains of argument-parameters and nonlocal variable-parameter
+bindings at all call sites. We introduce a naming memory locations technique which guarantees the correctness and enhances the
+precision of data-flow analysis. */
+
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> 
 
 #include "genC.h"
 
@@ -18,6 +36,7 @@
 #include "semantics.h"
 
 #include "instrumentation.h"
+#include "transformations.h"
 
 bool alias_propagation(char * module_name)
 {
