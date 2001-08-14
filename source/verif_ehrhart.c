cc8fd97c5c1359eb045f32b50e6f900c35e0a726
loechner <loechner>
2001-08-14 14:55:06 +0000

    Changed the call to polyhedron_Scan, included true context in D. Test/ehrhart/g13.in now passes.

--- source/verif_ehrhart.c
+++ source/verif_ehrhart.c
@@ -230,7 +230,13 @@ int main(int argc,char *argv[]) {
   value_set_si(p[i],1);
 
   /* S = scanning list of polyhedra */
-  S = Polyhedron_Scan(P,C,MAXRAYS);
+  /* S needs to contain the context in scan if there are equalities
+     between indices*/
+  /* changed by vin100, 14/08/01, g13 now passes */
+  /* was S = Polyhedron_Scan(P,C,MAXRAYS); */
+   S = Polyhedron_Scan(
+         DomainIntersection(P,align_context(C,P->Dimension,MAXRAYS),MAXRAYS),
+       C,MAXRAYS);
   
 #ifndef PRINT_ALL_RESULTS
   if(C->Dimension > 0) {
