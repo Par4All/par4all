8e35044e6c6205a02154d35f34c8517063e11e2a
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2001-11-29 15:13:33 +0000

    # on 2001/11/29 15:13:33, nguyen did: Add more statistics
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10792 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformations/array_resizing_statistic.c
+++ src/Libs/transformations/array_resizing_statistic.c
@@ -1,13 +1,12 @@
-/* This phase is to calculate the number of 1 and assumed-size (*) array declarators in one program */
+/* This phase computes the number of pointer-type A(1) and assumed-size A(*) array declarators 
+   in one program. There are two cases: these unnormalized declarations are formal array 
+   parameters or not */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "genC.h"
-
 #include "linear.h"
-
 #include "ri.h"
 #include "ri-util.h"
 #include "database.h"
@@ -20,21 +19,25 @@
 #include "instrumentation.h"
 #include "transformations.h"
 
-static int number_of_one_and_formal_array_declarators = 0;
-static int number_of_assumed_size_and_formal_array_declarators = 0;
-static int number_of_formal_array_declarators = 0;
-static int number_of_one_array_declarators = 0;
-static int number_of_assumed_size_array_declarators = 0;
-static int number_of_array_declarators = 0;
+static int number_of_pointer_type_and_formal_arrays = 0;
+static int number_of_assumed_size_and_formal_arrays = 0;
+static int number_of_formal_arrays = 0;
+static int number_of_pointer_type_and_not_formal_arrays = 0;
+static int number_of_assumed_size_and_not_formal_arrays = 0;
+static int number_of_not_formal_arrays = 0;
+
+static int number_of_processed_modules = 0;
 
 bool array_resizing_statistic(char *module_name)
 {
   entity module_ent = local_name_to_top_level_entity(module_name);
   list l_decl = code_declarations(entity_code(module_ent));
 
-  debug_on("ADN_INSTRUMENTATION_DEBUG_LEVEL");
+  number_of_processed_modules++;
+
+  debug_on("ARRAY_RESIZING_STATISTIC_DEBUG_LEVEL");
   ifdebug(1)
-    fprintf(stderr, " \n Begin  adn instrumentation for %s \n", module_name);   
+    fprintf(stderr, " \n Begin array resizing statistic for %s \n", module_name);   
    
   /* search for unnormalized array declarations in the list */  
   while(!ENDP(l_decl))
@@ -50,42 +53,69 @@ bool array_resizing_statistic(char *module_name)
 	      dimension last_dim =  find_ith_dimension(l_dims,length);
 	      expression exp = dimension_upper(last_dim);
 	      storage s = entity_storage(e);
-	      number_of_array_declarators ++;
 	      if (storage_formal_p(s))   
-		number_of_formal_array_declarators++;
+		number_of_formal_arrays++;
+	      else 
+		number_of_not_formal_arrays++;
 	      if (unbounded_dimension_p(last_dim)) 
 		{
-		  number_of_assumed_size_array_declarators++;
-		  if (storage_formal_p(s))   
-		    number_of_assumed_size_and_formal_array_declarators++;
+		  if (storage_formal_p(s))  
+		    number_of_assumed_size_and_formal_arrays++;
+		  else 
+		    number_of_assumed_size_and_not_formal_arrays++;
 		}
 	      if (expression_equal_integer_p(exp,1))
 		{
-		  number_of_one_array_declarators++;
-		  if (storage_formal_p(s))   
-		    number_of_one_and_formal_array_declarators++;
+		  if (storage_formal_p(s)) 
+		    number_of_pointer_type_and_formal_arrays++;
+		  else 
+		    number_of_pointer_type_and_not_formal_arrays++;
 		}
 	    }
 	}
       l_decl = CDR(l_decl);
     }
   
-  user_log(" \n Number of 1 and formal array declarators : %d \n"
-	  ,number_of_one_and_formal_array_declarators );
-  user_log(" \n Number of * and formal array declarators : %d \n"
-	  ,number_of_assumed_size_and_formal_array_declarators );
+  user_log(" \n Number of pointer-type A(1), formal arrays : %d \n"
+	  ,number_of_pointer_type_and_formal_arrays );
+  user_log(" \n Number of assumed-size A(*), formal arrays : %d \n"
+	   ,number_of_assumed_size_and_formal_arrays );
   user_log(" \n Number of formal array declarators : %d\n"
-	  ,number_of_formal_array_declarators );
-  user_log(" \n Number of 1 array declarators : %d\n"
-	  ,number_of_one_array_declarators );
-  user_log(" \n Number of * array declarators : %d\n"
-	  , number_of_assumed_size_array_declarators  );
-  user_log(" \n Number of array declarators : %d\n"
-	  , number_of_array_declarators  );
+	   ,number_of_formal_arrays );
+  user_log(" \n Number of pointer-type A(1), local arrays : %d \n"
+	  ,number_of_pointer_type_and_not_formal_arrays );
+  user_log(" \n Number of assumed-size A(*), local arrays : %d \n"
+	   ,number_of_assumed_size_and_not_formal_arrays );
+  user_log(" \n Number of local array declarators : %d\n"
+	   ,number_of_not_formal_arrays );
+  user_log(" \n Total number of pointer-type arrays : %d\n"
+	  ,number_of_pointer_type_and_formal_arrays + 
+	   number_of_pointer_type_and_not_formal_arrays );
+  user_log(" \n Total number of assumed-size arrays : %d\n"
+	   ,number_of_assumed_size_and_formal_arrays + 
+	   number_of_assumed_size_and_not_formal_arrays);
+  user_log(" \n Total number of arrays : %d\n"
+	   ,number_of_formal_arrays +
+	   number_of_not_formal_arrays);
+
+  user_log("\n Number of processed modules: %d \n"
+	   ,number_of_processed_modules);
+
   ifdebug(1)
-    fprintf(stderr, " \n End  adn instrumentation for %s \n", module_name);
+    fprintf(stderr, " \n End array resizing statistic for %s \n", module_name);
   debug_off();
   return TRUE;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
