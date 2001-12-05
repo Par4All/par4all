a7c8bd7784b0d176728f84c3b6ab3d0db1cb0897
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2001-12-05 17:23:56 +0000

    # on 2001/12/05 17:23:56, irigoin did: New sufixes for total preconditions added
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10808 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/semantics/semantics-local.h
+++ src/Libs/semantics/semantics-local.h
@@ -6,6 +6,8 @@
 #define USER_TRANSFORMER_SUFFIX ".utran"
 #define SEQUENTIAL_PRECONDITION_SUFFIX ".prec"
 #define USER_PRECONDITION_SUFFIX ".uprec"
+#define SEQUENTIAL_TOTAL_PRECONDITION_SUFFIX ".tprec"
+#define USER_TOTAL_PRECONDITION_SUFFIX ".utprec"
 
 /* Maximum number of nodes in a CFG/unstructured for an accurate analysis */
 #define SEMANTICS_MAX_CFG_SIZE1 (20)
