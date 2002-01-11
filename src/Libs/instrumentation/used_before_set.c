f62ed985c8f73bb9b8d285df2349be7646fa92c7
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-01-11 12:42:39 +0000

    # on 2002/01/11 12:42:39, nguyen did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10812 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/instrumentation/used_before_set.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+#include "ri-util.h"
+#include "database.h"
+#include "pipsdbm.h"
+#include "resources.h"
+#include "misc.h"
+#include "control.h"
+#include "properties.h"
+#include "semantics.h"
+#include "instrumentation.h"
+#include "transformations.h"
+
+bool used_before_set(char *module_name)
+{ 
+  return TRUE;
+}
