cedf8165ced59c74c69748d46841b3f7dff72304
phamdat <phamdat@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-04-23 19:43:43 +0000

    # on 2002/04/23 19:43:43, phamdat did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10974 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/prettyprint.c
+++ src/Libs/ri-util/prettyprint.c
@@ -2,6 +2,9 @@
  * $Id$
  *
  * $Log: prettyprint.c,v $
+ * Revision 1.150  2002/04/23 19:43:43  phamdat
+ * *** empty log message ***
+ *
  * Revision 1.149  2002/04/23 15:38:24  phamdat
  * *** empty log message ***
  *
@@ -289,7 +292,7 @@
  */
 
 #ifndef lint
-char lib_ri_util_prettyprint_c_rcsid[] = "$Header: /home/data/tmp/PIPS/pips_data/trunk/src/Libs/ri-util/RCS/prettyprint.c,v 1.149 2002/04/23 15:38:24 phamdat Exp $";
+char lib_ri_util_prettyprint_c_rcsid[] = "$Header: /home/data/tmp/PIPS/pips_data/trunk/src/Libs/ri-util/RCS/prettyprint.c,v 1.150 2002/04/23 19:43:43 phamdat Exp $";
 #endif /* lint */
 
  /*
@@ -2290,7 +2293,7 @@ text_statement(
     {
       string filename = "/users/tmp/phamdat/textout";
       FILE * my_file = safe_fopen(filename, "w");
-      print_text(my_file, temp);
+      print_text(my_file, r);
       safe_fclose(my_file, filename);
       free(filename);
     }
@@ -2298,10 +2301,11 @@ text_statement(
     {
       string filename = "/users/tmp/phamdat/textout";
       FILE * my_file = safe_fopen(filename, "w");
-      print_text(my_file, stmt);
+      print_text(my_file, r);
       safe_fclose(my_file, filename);
       free(filename);
     }
+
     ifdebug(1) {
 	if (instruction_sequence_p(i)) {
 	    if(!(statement_with_empty_comment_p(stmt)
