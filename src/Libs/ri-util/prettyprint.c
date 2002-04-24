8436a6c40e0b71e256ad03114d3a71be62d153b6
phamdat <phamdat@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-04-24 08:53:18 +0000

    # on 2002/04/24 08:53:18, phamdat did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@10979 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/prettyprint.c
+++ src/Libs/ri-util/prettyprint.c
@@ -2,6 +2,9 @@
  * $Id$
  *
  * $Log: prettyprint.c,v $
+ * Revision 1.155  2002/04/24 08:53:18  phamdat
+ * *** empty log message ***
+ *
  * Revision 1.154  2002/04/24 08:49:55  phamdat
  * *** empty log message ***
  *
@@ -304,7 +307,7 @@
  */
 
 #ifndef lint
-char lib_ri_util_prettyprint_c_rcsid[] = "$Header: /home/data/tmp/PIPS/pips_data/trunk/src/Libs/ri-util/RCS/prettyprint.c,v 1.154 2002/04/24 08:49:55 phamdat Exp $";
+char lib_ri_util_prettyprint_c_rcsid[] = "$Header: /home/data/tmp/PIPS/pips_data/trunk/src/Libs/ri-util/RCS/prettyprint.c,v 1.155 2002/04/24 08:53:18 phamdat Exp $";
 #endif /* lint */
 
  /*
@@ -1878,13 +1881,6 @@ init_text_statement(
       /* FI: before calling the hook,
        * statement_ordering(obj) should be checked */
 	r = (*text_statement_hook)( module, margin, obj );
-	{
-      string filename = "/users/tmp/phamdat/textout";
-      FILE * my_file = safe_fopen(filename, "w");
-      print_text(my_file, r);
-      safe_fclose(my_file, filename);
-      free(filename);
-    }
 	if (text_statement_hook != empty_text)
 	    attach_decoration_to_text(r);
     }
