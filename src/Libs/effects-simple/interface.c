8c68159b07c4cbedf9f47b224164d79ca8e61498
phamdat <phamdat@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-04-25 07:55:09 +0000

    # on 2002/04/25 07:55:09, phamdat did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11017 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/effects-simple/interface.c
+++ src/Libs/effects-simple/interface.c
@@ -240,7 +240,6 @@ text my_get_text_proper_effects(string module_name)
 {
     text t;
 
-    set_is_user_view_p(FALSE);
     set_methods_for_rw_effects_prettyprint(module_name);
     t = my_get_any_effect_type_text(module_name, DBR_PROPER_EFFECTS);
 
