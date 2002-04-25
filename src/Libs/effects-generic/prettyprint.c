311b16714001dd702dc7afdeaaa0112220029333
phamdat <phamdat@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-04-25 08:31:17 +0000

    # on 2002/04/25 08:31:17, phamdat did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11020 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/effects-generic/prettyprint.c
+++ src/Libs/effects-generic/prettyprint.c
@@ -337,7 +337,7 @@ entity module;
     /*MERGE_TEXTS(txt, text_summary_any_effect_type(module));*/
 
     /* then code with effects, using text_statement_any_effect_type */
-    MERGE_TEXTS(txt, text_module(module, module_stat));
+    MERGE_TEXTS(txt, my_text_named_module(module, module_stat));
     
     close_prettyprint();
 
