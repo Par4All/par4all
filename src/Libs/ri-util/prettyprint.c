3d6710e271997d90ea9c00cd366d8647b822d5b5
phamdat <phamdat@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-04-26 13:13:51 +0000

    # on 2002/04/26 13:13:51, phamdat did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11098 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/prettyprint.c
+++ src/Libs/ri-util/prettyprint.c
@@ -2,6 +2,9 @@
  * $Id$
  *
  * $Log: prettyprint.c,v $
+ * Revision 1.182  2002/04/26 13:13:51  phamdat
+ * *** empty log message ***
+ *
  * Revision 1.181  2002/04/26 12:55:16  phamdat
  * *** empty log message ***
  *
@@ -385,7 +388,7 @@
  */
 
 #ifndef lint
-char lib_ri_util_prettyprint_c_rcsid[] = "$Header: /home/data/tmp/PIPS/pips_data/trunk/src/Libs/ri-util/RCS/prettyprint.c,v 1.181 2002/04/26 12:55:16 phamdat Exp $";
+char lib_ri_util_prettyprint_c_rcsid[] = "$Header: /home/data/tmp/PIPS/pips_data/trunk/src/Libs/ri-util/RCS/prettyprint.c,v 1.182 2002/04/26 13:13:51 phamdat Exp $";
 #endif /* lint */
 
  /*
@@ -2377,7 +2380,7 @@ text_statement(
 	}
 	MERGE_TEXTS(r, temp);
       }
-      gen_free_text(t);
+      free_text(t);
       /**********************************/
       /*MERGE_TEXTS(r, init_text_statement(module, margin, stmt));
 	if (! string_undefined_p(comments)) {
