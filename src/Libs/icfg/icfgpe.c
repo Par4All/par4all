ac917b891394ff10cf3c08c39f6457686d37231c
phamdat <phamdat@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-05-02 08:40:08 +0000

    # on 2002/05/02 08:40:08, phamdat did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11143 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/icfg/icfgpe.c
+++ src/Libs/icfg/icfgpe.c
@@ -1,4 +1,29 @@
 
+#include <stdio.h>
+#include <string.h>
+
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+#include "control.h"      /* CONTROL_MAP is defined there */
+#include "text.h"
+#include "text-util.h"
+#include "ri-util.h"
+#include "properties.h"  /* get_bool_property */
+#include "misc.h"
+#include "database.h"
+#include "effects-generic.h"
+#include "effects-simple.h"
+#include "effects-convex.h"
+#include "resources.h"
+#include "semantics.h"
+#include "prettyprint.h"
+#include "complexity_ri.h"
+#include "complexity.h"
+#include "pipsdbm.h"      /* DB_PUT_FILE_RESOURCE is defined there */
+#include "text-util.h"
+#include "icfg.h"
+
 text get_text_proper_effects_flt(string module_name, entity e_flt)
 {
   text t;
