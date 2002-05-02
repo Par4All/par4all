66c0056d6ac877c014063ebfc5981f8cbc417201
phamdat <phamdat@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-05-02 13:11:41 +0000

    # on 2002/05/02 13:11:41, phamdat did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11153 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/icfg/icfgpe.c
+++ src/Libs/icfg/icfgpe.c
@@ -27,8 +27,8 @@
 text get_text_proper_effects_flt(string module_name, entity e_flt)
 {
   text t;
-  set_methodes_for_rw_effects_prettyprint(module_name);
+  set_methods_for_rw_effects_prettyprint(module_name);
   t = get_any_effect_type_text(module_name, DBR_PROPER_EFFECTS, string_undefined, TRUE);
-  reset_methodes_for_effects_prettyprint(module_name);
+  reset_methods_for_effects_prettyprint(module_name);
   return t;
 }
