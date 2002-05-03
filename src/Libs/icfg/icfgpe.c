f5dc977b7dcda186964766368098b8319b90b2f2
phamdat <phamdat@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-05-03 07:58:16 +0000

    # on 2002/05/03 07:58:16, phamdat did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11181 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/icfg/icfgpe.c
+++ src/Libs/icfg/icfgpe.c
@@ -65,7 +65,7 @@ list effects_filter(list l_effs, string str_filter)
     effect eff = EFFECT(CAR(ce));
     action ac = effect_action(eff);
     reference ref = effect_reference(eff);
-    list ls = effect_word_reference(ref);
+    list ls = effect_words_reference(ref);
     string t = words_to_string(ls);
     gen_free_string_list(ls);
     if (same_string_p(t, str_filter) && !action_read_p(ac))
