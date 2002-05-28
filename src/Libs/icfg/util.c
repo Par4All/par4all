c2b6967ddbf68ce6acf54196798169759930d3a6
phamdat <phamdat@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-05-28 09:35:48 +0000

    # on 2002/05/28 09:35:48, phamdat did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11247 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/icfg/util.c
@@ -0,0 +1,257 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+#include "control.h"      /* CONTROL_MAP is defined there */
+#include "text.h"
+#include "text-util.h"
+#include "ri-util.h"
+#include "properties.h"  /* get_bool_property */
+#include "misc.h"
+#include "database.h"
+#include "effects-generic.h"
+#include "effects-simple.h"
+#include "effects-convex.h"
+#include "resources.h"
+#include "semantics.h"
+#include "prettyprint.h"
+#include "complexity_ri.h"
+#include "complexity.h"
+#include "pipsdbm.h"      /* DB_PUT_FILE_RESOURCE is defined there */
+#include "text-util.h"
+#include "icfg.h"
+
+/* Graph is defined here for making daVinci */
+#include "dg.h"
+typedef dg_arc_label arc_label;
+typedef dg_vertex_label vertex_label;
+#include "graph.h"
+#define ADD_ELEMENT_TO_LIST( _list, _type, _element) \
+    (_list = gen_nconc( _list, CONS( _type, _element, NIL)))
+
+
+void safe_free_vertex(vertex ver, list l_of_vers)
+{
+    if (!gen_in_list_p(ver, l_of_vers))
+        free(ver);
+    return;
+}
+
+list safe_add_vertex_to_list(vertex ver, list l_of_vers)
+{
+    if (!gen_in_list_p(ver, l_of_vers))
+        ADD_ELEMENT_TO_LIST(l_of_vers, VERTEX, ver);
+    return l_of_vers;
+}
+
+list list_of_connected_nodes(vertex ver, list l_of_vers)
+{
+    MAP(SUCCESSOR, succ, {
+        vertex ver_child = successor_vertex(succ);
+	l_of_vers = safe_add_vertex_to_list(ver_child, l_of_vers);
+	l_of_vers = list_of_connected_nodes(ver_child, l_of_vers);
+    }, vertex_successors(ver));
+    return l_of_vers;
+}
+
+string remove_newline_of_string(string s)
+{
+    string r = strdup(s);
+    if (*(r + strlen(r) - 1) == '\n')
+        *(r + strlen(r) - 1) = '\0';
+    return r;
+}
+
+string add_flash_newline_to_string(string s)
+{
+    string r;
+    if (strlen(s) == 0)
+      return "";
+    else if (*(s + strlen(s) - 1) == '\n') {
+        r = (string)malloc(strlen(s) + 1);
+	memset(r, 0, strlen(s) + 1);
+	strncpy(r, s, strlen(s) - 1);
+    } else {
+        r = (string)malloc(strlen(s) + 2);
+	memset(r, 0, strlen(s) + 2);
+	strcpy(r, s);
+    }
+    strcat(r, "\\n");
+    return r;
+}
+
+vertex get_vertex_by_string(string str_name, list l_of_vers)
+{
+    MAP(VERTEX, ver, {
+        text ver_label = (text)vertex_vertex_label(ver);
+	list sens = text_sentences(ver_label);
+        sentence ver_first_sen = SENTENCE(CAR(sens));
+	string ver_name = first_word_of_sentence(ver_first_sen);
+	string ver_real_name = remove_newline_of_string(ver_name);
+	if (same_string_p(ver_real_name, str_name)) {
+	    free(ver_real_name);
+	    return ver;
+	} else
+	    free(ver_real_name);
+    }, l_of_vers);
+    return vertex_undefined;
+}
+
+string sentence_to_string(sentence sen)
+{
+    string r;
+    if (!sentence_formatted_p(sen)) {
+        string s = words_to_string(unformatted_words(sentence_unformatted(sen)));
+	r = add_flash_newline_to_string(s);
+	free(s);
+    } else {
+        string s = sentence_formatted(sen);
+	r = add_flash_newline_to_string(s);
+    }
+    return r;
+}
+
+list safe_make_successor(vertex ver_parent, vertex ver_child, list l_of_vers)
+{
+    successor succ;
+    sentence ver_child_first_sen = SENTENCE(CAR(text_sentences((text)vertex_vertex_label(ver_child))));
+    string ver_child_name = first_word_of_sentence(ver_child_first_sen);
+
+    /* search if it was made yet, do not make */
+    MAP(SUCCESSOR, succ, {
+        vertex ver = successor_vertex(succ);
+	sentence ver_first_sen = SENTENCE(CAR(text_sentences((text)vertex_vertex_label(ver))));
+	string ver_name = first_word_of_sentence(ver_first_sen);
+	if (same_string_p(ver_name, ver_child_name)) return l_of_vers;
+    }, vertex_successors(ver_parent));
+
+    /* if it was not made, do it */
+    succ = make_successor(NULL, ver_child);
+    ADD_ELEMENT_TO_LIST(vertex_successors(ver_parent), SUCCESSOR, succ);
+    /* add vertex parent to the list of vertex */
+    l_of_vers = safe_add_vertex_to_list(ver_parent, l_of_vers);
+
+    return l_of_vers;
+}
+
+
+void print_graph_of_text_to_daVinci(FILE * f_out, graph g_in)
+{
+    bool first_node_parent = TRUE;
+    fprintf(f_out, "[\n");
+    
+    MAP(VERTEX, ver_parent, {
+        bool first_node_child = TRUE;
+        text node_parent_text = (text)vertex_vertex_label(ver_parent);
+	bool first_sentence = TRUE;
+	
+	if (first_node_parent)
+	    first_node_parent = FALSE;
+	else
+	    fprintf(f_out, ",\n");
+	
+	MAP(SENTENCE, sen, {
+	    if (first_sentence) {
+	        fprintf(f_out, "l(\"%s\",n(\"\",[a(\"OBJECT\",\"", remove_newline_of_string(first_word_of_sentence(sen)));
+		first_sentence = FALSE;
+	    }
+	    fprintf(f_out, sentence_to_string(sen));
+	}, text_sentences(node_parent_text));
+	
+	fprintf(f_out, "\")],[\n");
+
+	MAP(SUCCESSOR, succ, {
+	    vertex ver_child = successor_vertex(succ);
+	    text node_child_text = (text)vertex_vertex_label(ver_child);
+	    sentence node_child_sen = SENTENCE(CAR(text_sentences(node_child_text)));
+	    string node_name_child = first_word_of_sentence(node_child_sen);
+	    
+	    if (first_node_child)
+	        first_node_child = FALSE;
+	    else
+	        fprintf(f_out, ",\n");
+	    fprintf(f_out, "  l(\"\",e(\"\",[],r(\"%s\")))", remove_newline_of_string(node_name_child));
+	}, vertex_successors(ver_parent));
+
+	fprintf(f_out, "]))");
+    }, graph_vertices(g_in));
+    
+    fprintf(f_out, "\n]");
+    
+    return;
+}
+
+void print_graph_daVinci_with_starting_node(FILE * f_out, vertex start_ver)
+{
+    graph g = NULL;
+    list l = NIL;
+    ADD_ELEMENT_TO_LIST(l, VERTEX, start_ver);
+    l = list_of_connected_nodes(start_ver, l);
+    g = make_graph(l);
+    print_graph_of_text_to_daVinci(f_out, g);
+    /*free_graph(g);*/
+    gen_free_list(l);
+    l = NIL;
+    return;
+}
+
+list /* of entity */ get_list_of_variable_to_filter() {
+    list l = NIL;
+    string variables_names = strdup(get_string_property("EFFECTS_FILTER_ON_VARIABLE"));
+    string saved = variables_names, s;
+    entity var = NULL;
+
+    for (s = variables_names; *s; s++) {
+        if (*s == ',') {
+	  *s = '\0';
+	  var = gen_find_tabulated(variables_names, entity_domain);
+	  
+	  if (!var || entity_undefined_p(var))
+	      pips_user_warning("reference variable '%s' not found\n", variables_names);
+	  else
+	      ADD_ELEMENT_TO_LIST(l, ENTITY, var);
+	  *s = ',';
+	  variables_names = s+1;
+	}
+    }
+	
+    var = gen_find_tabulated(variables_names, entity_domain);
+    
+    if (!var || entity_undefined_p(var))
+        pips_user_warning("reference variable '%s' not found\n", variables_names);
+    else
+        ADD_ELEMENT_TO_LIST(l, ENTITY, var);
+
+    free(saved);
+    saved = NULL;
+
+    return l;
+}
+
+list /* of effect */ effects_filter(list l_effs, list l_ents)
+{
+    list l_flt = NIL;
+    MAP(EFFECT, eff, {
+        action ac = effect_action(eff);
+	reference ref = effect_reference(eff);
+	entity ent = reference_variable(ref);
+	MAP(ENTITY, e_flt, {
+	    if (entity_conflict_p(e_flt, ent) && !action_read_p(ac)) {
+		ADD_ELEMENT_TO_LIST(l_flt, EFFECT, eff);
+		break;
+	    }
+	}, l_ents);
+    }, l_effs);
+    return l_flt;
+}
+
+
+
+
+
+
+
+
+
