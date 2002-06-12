9ad00f4b64a6905a81312eef170b6e89f54e3a06
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2002-06-12 10:43:24 +0000

    # on 2002/06/12 10:43:24, irigoin did: Ajout de l'option HRC, hidden return code.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11297 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/syntax/return.c
+++ src/Libs/syntax/return.c
@@ -8,6 +8,9 @@
  * $Id$
  *
  * $Log: return.c,v $
+ * Revision 1.13  2002/06/12 10:43:24  irigoin
+ * Ajout de l'option HRC, hidden return code.
+ *
  * Revision 1.12  2002/06/08 16:17:53  irigoin
  * Functions added to replace formal return labels and actual return labels
  * by character strings
@@ -25,19 +28,23 @@
 #include "properties.h"
 
 #include "syntax.h"
-
+
 /* Should alternate returns be substituted or not ?
  *
- * If alternate returns are substituted, the code declarations should
- * be regenerated. The corresponding property should be checked.
- */
+ * If alternate returns are substituted, the code declarations should be
+ * regenerated unless hide_rc_p is TRUE and some PIPS run-time Fortran
+ * functions provided. The corresponding property should be checked.  */
 static bool substitute_rc_p = FALSE;
 static bool substitute_stop_p = FALSE;
+static bool hide_rc_p = FALSE;
+#define GET_RC_PREFIX "GET_"
+#define SET_RC_PREFIX "SET_"
 
 void
 SubstituteAlternateReturns(string option)
 {
-    substitute_rc_p = (strcmp(option, "RC")==0);
+    substitute_rc_p = (strcmp(option, "RC")==0) || (strcmp(option, "HRC")==0) ;
+    hide_rc_p = (strcmp(option, "HRC")==0) ;
     substitute_stop_p = (strcmp(option, "STOP")==0);
 
     if(!(substitute_rc_p || substitute_stop_p || strcmp(option, "NO")==0)) {
@@ -70,25 +77,53 @@ SubstituteAlternateReturnsP()
  */
 static entity return_code_variable = entity_undefined;
 
-entity
-GetReturnCodeVariable()
+entity GetReturnCodeVariable()
 {
-    string rc_name = get_string_property("PARSER_RETURN_CODE_VARIABLE");
-    /* Cannot be asserted because the return_code_variable may either be a
-     * formal parameter if the current module uses multiple return, or
-     * a dynamic variable if it does not and if it calls a subroutine
-     * using alternate returns
-     */
-    /*
+  string rc_name = get_string_property("PARSER_RETURN_CODE_VARIABLE");
+  /* Cannot be asserted because the return_code_variable may either be a
+   * formal parameter if the current module uses multiple return, or
+   * a dynamic variable if it does not and if it calls a subroutine
+   * using alternate returns
+   */
+  /*
     pips_assert("entity return_code_variable is undefined",
-		entity_undefined_p(return_code_variable));
-		*/
-    return_code_variable = global_name_to_entity(get_current_module_name(), rc_name);
-    if(entity_undefined_p(return_code_variable)) {
-	return_code_variable = FindOrCreateEntity(get_current_module_name(), rc_name);
-    }
+    entity_undefined_p(return_code_variable));
+  */
+  return_code_variable = global_name_to_entity(get_current_module_name(), rc_name);
+  if(entity_undefined_p(return_code_variable)) {
+    return_code_variable = FindOrCreateEntity(get_current_module_name(), rc_name);
+  }
 
-    return return_code_variable;
+  return return_code_variable;
+}
+
+static entity GetFullyDefinedReturnCodeVariable()
+{
+  entity rc = GetReturnCodeVariable();
+
+  /* Type, storage and initial value may have been set up earlier in
+     MakeFormalParameter(). */
+  if(type_undefined_p(entity_type(rc))) {
+    /* We must be dealing with the actual variable, not with the formal
+       variable. */
+    string module_name = get_current_module_name();
+    entity f = local_name_to_top_level_entity(module_name);
+    entity a = global_name_to_entity(module_name, DYNAMIC_AREA_LOCAL_NAME); 
+
+    entity_type(rc) = MakeTypeVariable(make_basic(is_basic_int, (void *) 4), NIL);
+
+    entity_storage(rc) = 
+      make_storage(is_storage_ram,
+		   make_ram(f, a,
+			    add_variable_to_area(a, rc),
+			    NIL));
+
+    entity_initial(rc) = MakeValueUnknown();
+  }
+
+  pips_assert("rc is defined", !entity_undefined_p(rc));
+  
+  return rc;
 }
 
 bool
@@ -155,7 +190,7 @@ list add_formal_return_code(list fpl)
 {
     list new_fpl = fpl;
 
-    if(substitute_rc_p && uses_alternate_return_p()) {
+    if(uses_alternate_return_p() && !hide_rc_p && substitute_rc_p) {
 	entity frc = GetReturnCodeVariable();
 
 	/* Type, storage and initial value are set up later in MakeFormalParameter() */
@@ -166,31 +201,15 @@ list add_formal_return_code(list fpl)
 
 list add_actual_return_code(list apl)
 {
-    list new_apl = apl;
+  list new_apl = apl;
 
-    if(substitute_rc_p && !ENDP(get_alternate_returns())) {
-	entity frc = GetReturnCodeVariable();
+  if(substitute_rc_p && !hide_rc_p && !ENDP(get_alternate_returns())) {
+    entity frc = GetFullyDefinedReturnCodeVariable();
 
-	/* Type, storage and initial value may have been set up earlier in
-           MakeFormalParameter() */
-	if(type_undefined_p(entity_type(frc))) {
-	    string module_name = get_current_module_name();
-	    entity f = local_name_to_top_level_entity(module_name);
-	    entity a = global_name_to_entity(module_name, DYNAMIC_AREA_LOCAL_NAME); 
-
-	    entity_type(frc) = MakeTypeVariable(make_basic(is_basic_int, (void *) 4), NIL);
-
-	    entity_storage(frc) = 
-		make_storage(is_storage_ram,
-			     make_ram(f, a,
-				      add_variable_to_area(a, frc),
-				      NIL));
-
-	    entity_initial(frc) = MakeValueUnknown();
-	}
-	new_apl = gen_nconc(apl, CONS(EXPRESSION, entity_to_expression(frc), NIL));
-    }
-    return new_apl;
+    new_apl = gen_nconc(apl, CONS(EXPRESSION, entity_to_expression(frc), NIL));
+  }
+    
+  return new_apl;
 }
 
 /* Keep track of the labels used as actual arguments for alternate returns
@@ -236,15 +255,63 @@ reset_alternate_returns()
     alternate_returns = list_undefined;
     current_number_of_alternate_returns = -1;
 }
+
+static statement make_get_rc_statement(expression rc_ref)
+{
+  statement s_get = statement_undefined;
+  instruction i_get = instruction_undefined;
+  string get_rc_name = strdup(concatenate(GET_RC_PREFIX,
+					  get_string_property("PARSER_RETURN_CODE_VARIABLE"),
+					  NULL));
+  entity get_rc = global_name_to_entity(TOP_LEVEL_MODULE_NAME, get_rc_name);
+
+  if(entity_undefined_p(get_rc)) {
+    get_rc = FindOrCreateEntity(TOP_LEVEL_MODULE_NAME, get_rc_name);
+    /* get_rc takes an argument and returns void */
+    entity_type(get_rc) = 
+      make_type(is_type_functional,
+		make_functional(CONS(PARAMETER, 
+				     make_parameter(make_type(is_type_variable,
+							      make_variable(make_basic(is_basic_int, (value) 4),
+									    NIL)),
+						    make_mode(is_mode_reference, UU)),
+				     NIL),
+				make_type(is_type_void, UU)));
+    /*
+    entity_type(get_rc) =
+      make_type(is_type_functional,
+		make_functional(NIL,
+				make_type(is_type_variable,
+					  make_variable(make_basic(is_basic_int, (value) 4),
+							NIL))));
+    */
+    entity_storage(get_rc) = make_storage(is_storage_rom, UU);
+    entity_initial(get_rc) = make_value(is_value_code, code_undefined);
+  }
+  
+  pips_assert("Function get_rc is defined", !entity_undefined_p(get_rc));
+  
+  i_get = make_instruction(is_instruction_call,
+			   make_call(get_rc, CONS(EXPRESSION, rc_ref, NIL)));
+  s_get = instruction_to_statement(i_get);
+  statement_number(s_get) = look_at_next_statement_number();
+
+  return s_get;
+}
+
 
 instruction generate_return_code_checks(list labels)
 {
     instruction i = instruction_undefined;
     list lln = NIL;
-    entity rcv = GetReturnCodeVariable();
-    expression ercv = entity_to_expression(rcv);
+    entity rcv = GetFullyDefinedReturnCodeVariable();
+    expression ercv = expression_undefined;
 
     pips_assert("The label list is not empty", !ENDP(labels));
+
+    
+
+    ercv = entity_to_expression(rcv);
 
     MAP(ENTITY, l, {
 	lln = CONS(STRING, label_local_name(l), lln);
@@ -257,6 +324,18 @@ instruction generate_return_code_checks(list labels)
     gen_free_list(lln);
 
     instruction_consistent_p(i);
+
+    if(hide_rc_p) {
+      statement s_init_rcv = make_get_rc_statement(entity_to_expression(rcv));
+
+      ifdebug(2) {
+	pips_debug(2, "Additional statement generated for hide_rc_p:\n");
+	print_statement(s_init_rcv);
+	}
+      pips_assert("i is a sequence", instruction_block_p(i));
+      instruction_block(i) = CONS(STATEMENT, s_init_rcv, instruction_block(i));
+      instruction_consistent_p(i);
+    }
 
     return i;
 }
@@ -272,47 +351,112 @@ instruction generate_return_code_checks(list labels)
 
 LOCAL entity end_label = entity_undefined;
 LOCAL char *end_label_local_name = RETURN_LABEL_NAME;
+	  
+static entity set_rc_function()
+{
+  string set_rc_name = strdup(concatenate(SET_RC_PREFIX,
+					  get_string_property("PARSER_RETURN_CODE_VARIABLE"),
+					  NULL));
+  entity set_rc = global_name_to_entity(TOP_LEVEL_MODULE_NAME, set_rc_name);
+
+  if(entity_undefined_p(set_rc)) {
+    set_rc = FindOrCreateEntity(TOP_LEVEL_MODULE_NAME, set_rc_name);
+    /* set_rc takes no argument and returns a scalar int */
+    entity_type(set_rc) = 
+      make_type(is_type_functional,
+		make_functional(CONS(PARAMETER, 
+				     make_parameter(make_type(is_type_variable,
+							      make_variable(make_basic(is_basic_int, (value) 4),
+									    NIL)),
+						    make_mode(is_mode_reference, UU)),
+				     NIL),
+				make_type(is_type_void, UU)));
+    entity_storage(set_rc) = make_storage(is_storage_rom, UU);
+    entity_initial(set_rc) = make_value(is_value_code, code_undefined);
+  }
+  
+  pips_assert("Function set_rc is defined", !entity_undefined_p(set_rc));
+
+  return set_rc;
+}
+
+/* The return code may be directly assigned or indirectly through a PIPS
+   run-time function call.*/
+static statement make_set_rc_statement(expression e)
+{
+  expression rc = expression_undefined_p(e)? int_to_expression(0) : e;
+  statement src = statement_undefined;
+
+  if(hide_rc_p) {
+    instruction isrc = make_instruction(is_instruction_call,
+					make_call(set_rc_function(),
+						  CONS(EXPRESSION, rc, NIL)));
+	  
+    src = instruction_to_statement(isrc);
+  }
+  else {
+    src = make_assign_statement(entity_to_expression(GetReturnCodeVariable()), rc);
+  }
+
+  ifdebug(2) {
+    pips_debug(2, "Statement generated: ");
+    print_statement(src);
+  }
+  
+  return src;
+}
 
 instruction MakeReturn(expression e)
 {
-    instruction inst = instruction_undefined;
+  instruction inst = instruction_undefined;
 
-    if(!expression_undefined_p(e) && !substitute_rc_p && !substitute_stop_p) {
-	user_error("MakeReturn", 
-		   "Lines %d-%d: Alternate return not supported. "
-		   "Standard return generated\n",
-		   line_b_I,line_e_I);
-    }
+  if(!expression_undefined_p(e) && !substitute_rc_p && !substitute_stop_p) {
+    user_error("MakeReturn", 
+	       "Lines %d-%d: Alternate return not supported. "
+	       "Standard return generated\n",
+	       line_b_I,line_e_I);
+  }
 
-    if (end_label == entity_undefined) {
-	end_label = MakeLabel(end_label_local_name);
-    }
+  if (end_label == entity_undefined) {
+    end_label = MakeLabel(end_label_local_name);
+  }
 
-    if(substitute_rc_p && uses_alternate_return_p()) {
-	/* Assign e to the return code variable, but be sure not to count
-	 * this assignment as a user instruction. Wrap if with the Go To
-	 * in a block and return the block instruction.
-	 *
-	 * See how code is synthesized for computed goto's...
-	 */
-	expression rc = expression_undefined_p(e)? int_to_expression(0) : e;
-	statement src = make_assign_statement(entity_to_expression(GetReturnCodeVariable()), rc);
-	statement jmp = instruction_to_statement(MakeGotoInst(end_label_local_name));
-
-	statement_number(src) = look_at_next_statement_number();
-	statement_number(jmp) = look_at_next_statement_number();
-	(void) get_next_statement_number();
-	inst = make_instruction_block(CONS(STATEMENT, src, CONS(STATEMENT, jmp, NIL)));
-	instruction_consistent_p(inst);
-    }
-    else if(!expression_undefined_p(e) && substitute_stop_p && uses_alternate_return_p()) {
-	inst = MakeZeroOrOneArgCallInst("STOP", e);
+  if(substitute_rc_p && uses_alternate_return_p()) {
+    /* Assign e to the return code variable, but be sure not to count
+     * this assignment as a user instruction. Wrap if with the Go To
+     * in a block and return the block instruction.
+     *
+     * See how code is synthesized for computed goto's...
+     */
+    statement src = make_set_rc_statement(e);
+    statement jmp = instruction_to_statement(MakeGotoInst(end_label_local_name));
+	
+    statement_number(src) = look_at_next_statement_number();
+    statement_number(jmp) = look_at_next_statement_number();
+    (void) get_next_statement_number();
+    inst = make_instruction_block(CONS(STATEMENT, src, CONS(STATEMENT, jmp, NIL)));
+    instruction_consistent_p(inst);
+  }
+  else if(!expression_undefined_p(e) && substitute_stop_p && uses_alternate_return_p()) {
+    /* Let's try to provide more useful information to the user */
+    /* inst = MakeZeroOrOneArgCallInst("STOP", e); */
+    if(expression_call_p(e)) {
+      string mn = get_current_module_name();
+      string sn = entity_local_name(call_function(syntax_call(expression_syntax(e))));
+      
+      inst = MakeZeroOrOneArgCallInst
+	("STOP", 
+	 MakeCharacterConstantExpression(strdup(concatenate("\"", sn, " in ", mn, "\"", NULL))));
     }
     else {
-	inst = MakeGotoInst(end_label_local_name);
+      pips_internal_error("unexpected argument type for RETURN");
     }
+  }
+  else {
+    inst = MakeGotoInst(end_label_local_name);
+  }
 
-    return inst;
+  return inst;
 }
 
 /* Generate a unique call to RETURN per module */
@@ -326,7 +470,7 @@ GenerateReturn()
     if(substitute_rc_p && uses_alternate_return_p()) {
 	entity l = MakeLabel(strdup(end_label_local_name));
 	expression rc = int_to_expression(0);
-	statement src = make_assign_statement(entity_to_expression(GetReturnCodeVariable()), rc);
+	statement src = make_set_rc_statement(rc);
 	statement jmp = statement_undefined;
 	    (MakeZeroOrOneArgCallInst("RETURN", expression_undefined));
 
@@ -367,14 +511,14 @@ expression generate_string_for_alternate_return_argument(string i)
   return e;
 }
 
-/* * used as formal label parameter is replaced by a string variable as
-   suggested by Fabien Coelho. Its storage and initial value are lated
-   initialized by MakeFormalParameter(). */
+/* * (star) used as formal label parameter is replaced by a string
+   variable as suggested by Fabien Coelho. Its storage and initial value
+   are lated initialized by MakeFormalParameter(). */
 entity generate_pseudo_formal_variable_for_formal_label()
 {
   entity fs = entity_undefined;
-  /* string lsp = get_string_property("PARSER_FORMAL_LABEL_SUBSTITUTE_PREFIX"); */
-  string lsp = "FORMAL_RETURN_LABEL_";
+  string lsp = get_string_property("PARSER_FORMAL_LABEL_SUBSTITUTE_PREFIX");
+  /* string lsp = "FORMAL_RETURN_LABEL_"; */
   /* let's assume that there are fewer than 999 formal label arguments */
   char buffer[4];
   string sn = &buffer[0];
@@ -401,10 +545,31 @@ bool formal_label_replacement_p(entity fp)
   bool replacement_p = FALSE;
   
   string fpn = entity_local_name(fp);
-  /* string lsp = get_string_property("PARSER_FORMAL_LABEL_SUBSTITUTE_PREFIX"); */
-  string lsp = "FORMAL_RETURN_LABEL_";
+  string lsp = get_string_property("PARSER_FORMAL_LABEL_SUBSTITUTE_PREFIX");
+  /* string lsp = "FORMAL_RETURN_LABEL_"; */
 
   replacement_p = (strstr(fpn, lsp)==fpn);
+  
+  return replacement_p;
+}
+
+bool actual_label_replacement_p(expression eap)
+{
+  bool replacement_p = FALSE;
+  string ls = entity_local_name(call_function(syntax_call(expression_syntax(eap))));
+  string p = ls+1;
+
+  replacement_p = (strlen(ls) >= 4
+    && *ls=='"' && *(ls+1)=='*' && *(ls+strlen(ls)-1)=='"');
+
+  if(replacement_p) {
+    for(p=ls+2; p<ls+strlen(ls)-1; p++) {
+      if(*p<'0'||*p>'9') {
+	replacement_p =FALSE;
+	break;
+      }
+    }
+  }
   
   return replacement_p;
 }
