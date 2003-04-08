1bfbe3a8e4a40c5eff6696114b0004541179c530
ferrand <ferrand@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-04-08 15:23:24 +0000

    # on 2003/04/08 15:23:24, ferrand did: Fix undefined symbols.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11413 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/sac/simdizer.c
+++ src/Libs/sac/simdizer.c
@@ -9,6 +9,9 @@
 #include "ri-util.h"
 #include "pipsdbm.h"
 
+#include "semantics.h"
+#include "effects-generic.h"
+
 #include "sac.h"
 
 bool simdizer(char * module_name)
@@ -16,24 +19,24 @@ bool simdizer(char * module_name)
    /* get the resources */
    statement module_stmt = (statement)
       db_get_memory_resource(DBR_CODE, module_name, TRUE);
-   set_proper_effects_map((statement_mapping)
-      db_get_memory_resource(DBR_PROPER_EFFECTS, module_name, TRUE));
-   set_preconditions_map((statement_mapping)
-      db_get_memory_resource(DBR_PRECONDITIONS, module_name, TRUE));
 
    set_current_module_statement(module_stmt);
    set_current_module_entity(local_name_to_top_level_entity(module_name));
+   set_proper_rw_effects((statement_effects)
+      db_get_memory_resource(DBR_PROPER_EFFECTS, module_name, TRUE));
+   set_precondition_map((statement_mapping)
+      db_get_memory_resource(DBR_PRECONDITIONS, module_name, TRUE));
 
    debug_on("VARIABLE_WIDTH_DEBUG_LEVEL");
    /* Now do the job */
-printf("My code is called !!! Sooo sweet....\n");
+   printf("My code is called !!! Sooo sweet....\n");
    debug_off();
 
    /* update/release resources */
    reset_current_module_statement();
    reset_current_module_entity();
-   reset_proper_effects_map();
-   reset_preconditions_map();
+   reset_proper_rw_effects();
+   reset_precondition_map();
 
    return TRUE;
 }
