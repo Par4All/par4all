7905734fd884d42e087109451cf46d0de97f8851
ferrand <ferrand@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-04-15 14:16:57 +0000

    # on 2003/04/15 14:16:57, ferrand did: Compute variable size as declared. Shoud work fine :)
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11418 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/sac/varwidth.c
+++ src/Libs/sac/varwidth.c
@@ -11,31 +11,102 @@
 
 #include "sac.h"
 
-/* bool effective_variable_width(char * module_name) */
-bool simdizer(char * module_name)
+int effective_reference_width(reference r)
 {
-   /* get the resources */
-   statement module_stmt = (statement)
-      db_get_memory_resource(DBR_CODE, module_name, TRUE);
-   set_proper_effects_map((statement_mapping)
-      db_get_memory_resource(DBR_PROPER_EFFECTS, module_name, TRUE));
-   set_preconditions_map((statement_mapping)
-      db_get_memory_resource(DBR_PRECONDITIONS, module_name, TRUE));
+   basic b;
+   type t = entity_type(reference_variable(r));
 
-   set_current_module_statement(module_stmt);
-   set_current_module_entity(local_name_to_top_level_entity(module_name));
+   if (!type_variable_p(t))
+      return 1000; /* big number, to ensure no parallelization */
+   b = variable_basic(type_variable(t));
 
-   debug_on("VARIABLE_WIDTH_DEBUG_LEVEL");
-   /* Now do the job */
-printf("My code is called !!! Sooo sweet....\n");
-   debug_off();
+   switch(basic_tag(b))
+   {
+      case is_basic_int:
+	 return basic_int(b);
 
-   /* update/release resources */
-   reset_current_module_statement();
-   reset_current_module_entity();
-   reset_proper_effects_map();
-   reset_preconditions_map();
+      case is_basic_float:
+	 return basic_float(b);
 
-   return TRUE;
+      case is_basic_logical:
+	 return basic_logical(b);
+
+      default:
+	 return 1000; /* big number, to ensure no parallelization */
+   }
 }
+
+int effective_call_variables_width(call c)
+{
+   cons * args;
+   int maxArgsWidth;
+
+   maxArgsWidth = 0;
+
+   /* Look at each argument */
+   for( args = call_arguments(c);
+        args != NIL;
+	args = CDR(args) )
+   {
+      int curArgWidth;
+      syntax s;
+
+      /* make sure the argument is a reference */
+      s = expression_syntax(EXPRESSION(CAR(args)));
+      if (!syntax_reference_p(s))
+      {
+	 /* we are supposed to be in 3-form, so this should not happen */
+	 printf("WARNING: effective_variable_width called on non 3-form"
+		"call statemnent.\n");
+	 return 1000; /* big number, to ensure no parallelization */
+      }
+      
+      /* compute the actual width of the parameter */
+      curArgWidth = effective_reference_width(syntax_reference(s));
+
+      /* infer the maximum args width */
+      if (curArgWidth > maxArgsWidth)
+	 maxArgsWidth = curArgWidth;
+   }
+
+   return maxArgsWidth;
+}
+
+int effective_variables_width(instruction i)
+{
+   switch(instruction_tag(i))
+   {
+      case is_instruction_sequence:
+      {
+	 cons * j;
+	 int maxVarWidth = 0;
+
+	 for( j = sequence_statements(instruction_sequence(i));
+	      j != NIL;
+	      j = CDR(j) )
+	 {
+	    int varwidth = 
+	       effective_variables_width(statement_instruction(STATEMENT(CAR(j))));
+
+	    if (varwidth > maxVarWidth)
+	       maxVarWidth = varwidth;
+	 }
+	 
+	 return maxVarWidth;
+      }
+
+      case is_instruction_test:
+      case is_instruction_loop:
+      case is_instruction_whileloop:
+      case is_instruction_goto:
+      case is_instruction_unstructured:
+      default:
+	 printf("WARNING: complex loop !\n");
+	 return 1000; /* big number, to ensure no parallelization */
+
+      case is_instruction_call:
+	 return effective_call_variables_width(instruction_call(i));
+   }
+}
+
 
