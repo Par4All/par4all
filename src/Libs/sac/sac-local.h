643155575a1eea71474001e698a690805b9a5658
ferrand <ferrand@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-05-22 08:49:24 +0000

    # on 2003/05/22 08:49:24, ferrand did: Add structure definition for match, opcode and operation.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11469 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/sac/sac-local.h
+++ src/Libs/sac/sac-local.h
@@ -2,6 +2,28 @@
 #ifndef __SAVC_LOCAL_H__
 #define __SAVC_LOCAL_H__
 
-/* nothing much here.... */
+typedef struct {
+      int type;
+      list args;        /* <expression> */
+} _match, * match;
+
+#define MATCH(x) ((match)((x).p))
+
+
+typedef struct {
+      char * name;
+      int vectorSize;       /* nb of subwords in a register argument */
+      int subwordSize;      /* nb bits per subword */
+} _opcode, * opcode;
+
+#define OPCODE(x) ((opcode)((x).p))
+
+
+typedef struct {
+      int nbArgs;
+      list opcodes;     /* <opcode> */
+} _operation, * operation;
+
+#define OPERATION(x) ((operation)((x).p))
 
 #endif /*__SAVC_LOCAL_H__*/
