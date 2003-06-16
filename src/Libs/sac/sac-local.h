7bd33e58c7206b9641822deb4d9a265b45e5a00f
ferrand <ferrand@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-06-16 12:50:27 +0000

    # on 2003/06/16 12:50:27, ferrand did: Update to use newgen data structures.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11538 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/sac/sac-local.h
+++ src/Libs/sac/sac-local.h
@@ -2,28 +2,6 @@
 #ifndef __SAVC_LOCAL_H__
 #define __SAVC_LOCAL_H__
 
-typedef struct {
-      int type;
-      list args;        /* <expression> */
-} _match, * match;
-
-#define MATCH(x) ((match)((x).p))
-
-
-typedef struct {
-      char * name;
-      int vectorSize;       /* nb of subwords in a register argument */
-      int subwordSize;      /* nb bits per subword */
-} _opcode, * opcode;
-
-#define OPCODE(x) ((opcode)((x).p))
-
-
-typedef struct {
-      int nbArgs;
-      list opcodes;     /* <opcode> */
-} _operation, * operation;
-
-#define OPERATION(x) ((operation)((x).p))
+#include "sac_private.h"
 
 #endif /*__SAVC_LOCAL_H__*/
