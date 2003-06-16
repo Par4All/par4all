51702315a1f88804eccef99f638bc74c410467c9
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-06-16 15:05:34 +0000

    # on 2003/06/16 15:05:34, coelho did: char* -> void*
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11562 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/pipsdbm/pipsdbm-local.h
+++ src/Libs/pipsdbm/pipsdbm-local.h
@@ -14,13 +14,13 @@
 /* conform to old interface.
  */
 #define DB_PUT_MEMORY_RESOURCE(res_name, own_name, res_val) \
-  db_put_or_update_memory_resource(res_name, own_name, (char*) res_val, TRUE)
+  db_put_or_update_memory_resource(res_name, own_name, (void*) res_val, TRUE)
 
 /* put a resource which is a file. it is just a resource as any other.
  */
 #define DB_PUT_FILE_RESOURCE DB_PUT_MEMORY_RESOURCE
 #define DB_PUT_NEW_FILE_RESOURCE(res_name, own_name, res_val) \
-  db_put_or_update_memory_resource(res_name, own_name, (char*) res_val, FALSE)
+  db_put_or_update_memory_resource(res_name, own_name, (void*) res_val, FALSE)
 
 #define db_get_file_resource db_get_memory_resource
 #define db_unput_a_resource(r,o) (db_delete_resource(r,o), TRUE)
