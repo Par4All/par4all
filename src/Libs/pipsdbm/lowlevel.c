b03771a09c39c84ed9e8cd09e5307e4f709a113c
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-06-25 13:22:39 +0000

    # on 2003/06/25 13:22:39, coelho did: better debug.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11655 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/pipsdbm/lowlevel.c
+++ src/Libs/pipsdbm/lowlevel.c
@@ -126,13 +126,18 @@ string db_get_directory_name_for_module(string name)
  */
 string db_build_file_resource_name(string rname, string oname, string suffix)
 {
-    if (same_string_p(oname, "")) oname = DEFAULT_OWNER_NAME;
-    free(db_get_directory_name_for_module(oname));/* mkdir as a side effect. */
-    /* the next name must be compatible with the Display script...
-     * it may depend on the builder function maybe (if pipsmake tells)
-     * may include the resource name? as lower letters?
-     */       
-    return strdup(concatenate(oname, "/", oname, suffix, 0));
+  string result;
+  if (same_string_p(oname, "")) oname = DEFAULT_OWNER_NAME;
+  free(db_get_directory_name_for_module(oname));/* mkdir as a side effect. */
+  /* the next name must be compatible with the Display script...
+   * it may depend on the builder function maybe (if pipsmake tells)
+   * may include the resource name? as lower letters?
+   */       
+  result = strdup(concatenate(oname, "/", oname, suffix, 0));
+
+  pips_debug(8, "file name for %s[%s] with suffix '%s' is '%s'\n", 
+	     rname, oname, suffix, result);
+  return result;
 }
 
 /* allocate a full file name for the given resource.
