5119a4d5c5fa8e0dba81e3c7624273e0b021b7ed
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-06-27 12:10:08 +0000

    # on 2003/06/27 12:10:08, irigoin did: First version computing HEIGHT and DEPTh as well as CALLERS
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11688 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/pipsmake/callgraph.c
+++ src/Libs/pipsmake/callgraph.c
@@ -1,3 +1,15 @@
+/* Global computation of CALLERS, HEIGHT and DEPTH
+ *
+ * $Id$
+ *
+ * $Log: callgraph.c,v $
+ * Revision 1.9  2003/06/27 12:10:08  irigoin
+ * First version computing HEIGHT and DEPTh as well as CALLERS
+ *
+ *
+ */
+
+
 #include <stdio.h>
 #include <string.h>
 
@@ -10,6 +22,10 @@
 #include "ri-util.h"
 #include "pipsdbm.h"
 #include "resources.h"
+
+/* FI: just of call to rmake */
+/* #include "pipsmake.h" */
+#include "phases.h"
 
 static void print_callees(callees c)
 {
@@ -24,7 +40,7 @@ static void transitive_positions(set vertices,
 				 hash_table arcs,
 				 hash_table position)
 {
-  int n = 1;
+  int n = 1; /* Number of modules processed at the previous iteration */
   int iter = 0;
   int nvertices = 0;
 
@@ -34,7 +50,16 @@ static void transitive_positions(set vertices,
 
     SET_MAP(v1, {
       int cmp = 0; /* current module position */
-      list destinations = callees_callees((callees) hash_get(arcs, (void *) v1));
+      string source_module = (string) v1; /* gdb does not access v1 */
+      callees c = (callees) hash_get(arcs, (void *) v1);
+      list destinations = list_undefined;
+
+      if(c == (callees) HASH_UNDEFINED_VALUE) {
+	pips_internal_error("Arcs undefined for module %s\n", source_module);
+      }
+      else {
+	destinations = callees_callees(c);
+      }
 
       if(hash_get(position, v1) != HASH_UNDEFINED_VALUE)
 	/* already processed */
@@ -59,8 +84,8 @@ static void transitive_positions(set vertices,
       next: ;
       }
     }, vertices);
-    fprintf(stderr, "Iteration %d completed with %d vertices processed\n"
-	    "(total number of vertices processed: %d)\n", iter, n, nvertices);
+    pips_debug(1, "Iteration %d completed with %d vertices processed\n"
+	       "(total number of vertices processed: %d)\n", iter, n, nvertices);
   }
 
   /* Check that all vertices are associated to a position... which only is
@@ -71,10 +96,17 @@ static void transitive_positions(set vertices,
       pips_assert("p is defined", p != (int) HASH_UNDEFINED_VALUE);
     }, vertices);
   }
+
+  n = 0;
   SET_MAP(v1, {
-    if(hash_get(position, (void *) v1) == HASH_UNDEFINED_VALUE)
+    if(hash_get(position, (void *) v1) == HASH_UNDEFINED_VALUE) {
       pips_user_warning("Module %s might be part of a recursive call cycle\n", (string) v1);
+      n++;
+    }
   }, vertices);
+  if(n!=0)
+      pips_user_warning("%d module could not be given a position in the call graph,"
+			" probably because of a recursive call cycle.\n", n);
 }
 
 /* callgraph computes the caller list of each module, using the callees
@@ -96,6 +128,8 @@ bool callgraph(string name)
   gen_array_t module_array = db_get_module_list();
   int nmodules = gen_array_nitems(module_array);
   int i = 0;
+  int n_new_modules = 0; /* Number of modules called whose source code is
+                            missing but synthesized by PIPS */
   /* Should we deal with strings or with entities? */
   set modules = set_make(set_string);
   hash_table module_callers = hash_table_make(hash_string, 2*nmodules);
@@ -106,7 +140,8 @@ bool callgraph(string name)
   pips_assert("To silence gcc", name==name);
   pips_assert("The workspace contains at least one module", nmodules>0);
 
-  /* Define the module_set and initialize the module callers*/
+  /* Define the module_set and initialize the module callers, except for
+     modules whose source code is missing. */
   for(i=0; i<nmodules; i++) {
     string module_name = gen_array_item(module_array, i);
     callees c = callees_undefined;
@@ -117,7 +152,13 @@ bool callgraph(string name)
     hash_put(module_callers, (void *) module_name, (void *) c);
   }
 
-  /* Compute iteratively the callers from the callees. */
+  /* Compute iteratively the callers from the callees. Synthesize missing
+   * codes if necessary and if the corresponding property is set.
+   *
+   * The number of iterations depends on the order of the modules in the hash-tables.
+   *
+   * Simple but inefficient implementation for Cathar-2
+   * */
   for(i=0; i<nmodules; i++) {
     string module_name = gen_array_item(module_array, i);
     callees called_modules = callees_undefined;
@@ -136,8 +177,43 @@ bool callgraph(string name)
 
       /* Should not be an error as PIPS can synthesize missing code
 	 and does it elsewhere... */
-      if(!found) pips_user_error("no source file for module %s\n", 
-				 module_called);
+      if(!found) {
+	extern bool rmake(string, string);
+
+	pips_user_warning("no source file for module %s, let's try so synthesize code\n", 
+			  module_called);
+	reset_pips_current_computation();
+	/* set_pips_current_computation(DBR_CALLEES, module_called); */
+	/* push_pips_current_computation(DBR_CALLEES, module_called); */
+	if(rmake(DBR_CALLEES, module_called)) {
+	  /* It has no callees to exploit anyway; it does not matter that
+             it is not looped over by the main loop. module_callers is
+             going to be updated and will be used to store the results. */
+	  callees c = callees_undefined;
+
+	  n_new_modules++;
+
+	  set_add_element(modules, modules, module_called);
+
+	  c = make_callees(NIL);
+	  hash_put(module_callers, (void *) module_called, (void *) c);
+	  c =  (callees)
+	    db_get_memory_resource(DBR_CALLEES, module_called, TRUE);
+	  hash_put(module_callees, (void *) module_called, (void *) c);
+	}
+	else {
+	  /* You cannot call pips_user_error() again, as it has just been
+             called by rmake via apply_a_rule()*/
+	  /*
+	    pips_user_error("Provide or let PIPS synthesize source code for module %s\n",
+	    module_called);*/
+	  set_pips_current_computation(BUILDER_CALLGRAPH, name);
+	  return FALSE;
+	}
+	/* pop_pips_current_computation(DBR_CALLEES, module_called); */
+	/* reset_pips_current_computation(); */
+	set_pips_current_computation(BUILDER_CALLGRAPH, name);
+      }
 
       c = (callees) hash_get(module_callers, (void *) module_called);
       pips_assert("callers are always initialized",
@@ -148,10 +224,10 @@ bool callgraph(string name)
     }
   }
 
-  /*
+  pips_debug(1, "Compute heights from callee arcs");
   transitive_positions(modules, module_callees, module_height);
+  pips_debug(1, "Compute depths from caller arcs");
   transitive_positions(modules, module_callers, module_depth);
-  */
 
   ifdebug(7) {
     HASH_MAP(module_name, callers,
@@ -163,18 +239,42 @@ bool callgraph(string name)
       fprintf(stderr, "DEPTH = %d\n", (int) hash_get(module_depth, (string) module_name));
     }, module_callers);
   }
+
+  /* Since Fabien is not available to help with pipsdbm... Let's move on with Cathare-2! */
+  /*
+  if(TRUE) {
+    FILE * h_file = safe_fopen("height", "w");
+    FILE * d_file = safe_fopen("depth", "w");
+    HASH_MAP(module_name, callers,
+    {
+      fprintf(h_file,"%s\t%d\n", (string) module_name,
+	      (int) hash_get(module_height, (string) module_name));
+      fprintf(d_file,"%s\t%d\n", (string) module_name,
+	      (int) hash_get(module_depth, (string) module_name));
+    }, module_callers);
+    safe_fclose(h_file, "height");
+    safe_fclose(d_file, "depth");
+  }
+  */
     
   i=0;
 
   HASH_MAP(module_name, callers,
   {
+    char depth[13];
+    char height[13];
+
     DB_PUT_MEMORY_RESOURCE(DBR_CALLERS, (string) module_name, (void *) callers);
-    DB_PUT_MEMORY_RESOURCE(DBR_DEPTH, (string) module_name, (void *) strdup("0"));
-    DB_PUT_MEMORY_RESOURCE(DBR_HEIGHT, (string) module_name, (void *) strdup("1"));
+
+    sprintf(depth,"%d", (int) hash_get(module_depth, (string) module_name));
+    DB_PUT_MEMORY_RESOURCE(DBR_DEPTH, (string) module_name, (void *) strdup(depth));
+
+    sprintf(height,"%d", (int) hash_get(module_height, (string) module_name));
+    DB_PUT_MEMORY_RESOURCE(DBR_HEIGHT, (string) module_name, (void *) strdup(height));
     i++;
   }, module_callers);
 
-  pips_assert("The number of modules is unchanged", i==nmodules);
+  pips_assert("The number of modules is unchanged", i==nmodules+n_new_modules);
 
   hash_table_free(module_callees);
   hash_table_free(module_callers);
