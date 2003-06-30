ae8c35195205899fbc755d496071977c133ea46d
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-06-30 10:16:58 +0000

    # on 2003/06/30 10:16:58, nguyen did: Change phase names and add more functions to C prettyprinter
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11704 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/prettyprint/cprettyprinter.c
+++ src/Libs/prettyprint/cprettyprinter.c
@@ -8,11 +8,11 @@
    It might be really slow, but it should be safe.
    I should use some kind of string accumulator (array/list...)
 
-   cprinted_file > MODULE.crough
+   print_c_rough > MODULE.crough
                  < PROGRAM.entities
                  < MODULE.code
 
-   cindent_file  > MODULE.c
+   print_c_code  > MODULE.c_printed_file
                  < MODULE.crough
 */
 
@@ -71,7 +71,11 @@ c_basic_string(basic b)
     case is_basic_int:
       switch (basic_int(b))
 	{
+	case 2: result = "short" SPACE; 
+	  break;
 	case 4: result = "int" SPACE; 
+	  break;
+	case 6: result = "long" SPACE; 
 	  break;
 	case 8: result = "long long" SPACE; 
 	  break;
@@ -92,6 +96,8 @@ c_basic_string(basic b)
     case is_basic_string:
       result = "char" SPACE;
       break;
+
+      /* add (short, long, long long) signed, unsigned, bit, pointer, derived, typedef*/
     }
 
   return strdup(result);
@@ -109,39 +115,56 @@ int_to_string(int i)
    lower is assumed to be 0 in references.
    Caution: the dimension order is reversed! I want it!
  */
-static string
-c_dim_string(list /* of dimension */ ldim)
+static string c_dim_string(list ldim)
 {
   string result = strdup("");
-
   if (ldim) 
     {
       MAP(DIMENSION, dim,
       {
+	expression elow = dimension_lower(dim);
+	expression eup = dimension_upper(dim);
 	int low;
 	int up;
-	int size;
-	string oldresult;
-	string ssize;
+	string slow;
+	string sup;
 
-	expression_integer_value(dimension_lower(dim), &low);
-	expression_integer_value(dimension_upper(dim), &up);
+	/* In fact, the lower bound of array in C is always equal to 0, 
+	   we only need to print the upper dimension 
+	   but in order to handle Fortran code, we check all other possibilities.
 
-	size = up - low + 1;
-	oldresult = result;
-	ssize = int_to_string(size);
 
-	result = 
-	  strdup(concatenate(OPENBRACKET, ssize, CLOSEBRACKET, result, NULL));
+	NHAM ROI, X[N] TUONG DUONG VOI X[0] DEN X[N-1], nen khong phai tru 1*/
 
-	free(oldresult);
-	free(ssize);
+	if (expression_integer_value(elow, &low))
+	  {
+	    if (low == 0)
+	      result = strdup(concatenate(OPENBRACKET,words_to_string(words_expression(eup)),
+					  CLOSEBRACKET,result,NULL));
+	    else 
+	      {
+		if (expression_integer_value(eup, &up))
+		  result = strdup(concatenate(OPENBRACKET,int_to_string(up-low),CLOSEBRACKET,result,NULL));
+		else
+		  {
+		    sup = words_to_string(words_expression(eup));
+		    result = strdup(concatenate(OPENBRACKET,sup,"-",int_to_string(low),CLOSEBRACKET,result,NULL));
+		    free(sup);
+		  }
+	      }
+	  }
+	else 
+	  {
+	    slow = words_to_string(words_expression(elow));
+	    sup = words_to_string(words_expression(eup));
+	    result = strdup(concatenate(OPENBRACKET,sup,"-",slow,CLOSEBRACKET,result,NULL));
+	    free(slow);
+	    free(sup);
+	  }
       },
-	ldim);
+	  ldim);
     }
-  /* otherwise the list is empty, no dimension to declare.
-   */
-
+  /* otherwise the list is empty, no dimension to declare */
   return result;
 }
 
@@ -179,8 +202,6 @@ this_entity_cdeclaration(entity var)
 {
   string result = NULL;
   storage s = entity_storage(var);
-
-  /* fprintf(stderr, "-- %s\n", entity_name(var)); */
   
   if (storage_rom_p(s))
     {
@@ -205,6 +226,7 @@ this_entity_cdeclaration(entity var)
 	      free(svar);
 	      free(sval);
 	    }
+	  /* What about real, double, string, ... ?*/
 	}
     }
   else
@@ -214,11 +236,17 @@ this_entity_cdeclaration(entity var)
       string st, sd, svar;
       
       t = entity_type(var);
+
+      /* add other types here : functional, struct, union, enum */
+
       pips_assert("it is a variable", type_variable_p(t));
       v = type_variable(t);
   
       st = c_basic_string(variable_basic(v));
       sd = c_dim_string(variable_dimensions(v));
+
+      /* add variable qualifiers here */
+
       svar = c_entity_local_name(var);
       
       result= strdup(concatenate(st, SPACE, svar, sd, NULL));
@@ -231,6 +259,7 @@ this_entity_cdeclaration(entity var)
 
 static bool parameter_p(entity e)
 {
+  /* Constant variables */
   return storage_rom_p(entity_storage(e)) && 
     value_symbolic_p(entity_initial(e)) &&
     type_functional_p(entity_type(e));
@@ -245,6 +274,7 @@ static bool variable_p(entity e)
 
 static bool argument_p(entity e)
 {
+  /* Formal variables */
   return type_variable_p(entity_type(e)) && 
     storage_formal_p(entity_storage(e));
 }
@@ -264,6 +294,7 @@ c_declarations(entity module,
   c = value_code(entity_initial(module));
   MAP(ENTITY, var,
   {
+    debug(2, "\n Prettyprinter declaration for variable :",entity_local_name(var));   
     if (consider_this_entity(var))
       {
 	string old = result;
@@ -274,9 +305,7 @@ c_declarations(entity module,
 	free(svar);
 	first = FALSE;
       }
-  },
-    code_declarations(c));
-  
+  },code_declarations(c));
   return result;
 }
 
@@ -295,6 +324,7 @@ static string c_head(entity module)
 
   if (entity_main_module_p(module))
     {
+      /* another kind : "int main(void)" ?*/
       result = strdup(MAIN_DECLARATION);
     }
   else
@@ -395,24 +425,31 @@ static string ppt_unary(string in_c, list le)
 
 static string ppt_call(string in_c, list le)
 {
-  string scall = strdup(concatenate(in_c, OPENPAREN, NULL)), old;
-  bool first = TRUE;
+  string scall, old;
+  if (le == NIL)
+    { 
+      scall = strdup(concatenate(in_c, NULL));
+    }
+  else 
+    {
+      bool first = TRUE;
+      scall = strdup(concatenate(in_c, OPENPAREN, NULL));
+     
+      /* Attention: not like this for io statements*/
+      MAP(EXPRESSION, e,
+      {
+	string arg = c_expression(e);
+	old = scall;
+	scall = strdup(concatenate(old, first? "": ", ", arg, NULL));
+	free(arg);
+	free(old);
+	first = FALSE;
+      },le);
 
-  MAP(EXPRESSION, e,
-  {
-    string arg = c_expression(e);
-    old = scall;
-    scall = strdup(concatenate(old, first? "": ", ", arg, NULL));
-    free(arg);
-    free(old);
-    first = FALSE;
-  },
-      le);
-
-  old = scall;
-  scall = strdup(concatenate(old, CLOSEPAREN, NULL));
-  free(old);
-
+      old = scall;
+      scall = strdup(concatenate(old, CLOSEPAREN, NULL));
+      free(old);
+    }
   return scall;
 }
 
@@ -433,10 +470,12 @@ static struct s_ppt fortran_to_c[] =
   { ".OR.", "||", ppt_binary },
   { ".NOT.", "!", ppt_unary },
   { NULL, NULL, ppt_call }
+  /* add things here ? */
 };
 
 /* return the prettyprinter structure for c.
- */
+ 
+Not only from Fortran ?*/
 static struct s_ppt * get_ppt(entity f)
 {
   string called = entity_local_name(f);
@@ -510,8 +549,7 @@ static string c_reference(reference r)
     result = strdup(concatenate(OPENBRACKET, s, CLOSEBRACKET, old, NULL));
     free(old);
     free(s);
-  },
-      reference_indices(r));
+  }, reference_indices(r));
 
   old = result;
   svar = c_entity_local_name(reference_variable(r));
@@ -536,12 +574,160 @@ static string c_expression(expression e)
     case is_syntax_reference:
       result = c_reference(syntax_reference(s));
       break;
+      /* add cast, sizeof here */
     default:
       pips_internal_error("unexpected syntax tag");
     }
   return result;
 }
 
+static string c_statement(statement s);
+
+static string c_unstructured(unstructured u)
+{
+  string result =  strdup(EMPTY);
+  /* build an arbitrary reverse trail of control nodes */
+  list trail = unstructured_to_trail(u);
+  list cc = NIL;
+  trail = gen_nreverse(trail);
+  dump_trail(trail);
+  
+  for(cc=trail; !ENDP(cc); POP(cc)) 
+    {
+      control c = CONTROL(CAR(cc));
+      int nsucc = gen_length(control_successors(c));
+      statement st = control_statement(c);
+      string oldresult = result;   
+      switch(nsucc) 
+	{
+	case 0:
+	  {	       
+	    result = strdup(concatenate(oldresult,c_statement(st),NULL));
+	    fprintf(stderr, "Where 0 \n");
+	    break;
+	  }
+	case 1: 
+	  {
+	    control succ = CONTROL(CAR(control_successors(c)));
+	    if(check_io_statement_p(control_statement(succ)) &&
+	       !get_bool_property("PRETTYPRINT_CHECK_IO_STATEMENTS")) 
+	       {
+		 succ = CONTROL(CAR(CDR(control_successors(succ))));
+		 if(check_io_statement_p(control_statement(succ)) &&
+		   !get_bool_property("PRETTYPRINT_CHECK_IO_STATEMENTS")) 
+		  {
+		    
+		    succ = CONTROL(CAR(CDR(control_successors(succ))));
+		  }
+		 pips_assert("The successor is not a check io statement",
+			     !check_io_statement_p(control_statement(succ)));
+	       }
+	   
+	    result = strdup(concatenate(oldresult,c_statement(st),NULL));
+	    if(statement_does_return(st)) {
+	      if(!ENDP(CDR(cc))) {
+		control tsucc = CONTROL(CAR(CDR(cc)));
+		if(tsucc==succ) 
+		  {
+		    break;
+		  }
+	      }
+	    }
+	    fprintf(stderr, "Where 1 \n");
+	    break;
+	  }
+	case 2: 
+	  {
+	    control succ1 = CONTROL(CAR(control_successors(c)));
+	    control succ2 = CONTROL(CAR(CDR(control_successors(c))));
+	    instruction i = statement_instruction(st);
+	    test t = instruction_test(i);
+	    pips_assert("must be a test", instruction_test_p(i));
+	    fprintf(stderr, "Where 2 \n");
+	    result = strdup(concatenate(oldresult,c_statement(st),NULL));
+	    break;
+	  }
+	default:
+	  pips_internal_error("Too many successors for a control node\n");
+	}
+      free(oldresult);
+    }   
+  
+  gen_free_list(trail);
+  return result;
+}
+
+static string c_test(test t)
+{
+  string result;
+  bool no_false;
+  string cond, strue, sfalse;
+  cond = c_expression(test_condition(t));
+  strue = c_statement(test_true(t));
+  no_false = empty_statement_p(test_false(t));
+  
+  sfalse = no_false? NULL: c_statement(test_false(t));
+  
+  result = strdup(concatenate("if (", cond, ") {" NL, 
+			      strue, 
+			      no_false? "}" NL: "} else {" NL,
+			      sfalse, "}" NL, NULL));
+  free(cond);
+  free(strue);
+  if (sfalse) free(sfalse);
+  return result;
+}
+
+static string c_sequence(sequence seq)
+{
+  string result = strdup(EMPTY);
+  MAP(STATEMENT, s,
+  {
+    string oldresult = result;
+    string current = c_statement(s);
+    result = strdup(concatenate(oldresult, current, NULL));
+    free(current);
+    free(oldresult);
+  }, sequence_statements(seq));
+  return result;
+}
+
+static string c_loop(loop l)
+{
+  /* partial implementation...
+     However, there is not this kind of loop in C */
+  string result;
+  string body = c_statement(loop_body(l));
+  string index = c_entity_local_name(loop_index(l));
+  range r = loop_range(l);
+  string low = c_expression(range_lower(r));
+  string up = c_expression(range_upper(r));
+  /* what about step*/
+  result = strdup(concatenate("for (", index, "=", low, "; ",
+			      index, "<=", up, "; ",
+			      index, "++)", SPACE, OPENBRACE, NL, 
+			      body, CLOSEBRACE, NL, NULL));
+  free(body);
+  free(low);
+  free(up);
+  free(index);
+  return result;
+}
+
+static string c_whileloop(whileloop w)
+{
+  /* partial implementation... */
+  string result;
+  string body = c_statement(whileloop_body(w));
+  string cond = c_expression(whileloop_condition(w));
+  /*do while and while do loops */
+  result = strdup(concatenate("while (", cond, ") {" NL, 
+			      body, "}" NL, NULL));
+  
+  free(cond);
+  free(body);
+  return result;
+}
 /**************************************************************** STATEMENTS */
 
 static string c_statement(statement s)
@@ -552,92 +738,51 @@ static string c_statement(statement s)
     {
     case is_instruction_test:
       {
-	bool no_false;
 	test t = instruction_test(i);
-	string cond, strue, sfalse;
-	cond = c_expression(test_condition(t));
-	strue = c_statement(test_true(t));
-	
-	no_false = empty_statement_p(test_false(t));
-
-	sfalse = no_false? NULL: c_statement(test_false(t));
-
-	result = strdup(concatenate("if (", cond, ") {" NL, 
-				    strue, 
-				    no_false? "}" NL: "} else {" NL,
-				    sfalse, "}" NL, NULL));
-	free(cond);
-	free(strue);
-	if (sfalse) free(sfalse);
-
+	result = c_test(t);
 	break;
       }
-
     case is_instruction_sequence:
       {
-	result = strdup(EMPTY);
-	MAP(STATEMENT, s,
-	{
-	  string oldresult = result;
-	  string current = c_statement(s);
-	  result = strdup(concatenate(oldresult, current, NULL));
-	  free(current);
-	  free(oldresult);
-	},
-	    sequence_statements(instruction_sequence(i)));
+	sequence seq = instruction_sequence(i);
+	result = c_sequence(seq);
 	break;
       }
-
     case is_instruction_loop:
       {
-	/* partial implementation...
-	 */
 	loop l = instruction_loop(i);
-	string body = c_statement(loop_body(l));
-	string index = c_entity_local_name(loop_index(l));
-	range r = loop_range(l);
-	string low = c_expression(range_lower(r));
-	string up = c_expression(range_upper(r));
-	
-	result = strdup(concatenate("for (", index, "=", low, "; ",
-				    index, "<=", up, "; ",
-				    index, "++)", SPACE, OPENBRACE, NL, 
-				    body, CLOSEBRACE, NL, NULL));
-
-	free(body);
-	free(low);
-	free(up);
-	free(index);
-
+	result = c_loop(l);
 	break;
       }
-    
     case is_instruction_whileloop:
       {
 	whileloop w = instruction_whileloop(i);
-	string body = c_statement(whileloop_body(w));
-	string cond = c_expression(whileloop_condition(w));
-	
-	result = strdup(concatenate("while (", cond, ") {" NL, 
-				    body, "}" NL, NULL));
-
-	free(cond);
-	free(body);
-
+	result = c_whileloop(w);
 	break;
       }
-
     case is_instruction_call:
       {
 	string scall = c_call(instruction_call(i));
 	result = strdup(concatenate(scall, SEMICOLON, NULL));
 	break;
       }
-      
     case is_instruction_unstructured:
+      {
+	unstructured u = instruction_unstructured(i);
+	result = c_unstructured(u);
+	break;
+      }
     case is_instruction_goto:
+      {
+	statement g = instruction_goto(i);
+	entity el = statement_label(g);
+	string l = entity_local_name(el) + strlen(LABEL_PREFIX);
+	result = strdup(concatenate("goto ",l, SEMICOLON, NULL));
+	break;
+      }
+      /* add switch, forloop break, continue, return instructions here*/
     default:
-      result = strdup(concatenate(COMMENT, " inst. not implemented" NL, NULL));
+      result = strdup(concatenate(COMMENT, " Instruction not implemented" NL, NULL));
       break;
     }
 
@@ -648,8 +793,15 @@ static string c_code_string(entity module, statement stat)
 {
   string before_head, head, decls, body, result;
 
+  /* What about declarations that are external a module scope ?
+     Consider a source file as a module entity, put all declarations in it 
+     (external static + TOP-LEVEL) */
+
+  /* before_head only generates the constant declarations, such as #define*/
+
   before_head = c_declarations(module, parameter_p, NL, TRUE);
   head        = c_head(module);
+  /* What about declarations associated to statements */
   decls       = c_declarations(module, variable_p, SEMICOLON, TRUE);
   body        = c_statement(stat);
   
@@ -671,7 +823,7 @@ static string c_code_string(entity module, statement stat)
 #define CROUGH		".crough"
 #define CPRETTY		".c"
 
-bool cprinted_file(string module_name)
+bool print_c_rough(string module_name)
 {
   FILE * out;
   string ppt, crough, dir, filename;
@@ -687,10 +839,13 @@ bool cprinted_file(string module_name)
   set_current_module_entity(module);
   set_current_module_statement(stat);
 
+  debug_on("CPRETTYPRINTER_DEBUG_LEVEL");
+  pips_debug(1, "Begin C prettyprrinter for %s\n", entity_name(module));
   ppt = c_code_string(module, stat);
+  pips_debug(1, "end\n");
+  debug_off();  
 
-  /* save to file.
-   */
+  /* save to file */
   out = safe_fopen(filename, "w");
   fprintf(out, "/* C pretty print for module %s. */\n%s", module_name, ppt);
   safe_fclose(out, filename);
@@ -709,12 +864,12 @@ bool cprinted_file(string module_name)
 
 /* C indentation thru indent.
  */
-bool cindent_file(string module_name)
+bool print_c_code(string module_name)
 {
   string crough, cpretty, dir, cmd;
 
   crough = db_get_memory_resource(DBR_CROUGH, module_name, TRUE);
-  cpretty = db_build_file_resource_name(DBR_C, module_name, CPRETTY);
+  cpretty = db_build_file_resource_name(DBR_C_PRINTED_FILE, module_name, CPRETTY);
   dir = db_get_current_workspace_directory();
 
   cmd = strdup(concatenate(INDENT, " ", 
@@ -723,7 +878,7 @@ bool cindent_file(string module_name)
 
   safe_system(cmd);
 
-  DB_PUT_FILE_RESOURCE(DBR_C, module_name, cpretty);
+  DB_PUT_FILE_RESOURCE(DBR_C_PRINTED_FILE, module_name, cpretty);
   free(cmd);
   free(dir);
 
