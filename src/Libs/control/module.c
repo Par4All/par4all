ac70d36c10dec32874b2a5c38ff610d96c9074e3
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-07-21 12:21:18 +0000

    # on 2003/07/21 12:21:18, nguyen did: Add new control structures for C language, preliminary version
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11736 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/control/module.c
+++ src/Libs/control/module.c
@@ -24,38 +24,38 @@ bool controlizer(string module_name)
 {
   entity m = local_name_to_top_level_entity(module_name);
    
-    statement module_stat;
+  statement module_stat, parsed_mod_stat;
     
-    set_current_module_entity(m);
+  set_current_module_entity(m);
 
-    module_stat = 
-	copy_statement((statement)
-	    db_get_memory_resource(DBR_PARSED_CODE, module_name, TRUE)) ;
+  parsed_mod_stat = (statement) db_get_memory_resource(DBR_PARSED_CODE, module_name, TRUE);
+  module_stat =  copy_statement(parsed_mod_stat) ;
     
-       /* *module_stat can be re-used because control_graph reallocates
-	  statements; do not show that to any student!
-	  statement_instruction(module_stat) =
-	  make_instruction(is_instruction_unstructured,
-	        control_graph(module_stat));
-    Maintenant que je nettoie le code aussi avant le controlizer,
-       un programme sans instruction ne contient qu'un statement
-       RETURN, c'est a` dire un statement de CALL vers RETURN avec le
-       label 00000.  Comme la ligne ci-dessus recycle le label et le
-       commentaire, on se retrouve avec un unstructured avec le label
-       et le commentaire du RETURN qu'il contient... En plus il y avait
-       une re'cursion de l'unstructured vers module_stat. :-(
+  /* *module_stat can be re-used because control_graph reallocates
+     statements; do not show that to any student!
+     statement_instruction(module_stat) =
+     make_instruction(is_instruction_unstructured,
+     control_graph(module_stat));
+     Maintenant que je nettoie le code aussi avant le controlizer,
+     un programme sans instruction ne contient qu'un statement
+     RETURN, c'est a` dire un statement de CALL vers RETURN avec le
+     label 00000.  Comme la ligne ci-dessus recycle le label et le
+     commentaire, on se retrouve avec un unstructured avec le label
+     et le commentaire du RETURN qu'il contient... En plus il y avait
+     une re'cursion de l'unstructured vers module_stat. :-(
+     
+     So now correct the label and the comment: */
 
-       So now correct the label and the comment: */
-
-    module_stat = make_statement(entity_empty_label(), 
-				 STATEMENT_NUMBER_UNDEFINED,
-				 MAKE_ORDERING(0,1),
-				 empty_comments,
-				 make_instruction(is_instruction_unstructured,
-						  control_graph(module_stat)),NIL,NULL);
-    ifdebug(5) {
-	statement_consistent_p(module_stat);
-    }
+  module_stat = make_statement(entity_empty_label(), 
+			       STATEMENT_NUMBER_UNDEFINED,
+			       MAKE_ORDERING(0,1),
+			       empty_comments,
+			       make_instruction(is_instruction_unstructured,
+						control_graph(module_stat)),
+			       statement_declarations(parsed_mod_stat),NULL);
+  ifdebug(5) {
+    statement_consistent_p(module_stat);
+  }
 
     /* By setting this property, we try to unspaghettify the control
        graph of the module: */
