76a0e3e18500e5618fc656e88119d491b57489d0
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-08-04 14:21:10 +0000

    # on 2003/08/04 14:21:10, nguyen did: Preliminary version of the C parser
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11834 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/c_syntax/c_syntax-local.h
+++ src/Libs/c_syntax/c_syntax-local.h
@@ -1,8 +1,16 @@
 /* $Id$
    $Log: c_syntax-local.h,v $
+   Revision 1.2  2003/08/04 14:21:10  nguyen
+   Preliminary version of the C parser
+
    Revision 1.1  2003/06/24 07:25:13  nguyen
    Initial revision
 
 */
 
 extern FILE * c_in; /* the file read in by the c_lexer */
+
+/* The following declarations are used to avoid warning with implicit declarations,
+   although include <stdio.h>, include <string.h> are already added, I do not know why :-)*/
+extern int fileno(FILE *stream);
+extern char *strdup(const char *s1); 
