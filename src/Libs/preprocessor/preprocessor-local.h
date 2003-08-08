b04680373911bb7fa3d887aa83f9abba3b6630b4
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-08-08 15:55:38 +0000

    # on 2003/08/08 15:55:38, irigoin did: Initial revision
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11858 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/preprocessor/preprocessor-local.h
@@ -0,0 +1,10 @@
+/* Preprocessing and splitting of Fortran and C files
+ *
+ * $Id$
+ *
+ * $Log: preprocessor-local.h,v $
+ * Revision 1.1  2003/08/08 15:55:38  irigoin
+ * Initial revision
+ *
+ *
+ */
