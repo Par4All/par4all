cd42294cf96056a4b415e1333e2f6b4b71688b34
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-09-03 14:19:12 +0000

    # on 2003/09/03 14:19:12, coelho did: ++
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11924 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/misc/exception.c
+++ src/Libs/misc/exception.c
@@ -1,89 +1,33 @@
 /* Stack management for PIPS contexts
  *
- * A PIPS context contains a pointer to a long jump buffer and a pointer in the
- * PIPS debug level stack. The long jump buffers are initialized by the
- * caller since set_jmp() must be performed by the procedure catching
- * the exception. This package only stores pointers towards these buffers.
- *
+ * A PIPS context contains a pointer in the PIPS debug level stack.
  * When a PIPS context is popped, the debug level stack is restored.
- *
- * The PIPS context stack pointer, pcsp, points to the top of the stack.
- * The empty stack is represented by pcsp == -1.
+ * It may be the current level!
  *
  * Example: voir catch_user_error() in library pipmake, or a PIPS user
  * interface such as wpips, tpips or pips
  */
 
-#if defined(I_REALLY_WANT_THIS_OLD_EXCEPTION_STUFF)
-
 #include <stdio.h>
-#include <setjmp.h>
-	
-#include "genC.h"
+#include <genC.h>
 #include "misc.h"
 
-typedef struct pips_context {
-    jmp_buf * long_jump_buffer;
-    int debug_stack_pointer;
-} pips_context;
+static stack debug_stack = NULL;
 
-#define PIPS_CONTEXT_STACK_DEPTH 4
-static pips_context pips_context_stack[PIPS_CONTEXT_STACK_DEPTH];
-static int pcsp = -1;
-
-jmp_buf * top_pips_context_stack()
+void push_pips_context(char * file, char * function, int line)
 {
-    message_assert ("top_pips_context_stack",pcsp>=0)
-
-    return pips_context_stack[pcsp].long_jump_buffer;
+  pips_debug(9, "%s %s:%d\n", function, file, line);
+  if (!debug_stack) debug_stack = stack_make(0, 50, 0);
+  stack_push((void*) get_debug_stack_pointer(), debug_stack);
 }
 
-
-void set_pips_context_stack()
+void pop_pips_context(char * file, char * function, int line)
 {
-    /* pcsp == -1 when this function is called */
-    if (pcsp!=-1) {
-	pips_error("set_pips_context_stack",
-		   "Unexpected pointer value %d\n", pcsp);
-    }
+  pips_debug(9, "%s %s:%d\n", function, file, line);
+  if (!debug_stack) 
+    pips_internal_error("unexpected pop without push %s %s:%d\n",
+			function, file, line);
+  set_debug_stack_pointer((int) stack_pop(debug_stack));
 }
 
-void pop_pips_context()
-{
-    if(pcsp>=0) {
-	set_debug_stack_pointer(pips_context_stack[pcsp].debug_stack_pointer);
-	pcsp--;
-    }
-    else {
-	/* Once PIPS is started, the stack should never be empty again...
-	 but tpips is easier to program with an empty stack... */
-	pips_error ("pop_pips_context",
-		    "No more handler for user_error()!\n");
-    }
-}
-
-void push_pips_context(jmp_buf * pljb)
-{
-    int dsp = get_debug_stack_pointer();
-
-    pips_assert("push_pips_context", pcsp < PIPS_CONTEXT_STACK_DEPTH-1);
-
-    pcsp++;
-    pips_context_stack[pcsp].long_jump_buffer = pljb;
-    pips_context_stack[pcsp].debug_stack_pointer = dsp;
-}
-
-void print_pips_context_stack()
-{
-    int i;
-
-    (void) printf("Debug stack (last debug_on first): ");
-    for(i=pcsp;i>=0;i--) {
-	(void) printf("Debug stack pointer %d: debug level %d, long jump %p\n ", i,
-		      pips_context_stack[i].debug_stack_pointer,
-		      pips_context_stack[i].long_jump_buffer);
-    }
-}
-
-#endif /* I_REALLY_WANT_THIS_OLD_EXCEPTION_STUFF */
 /* That's all */
