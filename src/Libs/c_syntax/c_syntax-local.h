a95cb9df374e96471a8967e984789a1a0855e770
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-09-05 14:20:44 +0000

    # on 2003/09/05 14:20:44, nguyen did: Add c_parser_private Newgen structures
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11945 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/c_syntax/c_syntax-local.h
+++ src/Libs/c_syntax/c_syntax-local.h
@@ -1,5 +1,8 @@
 /* $Id$
    $Log: c_syntax-local.h,v $
+   Revision 1.4  2003/09/05 14:20:44  nguyen
+   Add c_parser_private Newgen structures
+
    Revision 1.3  2003/08/04 14:27:52  nguyen
    Remove extern strdup, ... declarations to avoid conflicts
 
@@ -10,6 +13,7 @@
    Initial revision
 
 */
-
+  
+#include "c_parser_private.h"
 extern FILE * c_in; /* the file read in by the c_lexer */
 
