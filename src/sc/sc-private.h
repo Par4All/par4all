4619b94e1b767a5efd249325fc7ac33dae62d5ef
duong <duong@8716bff9-3cf5-0310-8878-b27a67f92df8>
2003-09-08 09:58:30 +0000

    # on 2003/09/08 09:58:30, duong did: *** empty log message ***
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@1150 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/sc/sc-private.h
+++ src/sc/sc-private.h
@@ -1,26 +1,2 @@
 extern int sc_debug_level;
 
-extern int filtering_timeout_J;
-extern int filtering_timeout_S;
-extern int filtering_timeout_FM;
-extern int filtering_timeout_CH;
-extern int filtering_timeout_projection;
-
-extern int filtering_dimension_feasibility;
-extern int filtering_number_constraints_feasibility;
-extern int filtering_density_feasibility;
-extern long int filtering_magnitude_feasibility;
-
-extern int filtering_dimension_projection;
-extern int filtering_number_constraints_projection;
-extern int filtering_density_projection;
-extern long int filtering_magnitude_projection;
-
-extern int filtering_dimension_convex_hull;
-extern int filtering_number_constraints_convex_hull;
-extern int filtering_density_convex_hull;
-extern long int filtering_magnitude_convex_hull;
-
-extern boolean CONVEX_HULL_timeout;
-
-
