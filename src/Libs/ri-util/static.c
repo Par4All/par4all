20e988bdfbb8cdb037abf00ee1d169d4a0df75f4
nguyen <nguyen@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-12-05 17:12:00 +0000

    # on 2003/12/05 17:12:00, nguyen did: Replace module_local_name by entity_user_name to handle entities in C
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11975 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/static.c
+++ src/Libs/ri-util/static.c
@@ -5,6 +5,9 @@
  * $Id$
  *
  * $Log: static.c,v $
+ * Revision 1.11  2003/12/05 17:12:00  nguyen
+ * Replace module_local_name by entity_user_name to handle entities in C
+ *
  * Revision 1.10  2002/07/22 17:17:24  irigoin
  * A two-entry current_module_statement stack... Not glorious but enough to
  * solve problems linked to alternate returns.
@@ -73,7 +76,8 @@ error_reset_current_module_entity()
 string
 get_current_module_name()
 {
-    return module_local_name(current_module_entity);
+  /*  return module_local_name(current_module_entity);*/
+  return entity_user_name(current_module_entity);
 }
 
 
