fada35e3c4fa2add2b36cf2334208aca57f442aa
guerin <guerin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-12-17 15:46:41 +0000

    # on 2003/12/17 15:46:41, guerin did: Adding fundations for PHRASE project
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11991 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/phrase/fsm_generation.c
@@ -0,0 +1,14 @@
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+
+#include "resources.h"
+
+#include "misc.h"
+#include "ri-util.h"
+#include "pipsdbm.h"
+
+bool fsm_generation(char * mod_name)
+{
+  return TRUE;
+}
