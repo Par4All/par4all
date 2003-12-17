f1d325c406c16f08723395b051550282470b7e3f
guerin <guerin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-12-17 15:46:47 +0000

    # on 2003/12/17 15:46:47, guerin did: Adding fundations for PHRASE project
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11992 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/phrase/print_code_smalltalk.c
@@ -0,0 +1,14 @@
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+
+#include "resources.h"
+
+#include "misc.h"
+#include "ri-util.h"
+#include "pipsdbm.h"
+
+bool print_code_smalltalk(char * mod_name)
+{
+  return TRUE;
+}
