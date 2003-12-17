d4fcf5c01fcc890a91dd1bd4b96b51a02aa985d2
guerin <guerin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2003-12-17 15:46:33 +0000

    # on 2003/12/17 15:46:33, guerin did: Adding fundations for PHRASE project
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@11990 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/phrase/phrase_distributor.c
@@ -0,0 +1,14 @@
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+
+#include "resources.h"
+
+#include "misc.h"
+#include "ri-util.h"
+#include "pipsdbm.h"
+
+bool phrase_distributor(char * mod_name)
+{
+  return TRUE;
+}
