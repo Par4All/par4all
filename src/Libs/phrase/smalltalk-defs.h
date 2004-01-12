2e3329b2a0693b36f5db572b3854bb2feafd5c62
guerin <guerin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2004-01-12 14:07:36 +0000

    # on 2004/01/12 14:07:36, guerin did: Adding new phases for PHRASE project
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12027 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/phrase/smalltalk-defs.h
@@ -0,0 +1,50 @@
+#ifndef SMALLTALK_DEFS
+#define SMALLTALK_DEFS
+
+#define EMPTY         ""
+#define NL            "\n"
+#define STSEMICOLON   "." NL
+#define SPACE         " "
+
+#define OPENBRACKET   "["
+#define CLOSEBRACKET  "]"
+
+#define OPENPAREN     "("
+#define CLOSEPAREN    ")"
+
+#define OPENBRACE     "{"
+#define CLOSEBRACE    "}"
+
+#define BEGINTEMPVAR  "|"
+#define ENDTEMPVAR    "|"
+
+#define SETVALUE      "_"
+#define RETURNVALUE   "^"
+
+#define COMMENT       "\""
+
+#define ST_LE         "<="
+#define ST_PLUS       "+"
+#define ST_MINUS      "-"
+
+#define ST_WHILETRUE  "whileTrue:"
+
+#define ST_IFTRUE     "ifTrue:"
+#define ST_IFFALSE    "ifFalse:"
+
+#define ARRAY             "Array"
+#define ARRAY_NEW         "new:"
+#define ARRAY_AT          "at:"
+#define ARRAY_AT_PUT_1    "at:"
+#define ARRAY_AT_PUT_2    "put:"
+
+#define ARRAY2D               "Array2D"
+#define ARRAY2D_NEW1          "width:"
+#define ARRAY2D_NEW2          "height:"
+#define ARRAY2D_AT_AT_1       "at:"
+#define ARRAY2D_AT_AT_2       "at:"
+#define ARRAY2D_AT_AT_PUT_1   "at:"
+#define ARRAY2D_AT_AT_PUT_2   "at:"
+#define ARRAY2D_AT_AT_PUT_3   "put:"
+
+#endif
