6af2a71f640f1071c98a8a435490e2a274040a87
guerin <guerin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2004-01-12 14:08:03 +0000

    # on 2004/01/12 14:08:03, guerin did: Adding new phases for PHRASE project
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12032 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/phrase/fsm_split_state.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+
+#include "resources.h"
+
+#include "misc.h"
+#include "ri-util.h"
+#include "pipsdbm.h"
+
+#include "text-util.h"
+
+#include "transformations.h"
+
+#include "control.h"
+
+#include "fsm_generation.h"
+
+/*********************************************************
+ * Phase main
+ *********************************************************/
+
+bool fsm_split_state(string module_name)
+{
+  entity module;
+  entity state_variable;
+
+   /* get the resources */
+  statement stat = (statement) db_get_memory_resource(DBR_CODE, 
+						      module_name, 
+						      TRUE);
+
+  module = local_name_to_top_level_entity(module_name);
+  
+  set_current_module_statement(stat);
+  set_current_module_entity(local_name_to_top_level_entity(module_name));
+  
+  debug_on("FSM_SPLIT_STATE_DEBUG_LEVEL");
+  /* Now do the job */
+  
+  pips_assert("Statement is consistent after FSM_SPLIT_STATE", 
+	       statement_consistent_p(stat));
+  
+  /* Reorder the module, because new statements have been added */  
+  module_reorder(stat);
+  DB_PUT_MEMORY_RESOURCE(DBR_CODE, module_name, stat);
+  DB_PUT_MEMORY_RESOURCE(DBR_CALLEES, module_name, 
+			 compute_callees(stat));
+  
+  /* update/release resources */
+  reset_current_module_statement();
+  reset_current_module_entity();
+  
+  debug_off();
+  
+  return TRUE;
+}
