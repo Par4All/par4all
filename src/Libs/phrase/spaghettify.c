0b9aa6caa8989b06d8a90861e7b3aec2114ae420
guerin <guerin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2004-01-15 13:52:03 +0000

    # on 2004/01/15 13:52:03, guerin did: New phase FULL_SPAGHETTIFY
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12049 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/phrase/spaghettify.c
+++ src/Libs/phrase/spaghettify.c
@@ -1,3 +1,25 @@
+/**
+ * The spaghettifier is used in context of PHRASE project while
+ * creating "Finite State Machine"-like code portions in order to synthetise
+ * them in reconfigurables units.
+ *
+ * This phase transforms structured code portions (eg. loops) in
+ * unstructured statements.
+ * 
+ * To add flexibility, the behavior of \texttt{spaghettifier} is
+ * controlled by the properties 
+ * - DESTRUCTURE_TESTS
+ * - DESTRUCTURE_LOOPS
+ * - DESTRUCTURE_WHILELOOPS
+ * - DESTRUCTURE_FORLOOPS
+ * to allow more or less destruction power !
+ *
+ * spaghettify          > MODULE.code
+ *       < PROGRAM.entities
+ *       < MODULE.code
+ *
+ */
+
 #include <stdio.h>
 #include <ctype.h>
 
@@ -15,8 +37,121 @@
 
 #include "dg.h"
 #include "transformations.h"
+#include "properties.h"
 
 #include "control.h"
+#include "spaghettify.h"
+#include "phrase_tools.h"
+
+/* 
+ * This function is recursively called during spaghettization.
+ * It takes the statement stat as parameter and return a new 
+ * spaghettized statement (or the same if nothing has been done).
+ * Spaguettization is done:
+ * - on Tests (if property DESTRUCTURE_TESTS set to true)
+ * - on Loops (if property DESTRUCTURE_LOOPS set to true)
+ * - on WhileLoops (if property DESTRUCTURE_WHILELOOPS set to true)
+ * - on ForLoops (if property DESTRUCTURE_FORLOOPS set to true)
+ */
+statement spaghettify_statement (statement stat, string module_name)
+{
+  // Defaut behaviour is to return parameter statement stat
+  statement returned_statement = stat;
+  instruction i = statement_instruction(stat);
+
+  pips_debug(2,"\nSPAGHETTIFY: Module statement: =====================================\n");
+  ifdebug(2) {
+    print_statement(stat);
+  }
+  pips_debug(2,"domain number = %d\n", statement_domain_number(stat));
+  pips_debug(2,"entity = UNDEFINED\n");
+  pips_debug(2,"statement number = %d\n", statement_number(stat));
+  pips_debug(2,"statement ordering = %d\n", statement_ordering(stat));
+  if (statement_with_empty_comment_p(stat)) {
+    pips_debug(2,"statement comments = EMPTY\n");
+  }
+  else {
+    pips_debug(2,"statement comments = %s\n", statement_comments(stat));
+  }
+  pips_debug(2,"statement instruction = %s\n", statement_type_as_string(stat));
+  switch (instruction_tag(i)) {
+  case is_instruction_test: 
+    {
+      pips_debug(2, "TEST\n");   
+    if (get_bool_property("DESTRUCTURE_TESTS")) {
+      returned_statement = spaghettify_test (stat, module_name);
+    }
+      break;
+  }
+  case is_instruction_sequence: 
+    {
+      sequence seq = instruction_sequence(i);
+      pips_debug(2, "SEQUENCE\n");   
+      MAP(STATEMENT, current_stat,
+      {
+	statement new_stat = spaghettify_statement(current_stat, module_name);
+	if (new_stat != NULL) {
+	  gen_list_patch (sequence_statements(seq), current_stat, new_stat);
+	}
+      }, sequence_statements(seq));
+      break;
+    }
+  case is_instruction_loop: {
+    pips_debug(2, "LOOP\n");   
+    if (get_bool_property("DESTRUCTURE_LOOPS")) {
+      returned_statement = spaghettify_loop (stat, module_name);
+    }
+    break;
+  }
+  case is_instruction_whileloop: {
+    pips_debug(2, "WHILELOOP\n");   
+    if (get_bool_property("DESTRUCTURE_WHILELOOPS")) {
+      returned_statement = spaghettify_whileloop (stat, module_name);
+    }
+    break;
+  }
+  case is_instruction_forloop: {
+    pips_debug(2, "FORLOOP\n");   
+    if (get_bool_property("DESTRUCTURE_FORLOOPS")) {
+      returned_statement = spaghettify_forloop (stat, module_name);
+    }
+    break;
+  }
+  case is_instruction_call: {
+    pips_debug(2, "CALL\n");   
+    break;
+  }
+  case is_instruction_unstructured: {
+    pips_debug(2, "UNSTRUCTURED\n");  
+    break;
+  }
+  case is_instruction_goto: {
+    pips_debug(2, "GOTO\n");   
+    break;
+  }
+  default:
+    pips_debug(2, "UNDEFINED\n");   
+    break;
+  }
+
+  pips_debug(2,"\nSPAGHETTIFY: RETURN statement: =====================================\n");
+  ifdebug(2) {
+    print_statement(returned_statement);
+  }
+  pips_debug(2,"domain number = %d\n", statement_domain_number(returned_statement));
+  pips_debug(2,"entity = UNDEFINED\n");
+  pips_debug(2,"statement number = %d\n", statement_number(returned_statement));
+  pips_debug(2,"statement ordering = %d\n", statement_ordering(returned_statement));
+  if (statement_with_empty_comment_p(returned_statement)) {
+    pips_debug(2,"statement comments = EMPTY\n");
+  }
+  else {
+    pips_debug(2,"statement comments = %s\n", statement_comments(returned_statement));
+  }
+  pips_debug(2,"statement instruction = %s\n", statement_type_as_string(returned_statement));
+
+  return returned_statement;
+}
 
 /*********************************************************
  * Phase main
@@ -25,7 +160,6 @@
 bool spaghettify(string module_name)
 {
   entity module;
-  entity state_variable;
 
    /* get the resources */
   statement stat = (statement) db_get_memory_resource(DBR_CODE, 
@@ -38,8 +172,10 @@ bool spaghettify(string module_name)
   set_current_module_entity(local_name_to_top_level_entity(module_name));
   
   debug_on("SPAGUETTIFY_DEBUG_LEVEL");
-  /* Now do the job */
-  
+
+  /* Now do the job */  
+  stat = spaghettify_statement(stat,module_name);
+
   pips_assert("Statement is consistent after SPAGUETTIFY", 
 	       statement_consistent_p(stat));
   
