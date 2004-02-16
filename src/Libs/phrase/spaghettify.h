31c65b7fffc303fcdc97724fefb18ce9e99711fd
guerin <guerin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2004-02-16 15:39:32 +0000

    # on 2004/02/16 15:39:32, guerin did: First (stable) version of FSMizer
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12059 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/phrase/spaghettify.h
+++ src/Libs/phrase/spaghettify.h
@@ -1,10 +1,10 @@
 #ifndef SPAGHETTIFY_DEFS
 #define SPAGHETTIFY_DEFS
 
-#define INDEX_VARIABLE_NAME "INDEX"
-#define BEGIN_VARIABLE_NAME "BEGIN"
-#define END_VARIABLE_NAME "END"
-#define INCREMENT_VARIABLE_NAME "INCREMENT"
+#define INDEX_VARIABLE_NAME "DO%d_INDEX"
+#define BEGIN_VARIABLE_NAME "DO%d_BEGIN"
+#define END_VARIABLE_NAME "DO%d_END"
+#define INCREMENT_VARIABLE_NAME "DO%d_INCREMENT"
 
 statement spaghettify_loop (statement stat, string module_name);
 
@@ -13,5 +13,7 @@ statement spaghettify_whileloop (statement stat, string module_name);
 statement spaghettify_forloop (statement stat, string module_name);
 
 statement spaghettify_test (statement stat, string module_name);
+
+statement spaghettify_statement (statement stat, string module_name);
 
 #endif
