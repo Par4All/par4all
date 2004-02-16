d67f6d8dad14c850f85021f55333e42e9a8dfa03
guerin <guerin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2004-02-16 15:39:34 +0000

    # on 2004/02/16 15:39:34, guerin did: First (stable) version of FSMizer
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12069 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/phrase/phrase_tools.c
+++ src/Libs/phrase/phrase_tools.c
@@ -80,6 +80,156 @@ string statement_type_as_string (statement stat)
 }
 
 /**
+ * DEBUG FUNCTION: print debugging informations for
+ * a statement stat
+ */
+void debug_statement (string comments, statement stat, int debug_level)
+{
+  pips_debug(debug_level,"%s\n",comments);
+  ifdebug(debug_level) {
+    print_statement(stat);
+  }
+  pips_debug(debug_level,"domain number         = %d\n", statement_domain_number(stat));
+  pips_debug(debug_level,"entity                = UNDEFINED\n");
+  pips_debug(debug_level,"statement number      = %d\n", statement_number(stat));
+  pips_debug(debug_level,"statement ordering    = %d\n", statement_ordering(stat));
+  if (statement_with_empty_comment_p(stat)) {
+    pips_debug(debug_level,"statement comments   = EMPTY\n");
+  }
+  else {
+    pips_debug(debug_level,"statement comments  = %s\n", statement_comments(stat));
+  }
+  pips_debug(debug_level,"statement instruction = %s\n", statement_type_as_string(stat));
+
+}
+
+/**
+ * DEBUG FUNCTION: print debugging informations for
+ * a control a_control
+ */
+void debug_control (string comments, control a_control, int debug_level) {
+
+  debug_statement (comments, control_statement(a_control), debug_level);
+  pips_debug(debug_level,"  predecessors          = %d\n", gen_length(control_predecessors(a_control)));
+  pips_debug(debug_level,"  successors            = %d\n", gen_length(control_successors(a_control)));
+
+}
+
+/**
+ * DEBUG FUNCTION: print debugging informations for
+ * an unstructured an_unstructured
+ */
+void debug_unstructured (unstructured an_unstructured, 
+			 int debug_level) 
+{
+  list blocs = NIL ;
+  string line = "***********************************************************************\n";
+
+  ifdebug (debug_level) {
+    CONTROL_MAP (current_control, { 
+      statement s = control_statement (current_control);
+      string next_nodes_as_string = "";
+      string previous_nodes_as_string = "";
+      char title[80];
+      list predecessors = control_predecessors(current_control);
+      list successors = control_successors(current_control);
+      char temp[50];
+      int i;
+      int ordering;
+
+      for (i=0; i<gen_length(predecessors); i++) {
+	ordering = statement_ordering(control_statement
+				      (CONTROL(gen_nth(i,predecessors))));
+	ordering = beautify_ordering (ordering);
+	/*if (ordering > 65535) ordering = ordering >> 16;*/
+	sprintf (temp, "[%p] ",ordering);
+	previous_nodes_as_string = strdup (concatenate(previous_nodes_as_string,
+						       strdup(temp),
+						       NULL));
+      }
+      
+      for (i=0; i<gen_length(successors); i++) {
+	ordering = statement_ordering(control_statement
+				      (CONTROL(gen_nth(i,successors))));
+	ordering = beautify_ordering (ordering);
+	/*if (ordering > 65535) ordering = ordering >> 16;*/
+	sprintf (temp, "[%p] ",ordering);
+	next_nodes_as_string = strdup (concatenate(next_nodes_as_string,
+						   strdup(temp),
+						   NULL));
+      }
+      
+      ordering = beautify_ordering (ordering);
+      ordering = statement_ordering(s);
+      /*if (ordering > 65535) ordering = ordering >> 16;*/
+      sprintf (title, "CONTROL: %p\n", ordering);
+      pips_debug(debug_level, "%s\n",
+		 strdup(concatenate("\n", line,
+				    "* ", strdup(title),
+				    line, NULL)));
+      print_statement(s);
+      pips_debug(debug_level, "%s\n",
+		 strdup(concatenate("\n", line,
+				    "NEXT: ", next_nodes_as_string, "\n",
+				    "PREVIOUS: ", previous_nodes_as_string, "\n",
+				    line, NULL)));
+		 
+    }, unstructured_entry(an_unstructured), blocs);
+  }
+}
+
+/**
+ * DEBUG FUNCTION: print debugging informations for
+ * an unstructured an_unstructured (short version)
+ */
+void short_debug_unstructured (unstructured an_unstructured, 
+			       int debug_level) 
+{
+  list blocs = NIL ;
+  string entry_as_string, exit_as_string;
+  char temp[50];
+
+  ifdebug (debug_level) {
+    sprintf (temp, "[%p] ",unstructured_entry(an_unstructured));
+    entry_as_string = strdup(temp);
+    sprintf (temp, "[%p] ",unstructured_exit(an_unstructured));
+    exit_as_string = strdup(temp);
+    pips_debug(debug_level, "%s\n",
+	       strdup(concatenate("UNSTRUCTURED\n",
+				  "ENTRY: ", entry_as_string, "\n",
+				  "PREVIOUS: ", exit_as_string, "\n",
+				  NULL)));
+
+    CONTROL_MAP (current_control, { 
+      string next_nodes_as_string = "";
+      string previous_nodes_as_string = "";
+      char title[80];
+
+      MAP(CONTROL, c, {
+	sprintf (temp, "[%p] ",c);
+	previous_nodes_as_string = strdup (concatenate(previous_nodes_as_string,
+						       strdup(temp),
+						       NULL));
+      }, control_predecessors(current_control));
+
+      MAP(CONTROL, c, {
+	sprintf (temp, "[%p] ",c);
+	next_nodes_as_string = strdup (concatenate(next_nodes_as_string,
+						   strdup(temp),
+						   NULL));
+      }, control_successors(current_control));
+
+      sprintf (title, "CONTROL: %p\n", current_control);
+      pips_debug(debug_level, "%s\n",
+		 strdup(concatenate(title,
+				    "NEXT: ", next_nodes_as_string, "\n",
+				    "PREVIOUS: ", previous_nodes_as_string, "\n",
+				    NULL)));
+    }, unstructured_entry(an_unstructured), blocs);
+  }
+}
+
+/**
  * This function build and return an expression given
  * an entity an_entity
  */
@@ -113,11 +263,26 @@ entity clone_variable_with_new_name(entity a_variable,
 			  entity_domain)) == entity_undefined) 
     { 
       /* This entity does not exist, we can safely create it */
-      new_variable = copy_entity (a_variable);
-      entity_name(new_variable) 
-	= strdup(concatenate(module_name, 
-			     MODULE_SEP_STRING, 
-			     new_name, NULL));
+      
+      /* new_variable = copy_entity (a_variable);
+	 entity_name(new_variable)  
+	 = strdup(concatenate(module_name, 
+	 MODULE_SEP_STRING, 
+	 new_name, NULL)); */
+      
+      new_variable = make_entity (strdup(concatenate(module_name, 
+						     MODULE_SEP_STRING, 
+						     new_name, NULL)),
+				  copy_type (entity_type(a_variable)),
+				  copy_storage (entity_storage(a_variable)),
+				  copy_value (entity_initial(a_variable)));
+      
+      /*new_variable 
+	= find_or_create_scalar_entity (strdup(concatenate(module_name, 
+	MODULE_SEP_STRING, 
+	new_name, NULL)),module_name,
+	is_basic_int);*/
+      
       add_variable_declaration_to_module(module, new_variable);
       return new_variable;
     }
@@ -148,10 +313,9 @@ entity make_variable_from_name_and_entity (entity cloned_variable,
   
   while (returned_variable == NULL) {
     
-    sprintf(buffer, "%d", index++);
-    variable_name = strdup(concatenate(base_name,
-				    strdup(buffer),
-				    NULL));
+    sprintf(buffer, base_name, index++);
+    variable_name = strdup(concatenate(strdup(buffer),
+				       NULL));
     returned_variable 
       = clone_variable_with_new_name (cloned_variable,
 				      variable_name,
@@ -201,4 +365,27 @@ statement make_assignement_statement (entity a_variable,
 				     make_expression_from_entity(a_variable),
 				     an_expression,
 				     stat);
+}
+
+/** 
+ * Return unstructured for a statement asserting that this one
+ * represent an unstructured
+ */
+unstructured statement_unstructured (statement stat) 
+{
+  pips_assert("Statement is UNSTRUCTURED", 
+	      instruction_tag(statement_instruction(stat)) 
+	      == is_instruction_unstructured);
+  return instruction_unstructured(statement_instruction(stat));
+}
+
+/**
+ * Special function made for Ronan Keryell who likes a lot
+ * when a integer number is coded on 3 bits :-)
+ */
+int beautify_ordering (int an_ordering) 
+{
+  int ordering_up = (an_ordering & 0xffff0000) >> 16;
+  int ordering_down = (an_ordering & 0x0000ffff);
+  return ordering_up + ((ordering_down-1) << 16);
 }
