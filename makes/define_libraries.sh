9aa9a4f119702057849f317d6f2adcc91cb4d0b7
coelho <coelho@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2005-03-14 09:57:22 +0000

    # on 1997/03/27 13:39:00, coelho did: fiew more comments.
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake@532 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/define_libraries.sh
+++ makes/define_libraries.sh
@@ -1,18 +1,24 @@
-# $RCSfile: define_libraries.sh,v $ (version $Revision$)
-# $Date: 1996/12/31 11:45:20 $, 
+#
+# $Id$
+#
+# $Log: define_libraries.sh,v $
+# Revision 1.10  1997/03/27 13:39:00  coelho
+# fiew more comments.
+#
 #
 # this file define newgen, linear and pips libraries.
 # it is to be included by some shell-scripts and makefiles (after a sed).
 
 #
-# Newgen
+# NEWGEN
 
 NEWGEN_ORDERED_LIBS='doc scripts genC'
 
 NEWGEN_LIBS='-lgenC'
 
 #
-# C3/linear
+# C3/LINEAR
+
 LINEAR_ORDERED_LIBS='arithmetique vecteur contrainte sc matrice matrix ray_dte sommet sg polynome polyedre plint sparse_sc union'
 
 # removed because not used by pips: plint
@@ -20,7 +26,7 @@ LINEAR_ORDERED_LIBS='arithmetique vecteur contrainte sc matrice matrix ray_dte s
 LINEAR_LIBS='-lmatrice -lunion -lpolyedre -lsparse_sc -lsc -lcontrainte -lsg -lsommet -lray_dte -lpolynome -lmatrix -lvecteur -larithmetique'
 
 #
-# Pips
+# PIPS
 
 # static ordering for bootstraping PIPS...
 PIPS_ORDERED_LIBS='xpomp text-util properties misc ri-util conversion movements pipsdbm comp_sections transformer bootstrap control hyperplane flint syntax prettyprint static_controlize effects semantics complexity continuation reductions regions callgraph icfg paf-util pip ricedg array_dfg prgm_mapping scheduling reindexing chains rice transformations hpfc atomizer wp65 pipsmake top-level pips tpips wpips'
@@ -36,16 +42,18 @@ PIPS_X11_ADDED_LDFLAGS='-L$(X11_ROOT)/lib'
 PIPS_X11_ADDED_LIBS='-lX11'
 
 #
-# wpips
+# WPIPS
 
 WPIPS_ADDED_CPPFLAGS='-I$(OPENWINHOME)/include -Iicons'
 WPIPS_ADDED_LIBS='-lxview -lolgx -lX11'
 WPIPS_ADDED_LDFLAGS='-L$(OPENWINHOME)/lib'
 
 #
-# tpips
+# TPIPS
+
+# is this so portable?
 
 TPIPS_ADDED_LIBS='-lreadline -ltermcap'
 
-# end of $RCSfile: define_libraries.sh,v $
+# end of it!
 #
