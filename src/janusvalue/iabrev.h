f24a9ee79f29f662dde81dbf962cfb6d09d2d175
duong <duong@8716bff9-3cf5-0310-8878-b27a67f92df8>
2005-03-24 11:36:39 +0000

    # on 2005/03/24 11:36:39, duong did: First version of janusvalue, adaptation for Janus
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@1157 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/janusvalue/iabrev.h
+++ src/janusvalue/iabrev.h
@@ -1,3 +1,17 @@
+/* ========================================================================= */
+/*                       SIMPLEXE for integer variables                      */
+/*                            ALL-INTEGER METHODS                            */
+/*                             Jean Claude SOGNO                             */
+/*                     Projet CHLOE -- INRIA ROCQUENCOURT                    */
+/*                                Juin 1994                                  */
+/* ========================================================================= */
+
+/* ========================================================================= */
+/*                             Duong NGUYEN QUE                              */
+/*                 Adaption to abstract computation: janusvalue              */
+/*                               CRI-ENSMP                                   */
+/* ========================================================================= */
+
 #define FTRACE XX->ftrace 
 /* ................ variables resultats ....................... */
 #define VRESULT XX->result
