67fa7408d94921588358778a10eba37e0fcd8f7e
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
2005-05-03 08:00:05 +0000

    ++
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@865 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- local.mk
+++ local.mk
@@ -7,3 +7,8 @@ install:
 	$(MAKE) -C src phase1
 	$(MAKE) -C src phase2
 	$(MAKE) -C src phase3
+	$(MAKE) -C src phase4
+	$(MAKE) -C src phase5
+
+uninstall:
+	$(RM) -r ./Bin ./Include ./Lib ./Share ./Utils ./Doc ./Runtime
