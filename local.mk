5774d14716aa2a04fd50ce4046b5a158ba576b37
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
2005-05-03 07:45:23 +0000

    ++
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@1189 8716bff9-3cf5-0310-8878-b27a67f92df8

--- local.mk
+++ local.mk
@@ -2,12 +2,15 @@
 
 FWD_DIRS	= src
 
+# multi-phase compilation...
 install:
 	$(MAKE) -C src phase0
 	$(MAKE) -C src phase1
 	$(MAKE) -C src phase2
 	$(MAKE) -C src phase3
+	$(MAKE) -C src phase4
+	$(MAKE) -C src phase5
 
-
+# coldly clean everything
 uninstall:
 	$(RM) -r ./Include ./Lib ./Bin ./Share ./Utils ./Documentation
