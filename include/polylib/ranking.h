111c28b51af7815fb41dfc40fe50983001d4ef12
loechner <loechner>
2005-06-29 12:13:23 +0000

    Added a function giving the /ranking/ of an integer point I in a polyhedron P. As suggested by Sven, I is defined using a matrix M : I = M.(J N 1)^T
    where J are new parameters, and N are the original parameters.
    The ranking of I is the number of integer points in P that are lexicographically lesser than I.
    It is a function of J and N (an Ehrhart polynomial)
    The classical case is when I = J.
    
     Modified Files:
     	Makefile.am
     Added Files:
     	include/polylib/ranking.h source/ehrhart/ranking.c

---
+++ include/polylib/ranking.h
@@ -0,0 +1,18 @@
+// Tools to compute the ranking function of an iteration J : the number of integer points in P that are lexicographically inferior to J
+// (C) B. Meister 6/2005
+// LSIIT, UMR 7005 CNRS Université Louis Pasteur
+// HiPEAC Network
+// ICPS
+
+#ifndef __BM_POLYLIB_RANKING_H__
+#define __BM_POLYLIB_RANKING_H__
+#include <polylib/polylib.h>
+
+
+// given the constraints of a polyhedron P (the matrix Constraints), returns the number of points that are lexicographically stricly lesser than a point I of P,
+// defined by I = M.(J N 1)^T
+// J are the n' first variables of the returned Ehrhart polynomial.
+// If M is NULL, I = J is taken by default.
+Enumeration *Ranking(Matrix * Constraints, Matrix * C, Matrix * M, unsigned MAXRAYS, char ** param_name)
+
+#endif // __BM_POLYLIB_RANKING_H__
