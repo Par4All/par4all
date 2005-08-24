f67be79420ba8bfc724446d8006bd7e7feb346ee
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
2005-08-24 15:12:12 +0000

    delete_ does not need a result. copypaste error;-)
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@875 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- src/genC/newgen_generic_mapping.h
+++ src/genC/newgen_generic_mapping.h
@@ -70,7 +70,7 @@ PREFIX result load_##type##_##name(type s)\
   t = (result) hash_get((hash_table) (name##_map), (void*) (s));\
   if (t ==(result) HASH_UNDEFINED_VALUE) t = result##_undefined; return t;}\
 PREFIX void delete_##type##_##name(type s)\
-{ result t; message_assert("key defined", s != type##_undefined);\
+{ message_assert("key defined", s != type##_undefined);\
   (void) hash_del((hash_table) (name##_map), (void*) (s));}\
 PREFIX bool type##_##name##_undefined_p(type s)\
 { return(load_##type##_##name(s)==result##_undefined);}\
