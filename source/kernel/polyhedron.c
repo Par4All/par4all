Par4All/par4all

commit 7190a5fd221f87003cd622835604dfdd6b81f85a

- Optimized the polyhedron expansion in Ehrhart_Quick_Apx_Full_Dim - exploited the recently introduced macro "value_addmul" in a lot of files (listed hereunder), in order to optimize this kind of operations and to spare some temporary variables. Especially appreciated in gmp mode.
B. Meister

 Modified Files:
 	source/ehrhart/ehrhart.c source/ehrhart/eval_ehrhart.c
 	source/ehrhart/ext_ehrhart.c source/kernel/Lattice.c
 	source/kernel/NormalForms.c source/kernel/SolveDio.c
 	source/kernel/compress_parms.c source/kernel/matrix.c
	source/kernel/polyhedron.c source/kernel/vector.c

source/kernel/polyhedron.c
@@ -399,7 +399,7 @@ static int Chernikova (Matrix *Mat,Matrix *Ray,SatMatrix *Sat, unsigned NbBid, u
   int *Temp, aux;
   int *ip1, *ip2;
   unsigned bx, m, jx;
-  Value tmp;
+  // Value tmp;
   Value *p1, *p2, *p3;
 
 #ifdef POLY_CH_DEBUG
@@ -411,7 +411,7 @@ static int Chernikova (Matrix *Mat,Matrix *Ray,SatMatrix *Sat, unsigned NbBid, u
   SMPrint(Sat);
 #endif
   
-  value_init(tmp);
+  // value_init(tmp);
   NbConstraints=Mat->NbRows;
   NbRay = Ray->NbRows;
   Dimension = Mat->NbColumns-1;         /* Homogeneous Dimension */
@@ -423,7 +423,7 @@ static int Chernikova (Matrix *Mat,Matrix *Ray,SatMatrix *Sat, unsigned NbBid, u
   Temp=(int *)malloc(RowSize2);
   if(!Temp) {	
     errormsg1("Chernikova", "outofmem", "out of memory space");
-    value_clear(tmp);
+    // value_clear(tmp);
     return 0;
   }
   CATCH(any_exception_error) {
@@ -432,7 +432,7 @@ static int Chernikova (Matrix *Mat,Matrix *Ray,SatMatrix *Sat, unsigned NbBid, u
    * In case of overflow, free the allocated memory!
    * Rethrow upwards the stack to forward the exception.
    */
-    value_clear(tmp);
+    // value_clear(tmp);
     free(Temp);
     RETHROW();
   }
@@ -458,8 +458,9 @@ static int Chernikova (Matrix *Mat,Matrix *Ray,SatMatrix *Sat, unsigned NbBid, u
 	for (j=1; j<Dimension; j++) {	
 	  
 	  /* *p3 +=  *p1 * *p2 */
-	  value_multiply(tmp,*p1,*p2);
-	  value_addto(*p3,*p3,tmp);
+	  // value_multiply(tmp,*p1,*p2);
+	  // value_addto(*p3,*p3,tmp);
+	  value_addmul(*p3, *p1, *p2);
 	  p1++; p2++;
 	}
 	if (value_notzero_p(*p3) && (i<index_non_zero)) 
@@ -739,7 +740,7 @@ static int Chernikova (Matrix *Mat,Matrix *Ray,SatMatrix *Sat, unsigned NbBid, u
   SMPrint (Sat);
 #endif
   
-  value_clear(tmp);
+  // value_clear(tmp);
   return 0;
 } /* Chernikova */
 
@@ -2218,15 +2219,15 @@ static SatMatrix *BuildSat(Matrix *Mat,Matrix *Ray,unsigned NbConstraints,unsign
   SatMatrix *Sat = NULL;
   int i, j, k, jx;
   Value *p1, *p2, *p3;
-  Value tmp;
+  // Value tmp;
   unsigned Dimension, NbRay, bx, nbcolumns;
   
-  value_init(tmp);
+  // value_init(tmp);
   
   CATCH(any_exception_error) {
     if (Sat) 
       SMFree(&Sat);
-    value_clear(tmp);
+    // value_clear(tmp);
     RETHROW();
   }
   TRY {
@@ -2249,8 +2250,9 @@ static SatMatrix *BuildSat(Matrix *Mat,Matrix *Ray,unsigned NbConstraints,unsign
 	p3 = Ray->p[i];
 	value_set_si(*p3,0);
 	for (j=0; j<Dimension; j++) {
-	  value_multiply(tmp,*p1,*p2);
-	  value_addto(*p3,*p3,tmp);
+	  //  value_multiply(tmp,*p1,*p2);
+	  // value_addto(*p3,*p3,tmp);
+	  value_addmul(*p3, *p1, *p2);
 	  p1++; p2++;
 	}
       }
@@ -2266,7 +2268,7 @@ static SatMatrix *BuildSat(Matrix *Mat,Matrix *Ray,unsigned NbConstraints,unsign
   } /* end of TRY */
   
   UNCATCH(any_exception_error);
-  value_clear(tmp);
+  // value_clear(tmp);
   return Sat;
 } /* BuildSat */
 
@@ -2355,9 +2357,11 @@ int PolyhedronIncludes(Polyhedron *Pol1,Polyhedron *Pol2) {
 	
   int Dimension = Pol1->Dimension + 1;   /* Homogenous Dimension */
   int i, j, k;
-  Value *p1, *p2, p3, tmp;
+  Value *p1, *p2, p3;
+  //, tmp;
   
-  value_init(p3); value_init(tmp);
+  value_init(p3); 
+  // value_init(tmp);
   for (k=0; k<Pol1->NbConstraints; k++) {
     for (i=0;i<Pol2->NbRays;i++) {
       
@@ -2366,8 +2370,9 @@ int PolyhedronIncludes(Polyhedron *Pol1,Polyhedron *Pol2) {
       p2 = Pol1->Constraint[k]+1;
       value_set_si(p3,0);
       for(j=0;j<Dimension;j++) {
-	value_multiply(tmp,*p1,*p2);
-	value_addto(p3,p3,tmp);
+	// value_multiply(tmp,*p1,*p2);
+	// value_addto(p3,p3,tmp);
+	value_addmul(p3, *p1,*p2);
 	p1++; p2++;
       }
      
@@ -2376,12 +2381,14 @@ int PolyhedronIncludes(Polyhedron *Pol1,Polyhedron *Pol2) {
       if(value_neg_p(p3) ||
           (value_notzero_p(p3)
              && (value_zero_p(Pol1->Constraint[k][0]) || (value_zero_p(Pol2->Ray[i][0]))   ) )) {
-	value_clear(p3); value_clear(tmp);
+	value_clear(p3); 
+	// value_clear(tmp);
 	return 0;
       }
     }
   } 
-  value_clear(p3); value_clear(tmp);
+  value_clear(p3); 
+  // value_clear(tmp);
   return 1;
 } /* PolyhedronIncludes */
 
@@ -2957,8 +2964,9 @@ static void FindSimple(Polyhedron *P1,Polyhedron *P2,unsigned *Filter,unsigned N
 	    p2 = P1->Constraint[k]+1;
 	    value_set_si(p3,0);
 	    for (j=0; j<Dimension; j++) {
-	      value_multiply(tmp,*p1,*p2);
-	      value_addto(p3,p3,tmp);
+	      // value_multiply(tmp,*p1,*p2);
+	      // value_addto(p3,p3,tmp);
+	      value_addmul(p3, *p1, *p2);
 	      p1++; p2++;
 	    }
 	    if(value_zero_p(p3) ||
@@ -3840,8 +3848,9 @@ static void Rays_Mult(Value **A, Matrix *B, Value **C, unsigned NbRays)
 	for (k=0; k<Dimension1; k++) {
 	  
 	  /* Sum+=A[i][k+1] * B->p[k][j]; */
-	  value_multiply(tmp,A[i][k+1],B->p[k][j]);
-	  value_addto(Sum,Sum,tmp);
+	  // value_multiply(tmp,A[i][k+1],B->p[k][j]);
+	  // value_addto(Sum,Sum,tmp);
+	  value_addto(Sum, A[i][k+1], B->p[k][j]);
 	}
 	value_assign(C[i][j+1],Sum);
       }
@@ -3881,8 +3890,9 @@ static void Rays_Mult_Transpose(Value **A, Matrix *B, Value **C,
 	for (k=0; k<Dimension1; k++) {
 	  
 	  /* Sum+=A[i][k+1] * B->p[j][k]; */
-	  value_multiply(tmp,A[i][k+1],B->p[j][k]);
-	  value_addto(Sum,Sum,tmp);
+	  // value_multiply(tmp,A[i][k+1],B->p[j][k]);
+	  // value_addto(Sum,Sum,tmp);
+	  value_addmul(Sum, A[i][k+1], B->p[j][k]);
 	}
 	value_assign(C[i][j+1],Sum);
       }
@@ -3907,14 +3917,17 @@ Polyhedron *Polyhedron_Preimage(Polyhedron *Pol,Matrix *Func,unsigned NbMaxRays)
   Polyhedron *NewPol = NULL;
   unsigned NbConstraints, Dimension1, Dimension2;
   int i, j, k;
-  Value Sum,tmp;
+  Value Sum;
+  //, tmp;
 
-  value_init(Sum); value_init(tmp);
+  value_init(Sum); 
+  // value_init(tmp);
 
   CATCH(any_exception_error) {
     if (Constraints) Matrix_Free(Constraints);
     if (NewPol) Polyhedron_Free(NewPol);
-    value_clear(Sum); value_clear(tmp);
+    value_clear(Sum); 
+    // value_clear(tmp);
     RETHROW();
   }
   TRY {
@@ -3925,7 +3938,8 @@ Polyhedron *Polyhedron_Preimage(Polyhedron *Pol,Matrix *Func,unsigned NbMaxRays)
     if (Dimension1!=(Func->NbRows)) {
       errormsg1("Polyhedron_Preimage", "dimincomp", "incompatable dimensions");
       UNCATCH(any_exception_error);
-      value_clear(Sum); value_clear(tmp);
+      value_clear(Sum); 
+      // value_clear(tmp);
       return Empty_Polyhedron(Dimension2-1);
     }
     
@@ -3942,7 +3956,8 @@ Polyhedron *Polyhedron_Preimage(Polyhedron *Pol,Matrix *Func,unsigned NbMaxRays)
       errormsg1("Polyhedron_Preimage", "outofmem", "out of memory space\n");
       Pol_status = 1;
       UNCATCH(any_exception_error);
-      value_clear(Sum); value_clear(tmp);
+      value_clear(Sum); 
+      // value_clear(tmp);
       return 0;
     }
     
@@ -3956,8 +3971,9 @@ Polyhedron *Polyhedron_Preimage(Polyhedron *Pol,Matrix *Func,unsigned NbMaxRays)
 	for (k=0; k<Dimension1; k++) {
 	  
 	  /* Sum+=Pol->Constraint[i][k+1] * Func->p[k][j]; */
-	  value_multiply(tmp,Pol->Constraint[i][k+1],Func->p[k][j]);
-	  value_addto(Sum,Sum,tmp);
+	  // value_multiply(tmp,Pol->Constraint[i][k+1],Func->p[k][j]);
+	  // value_addto(Sum,Sum,tmp);
+	  value_addmul(Sum, Pol->Constraint[i][k+1], Func->p[k][j]);
 	}
 	value_assign(Constraints->p[i][j+1],Sum);
       }
@@ -4024,14 +4040,17 @@ Polyhedron *Polyhedron_Image(Polyhedron *Pol, Matrix *Func,unsigned NbMaxConstrs
   Polyhedron *NewPol = NULL;
   unsigned NbRays, Dimension1, Dimension2;
   int i, j, k;
-  Value Sum, tmp;
+  Value Sum;
+  // , tmp;
   
-  value_init(Sum); value_init(tmp);
+  value_init(Sum); 
+  // value_init(tmp);
 
   CATCH(any_exception_error) {
     if (Rays) Matrix_Free(Rays);
     if (NewPol) Polyhedron_Free(NewPol);
-    value_clear(Sum); value_clear(tmp);
+    value_clear(Sum); 
+    // value_clear(tmp);
     RETHROW();
   }
   TRY {
@@ -4042,7 +4061,8 @@ Polyhedron *Polyhedron_Image(Polyhedron *Pol, Matrix *Func,unsigned NbMaxConstrs
     if (Dimension1!=Func->NbColumns) {
       errormsg1("Polyhedron_Image", "dimincomp", "incompatable dimensions");
       UNCATCH(any_exception_error);
-      value_clear(Sum); value_clear(tmp);
+      value_clear(Sum); 
+      // value_clear(tmp);
       return Empty_Polyhedron(Dimension2-1);
     }
     
@@ -4061,7 +4081,8 @@ Polyhedron *Polyhedron_Image(Polyhedron *Pol, Matrix *Func,unsigned NbMaxConstrs
     if (!Rays) {
       errormsg1("Polyhedron_Image", "outofmem", "out of memory space\n");
       UNCATCH(any_exception_error);
-      value_clear(Sum); value_clear(tmp);
+      value_clear(Sum); 
+      // value_clear(tmp);
       return 0;
     }
     
@@ -4074,8 +4095,9 @@ Polyhedron *Polyhedron_Image(Polyhedron *Pol, Matrix *Func,unsigned NbMaxConstrs
 	for (k=0; k<Dimension1; k++) {
 	  
 	  /* Sum+=Pol->Ray[i][k+1] * Func->p[j][k]; */
-	  value_multiply(tmp,Pol->Ray[i][k+1],Func->p[j][k]);
-	  value_addto(Sum,Sum,tmp);
+	  // value_multiply(tmp,Pol->Ray[i][k+1],Func->p[j][k]);
+	  // value_addto(Sum,Sum,tmp);
+	  value_addmul(Sum, Pol->Ray[i][k+1], Func->p[j][k]);
 	}
 	value_assign(Rays->p[i][j+1],Sum);
       }
@@ -4086,7 +4108,8 @@ Polyhedron *Polyhedron_Image(Polyhedron *Pol, Matrix *Func,unsigned NbMaxConstrs
   } /* end of TRY */
   
   UNCATCH(any_exception_error);
-  value_clear(Sum); value_clear(tmp);
+  value_clear(Sum); 
+  // value_clear(tmp);
   return NewPol;
 } /* Polyhedron_Image */
 

source/ehrhart/ehrhart.c
@@ -731,9 +731,10 @@ Polyhedron *Polyhedron_Preprocess(Polyhedron *D,Value *size,unsigned MAXRAYS)
     Matrix *M;
     int i, j, d;
     Polyhedron *T, *S, *H, *C;
-    Value *min,tmp;
+    Value *min;
+    //, tmp;
   
-    value_init(tmp);
+    // value_init(tmp);
     d = D->Dimension;
     if (MAXRAYS < 2*D->NbConstraints)
 	MAXRAYS = 2*D->NbConstraints;
@@ -764,8 +765,9 @@ Polyhedron *Polyhedron_Preprocess(Polyhedron *D,Value *size,unsigned MAXRAYS)
         Vector_Copy(D->Constraint[i],M->p[M->NbRows],(d+2));
         for(j=1;j<=d;j++)
             if(value_neg_p(D->Constraint[i][j])) {
-                value_multiply(tmp,D->Constraint[i][j],size[j-1]);
-                value_addto(M->p[M->NbRows][d+1],M->p[M->NbRows][d+1],tmp);
+	      // value_multiply(tmp,D->Constraint[i][j],size[j-1]);
+	      // value_addto(M->p[M->NbRows][d+1],M->p[M->NbRows][d+1],tmp);
+	      value_addmul(M->p[M->NbRows][d+1], D->Constraint[i][j], size[j-1]);
             }
     
         /* If anything changed, add this new constraint */
@@ -783,7 +785,7 @@ Polyhedron *Polyhedron_Preprocess(Polyhedron *D,Value *size,unsigned MAXRAYS)
     if (!T || emptyQ(T)) {
         if(T)
             Polyhedron_Free(T);
-        value_clear(tmp);
+        // value_clear(tmp);
         return(NULL);
     }
   
@@ -814,7 +816,7 @@ Polyhedron *Polyhedron_Preprocess(Polyhedron *D,Value *size,unsigned MAXRAYS)
         {
             for(i=0;i<=(d+1);i++)
                 value_clear(min[i]);
-            value_clear(tmp);
+            // value_clear(tmp);
             return(NULL);
         }
     Domain_Free(S);
@@ -865,7 +867,7 @@ Polyhedron *Polyhedron_Preprocess(Polyhedron *D,Value *size,unsigned MAXRAYS)
     for(i=0;i<=(d+1);i++)
         value_clear(min[i]);
     free(min);
-    value_clear(tmp);
+    // value_clear(tmp);
     return(H);
 } /* Polyhedron_Preprocess */
 
@@ -1098,8 +1100,9 @@ Polyhedron *old_Polyhedron_Preprocess(Polyhedron *D,Value size,unsigned MAXRAYS)
                 Vector_Combine(&(C[ub][1]),&(C[lb][1]),&(M->p[new][1]),
                         b1,abs_a,dim+1);
                 value_multiply(aa,a1,b1);
-                value_multiply(abs_a,aa,size_copy);
-                value_addto(M->p[new][dim+1],M->p[new][dim+1],abs_a);
+                // value_multiply(abs_a,aa,size_copy);
+                // value_addto(M->p[new][dim+1],M->p[new][dim+1],abs_a);
+		value_addmul(M->p[new][dim+1], aa, size_copy);
                 Vector_Normalize(&(M->p[new][1]),(dim+1));
                 new++;
             }

source/ehrhart/eval_ehrhart.c
@@ -28,24 +28,25 @@ int in_domain(Polyhedron *P, Value *list_args) {
   int col,row;
   Value v; /* value of the constraint of a row when
 	       parameters are instanciated*/
-  Value tmp;
+  //  Value tmp;
 
   value_init(v); 
-  value_init(tmp);
+  // value_init(tmp);
   
   /*P->Constraint constraint matrice of polyhedron P*/  
   for(row=0;row<P->NbConstraints;row++) {
     value_assign(v,P->Constraint[row][P->Dimension+1]); /*constant part*/
     for(col=1;col<P->Dimension+1;col++) {
-      value_multiply(tmp,P->Constraint[row][col],list_args[col-1]);
-      value_addto(v,v,tmp);
+      // value_multiply(tmp,P->Constraint[row][col],list_args[col-1]);
+      // value_addto(v,v,tmp);
+      value_addmul(v, P->Constraint[row][col], list_args[col-1]); 
     }  
     if (value_notzero_p(P->Constraint[row][0])) {
 	
       /*if v is not >=0 then this constraint is not respected */
       if (value_neg_p(v)) {
 	value_clear(v);
-	value_clear(tmp);
+	// value_clear(tmp);
 	return 0;
       }	
     }
@@ -54,7 +55,7 @@ int in_domain(Polyhedron *P, Value *list_args) {
       /*if v is not = 0 then this constraint is not respected */
       if (value_notzero_p(v)) {
 	value_clear(v);
-	value_clear(tmp);
+	// value_clear(tmp);
 	return 0;
       }
     }
@@ -63,7 +64,7 @@ int in_domain(Polyhedron *P, Value *list_args) {
   /*if not return before this point => all 
     the constraints are respected */
   value_clear(v);
-  value_clear(tmp);
+  // value_clear(tmp);
   return 1;
 } /* in_domain */
 

source/ehrhart/ext_ehrhart.c
@@ -35,7 +35,8 @@ Matrix *CalcBase( Matrix *R )
 	int lnn;
 	int dimbase;
 	int u;		/* vecteur util en cours de calcul */
-	Value  som, t;
+	Value  som;
+	// , t;
 	int c;
 	/* diagonalisation de R : algo du pivot */
 	/* avec conservation des pivots nuls */
@@ -87,16 +88,17 @@ Matrix *CalcBase( Matrix *R )
 			{
 				/* somme des coef deja calcules * coef dans la matrice */
 				value_set_si(som,0);
-				value_init( t );
+				// value_init( t );
 				for( c=l ; c>i ; --c )
 				{
 			       //som += R->p[i][c] * B->p[u][c];
-					value_multiply( t, R->p[i][c] , B->p[u][c]);
-					value_addto(som, som, t );
+				  // value_multiply( t, R->p[i][c] , B->p[u][c]);
+				  // value_addto(som, som, t );
+				  value_addmul(som, R->p[i][c], B->p[u][c]);
 					//B->p[u][c] *= R->p[i][i];
 					value_multiply(B->p[u][c] ,B->p[u][c] , R->p[i][i]);	
 				}
-				value_clear( t );
+				// value_clear( t );
 				value_oppose(B->p[u][i] , som );
 			}
 		}
@@ -524,19 +526,21 @@ Matrix *Reduce_Matrix (Matrix *Mat) {
   /* Computes the scalar product (in euclidien space) of two vectors */ 
   
 void Scalar_product(Value *p1,Value *p2,unsigned length, Value *r) {
-        Value *cp1, *cp2, tmp;
+        Value *cp1, *cp2;
+	// , tmp;
 	
         int i;
           cp1=p1;
        	  cp2=p2;
-	  value_init(tmp);
+	  // value_init(tmp);
 	  value_set_si(*r,0);
                for (i=0;i<length;i++) {
-                 value_multiply(tmp,*cp1,*cp2);    		       
-                 value_addto(*r,*r,tmp);
+                 // value_multiply(tmp,*cp1,*cp2);    		       
+                 // value_addto(*r,*r,tmp);
+		 value_addmul(*r, *cp1, *cp2);
 		  cp1++; cp2++;
 	       }
-	       value_clear(tmp);
+	       // value_clear(tmp);
   } /* Scalar_product */
 
     /* computes the scm of two integrals  */

source/kernel/Lattice.c
@@ -206,7 +206,8 @@ void AffineSmith(Lattice *A, Lattice **U, Lattice **V, Lattice **Diag) {
   Lattice *temp;
   Lattice *Uinv;
   int i,j;
-  Value sum, tmp, quo, rem;
+  Value sum, quo, rem;
+  //, tmp;
   
 #ifdef DOMDEBUG
   FILE *fp;
@@ -215,7 +216,8 @@ void AffineSmith(Lattice *A, Lattice **U, Lattice **V, Lattice **Diag) {
   fclose(fp);
 #endif
   
-  value_init(sum); value_init(tmp);
+  value_init(sum); 
+  // value_init(tmp);
   value_init(quo); value_init(rem);
   temp = Homogenise(A,True);  
   Smith((Matrix *)temp, (Matrix **)U, (Matrix **)V, (Matrix **)Diag);
@@ -244,8 +246,9 @@ void AffineSmith(Lattice *A, Lattice **U, Lattice **V, Lattice **Diag) {
   for (i = 0; i < U[0]->NbRows-1; i ++) {
     value_set_si(sum,0);
     for(j = 0; j < U[0]->NbColumns-1; j ++) {
-      value_multiply(tmp,Uinv->p[i][j],U[0]->p[j][U[0]->NbColumns-1]);
-      value_addto(sum,sum,tmp);
+      // value_multiply(tmp,Uinv->p[i][j],U[0]->p[j][U[0]->NbColumns-1]);
+      // value_addto(sum,sum,tmp);
+      value_addmul(sum, Uinv->p[i][j], U[0]->p[j][U[0]->NbColumns-1]);
     }
     value_assign(Diag[0]->p[i][j],sum);
   }
@@ -275,7 +278,8 @@ void AffineSmith(Lattice *A, Lattice **U, Lattice **V, Lattice **Diag) {
     value_assign( Diag[0]->p[i][Diag[0]->NbColumns-1],rem);
     value_assign(V[0]->p[i][V[0]->NbColumns-1],quo);
   }  
-  value_clear(sum); value_clear(tmp);
+  value_clear(sum); 
+  // value_clear(tmp);
   value_clear(quo); value_clear(rem);
   return;
 } /* AffineSmith */
@@ -928,8 +932,9 @@ static void AddLattice (LatticeUnion *Head, Matrix  *B1,  Matrix *B2, int NumofT
       tempMatrix = (Lattice *)Matrix_Copy(temp->M);	  
       for (j = 0; j < B2->NbRows; j++) {
 	value_set_si(tmp,i);
-	value_multiply(tmp,tmp,B1->p[j][Colnumber]);
-	value_addto(tempMatrix->p[j][B2->NbColumns-1],tempMatrix->p[j][B2->NbColumns-1],tmp);	
+	// value_multiply(tmp,tmp,B1->p[j][Colnumber]);
+	// value_addto(tempMatrix->p[j][B2->NbColumns-1],tempMatrix->p[j][B2->NbColumns-1],tmp);	
+	value_addmul(tempMatrix->p[j][B2->NbColumns-1], tmp, B1->p[j][Colnumber]);
       }
       tail->next = (LatticeUnion *)malloc(sizeof(LatticeUnion)); 
       AffineHermite(tempMatrix,&H,&U);

source/kernel/NormalForms.c
@@ -105,20 +105,21 @@ static void ligne(Value *a,int i,int j,Value x,int n,int p) {
  
   int k;
   Value *c1,*c2;
-  Value tmp;
+  // Value tmp;
 
-  value_init(tmp);
+  // value_init(tmp);
   c1=a+(i-1)*p;
   c2=a+(j-1)*p;
   
   for(k=1;k<=p;k++) {
 
-    value_multiply(tmp,x,*c2);
-    value_addto(*c1,*c1,tmp);
+    // value_multiply(tmp,x,*c2);
+    // value_addto(*c1,*c1,tmp);
+    value_addmul(*c1, x, *c2);
     c1++;
     c2++;
   }
-  value_clear(tmp);
+  // value_clear(tmp);
   return;
 } /* ligne */
 
@@ -134,19 +135,20 @@ static void colonne(Value *a,int i,int j,Value x,int n,int p) {
   
   int k;
   Value *c1,*c2;
-  Value tmp;
+  //  Value tmp;
 
-  value_init(tmp);
+  // value_init(tmp);
   c1=a+(i-1);
   c2=a+(j-1);
   
   for(k=1;k<=n;k++) {
-    value_multiply(tmp,x,*c2);
-    value_addto(*c1,*c1,tmp);
+    // value_multiply(tmp,x,*c2);
+    // value_addto(*c1,*c1,tmp);
+    value_addmul(*c1, x, *c2);
     c1=c1+p;
     c2=c2+p;
   }
-  value_clear(tmp);
+  // value_clear(tmp);
   return;
 } /* colonne */
 	

source/kernel/SolveDio.c
@@ -122,8 +122,9 @@ int SolveDiophantine(Matrix *M, Matrix **U, Vector **X) {
   for (i = 0; i < rank ; i++) {
     value_set_si(sum,0);
     for (j = 0; j < i; j++) {
-      value_multiply(tmp,T[j],hermi->p[i][j]);
-      value_addto(sum,sum,tmp);
+      //  value_multiply(tmp,T[j],hermi->p[i][j]);
+      // value_addto(sum,sum,tmp);
+      value_addmul(sum, T[j], hermi->p[i][j]);
     } 
     value_substract(tmp,C[i],sum);
     value_modulus(tmp,tmp,hermi->p[i][i]);
@@ -140,8 +141,8 @@ int SolveDiophantine(Matrix *M, Matrix **U, Vector **X) {
       return (-1);
     };
     value_substract(tmp,C[i],sum);
-    value_division(tmp,tmp,hermi->p[i][i]);
-    value_assign(T[i],tmp);
+    value_division(T[i],tmp,hermi->p[i][i]);
+    // value_assign(T[i],tmp);
   }
   
   /** Case when rank < Number of Columns; **/
@@ -156,8 +157,9 @@ int SolveDiophantine(Matrix *M, Matrix **U, Vector **X) {
   for (i = rank; i < hermi->NbRows; i++) {
     value_set_si(sum,0);
     for (j = 0; j < hermi->NbColumns; j++) {
-      value_multiply(tmp,T[j],hermi->p[i][j]);
-      value_addto(sum,sum,tmp);
+      //      value_multiply(tmp,T[j],hermi->p[i][j]);
+      //      value_addto(sum,sum,tmp);
+      value_addmul(sum, T[j], hermi->p[i][j]);
     }  
     if (value_ne(sum,C[i])) {
       *U = Matrix_Alloc(0,0);
@@ -192,8 +194,9 @@ int SolveDiophantine(Matrix *M, Matrix **U, Vector **X) {
     /* Calculating the vector X = Uinv * T */
     value_set_si(sum,0);
     for (j = 0; j < unimodinv->NbColumns; j++) {
-      value_multiply(tmp,unimodinv->p[i][j],T[j]);
-      value_addto(sum,sum,tmp);
+      // value_multiply(tmp,unimodinv->p[i][j],T[j]);
+      // value_addto(sum,sum,tmp);
+      value_addmul(sum, unimodinv->p[i][j], T[j]);
     }  
     value_assign(X[0]->p[i],sum);
   }

source/kernel/compress_parms.c
@@ -427,16 +427,15 @@ Matrix * full_dimensionize(Matrix const * M, int nb_parms, Matrix ** Validity_La
   Matrix_Free(Eqs);
   Matrix_Free(Ineqs);
   mpolyhedron_compress_last_vars(Permuted_Ineqs, Whole_Validity_Lattice);
-  show_matrix(Permuted_Eqs);
-  show_matrix(Whole_Validity_Lattice);
+  // show_matrix(Whole_Validity_Lattice);
 
   // 3- eliminate the first variables
   if (!mpolyhedron_eliminate_first_variables(Permuted_Eqs, Permuted_Ineqs)) {
     fprintf(stderr,"full-dimensionize > variable elimination failed. \n"); 
     return NULL;
   }
-  show_matrix(Permuted_Eqs);
-  show_matrix(Permuted_Ineqs);
+  // show_matrix(Permuted_Eqs);
+  // show_matrix(Permuted_Ineqs);
 
   // 4- get rid of the first (zero) columns, which are now useless, and put the parameters back at the end
   Full_Dim = Matrix_Alloc(Permuted_Ineqs->NbRows, Permuted_Ineqs->NbColumns-nb_elim_vars);
@@ -449,7 +448,7 @@ Matrix * full_dimensionize(Matrix const * M, int nb_parms, Matrix ** Validity_La
     value_assign(Full_Dim->p[i][Full_Dim->NbColumns-1], Permuted_Ineqs->p[i][Permuted_Ineqs->NbColumns-1]);
   }
   Matrix_Free(Permuted_Ineqs);
-  show_matrix(Full_Dim);
+  // show_matrix(Full_Dim);
   // return Full_Dim; 
 
   // 4- Un-permute (so that the parameters are at the end as usual)

commit f48b9b570c4cd4b5915490811ddaef4764b23341

Fix Rays_Mult.
This fixes a typo introduced in 7190a5fd221f87003cd622835604dfdd6b81f85a:
    - exploited the recently introduced macro "value_addmul" in a lot of files (listed hereunder), in order to optimize this kind of operations and to spare some temporary variables.

source/kernel/polyhedron.c
@@ -3945,7 +3945,7 @@ static void Rays_Mult(Value **A, Matrix *B, Value **C, unsigned NbRays)
 	for (k=0; k<Dimension1; k++) {
 	  
 	  /* Sum+=A[i][k+1] * B->p[k][j]; */
-	  value_addto(Sum, A[i][k+1], B->p[k][j]);
+	  value_addmul(Sum, A[i][k+1], B->p[k][j]);
 	}
 	value_assign(C[i][j+1],Sum);
       }