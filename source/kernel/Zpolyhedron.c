1a019024f1b3d0eceb1a4d678f3fc568a34397f2
Sven Verdoolaege <skimo@kotnet.org>
2005-09-15 15:36:09 +0000

    ZDomainDifference: add back commented out code
    For some reason one loop was effectively removed from
    ZDomainDifference.
    569b0a052f5e150b9751c497f83a8f5cb116e64f also added back
    this code, but also made some other changes and was therefore
    reverted in e8658b2360c78e980d0b6da68659cf902b62b525.
    This patch simply adds back this loop.
    Without it, the code is "obviously incorrect", although
    apparently no test elicits this problem.

--- source/kernel/Zpolyhedron.c
+++ source/kernel/Zpolyhedron.c
@@ -438,13 +438,13 @@ ZPolyhedron *ZDomainDifference(ZPolyhedron  *A, ZPolyhedron *B) {
     
     for(tempB = B; tempB != NULL; tempB = tempB->next) {
       templist = NULL; res = NULL;
-      //for(i = temp; i != NULL; i = i->next) {
+      for(i = temp; i != NULL; i = i->next) {
 	i=temp;
 	res = ZPolyhedronDifference(i,tempB);
 	for (j = res; j != NULL; j = j->next )
 	  templist = AddZPoly2ZDomain(j,templist);
 	ZDomain_Free(res);
-      //}
+      }
       ZDomain_Free (temp);
       temp = NULL; 
       for(i = templist; i != NULL; i = i->next)
