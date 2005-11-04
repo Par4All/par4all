77c06670da69d919bbe5ce18656b7a999c169a27
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2005-11-04 13:48:10 +0000

    # on 2005/11/04 13:48:10, irigoin did: Experimental version, maybe OK for a DREAM-Up demo, but not much more! No serious debugging or validation yet.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12210 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformations/variable_expansion.c
+++ src/Libs/transformations/variable_expansion.c
@@ -43,7 +43,7 @@ static entity expanded_variable = entity_undefined;
 
 static list processed_variables = list_undefined;
 
-static bool expand_reference(reference r)
+static bool perform_reference_expansion(reference r)
 {
   entity v = reference_variable(r);
 
@@ -65,7 +65,7 @@ static bool expand_reference(reference r)
   return TRUE;
 }
 
-static bool prepare_and_perform_expansion(loop l)
+static bool prepare_expansion(loop l)
 {
   entity i = loop_index(l);
   range r = loop_range(l);
@@ -84,7 +84,6 @@ static bool prepare_and_perform_expansion(loop l)
      && (iinc==1 /* || iiinc==-1 */ )) {
     dimension d = make_dimension(copy_expression(lb), copy_expression(ub));
     expression ie = entity_to_expression(i);
-    list evl = NIL;
 
     /* Update information about the nesting loops. */
     loop_dimensions = gen_append(loop_dimensions, CONS(DIMENSION, d, NIL));
@@ -96,39 +95,6 @@ static bool prepare_and_perform_expansion(loop l)
       print_entities(loop_locals(l));
       pips_debug(9, "\n");
     }
-
-    /* Does it contain private variables? */
-    MAP(ENTITY, lv, {
-      /* Do not expand loop indices! */
-      if(lv!=i) {
-	type t = entity_type(lv);
-	variable v = type_variable(t);
-	list dims = variable_dimensions(v);
-	statement bs = loop_body(l);
-
-	pips_assert("Scalar expansion", dims==NIL);
-
-	evl = CONS(ENTITY, lv, evl);
-
-	pips_debug(9, "Update type of %s\n", entity_local_name(lv));
-
-	/* Update its type */
-	variable_dimensions(v) = gen_full_copy_list(loop_dimensions);
-
-	/* print_type(); */
-
-	/* Update its references in the loop body */
-
-	pips_debug(9, "Expand references to %s\n", entity_local_name(lv));
-	expanded_variable = lv;
-	gen_recurse(bs, reference_domain, expand_reference, gen_null);
-	expanded_variable = entity_undefined;
-      }
-    }, loop_locals(l));
-
-    /* Remove the expanded variables from the local variable list */
-    gen_list_and_not(&loop_locals(l), evl);
-    gen_free_list(evl);
   }
 
   /* If go down is FALSE here, we should reset loop_indices and
@@ -137,14 +103,57 @@ static bool prepare_and_perform_expansion(loop l)
   return go_down;
 }
 
-static void unstack_index_and_dimension(loop l)
+static void perform_expansion_and_unstack_index_and_dimension(loop l)
 {
   entity i = loop_index(l);
 
+  /* Select loops marked as relevant on the way down. */
   if(!list_undefined_p(loop_indices)) {
     expression eli = EXPRESSION(CAR(gen_last(loop_indices)));
     if(i==reference_variable(expression_reference(eli))) {
       dimension d = DIMENSION(CAR(gen_last(loop_dimensions)));
+      list evl = NIL;
+
+      ifdebug(9) {
+	pips_debug(9, "Going up, local variables: ");
+	print_entities(loop_locals(l));
+	pips_debug(9, "\n");
+      }
+
+      /* Does it contain private variables? */
+      MAP(ENTITY, lv, {
+	/* Do not expand loop indices nor variables already processed! */
+	if(lv!=i && !gen_in_list_p(lv, processed_variables)) {
+	  type t = entity_type(lv);
+	  variable v = type_variable(t);
+	  list dims = variable_dimensions(v);
+	  statement bs = loop_body(l);
+
+	  pips_assert("Scalar expansion", dims==NIL);
+
+	  evl = CONS(ENTITY, lv, evl);
+
+	  pips_debug(9, "Update type of %s\n", entity_local_name(lv));
+
+	  /* Update its type */
+	  variable_dimensions(v) = gen_full_copy_list(loop_dimensions);
+
+	  /* print_type(); */
+
+	  /* Update its references in the loop body */
+
+	  pips_debug(9, "Expand references to %s\n", entity_local_name(lv));
+	  expanded_variable = lv;
+	  gen_recurse(bs, reference_domain, perform_reference_expansion, gen_null);
+	  expanded_variable = entity_undefined;
+	}
+      }, loop_locals(l));
+
+      /* Remove the expanded variables and the loop index from the local variable list */
+      gen_list_and_not(&loop_locals(l), processed_variables);
+      gen_list_and_not(&loop_locals(l), evl);
+      processed_variables = gen_append(processed_variables, evl);
+      processed_variables = gen_append(processed_variables, CONS(ENTITY, i, NIL));
 
       gen_remove(&loop_indices, (void *) eli);
       gen_remove(&loop_dimensions, (void *) d);
@@ -153,15 +162,13 @@ static void unstack_index_and_dimension(loop l)
   }
 }
 
-bool variable_expansion(char *module_name)
-{
-  return scalar_expansion(module_name);
-}
-
 bool scalar_expansion(char *module_name)
 {
     entity module;
     statement module_stat;
+
+    pips_user_warning("\nExperimental phase: on-going debugging!\n");
+    pips_user_warning("\nPrivatize variables before you run this phase\n");
 
     /* Why would I need this? To access declarations for instance */
     set_current_module_entity( local_name_to_top_level_entity(module_name) );
@@ -193,16 +200,23 @@ bool scalar_expansion(char *module_name)
 
     loop_indices = NIL;
     loop_dimensions = NIL;
-    gen_recurse(module_stat, loop_domain, prepare_and_perform_expansion,
-		unstack_index_and_dimension);
+    processed_variables = NIL;
+    gen_recurse(module_stat, loop_domain, prepare_expansion,
+		perform_expansion_and_unstack_index_and_dimension);
 
     pips_assert("Supporting lists are both empty", loop_indices==NIL);
     pips_assert("Supporting lists are both empty", loop_dimensions==NIL);
 
     loop_indices = list_undefined;
     loop_dimensions = list_undefined;
+    gen_free_list(processed_variables);
+    processed_variables = list_undefined;
     reset_current_module_entity();
     reset_current_module_statement();
+
+    /* Declarations must be regenerated for the code to be compilable */
+    free(code_decls_text(value_code(entity_initial(module))));
+    code_decls_text(value_code(entity_initial(module))) = strdup("");
 
     pips_debug(1, "end\n");
     debug_off();
@@ -210,4 +224,9 @@ bool scalar_expansion(char *module_name)
     DB_PUT_MEMORY_RESOURCE(DBR_CODE, strdup(module_name), module_stat);
 
     return(TRUE);
+}
+
+bool variable_expansion(char *module_name)
+{
+  return scalar_expansion(module_name);
 }
