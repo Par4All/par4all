1510e5dce4bb6d8ab828b5f95d838e8fff515409
fabien <fabien@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2005-12-24 15:59:28 +0000

    missing file.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12323 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/hyperplane/unimodular.c
@@ -0,0 +1,16 @@
+/*
+ * $Id$
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+#include "misc.h"
+
+int loop_unimodular(string name)
+{
+  return TRUE;
+}
