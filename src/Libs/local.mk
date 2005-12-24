b458b9d520b6aff9241d1574ddbad09d57974a75
fabien <fabien@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2005-12-24 18:38:51 +0000

    more stuff in old.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12348 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/local.mk
+++ src/Libs/local.mk
@@ -1,7 +1,7 @@
 # $Id$
 
 # old stuff:
-# paf-util pip prgm_mapping scheduling static_controlize reindexing
+# paf-util pip prgm_mapping scheduling static_controlize reindexing array_dfg
 
 # there is no rationnal order to compile the libraries:-(
 # see local TODO
@@ -12,7 +12,7 @@ FWD_DIRS	= \
 	syntax c_syntax prettyprint \
 	effects effects-generic effects-simple semantics complexity \
 	continuation reductions regions effects-convex alias-classes \
-	callgraph icfg ricedg array_dfg \
+	callgraph icfg ricedg \
 	chains rice hyperplane transformations expressions \
 	statistics instrumentation hpfc atomizer sac phrase wp65 \
 	preprocessor pipsmake
