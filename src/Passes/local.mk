00a25480993668a71d85ddbc1ebcf9d6cbf6c439
fabien <fabien@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2005-12-25 09:08:46 +0000

    new build stuff.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12354 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Passes/local.mk
@@ -0,0 +1,3 @@
+# $Id$
+
+FWD_DIRS	= pips tpips wpips fpips
--- src/Passes/tpips/build_completion_lists.sh
+++ src/Passes/tpips/build_completion_lists.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
 echo "static char *tp_phase_names[]={"
-sed '/define/!d;s|.*\(".*"\).*|	\1 ,|;' $PIPS_ROOT/Include/phases.h
+sed '/define/!d;s|.*\(".*"\).*|	\1 ,|;' $PIPS_ROOT/include/phases.h
 echo "(char*)NULL};"
 
 echo "static char *tp_resource_names[]={"
-sed '/define/!d;s|.*\(".*"\).*|	\1 ,|;' $PIPS_ROOT/Include/resources.h
+sed '/define/!d;s|.*\(".*"\).*|	\1 ,|;' $PIPS_ROOT/include/resources.h
 echo "(char*)NULL};"
 
 echo "static char *tp_file_rsc_names[]={"
 sed '/define/!d;s|.*\(".*"\).*|	\1 ,|;' \
-    $PIPS_ROOT/Include/printable_resources.h
+    $PIPS_ROOT/include/printable_resources.h
 echo "(char*)NULL};"
 
 echo "static char *tp_property_names[]={"
 sed '/#/d;/^[ 	]*$/d;s|^[ ]*\([^ ][^ ]*\).*|	"\1" ,|;' \
-    $PIPS_ROOT/Share/properties.rc
+    $PIPS_ROOT/etc/properties.rc
 echo "(char*)NULL};"
-
