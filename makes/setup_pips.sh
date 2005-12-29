49076f6f8758d66bc340056ca7cf23eaa383d3a1
coelho <coelho@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2005-12-29 16:25:13 +0000

    check needed executables. generate better rc file.
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@808 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/setup_pips.sh
+++ makes/setup_pips.sh
@@ -6,7 +6,8 @@
 #
 
 # where to get pips
-PIPS_SVN='http://svn.cri.ensmp.fr/svn/pips'
+SVN_CRI='http://svn.cri.ensmp.fr/svn'
+PIPS_SVN=$SVN_CRI/pips
 
 #POLYLIB_SITE='http://icps.u-strasbg.fr/polylib/polylib_src'
 POLYLIB_SITE='http://www.cri.ensmp.fr/pips'
@@ -31,6 +32,12 @@ mkdir $destination || error "cannot mkdir $destination"
 
 prod=$destination/prod
 
+echo "### checking needed softwares"
+for exe in svn wget tar gunzip make cproto flex bison gcc perl sed tr
+do
+  type $exe || error "no such executable, please install: $exe"
+done
+
 echo "### downloading pips"
 svn checkout $PIPS_SVN/bundles/trunks $prod || error "cannot checkout pips"
 PIPS_ARCH=`$prod/pips/makes/arch.sh`
@@ -42,7 +49,8 @@ test -f $POLYLIB.tar.gz && error "some $POLYLIB.tar.gz file already there"
 wget $POLYLIB_SITE/$POLYLIB.tar.gz || error "cannot wget polylib"
 
 echo "### building $POLYLIB"
-tar xzf $POLYLIB.tar.gz || error "cannot untar polylib"
+gunzip $POLYLIB.tar.gz || error "cannot decompress polylib"
+tar xf $POLYLIB.tar || error "cannot untar polylib"
 cd $POLYLIB || error "cannot cd into polylib"
 ./configure --prefix=$prod/extern || error "cannot configure polylib"
 make || error "cannot make polylib"
@@ -51,7 +59,7 @@ mkdir $prod/extern || error "cannot mkdir $prod/extern"
 make install || error "cannot install polylib"
 cd .. || error "cannot cd .."
 rm -rf $POLYLIB || error "cannot remove polylib"
-rm -f $POLYLIB.tar.gz || error "cannot remove polylib tar"
+rm -f $POLYLIB.tar || error "cannot remove polylib tar"
 
 echo "### fixing $POLYLIB"
 mkdir $prod/extern/lib/$PIPS_ARCH || error "cannot mkdir"
@@ -74,9 +82,21 @@ echo "### creating pipsrc.sh"
 cat <<EOF > $destination/pipsrc.sh
 # minimum rc file for sh-compatible shells
 
+# default architecture
 PIPS_ARCH=$PIPS_ARCH
 export PIPS_ARCH
 
+# subversion repositories
+NEWGEN_SVN=$SVN_CRI/newgen
+export NEWGEN_SVN
+
+LINEAR_SVN=$SVN_CRI/linear
+export LINEAR_SVN
+
+PIPS_SVN=$SVN_CRI/pips
+export PIPS_SVN
+
+# software roots
 EXTERN_ROOT=$prod/extern
 export EXTERN_ROOT
 
@@ -89,8 +109,10 @@ export LINEAR_ROOT
 PIPS_ROOT=$prod/pips
 export PIPS_ROOT
 
+# path
 PATH=\$PIPS_ROOT/bin:\$PIPS_ROOT/utils:\$PATH
 EOF
 
+# this should fail if not a developer
 echo "### getting pips development"
 svn checkout $PIPS_SVN/branches/$developer $destination/pips_dev
