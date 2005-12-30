4bf3f04dedd0d3f76882f35ef1f08b9d9ab8572c
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2005-12-30 15:02:59 +0000

    add hpfc compilation which is needed for the validation.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12430 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/local.mk
+++ src/Scripts/local.mk
@@ -3,5 +3,5 @@
 clean: NO_INCLUDES=1
 export NO_INCLUDES
 
-# not needed? stats stf hpfc jpips make
-FWD_DIRS =	drivers dev env misc epips validation davinci
+# not needed? stats stf jpips make
+FWD_DIRS =	drivers dev env misc epips validation davinci hpfc
