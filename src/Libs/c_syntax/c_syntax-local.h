0f39caa8450f987f42330a74647381931e7f3617
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2006-08-24 12:06:09 +0000

    * c_syntax-local.h: declarations for functions defined in clex.l added. The cproto step does not handle them currently.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12566 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/c_syntax/c_syntax-local.h
+++ src/Libs/c_syntax/c_syntax-local.h
@@ -3,3 +3,16 @@
 #include "c_parser_private.h"
 extern FILE * c_in; /* the file read in by the c_lexer */
 
+/* Functions declared in clex.l */
+extern int get_current_C_line_number(void);
+extern void set_current_C_line_number(void);
+extern void reset_current_C_line_number(void);
+
+extern string get_current_C_comment(void);
+extern string pop_current_C_comment(void);
+extern void push_current_C_comment(void);
+extern string update_C_comment(string cc, string nc);
+extern void discard_C_comment(void);
+extern void reset_C_comment(bool);
+extern void clear_C_comment();
+
