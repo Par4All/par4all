17eaa8b02e06d838d814a1697c35ba046b8499a7
coelho <coelho@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2007-04-19 08:11:21 +0000

    cleanup dates for fr locales...
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@862 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/main.mk
+++ makes/main.mk
@@ -125,7 +125,7 @@ PATH	:= $(PATH):$(NEWGEN_ROOT)/bin:$(NEWGEN_ROOT)/bin/$(ARCH)
 
 ###################################################################### DO STUFF
 
-UTC_DATE := $(shell date -u | tr ' ' '_')
+UTC_DATE := $(shell date -u | tr ' ,()' '_')
 CPPFLAGS += -DSOFT_ARCH='$(ARCH)' -I$(ROOT)/include
 
 # {C,CPP,LD,L,Y}OPT macros allow to *add* things from the command line
