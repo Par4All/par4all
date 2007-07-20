1ecb8921f9e20f79b15402434dab4e2741c66998
coelho <coelho@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2007-07-20 14:19:51 +0000

    transferred to "arch.mk"
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@870 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/main.mk
+++ makes/main.mk
@@ -59,26 +59,6 @@ MAKE.d	= $(ROOT)/makes
 
 include $(MAKE.d)/arch.mk
 
-ifndef ARCH
-ifdef PIPS_ARCH
-ARCH	= $(PIPS_ARCH)
-else
-ifdef NEWGEN_ARCH
-ARCH	= $(NEWGEN_ARCH)
-else
-ifdef LINEAR_ARCH
-ARCH	= $(LINEAR_ARCH)
-else
-ARCH	:= $(shell $(MAKE.d)/arch.sh)
-endif # LINEAR_ARCH
-endif # NEWGEN_ARCH
-endif # PIPS_ARCH
-endif # ARCH
-
-ifndef ARCH
-$(error "ARCH macro is not defined")
-endif
-
 ###################################################### INSTALLATION DIRECTORIES
 
 # where to install stuff
