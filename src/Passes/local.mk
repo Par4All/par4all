bbcbdb6080f9c44417e48174febef6419f6d6b3e
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2008-02-19 17:30:14 +0000

    Modified to skip even the wpips Pass compilation if asked by nowpips.mk.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12812 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/local.mk
+++ src/Passes/local.mk
@@ -1,6 +1,24 @@
 # $Id$
 
+#debug_output := $(shell echo local.mk  > /dev/tty)
+
 clean: NO_INCLUDES=1
 export NO_INCLUDES
 
-FWD_DIRS	= pips tpips wpips fpips
+# We need to read the $(ARCH).mk to know if we need to compile wpips or not:
+ifdef PIPS_ROOT
+ROOT    = $(PIPS_ROOT)
+else
+ROOT    = ../..
+endif
+MAKE.d	= $(ROOT)/makes
+include $(MAKE.d)/arch.mk
+include $(MAKE.d)/$(ARCH).mk
+
+FWD_DIRS	= pips tpips fpips
+
+# Skip compiling WPips is not required:
+
+ifndef NOWPIPS
+	FWD_DIRS	+= wpips
+endif
