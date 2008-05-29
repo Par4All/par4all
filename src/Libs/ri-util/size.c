Par4All/par4all

commit f11e6ea6a0064b411ab0566188d705e173ab58fe

Cleaning of c_syntax, comp_sections, misc, movements, prettyprint, ri-util. Move some functions to misc and ri-util. Introduce a new get_variable_name_t to ease variable_name method passing without warning. Changed to compile on Linux x86_64, but still compile on x86_32 and validate. Remove many compilation warnings in both versions. Some presentation cleaning.
git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12833 32a0c1e6-9308-0410-80dd-f9d2257a62c7

src/Libs/ri-util/size.c
@@ -433,6 +433,7 @@ int
 storage_space_of_variable(entity v)
 {
     /* Storage size is expressed in bytes */
+    char * s;
     int l;
 
     if (entity_to_size == hash_table_undefined) {
@@ -440,14 +441,14 @@ storage_space_of_variable(entity v)
 		     "hash table should have been allocated\n");
 	entity_to_size = hash_table_make(hash_pointer, 0);
     }
-
-    if ((l = (int) hash_get(entity_to_size, (char *) v))
-	== (int) HASH_UNDEFINED_VALUE) {
-	if(!SizeOfArray(v, &l)) {
-	    fprintf(stderr, "[storage_space_of_variable] Non constant array size\n");
-	    abort();
-	}
-	hash_put(entity_to_size, (char *) v, (char *) l);
+    s = hash_get(entity_to_size, (char *) v);
+    if (s == HASH_UNDEFINED_VALUE) {
+      l = (intptr_t) s;
+      if(!SizeOfArray(v, &l)) {
+	fprintf(stderr, "[storage_space_of_variable] Non constant array size\n");
+	abort();
+      }
+      hash_put(entity_to_size, (char *) v, (char *) (intptr_t) l);
     }
 
     return l;

src/Libs/c_syntax/c_parser.c
@@ -122,11 +122,11 @@ void reset_keyword_typedef_table()
    the hash table keyword_typedef_table.
    It returns an integer number corresponding to the keyword. 
    It returns 0 if s is not a keyword/typedef name */
-
-int is_c_keyword_typedef(char * s)
+intptr_t
+is_c_keyword_typedef(char * s)
 {
-  int i = (int) hash_get(keyword_typedef_table,s);
-  return (char *) i == HASH_UNDEFINED_VALUE? 0: i;
+  intptr_t i = hash_get(keyword_typedef_table,s);
+  return ((char *) i == HASH_UNDEFINED_VALUE) ? 0: i;
 }
 
 /* parsing function generated by Bison, from cyacc.y*/

src/Libs/c_syntax/cyacc.y
@@ -1179,7 +1179,7 @@ init_declarator:                             /* ISO 6.7 */
 				    print_expression(value_expression(oiv));
 				  }
 				  else {
-				    fprintf(stderr, "Value tag: %d\n", value_tag(entity_initial(v)));
+				    fprintf(stderr, "Value tag: %td\n", value_tag(entity_initial(v)));
 				  }
 				  pips_internal_error("Scoping not implemented yet, might be the reason\n");
 				}

src/Libs/c_syntax/statement.c
@@ -28,6 +28,7 @@
 #include "pipsdbm.h"
 #include "text-util.h"
 #include "properties.h"
+#include "instrumentation.h"
 
 extern statement ModuleStatement;
 

src/Libs/c_syntax/util.c
@@ -238,7 +238,7 @@ expression MemberDerivedIdentifierToExpression(type t,string m)
   if (type_variable_p(t))
     {
       basic b = variable_basic(type_variable(t));
-      pips_debug(6,"Basic tag is %d",basic_tag(b));
+      pips_debug(6,"Basic tag is %td",basic_tag(b));
       switch (basic_tag(b)) {
       case is_basic_pointer:
 	{
@@ -529,21 +529,6 @@ type make_standard_long_integer_type(type t)
 /******************* ENTITIES *******************/
 
 
-bool static_module_p(entity e)
-{
-  return static_module_name_p(entity_name(e));
-}
-
-/* FC: move to ri-util? */
-bool compilation_unit_p(string module_name)
-{
-  /* A module name is a compilation unit if and only if its last character is 
-     FILE_SEP */
-  if (module_name[strlen(module_name)-1]==FILE_SEP)
-    return TRUE;
-  return FALSE;
-}
-
 entity FindEntityFromLocalName(string name)
 {
   /* Find an entity from its local name.
@@ -812,7 +797,7 @@ type UpdateFinalPointer(type pt, type t)
   /* This function replaces the type pointed by the pointer pt
      (this can be a pointer of pointer,... so we have to go until the last one)
      by the type t*/
-  pips_debug(3,"Update final pointer type %d and %d\n",type_tag(pt),type_tag(t));
+  pips_debug(3,"Update final pointer type %td and %td\n",type_tag(pt),type_tag(t));
   if (type_variable_p(pt) && basic_pointer_p(variable_basic(type_variable(pt))))
     {
       type ppt = basic_pointer(variable_basic(type_variable(pt)));
@@ -964,13 +949,13 @@ void CCompilationUnitMemoryAllocation(entity module)
 {
   /* Should be followed by preconditions */
   entity msae = FindOrCreateEntity(compilation_unit_name,  STATIC_AREA_LOCAL_NAME);
-  area msa = type_area(entity_type(msae));
+  //area msa = type_area(entity_type(msae));
   entity gsae = FindOrCreateEntity(TOP_LEVEL_MODULE_NAME,  STATIC_AREA_LOCAL_NAME);
-  area gsa = type_area(entity_type(gsae));
+  //area gsa = type_area(entity_type(gsae));
   entity fdae = FindOrCreateEntity(get_current_module_name(), DYNAMIC_AREA_LOCAL_NAME);
-  area fda = type_area(entity_type(fdae));
+  //area fda = type_area(entity_type(fdae));
   entity fsae = FindOrCreateEntity(get_current_module_name(), STATIC_AREA_LOCAL_NAME);
-  area fsa = type_area(entity_type(fsae));
+  //area fsa = type_area(entity_type(fsae));
   
   /* Code for reallocation of memory problem due to reparsing of compilation unit
      previouscompunit = get_current_compilation_unit_entity();
@@ -1110,7 +1095,7 @@ void UpdateEntity(entity e, stack ContextStack, stack FormalStack, stack Functio
 
   if (!storage_undefined_p(c_parser_context_storage(context)))
     {
-      pips_debug(3,"Current storage context is %d\n",
+      pips_debug(3,"Current storage context is %td\n",
 		 storage_tag(c_parser_context_storage(context)));
       entity_storage(e) = c_parser_context_storage(context);
     }
@@ -1357,8 +1342,8 @@ storage MakeStorageRam(entity v, bool is_external, bool is_static)
   entity globalStaticArea = FindOrCreateEntity(TOP_LEVEL_MODULE_NAME,  STATIC_AREA_LOCAL_NAME);
   area gsa = type_area(entity_type(globalStaticArea));
   area stack = type_area(entity_type(StackArea));
-  area heap = type_area (entity_type(HeapArea));
-  entity m = get_current_module_entity();
+  //area heap = type_area (entity_type(HeapArea));
+  //entity m = get_current_module_entity();
   
   pips_assert("RAM Storage is used only for variables", type_variable_p(entity_type(v)));
 
@@ -1461,21 +1446,6 @@ string CreateMemberScope(string derived, bool is_external)
   return s;
 }
 
-/* Move this to ri-util !!!*/
-
-string list_to_string(list l)
-{
-  string result = NULL;
-  if (l==NIL) return "";
-  MAP(STRING,s, 
-  {
-    if (result==NULL)
-      result = strdup((const char *)s);
-    else 
-      result = strdup(concatenate(result,s,NULL)); 
-  }, l);
-  return result;
-}
 
 value MakeEnumeratorInitialValue(list enum_list, int counter)
 {
@@ -1649,7 +1619,8 @@ static bool callnodeclfilter(call c)
   }
 }
 
-void nodecl_p(entity module, statement stat)
+void
+nodecl_p(entity __attribute__ ((unused)) module, statement stat)
 {
   declarationerror_p = FALSE;
   gen_multi_recurse(stat, reference_domain,referencenodeclfilter,gen_null,

src/Libs/comp_sections/myintrinsics.c
@@ -285,10 +285,10 @@ transformer context;
 /*}}}*/
 /*{{{  no_write_comp_regions*/
 /*===============================================================================*/
-list no_write_comp_regions(e, args, context)
-entity e;
-list args;
-transformer context;
+list
+no_write_comp_regions(entity __attribute__ ((unused)) e,
+		      list args,
+		      transformer context)
 {
     list lr;
 
@@ -302,10 +302,10 @@ transformer context;
 /*}}}*/
 /*{{{  affect_comp_regions*/
 /*===============================================================================*/
-list affect_comp_regions(e, args, context)
-entity e;
-list args;
-transformer context;
+list
+affect_comp_regions(entity __attribute__ ((unused)) e,
+		    list args,
+		    transformer context)
 {
     list le = NIL;
 

src/Libs/comp_sections/operators.c
@@ -46,12 +46,14 @@ comp_desc InitCompDesc(reference ref, tag ReadWrite)
 /* The multidimensional union of two complementary sections
  * performed on a 2-d basis
  */
-comp_sec CompUnion(comp_sec cs1, comp_sec cs2)
+comp_sec
+CompUnion(comp_sec __attribute__ ((unused)) cs1,
+	  comp_sec __attribute__ ((unused)) cs2)
 {
   comp_sec result;
-  int i;
-  unsigned Rank = 0;
-  unsigned NoOfImages = (Rank*Rank)/2;
+  unsigned int i;
+  unsigned int Rank = 0;
+  unsigned int NoOfImages = (Rank*Rank)/2;
   
   for (i = 0; i < NoOfImages; i++)  {
   }
@@ -60,8 +62,10 @@ comp_sec CompUnion(comp_sec cs1, comp_sec cs2)
   return(result);
 }
 
-bool CompIntersection(comp_sec cs1, comp_sec cs2)
+bool
+CompIntersection(comp_sec __attribute__ ((unused)) cs1,
+		 comp_sec __attribute__ ((unused)) cs2)
 {
-
+  return FALSE;
 }
 

src/Libs/comp_sections/prettyprint.c
@@ -4,6 +4,7 @@
 
 #include "all.h"
 #include "text-util.h"
+#include "text.h"
 #include "prettyprint.h"
 #include "top-level.h"
 
@@ -212,10 +213,10 @@ static text get_any_comp_regions_text(string module_name,
  * comment  : if the number of array comp_regions is not nul, then empty lines are
  *            added before and after the text of the list of comp_regions.
  */
-static text text_statement_array_comp_regions(module, margin, stat)
-entity module;
-int margin;
-statement stat;
+static text
+text_statement_array_comp_regions(entity __attribute__ ((unused)) module,
+				  int __attribute__ ((unused)) margin,
+				  statement stat)
 {
     list l_reg = NIL;
 
@@ -364,7 +365,7 @@ effect reg;
     }
     else
     {
-	gen_free(t_reg);
+	free_text(t_reg);
 	t_reg = words_predicate_to_commentary(words_comp_region(reg), str_prefix);
     }
 

commit 8e30e5c3f905071f5d03081b1becce16b915654b

Fixed a bug I introduced in r12833. Shame on me...
git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12838 32a0c1e6-9308-0410-80dd-f9d2257a62c7

src/Libs/ri-util/size.c
@@ -433,8 +433,8 @@ int
 storage_space_of_variable(entity v)
 {
     /* Storage size is expressed in bytes */
-    char * s;
     int l;
+    char * s;
 
     if (entity_to_size == hash_table_undefined) {
 	user_warning("storage_space_of_variable",
@@ -442,8 +442,8 @@ storage_space_of_variable(entity v)
 	entity_to_size = hash_table_make(hash_pointer, 0);
     }
     s = hash_get(entity_to_size, (char *) v);
+    l = (intptr_t) s;
     if (s == HASH_UNDEFINED_VALUE) {
-      l = (intptr_t) s;
       if(!SizeOfArray(v, &l)) {
 	fprintf(stderr, "[storage_space_of_variable] Non constant array size\n");
 	abort();