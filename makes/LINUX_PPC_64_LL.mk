766998f50c32dcc676ce2560e8ea043226bb63f5
coelho <coelho@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2008-06-17 08:52:58 +0000

    tentative pour powerpc 64bits
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@906 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

---
+++ makes/LINUX_PPC_64_LL.mk
@@ -0,0 +1,7 @@
+# $Id: LINUXI86LL.mk 732 2005-03-14 11:00:18Z coelho $
+
+include $(ROOT)/makes/GNULL.mk
+include $(ROOT)/makes/linux.mk
+#include $(ROOT)/makes/x86_64.mk
+include $(ROOT)/makes/no_wpips.mk
+include $(ROOT)/makes/no_jpips.mk
--- makes/arch.sh
+++ makes/arch.sh
@@ -6,12 +6,18 @@
 #
 
 case `uname -s` in
-    Linux) if [ `uname -m` = 'x86_64' ]; then 
+    Linux) 
+      if [ `uname -m` = 'x86_64' ] ; then 
 	echo "LINUX_x86_64_LL"
-	else 
+      elif [ `uname -m` = 'ppc64' ] ; then
+	echo "LINUX_PPC_64_LL"
+      else
 	echo "LINUXI86LL"
-	fi;;
-    SunOS) echo "GNUSOL2LL" ;;
-    FreeBSD) echo "FREEBSDLL" ;;
-    *) echo "DEFAULT" ;;
+      fi;;
+    SunOS)
+	echo "GNUSOL2LL" ;;
+    FreeBSD)
+	echo "FREEBSDLL" ;;
+    *) 
+	echo "DEFAULT" ;;
 esac
