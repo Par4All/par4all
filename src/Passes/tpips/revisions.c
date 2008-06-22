53f6b37cce0b3db15e0ff79db372bc489a29e4f6
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2008-06-22 15:59:06 +0000

    simpler version.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12886 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/tpips/revisions.c
+++ src/Passes/tpips/revisions.c
@@ -1,11 +1,6 @@
 #include "revisions.h"
 
-#define STRINGIFY_SECOND_STAGE(symbol) #symbol
-#define STRINGIFY(symbol) STRINGIFY_SECOND_STAGE(symbol)
-
 /* could be shared somewhere? */
 char * soft_revisions = 
-  "newgen=" STRINGIFY(NEWGEN_REV) 
-  ", linear=" STRINGIFY(LINEAR_REV) 
-  ", pips=" STRINGIFY(PIPS_REV)
-  ", nlpmake=" STRINGIFY(NLPMAKE_REV);
+  "newgen=" NEWGEN_REV ", linear=" LINEAR_REV
+  ", pips=" PIPS_REV ", nlpmake=" NLPMAKE_REV;
