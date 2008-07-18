fc6857793f3e4c2bec55ca12b1632bd916eb8a42
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2008-07-18 12:56:34 +0000

    parallel01 and 03 added for C parallelization; complexification of one_liner06.c for comment chasing; missing xxx.result/test added
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@121 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/one_liner_06.c
+++ C_syntax/one_liner_06.c
@@ -2,13 +2,14 @@
 void one_liner_06()
 {
   int x,y,z;
-  /*first test*/
+  /* first high-level test*/
   if(x>0)
     y++;
   else if(x>0){
     //to please the parser
     y++;
-  }
+  }  x = 3;
+  /* second high-level test*/
   if(x>0) {
     y++;
     z++;
@@ -17,8 +18,23 @@ void one_liner_06()
     y++;
     z++;
   }
+  /* third high-level test*/
   if(x>0)
-     ;
+    ;
   else if(x>0)
     ;
+  /* first for */
+  for(x=0; x<10; x++)
+    ;
+  if(x>0)
+    ;
+  else
+    /* first while */
+    while(x>0)
+      x--;
+  if(x<0)
+    /* first switch */
+    switch(x) {
+    default: break;
+    }
 }
