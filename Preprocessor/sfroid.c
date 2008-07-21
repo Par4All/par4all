e608a1697ccb770775cf2089d7112d63c9be0db5
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2008-07-21 14:33:25 +0000

    Name mention removed
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@129 67b5017d-e62d-0410-a871-f72b1c62810f

--- Preprocessor/sfroid.c
+++ Preprocessor/sfroid.c
@@ -17,7 +17,6 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 /*
-  $Id$
   `ptmalloc2', a malloc implementation for multiple threads without
   lock contention, by Wolfram Gloger <wg@malloc.de>.
 
