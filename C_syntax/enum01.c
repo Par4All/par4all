c8399e71400225b4ac47006e01fbf2558277ff76
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2008-07-23 12:27:22 +0000

    A few bug fixes and two new validation modules in C_syntax, one for CParserError and one for enum fields
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@134 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ C_syntax/enum01.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+
+main()
+{
+  enum fleurs {rose=0x0001, marguerite=0, jasmin};
+  enum legumes{carotte=rose+50, haricot};
+  enum fleurs ma1, ma2, ma3;
+  enum legumes mon1, mon2;
+  int i;
+
+  ma1 = rose;
+  ma2 = marguerite;
+  ma3 = jasmin;
+  mon1 = carotte;
+  mon2 = haricot;
+
+  i = ma1+ma2+ma3+mon1+mon2;
+
+  // printf("rose=%d, margueritte=%d, jasmin=%d\n", ma1, ma2, ma3);
+  //printf("carotte=%d, haricot=%d\n", mon1, mon2);
+}
