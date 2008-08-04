92d6cf77feb06e242821d484d083097d804f0e27
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2008-08-04 16:20:16 +0000

    Check for C keyword inline, for Fortran formats and for a case sent by Amira Mensi
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@146 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Syntax/format03.f
@@ -0,0 +1,7 @@
+      program format03
+
+C     Check that C extensions do not dmagae Fortran format print-out
+
+ 150   FORMAT(//1X,'*** AVERAGE VALUES AFTER ',I8,' STEPS:'//)           0007
+
+       END
