39a2a3cd50d81d4d33cfeb38d9547fe2b047cd0c
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
2008-08-05 07:08:59 +0000

    cleaner macro definitions
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@910 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- src/genC/newgen_map.h
+++ src/genC/newgen_map.h
@@ -23,25 +23,31 @@
  * These are the functions defined in the Newgen map library. 
  */
 
-#define HASH_GET(start,image,h,k) \
-        hash_map_get((h), (void*)(k))
-#define HASH_BOUND_P(start, image, h, k) \
-        hash_map_defined_p((h), (void*)(k))
-#define HASH_UPDATE(start,image,h,k,v) \
-        hash_map_update((h), (void*)(k), (void*)(v))
-#define HASH_EXTEND(start,image,h,k,v) \
-        hash_map_put((h), (void*)(k), (void*)(v))
-#define HASH_DELETE(start,image,h,k) \
-        hash_map_del((h), (void*)(k))
+#define HASH_GET(start,image,h,k)		\
+  hash_map_get((h), (void*)(k))
+#define HASH_BOUND_P(start, image, h, k)	\
+  hash_map_defined_p((h), (void*)(k))
+#define HASH_UPDATE(start,image,h,k,v)		\
+  hash_map_update((h), (void*)(k), (void*)(v))
+#define HASH_EXTEND(start,image,h,k,v)		\
+  hash_map_put((h), (void*)(k), (void*)(v))
+#define HASH_DELETE(start,image,h,k)		\
+  hash_map_del((h), (void*)(k))
 
-#define FUNCTION_MAP(typename, start, image, k, v, code, fun) \
-    { hash_table _map_hash_h = ((gen_chunk*)fun+1)->h ;\
-      void * _map_hash_p = NULL; \
-      void * _map_k; void * _map_v; \
-      while ((_map_hash_p = \
-	hash_table_scan(_map_hash_h,_map_hash_p,&_map_k,&_map_v))) { \
-        typename##_key_type k = (typename##_key_type)((gen_chunk*)_map_k)->start ; \
-        typename##_value_type v = (typename##_value_type)((gen_chunk*)_map_v)->image;\
-        code ; }}
+#define FUNCTION_MAP(typename, start, image, k, v, code, fun)		\
+  {									\
+    hash_table _map_hash_h = ((gen_chunk*)fun+1)->h ;			\
+    void * _map_hash_p = NULL;						\
+    void * _map_k; void * _map_v;					\
+    while ((_map_hash_p =						\
+	    hash_table_scan(_map_hash_h,_map_hash_p,&_map_k,&_map_v)))	\
+      {									\
+	typename##_key_type k =						\
+	  (typename##_key_type)((gen_chunk*)_map_k)->start ;		\
+	typename##_value_type v =					\
+	  (typename##_value_type)((gen_chunk*)_map_v)->image;		\
+	code ;								\
+      }									\
+  }
 
 #endif /* newgen_map_included */
