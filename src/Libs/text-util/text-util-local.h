e814ece5cee28e3777935a637b29d68ef4ee20cf
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2008-08-05 07:05:59 +0000

    cleaner macro definitions.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@12988 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/text-util/text-util-local.h
+++ src/Libs/text-util/text-util-local.h
@@ -18,24 +18,29 @@ extern bool is_fortran;
 #define MAKE_IWORD(i) i2a(i)
 #define MAKE_FWORD(f) f2a(f)
 
-#define CHAIN_SWORD(l,s) gen_nconc(l,CONS(STRING, MAKE_SWORD(s), NIL))
-#define CHAIN_IWORD(l,i) gen_nconc(l,CONS(STRING, MAKE_IWORD(i), NIL))
-#define CHAIN_FWORD(l,f) gen_nconc(l,CONS(STRING, MAKE_FWORD(f), NIL))
+#define CHAIN_SWORD(l,s) gen_nconc(l, CONS(STRING, MAKE_SWORD(s), NIL))
+#define CHAIN_IWORD(l,i) gen_nconc(l, CONS(STRING, MAKE_IWORD(i), NIL))
+#define CHAIN_FWORD(l,f) gen_nconc(l, CONS(STRING, MAKE_FWORD(f), NIL))
 
-#define MAKE_ONE_WORD_SENTENCE(m, s) \
-    make_sentence(is_sentence_unformatted, \
-		  make_unformatted((char *) NULL, 0, m, CHAIN_SWORD(NIL, s)))
+#define MAKE_ONE_WORD_SENTENCE(m, s)					\
+  make_sentence(is_sentence_unformatted,				\
+		make_unformatted((char *) NULL, 0, m, CHAIN_SWORD(NIL, s)))
 
-#define ADD_SENTENCE_TO_TEXT(t,p) { text _t_ = (t); \
- text_sentences(_t_)=gen_nconc(text_sentences(_t_), CONS(SENTENCE, (p), NIL));}
+#define ADD_SENTENCE_TO_TEXT(t,p)					\
+  {									\
+    text _t_ = (t);							\
+    text_sentences(_t_) =						\
+      gen_nconc(text_sentences(_t_), CONS(SENTENCE, (p), NIL));		\
+  }
 
-#define MERGE_TEXTS(r,t) { \
-       text _r_ = (r); text _t_ = (t); \
-       text_sentences(_r_) = \
-	   gen_nconc(text_sentences(_r_), text_sentences(_t_)); \
-       text_sentences(_t_) = NIL; \
-       free_text(_t_); \
-       }
+#define MERGE_TEXTS(r,t)					\
+  {								\
+    text _r_ = (r); text _t_ = (t);				\
+    text_sentences(_r_) =					\
+      gen_nconc(text_sentences(_r_), text_sentences(_t_));	\
+    text_sentences(_t_) = NIL;					\
+    free_text(_t_);						\
+  }
 
 /* maximum length of a line when prettyprinting...
  * from 0 to 69, i.e. 70 chars, plus "\n\0" 
