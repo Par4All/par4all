c33f988cf64abe5410a5b8cb08838ba3159b6bb0
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2008-08-06 16:32:05 +0000

    C parser: non ANSI comptaible comment (see handling in comment04.test) + non ANSI compatible empty C file (avoid core dump)
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@150 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/decl06.c
+++ C_syntax/decl06.c
@@ -1,8 +1,9 @@
 struct _err_manager
 {
-  char function[500]; /* fonction ou est cree le manager */
-  void (* push)(const char * functions);
+  char function/*[500]*/; /* fonction ou est cree le manager */
+  void (* push)(/* const char * */ int functions);
 } ;
-
+/*
 void ERR_mgr_ctor(const char * file,
 		  const char * function);
+*/
