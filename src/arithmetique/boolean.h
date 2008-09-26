5e51ad16a3740f5bfe41bf362f58f1d0a11b10c0
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
2008-09-26 07:54:03 +0000

    cproto fix, to be tested.
    git-svn-id: http://svn.cri.ensmp.fr/svn/linear/trunk@1261 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/arithmetique/boolean.h
+++ src/arithmetique/boolean.h
@@ -1,5 +1,12 @@
+/* $Id$ */
+
 #ifndef BOOLEAN_INCLUDED
 #define BOOLEAN_INCLUDED
+
+/* put here because should be included before any other header in linear */
+#ifdef CPROTO_ATTRIBUTE_FIX
+#define __attribute__(x) /* nope! */
+#endif /* old cproto attribute fix for Ronan */
 
 typedef enum { false, true } boolean;
 #define	TRUE     true
@@ -7,4 +14,4 @@ typedef enum { false, true } boolean;
 
 #define boolean_string(b) ((b)? "TRUE" : "FALSE")
 
-#endif
+#endif /* BOOLEAN_INCLUDED */
