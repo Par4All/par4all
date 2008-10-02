f15a19b01fe127b3cd906b352cf5f183637afa3c
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2008-10-02 12:30:50 +0000

    Check for operator priorities and for complex declaration
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@186 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/priority01.c
+++ C_syntax/priority01.c
@@ -1,7 +1,7 @@
-int main() {
+int priority01() {
   char a, b, c;
-  a = 1;
-  b = 2;
+  //a = 1;
+  //b = 2;
   /* Should be prettyprint as "(c = a + b) != '\n'"
      and not "c = a + b != '\n'" */
   return (c = a + b) != '\n';
