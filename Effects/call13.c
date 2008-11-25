10afbc2c9a3493d30fb39ad6283a98890015379d
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2008-11-25 17:50:49 +0000

    updates for C effects
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@282 67b5017d-e62d-0410-a871-f72b1c62810f

--- Effects/call13.c
+++ Effects/call13.c
@@ -10,7 +10,7 @@ void call13(int nrec, int nsa, int ntt, Cplfloat ptrin[nrec][nsa],Cplfloat ptrou
   
   R = 0.0;
   I = 0.0;
-  
+
   for(i=0;i<nrec-ntt+1;i++)
     {
      //    MOTIF     
@@ -23,13 +23,12 @@ void call13(int nrec, int nsa, int ntt, Cplfloat ptrin[nrec][nsa],Cplfloat ptrou
 		  for(k2=0;k2<nsa;k2++)
 		    {
 		      R = ptrin[i+j1][j2].re * ptrin[i+k1][k2].re + ptrin[i+j1][j2].im * ptrin[i+k1][k2].im;
-		      //I = - ptrin[i+j1][j2].re * ptrin[i+k1][k2].im + ptrin[i+j1][j2].im * ptrin[i+k1][k2].re;
-		      //ptrout[i][j1][j2][k1][k2].re = R;
-		      //ptrout[i][j1][j2][k1][k2].im = I;
+		      I = - ptrin[i+j1][j2].re * ptrin[i+k1][k2].im + ptrin[i+j1][j2].im * ptrin[i+k1][k2].re;
+		      ptrout[i][j1][j2][k1][k2].re = R;
+		      ptrout[i][j1][j2][k1][k2].im = I;
 		    }
 		}
 	    }
 	}
     }
-  
 }
