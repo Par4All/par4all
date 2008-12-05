c7a05283fcde00ebd58d72089cb451d98556b83d
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2008-12-05 16:41:54 +0000

    Three inlining test cases
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@302 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/inlining01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+static void pmax(int a, int b)
+{
+    if( a > b )
+        return;
+    printf("%d\n",a > b ? a : b);
+}
+
+int main(int argc, char **argv)
+{
+    int d=0;
+    pmax(2,argc);
+    return d;
+}
+
