36bf12cc40c6006b9b87c33ee4bf37c1137e054b
coelho <coelho@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2009-01-28 13:56:20 +0000

    explicit architecture for Darwin/MacOS X
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@939 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/arch.sh
+++ makes/arch.sh
@@ -6,8 +6,8 @@
 #
 
 case `uname -s` in
-    Linux) 
-      if [ `uname -m` = 'x86_64' ] ; then 
+    Linux)
+      if [ `uname -m` = 'x86_64' ] ; then
 	echo "LINUX_x86_64_LL"
       elif [ `uname -m` = 'ppc64' ] ; then
 	echo "LINUX_PPC_64_LL"
@@ -18,6 +18,8 @@ case `uname -s` in
 	echo "GNUSOL2LL" ;;
     FreeBSD)
 	echo "FREEBSDLL" ;;
-    *) 
+    Darwin)
+	echo "MACOSX" ;;
+    *)
 	echo "DEFAULT" ;;
 esac
