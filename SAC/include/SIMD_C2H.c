e5892f7fa2927e45b173337967e72f9c2298caeb
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-02-07 16:13:01 +0000

    update sac testsuite, add c2h testcase
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@340 67b5017d-e62d-0410-a871-f72b1c62810f

--- SAC/include/SIMD_C2H.c
+++ SAC/include/SIMD_C2H.c
@@ -1,4 +1,32 @@
 void
+SIMD_LOAD_V2HI_TO_V2SI(int VEC[2], short BASE[2])
+{
+    VEC[0] = BASE[0];
+    VEC[1] = BASE[1];
+}
+void
+SIMD_SAVE_V2HI_TO_V2SI(short VEC[2], int BASE[2])
+{
+    VEC[0] = BASE[0];
+    VEC[1] = BASE[1];
+}
+void
+SIMD_LOAD_V4QI_TO_V4HI (short VEC[4], char BASE[4])
+{
+    VEC[0] = BASE[0];
+    VEC[1] = BASE[1];
+    VEC[2] = BASE[2];
+    VEC[3] = BASE[3];
+}
+void
+SIMD_LOAD_GENERIC_V2HI (short VEC[4], short X1, short X2)
+{
+
+    VEC[0] = X1;
+    VEC[1] = X2;
+}
+
+void
 SIMD_LOAD_GENERIC_V4HI (short VEC[4], short X1, short X2, short X3, short X4)
 {
 
@@ -6,6 +34,20 @@ SIMD_LOAD_GENERIC_V4HI (short VEC[4], short X1, short X2, short X3, short X4)
     VEC[1] = X2;
     VEC[2] = X3;
     VEC[3] = X4;
+}
+void
+SIMD_SAVE_GENERIC_V2HI (short VEC[4], short X1[1], short X2[1])
+{
+
+    X1 [0]= VEC[0];
+    X2 [0]= VEC[1];
+}
+void
+SIMD_SAVE_V2HI (short VEC[2], short BASE[2])
+{
+
+    BASE [0]= VEC[0];
+    BASE [1]= VEC[1];
 }
 void
 SIMD_SAVE_GENERIC_V4HI (short VEC[4], short X1[1],
@@ -23,6 +65,13 @@ SIMD_SAVE_V4HI (short VEC[4], short BASE[4])
     BASE[1] = VEC[1];
     BASE[2] = VEC[2];
     BASE[3] = VEC[3];
+}
+
+void
+SIMD_LOAD_V2HI (short VEC[2], short BASE[2])
+{
+    VEC[0] = BASE[0];
+    VEC[1] = BASE[1];
 }
 
 void
--- SAC/kernels/C2H/c2h01.c
+++ SAC/kernels/C2H/c2h01.c
@@ -1,6 +1,9 @@
-void average(short* a, short* b, short *c, int n)
+void average(short* a, short* b, short *c)
 {
     int i;
-    for(i=0;i<n;i++)
+    for(i=0;i<100;i++)
+    {
         a[i]=(b[i]/2) + (c[i]/2);
+//        a[i]+=(b[i]/2) + (c[i]/2);
+    }
 }
