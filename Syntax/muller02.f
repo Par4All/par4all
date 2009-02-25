8fe66e595887c40f7267541c996e1f4f8b35c3ad
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-02-25 12:35:04 +0000

    add cases muller02 and muller03 to muller01; bug found by Alain Muller visible with muller03 only; it is not clear to me why Alain bumped into it as the regenerated declarations seem to follow the variable appearance ordre and sonce the appearance ordre is logical and compatible with the declaration dependencies
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@351 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Syntax/muller02.f
@@ -0,0 +1,11 @@
+      SUBROUTINE COMPUTE_INDEXMAP_PARDO1(K, K_L, K_U, KK, NZ, KK2, J
+     &, DIMS, JJ, NY, KJ2, I, II, NX, D, AINDEXMAP)
+      INTEGER NX, II, I, KJ2, NY, JJ, J, KK2, NZ, KK, K_U, K_L, K
+! Check that the declaration of D happens before the declaration of AINDEXMAP
+!      INTEGER INDEXMAP(1:D(1)+1, 1:D(2), 1:D(3)), D(1:3), DIMS(1:3)
+      INTEGER D(1:3), AINDEXMAP(1:D(1)+1, 1:D(2), 1:D(3)), DIMS(1:3)
+
+! Check that i declaration is added
+      new = 1
+
+      end
