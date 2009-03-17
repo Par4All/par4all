2453daf7e3ae21adf1e3591d9ea88cf1478b0454
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
2009-03-17 16:43:55 +0000

    improve readability.
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@964 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- src/genC/newgen_generic_stack.h
+++ src/genC/newgen_generic_stack.h
@@ -1,63 +1,95 @@
 /* Made after the generic current mappings
  * Fabien COELHO, 05/12/94
- * 
+ *
  * $Id$
  */
 
-#define DEFINE_STACK(PREFIX, name, type) \
-static stack name##_stack = stack_undefined; \
-PREFIX void make_##name##_stack(void) \
-{ assert(name##_stack==stack_undefined);\
-  name##_stack = stack_make(type##_domain, 0, 0);}\
-PREFIX void free_##name##_stack(void) \
-{ stack_free(&name##_stack); \
-  name##_stack = stack_undefined;}\
-PREFIX stack get_##name##_stack(void) \
-{ return(name##_stack);}\
-PREFIX void set_##name##_stack(stack s) \
-{ assert(name##_stack==stack_undefined);\
-  name##_stack = s;}\
-PREFIX void reset_##name##_stack(void)\
-{ assert(name##_stack!=stack_undefined);\
-  name##_stack = stack_undefined;}\
-PREFIX void name##_push(type i)\
-{ stack_push((char *)i, name##_stack);}\
-PREFIX bool name##_filter(type i)\
-{ stack_push((char *)i, name##_stack);\
-  return TRUE;}\
-PREFIX void name##_rewrite(type i)\
-{ assert((type)stack_pop(name##_stack)==i);}\
-PREFIX type name##_replace(type i)\
-{ return((type) stack_replace((char *)i, name##_stack));}\
-PREFIX type name##_pop()\
-{ return((type) stack_pop(name##_stack));}\
-PREFIX type name##_head()\
-{ return((type) stack_head(name##_stack));}\
-PREFIX bool name##_empty_p()\
-{ return(stack_empty_p(name##_stack));}\
-PREFIX int name##_size()\
-{ return(stack_size(name##_stack));}\
-PREFIX void error_reset_##name##_stack(void)\
-{ name##_stack = stack_undefined;} \
-static void check_##name##_stack()\
-{ stack s = get_##name##_stack();\
-  char *item_1 = (char *) check_##name##_stack,\
-       *item_2 = (char *) get_##name##_stack;\
-  reset_##name##_stack();\
-  make_##name##_stack();\
-  assert(name##_empty_p());\
-  name##_push((type) item_1);\
-  assert((char *) name##_head()==item_1);\
-  name##_replace((type) item_2);\
-  assert((char *) name##_pop()==item_2);\
-  assert(name##_filter((type) item_1));\
-  name##_rewrite((type) item_1);\
-  assert(name##_size()==0);\
-  free_##name##_stack();\
-  reset_##name##_stack();\
-  error_reset_##name##_stack();\
-  set_##name##_stack(s);\
-}
+#define DEFINE_STACK(PREFIX, name, type)			\
+  /* the stack */						\
+  static stack name##_stack = stack_undefined;			\
+  /* its functions */						\
+  PREFIX void make_##name##_stack(void)				\
+  {								\
+    assert(name##_stack==stack_undefined);			\
+    name##_stack = stack_make(type##_domain, 0, 0);		\
+  }								\
+  PREFIX void free_##name##_stack(void)				\
+  {								\
+    stack_free(&name##_stack);					\
+    name##_stack = stack_undefined;				\
+  }								\
+  PREFIX stack get_##name##_stack(void)				\
+  {								\
+    return name##_stack;					\
+  }								\
+  PREFIX void set_##name##_stack(stack s)			\
+  {								\
+    assert(name##_stack==stack_undefined);			\
+    name##_stack = s;						\
+  }								\
+  PREFIX void reset_##name##_stack(void)			\
+  {								\
+    assert(name##_stack!=stack_undefined);			\
+    name##_stack = stack_undefined;				\
+  }								\
+  PREFIX void name##_push(type i)				\
+  {								\
+    stack_push((void *)i, name##_stack);			\
+  }								\
+  PREFIX bool name##_filter(type i)				\
+  {								\
+    stack_push((void *)i, name##_stack);			\
+    return TRUE;						\
+  }								\
+  PREFIX void name##_rewrite(type i)				\
+  {								\
+    assert((type)stack_pop(name##_stack)==i);			\
+  }								\
+  PREFIX type name##_replace(type i)				\
+  {								\
+    return (type) stack_replace((void *)i, name##_stack);	\
+  }								\
+  PREFIX type name##_pop(void)					\
+  {								\
+    return (type) stack_pop(name##_stack);			\
+  }								\
+  PREFIX type name##_head(void)					\
+  {								\
+    return (type) stack_head(name##_stack);			\
+  }								\
+  PREFIX bool name##_empty_p(void)				\
+  {								\
+    return stack_empty_p(name##_stack);				\
+  }								\
+  PREFIX int name##_size(void)					\
+  {								\
+    return stack_size(name##_stack);				\
+  }								\
+  PREFIX void error_reset_##name##_stack(void)			\
+  {								\
+    name##_stack = stack_undefined;				\
+  }								\
+  static void check_##name##_stack(void)			\
+  {								\
+    stack s = get_##name##_stack();				\
+    void							\
+      *item_1 = (void *) check_##name##_stack,			\
+      *item_2 = (void *) get_##name##_stack;			\
+    reset_##name##_stack();					\
+    make_##name##_stack();					\
+    assert(name##_empty_p());					\
+    name##_push((type) item_1);					\
+    assert((void *) name##_head()==item_1);			\
+    name##_replace((type) item_2);				\
+    assert((void *) name##_pop()==item_2);			\
+    assert(name##_filter((type) item_1));			\
+    name##_rewrite((type) item_1);				\
+    assert(name##_size()==0);					\
+    free_##name##_stack();					\
+    reset_##name##_stack();					\
+    error_reset_##name##_stack();				\
+    set_##name##_stack(s);					\
+  }
 
 #define DEFINE_LOCAL_STACK(name, type) DEFINE_STACK(static, name, type)
 #define DEFINE_GLOBAL_STACK(name, type) DEFINE_STACK(extern, name, type)
