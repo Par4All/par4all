dbf7e92b558065bde6844ce365770e523ee46b2a
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-04-02 13:23:13 +0000

    add example to illustrate 115
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@394 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Prettyprint/omp.c
@@ -0,0 +1,23 @@
+int main () {
+  float a[10][10][10][10][10];
+  int i,j,k,l,m;
+  float x = 2.12;
+
+  for (i = 0; i < 10; i++) {
+    for (j = 2; j < 7; j++) {
+      for (k = 4; k < 3; k++) {
+	for (l = 2; l < 10; l++) {
+	  for (m = 0; m < 10; m++) {
+	    float y;
+	    float w = 1.0;
+	    float z=2.32 + y;
+	    y = 3.5 + x;	    
+	    w=1.2 + z;
+	    a[i][j][k][l][m] = x*y+x+z;
+	  }
+	}
+      }
+    }
+  }
+  return 0;
+}
