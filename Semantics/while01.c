a6378c872485506b700071f57498c8b15f60e3a7
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-04-14 16:06:56 +0000

    test cases added to investigate claim by Ronan Keryell that preconditions are not precise in while loops; not such thing observable here, but the partial evaluation of while loop conditions was bugged; partial evaluation of conditions may also require additional work
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@420 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Semantics/while01.c
@@ -0,0 +1,16 @@
+/* Easiest case, because the loop is always entered */
+
+int while01()
+{
+  int i, j, n;
+
+  i = 0;
+  j = 1;
+  n = 10;
+
+  while(j<n) {
+    i++;
+    j += 2;
+  }
+  return i+j;
+}
