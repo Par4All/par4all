2674c5660990e9ed29aeca1227fca43546c786f4
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
2009-04-20 09:54:58 +0000

    + dup option.
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@975 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- src/genC/newgen_string_buffer.h
+++ src/genC/newgen_string_buffer.h
@@ -12,6 +12,7 @@ string_buffer string_buffer_make(void);
 void string_buffer_free(string_buffer *, bool);
 string string_buffer_to_string(string_buffer);
 void string_buffer_append(string_buffer, string);
+void string_buffer_append_dup(string_buffer, string);
 
 #endif /* STRING_BUFFER_INCLUDED */
 
--- src/genC/string_buffer.c
+++ src/genC/string_buffer.c
@@ -6,50 +6,55 @@
 
 typedef struct __string_buffer_head
 {
-	stack ins;
+  stack ins;
 }
   _string_buffer_head;
 
 string_buffer string_buffer_make(void)
 {
-	string_buffer n = (string_buffer) malloc(sizeof(_string_buffer_head));
-	message_assert("n allocated", n!=NULL);
-	n->ins = stack_make(0, 0, 0);
-	return n;
+  string_buffer n = (string_buffer) malloc(sizeof(_string_buffer_head));
+  message_assert("n allocated", n!=NULL);
+  n->ins = stack_make(0, 0, 0);
+  return n;
 }
 
 void string_buffer_free(string_buffer *psb, bool free_strings)
 {
-	if (free_strings)
-		STACK_MAP_X(s, string, free(s), (*psb)->ins, 0);
-	stack_free(&((*psb)->ins));
-	free(*psb);
-	*psb = NULL;
+  if (free_strings)
+    STACK_MAP_X(s, string, free(s), (*psb)->ins, 0);
+  stack_free(&((*psb)->ins));
+  free(*psb);
+  *psb = NULL;
 }
 
 string string_buffer_to_string(string_buffer sb)
 {
-	int bufsize = 0, current = 0;
-	char * buf = NULL;
+  int bufsize = 0, current = 0;
+  char * buf = NULL;
 
-	STACK_MAP_X(s, string, bufsize+=strlen(s), sb->ins, 0);
+  STACK_MAP_X(s, string, bufsize+=strlen(s), sb->ins, 0);
 
-	buf = (char*) malloc(sizeof(char)*(bufsize+1));
-	buf[current] = '\0';
+  buf = (char*) malloc(sizeof(char)*(bufsize+1));
+  buf[current] = '\0';
 
-	STACK_MAP_X(s, string, 
-	{
-		int len = strlen(s);
-		(void) memcpy(&buf[current], s, len);
-		current += len;
-		buf[current] = '\0';
-	},
-				sb->ins, 0);
-	
-	return buf;
+  STACK_MAP_X(s, string,
+  {
+    int len = strlen(s);
+    (void) memcpy(&buf[current], s, len);
+    current += len;
+    buf[current] = '\0';
+  },
+	      sb->ins, 0);
+
+  return buf;
 }
 
 void string_buffer_append(string_buffer sb, string s)
 {
-	stack_push(s, sb->ins);
+  stack_push(s, sb->ins);
+}
+
+void string_buffer_append_dup(string_buffer sb, string s)
+{
+  string_buffer_append_dup(sb, strdup(s));
 }
