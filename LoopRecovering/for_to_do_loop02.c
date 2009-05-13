d5277b5e325db23db5b09a9e4231d87ce8d24e5e
keryell <keryell@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-05-13 16:23:13 +0000

    Added new example that makes transformers to fail with [transformer_argument_general_consistency_p] ([...]pips/src/Libs/transformer/basic.c:349) assertion failed
     'Empty transformer must have no arguments' not verified
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@441 67b5017d-e62d-0410-a871-f72b1c62810f

--- LoopRecovering/for_to_do_loop02.c
+++ LoopRecovering/for_to_do_loop02.c
@@ -2,9 +2,6 @@ int main() {
   int a[100];
   int i, j;
 
-  for(i = 2; i <= 50; i = 2 + 1)
-    a[i] = 2;
-
   // A do loop ! Should be parallel
   for(i = 2; i <= 50; i = i + 1)
     a[i] = 2;
@@ -16,6 +13,9 @@ int main() {
   // Should be parallel but for->do here not implemented yet...
   for(j = 60; j > 10; j = j - 3)
     a[j] = 2;
+
+  for(i = 2; i <= 50; i = 2 + 1)
+    a[i] = 2;
 
   return 0;
 }
