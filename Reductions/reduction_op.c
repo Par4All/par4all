f79eb1b792268878ef2dba88931668f703b7f71f
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-05-14 13:37:38 +0000

    new validation file to test reduction and effects on different operators + modif validation because reductions comments in fortran are now "C" instead of "!"
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@443 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Reductions/reduction_op.c
@@ -0,0 +1,87 @@
+int main () {
+  int b = 0;
+  int i = 0;
+  float y = 10.0;
+  float x = 1.0;
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b = b + i;
+  }
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b = b * i;
+  }
+
+  // b should not be reduced here
+  // because "/" on int is not accepted
+  for (i=0; i < 100; i++) {
+    b = b / i;
+  }
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b = b & i;
+  }
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b = b ^ i;
+  }
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b = b | i;
+  }
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b += i;
+  }
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b *= i;
+  }
+
+  // b should not be reduced here
+  // because "/" on int is not accepted
+  for (i=0; i < 100; i++) {
+    b /= i;
+  }
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b -= i;
+  }
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b &= i;
+  }
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b |= i;
+  } 
+
+  // b should be reduced here
+  for (i=0; i < 100; i++) {
+    b ^= i;
+  }
+
+  // b should be reduced here
+  // because "/" on float is accepted
+  for (i=0; i < 100; i++) {
+    y = y / x;
+  }
+
+  // b should be reduced here
+  // because "/" on float is accepted
+  for (i=0; i < 100; i++) {
+    y = y / x;
+  }
+
+  return 0;
+}
