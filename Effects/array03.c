17feae2e6e11995866174352404f053a1f757de0
creusillet <creusillet@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-05-19 14:02:23 +0000

    add some test cases for the new representation of effects and regions.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@458 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Effects/array03.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 5
+#define M 3
+
+
+int foo(float (*b)[M])
+{
+  float c;
+  (*b)[3] = 2.0;
+  c = (*b)[3];
+  b[1][3] = 2.0;
+  c = b[1][3];
+  
+  ((*b)[3])++;
+  (*b)[3] += 5.0;
+  (b[1][3])++;
+  b[1][3] += 5.0;
+
+  return (1);
+}
+
+int foo2(float b[N][M])
+{ 
+  float c;
+  (*b)[3] = 2.0;
+  c = (*b)[3];
+  b[1][3] = 2.0;
+  c = b[1][3];
+  
+  ((*b)[3])++;
+  (*b)[3] += 5.0;
+  (b[1][3])++;
+  b[1][3] += 5.0;
+
+  return 1;
+}
+
+int main() 
+{
+  float a[N][M], ret;
+  
+  ret = foo(a);
+  ret = foo2(a);
+  
+  return 1;
+}
