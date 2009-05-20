b378e9943f0db5b573cb989bd01b95f89992a872
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-05-20 16:50:11 +0000

    show something.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@464 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia_00.c
+++ HardwareAccelerator/freia_00.c
@@ -2,13 +2,16 @@ typedef struct __freia_data_2d;
 typedef struct __freia_data_2d * freia_data_2d;
 typedef int freia_error;
 
-freia_error freia_api_add(freia_data_2d o, freia_data_2d i0, freia_data_2d i1);
-freia_error freia_api_sub(freia_data_2d o, freia_data_2d i0, freia_data_2d i1);
+extern freia_error
+freia_aipo_add(freia_data_2d o, freia_data_2d i0, freia_data_2d i1);
+extern freia_error
+freia_aipo_sub(freia_data_2d o, freia_data_2d i0, freia_data_2d i1);
 
-freia_error freia_stuff(freia_data_2d o0, freia_data_2d o1,
-			freia_data_2d i0, freia_data_2d i1)
+freia_error
+freia_stuff(freia_data_2d o0, freia_data_2d o1,
+	    freia_data_2d i0, freia_data_2d i1)
 {
-  freia_api_add(o0, i0, i1);
-  freia_api_sub(o1, o0, i1);
+  freia_aipo_add(o0, i0, i1);
+  freia_aipo_sub(o1, o0, i1);
   return 0;
 }
