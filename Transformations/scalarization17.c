36ccdc419a09009f17953a8a91a7d7747689a671
daverio <daverio@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-05-28 09:10:12 +0000

    Added a test which show a problem with C Pretty Printer (PRIVATE declaration) and maybe, additionally, an internal representation bug.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@490 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/scalarization17.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int func(int n)
+{
+  int x[n], y[n][n], t[n];
+  int i, j;
+
+  for (i=0 ; i < n ; i++) {
+    x[i] = i;
+    for (j=0 ; j < n ; j++)
+      y[i][j] = x[i] ^ 2;
+      y[i][j] = x[i] + j;
+  }
+  return y[n][n];
+}
+
+int main(int argc, char **argv)
+{
+  printf("%d\n", func(5));
+}
+#define SIZE 100
