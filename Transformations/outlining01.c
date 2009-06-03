49334fab96e073aeed30c886b002258ac5a328e1
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-06-03 15:19:47 +0000

    add outlining validation
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@518 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/outlining01.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int n;
+	char * str;
+	if(argc == 1 ) return 1;
+	n=atoi(argv[1]);
+	if( !(str=malloc(n*sizeof(char))) ) return 2;
+	{
+		int i;
+		for(i=0;i<(n-1);i++)
+			str[i]=('a'+(char)i);
+		str[n-1]=0;
+	}
+	printf("%s\n",str);
+	return 0;
+}
