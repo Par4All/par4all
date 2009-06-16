43f933ae543a4e2a65ed36819d3cab290c658ded
creusillet <creusillet@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-06-16 11:44:29 +0000

    test case array03 modified, and result/test also modified accordingly to expected results. There are still problems due to the internal representation of the formal argument of foo2.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@551 67b5017d-e62d-0410-a871-f72b1c62810f

--- Effects/array03.c
+++ Effects/array03.c
@@ -4,6 +4,7 @@
 #define N 5
 #define M 3
 
+float d[N][M];
 
 int foo(float (*b)[M])
 {
@@ -37,12 +38,31 @@ int foo2(float b[N][M])
   return 1;
 }
 
+int foo3()
+{ 
+  float c;
+  (*d)[3] = 2.0;
+  c = (*d)[3];
+  d[1][3] = 2.0;
+  c = d[1][3];
+  
+  ((*d)[3])++;
+  (*d)[3] += 5.0;
+  (d[1][3])++;
+  d[1][3] += 5.0;
+
+  return 1;
+}
+
+
+
 int main() 
 {
   float a[N][M], ret;
   
   ret = foo(a);
   ret = foo2(a);
+  ret = foo3();
   
   return 1;
 }
