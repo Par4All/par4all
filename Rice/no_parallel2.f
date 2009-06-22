4ee0915008d3e8088c3c138f1b04c3319a93b38e
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-06-22 07:19:22 +0000

    simplify the test case
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@573 67b5017d-e62d-0410-a871-f72b1c62810f

--- Rice/no_parallel2.f
+++ Rice/no_parallel2.f
@@ -1,29 +1,18 @@
       PROGRAM NO_PARALLEL2
-      INTEGER I,J,K,L,M
+      INTEGER K
 
-      REAL*8 R(10,10,10,10)
       real*8 R2
+
+      R2 = 1500.0
 
       DO WHILE (R2.GT.0.0D0)
          DO K = 1, 10
-            DO J = 1, 10
-               DO I = 1, 10
-                  DO L = 1, 10
-                     R2 = R2+R(L,I,J,K)*R(L,I,J,K)
-                  ENDDO
-               ENDDO
-            ENDDO
+            R2 = R2-5.0
          ENDDO
       ENDDO
 
       DO K = 1, 10
-         DO J = 1, 10
-            DO I = 1, 10
-               DO L = 1, 10
-                  R2 = R2+R(L,I,J,K)*R(L,I,J,K)
-               ENDDO
-            ENDDO
-         ENDDO
+         R2 = R2+5.0
       ENDDO
 
       write(10,*) 'result =', R2
