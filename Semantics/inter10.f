c491aeb5c63baf96ad3a85a34a23123659a316ad
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-07-06 07:10:50 +0000

    validation status back to normal: a few bugs only
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@596 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics/inter10.f
+++ Semantics/inter10.f
@@ -1,4 +1,12 @@
+C     Since i is not used by foo, the precondition i==5 will not be
+C     displayed as it is rightly assumed to be useless. Uncomment the
+C     "print *, i" to see the precondition.
+
+C     It is not clear if this feature is helpful, or too surprising for
+C     a standard PIPS user who will assume an internal error in PIPS
+C     when not seeing information about i.
       subroutine foo(i)
+C      print *, i
       end
 
       integer function inter10(i)
