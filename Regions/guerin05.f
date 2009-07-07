af42248876103e54ab50a9bcc9ca1d631022ff48
creusillet <creusillet@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-07-07 09:25:19 +0000

    change Regions/guerin05 test case afer bug fixed (ticket #121) in rev. 14385.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@600 67b5017d-e62d-0410-a871-f72b1c62810f

--- Regions/guerin05.f
+++ Regions/guerin05.f
@@ -1,9 +1,7 @@
       program guerin05
 
-C     Bugs in IN and OUT scalar regions for the specified sections:
-C     scalar k is not seen as imported by the loop, although it is
-C     imported by the loop body. But, if k is replaced by k(1), k(1) is
-C     seen as simported.
+C     Bug fixed in IN scalar regions for the specified sections:
+C     scalar k is now correctly seen as imported by the loop.
 
       implicit none
 
@@ -13,5 +11,7 @@ C     seen as simported.
       do i = 1, 1
          k = k + 1
       enddo
+
+      print *,k
 
       end
