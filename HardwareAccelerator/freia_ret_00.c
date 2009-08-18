6acc77a9679f92e9c21dda448bf12c61930b7ea6
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-08-18 17:04:58 +0000

    handle returned states in some cases.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@812 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia_ret_00.c
+++ HardwareAccelerator/freia_ret_00.c
@@ -7,6 +7,6 @@ freia_ret_00(freia_data2d * o, freia_data2d * i0, freia_data2d * i1)
   freia_data2d * t = freia_common_create_data(16, 128, 128);
   ret = freia_aipo_addsat(t, i0, i1);
   ret |= freia_aipo_sub(o, i0, t);
-  ret |= freia_common_destruct_data(t);
+  freia_common_destruct_data(t);
   return ret;
 }
