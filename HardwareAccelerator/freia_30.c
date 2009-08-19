658348ae5434cc46da49e68374d35acb920e4c07
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-08-19 17:57:45 +0000

    some more parallel reuse examples.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@833 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ HardwareAccelerator/freia_30.c
@@ -0,0 +1,13 @@
+#include "freia.h"
+
+freia_status
+freia_30(freia_data2d * o0, freia_data2d * o1, freia_data2d * i)
+{
+  // parallel reuses (similar to freia_27)
+  // o0 = f(i)
+  // o1 = i | i
+  freia_aipo_threshold(o0, i, 10, 20, true);
+  freia_aipo_or(o1, i, i);
+
+  return FREIA_OK;
+}
