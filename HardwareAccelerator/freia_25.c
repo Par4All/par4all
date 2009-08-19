711f63196642368f04aeb5344c73e004f7ce7207
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-08-19 16:42:47 +0000

    parallel reuses...
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@831 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ HardwareAccelerator/freia_25.c
@@ -0,0 +1,13 @@
+#include "freia.h"
+
+freia_status
+freia_25(freia_data2d * o0, freia_data2d * o1, freia_data2d * i)
+{
+  // parallel reuses
+  // o0 = i + i
+  // o1 = i * i
+  freia_aipo_add(o0, i, i);
+  freia_aipo_mul(o1, i, i);
+
+  return FREIA_OK;
+}
---
+++ HardwareAccelerator/freia_27.c
@@ -0,0 +1,13 @@
+#include "freia.h"
+
+freia_status
+freia_27(freia_data2d * o0, freia_data2d * o1, freia_data2d * i, int32_t * k)
+{
+  // parallel reuses
+  // o0 = i & i
+  // o1 = f(i)
+  freia_aipo_and(o0, i, i);
+  freia_aipo_erode_8c(o1, i, k);
+
+  return FREIA_OK;
+}
