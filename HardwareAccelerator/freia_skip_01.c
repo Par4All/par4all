dc963273f7dbb11cf73f8fda6b1ddf772adb3406
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-08-20 12:21:21 +0000

    better handling of scalar deps, which removes empty dags that where output by the splitting algorithm.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@842 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia_skip_01.c
+++ HardwareAccelerator/freia_skip_01.c
@@ -3,7 +3,7 @@
 freia_status
 freia_skip_01(freia_data2d * o, freia_data2d * i)
 {
-  freia_status ret;
+  freia_status ret = FREIA_OK;
   freia_data2d * t = freia_common_create_data(16, 128, 128);
   int32_t max, seuil;
   freia_aipo_global_max(i, &max);
