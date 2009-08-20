1ef1d79bd6b3805c09ca1047453016e4b2a352c5
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-08-20 12:37:36 +0000

    one more scalar dependence test.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@843 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia_scalar_02.c
+++ HardwareAccelerator/freia_scalar_02.c
@@ -6,7 +6,7 @@ freia_status
   freia_data2d * tmp = freia_common_create_data(16, 128, 128);
   // dependency with a dereferencement
   freia_aipo_add(tmp, i0, i1);
-  freia_aipo_max(tmp, pmax);
+  freia_aipo_global_max(tmp, pmax);
   freia_aipo_threshold(o, tmp, 10, *pmax, false);
   freia_common_destruct_data(tmp);
   return FREIA_OK;
