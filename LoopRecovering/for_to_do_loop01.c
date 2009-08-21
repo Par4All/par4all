29e8ee97db59bad3b1ec5a5d884a1a5f10c912ed
keryell <keryell@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-08-21 17:26:49 +0000

    Update and new test case around for() to while() loops.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@877 67b5017d-e62d-0410-a871-f72b1c62810f

--- LoopRecovering/for_to_do_loop01.c
+++ LoopRecovering/for_to_do_loop01.c
@@ -6,6 +6,8 @@ int main() {
   for(i = 2; i < 100; i += 1)
      a[i] = 2;
 
+  /* The following code should not prevent the previous loop to be
+     parallel: */
   // Should not be a do loop because j is not the expected index !
   for(i = 2; i <= 50; j += 1)
      a[i] = 2;
