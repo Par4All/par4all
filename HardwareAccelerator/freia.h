0aea577dd1080140a90907f2a928e88121386d7d
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-08-22 06:40:04 +0000

    add const where appropriate
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@886 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia.h
+++ HardwareAccelerator/freia.h
@@ -1,4 +1,8 @@
+// $Id$
 // Minimal self contained types for these headers
+
+#ifndef _freia_h_included_
+#define _freia_h_included_
 #ifndef NULL
 #define NULL ((void*)0)
 #endif // NULL
@@ -25,58 +29,59 @@ extern freia_status freia_common_destruct_data(freia_data2d *);
 // IO
 extern freia_status freia_common_open_input(freia_dataio *, uint32_t);
 extern freia_status freia_common_open_output(freia_dataio *, uint32_t, uint32_t, uint32_t, uint32_t);
-extern freia_status freia_common_rx_image(freia_data2d *, freia_dataio *);
+extern freia_status freia_common_rx_image(const freia_data2d *, freia_dataio *);
 extern freia_status freia_common_tx_image(freia_data2d *, freia_dataio *);
-extern freia_status freia_common_close_input(freia_data2d *);
-extern freia_status freia_common_close_output(freia_data2d *);
+extern freia_status freia_common_close_input(freia_dataio *);
+extern freia_status freia_common_close_output(freia_dataio *);
 
 // 2 CIPO functions
-extern freia_status freia_cipo_gradient(freia_data2d *, freia_data2d *, int32_t, uint32_t);
-extern freia_status freia_cipo_inner_gradient(freia_data2d *, freia_data2d *, int32_t, uint32_t);
+extern freia_status freia_cipo_gradient(freia_data2d *, const freia_data2d *, int32_t, uint32_t);
+extern freia_status freia_cipo_inner_gradient(freia_data2d *, const freia_data2d *, int32_t, uint32_t);
 
 // AIPO function definitions
 // Arithmetic
-extern freia_status freia_aipo_inf(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_inf_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_sup(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_sup_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_sub(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_sub_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_subsat(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_subsat_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_add(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_add_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_addsat(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_addsat_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_absdiff(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_absdiff_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_mul(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_mul_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_div(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_div_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_and(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_and_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_or(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_or_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_xor(freia_data2d *, freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_xor_const(freia_data2d *, freia_data2d *, int32_t);
-extern freia_status freia_aipo_not(freia_data2d *, freia_data2d *);
+extern freia_status freia_aipo_inf(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_inf_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_sup(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_sup_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_sub(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_sub_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_subsat(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_subsat_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_add(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_add_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_addsat(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_addsat_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_absdiff(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_absdiff_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_mul(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_mul_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_div(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_div_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_and(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_and_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_or(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_or_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_xor(freia_data2d *, const freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_xor_const(freia_data2d *, const freia_data2d *, int32_t);
+extern freia_status freia_aipo_not(freia_data2d *, const freia_data2d *);
 // Linear
-extern freia_status freia_aipo_convolution(freia_data2d *, freia_data2d *, int32_t *, uint32_t, uint32_t);
-extern freia_status freia_aipo_fast_correlation(freia_data2d *, freia_data2d *, freia_data2d *, uint32_t);
+extern freia_status freia_aipo_convolution(freia_data2d *, const freia_data2d *, int32_t *, uint32_t, uint32_t);
+extern freia_status freia_aipo_fast_correlation(freia_data2d *, const freia_data2d *, const freia_data2d *, uint32_t);
 // Measure
-extern freia_status freia_aipo_global_min(freia_data2d *, int32_t *);
-extern freia_status freia_aipo_global_max(freia_data2d *, int32_t *);
-extern freia_status freia_aipo_global_min_coord(freia_data2d *, int32_t *, int32_t *, int32_t *);
-extern freia_status freia_aipo_global_max_coord(freia_data2d *, int32_t *, int32_t *, int32_t *);
-extern freia_status freia_aipo_global_vol(freia_data2d *, int32_t *);
+extern freia_status freia_aipo_global_min(const freia_data2d *, int32_t *);
+extern freia_status freia_aipo_global_max(const freia_data2d *, int32_t *);
+extern freia_status freia_aipo_global_min_coord(const freia_data2d *, int32_t *, int32_t *, int32_t *);
+extern freia_status freia_aipo_global_max_coord(const freia_data2d *, int32_t *, int32_t *, int32_t *);
+extern freia_status freia_aipo_global_vol(const freia_data2d *, int32_t *);
 // Misc
-extern freia_status freia_aipo_copy(freia_data2d *, freia_data2d *);
-extern freia_status freia_aipo_cast(freia_data2d *, freia_data2d *);
+extern freia_status freia_aipo_copy(freia_data2d *, const freia_data2d *);
+extern freia_status freia_aipo_cast(freia_data2d *, const freia_data2d *);
 extern freia_status freia_aipo_set_constant(freia_data2d *, int32_t);
-extern freia_status freia_aipo_threshold(freia_data2d *, freia_data2d *, int32_t, int32_t, bool);
+extern freia_status freia_aipo_threshold(freia_data2d *, const freia_data2d *, int32_t, int32_t, bool);
 // Morpho
-extern freia_status freia_aipo_erode_8c(freia_data2d *, freia_data2d *, int32_t *);
-extern freia_status freia_aipo_dilate_8c(freia_data2d *, freia_data2d *, int32_t *);
-extern freia_status freia_aipo_erode_6c(freia_data2d *, freia_data2d *, int32_t *);
-extern freia_status freia_aipo_dilate_6c(freia_data2d *, freia_data2d *, int32_t *);
+extern freia_status freia_aipo_erode_8c(freia_data2d *, const freia_data2d *, const int32_t *);
+extern freia_status freia_aipo_dilate_8c(freia_data2d *, const freia_data2d *, const int32_t *);
+extern freia_status freia_aipo_erode_6c(freia_data2d *, const freia_data2d *, const int32_t *);
+extern freia_status freia_aipo_dilate_6c(freia_data2d *, const freia_data2d *, const int32_t *);
+#endif // _freia_h_included_
