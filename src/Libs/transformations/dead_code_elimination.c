e34abb50fa9878f7e5388b2947c688d6c9cc92e0
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2009-08-24 12:09:48 +0000

    reverse to previous behavior where only c_modules are cleaned
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@15093 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformations/dead_code_elimination.c
+++ src/Libs/transformations/dead_code_elimination.c
@@ -1088,8 +1088,9 @@ statement_write_effect_p(statement s)
 void statement_remove_useless_label(statement s)
 {
   instruction i = statement_instruction(s);
-  if(!instruction_unstructured_p(i)
-     && !( instruction_loop_p(i) || instruction_whileloop_p(i) || instruction_forloop_p(i))
+  if(!instruction_unstructured_p(i) &&
+        c_module_p(get_current_module_entity()) &&
+        !( instruction_loop_p(i) || instruction_whileloop_p(i) || instruction_forloop_p(i))
      ) {
     if( !entity_empty_label_p( statement_label(s)) && !fortran_return_statement_p(s) ) {
       /* SG should free_entity ? */
