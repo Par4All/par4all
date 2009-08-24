4d9d4aef9647d718566603ba6bf0a0c0516b68f2
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-08-24 13:53:21 +0000

    inlining23 example.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@912 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/inlining23.c
@@ -0,0 +1,19 @@
+int foo(void)
+{
+  int foo_int = 1;
+  return foo_int;
+}
+
+int bla(void)
+{
+  int f = foo();
+  int bla_int = f + 2;
+  return bla_int;
+}
+
+int main(void)
+{
+  int f = foo();
+  int b = bla();
+  return 0;
+}
