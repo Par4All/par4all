81993a64d78ab2e2bf502983261eb27a41c1980e
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-08-25 15:15:29 +0000

    obscure issue with declarations.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@931 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/inlining26.c
@@ -0,0 +1,22 @@
+int erode(void)
+{
+  return 0;
+}
+
+int dilate(void)
+{
+  int i;
+  if (0) {
+  }
+  else
+    return 1;
+
+  return 0;
+}
+
+int gradient(void) {
+  dilate();
+  erode();
+  return 0;
+}
+
