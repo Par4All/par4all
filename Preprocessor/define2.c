1b1562770ebba1cda62e73b3620b00c16b65d146
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-09-01 09:02:37 +0000

    It is a prettyprinter issue.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1025 67b5017d-e62d-0410-a871-f72b1c62810f

--- Preprocessor/define2.c
+++ Preprocessor/define2.c
@@ -1,10 +1,15 @@
 
 #define INDICE(x,y) (x+y)
+#define INDICE2(x,y) ((x)+(y))
 
 main()
 {
   int tmp;
   tmp = INDICE (+1,-1);
   tmp = INDICE (-1,+1);
+  tmp = INDICE2(+1,-1);
+  tmp = INDICE2(-1,+1);
+  tmp = (+1 +-1);
+  tmp = (-1 + +1);
   return tmp;
 }
