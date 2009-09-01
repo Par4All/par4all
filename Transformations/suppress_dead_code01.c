6ac28339b0253e66b91c0bb4958ae867fb4e4e1a
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-09-01 15:18:24 +0000

    updates due to malloc effects and to declaration of undeclared functions
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1044 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/suppress_dead_code01.c
+++ Transformations/suppress_dead_code01.c
@@ -5,13 +5,19 @@ void suppress_dead_code01(int *x)
 {
   char * p;
 
+  /* Not dead code because of memory read-write effects, even if
+     masked because the implementation is thread-safe. */
+  p = malloc(10);
+  free(p);
+
   /* Not dead code because of control effect */
   while(1) {
     ;
   }
 
   /* Not dead code because of memory read-write effects, even if
-     masked because the implementation is thread-safe. */
+     masked because the implementation is thread-safe, but dead
+     because the previous loop never terminates. */
   p = malloc(10);
   free(p);
 }
