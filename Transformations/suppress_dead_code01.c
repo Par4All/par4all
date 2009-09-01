d1fb9b9800c866ec0a028197dc210c5a96d1edaf
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-09-01 08:28:35 +0000

    test case for suppress_dead_code: control effects and hidden effects must be taken into account
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1023 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/suppress_dead_code01.c
@@ -0,0 +1,17 @@
+extern void * malloc(int);
+extern void free(void *);
+
+void suppress_dead_code01(int *x)
+{
+  char * p;
+
+  /* Not dead code because of control effect */
+  while(1) {
+    ;
+  }
+
+  /* Not dead code because of memory read-write effects, even if
+     masked because the implementation is thread-safe. */
+  p = malloc(10);
+  free(p);
+}
