8792a4e0b45201a85505d5931d83bd759045f1e3
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-09-08 09:49:54 +0000

    two new test cases suggested by Serge Guelton; both give correct results, but the second one should have more precise results
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1152 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ C_syntax/initialization06.c
@@ -0,0 +1,15 @@
+/* Check what happens with a name conflict solved with a scope. The
+   internal a should be a copy of the parameter and the function
+   should return a+1, with a its formal parameter. It does not
+   work. The problem may occurs within the parser because a is
+   declared before the initialization expression is parsed.  */
+
+int initialization06(int a)
+{
+  int a = a;
+  int i;
+
+  i = a+1;
+  a = 2;
+  return i;
+}
