0092f59153e15576d49882c359d90ac265c8bf6d
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-09-08 14:14:18 +0000

    * fix order change in unfolding * add failing test case spotted by FI for inlining
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1154 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/inlining27.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int foo(int i)
+{
+    int i__=3;
+    {
+        int i_=i__;
+        i+=i_;
+    }
+    return i;
+}
+int bar(int i_)
+{
+    int i__=0,i=0;
+    int j=0;
+    j+=foo(i_);
+    return j;
+}
+int main()
+{
+    printf("%d\n",bar(0));
+    return 0;
+}
+
