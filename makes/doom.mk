4f6e4503860d659a3be56e84e2b90164436608f3
keryell <keryell@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2009-09-17 08:16:07 +0000

    Transform an environment variable into a compilation flag also.
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@990 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/doom.mk
+++ makes/doom.mk
@@ -25,6 +25,7 @@ ifdef USE_NEWGEN_DOOM
 
 NEWGEN_DOOM_INCLUDES=$(shell pkg-config --cflags glib-2.0 libxml-2.0)
 NEWGEN_DOOM_LIBS=$(shell pkg-config --libs glib-2.0 libxml-2.0)
+CPPFLAGS += -DUSE_NEWGEN_DOOM
 
 else
 
