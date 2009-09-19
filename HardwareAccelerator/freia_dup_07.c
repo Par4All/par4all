509ba4c9e01bd495cf0c92c34a3a0289ab1a435a
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-09-19 20:58:01 +0000

    algebraic optimization test with commutators.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1182 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ HardwareAccelerator/freia_dup_07.c
@@ -0,0 +1,15 @@
+#include "freia.h"
+
+freia_status
+freia_dup_07(freia_data2d * o, freia_data2d * i0, freia_data2d * i1)
+{
+  freia_data2d * t = freia_common_create_data(16, 128, 128);
+  // same operation performed twice, with commutator
+  // t = inf(i0, i1)
+  freia_aipo_inf(t, i0, i1);
+  // o = sup(i1, i0)
+  // could be replaced by o = t
+  freia_aipo_sup(o, i1, i0);
+  freia_common_destruct_data(t);
+  return FREIA_OK;
+}
