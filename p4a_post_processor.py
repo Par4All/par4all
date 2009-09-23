c5f06fc51630f40355af0c300362448d56da10eb
keryell <keryell@576080ae-5d29-4e32-8499-467c2d9a9582>
2009-09-23 12:27:25 +0000

    Added Python script to post-process PIPS output for GPU
    git-svn-id: svn+ssh://svn.freia.enstb.org/subversion/freia@335 576080ae-5d29-4e32-8499-467c2d9a9582

---
+++ p4a_post_processor.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+"""
+Convert the PIPS C output to a P4A output
+
+Ronan.Keryell@hpc-project.com
+"""
+
+import sys, re
+
+def patch_to_use_p4a_methods(file_name):
+    print 'Patching', file_name
+    # Parse the PIPS output:
+    f = open(file_name)
+    # slurp all the file in a string:
+    content = f.read()
+    f.close()
+
+    ## Change
+    ##    // To be replaced with a call to P4A_VP_1
+    ##    j = j;
+    ## into
+    ##     // Index has been replaced by P4A_VP_1
+    ##    j = P4A_VP_1();
+    content = re.sub("( *)// To be replaced with a call to (P4A_VP_[0-9]+)\n[^=]+= ([^;]+)",
+                     "\\1// Index has been replaced by \\2\n\\1\\3 = \\2()", content)
+
+    # Insert a
+    #include <p4a_accel.h>
+    content = re.sub("^",
+                     "#include <p4a_accel.h>\n", content)
+
+    # Compatibility
+    content = re.sub("P4A_VP_0", "P4A_VP_X", content)
+    content = re.sub("P4A_VP_1", "P4A_VP_Y", content)
+
+    print content,
+
+    # Rewrite the content:
+    f = open(file_name, 'w')
+    f.write(content)
+    f.close()
+
+
+for name in sys.argv[1:]:
+    patch_to_use_p4a_methods(name)
