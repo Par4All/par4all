a217d084590b50fdfe5890ec89bbd00d2a8e94d5
daverio <daverio@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-09-23 14:33:06 +0000

    Add new test case for flatten_code: check that renaming a variable declaration doesn't create a naming conflict with a formal parameter.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1192 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/flatten_code15.c
+++ Transformations/flatten_code15.c
@@ -1,26 +1,25 @@
 /* Make sure that variables are properly renamed.
 
+   This test case is derived from 'flatten_code12':
+
    We must ensure that moving up the declaration of variable 'foo',
-   will not create a conflict with variable 'foo_0' declared just
-   above function flatten_code12: 'foo' should be renamed into
-   'foo_1', not 'foo_0'.
+   originally declared inside the for(...) loop, will not create a
+   conflict with formal parameter 'foo' of function flatten_code15.
  */
 
 #include <stdio.h>
 
 static float foo_0 = 3.;
 
-void flatten_code12(void)
+void flatten_code15(int foo)
 {
   int i = (int) foo_0;
 
-  foo_0++;
+  foo_0 += foo;
 
   for(;i<7;i++) {
     static int foo = 0;
-
     foo++;
-
     fprintf(stdout, "foo=%d\n", foo);
   }
   fprintf(stdout, "foo_0=%f\n", foo_0);
@@ -28,7 +27,7 @@ void flatten_code12(void)
 
 int main()
 {
-  flatten_code12();
+  flatten_code15(1);
 
   return 0;
 }
