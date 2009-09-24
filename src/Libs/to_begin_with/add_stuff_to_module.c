bba12747814d119089e1306adc39416f96f1ece3
keryell <keryell@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2009-09-24 09:02:38 +0000

    New pedagogical phase to prepend a comment to a module. I use it to inject initialization code into a main() by postprocessing. Sorry for the inconvenience: I forgot to push it yesterday... :-(
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@15461 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/to_begin_with/add_stuff_to_module.c
@@ -0,0 +1,31 @@
+/* A simple phase to show how to add a comment to a module.
+
+ Pedagogical, but useful too for me for a project...
+
+ Ronan.Keryell@hpc-project.com
+
+*/
+
+#include "genC.h"
+#include "linear.h"
+#include "ri.h"
+#include "ri-util.h"
+#include "misc.h"
+#include "control.h"
+#include "pipsdbm.h"
+#include "resources.h"
+
+bool prepend_comment(char * module_name) {
+  /* Use this module name and this environment variable to set */
+
+  statement module_statement = PIPS_PHASE_PRELUDE(module_name,
+						  "PREPEND_COMMENT_DEBUG_LEVEL");
+
+  // Get the value of the property that content the comment to prepend
+  string comment = get_string_property("PREPEND_COMMENT");
+
+  insert_comments_to_statement(module_statement, comment);
+
+  /* Put back the new statement module */
+  PIPS_PHASE_POSTLUDE(module_statement);
+}
