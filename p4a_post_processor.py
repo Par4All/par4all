190367a213d2d9c046f2b063a69e372f83d338b8
keryell <keryell@576080ae-5d29-4e32-8499-467c2d9a9582>
2009-09-24 16:42:31 +0000

    Take advantage of last PIPS version for GPU. Generate call to accelerated kernels, clean up headers...
    git-svn-id: svn+ssh://svn.freia.enstb.org/subversion/freia@348 576080ae-5d29-4e32-8499-467c2d9a9582

--- p4a_post_processor.py
+++ p4a_post_processor.py
@@ -34,9 +34,37 @@ def patch_to_use_p4a_methods(file_name):
     content = re.sub("// Prepend here P4A_INIT_ACCEL\n",
                      "P4A_INIT_ACCEL;\n", content)
 
+    # Add accelerator attributes on accelerated parts:
+    content = re.sub("(void p4a_kernel_wrapper_[0-9]+[^\n]+)",
+                     "P4A_ACCEL_KERNEL_WRAPPER \\1", content)
+    content = re.sub("(void p4a_kernel_[0-9]+[^\n]+)",
+                     "P4A_ACCEL_KERNEL \\1", content)
+
+    # Generate accelerated kernel calls:
+    ## Replace
+    ## // Loop nest P4A begin,2D(500,500)
+    ## for(i = 0; i <= 500; i += 1)
+    ##   for(j = 0; j <= 500; j += 1)
+    ##      // Loop nest P4A end
+    ##      if (i<=500&&j<=500)
+    ##         p4a_kernel_wrapper_2(space, i, j);
+    ##
+    ## with
+    ## P4A_CALL_ACCEL_KERNEL_2D(p4a_kernel_wrapper_2, 500, 500, i, j);
+
+    ## content = re.sub("// Loop nest P4A begin,2D\(([0-9]+),([0-9]+)\)\n[^\n]+\n[^\n]+\n +// Loop nest P4A end\n[^\n]+\n +(p4a_kernel_wrapper_\\d+)\\(([^)]*)\\)\n",
+    ##                  "P4A_CALL_ACCEL_KERNEL_2D(\\3,\\1,\\2,\\4)", content)
+
+    content = re.sub("(?s)// Loop nest P4A begin,2D\((\\d+),(\\d+)\).*// Loop nest P4A end\n[^\n]+\n +(p4a_kernel_wrapper_\\d+)\\(([^)]*)\\);\n",
+                     "P4A_CALL_ACCEL_KERNEL_2D(\\3,\\1,\\2,\\4);\n", content)
+
     # Compatibility
-    content = re.sub("P4A_VP_0", "P4A_VP_X", content)
-    content = re.sub("P4A_VP_1", "P4A_VP_Y", content)
+    content = re.sub("P4A_VP_0\\(\\)", "P4A_VP_X", content)
+    content = re.sub("P4A_VP_1\\(\\)", "P4A_VP_Y", content)
+
+    # Clean-up headers and inject standard header injection:
+    content = re.sub("(?s)(/\\*\n \\* file for [^\n]+\n \\*/\n).*extern int getloadavg\\(double __loadavg\\[], int __nelem\\);",
+                     "\\1#include <stdio.h>\n", content)
 
     print content,
 
