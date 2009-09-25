10247fd6b60054d11a1f964dfe53350c7950c7df
creusillet <creusillet@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-09-25 07:13:36 +0000

    add test cases for gpu_loop_nest_annotate
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1195 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Gpu/gpu_lna01.c
@@ -0,0 +1,17 @@
+
+/*
+ * file for p4a_kernel_launcher_1.c
+ */
+typedef float float_t;
+extern void p4a_kernel_wrapper_1(float_t save[501][501], float_t space[501][501], int i, int j);
+
+void p4a_kernel_launcher_1(float_t save[501][501], float_t space[501][501])
+{
+   int i;
+   int j;
+kernel2:
+   for(i = 0; i <= 498; i += 1)
+      for(j = 0; j <= 498; j += 1)
+         p4a_kernel_wrapper_1(save, space, i+1, j+1);
+}
+
