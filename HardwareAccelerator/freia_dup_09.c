9525da832d4c3d3cbb06b5972b41b3042344e3c5
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-09-27 11:30:42 +0000

    two is enough to show the issue.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1204 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia_dup_09.c
+++ HardwareAccelerator/freia_dup_09.c
@@ -5,5 +5,4 @@ void freia_dup_09(freia_data2d * in0, int32_t * x)
   // artificial WW dependencies, could be merged...
   freia_aipo_global_min(in0, x);
   freia_aipo_global_min(in0, x);
-  freia_aipo_global_min(in0, x);
 }
