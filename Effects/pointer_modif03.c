30b32467be88ef5f8dc64061bc76b896ab6f2f61
creusillet <creusillet@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-10-05 08:19:46 +0000

    some more test cases for pointer modification detection
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1213 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Effects/pointer_modif03.c
@@ -0,0 +1,25 @@
+typedef struct {int entier; float reel;} s_t;
+
+s_t make_s(int i, float x)
+{
+  s_t * p;
+  extern char * malloc(int);
+
+  p = (s_t *) malloc(sizeof(s_t));
+  p->entier = i;
+  p->reel = x;
+  return *p;
+}
+
+int main()
+{
+  s_t s;
+  int j;
+
+  s = make_s(1, 2.);
+  s.entier = 4;
+
+  j = make_s(1, 2.).entier;
+
+  return j;
+}
---
+++ Effects/pointer_modif04.c
@@ -0,0 +1,33 @@
+typedef struct {int max; float *tab;} s_t;
+
+void init_s(s_t s, int max)
+{
+  extern char * malloc(int);
+
+  s.tab = (float *) malloc(max * sizeof(float));
+  s.max = max;
+  
+}
+
+void compute_s(s_t s, int max)
+{
+  int i;
+
+  for (i=0; i<max; i++)
+    s.tab[i] = i*2.0;
+  
+}
+
+
+int main()
+{
+  s_t s;
+  int j;
+
+  init_s(s, 10);
+  
+  compute_s(s, 10);
+  
+
+  return 0;
+}
