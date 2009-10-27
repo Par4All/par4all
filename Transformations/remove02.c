e85401b00ebb36c1883bdccc4fec25a9bfeccf59
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-10-27 21:09:57 +0000

    update validation
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1245 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/remove02.c
@@ -0,0 +1,25 @@
+int main () {
+  float a[10];
+  float b[10][10][10][10][10];
+  int m = 0;
+  int n = 0;
+  int o = 2;
+
+l0:  for (m = 0; m < 10; m++) {
+    n = n+1;
+    a[m] = n;
+  }
+
+l1:  for (m = 0; m < 10; m++) {
+    // comments 4
+    n = n+1;
+    o = 2+o;
+    o = 2+o;
+    n = n+5;
+    b[0][0][1][m][0] = n;
+  }
+     printf("%d\n",o);
+
+  return 0;
+}
+
