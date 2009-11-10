39886b1182173b5b506ebd29eecb12fa2c64d36b
daverio <daverio@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-11-10 18:12:55 +0000

    Add blank lines to example file.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1284 67b5017d-e62d-0410-a871-f72b1c62810f

--- TutorialPPoPP2010/add_stuff_to_module-06.c
+++ TutorialPPoPP2010/add_stuff_to_module-06.c
@@ -7,9 +7,13 @@
 #include "resources.h"
 
 bool prepend_comment(string mn) {
+
   statement s = PIPS_PHASE_PRELUDE(
     mn, "PREPEND_COMMENT_DEBUG_LEVEL");
+
   string c = get_string_property("PREPEND_COMMENT");
+
   insert_comments_to_statement(s, c);
+
   PIPS_PHASE_POSTLUDE(s);
 }
