58e50aa26392fe6d78781e53484e26d530e9859b
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-11-14 18:57:06 +0000

    Improved comment in ndecl05; new test case for struct: ndecl06
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1289 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/ndecl05.c
+++ C_syntax/ndecl05.c
@@ -1,7 +1,7 @@
 /* Purpose: debug the new version of the parser which keep a statement
    for each declaration. Problem with declarations of function formal
    arguments: the second function declared loses its formal
-   arguments...*/
+   arguments... Was a prettyprint bug. */
 
 void ndecl05(char *dir)
 {
---
+++ C_syntax/ndecl06.c
@@ -0,0 +1,10 @@
+/* Purpose: what's wrong with struct? */
+
+void ndecl06(char *dir)
+{
+  struct m {
+    int a;
+    float b;
+  };
+
+}
