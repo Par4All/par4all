86a0fa0bc16b7c7b45216f6f2b59fbcf5f9b4962
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-11-17 11:01:09 +0000

    update jacobi tpips and python version
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1292 67b5017d-e62d-0410-a871-f72b1c62810f

--- TutorialPPoPP2010/jacobi.py
+++ TutorialPPoPP2010/jacobi.py
@@ -16,10 +16,8 @@ w.all.display()
 
 w.all.gpu_ify()
 
-launchers= ['p4a_kernel_launcher_0', 'p4a_kernel_launcher_1', 'p4a_kernel_launcher_2', 'p4a_kernel_launcher_3', 'p4a_kernel_launcher_4']
-map(lambda name: w[name].kernel_load_store(), launchers)
-map(lambda name: w[name].gpu_loop_nest_annotate(),launchers)
-map(lambda name: w[name].inlining(),launchers)
-
-outdir='p4a_gen'
-w.save(indir=outdir)
+launchers= modules(['p4a_kernel_launcher_0', 'p4a_kernel_launcher_1', 'p4a_kernel_launcher_2', 'p4a_kernel_launcher_3', 'p4a_kernel_launcher_4'])
+launchers.kernel_load_store()
+launchers.gpu_loop_nest_annotate()
+launchers.inlining()
+...
