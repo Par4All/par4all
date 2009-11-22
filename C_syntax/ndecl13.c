b83a1e8f4e4464c14c0b563b109e6535af9dcac1
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-11-22 08:57:24 +0000

    Mostly new test cases
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1301 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ C_syntax/ndecl13.c
@@ -0,0 +1,18 @@
+/* Check the behavior of the prettyprinter for extern, static and
+   typedef */
+
+extern int i;
+int j;
+static int k;
+typedef int km;
+
+
+int main()
+{
+
+  extern int i_m;
+  int j_m;
+  static int k_m;
+  typedef int km_m;
+
+}
