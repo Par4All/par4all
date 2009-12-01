d00781bec08d84a628c5a793098b3fe217d01a77
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-12-01 10:23:29 +0000

    clone01 test case added for PIPS tutorial
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1317 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ TutorialPPoPP2010/clone01.c
@@ -0,0 +1,18 @@
+int clone01(int n, int s)
+{
+  int r = n;
+  if(s<0)
+    r = n-1;
+  else if(s>0)
+    r = n+1;
+
+  return r;
+}
+
+int main()
+{
+  int i = 1;
+  i = clone01(i,-1);
+  i = clone01(i,1);
+  i = clone01(i,0);
+}
