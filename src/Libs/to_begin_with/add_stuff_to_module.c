6d419102023a1e6bcc384b89417c4da4169270bd
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2009-12-06 18:33:35 +0000

    comments added for prepend_call(); the debugging of this function, started by Ronan, could be left open for another PIPS tutorial...
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@15826 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/to_begin_with/add_stuff_to_module.c
+++ src/Libs/to_begin_with/add_stuff_to_module.c
@@ -1,4 +1,4 @@
-/* Simple phases to show how to add a stuff to a module: a comment, a call,...
+/* Simple phases to show how to add stuff to a module: a comment, a call,...
 
  Pedagogical, but also useful for RK for a project...
 
@@ -12,6 +12,7 @@
 #include "ri-util.h"
 #include "misc.h"       // for debug_on()
 #include "control.h"    // for module_reorder()
+#include "callgraph.h"  // for compute_callees()
 #include "properties.h" // for get_string_property()
 #include "pipsdbm.h"
 #include "resources.h"
@@ -51,15 +52,24 @@ bool prepend_comment(char * module_name) {
 }
 
 
-/** This function adds a call to function MY_TRACK as first statement
-    of module mn.
+/** This function adds a call to ficticious global function "MY_TRACK" as
+    first executabble statement of module "mn" in C89 style. It is not
+    supposed to be of any use beyond training new comers to PIPS.
 
-    It is written to show how to use Newgen primitives, not to be
-    used. See make_empty_subroutine() in ri-util.
+    Function "MY_TRACK" is assumed to take no arguments and to return
+    no results. No source code is available for it.
+
+    It is written to show how to use Newgen primitives in PIPS PPoPP
+    2010 tutorial, not to be used. See make_empty_subroutine() in
+    ri-util/entity.c for a similar function which might be moved into
+    ri-util/module.c.
 
     @param mn is the name of the module to apply this phase on.
 
-    @return TRUE as we assume it always works.
+    @return TRUE as we assume it always works, although repetitive
+    calls to make_entity() with the same global name might
+    fail:-). Not tested on Fortran code. No non-regression tests for
+    it either.
  */
 bool prepend_call(string mn) {
   type rt = make_type_void();
@@ -74,12 +84,15 @@ bool prepend_call(string mn) {
 				  MODULE_SEP_STRING,
 				  name,
 				  NULL));
+  /* This works only once. So use FindOrCreateEntity() instead... or
+     rely on functions in library ri-util. */
   entity f = make_entity(ffn, t, st, v);
   call ca = make_call(f, NIL);
   instruction i = make_instruction_call(ca);
   statement ns = instruction_to_statement(i);
   statement module_statement = PIPS_PHASE_PRELUDE(mn,
 						  "PREPEND_CALL_DEBUG_LEVEL");
+  pips_assert("f really is an entity", check_entity(f));
   insert_statement(module_statement, ns, TRUE);
   DB_PUT_MEMORY_RESOURCE(DBR_CALLEES, mn, compute_callees(module_statement));
   PIPS_PHASE_POSTLUDE(module_statement);
