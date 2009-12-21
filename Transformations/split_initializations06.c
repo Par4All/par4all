6898e4b1da049fdf515c50f921bdcb49d621b735
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2009-12-21 22:36:04 +0000

    new test cases + new results for split_initializations adapted to the new declaration representation
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1367 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/split_initializations06.c
@@ -0,0 +1,10 @@
+/* Second basic test case: new assignments should be generated... at
+   the right place if you want to stay C89 compatible */
+
+int split_initializations06()
+{
+  int i = 1, l = 4;
+  int j = 2;
+  int k = 3;
+  i++;
+}
