b7708dd22801de33fcf655e38cc4a5309983d8b2
villalon <villalon@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2009-12-30 12:03:17 +0000

    split extension and pragma in two different files in ri-util.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@15906 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Libs/ri-util/extension.c
@@ -0,0 +1,149 @@
+/*
+
+  Copyright 1989-2009 HPC Project
+
+  This file is part of PIPS.
+
+  PIPS is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  PIPS is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.
+
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with PIPS.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+/*
+  Pierre.Villalon@hpc-project.com
+  Ronan.Keryell@hpc-project.com
+*/
+
+#include "linear.h"
+#include "genC.h"
+#include "misc.h"
+#include "ri.h"
+#include "ri-util.h"
+#include "text-util.h"
+#include "outlining_private.h"
+#include "step_private.h"
+#include "step.h"
+#include "properties.h"
+
+/*****************************************************A CONSTRUCTOR LIKE PART
+ */
+
+/** @return an empty extensions
+ */
+extensions empty_extensions (void) {
+  return make_extensions (NIL);
+}
+
+/** @return TRUE if the extensions field is empty
+    @param es the extensions to test
+*/
+bool empty_extensions_p (extensions es) {
+  return (extensions_extension (es) == NIL);
+}
+
+/***************************************************** PRETTYPRINT PART
+ */
+
+/** @return a new allocated string to close the extension.
+ *  @param es, the extension to be closed
+ *
+ *  Today we only generate omp parallel do pragma so the close is pretty
+ *  easy. Later we will have to analyze the extension to generate the
+ *  close string accordingly.
+ */
+string close_extension (extension e) {
+  return close_pragma (extension_pragma(e));
+}
+
+/** @return a new allocated string to close the extensions.
+ *  @param es, the extensions to be closed
+ *  @param nl, set to TRUE to get the string with a final new line character
+ */
+string
+close_extensions (extensions es, bool nl) {
+  string s = string_undefined;
+
+  if (empty_extensions_p (es) == FALSE) {
+    /* Use a string_buffer for efficient string concatenation: */
+    string_buffer sb = string_buffer_make(FALSE);
+
+    list el = extensions_extension(es);
+    FOREACH(EXTENSION, e, el) {
+      s = close_extension(e);
+      if (s != string_undefined) {
+	string_buffer_append(sb, s);
+	if (nl == TRUE) string_buffer_append(sb, strdup ("\n"));
+	nl = TRUE;
+      }
+    }
+    s = string_buffer_to_string(sb);
+    /* Free the buffer with its strings: */
+    string_buffer_free_all(&sb);
+  }
+
+  return s;
+}
+
+
+/** @return a new allocated string with the extension textual representation.
+ */
+string
+extension_to_string(extension e) {
+  string s;
+  /* to be added later when extension can be something else than pragma
+  switch (extension_tag(e)) {
+  case is_extension_pragma:
+  */
+    s = pragma_to_string(extension_pragma(e));
+    /*  default:
+    pips_internal_error("Unknown extension type\n");
+    }*/
+  return s;
+}
+
+/** @brief return a new allocated string with the string representation of the
+ *  extensions. Basically you'll get one extension per line
+ *
+ *  Assume that all the extension from the extensions (note the presence
+ *  or not of the "s"...) are defined below.
+ *
+ *  @return string_undefined if es is extension_undefined, an malloc()ed
+ *  textual string either.
+ *  @param es, the extensions to translate to strings
+ *  @param nl, set to TRUE to get the string with a final new line character
+ */
+string
+extensions_to_string(extensions es, bool nl) {
+  string s = string_undefined;
+
+  /* Prettyprint in the correct language: */
+  set_prettyprint_is_fortran_p(!get_bool_property("PRETTYPRINT_C_CODE"));
+
+  if (empty_extensions_p (es) == FALSE) {
+    /* Use a string_buffer for efficient string concatenation: */
+    string_buffer sb = string_buffer_make(FALSE);
+
+    list el = extensions_extension(es);
+    FOREACH(EXTENSION, e, el) {
+      s = extension_to_string(e);
+      string_buffer_append(sb, s);
+      if (nl == TRUE) string_buffer_append(sb, strdup ("\n"));
+      nl = TRUE;
+    }
+    s = string_buffer_to_string(sb);
+    /* Free the buffer with its strings: */
+    string_buffer_free_all(&sb);
+  }
+
+  return s;
+}
--- src/Libs/ri-util/pragma.c
+++ src/Libs/ri-util/pragma.c
@@ -50,21 +50,6 @@ static const string FORTRAN_PRAGMA_HEADER = "!$";
 static const string FORTRAN_OMP_CONTINUATION = "\n!$omp& ";
 
 
-/*****************************************************A CONSTRUCTOR LIKE PART
- */
-
-/** @return an empty extensions
- */
-extensions empty_extensions (void) {
-  return make_extensions (NIL);
-}
-
-/** @return TRUE if the extensions field is empty
-    @param es the extensions to test
-*/
-bool empty_extensions_p (extensions es) {
-  return (extensions_extension (es) == NIL);
-}
 
 /***************************************************PRAGMA AS EXPRESSION PART
  */
@@ -140,55 +125,21 @@ list pragma_omp_parallel_for_as_exprs (void) {
 
 /***************************************************** PRETTYPRINT PART
  */
-
-
-/** @return a new allocated string to close the extension.
- *  @param es, the extension to be closed
+/** @return a new allocated string to close the pragma.
+ *  @param p, the pragma to be closed
  *
- *  Today we only generate omp parallel do pragma so the close is pretty
- *  easy. Later we will have to analyze the extension to generate the
- *  close string accordingly.
  */
-string close_extension (extension e) {
+string close_pragma (pragma p) {
   string result = string_undefined;
   if (get_prettyprint_is_fortran () == TRUE)
     {
-      if (pragma_entity_p(extension_pragma(e)))
-	result=directive_to_string(load_global_directives(pragma_entity(extension_pragma(e))),true);
+      if (pragma_entity_p (p))
+	result=directive_to_string(load_global_directives(pragma_entity(p)),true);
       else
 	result = strdup(concatenate (FORTRAN_PRAGMA_HEADER, "omp end parallel do",
 				     NULL));
     }
   return result;
-}
-
-/** @return a new allocated string to close the extensions.
- *  @param es, the extensions to be closed
- *  @param nl, set to TRUE to get the string with a final new line character
- */
-string
-close_extensions (extensions es, bool nl) {
-  string s = string_undefined;
-
-  if (empty_extensions_p (es) == FALSE) {
-    /* Use a string_buffer for efficient string concatenation: */
-    string_buffer sb = string_buffer_make(FALSE);
-
-    list el = extensions_extension(es);
-    FOREACH(EXTENSION, e, el) {
-      s = close_extension(e);
-      if (s != string_undefined) {
-	string_buffer_append(sb, s);
-	if (nl == TRUE) string_buffer_append(sb, strdup ("\n"));
-	nl = TRUE;
-      }
-    }
-    s = string_buffer_to_string(sb);
-    /* Free the buffer with its strings: */
-    string_buffer_free_all(&sb);
-  }
-
-  return s;
 }
 
 /** @return a new allocated string with the pragma textual representation.
@@ -251,61 +202,6 @@ pragma_to_string (pragma p) {
     else
       s = strdup(concatenate (C_PRAGMA_HEADER, " ", s, NULL));
   }
-  return s;
-}
-
-/** @return a new allocated string with the extension textual representation.
- */
-string
-extension_to_string(extension e) {
-  string s;
-  /* For later other extensions:
-  switch (extension_tag(e)) {
-  case is_extension_pragma:
-  */
-  s = pragma_to_string(extension_pragma(e));
-  /*
-  default:
-    pips_internal_error("Unknown extension type\n");
-  }
-  */
-  return s;
-}
-
-/** @brief return a new allocated string with the string representation of the
- *  extensions. Basically you'll get one extension per line
- *
- *  Assume that all the extension from the extensions (note the presence
- *  or not of the "s"...) are defined below.
- *
- *  @return string_undefined if es is extension_undefined, an malloc()ed
- *  textual string either.
- *  @param es, the extensions to translate to strings
- *  @param nl, set to TRUE to get the string with a final new line character
- */
-string
-extensions_to_string(extensions es, bool nl) {
-  string s = string_undefined;
-
-  /* Prettyprint in the correct language: */
-  set_prettyprint_is_fortran_p(!get_bool_property("PRETTYPRINT_C_CODE"));
-
-  if (empty_extensions_p (es) == FALSE) {
-    /* Use a string_buffer for efficient string concatenation: */
-    string_buffer sb = string_buffer_make(FALSE);
-
-    list el = extensions_extension(es);
-    FOREACH(EXTENSION, e, el) {
-      s = extension_to_string(e);
-      string_buffer_append(sb, s);
-      if (nl == TRUE) string_buffer_append(sb, strdup ("\n"));
-      nl = TRUE;
-    }
-    s = string_buffer_to_string(sb);
-    /* Free the buffer with its strings: */
-    string_buffer_free_all(&sb);
-  }
-
   return s;
 }
 
