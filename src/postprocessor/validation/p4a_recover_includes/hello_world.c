0e1c2fea5a3df0e7e6c086f4921a8a3728c55a85
Ronan Keryell <Ronan.Keryell@hpc-project.com>
2010-01-12 17:43:28 +0100

    Improved includes recovering in PIPS output.
    There is still a bug I need to solve by adding a lexer that deals with
    { } mathing...

---
+++ src/postprocessor/validation/p4a_recover_includes/hello_world.c
@@ -0,0 +1,12 @@
+/* Pour pouvoir utiliser proprement
+   la fonction de sortie : */
+#include <stdio.h>
+/* Définit entre autres les codes de retour : */
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  /* Affiche sur la « sortie-standard » */
+  puts("Hello, world!");
+  // Renvoie une marque de réussite :
+  return EXIT_SUCCESS;
+}
