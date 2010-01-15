2a04baffdc2984012bcb049979982a5018d1c001
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-01-15 13:15:04 +0000

    add validation for privatizing, it shows what is missing but will be supported once my branch is merge back
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1382 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Privatizing/privatize05.c
@@ -0,0 +1,16 @@
+void c_est_pourtant_magnifique()
+{
+    int y,m,c,a;
+    int u[2];
+    for(y=2;y!=45;y+=8)
+    {
+        m=y;
+        for(c=m;c<=y*2;c++)
+        {
+            a=m*4;
+            u[0]=2;
+        }
+        u[0]=a;
+    }
+}
+
