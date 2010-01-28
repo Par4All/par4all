790e80fb961c92553528b0a624d6ce5cd4a163ef
guelton <guelton@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2010-01-28 12:55:05 +0000

    some modif for auto-*
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@1026 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/cproto.mk
+++ makes/cproto.mk
@@ -1,21 +1,24 @@
 $(TARGET).h:$(TARGET)-local.h $(SOURCES)
 	cat $< > $(TARGET).h
 	{ \
+		SOURCES=`for s in $^ ; do case $$s in *.[ch]) echo $$s ;; esac ; done`; \
+		guard=`echo $(TARGET)_header_included | tr - _`;\
       	echo "/* Warning! Do not modify this file that is automatically generated! */"; \
       	echo "/* Modify src/Libs/$(TARGET)/$(TARGET)-local.h instead, to add your own modifications. */"; \
       	echo ""; \
       	echo "/* header file built by $(CPROTO) */"; \
       	echo ""; \
-      	echo "#ifndef $(TARGET)_header_included";\
-      	echo "#define $(TARGET)_header_included";\
-		echo "#ifdef HAVE_CONFIG_H";\
-		echo "#include \"$(CONFIG_HEADER)\"";\
-		echo "#endif";\
+      	echo "#ifndef $${guard}";\
+      	echo "#define $${guard}";\
       	cat $< ;\
-		$(CPROTO) -evcf2 -E "$(CPP) $(INCLUDES) $(DEFAULT_INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) -DCPROTO_IS_PROTOTYPING" $^ ;\
-      	echo "#endif /* $(name)_header_included */"; \
+		$(CPROTO) -evcf2 -E "$(CPP) $(INCLUDES) $(DEFAULT_INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) -DCPROTO_IS_PROTOTYPING" $$SOURCES ;\
+      	echo "#endif /* $${guard} */"; \
 	} > $(TARGET).h-tmp
 	rm $(TARGET).h
 	mv $(TARGET).h-tmp $(TARGET).h
 
+$(TARGET)-local.h:
+	touch $@
+
 CLEANFILES=$(TARGET).h
+EXTRA_DIST=$(TARGET)-local.h
