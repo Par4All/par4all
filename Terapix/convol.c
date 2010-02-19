d0cb553bb6655895d695a259568598fac1b67184
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-02-19 10:37:50 +0000

    extend valdiation for new flavour of loop_expansion add comments on silber01 for missing transformations improve convol terapixification linpack benchmarks is ok with openmp now but the pretty print fails "misérablement"
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1469 67b5017d-e62d-0410-a871-f72b1c62810f

--- Terapix/convol.c
+++ Terapix/convol.c
@@ -1,77 +1,62 @@
-//     goal: show effect of cloning, partial evaluation and loop unrolling
-//     and reduction parallelization for a Power architecture
-//     kernel_size must be odd
-
 void convol();
 #define kernel_size 3
 
-int main() //      program image_processing
+int main() 
 {
-    const int image_size = 512, nsteps=20;
-    float image[image_size][image_size];
-    float new_image[image_size][image_size];
-    float kernel[kernel_size][kernel_size];
+  const int image_size = 512, nsteps=20;
+  float image[image_size][image_size];
+  float new_image[image_size][image_size];
+  float kernel[kernel_size][kernel_size];
 
-    int i, j, n;
+  int i, j, n;
 
-    for( i = 0; i< kernel_size; i++) {
-        for( j = 0; j< kernel_size; j++) {
-            kernel[i][j] = 1;
-        }
+  for( i = 0; i< kernel_size; i++) {
+    for( j = 0; j< kernel_size; j++) {
+      kernel[i][j] = 1;
     }
+  }
 
-    //     read *, image
-    for( i = 0; i< image_size; i++) {
-        for( j = 0; j< image_size; j++)
-            image[i][j] = 1.;
-    }
+  for( i = 0; i< image_size; i++) {
+    for( j = 0; j< image_size; j++)
+      image[i][j] = 1.;
+  }
 
 
-    for( n = 0; n< nsteps; n++) {
-        convol(image_size, image_size, new_image, image, kernel);
-    }
+  for( n = 0; n< nsteps; n++) {
+    convol(image_size, image_size, new_image, image, kernel);
+  }
 
-    for( i = 0; i< image_size; i++) {
-        for( j = 0; j< image_size; j++)
-            printf("%f ",new_image[i][j]);
-    }
-    //     print *, new_image
-    //      print *, new_image (image_size/2, image_size/2)
+  for( i = 0; i< image_size; i++) {
+    for( j = 0; j< image_size; j++)
+      printf("%f ",new_image[i][j]);
+  }
 
-    return 1; 
+  return 1; 
 }
 
 void convol(int isi, int isj, float new_image[isi][isj], float image[isi][isj],
-        float kernel[kernel_size][kernel_size])
+    float kernel[kernel_size][kernel_size])
 {
-    //     The convolution kernel is not applied on the outer part
-    //     of the image
+  //     The convolution kernel is not applied on the outer part
+  //     of the image
+  int i, j;
+  for(i = 0; i< isi; i++)
+    for(j = 0; j< isj; j++)
+      new_image[i][j] = image[i][j];
 
-    int i, j;
-
-    for(i = 0; i< isi; i++) {
-        for(j = 0; j< isj; j++) {
-            new_image[i][j] = image[i][j];
-        }
-    }
-
-    for(i =  kernel_size/2; i<isi - kernel_size/2; i++) {
-        for(j =  kernel_size/2; j<isj - kernel_size/2; j++) {
-            run_kernel(i,j,isi,isj,new_image,image,kernel);
-        }
-    }
+  for(i =  kernel_size/2; i<isi - kernel_size/2; i++)
+    for(j =  kernel_size/2; j<isj - kernel_size/2; j++)
+      run_kernel(i,j,isi,isj,new_image,image,kernel);
 }
 
 void run_kernel(int i, int j, int isi, int isj,float new_image[isi][isj], float image[isi][isj], float kernel[kernel_size][kernel_size])
 {
-    int ki,kj;
-    new_image[i][j] = 0.;
-    for(ki = 0; ki<kernel_size; ki++) {
-        for(kj = 0; kj<kernel_size; kj++) {
-            new_image[i][j] = new_image[i][j] + 
-                image[i+ki-kernel_size/2][j+kj-kernel_size/2]* 
-                kernel[ki][kj];
-        }
-    }
-    new_image[i][j] = new_image[i][j]/(kernel_size*kernel_size);
+  int ki,kj;
+  new_image[i][j] = 0.;
+  for(ki = 0; ki<kernel_size; ki++)
+    for(kj = 0; kj<kernel_size; kj++)
+      new_image[i][j] = new_image[i][j] + 
+        image[i+ki-kernel_size/2][j+kj-kernel_size/2]* 
+        kernel[ki][kj];
+  new_image[i][j] = new_image[i][j]/(kernel_size*kernel_size);
 }
