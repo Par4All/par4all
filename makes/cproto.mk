9ba0a31bb59d0d4631f2362073e8275fab041ab4
guelton <guelton@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2010-02-25 12:56:38 +0000

    some more comment on cproto.mk makefile and fix some minor buggy behavior thanks FC and FI for their advices
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@1043 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/cproto.mk
+++ makes/cproto.mk
@@ -1,5 +1,16 @@
+# this rule is quite complicated
+# it is in charge of wrapping cproto stuff and be compatible with automake
+# the first requirement is that TARGET must be defined
+# the second requirement is that there must be a version of $(TARGET).h present 
+#   if not just copy the -local version
+# finally we call cproto to generate the full new header
+# if a change occured, we validate it !
+# erros from cproto are just dropped (-O switch)
+#
 $(TARGET).h:$(srcdir)/$(TARGET)-local.h $(SOURCES)
-	cat `( test -f $(TARGET)-local.h && echo $(TARGET)-local.h ) || echo $(srcdir)/$(TARGET)-local.h ` > $(TARGET).h
+	if ! test -f $(TARGET).h ; then \
+		cp $(srcdir)/$(TARGET)-local.h $(TARGET).h ;\
+	fi
 	{ \
 		SOURCES=`for s in $(TARGET)-local.h $(SOURCES) ; do ( test -f $$s && echo $$s ) || echo $(srcdir)/$$s ; done`; \
 		guard=`echo $(TARGET)_header_included | tr - _`;\
@@ -11,11 +22,17 @@ $(TARGET).h:$(srcdir)/$(TARGET)-local.h $(SOURCES)
       	echo "#ifndef $${guard}";\
       	echo "#define $${guard}";\
       	cat `( test -f $(TARGET)-local.h && echo $(TARGET)-local.h ) || echo $(srcdir)/$(TARGET)-local.h ` ;\
-		$(CPROTO) -evcf2 -E "$(CPP) $(INCLUDES) $(DEFAULT_INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) -DCPROTO_IS_PROTOTYPING" $$SOURCES ;\
+		$(CPROTO) -evcf2 -O /dev/null -E "$(CPP) $(INCLUDES) $(DEFAULT_INCLUDES) $(AM_CPPFLAGS) $(CPPFLAGS) -DCPROTO_IS_PROTOTYPING" $$SOURCES ;\
       	echo "#endif /* $${guard} */"; \
-	} | sed -e '/ yy/ d' > $(TARGET).h-tmp
-	rm $(TARGET).h
-	mv $(TARGET).h-tmp $(TARGET).h
+	} > $(TARGET).h-tmp
+	if cmp -s $(TARGET).h $(TARGET).h-tmp ; then \
+		echo "file is unchanged" ; \
+		rm $(TARGET).h-tmp ;\
+	 else \
+	 	echo "udpating file"; \
+		rm $(TARGET).h ; \
+	    mv $(TARGET).h-tmp $(TARGET).h ;\
+	 fi
 
 $(TARGET)-local.h:
 	touch $@
