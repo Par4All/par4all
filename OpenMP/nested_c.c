8680f6aac6bee837fc4416f2c69888acc5555483
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-03-08 17:47:19 +0000

    update some examples
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1536 67b5017d-e62d-0410-a871-f72b1c62810f

--- OpenMP/nested_c.c
+++ OpenMP/nested_c.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
+int size = 100;
+
 int main (void) {
   int k =0, i = 0;
-  int sum = 0;
+  float sum = 0;
+  float array [size][size];
 
-  for (k = 0; k < 100; k++) {
-    for (i = 0; i < 100; i++) {
-      sum += i;
+  for (k = 0; k < size; k++) {
+    for (i = 0; i < size; i++) {
+      array[k][i] = i + k;
     }
-    sum += k;
   }
 
-  for (i = 0; i < 100; i++) {
-    sum += i;
+  for (i = 0; i < size; i++) {
+    for (k = 0; k < size; k++) {
+      sum += array[i][k];
+    }
   }
 
-  printf ("sum: %d\n", sum);
+  printf ("sum: %f\n", sum);
 
   return 0;
 }
