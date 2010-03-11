e5b91432c72518c43fc78f1fab123b9f79f9c196
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-03-11 09:25:55 +0000

    add the.c and the .c of the test case  struct_pt
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1549 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pointers/struct_pt.c
@@ -0,0 +1,17 @@
+int main()
+{
+	typedef struct my_struct {
+		int i;
+		int *p;
+	} my_str;
+	my_str *m, *n;
+	int j;
+	j = 2;
+	m = (my_str *) malloc(sizeof(my_str));
+	m->i = 1;
+	m->p = &j;
+	n = m;
+	
+	return 0;
+ 
+}
