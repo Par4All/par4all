5e1c52e4ba3e5d81457889fdf4ac1ceb846ddcc6
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-03-25 07:28:27 +0000

    very_very_long_name test case added: no bug on my version of tpips; void_func_ptr: bug fixed on my laptop; some more test cases to debug the parsing of the const type qualifier
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1579 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/void_func_ptr.c
+++ C_syntax/void_func_ptr.c
@@ -1,3 +1,6 @@
-void test(void (*func)(void *))
+/* Bug in handling the functional pointer, probably interpreted as a
+   function. No fix but work around the bug in util.c,
+   RemoveFromExterns() */
+void void_func_ptr(void (*func)(void *))
 {
 }
