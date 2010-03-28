3c634a934bc178dcbf0a50b0fcf67e40b2f51346
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-03-28 06:42:02 +0000

    fixed result for struct_array_init
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1593 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/struct_array_init.c
+++ C_syntax/struct_array_init.c
@@ -1,19 +1,23 @@
 struct test
 {
-    int a;
-    int b[2];
+  int a;
+  int b[2];
 };
 
 
 
 int main()
 {
-    struct test t[]=
+  struct test t[]=
     {
-        {
-            1,
-            {2, 3},
-        }
+      {
+	1,
+	{2, 3},
+      },
+      {
+	1,
+	{2, 3},
+      }
     };
-    return 0;
+  return 0;
 }
