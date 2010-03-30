12e8c4d7cec1c90a25d487117fba3c593ab0a585
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-03-30 16:30:09 +0000

    add validation example found by jfl while evaluating pips on HPC example : ParticuleSimulation. This a bad handling of the precedence of cast operator
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1600 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ C_syntax/precedence_operator.c
@@ -0,0 +1,19 @@
+struct test2
+{
+    int a;
+};
+
+struct test1
+{
+    struct test2 t2;
+};
+
+int main()
+{
+    struct test1 t1;
+    void* t1_ptr=&t1;
+
+    int i=((struct test1*)t1_ptr)->t2.a;
+
+    return 0;
+}
