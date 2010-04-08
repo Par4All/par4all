c5dc30a1d32229ae413ef729969917b7d0e8bf11
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-04-08 14:37:13 +0000

    update loop iteration validation for pyps now that iteration over loops is nested
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1637 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pyps/basics2.py
+++ Pyps/basics2.py
@@ -3,7 +3,7 @@ from pyps import *
 ws=workspace(["basics2.c"])
 conv=ws["convol"]
 conv.unfolding()
-lbl=conv.loops()[1].label
+lbl=conv.loops()[0].loops()[0].label
 conv.outline(label=lbl,module_name=(conv.name+"_outlined"))
 ws.all.display()
 ws.close()
