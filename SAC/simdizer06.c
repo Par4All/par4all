3143e48bd5c4d6d391a48654dfaf96d2eb298a5d
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-04-14 15:48:34 +0000

    update validation for sac due to recent optimizations
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1691 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ SAC/simdizer06.c
@@ -0,0 +1,20 @@
+main()
+{
+    float a[4]={0,1,2,3};
+    float b[4]={3,2,1,0};
+    int i;
+
+    for(i=0;i<1;i++)
+    {
+        /* this should be a first pack */
+        a[0]=b[0]+a[0];
+        a[1]=b[1]+a[1];
+        a[2]=b[2]+a[2];
+        a[3]=b[3]+a[3];
+        /* this should be a second pack */
+        a[0]=b[0]+a[0];
+        a[1]=a[1]+b[1];
+        a[2]=b[2]+a[2];
+        a[3]=b[3]+a[3];
+    }
+}
