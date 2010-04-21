254403fb1caa358bd1da203b18f2eac180ff4886
amini <amini@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2010-04-21 00:12:46 +0000

    A file was missing for autotools pips building with Fortran95
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@16801 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Passes/fortran95/gfc2pips-autotools.mk
@@ -0,0 +1,55 @@
+# $Id$
+#
+# Copyright 1989-2010 MINES ParisTech
+#
+# This file is part of PIPS.
+#
+# PIPS is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# PIPS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PIPS.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+
+# PIPS Libraries
+PIPSLIBS_LIBS = ../../../../../Libs/
+LIBS_PIPS =  $(PIPSLIBS_LIBS)/ri-util/.libs/libri-util.a \
+	$(PIPSLIBS_LIBS)/text-util/.libs/libtext-util.a \
+	$(PIPSLIBS_LIBS)/syntax/.libs/libsyntax.a \
+	$(PIPSLIBS_LIBS)/alias-classes/.libs/libalias-classes.a \
+	$(PIPSLIBS_LIBS)/ri-util/.libs/libri-util.a \
+	$(PIPSLIBS_LIBS)/misc/.libs/libmisc.a \
+	$(PIPSLIBS_LIBS)/newgen/.libs/libnewgen.a
+
+# The compiler itself is called f951.
+f951$(exeext): $(F95_OBJS) $(LIBS_PIPS) fortran/gfc2pips.o \
+		$(BACKEND) $(LIBDEPS) attribs.o
+	$(CC) $(ALL_CFLAGS) $(LDFLAGS) -o $@ \
+		$(F95_OBJS) $(BACKEND) $(LIBS) fortran/gfc2pips.o $(LIBS_PIPS) $(LINEARLIBS_LIBS) $(NEWGENLIBS_LIBS)  attribs.o $(BACKENDLIBS) -lgmp -lmpfr
+
+#INCLUDES += -I$(NEWGEN_ROOT)/include/ -I$(PIPS_ROOT)/include/ -I $(LINEAR_ROOT)/include/
+INCLUDES += -I../../../../../Documentation/newgen/ 
+INCLUDES += -I../../../../../Libs/preprocessor/
+INCLUDES += -I../../../../../Libs/ri-util/
+INCLUDES += -I../../../../../Libs/syntax/
+INCLUDES += -I../../../../../Libs/misc/
+INCLUDES += -I../../../../../Libs/newgen/
+INCLUDES += -I../../../$(pipssrcdir)/../../Documentation/constants/
+INCLUDES += -I../../../$(pipssrcdir)/../../Documentation/newgen/
+INCLUDES += -I../../../../../Documentation/newgen/ 
+INCLUDES += $(LINEARLIBS_CFLAGS) $(NEWGENLIBS_CFLAGS) 
+
+fortran/gfc2pips.o: fortran/gfc2pips.c fortran/gfc2pips_stubs.c fortran/gfc2pips.h fortran/gfc2pips-private.h
+	$(CC) $( -std=c99 -g -c $(ALL_CPPFLAGS) -DBASEVER=$(BASEVER_s)  \
+		$< $(OUTPUT_OPTION)
+
