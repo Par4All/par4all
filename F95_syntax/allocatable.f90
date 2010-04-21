4dea53cd09203b3bb3aa7c918c5fd80cc2ab23d9
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-04-21 18:45:55 +0000

    - Creating validation repository for Fortran95 parser ; aka GFC2PIPS - Adding two "trivials" test cases
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1765 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ F95_syntax/allocatable.f90
@@ -0,0 +1,12 @@
+! Try to parse and print again ALLOCATABLE & ALLOCATE
+      SUBROUTINE ALLOCATABLE ()
+
+      ALLOCATABLE UBAR(:), RBAR(:)
+
+      JMAX = 10
+      ALLOCATE(UBAR(JMAX-1), RBAR(JMAX-1))
+      UBAR=0D0
+      RBAR=0D0
+
+      RETURN
+      END
---
+++ F95_syntax/program.f90
@@ -0,0 +1,4 @@
+! Early version of gfc2pips use to "forget" whole program ; handling only function and subroutine
+program toto
+    print*, "hello"
+end program toto
