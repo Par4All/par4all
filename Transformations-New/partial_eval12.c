f3138aef5a3d6ff738e3b7ddfdd06cf3794293c7
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-04-24 18:50:19 +0000

    Mostly updates for partial evaluation; some changes to hanfle Benchmarks/whetstone.c
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1781 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations-New/partial_eval12.c
+++ Transformations-New/partial_eval12.c
@@ -6,7 +6,13 @@
 int main()
 {
   int i;
+  i = (3)%(2);
+  printf("pos-pos 3%c2=%d (must be 1)\n", '%',i);
+  i = (-3)%(2);
+  printf("neg pos (-3)%c2=%d (must be -1)\n", '%', i);
+  i = (3)%(-2);
+  printf("pos neg 3%c(-2)%d (must be 1)\n", '%', i);
   i = (-3)%(-2);
-  printf("%d\n", i);
+  printf("neg neg (-3)%c(-2)=%d (must be -1)\n", '%', i);
   return i;
 }
--- Transformations-New/partial_eval13.f
+++ Transformations-New/partial_eval13.f
@@ -2,7 +2,16 @@ C bug seen in Transformations/eval.c: modulo evaluatiaon for negative arguments
 
       program main
 
+      i = mod(3, 2)
+      print *, i, "must be 1"
+
+      i = mod(-3, 2)
+      print *, i, "must be -1"
+
+      i = mod(3, -2)
+      print *, i, "must be 1"
+
       i = mod(-3, -2)
-      print *, i
+      print *, i, "must be -1"
 
       end
