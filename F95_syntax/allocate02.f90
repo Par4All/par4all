9af5c3bc915cebec16d844ab3edd89979ac26b87
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-04-27 16:43:08 +0000

    change test case name
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1808 67b5017d-e62d-0410-a871-f72b1c62810f

--- F95_syntax/allocate02.f90
+++ F95_syntax/allocate02.f90
@@ -1,12 +1,17 @@
 ! Try to parse and print again ALLOCATABLE & ALLOCATE
       SUBROUTINE ALLOCATABLE ()
-
+      
       ALLOCATABLE UBAR(:), RBAR(:)
       INTEGER ERR
       JMAX = 10
       ALLOCATE(UBAR(JMAX-1), RBAR(JMAX-1), STAT=ERR)
       UBAR=0D0
       RBAR=0D0
-
+      print *, ubar
+      print *, rbar
       RETURN
       END
+
+program main
+  call allocatable
+end
