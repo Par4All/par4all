07fc7829673bf81e8e9f3f7955395b7487ba3d24
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-04-28 14:32:26 +0000

    make a safer rm on object files and add a new test case on F90 loops
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1813 67b5017d-e62d-0410-a871-f72b1c62810f

--- F95_syntax/test1.f90
+++ F95_syntax/test1.f90
@@ -1,7 +1,5 @@
-      program test1
-
-      CHARACTER*8 :: UHF_STR
-      EQUIVALENCE (UHF, UHF_STR)
-      DATA UHF_STR/"UHF     "/
-
-      end
+program test1
+  CHARACTER*8 :: UHF_STR
+  EQUIVALENCE (UHF, UHF_STR)
+  DATA UHF_STR/"UHF     "/
+end program test1
