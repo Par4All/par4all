76627ff4bcd73e6503d14c5647fcacd614ed8d3b
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-04-30 11:07:36 +0000

    Updates in some Transformations tpips script to avoid displaying the header files after Beatrice Creusillet added the stdio.h includes in the corresponding source files
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1828 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/isolate02.c
+++ Transformations/isolate02.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 void lonesome_cowboy()
 {
-    int i,a[12];
-    for(i=0;i<12;i++)a[i]=1;
-isolate:
-    for(i=1;i<11;i++)
-        a[i]=0;
-    for(i=0;i<12;i++) printf("%d",a[i]);
+  int i,a[12];
+  for(i=0;i<12;i++)a[i]=1;
+ isolate:
+  for(i=1;i<11;i++)
+    a[i]=0;
+  for(i=0;i<12;i++) printf("%d",a[i]);
 }
 main()
 {
