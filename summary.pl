9a3e52408d21f8690e231b24178456b924704f2f
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-05-03 02:42:52 +0000

    improved summary
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1836 67b5017d-e62d-0410-a871-f72b1c62810f

--- summary.pl
+++ summary.pl
@@ -7,12 +7,17 @@
 my $failed = 0;
 my $changed = 0;
 my $passed = 0;
-my $warned = 0;
+my $skipped = 0;
+my $scripts = 0;
+my $sources = 0;
+my $broken = 0;
 
 my %failed = ();
 my %changed = ();
 my %passed = ();
-my %warned = ();
+my %skipped = ();
+my %scripts = ();
+my %sources = ();
 
 my %dir = ();
 
@@ -33,21 +38,45 @@ while (<>)
     $passed{$1}++;
     $dir{$1} = 1;
   }
+  elsif (/^broken-directory: (\w+)/) {
+    $broken++;
+    #$broken{$1}++;
+  }
   elsif (/^(skipped|multi-script|multi-source): (\w+)/)
   {
-    $warned++;
-    $warned{$1}++;
+    if ($1 eq 'skipped') {
+      $skipped++;
+      $skipped{$1}++;
+    }
+    elsif ($1 eq 'multi-script') {
+      $scripts++;
+      $scripts{$1}++;
+    }
+    elsif ($1 eq 'multi-source') {
+      $sources++;
+      $sources{$1}++;
+    }
+    else {
+      die "dead end, should not get there";
+    }
   }
 }
 
 my $count = $failed + $changed + $passed;
+my $not_passed = $failed + $changed;
+my $warned = $skipped + $scripts + $sources;
 
 printf
   "total: $count\n" .
-  "failed: $failed\n" .
-  "changed: $changed\n" .
-  "passed: $passed\n" .
+  " * passed: $passed\n" .
+  " * not passed: $not_passed\n" .
+  " - failed: $failed\n" .
+  " - changed: $changed\n" .
   "warned: $warned\n" .
+  " * skipped: $skipped\n" .
+  " * multi-script: $scripts\n" .
+  " * multi-source: $sources\n" .
+  "broken directory: $broken\n" .
   "success: %5.1f%%\n" .
   "\n",
   $passed*100.0/$count;
