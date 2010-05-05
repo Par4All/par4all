0cc32fbcd53197f98927fef81014ee5adf654d2b
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-05-05 13:51:33 +0000

    Add multiple implied-do in write check
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@1901 67b5017d-e62d-0410-a871-f72b1c62810f

--- F95_syntax/write.f90
+++ F95_syntax/write.f90
@@ -1,5 +1,6 @@
 program main
-     integer i, a(1:10), b(100:200)
+     integer i, j, a(1:10), b(100:200), c(1:10, 1:10)
+
 
 ! Print can be converted to write, but the format must be created
     print*,"hello world"
@@ -11,6 +12,9 @@ program main
 
 ! Try to handle also write with implied-do loop inside (this one was tricky)
     write (*,*) (a(i), i=1, 10), (b(i), i=100, 110)
+
+! Try to handle also write with mutliple implied-do loop inside (this one was even more tricky)
+    write (*,*) ((c(i, j), i=1, 10), j=1, 10)
 
 
 end program main
