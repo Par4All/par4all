c40037e6b6765b5277eb049dbf67f72b121c0c05
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2010-05-11 09:27:09 +0000

    Add support for pips exception in pyps: each time a user error is raised, it throws a RuntimeError python exception instead swig is reaaaaaaaly a nice tool :)
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@17042 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/pyps/pypips.c
+++ src/Passes/pyps/pypips.c
@@ -57,14 +57,13 @@ void atinit()
 
 void create(char* workspace_name, char ** filenames)
 {
+    /* create the array of arguments */
     gen_array_t filename_list = gen_array_make(0);
     while(*filenames)
     {
-        //printf("appending '%s'\n",*filenames);
         gen_array_append(filename_list,*filenames);
         filenames++;
     }
-
 
     if (workspace_exists_p(workspace_name))
         pips_user_error
@@ -80,25 +79,9 @@ void create(char* workspace_name, char ** filenames)
         {
             if (!create_workspace(filename_list))
             {
-                db_close_workspace(FALSE);
-                /* If you need to preserve the workspace
-                   for debugging purposes, use property
-                   ABORT_ON_USER_ERROR */
-                if(!get_bool_property("ABORT_ON_USER_ERROR")) {
-                    user_log("Deleting workspace...\n");
-                    delete_workspace(workspace_name);
-                }
+                db_close_workspace(false);
                 pips_user_error("Could not create workspace %s\n", 
                         workspace_name);
-            }
-
-            string main_module_name = get_first_main_module();
-
-            if (!string_undefined_p(main_module_name)) {
-                /* Ok, we got it ! Now we select it: */
-                user_log("Main module PROGRAM \"%s\" selected.\n",
-                        main_module_name);
-                lazy_open_module(main_module_name);
             }
         }
         else {
