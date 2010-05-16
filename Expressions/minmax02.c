3996bbf632158d65612f7b6c7370f3521690def3
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-05-16 21:10:21 +0000

    A few updates for prettyprinter improvements; more information about minmax01/02: I have no idea why the code added by Serge does not work for minmax01 and I do not understand minmax02
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2008 67b5017d-e62d-0410-a871-f72b1c62810f

--- Expressions/minmax02.c
+++ Expressions/minmax02.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+
+/* FI->SG: I do not understand why/how MAX should disappear after
+   partial evaluation */
+
 void main()
 {
-    int al,lo;
-    for(lo=0;lo<10;lo++)
-        al=MAX(al,lo);
-    printf("%d\n",al);
+  int al,lo;
+  for(lo=0;lo<10;lo++)
+    al=MAX(al,lo);
+  printf("%d\n",al);
 }
