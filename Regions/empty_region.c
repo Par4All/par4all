01ef96334c6413f15d620f4b76bdf4820645104a
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-05-20 13:12:18 +0000

    update empty_region for fi
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2041 67b5017d-e62d-0410-a871-f72b1c62810f

--- Regions/empty_region.c
+++ Regions/empty_region.c
@@ -4,6 +4,7 @@ int main() {
     for(j=0;j<5;j++)
     {
         for(i=j*4;i<MIN((j+1)*4,18);i++)
+            /* here, preconditions should be i >= j*4 , i<=(j+1)*4 , i <= 18 */
             a[i]=0;
         /* this loop should only write element that do not exist */
         for(i=MIN((j+1)*4,18);i<(j+1)*4;i++)
