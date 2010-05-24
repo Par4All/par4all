2adcfbbc45817e41a3463d0ff62810e5ee8f09e0
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-05-24 21:23:34 +0000

    add validation for capply in pyps
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2051 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pyps/capply01.f
@@ -0,0 +1,32 @@
+C     Check that all static initialization are taken into account:
+C     k==3 in OUTPUT
+
+C     Check also that dead paths in the call graph are not used: 
+C     l==1 in OUTPUT
+
+      program all04
+      common /bar/ k
+      call foo1(i)
+      print *, i, k
+      end
+
+      subroutine foo1(j)
+      j = 1
+      call output(j)
+      end
+
+      subroutine foo2(j)
+      common /bar/ k
+      data k /3/
+      j = 2
+      call foo3(j)
+      end
+
+      subroutine foo3(j)
+      call output(j)
+      end
+
+      subroutine output(l)
+      common /bar/ k
+      print *, l, k
+      end
