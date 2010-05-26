a2b289b7adc9cd18f0d4646fd8dca12780abf5a5
becher <becher@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-05-26 11:30:13 +0000

    add of test case for offsetof which induces a splitc_error
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2061 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ CIntrinsics-Bugs/offsetof01.c
@@ -0,0 +1,20 @@
+/* offsetof example : This macro with functional form returns the offset value in bytes of a member in the structure type.*/
+#include <stdio.h>
+#include <stddef.h>
+
+struct mystruct {
+  char singlechar;
+  char arraymember[10];
+  char anotherchar;
+};
+
+typedef struct mystruct str;
+
+int main ()
+{
+  printf ("offsetof(mystruct,singlechar) is %lu\n",offsetof(str,singlechar));
+  printf ("offsetof(mystruct,arraymember) is %lu\n",offsetof(str,arraymember));
+  printf ("offsetof(mystruct,anotherchar) is %lu\n",offsetof(str,anotherchar));
+  
+  return 0;
+}
