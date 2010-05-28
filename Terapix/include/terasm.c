4b2d3b03f6ca71fde6513226e8eeb71a81216bec
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-05-28 22:47:43 +0000

    update various validation for Terapix
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2071 67b5017d-e62d-0410-a871-f72b1c62810f

--- Terapix/include/terasm.c
+++ Terapix/include/terasm.c
@@ -12,9 +12,29 @@ TYPE OP(add,SUFF)(TYPE lhs, TYPE rhs)
 {
     return lhs=lhs+rhs;
 }
+TYPE OP(addr,SUFF)(TYPE lhs, TYPE *rhs)
+{
+    return lhs=lhs+*rhs;
+}
 TYPE OP(sub,SUFF)(TYPE lhs, TYPE rhs)
 {
     return lhs=lhs-rhs;
+}
+TYPE OP(subr,SUFF)(TYPE lhs, TYPE *rhs)
+{
+    return lhs=lhs-*rhs;
+}
+TYPE OP(lshift,SUFF)(TYPE lhs, TYPE rhs)
+{
+    return lhs=lhs<<rhs;
+}
+TYPE OP(rshift,SUFF)(TYPE lhs, TYPE rhs)
+{
+    return lhs=lhs>>rhs;
+}
+TYPE OP(prshift,SUFF)(TYPE *lhs, TYPE rhs)
+{
+    return *lhs=*lhs>>rhs;
 }
 TYPE OP(mul,SUFF)(TYPE lhs, TYPE rhs)
 {
@@ -24,12 +44,28 @@ TYPE OP(div,SUFF)(TYPE lhs, TYPE rhs)
 {
     return lhs=lhs/rhs;
 }
+TYPE OP(mulr,SUFF)(TYPE lhs, TYPE *rhs)
+{
+    return lhs=lhs**rhs;
+}
 TYPE OP(set,SUFF)(TYPE lhs, TYPE rhs)
 {
     return lhs=rhs;
 }
+TYPE OP(pset,SUFF)(TYPE *lhs, TYPE rhs)
+{
+    return *lhs=rhs;
+}
+TYPE OP(setp,SUFF)(TYPE lhs, TYPE *rhs)
+{
+    return lhs=*rhs;
+}
 TYPE OP(padd,SUFF)(TYPE *lhs, int rhs)
 {
-    return lhs=lhs[rhs];
+    return lhs=lhs+rhs;
+}
+TYPE OP(psub,SUFF)(TYPE *lhs, int rhs)
+{
+    return lhs=lhs-rhs;
 }
 #endif
