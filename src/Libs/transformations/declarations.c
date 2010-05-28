1ef26d78b5ce1e895442dc096457faebb4516144
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2010-05-28 22:37:50 +0000

    make clean_declarations far smarter: it now uses rw effects to remove some assignment statements, and so it clears far more declarations !
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@17245 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/transformations/declarations.c
+++ src/Libs/transformations/declarations.c
@@ -46,7 +46,93 @@
 #include "pipsdbm.h"
 #include "misc.h"
 #include "properties.h"
+#include "effects-generic.h"
+#include "alias-classes.h"
 #include "control.h" // for clean_up_sequences
+
+static void remove_unread_variable(statement s, entity e)
+{
+    if(statement_call_p(s)&&!declaration_statement_p(s))
+    {
+        list effects = load_cumulated_rw_effects_list(s);
+        bool entity_written = false;
+        FOREACH(EFFECT,eff,effects)
+            if(effect_write_p(eff) && entities_must_conflict_p(reference_variable(effect_any_reference(eff)),e))
+            { entity_written = true; break; }
+        if(entity_written) /* we only manage written entity in the form of e binary_op something else */
+        {
+            call c = statement_call(s);
+            expression lhs = binary_call_lhs(c);
+            if(expression_reference_p(lhs) && entities_may_conflict_p(reference_variable(expression_reference(lhs)),e))
+            {
+                list next = CDR(call_arguments(c));
+                if(ENDP(next))
+                    call_function(c) = entity_intrinsic(CONTINUE_FUNCTION_NAME);
+                else if( ENDP(CDR(next)) ) {
+                    expression rhs =  binary_call_rhs(c);
+                    if(expression_call_p(rhs))
+                    {
+                        call new_c = expression_call(rhs);
+                        syntax_call(expression_syntax(rhs))=call_undefined;
+                        free_call(c);
+                        instruction_call(statement_instruction(s))=new_c;
+                    }
+                    else {
+                        expression exp = copy_expression(rhs);
+                        free_instruction(statement_instruction(s));
+                        statement_instruction(s)=make_instruction_expression(exp);
+                    }
+                }
+                else {
+                    pips_internal_error("case unhadled yet\nfell free to contribute :-)\n");
+                }
+            }
+        }
+
+    }
+}
+static bool entity_may_conflict_with_a_formal_parameter_p(entity e, entity module)
+{
+    FOREACH(entity,d,entity_declarations(module))
+        if(formal_parameter_p(d) && entities_may_conflict_p(e,d)) return true;
+    return false;
+}
+
+typedef struct {
+    entity e;
+    bool result;
+} entity_used_somewhere_param;
+static void entity_used_somewhere_walker(statement s, entity_used_somewhere_param *p)
+{
+    list effects =load_cumulated_rw_effects_list(s);
+    FOREACH(EFFECT,eff,effects)
+        if(effect_read_p(eff) && entities_must_conflict_p(reference_variable(effect_any_reference(eff)),p->e))
+        { p->result = true; gen_recurse_stop(0); }
+}
+
+static bool entity_used_somewhere_p(entity e, statement in)
+{
+    entity_used_somewhere_param p = { e, false };
+    gen_context_recurse(in,&p,statement_domain,gen_true,entity_used_somewhere_walker);
+    return p.result;
+}
+static void remove_unread_variables(statement s)
+{
+    if(statement_block_p(s))
+    {
+        list effects = load_cumulated_rw_effects_list(s);
+        FOREACH(ENTITY,e,statement_declarations(s))
+        {
+            if(!entity_may_conflict_with_a_formal_parameter_p(e,get_current_module_entity()) && /* it is useless to try to remove formal parameters */
+                    !entity_pointer_p(e) ) /* and we cannot afford removing something that may implies aliasing */
+            {
+                bool entity_read = entity_used_somewhere_p(e,s);
+                if(!entity_read) /* the entity is never read it is disposable */
+                    gen_context_recurse(s,e,statement_domain,gen_true,remove_unread_variable);
+            }
+        }
+    }
+}
 
 /**
  * recursively call statement_remove_unused_declarations on all module statement
@@ -59,19 +145,21 @@ clean_declarations(char * module_name)
 {
   /* prelude*/
   set_current_module_entity(module_name_to_entity( module_name ));
-  set_current_module_statement
-    ((statement) db_get_memory_resource(DBR_CODE, module_name, TRUE) );
+  set_current_module_statement((statement) db_get_memory_resource(DBR_CODE, module_name, TRUE) );
+  set_cumulated_rw_effects((statement_effects)db_get_memory_resource(DBR_CUMULATED_EFFECTS,module_name,TRUE));
+
+  /* first remove any statement that writes only variable that are never read */
+  gen_recurse(get_current_module_statement(),statement_domain,gen_true,remove_unread_variables);
 
   /* body*/
-  entity_clean_declarations(get_current_module_entity(),
-			    get_current_module_statement());
+  entity_clean_declarations(get_current_module_entity(),get_current_module_statement());
 
-  gen_recurse(get_current_module_statement(),
-	      statement_domain, gen_true, statement_clean_declarations);
+  gen_recurse(get_current_module_statement(),statement_domain, gen_true, statement_clean_declarations);
 
   DB_PUT_MEMORY_RESOURCE(DBR_CODE, module_name, get_current_module_statement());
 
   /*postlude */
+  reset_cumulated_rw_effects();
   reset_current_module_entity();
   reset_current_module_statement();
   return true;
@@ -270,6 +358,7 @@ bool clean_unused_dynamic_variables(string module)
   clean_up_sequences(get_current_module_statement());
 
   // results
+  module_reorder(get_current_module_statement());
   DB_PUT_MEMORY_RESOURCE(DBR_CODE, module, get_current_module_statement());
 
   // cleanup
