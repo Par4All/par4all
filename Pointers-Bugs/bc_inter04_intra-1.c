4492b3459eae2438fb0961d2b3edd5300e16665e
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-05-31 08:24:45 +0000

    test case bc_inter04_intra-1 added
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2099 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers-Bugs/bc_inter04_intra-1.c
+++ Pointers-Bugs/bc_inter04_intra-1.c
@@ -4,27 +4,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void foo(int ***p1, int ***p2, int**q3, int *r4){
-  int **l;
-  l = *p2;
-  *p1 = q3;
-  *l = r4;
-}
-
 
 int main(){
   int ***a1;
   int ***a2;
-
+  
   int ** b3;
   int *c4;
+  int **l;
 
   a1 = (int ***) malloc(sizeof(int**));
   *a1 = (int **) malloc(sizeof(int*));
   **a1 = (int *) malloc(sizeof(int));
-
+  
   ***a1 = 1;
-
+  
   a2 = (int***) malloc(sizeof(int**));
   *a2 = *a1;
 
@@ -34,8 +28,10 @@ int main(){
 
   c4 = (int *) malloc(sizeof(int));
   *c4 = 4;
-
-  foo(a1, a2, b3, c4);
+  
+  l = *a2;
+  *a1 = b3;
+  *l = c4;
 
   printf("%d\n", ***a1);
   printf("%d\n", ***a2);
