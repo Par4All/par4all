0e519ea2ec97cd8ff466d9ebbd6bf9c36bb37e29
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2010-06-07 15:23:26 +0000

    upgrade statement insertion it is now based on out regions for dead code detection, and uses pragma extension to output its result so that further processings are made easier
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@17298 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/accel-util/statement_insertion.c
+++ src/Libs/accel-util/statement_insertion.c
@@ -64,7 +64,7 @@
 static Pcontrainte dimensions_to_psysteme(list dims,list phis)
 {
     pips_assert("as many dimensions as phis",gen_length(dims) == gen_length(phis));
-    Pvecteur vect_new = VECTEUR_NUL;
+    Pcontrainte pc = NULL;
     FOREACH(DIMENSION,dim,dims)
     {
         entity phi = ENTITY(CAR(phis));
@@ -79,93 +79,72 @@ static Pcontrainte dimensions_to_psysteme(list dims,list phis)
             Pvecteur vlower = vect_dup(normalized_linear(expression_normalized(lower)));
             Pvecteur vupper = vect_dup(normalized_linear(expression_normalized(upper)));
             vect_add_elem(&vlower,phi,1);
-            vect_add_elem(&vupper,phi,1);
+            vect_add_elem(&vupper,phi,-1);
             vect_chg_sgn(vlower);
-            for(Pvecteur iter=vlower;!VECTEUR_NUL_P(iter);iter=vecteur_succ(iter))
-                vect_add_elem(&vect_new,vecteur_var(iter),vecteur_val(iter));
-            for(Pvecteur iter=vupper;!VECTEUR_NUL_P(iter);iter=vecteur_succ(iter))
-                vect_add_elem(&vect_new,vecteur_var(iter),vecteur_val(iter));
-            vect_rm(vlower);
-            vect_rm(vupper);
+            vect_chg_sgn(vupper);
+            pc=contrainte_append(pc,contrainte_make(vlower));
+            pc=contrainte_append(pc,contrainte_make(vupper));
         }
         else {
-            vect_rm(vect_new);
+            contrainte_rm(pc);
             return NULL;
         }
         POP(phis);
     }
-    return contrainte_make(vect_new);
+    return pc;
 }
 
-/* returns true if the statement writes or read an already defined area */
-static bool statement_insertion_no_conflicting_access(list regions,enum action_utype tag)
+/* fixes statement declaration depending on region access */
+static void statement_insertion_fix_access(list regions)
 {
     FOREACH(REGION,reg,regions)
     {
         reference r = region_any_reference(reg);
         entity e = reference_variable(r);
-        Pcontrainte dims_sc = dimensions_to_psysteme(variable_dimensions(type_variable(entity_type(e))),reference_indices(r));
+        list phis = expressions_to_entities(reference_indices(r));
+        Pcontrainte dims_sc = dimensions_to_psysteme(variable_dimensions(type_variable(entity_type(e))), phis);
         Psysteme access_syst = region_system(reg);
-        Psysteme inter_syst = sc_dup(access_syst);
-        sc_add_inegalite(inter_syst,dims_sc);// there should not be any basis issue, they share the same ... */
-        volatile bool feasible;
+
+        volatile Psysteme stmp ,sr;
         CATCH(overflow_error)
-        {	
-            pips_debug(3, "overflow error \n");
-            feasible = true;
+        {
+            pips_debug(1, "overflow error\n");
+            return ;
         }
         TRY
-        {    
-            feasible = sc_integer_feasibility_ofl_ctrl(inter_syst,FWD_OFL_CTRL, true);
-            UNCATCH(overflow_error);
-
+        {
+            stmp = sc_make(contrainte_new(),CONTRAINTE_UNDEFINED);
+            for(Pcontrainte iter=dims_sc;!CONTRAINTE_UNDEFINED_P(iter);iter=contrainte_succ(iter)) {
+                Pcontrainte toadd = contrainte_make(vect_dup(contrainte_vecteur(iter)));
+                sc_add_inegalite(stmp,toadd);// there should not be any basis issue, they share the same ... */
+            }
+            sr = sc_cute_convex_hull(access_syst, stmp);
+            sc_rm(stmp);
             contrainte_rm(dims_sc);
-            if(feasible && (tag == is_action_write) ) {
-                sc_free(inter_syst);
-                pips_user_warning("inserted statement modifies the store");
-                return false;
+            sc_nredund(&sr);
+            UNCATCH(overflow_error);
+        }
+        /* if we reach this point, we are ready for backward translation from vecteur to dimensions :) */
+        list new_dimensions = NIL;
+        FOREACH(ENTITY,phi,phis)
+        {
+            Pcontrainte lower,upper;
+            constraints_for_bounds(phi, &sc_inegalites(sr), &lower, &upper);
+            if( !CONTRAINTE_UNDEFINED_P(lower) && !CONTRAINTE_UNDEFINED_P(upper))
+            {
+                expression elower = constraints_to_loop_bound(lower,phi,true,entity_intrinsic(DIVIDE_OPERATOR_NAME));
+                expression eupper = constraints_to_loop_bound(upper,phi,false,entity_intrinsic(DIVIDE_OPERATOR_NAME));
+                new_dimensions=CONS(DIMENSION, make_dimension(elower,eupper),new_dimensions);
             }
             else {
-                /*no interference with the store, go on and update dimensions to avoid out of bound exceptions*/
-                volatile Psysteme stmp ,sr;
-                CATCH(overflow_error)
-                {
-                    pips_debug(1, "overflow error\n");
-                    return false;
-                }
-                TRY
-                {
-                    stmp = sc_make(contrainte_new(),sc_inegalites(inter_syst));
-                    sr = sc_cute_convex_hull(access_syst, stmp);
-                    sc_rm(stmp);
-                    sc_nredund(&sr);
-                    UNCATCH(overflow_error);
-                }
-                /* if we reach this point, we are ready for backward translation from vecteur to dimensions :) */
-                list new_dimensions = NIL;
-                FOREACH(EXPRESSION,phi,reference_indices(r))
-                {
-                    Pcontrainte lower,upper;
-                    constraints_for_bounds(phi, &sc_inegalites(sr), &lower, &upper);
-                    if( !CONTRAINTE_UNDEFINED_P(lower) && !CONTRAINTE_UNDEFINED_P(upper))
-                    {
-                        expression elower = constraints_to_loop_bound(lower,phi,true,entity_intrinsic(DIVIDE_OPERATOR_NAME));
-                        expression eupper = constraints_to_loop_bound(upper,phi,false,entity_intrinsic(DIVIDE_OPERATOR_NAME));
-                        new_dimensions=CONS(DIMENSION, make_dimension(elower,eupper),new_dimensions);
-                    }
-                    else {
-                        pips_user_warning("failed to translate region\n");
-                        return false;
-                    }
-                }
-                new_dimensions=gen_nreverse(new_dimensions);
-                gen_full_free_list(variable_dimensions(type_variable(entity_type(e))));
-                variable_dimensions(type_variable(entity_type(e)))=new_dimensions;
+                pips_user_warning("failed to translate region\n");
             }
-
         }
+        new_dimensions=gen_nreverse(new_dimensions);
+        gen_full_free_list(variable_dimensions(type_variable(entity_type(e))));
+        variable_dimensions(type_variable(entity_type(e)))=new_dimensions;
+        gen_free_list(phis);
     }
-        return true;
 }
 
 static bool do_statement_insertion(statement s)
@@ -182,17 +161,50 @@ static bool do_statement_insertion(statement s)
         }
         else {
             FOREACH(STATEMENT,flagged_statement,flagged_statements) {
-                list regions = load_cumulated_rw_effects_list(flagged_statement);
-                list read_regions = regions_read_regions(regions);
-                list write_regions = regions_write_regions(regions);
+                list out_regions = effects_effects(load_proper_rw_effects(flagged_statement));
+                if(ENDP(out_regions))
+                {
+                    /* make sure all data access are ok by building the convex union of data access and data declarations */
+                    list regions = load_cumulated_rw_effects_list(flagged_statement);
+                    statement_insertion_fix_access(regions);
+                    /* update pragma */
+                    FOREACH(EXTENSION,ext,extensions_extension(statement_extensions(flagged_statement)))
+                    {
+                        if(pragma_string_p(extension_pragma(ext)))
+                        {
+                            string str_pragma = pragma_string(extension_pragma(ext));
+                            if(strstr( str_pragma , inserted_pragma ))
+                            {
+                                free(str_pragma);
+                                pragma_string(extension_pragma(ext))=strdup(get_string_property("STATEMENT_INSERTION_SUCCESS_PRAGMA"));
+                            }
+                        }
+                    }
+                    return true;
+                }
+                else
+                {
+                    pips_user_warning("inserted statment has out effects");
+                    /* update pragma */
+                    FOREACH(EXTENSION,ext,extensions_extension(statement_extensions(flagged_statement)))
+                    {
+                        if(pragma_string_p(extension_pragma(ext)))
+                        {
+                            string str_pragma = pragma_string(extension_pragma(ext));
+                            if(strstr( str_pragma , inserted_pragma ))
+                            {
+                                free(str_pragma);
+                                pragma_string(extension_pragma(ext))=strdup(get_string_property("STATEMENT_INSERTION_FAILURE_PRAGMA"));
+                            }
+                        }
+                    }
+                }
 
-                statement_insertion_no_conflicting_access(write_regions,is_action_write);
-                statement_insertion_no_conflicting_access(read_regions,is_action_read);
             }
         }
 
     }
-        return false;
+    return false;
 }
 
 bool statement_insertion(const char *module_name)
@@ -201,6 +213,9 @@ bool statement_insertion(const char *module_name)
     set_current_module_entity(module_name_to_entity( module_name ));
     set_current_module_statement((statement) db_get_memory_resource(DBR_CODE, module_name, true) );
     set_cumulated_rw_effects((statement_effects)db_get_memory_resource(DBR_REGIONS, module_name, true));
+    set_proper_rw_effects((statement_effects)db_get_memory_resource(DBR_OUT_REGIONS, module_name, true));
+    debug_on("STATEMENT_INSERTION_DEBUG_LEVEL");
+
     /* do */
     if(do_statement_insertion(get_current_module_statement()))
     {
@@ -209,8 +224,11 @@ bool statement_insertion(const char *module_name)
         DB_PUT_MEMORY_RESOURCE(DBR_CODE, module_name,get_current_module_statement());
     }
 
+    debug_off();
     reset_current_module_entity();
     reset_current_module_statement();
     reset_cumulated_rw_effects();
+    reset_proper_rw_effects();
     return true;
 }
+
