e8362a037822b421dd8fd931f966f31036933725
becher <becher@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-06-15 15:07:29 +0000

    Remove of the initialization of complex numbers with I
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2158 67b5017d-e62d-0410-a871-f72b1c62810f

--- CIntrinsics-Bugs/long01.c
+++ CIntrinsics-Bugs/long01.c
@@ -10,11 +10,11 @@ int main()
   long long int e = 3669993212;
   double f = 2560.000;
   long double g = 90.500l;
-  float complex h = 2.1 + 7.7*I;
-  double complex z = 42.1550 + 42.2000*I;
-  long double complex y = 8.6523l + 98.23541l*I;
+  float complex h; // = 2.1 + 7.7*I;
+  double complex z;  //= 42.1550 + 42.2000*I;
+  long double complex y; // = 8.6523l + 98.23541l*I;
  	   
-  printf("float complex h= %f + %fI, double complex z= %f + %fI, long double complex y= %Lf + %LfI \n", crealf(h), cimagf(h), creal(z), cimag(z), creall(y), cimagl(y));  
+  //printf("float complex h= %f + %fI, double complex z= %f + %fI, long double complex y= %Lf + %LfI \n", crealf(h), cimagf(h), creal(z), cimag(z), creall(y), cimagl(y));  
   printf ("int a=%d, long b=%ld, long int c=%ld, long long d=%lld, long long int e=%lld, double f=%f, long double g=%Lf\n",a,b,c,d,e,f,g);
   return 0;
 }
