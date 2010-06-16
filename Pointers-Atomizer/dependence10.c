9afae8af27d7c934c44bc8a2d6c1cea2fdcd46eb
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-06-16 09:29:04 +0000

    Applying atomizer and unfolding to Pointers-Bugs test cases
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2161 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pointers-Atomizer/dependence10.c
@@ -0,0 +1,20 @@
+*****************************************************************************
+ * X - MULTIPLE PATH
+ ****************************************************************************/
+void dependence10( int flag, int *a ) {
+  int *p;
+  int b[10];
+  int i;
+
+  // Two paths
+  if( flag)
+    p = a;
+  else
+    p = b;
+
+  // p may points to a[0] or b[0];
+
+  for ( i = 0; i < 10; i++ ) {
+    p[i] = 0; // We may write a or b, but only a is visible from outside
+  }
+}
