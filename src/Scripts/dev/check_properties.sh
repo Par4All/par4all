569d73a7a919c6b8f673ebe75041acbe3e190b4c
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2010-06-19 15:06:12 +0000

    Take into account not grep-able property from HPFC in check_properties.h
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@17381 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Scripts/dev/check_properties.sh
+++ src/Scripts/dev/check_properties.sh
@@ -6,6 +6,8 @@ by serge guelton o(^_-)O
 ==========================
 EOF
 
+
+
 if ! test -f "$1" || ! test `basename "$1"` = properties.rc ; then
 	echo "must be called with the property file as first parameter" 1>&2
 	exit 1
@@ -20,7 +22,7 @@ all_sources="`find $2/src -name '*.[chly]'`"
 sed -e '/^$/ d' $1 | cut -d ' ' -f 1 | \
 while read line ; do
 	if test "$line" ; then
-		if ! grep -q $line $all_sources ; then
+		if ! grep -q $line $all_sources && ! grep -q $line $0; then
 			echo "$line never used"
 			all_ok=1
 		fi
@@ -28,3 +30,9 @@ while read line ; do
 done 
 
 test $all_ok = 0 && echo "nothing found" 1>&2
+exit $all_ok
+
+# use a white list because some directives are not grepable
+HPFC_IGNORE_FCD_SYNCHRO
+HPFC_IGNORE_FCD_SET
+HPFC_IGNORE_FCD_TIME
