1d621d5743ccdf83369a6a8d265d65255f97553b
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-06-29 09:36:31 +0000

    modify example to validate only on thing at a time. On one hand omp and on the other hand all_private variables
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2214 67b5017d-e62d-0410-a871-f72b1c62810f

--- Prettyprint/all_privates.c
+++ Prettyprint/all_privates.c
@@ -1,3 +1,5 @@
+#include "stdio.h"
+
 int main () {
   float a[10][10][10][10][10];
   int i,j,k,l,m;
@@ -18,5 +20,21 @@ int main () {
       }
     }
   }
+
+  // use the value of the array to prevent pips doing optilization on unused
+  // values
+  for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++) {
+      for (k = 0; k < 10; k++) {
+	z = k * 2.0;
+	for (l = 0; l < 10; l++) {
+	  for (m = 0; m < 10; m++) {
+	    fprintf (stdout, "%f", a[i][j][k][l][m]);
+	  }
+	}
+      }
+    }
+  }
+
   return 0;
 }
