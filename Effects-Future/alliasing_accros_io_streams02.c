ee36e204f31867afaa293c561dde7a6b96298734
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-06-29 09:44:19 +0000

    adding examples for beatrice
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2215 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Effects-Future/alliasing_accros_io_streams02.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+
+int main () {
+  float a[10][10][10][10][10];
+  int i,j,k,l,m;
+  float x = 2.12;
+  float z = 0.0;
+  float y = 2.0;
+
+  for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++) {
+      for (k = 0; k < 10; k++) {
+	z = k * 2.0;
+	for (l = 0; l < 10; l++) {
+	  for (m = 0; m < 10; m++) {
+	    y = 3.5 + x + z;
+	    a[i][j][k][l][m] = x*y;
+	  }
+	}
+      }
+    }
+  }
+
+  // use the value of the array to prevent pips doing optilization on unused
+  // values
+  for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++) {
+      for (k = 0; k < 10; k++) {
+	for (l = 0; l < 10; l++) {
+	  for (m = 0; m < 10; m++) {
+	    fprintf (stdout, "%f", a[i][j][k][l][m]);
+	  }
+	}
+      }
+    }
+  }
+
+  return 0;
+}
