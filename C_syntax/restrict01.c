40e82a3db3e7e10cbc58320f69667cd6f7d88ac7
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-07-06 22:38:12 +0000

    test case restrict01.c added for pre c99 keyword __const and __restrict
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2234 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ C_syntax/restrict01.c
@@ -0,0 +1,11 @@
+/* Check prettyprint of restrict */
+
+/* non standard version: void restrict01(int * __restrict p, int * __restrict q);*/
+/* C99 version: void restrict01(int * restrict p, int * restrict q);*/
+
+void restrict01(int * restrict p, int * restrict q);
+
+void restrict01a(const int * restrict p, int * const q);
+
+void restrict01b(__const int * __restrict p, int * __const q);
+
