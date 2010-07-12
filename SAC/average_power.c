39663e2bbd6bfce6ae23c0b383fae292360fbe7e
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-07-12 07:21:09 +0000

    Upload new version of average_power.c.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2246 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ SAC/average_power.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+// Define complex number
+typedef struct {
+    float re;
+    float im;
+} Cplfloat;
+
+void average_power(int Nth, int Nrg, int Nv, Cplfloat ptrin[Nth][Nrg][Nv],
+        Cplfloat Pow[Nth]) {
+
+    float PP;
+    int th, v, rg;
+
+    for (th=0; th<Nth; th++) {
+        PP=0.;
+        for (rg=0; rg<Nrg; rg++) {
+            for (v=0; v<Nv; v++) {
+                PP += ptrin[th][rg][v].re *ptrin[th][rg][v].re
+                    +ptrin[th][rg][v].im *ptrin[th][rg][v].im;
+            }
+        }
+        Pow[th].re= (float)(PP/((float)(Nv*Nrg)));
+        Pow[th].im= 0.;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int i,j,k;
+    int th,rg,v;
+    th=16,rg=13,v=12;
+    {
+        Cplfloat in[th][rg][v],pow[th];
+        for(i=0;i<th;i++)
+            for(j=0;j<rg;j++)
+                for(k=0;k<v;k++)
+                {
+                    in[i][j][k].re=i*j*k;
+                    in[i][j][k].im=i*j+k;
+                }
+        average_power(th,rg,v,in,pow);
+        /* only print with bad precision for validation */
+        for(i=0;i<th;i++)
+            printf("-%d-%d-", ((int)pow[i].re)/10, ((int)pow[i].im))/10;
+    }
+    return 0;
+}
+
