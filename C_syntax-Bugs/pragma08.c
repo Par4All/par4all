a3619c9408f981c2c1b1ff6f40de84597fb2f593
keryell <keryell@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-07-20 17:32:13 +0000

    Updated validation to cope with corrected #pragma with label prettyprinting in C
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2261 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax-Bugs/pragma08.c
+++ C_syntax-Bugs/pragma08.c
@@ -9,22 +9,4 @@ void pragma01()
 
     j = i;
   }
-
-   #  pragma omp parallel for
-  for(i = 0; i < 10; i++) {
-    int j = i + 1;
-  }
-  /* Some comment */
-   #  \
- pragma omp parallel for
-   // And other
-  for(i = 0; i < 10; i++) {
-    int j = i + 1;
-  }
-# pragma omp \
-  parallel\
-  for
-  for(i = 0; i < 10; i++) {
-    int j = i + 1;
-  }
 }
