b0a7030a3b45b0f22d00ba974bf702b29afac2cb
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2010-07-21 10:34:45 +0000

    Straightforward port of dotprod_c.tpips completed.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@17595 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/sac/sac.py
+++ src/Libs/sac/sac.py
@@ -6,17 +6,93 @@ modulename = "DOTPROD"
 wsname = "dotprod_c"
 
 sources = ["kernels/%s/%s.c" % (modulename, modulename), "include/SIMD.c"]
-shutil.rmtree(wsname + ".database")
+try:
+    shutil.rmtree(wsname + ".database")
+except:
+    pass
+
 ws = workspace(sources, name = wsname)
 ws.set_property(ABORT_ON_USER_ERROR = True)
 
+print "Initial code"
 dotprod = ws['dotprod']
-
-print "initial code"
+print "Module dotprod selected"
 dotprod.display()
 
-print "simdized code"
 dotprod.split_update_operator()
+
+# benchmark.tpips.h begin
+ws.activate("MUST_REGIONS")
+ws.activate("PRECONDITIONS_INTER_FULL")
+ws.activate("TRANSFORMERS_INTER_FULL")
+
+ws.set_property(RICEDG_STATISTICS_ALL_ARRAYS = True)
+ws.activate("RICE_SEMANTICS_DEPENDENCE_GRAPH")
+
+ws.set_property(SIMD_FORTRAN_MEM_ORGANISATION = False)
+ws.set_property(SAC_SIMD_REGISTER_WIDTH = 128)
+ws.set_property(SIMDIZER_AUTO_UNROLL_SIMPLE_CALCULATION = False)
+ws.set_property(SIMDIZER_AUTO_UNROLL_MINIMIZE_UNROLL = False)
+ws.set_property(PRETTYPRINT_ALL_DECLARATIONS = True)
+
+dotprod.split_update_operator()
+
+dotprod.display()
+print ws.get_property("IF_CONVERSION_INIT_THRESHOLD")
+dotprod.if_conversion_init()
+dotprod.display()
+
+dotprod.if_conversion()
+dotprod.display()
+
+dotprod.if_conversion_compact()
+#dotprod.use_def_elimination()
+dotprod.display()
+
+dotprod.partial_eval()
+dotprod.simd_atomizer()
+dotprod.display()
+
+dotprod.simdizer_auto_unroll()
+dotprod.partial_eval()
+dotprod.clean_declarations()
+dotprod.suppress_dead_code()
+dotprod.display()
+#make DOTDG_FILE
+dotprod.simd_remove_reductions()
+dotprod.display()
+
+#dotprod.deatomizer()
+#dotprod.partial_eval()
+#dotprod.use_def_elimination()
+#dotprod.display()
+
+dotprod.print_dot_dependence_graph()
+dotprod.single_assignment()
+
+dotprod.display()
+
+dotprod.simdizer()
+
+dotprod.display()
+
+#dotprod.use_def_elimination()
+#dotprod.display()
+
+dotprod.simd_loop_const_elim()
+#setproperty EOLE_OPTIMIZATION_STRATEGY "ICM"
+#dotprod.optimize_expressions()
+#dotprod.partial_redundancy_elimination()
+dotprod.display()
+
+#dotprod.use_def_elimination()
+dotprod.clean_declarations()
+dotprod.suppress_dead_code()
+dotprod.display()
+
+# benchmark.tpips.h end
+
+print "simdized code"
 dotprod.display()
 
 dotprod.unsplit()
