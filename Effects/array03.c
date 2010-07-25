0441adc94fbaddac9f699a430a5ff63733a6a5da
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-07-25 14:58:20 +0000

    useless SPACE characters removed
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2277 67b5017d-e62d-0410-a871-f72b1c62810f

--- Effects/array03.c
+++ Effects/array03.c
@@ -13,7 +13,7 @@ int foo(float (*b)[M])
   c = (*b)[3];
   b[1][3] = 2.0;
   c = b[1][3];
-  
+
   ((*b)[3])++;
   (*b)[3] += 5.0;
   (b[1][3])++;
@@ -23,13 +23,13 @@ int foo(float (*b)[M])
 }
 
 int foo2(float b[N][M])
-{ 
+{
   float c;
   (*b)[3] = 2.0;
   c = (*b)[3];
   b[1][3] = 2.0;
   c = b[1][3];
-  
+
   ((*b)[3])++;
   (*b)[3] += 5.0;
   (b[1][3])++;
@@ -39,13 +39,13 @@ int foo2(float b[N][M])
 }
 
 int foo3()
-{ 
+{
   float c;
   (*d)[3] = 2.0;
   c = (*d)[3];
   d[1][3] = 2.0;
   c = d[1][3];
-  
+
   ((*d)[3])++;
   (*d)[3] += 5.0;
   (d[1][3])++;
@@ -56,13 +56,13 @@ int foo3()
 
 
 
-int main() 
+int main()
 {
   float a[N][M], ret;
-  
+
   ret = foo(a);
   ret = foo2(a);
   ret = foo3();
-  
+
   return 1;
 }
