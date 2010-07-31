fc6c70d0aa5b46ff9d33d943eba3768a9f2240e2
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-07-31 15:37:53 +0000

    Accept stuff related to changes linked to private variables
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2297 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics/yang01.c
+++ Semantics/yang01.c
@@ -1,10 +1,20 @@
-/* Model Checking Sequential Software Programs
+/* Model Checking Sequential Software Programs via mixed symbolic
+   analysis
+   Z. Yang & al.
    ACM Trans. on Design Automation of Electronic Systems
    26 pages, article 10, 2009, v. 14, n. 1
 
-   Check te example in the paper above. foo() is never exited under
+   Check the example in the paper above. foo() is never exited under
    the present preconditions after inlining.
 */
+
+
+#include <stdio.h>
+
+main()
+{
+  bar();
+}
 
 int bar() {
   int x = 3;
@@ -13,12 +23,17 @@ int bar() {
   while(x<=4) {
     y++;
     x = foo(x);
+    // Execution: (x,y) = (4,1) and then (5,2) and the loop is exited
+    printf("In loop: x=%d, y=%d\n", x, y);
   }
   y = foo(y);
 
+  // Execution: (x,y) = (5,3)
+  printf("At the end: x=%d, y=%d\n", x, y);
   return y;
 }
 
+/* In the context used, this functions returns s+1 */
 int foo(int s)
 {
   int t = s + 2;
