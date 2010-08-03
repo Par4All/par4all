1a8b41cb2c18d9bbe025178453e7777b5b26a9de
guelton <guelton@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
2010-08-03 14:54:31 +0000

    Fix warning in implementation of HASH_FOREACH
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@1226 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- src/genC/newgen_hash.h
+++ src/genC/newgen_hash.h
@@ -81,7 +81,7 @@ typedef int (* hash_equals_t)(const void *, const void *);
     key_type k;\
     value_type v;\
     while((UNIQUE_NAME =\
-                 hash_table_scan(h,UNIQUE_NAME,&k,&v)))
+                 hash_table_scan(h,UNIQUE_NAME,(void**)&k,(void**)&v)))
 
 
 // hash_put_or_update() uses the warn_on_redefinition
