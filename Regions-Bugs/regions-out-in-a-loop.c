77a261cdbe4cc10deab4f49f2609fb63f10978eb
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-08-11 13:55:53 +0000

    Improve the example readability
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2319 67b5017d-e62d-0410-a871-f72b1c62810f

--- Regions-Bugs/regions-out-in-a-loop.c
+++ Regions-Bugs/regions-out-in-a-loop.c
@@ -3,32 +3,44 @@
 
 
 #define SIZE 100000
-#define T_MAX 100
-#define DT 0.1
 
-void compute( int a[SIZE], int b[SIZE], int c[SIZE]  ) {
+
+/* The two following functions are identical, only the call site changes */
+void compute_seq( int a[SIZE], int b[SIZE], int c[SIZE]  ) {
   int i;
   for(i=0; i<SIZE; i++) {
     a[i] += b[i]+c[i];
   } 
 }
 
-void compute_clone( int a[SIZE], int b[SIZE], int c[SIZE]  ) {
+void compute_loop( int a[SIZE], int b[SIZE], int c[SIZE]  ) {
   int i;
   for(i=0; i<SIZE; i++) {
     a[i] += b[i]+c[i];
   } 
 }
+
+
+/* Here we simply call two time the "compute" function sequentially */
+void sequential() {
+  int a[SIZE],b[SIZE],c[SIZE];
+  compute_seq(a,b,c);
+  compute_seq(a,b,c);
+}
+
+
+/* Here we call two time the "compute" function, but with a loop */
+void loop() {
+  int i;
+  int a[SIZE],b[SIZE],c[SIZE];
+  for(i = 0; i< 0.2; i+= 0.1 ) {
+    compute_loop(a,b,c);
+  }
+}
+
 
 
 int main() {
-  int a[SIZE],b[SIZE],c[SIZE];
-  int i;
-
-  compute(a,b,c);
-
-  for(i = 0; i< T_MAX; i+= DT ) {
-    compute_clone(a,b,c);
-  }
-
+  loop();
+  sequential();
 }
