afb2a181a23551b0536c1025783eaeaeb0fa32b4
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-08-12 06:16:09 +0000

    read and write effects of the incrementation expression in a for loop should be may effects, not must effects when the value of the loop exit condition cannot be evaluated
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2324 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Effects-Bugs/for02.c
@@ -0,0 +1,15 @@
+/* Check that parallel loops located inside a non-convertibal for foop are parallelized */
+
+
+void for02(int n)
+{
+  int j;
+  float x[100];
+  float t, delta_t, t_max;
+
+  for(t = 0.0; t<t_max; t += delta_t) {
+    for(j=0;j<100;j++) {
+      x[j] = 0.;
+    }
+  }
+}
