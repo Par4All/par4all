8f4bac04d36afd01eb9a4277aaacb8bd8cd83f23
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-08-17 15:16:06 +0000

    licence plate version with copies.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2329 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ HardwareAccelerator/license_plate_copies.c
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include "freia.h"
+
+// short image names for graph display
+#define imin in
+#define imoutrepl out
+
+int license_plate_copies(void)
+{
+   freia_dataio fdin;
+   freia_dataio fdout;
+
+   freia_data2d *imin;
+   freia_data2d *immir;
+   freia_data2d *imopen;
+   freia_data2d *imclose;
+   freia_data2d *imopenth;
+   freia_data2d *imcloseth;
+   freia_data2d *imand;
+   freia_data2d *imfilt;
+   freia_data2d *imout;
+   freia_data2d *imoutrepl;
+   
+   const  int32_t kernel1x3[9] = {0, 0, 0, 1, 1, 1, 0, 0, 0};
+   const  int32_t kernel3x1[9] = {0, 1, 0, 0, 1, 0, 0, 1, 0};
+
+   freia_status _return0, _return1, _return2, _return3, _return4;
+   freia_data2d *imtmp_0;
+   freia_status ret_0;
+   int i_0;
+   freia_data2d *imtmp_1;
+   freia_status ret_1;
+   int i_1;
+   freia_data2d *imtmp_2;
+   freia_status ret_2;
+   int i_2;
+   freia_data2d *imtmp_3;
+   freia_status ret_3;
+   int i_3, i_4;
+   
+   /* open videos flow */
+   freia_common_open_input(&fdin, 0);
+   freia_common_open_output(&fdout, 0, fdin.framewidth, fdin.frameheight, 8);
+   
+   /* images creation */
+   imin = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   immir = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   imopen = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   imclose = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   imopenth = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   imcloseth = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   imand = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   imfilt = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   imout = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   imoutrepl = freia_common_create_data(16, fdin.framewidth, fdin.frameheight);
+   
+   /* processing */
+   freia_common_rx_image(imin, &fdin);
+
+   imtmp_0 = freia_common_create_data(imopen->bpp, imopen->widthWa, imopen->heightWa);
+   
+   ret_0 = freia_aipo_copy(imtmp_0, imin);
+
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_erode_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   i_0 = 15;
+
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   ret_0 |= freia_aipo_dilate_8c(imopen, imtmp_0, kernel1x3);
+   ret_0 |= freia_aipo_copy(imtmp_0, imopen);
+   i_0 = 15;
+
+   freia_common_destruct_data(imtmp_0);
+   
+   _return1 = ret_0;
+
+   imtmp_1 = freia_common_create_data(imclose->bpp, imclose->widthWa, imclose->heightWa);
+
+   ret_1 = freia_aipo_copy(imtmp_1, imin);
+
+   ret_1 |= freia_aipo_dilate_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_dilate_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_dilate_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_dilate_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_dilate_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_dilate_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_dilate_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_dilate_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   i_1 = 8;
+
+   ret_1 |= freia_aipo_erode_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_erode_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_erode_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_erode_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_erode_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_erode_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_erode_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   ret_1 |= freia_aipo_erode_8c(imclose, imtmp_1, kernel1x3);
+   ret_1 |= freia_aipo_copy(imtmp_1, imclose);
+   i_1 = 8;
+
+   freia_common_destruct_data(imtmp_1);
+   
+   _return0 = ret_1;
+
+   freia_aipo_threshold(imopenth, imopen, 1, 50, 1);
+   freia_aipo_threshold(imcloseth, imclose, 150, 255, 1);
+
+   freia_aipo_and(imand, imopenth, imcloseth);
+
+   imtmp_2 = freia_common_create_data(imfilt->bpp, imfilt->widthWa, imfilt->heightWa);
+   
+   ret_2 = freia_aipo_copy(imtmp_2, imand);
+
+   ret_2 |= freia_aipo_erode_8c(imfilt, imtmp_2, kernel3x1);
+   ret_2 |= freia_aipo_copy(imtmp_2, imfilt);
+   ret_2 |= freia_aipo_erode_8c(imfilt, imtmp_2, kernel3x1);
+   ret_2 |= freia_aipo_copy(imtmp_2, imfilt);
+   ret_2 |= freia_aipo_erode_8c(imfilt, imtmp_2, kernel3x1);
+   ret_2 |= freia_aipo_copy(imtmp_2, imfilt);
+   ret_2 |= freia_aipo_erode_8c(imfilt, imtmp_2, kernel3x1);
+   ret_2 |= freia_aipo_copy(imtmp_2, imfilt);
+   i_2 = 4;
+
+   ret_2 |= freia_aipo_dilate_8c(imfilt, imtmp_2, kernel3x1);
+   ret_2 |= freia_aipo_copy(imtmp_2, imfilt);
+   ret_2 |= freia_aipo_dilate_8c(imfilt, imtmp_2, kernel3x1);
+   ret_2 |= freia_aipo_copy(imtmp_2, imfilt);
+   ret_2 |= freia_aipo_dilate_8c(imfilt, imtmp_2, kernel3x1);
+   ret_2 |= freia_aipo_copy(imtmp_2, imfilt);
+   ret_2 |= freia_aipo_dilate_8c(imfilt, imtmp_2, kernel3x1);
+   ret_2 |= freia_aipo_copy(imtmp_2, imfilt);
+   i_2 = 4;
+
+   freia_common_destruct_data(imtmp_2);
+   
+   _return2 = ret_2;
+
+   imtmp_3 = freia_common_create_data(imout->bpp, imout->widthWa, imout->heightWa);
+   
+   ret_3 = freia_aipo_copy(imtmp_3, imfilt);
+
+   ret_3 |= freia_aipo_erode_8c(imout, imtmp_3, kernel1x3);
+   ret_3 |= freia_aipo_copy(imtmp_3, imout);
+   ret_3 |= freia_aipo_erode_8c(imout, imtmp_3, kernel1x3);
+   ret_3 |= freia_aipo_copy(imtmp_3, imout);
+   ret_3 |= freia_aipo_erode_8c(imout, imtmp_3, kernel1x3);
+   ret_3 |= freia_aipo_copy(imtmp_3, imout);
+   ret_3 |= freia_aipo_erode_8c(imout, imtmp_3, kernel1x3);
+   ret_3 |= freia_aipo_copy(imtmp_3, imout);
+   i_3 = 4;
+
+   ret_3 |= freia_aipo_dilate_8c(imout, imtmp_3, kernel1x3);
+   ret_3 |= freia_aipo_copy(imtmp_3, imout);
+   ret_3 |= freia_aipo_dilate_8c(imout, imtmp_3, kernel1x3);
+   ret_3 |= freia_aipo_copy(imtmp_3, imout);
+   ret_3 |= freia_aipo_dilate_8c(imout, imtmp_3, kernel1x3);
+   ret_3 |= freia_aipo_copy(imtmp_3, imout);
+   ret_3 |= freia_aipo_dilate_8c(imout, imtmp_3, kernel1x3);
+   ret_3 |= freia_aipo_copy(imtmp_3, imout);
+   i_3 = 4;
+
+   freia_common_destruct_data(imtmp_3);
+   
+   _return3 = ret_3;
+   
+   freia_aipo_dilate_8c(imoutrepl, imout, freia_morpho_kernel_8c);
+   freia_aipo_dilate_8c(imoutrepl, imoutrepl, freia_morpho_kernel_8c);
+   freia_aipo_dilate_8c(imoutrepl, imoutrepl, freia_morpho_kernel_8c);
+   i_4 = 3;
+   
+   _return4 = 0;
+
+   freia_aipo_and(imoutrepl, imoutrepl, imin);
+   freia_common_tx_image(imoutrepl, &fdout);
+   
+   /* images destruction */
+   freia_common_destruct_data(imin);
+   freia_common_destruct_data(immir);
+   freia_common_destruct_data(imopen);
+   freia_common_destruct_data(imclose);
+   freia_common_destruct_data(imopenth);
+   freia_common_destruct_data(imcloseth);
+   freia_common_destruct_data(imand);
+   freia_common_destruct_data(imfilt);
+   freia_common_destruct_data(imout);
+   freia_common_destruct_data(imoutrepl);
+   
+   /* close videos flow */
+   freia_common_close_input(&fdin);
+   freia_common_close_output(&fdout);
+
+   return 0;
+}
