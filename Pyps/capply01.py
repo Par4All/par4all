7845281e2f8fee64754f839717121c7f0baacd17
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-08-18 07:04:47 +0000

    adding a pyps exemple. This one shows who to get cumulated effects printed out
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2331 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pyps/capply01.py
+++ Pyps/capply01.py
@@ -1,7 +1,7 @@
 # convertion of Semantics/all04.tpips into pyps
 # tests usage of capply
 from pyps import *
-w = workspace(['capply01.f'],activates=['TRANSFORMERS_INTER_FULL','INTERPROCEDURAL_SUMMARY_PRECONDITION','PRECONDITIONS_INTER_FULL'])
+w = workspace(['capply01.f'],activates=['TRANSFORMERS_INTER_FULL','INTERPROCEDURAL_SUMMARY_PRECONDITION','PRECONDITIONS_INTER_FULL','PRINT_CODE_CUMULATED_EFFECTS'])
 print " Initial code with preconditions for ALL04 after cleanup"
 
 w.all.display()
