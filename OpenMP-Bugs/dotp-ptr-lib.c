d63d93c04568690e09547309fcce5c5e6a8f481a
darsch <darsch@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-08-18 14:27:42 +0000

    added double pragme bug - ticket on its way
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2335 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ OpenMP-Bugs/dotp-ptr-lib.c
@@ -0,0 +1,26 @@
+// parallelization of a never called function
+
+typedef double t_real;
+
+t_real dotp_ptr_lib (const long size, const t_real* x) {
+  long   i;
+  t_real result = 0.0;
+
+  // compare loop
+  for (i = 0; i < size; i++) {
+    result += x[i] * x[i];
+  }
+  return result;
+}
+
+
+int main () {
+  long    i;
+  t_real  v[100];
+
+  // load vector
+  for (i = 0; i < 100; i++) v[i] = 1.0;
+
+  // exit without doing anything
+  return 0;
+}
