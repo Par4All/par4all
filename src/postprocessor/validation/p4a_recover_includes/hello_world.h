137043a0680f1093a6638a005c815f6ff6decdb1
Ronan Keryell <Ronan.Keryell@hpc-project.com>
2010-08-25 18:18:11 +0200

    Recover any #include, not only standard ones.
    Needed by Onil for parallelization Scilab2C and is useful for everybody anyway.

---
+++ src/postprocessor/validation/p4a_recover_includes/hello_world.h
@@ -0,0 +1,6 @@
+/* Pour pouvoir utiliser proprement
+   la fonction de sortie : */
+#include <stdio.h>
+/* Définit entre autres les codes de retour : */
+#include <stdlib.h>
+
---
+++ src/postprocessor/validation/p4a_recover_includes/hello_world_h.c
@@ -0,0 +1,11 @@
+/* To test user include: */
+#include "hello_world.h"
+
+int main(int argc, char *argv[]) {
+  /* Affiche sur la « sortie-standard » */
+  puts("Hello, world!");
+  // Renvoie une marque de réussite :
+  return EXIT_SUCCESS;
+}
+
+
