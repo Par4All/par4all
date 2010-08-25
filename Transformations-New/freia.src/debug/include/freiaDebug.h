e87cbf6e68d9e1200e4d9d2e17f0e4d6a9c519e0
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-08-25 07:12:29 +0000

    add reduced freia sources, with headers & CIPO/AIPO functions
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2375 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations-New/freia.src/debug/include/freiaDebug.h
@@ -0,0 +1,25 @@
+/***************************************************************
+ * Author : Christophe Clienti
+ * Date : 10/22/2008
+ * Copyright (c) 2008 Thales Research & Technologies (FR)
+ ***************************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifndef __FREIA_DEBUG_H__
+#define __FREIA_DEBUG_H__
+
+#include <freiaDebugErrors.h>
+
+  /*!
+   * \defgroup freia_debug Standard set of functions to debug and print stack backtrace
+   */
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
