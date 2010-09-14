f95a291b11bf4a026ef39ff8f8fa307614545509
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-09-14 08:51:43 +0000

    move example frm bug to normal validation
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2479 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pyps/loop_tiling.py
+++ Pyps/loop_tiling.py
@@ -11,9 +11,6 @@ if os.path.isdir("loop_tiling.database"):
 ws = workspace(["loop_tiling.c"], name="loop_tiling")
 
 ws.set_property(ABORT_ON_USER_ERROR = True)
-#prepare property for tiling
-ws.set_property (LOOP_LABEL = "l300")
-ws.set_property (LOOP_TILING_MATRIX = "1111 0 0 , 0 2222 0 , 0 0 3333")
 
 fct = ws.fun.main
 # try a basic function
@@ -21,7 +18,11 @@ fct.privatize_module ()
 
 # do some tiling on loop
 print "do some tiling on loop"
-fct.loop_tiling ()
+#look for the desired loop
+for loop in fct.loops ():
+    lbl = loop.label
+    if lbl == "l300":
+        loop.loop_tiling (LOOP_TILING_MATRIX = "1111 0 0 , 0 2222 0 , 0 0 3333")
 
 # print result
 fct.display ()
