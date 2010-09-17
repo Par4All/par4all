136d77160095fb5e623e6770cfd6c0d09302e471
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-09-17 06:41:45 +0000

    non working examples, put here because freia cipo sources are needed.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2492 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations-New/median_2.c
@@ -0,0 +1,25 @@
+#include <freiaDebug.h>
+#include <freiaCommon.h>
+#include <freiaAtomicOp.h>
+#include <freiaComplexOp.h>
+
+freia_status
+median_2(freia_data2d *o, freia_data2d *i)
+{
+  int32_t c = 2;
+  freia_status ret;
+  freia_data2d * t = freia_common_create_data(i->bpp, i->widthWa, i->heightWa);
+  ret =  freia_cipo_close(t, i, c, 1);
+  ret |= freia_cipo_open(t, i, c, 1);
+  ret |= freia_cipo_close(t, t, c, 1);
+
+  ret |= freia_aipo_inf(o, t, i);
+
+  ret |= freia_cipo_open(t, i, c, 1);
+  ret |= freia_cipo_close(t, t, c, 1);
+  ret |= freia_cipo_open(t, t, c, 1);
+
+  ret |= freia_aipo_sup(o, o, t);
+  ret |= freia_common_destruct_data(t);
+  return ret;
+}
