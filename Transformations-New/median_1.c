745a562dcd54605cc63a408f458584e1f4cc6941
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-09-17 06:55:29 +0000

    fix formula
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2494 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations-New/median_1.c
+++ Transformations-New/median_1.c
@@ -10,7 +10,7 @@ median_1(freia_data2d *o, freia_data2d *i)
   freia_status ret;
   freia_data2d * t = freia_common_create_data(i->bpp, i->widthWa, i->heightWa);
   ret =  freia_cipo_close(t, i, c, 1);
-  ret |= freia_cipo_open(t, i, c, 1);
+  ret |= freia_cipo_open(t, t, c, 1);
   ret |= freia_cipo_close(t, t, c, 1);
 
   ret |= freia_aipo_inf(o, t, i);
