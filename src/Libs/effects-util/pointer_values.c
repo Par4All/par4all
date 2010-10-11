b9b1a9c7df16d0044c06f179c108609c7836f656
creusillet <creusillet@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2010-10-11 10:08:12 +0000

    moving abstract values from pointer_values directory to effects-util
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@18068 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/effects-util/pointer_values.c
+++ src/Libs/effects-util/pointer_values.c
@@ -35,6 +35,99 @@
 #include "ri-util.h"
 #include "effects.h"
 #include "effects-util.h"
+#include "misc.h"
+
+/***************** ABSTRACT VALUES */
+
+entity undefined_pointer_value_entity()
+{
+  entity u = entity_undefined;
+  string u_name = strdup(concatenate(ANY_MODULE_NAME,
+				     MODULE_SEP_STRING,
+				     UNDEFINED_POINTER_VALUE_NAME,
+				     NULL));
+  u = gen_find_tabulated(u_name, entity_domain);
+  if(entity_undefined_p(u)) {
+    area a = make_area(0,NIL); /* Size and layout are unknown */
+    type t = make_type_area(a);
+    u = make_entity(u_name,
+		    t, make_storage_rom(), make_value_unknown());
+  }
+  return u;
+}
+
+entity null_pointer_value_entity()
+{
+  entity u = entity_undefined;
+  string u_name = strdup(concatenate(ANY_MODULE_NAME,
+				     MODULE_SEP_STRING,
+				     NULL_POINTER_VALUE_NAME,
+				     NULL));
+  u = gen_find_tabulated(u_name, entity_domain);
+  if(entity_undefined_p(u)) {
+    area a = make_area(0,NIL); /* Size and layout are unknown */
+    type t = make_type_area(a);
+    u = make_entity(u_name,
+		    t, make_storage_rom(), make_value_unknown());
+  }
+  return u;
+}
+
+
+cell make_undefined_pointer_value_cell()
+{
+  entity u = undefined_pointer_value_entity();
+  return make_cell_reference(make_reference(u, NIL)); 
+}
+
+bool undefined_pointer_value_entity_p(entity e)
+{
+  bool res;
+  res = same_string_p(entity_local_name(e), UNDEFINED_POINTER_VALUE_NAME);
+  res = res && same_string_p(entity_module_name(e), ANY_MODULE_NAME);
+  return res;  
+}
+
+bool undefined_pointer_value_cell_p(cell c)
+{
+  reference r;
+  if (cell_gap_p(c)) return false;
+  else if (cell_reference_p(c))
+    r = cell_reference(c);
+  else 
+    r = preference_reference(cell_preference(c));
+  return(undefined_pointer_value_entity_p(reference_variable(r)));
+}
+
+cell make_null_pointer_value_cell()
+{
+  entity u = null_pointer_value_entity();
+  return make_cell_reference(make_reference(u, NIL));
+}
+
+bool null_pointer_value_entity_p(entity e)
+{
+  bool res;
+  res = same_string_p(entity_local_name(e), NULL_POINTER_VALUE_NAME);
+  res = res && same_string_p(entity_module_name(e), ANY_MODULE_NAME);
+  return res;  
+}
+
+bool null_pointer_value_cell_p(cell c)
+{
+  reference r;
+  if (cell_gap_p(c)) return false;
+  else if (cell_reference_p(c))
+    r = cell_reference(c);
+  else 
+    r = preference_reference(cell_preference(c));
+  return(null_pointer_value_entity_p(reference_variable(r)));
+}
+
+
+
+/***************** SHORTCUTS FOR MAKING POINTER_VALUES CELL_RELATIONS */
+
 
 cell_relation make_value_of_pointer_value(cell c1, cell c2, tag app_tag, descriptor d)
 {
