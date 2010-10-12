031c0fa41ed4f3cc635dc3b6c6030fab43506252
keryell <keryell@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-10-12 09:03:40 +0000

    Added an example for flatten_code that should not create variables.
    Some homework for François-Xavier.
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2539 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/flatten_code16.c
@@ -0,0 +1,17 @@
+#include <math.h>
+
+void f() {
+  double U[2];
+  double _return391;
+  {
+    {
+      {
+	{
+	  double F_212;
+	  F_212 = U[1];
+	  _return391 = F_212;
+	}
+      }
+    }
+  }
+}
