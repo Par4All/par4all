056793cd0780201ce8b8a43eb95377588afdc0c8
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-10-14 17:00:10 +0000

    A few updates due to a performance bug fix in precondition computation and another bug fix in suppress_dead_code: the final value of the loop index is computed when the loop is removed because only one iteration is performed
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2550 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics-Future/schrammel05.c
+++ Semantics-Future/schrammel05.c
@@ -3,11 +3,14 @@
 main()
 `{
   int i, j = 2, k;
+  float x;
 
   while(i*i>1) {
     while(k*k>1)
       j = 1;
-    j = j;
+    // Oops, this generate an indentity transformer that is not identified
+    //    j = j;
+    x = 0.;
   }
   j = j;
 }
