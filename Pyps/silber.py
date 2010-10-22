8397da376b3d19d9d936fb532c421f79e955e23c
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-10-22 19:10:57 +0000

    Update pyps validation.
    Extend silber example for Greg and its demo
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2611 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pyps/silber.py
+++ Pyps/silber.py
@@ -1,7 +1,17 @@
 from pyps import workspace, module
-with workspace("silber.c","include/adds.c") as w:
+from os import system
+
+with workspace("silber.c","include/adds.c",verbose=False) as w:
 	# print out all functions
-	w.all.display()
+	w.all_functions.display()
+	a_out=w.compile()
+	system("./"+a_out+" include/input.pgm include/mapfile.amp /dev/null")
+
+	# print all function with both callers and callees
+	for fun in w.all:
+		if fun.callers and fun.callees:
+			fun.display()
+	
 	# get some help
 	help(module.unfolding)
 	# apply what we have just learnt
@@ -10,6 +20,7 @@ with workspace("silber.c","include/adds.c") as w:
 	t = w.fun.transfo
 	t.display()
 	t.flatten_code()
+	t.display()
 	# this will stop when force_loop_fusion fails
 	try:
 		while len(t.loops()) > 0:
@@ -20,6 +31,7 @@ with workspace("silber.c","include/adds.c") as w:
 	except:pass
 	# if everything is ok ...
 	if len(t.loops()) == 1:
+		outline_module="inner"
 		t.forward_substitute(optimistic_clean=True)
 		t.display()
 		t.common_subexpression_elimination(skip_lhs=False)
@@ -33,4 +45,18 @@ with workspace("silber.c","include/adds.c") as w:
 		t.coarse_grain_parallelization()
 		# great ! OMP pragmas
 		t.display()
+		a_out=w.compile(CFLAGS='-g -O2 -fopenmp')
+		system("./"+a_out+" include/input.pgm include/mapfile.amp /dev/null")
+
+		w.props.constant_path_effects=False
+		lbl=t.loops()[0].loops()[0].label
+		t.outline(module_name=outline_module,label=lbl)
+		t.display()
+		w[outline_module].display()
+
+		t=w[outline_module]
+
+		t.run(["sed","s/current/voltage/g"])
+		t.display()
 		a_out=w.compile()
+		#a_out
