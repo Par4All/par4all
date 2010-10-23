72a3123d51364f922cfc0c783cbd682dfe67af09
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-10-23 16:11:29 +0000

    argh, remove non ascii chars added by copying gfortran output.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2653 67b5017d-e62d-0410-a871-f72b1c62810f

--- Regions/LORDRE.f
+++ Regions/LORDRE.f
@@ -4,7 +4,7 @@
       FUNCTION  LORDRE( L1 )
       INTEGER L1, LORDRE, I
       LORDRE=0
-! LORDRE.f:8: warning: ‘l’ is used uninitialized in this function
+! LORDRE.f:8: warning: "l" is used uninitialized in this function
       DO 10 I=1,L
          IF (L1.EQ.I) GOTO 15
    10 CONTINUE
