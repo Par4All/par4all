2aeb8b7a8e42af3a8be5c4d51bcf042be1516fb1
coelho <coelho@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2010-10-26 14:26:42 +0000

    this is a bad idea.
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@1094 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/build_root.mk
+++ makes/build_root.mk
@@ -99,7 +99,7 @@ cscope-clean:
 	$(RM) cscope.out
 
 # autoconf compilation
-# --enable-doc
+# --enable-doc --enable-devel-mode
 BUILD.dir	= _build
 HERE	:= $(shell pwd)
 INSTALL.dir	= $(HERE)/../../install
@@ -122,8 +122,7 @@ auto: auto-clean
 	../configure --disable-static --prefix=$(INSTALL.dir) \
 		PATH=$(INSTALL.dir)/bin:$$PATH \
 		PKG_CONFIG_PATH=$(INSTALL.dir)/lib/pkgconfig:$(EXTERN_ROOT)/lib/pkgconfig \
-		--enable-hpfc --enable-pyps --enable-fortran95 \
-		--enable-gpips --enable-devel-mode
+		--enable-hpfc --enable-pyps --enable-fortran95 --enable-gpips
 	$(MAKE) -C $(BUILD.dir) DL.d=$(DOWNLOAD.dir)
 	$(MAKE) -C $(BUILD.dir) install
 
