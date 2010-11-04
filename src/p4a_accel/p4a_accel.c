e0be67b15508834f7b5f3770300a110dfe82e24c
Ronan Keryell <Ronan.Keryell@hpc-project.com>
2010-11-04 22:13:43 +0100

    2d communications functions are back in the p4a accel runtime.
    They disappeared when I added the 3d version... :-(

--- src/p4a_accel/p4a_accel.c
+++ src/p4a_accel/p4a_accel.c
@@ -208,6 +208,43 @@ void P4A_copy_to_accel_1d(size_t element_size,
 }
 
 
+/** Function for copying memory from the hardware accelerator to a 2D array in
+    the host.
+*/
+void P4A_copy_from_accel_2d(size_t element_size,
+			    size_t d1_size, size_t d2_size,
+			    size_t d1_block_size, size_t d2_block_size,
+			    size_t d1_offset, size_t d2_offset,
+			    void *host_address,
+			    const void *accel_address) {
+  size_t i, j;
+  char * cdest = d2_offset*element_size + (char*)host_address;
+  const char * csrc = (char*)accel_address;
+  for(i = 0; i < d1_block_size; i++)
+    for(j = 0; j < d2_block_size*element_size; j++)
+      cdest[(i + d1_offset)*element_size*d2_size + j] =
+        csrc[i*element_size*d2_block_size + j];
+}
+
+
+/** Function for copying a 2D memory zone from the host to a compact memory
+    zone in the hardware accelerator.
+*/
+void P4A_copy_to_accel_2d(size_t element_size,
+			  size_t d1_size, size_t d2_size,
+			  size_t d1_block_size, size_t d2_block_size,
+			  size_t d1_offset,   size_t d2_offset,
+			  const void *host_address,
+			  void *accel_address) {
+  size_t i, j;
+  char * cdest = (char *)accel_address;
+  const char * csrc = d2_offset*element_size + (char *)host_address;
+  for(i = 0; i < d1_block_size; i++)
+    for(j = 0; j < d2_block_size*element_size; j++)
+      cdest[i*element_size*d2_block_size + j] =
+        csrc[(i + d1_offset)*element_size*d2_size + j];
+}
+
 /** Function for copying memory from the hardware accelerator to a 3D array in
     the host.
 */
