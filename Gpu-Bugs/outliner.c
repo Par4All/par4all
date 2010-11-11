1707cf7a0a348c1d27920b68529d0733791a97e8
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-11-11 14:55:33 +0000

    Add a new bugs for GPU_IFY
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2740 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Gpu-Bugs/outliner.c
@@ -0,0 +1,29 @@
+#define NCELL 128
+
+
+
+//========================================================================
+void updateBug(float force[NCELL][NCELL][NCELL]) {
+  int i, j, k;
+  for (i = 0; i < NCELL; i++) {
+    for (j = 0; j < NCELL; j++) {
+      for (k = 0; k < NCELL; k++) {
+        int x = 0; // This cause GPU_IFY to badly outline the kernel
+        force[i][j][k] = 0;
+      }
+    }
+  }
+}
+
+
+
+int main() {
+  float force[NCELL][NCELL][NCELL]; // Force for each particle
+  updateBug(force);
+  return 0;
+}
+
+
+
+
+
