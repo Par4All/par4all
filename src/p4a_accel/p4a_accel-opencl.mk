9aaad1d594b5b32f17f3f90a6306c5192f564609
Stéphanie Even <Stephanie.Even@telecom-bretagne.eu>
2010-12-03 11:18:56 +0100

    Utilisation de LDFLAGS pour le path des LIBS.

--- src/p4a_accel/p4a_accel-opencl.mk
+++ src/p4a_accel/p4a_accel-opencl.mk
@@ -14,17 +14,18 @@ p4a_accel.cpp: $(P4A_ACCEL_DIR)/p4a_accel.c
 
 CXXFLAGS = -I.. -I.  -DP4A_ACCEL_CL -DP4A_DEBUG -DP4A_PROFILING -I$(OCL_DIR)/inc -I$(OCL_DIR)/inc/CL -I$(SHARED_DIR)/inc -DUNIX
 
-LDFLAGS = -fPIC -L/usr/lib 
+LDFLAGS = -fPIC -L/usr/lib -L$(SHARED_DIR)/lib -L$(OCL_DIR)/lib
 
 #LDLIBS = -lcudart -lcutil_x86_64
-LDLIBS =  $(OCL_DIR)/lib/liboclUtil_x86_64.a $(SHARED_DIR)/lib/libshrutil_x86_64.a -lOpenCL
+#LDLIBS =  $(OCL_DIR)/lib/liboclUtil_x86_64.a $(SHARED_DIR)/lib/libshrutil_x86_64.a -lOpenCL
+LDLIBS =  -loclUtil_x86_64 -lshrutil_x86_64 -lOpenCL
 
 # New default rule to compile OpenCL source files:
 %.o: %.cpp
 	$(CXX) -c $(CXXFLAGS) $<
 
 $(EXECUTABLE): $(OBJFILES)
-	$(LINK) $(CXXFLAGS) -o $@ $(OBJFILES) $(LDLIBS)
+	$(LINK) $(CXXFLAGS) -o $@ $(OBJFILES) $(LDLIBS) $(LDFLAGS)
 
 clean::
 	rm -f $(EXECUTABLE) $(OBJFILES)
