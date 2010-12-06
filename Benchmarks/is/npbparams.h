07cc81669e55afc1866bf4865605d354c49e5a99
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-12-06 14:43:01 +0000

    Add the NAS benchmark IS, parses and openmp code generation seems ok :)
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2860 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Benchmarks/is/npbparams.h
@@ -0,0 +1,17 @@
+#define CLASS 'S'
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.
+   This file provided for backward compatibility.
+   It is not used in DC benchmark.   */
+   
+long long int input_tuples=1000, attrnum=5;
+#define COMPILETIME "06 Dec 2010"
+#define NPBVERSION "3.3.1"
+#define CC "cc"
+#define CFLAGS "-O"
+#define CLINK "$(CC)"
+#define CLINKFLAGS "-O"
+#define C_LIB "(none)"
+#define C_INC "(none)"
