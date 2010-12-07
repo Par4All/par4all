a0a0e8a6275e9e308ca412ed1d962dd15df24f1c
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-12-07 12:31:00 +0000

    maisonneuve03 moved to Semantics-New and maisonneuve04 added to Semantics-Future
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2864 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Semantics-New/maisonneuve03.c
@@ -0,0 +1,30 @@
+// This example makes PIPS segfault (at least revisions 18437 & 18499)
+
+// $Id$
+
+#include <stdlib.h>
+
+int flip(void)
+{
+  return rand() % 2;
+}
+
+void run(void)
+{
+  int x;
+
+  while (flip()) {
+    if (flip()) {
+      if (x < 0) {
+	x++;
+      }
+    }
+  }
+}
+
+int main(void)
+{
+  run();
+  return 0;
+}
+
