67f73b2b3f4ceeaf79e4f253ae1c19ac47e0490d
Ronan Keryell <Ronan.Keryell@hpc-project.com>
2010-12-08 10:23:10 +0100

    Removed p4a_coffee and p4a_pack. Rendered p4a_coffee.py and p4a_pack.py directly usable.
    Simplified the script organization.
    
    In the side bar, fixed p4a_coffee that was no longer working since the
    p4a_setup removing.

--- src/simple_tools/p4a_coffee.py
+++ src/simple_tools/p4a_coffee.py
@@ -14,6 +14,7 @@ from p4a_util import *
 from p4a_builder import *
 from p4a_git import *
 from p4a_version import *
+from p4a_opts import *
 
 import p4a_setup
 import p4a_pack
@@ -41,110 +42,123 @@ def add_module_options(parser):
     p4a_pack.add_module_options(parser)
 
 
-def main(options, args = []):
+def main():
+    '''The function called when this program is executed by its own'''
 
-    # To be able to execute the very same version of this command later:
-    exec_path_name = os.path.abspath(sys.argv[0])
+    parser = optparse.OptionParser(description = __doc__, usage = "%prog [options]; run %prog --help for options")
 
-    #~ work_dir = ""
-    #~ if options.work_dir:
-        #~ work_dir = os.path.abspath(os.path.expanduser(options.work_dir))
-    #~ else:
-    work_dir = ""
+    add_module_options(parser)
 
-    #~ if not os.path.isdir(work_dir):
-    #~ os.makedirs(work_dir)
+    add_common_options(parser)
 
-    try:
-        if options.here:
-            #os.system("env")
+    (options, args) = parser.parse_args()
 
-            setup_options = options
-            #~ options.packages_dir = os.path.join(work_dir_p4a_version, "packages")
-            #~ warn("Forcing --packages-dir=" + options.packages_dir)
-            p4a_setup.main(setup_options)
+    if process_common_options(options, args):
+        # To be able to execute the very same version of this command later:
+        exec_path_name = os.path.abspath(sys.argv[0])
 
-            pack_options = options
+        #~ work_dir = ""
+        #~ if options.work_dir:
+            #~ work_dir = os.path.abspath(os.path.expanduser(options.work_dir))
+        #~ else:
+        work_dir = ""
 
-            if options.git_revision:
+        #~ if not os.path.isdir(work_dir):
+        #~ os.makedirs(work_dir)
+
+        try:
+            if options.here:
+                #os.system("env")
+
+                setup_options = options
+                p4a_setup.work(setup_options)
+
+                pack_options = options
+
+                if options.git_revision:
+                    run([ "git", "checkout", options.git_revision ])
+
+                #~ options.pack_dir = work_dir_p4a_version
+                #~ warn("Forcing --pack-dir=" + options.pack_dir)
+
+                # Unset P4A_ROOT environment variable if set, to avoid using
+                # the packages from somewhere else:
+                os.environ.pop("P4A_ROOT", None)
+                p4a_pack.work(pack_options)
+
+            else:
+                work_dir = tempfile.mkdtemp(prefix = "p4a_coffee_")
+
+                prev_cwd = os.getcwd()
+                os.chdir(work_dir)
+
+                work_dir_p4a = os.path.join(work_dir, "p4a")
+                #~ if os.path.isdir(work_dir_p4a):
+                    #~ warn("p4a directory already exists (" + work_dir_p4a + "), will not clone the repository again")
+                    #~ os.chdir(work_dir_p4a)
+                    #~ run([ "git", "checkout", "-b", "p4a", "remotes/origin/p4a" ])
+                    #~ run([ "git", "pull" ])
+                #~ else:
+                run([ "git", "clone", "git://git.hpc-project.com/par4all", "p4a" ])
+                os.chdir(work_dir_p4a)
+
+                if not options.git_revision:
+                    # The revision by default:
+                    options.git_revision = 'p4a'
                 run([ "git", "checkout", options.git_revision ])
 
-            #~ options.pack_dir = work_dir_p4a_version
-            #~ warn("Forcing --pack-dir=" + options.pack_dir)
+                suffix = utc_datetime()
+                revision = p4a_git(work_dir_p4a).current_revision()
+                if revision:
+                    suffix += "~" + revision
 
-            # Unset P4A_ROOT environment variable if set, to avoid using
-            # the packages from somewhere else:
-            os.environ.pop("P4A_ROOT", None)
-            p4a_pack.main(pack_options)
+                # Include the revision in the path so that it appears in debug messages
+                # of PIPS and we can trace back a faulty revision.
+                work_dir_p4a_version = os.path.join(work_dir, "p4a_" + suffix)
+                #~ run([ "rm", "-fv", work_dir_p4a_version ])
+                #~ run([ "ln", "-sv", work_dir_p4a, work_dir_p4a_version ])
+                run([ "mv", "-v", work_dir_p4a, work_dir_p4a_version ])
 
-        else:
-            work_dir = tempfile.mkdtemp(prefix = "p4a_coffee_")
+                os.chdir(prev_cwd)
+                ## Make sure child coffee maker will be using the python modules
+                ## which come with the git repos which was just cloned:
+                #os.environ["PYTHONPATH"] = os.path.join(work_dir_p4a_version, "src/simple_tools")
+                # To be able to build old version, use current p4_coffee instead.
+                os.environ["PYTHONPATH"] = os.path.join(os.path.dirname(exec_path_name))
+                ret = os.system(exec_path_name + " --here --root " + work_dir_p4a_version + " " + " ".join(sys.argv[1:]))
+                if ret:
+                    raise p4a_error("Child p4a_coffee failed")
 
-            prev_cwd = os.getcwd()
-            os.chdir(work_dir)
+                done('''All done. Here is your cup of coffee:
 
-            work_dir_p4a = os.path.join(work_dir, "p4a")
-            #~ if os.path.isdir(work_dir_p4a):
-                #~ warn("p4a directory already exists (" + work_dir_p4a + "), will not clone the repository again")
-                #~ os.chdir(work_dir_p4a)
-                #~ run([ "git", "checkout", "-b", "p4a", "remotes/origin/p4a" ])
-                #~ run([ "git", "pull" ])
-            #~ else:
-            run([ "git", "clone", "git://git.hpc-project.com/par4all", "p4a" ])
-            os.chdir(work_dir_p4a)
+             (    (     (     (
+              )    )     )     )
+            _(____(_____(_____(___
+            |                     |
+            |                     |______
+            |                      ___   |
+            |                     |   |  |
+            |    P a r 4 A l l    |___|  |
+            |                     ______/
+            |                     |
+             \___________________/
 
-            if not options.git_revision:
-                # The revision by default:
-                options.git_revision = 'p4a'
-            run([ "git", "checkout", options.git_revision ])
+    ''')
 
-            suffix = utc_datetime()
-            revision = p4a_git(work_dir_p4a).current_revision()
-            if revision:
-                suffix += "~" + revision
+        except:
+            #~ if not options.work_dir:
+            if work_dir:
+                warn("Work directory was " + work_dir + " (not removed)")
+            raise
 
-            # Include the revision in the path so that it appears in debug messages
-            # of PIPS and we can trace back a faulty revision.
-            work_dir_p4a_version = os.path.join(work_dir, "p4a_" + suffix)
-            #~ run([ "rm", "-fv", work_dir_p4a_version ])
-            #~ run([ "ln", "-sv", work_dir_p4a, work_dir_p4a_version ])
-            run([ "mv", "-v", work_dir_p4a, work_dir_p4a_version ])
-
-            os.chdir(prev_cwd)
-            ## Make sure child coffee maker will be using the python modules
-            ## which come with the git repos which was just cloned:
-            #os.environ["PYTHONPATH"] = os.path.join(work_dir_p4a_version, "src/simple_tools")
-            # To be able to build old version, use current p4_coffee instead.
-            os.environ["PYTHONPATH"] = os.path.join(os.path.dirname(exec_path_name))
-            ret = os.system(exec_path_name + " --here --root " + work_dir_p4a_version + " " + " ".join(sys.argv[1:]))
-            if ret:
-                raise p4a_error("Child p4a_coffee failed")
-
-            done('''All done. Here is your cup of coffee:
-
-         (    (     (     (
-          )    )     )     )
-        _(____(_____(_____(___
-        |                     |
-        |                     |______
-        |                      ___   |
-        |                     |   |  |
-        |    P a r 4 A l l    |___|  |
-        |                     ______/
-        |                     |
-         \___________________/
-
-''')
-
-    except:
         #~ if not options.work_dir:
         if work_dir:
-            warn("Work directory was " + work_dir + " (not removed)")
-        raise
+            rmtree(work_dir)
 
-    #~ if not options.work_dir:
-    if work_dir:
-        rmtree(work_dir)
+
+# If this file is called as a script, execute the main:
+if __name__ == "__main__":
+    exec_and_deal_with_errors(main)
 
 
 # Some Emacs stuff:
