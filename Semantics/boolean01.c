10ac93ef29926b3a4551b4b18fcba5f8e26009f3
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-12-09 11:07:55 +0000

    add future boolean examples, with manual or standard booleans && || ! logical operator results are necessary in 0..1? if so, this should be told by the transformer/preconditions!
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2873 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Semantics/boolean01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool alea(void)
+{
+  // rand() >=0
+  // 0 <= ((>=0) % 2) <= 1
+  return rand()%2;
+}
+
+int main(void)
+{
+  bool b1, b2, b3, b4, b5, b6, b7;
+  b1 = true;
+  b2 = false;
+  // b3 in 0..1 because of type? %2?
+  b3 = alea();
+  // b4 in 0..1 because logical
+  b4 = b1 && b3;
+  // b5 in 0..1 because logical
+  b5 = b1 || b3;
+  // b6 in 0..1 because logical
+  b6 = !b3;
+  // b7 ???
+  b7 = b1 ^ b2;
+}
