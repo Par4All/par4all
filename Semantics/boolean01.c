b252ab1997a7df59b19455e7323aca606356867c
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-12-10 14:20:41 +0000

    add the validation results for three test cases of Nicolas Halbwachs; tpips fixed for lra_halbwachs03
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2885 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics/boolean01.c
+++ Semantics/boolean01.c
@@ -22,6 +22,9 @@ int main(void)
   b5 = b1 || b3;
   // b6 in 0..1 because logical
   b6 = !b3;
-  // b7 ???
+  // b7: xor is a bitwise operator returning an int
+  // it requires a special handling when both operand are boolean,
+  // which is not (yet) implemented
   b7 = b1 ^ b2;
+  return 0;
 }
