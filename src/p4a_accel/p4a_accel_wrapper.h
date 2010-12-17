ccd9b943d68542e4db6c7850a8217d6a2b0608c0
Stéphanie Even <Stephanie.Even@telecom-bretagne.eu>
2010-12-17 09:22:29 +0100

    Adding the header p4a_accel_wrapper.h.

---
+++ src/p4a_accel/p4a_accel_wrapper.h
@@ -0,0 +1,12 @@
+#ifdef P4A_ACCEL_CUDA
+#include <p4a_accel_wrapper-CUDA.h>
+#endif
+
+#ifdef P4A_ACCEL_OPENMP
+#include <p4a_accel_wrapper-OpenMP.h>
+#endif
+
+#ifdef P4A_ACCEL_OPENCL
+#include <p4a_accel_wrapper-OpenCL.h>
+#endif
+
