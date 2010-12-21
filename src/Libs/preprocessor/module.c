68e92333000718ae090d052a066726505cb4314e
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2010-12-21 16:17:42 +0000

    Compilation unit handling improvements.
    When a compilation unit for a fortran module is asked, an assert is
    now triggered.
    Fix add_module_from_text to take this into account.
    As a side effect, intrinsics are considred both c and fortran modules.
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@18743 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/preprocessor/module.c
+++ src/Libs/preprocessor/module.c
@@ -377,8 +377,8 @@ void AddEntityToCompilationUnit(entity e, entity cu ) {
     }
     /* SG: when adding a new entity to compilation unit,
      * one should check the entity is not already present
-     * but an entity with the same name may already be defiend there
-     * so check this with a avery costly test*/
+     * but an entity with the same name may already be defined there
+     * so check this with a very costly test*/
     list cu_entities = entity_declarations(cu);
     FOREACH(ENTITY,cue,cu_entities)
         if(same_string_p(entity_user_name(e),entity_user_name(cue)))
