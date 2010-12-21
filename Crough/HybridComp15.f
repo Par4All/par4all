8fbb91e0c735395e608fbd47d6c4208cb2ebf1bb
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2010-12-21 16:22:41 +0000

    new test cases
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@2963 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Crough/HybridComp15.f
@@ -0,0 +1,17 @@
+      program HybridComp15
+      integer i
+      integer j
+      real a(5, 5)
+      real cst(5)
+      cst (1) = 1.0
+      cst (2) = 2.0
+      cst (3) = 3.0
+      cst (4) = 4.0
+      cst (5) = 5.0
+      do 10 i = 1, 5
+         do j = 1, 5
+            a(i,j) = cst(i) * 2.4E38
+         enddo
+ 10   continue
+      PRINT *,a
+      end
