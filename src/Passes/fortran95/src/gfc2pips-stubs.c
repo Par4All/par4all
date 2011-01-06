7c1085fff3efbba0d9efe54fbab4c2a33a7709b0
amini <amini@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2011-01-06 13:21:47 +0000

    Fix the licence header that was hurted by previous commit :)
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@18825 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/fortran95/src/gfc2pips-stubs.c
+++ src/Passes/fortran95/src/gfc2pips-stubs.c
@@ -18,7 +18,7 @@
 
  See the GNU General Public License for more details.
 
- You should have rea copy of the GNU General Public License
+ You should have received a copy of the GNU General Public License
  along with PIPS.  If not, see <http://www.gnu.org/licenses/>.
 
  */
