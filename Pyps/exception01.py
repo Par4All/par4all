ba434f63b155ef04a6833a53f9e82bfc5c7cd6a4
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-01-07 14:13:05 +0000

    Use python 2.5 style for exception handling.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3014 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pyps/exception01.py
+++ Pyps/exception01.py
@@ -4,6 +4,6 @@ from pyps import workspace
 with workspace('exception01.c',name='exception01',deleteOnClose=True) as ws:
 	try:
 		ws.fun.brout.outline(module_name="e",label="i-am-rubber-you-are-glue-everything-bouce-of-me-and-sticks-to-you")
-	except RuntimeError as r:
+	except RuntimeError , r:
 		print "Exception caught:", str(r)
 
