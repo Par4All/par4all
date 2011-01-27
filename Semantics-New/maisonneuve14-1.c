87716f9eee19835589193640a92cdc1ee8d358a6
maisonneuve <maisonneuve@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-01-27 13:09:53 +0000

    Added a new system: maisonneuve14, in which a variable simply flips between values 0 and 2 (so, non convex domain). * Without restructuration, not able to check n!=1, even with transformer lists   (maisonneuve14-1). * With restructuration, works (maisonneuve14-2).
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3098 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Semantics-New/maisonneuve14-1.c
@@ -0,0 +1,28 @@
+// This system has one variable n, whose value flips between 0 and 2
+// Here PIPS is not able to find that n != 1, even with transformer lists
+
+// $Id$
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int flip(void) {
+	return rand() % 2;
+}
+
+void run(void) {
+	
+	int n = 0;
+	while (1) {
+		if (n == 1) printf("unreachable");
+		if (n == 0) n = 2;
+		else if (n == 2) n = 0;
+	}
+	
+}
+
+int main(void) {
+	run();
+	return 0;
+}
+
