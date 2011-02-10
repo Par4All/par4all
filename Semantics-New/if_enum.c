526fc6c5a910788885dbabd8001d6697c1f8b698
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-02-10 17:19:00 +0000

    test case Semantics-New/if_enum moved from Semantics-Bugs
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3127 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics-New/if_enum.c
+++ Semantics-New/if_enum.c
@@ -5,6 +5,8 @@ enum mybool {
 	mybool_false = 0
 };
 
+typedef enum mybool mybool;
+
 void run_int(void) {
 	int b;
 	b = 0;
@@ -23,10 +25,24 @@ void run_enum2(void) {
 	if (b == mybool_true) "unreachable";
 }
 
+void run_enum3(void) {
+	mybool b;
+	b = 0;
+	if (b == 1) "unreachable";
+}
+
+void run_enum4(void) {
+	mybool b;
+	b = mybool_false;
+	if (b == mybool_true) "unreachable";
+}
+
 int main(void) {
 	run_int();
 	run_enum1();
 	run_enum2();
+	run_enum3();
+	run_enum4();
 	return 0;
 }
 
