2bec1894db4d96b093386cf241236fc41fe48dcf
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-03-04 14:36:38 +0000

    Add a new test case to illustrate the need to project variables after computing points-to and accept changes due to points_to_projection()
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3188 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pointers/proj01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+  int i, j;
+  int **pp;
+  srand(time(NULL));
+  {
+    int * pi = &i;
+    pp = &pi;
+    **pp = 1;
+  }
+
+  // pp points to a dead value in the stack
+  // should generate an error
+  **pp = 0; // segfault?
+  *pp = &j;
+  return 0;
+}
