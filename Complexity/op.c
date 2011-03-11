d926949ac07230625a60464740ea5126d76e7411
becher <becher@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-03-11 16:10:43 +0000

    file modified to append other tests of operators
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3199 67b5017d-e62d-0410-a871-f72b1c62810f

--- Complexity/op.c
+++ Complexity/op.c
@@ -13,4 +13,27 @@ int main()
   a /= b;
   c -= a;
   b += b;
+  a << 1;
+  b >> 1;
+  a | b;
+  z |= a;
+  a & b;
+  a ^ b;
+ 
+  if (a!=b)
+    a=b;
+ 
+  if ((z==a || z==b) && z>1)
+    z = z % c;
+ 
+  a <<= b;
+  b >>= a;
+ 
+  if (!a)
+    a=z;
+  
+  c ^= b;
+  b &= a;
+  a %= z;
+  (~a) ? (z=a) : (z=b);
 }
