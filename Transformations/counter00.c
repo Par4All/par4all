9d30080b0c3d330c738b59b91b35d23a5454b17d
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-03-15 15:10:31 +0000

    simple test for add_control_counter transformation
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3202 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/counter00.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void counter00(int n)
+{
+  int i;
+  // simple while
+  i = 0;
+  while (i<n)
+    i++;
+  // while with sequence
+  i = 0;
+  while (i<n)
+  {
+    i++;
+    fprintf(stdout, "%d\n", i);
+  }
+  // do loop
+  for (i=0; i<n; i++)
+    fprintf(stdout, "%d\n", i);
+  // for loop
+  for (i=0; i<n && n<1000; i++)
+    fprintf(stdout, "%d\n", i);
+  // do-while loop
+  i = 0;
+  do {
+    i++;
+    fprintf(stdout, "%d\n", i);
+  } while (i<n);
+  // simple test
+  if (n%2==0)
+    i++;
+  else
+    i--;
+  // sequence test
+  if (n%2==0)
+  {
+    i++;
+    fprintf(stdout, "%d\n", i);
+  }
+  else
+  {
+    i--;
+    fprintf(stdout, "%d\n", i);
+  }
+}
