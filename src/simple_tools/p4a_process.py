8f4285e654527c4ae2fc57b8632dc2caa9b5df4d
Pierre Villalon <pierre.villalon@hpc-project.com>
2011-03-22 17:54:48 +0100

    Correct the inlude bug. Merge wrappers and kernels in the same file

--- src/simple_tools/p4a_process.py
+++ src/simple_tools/p4a_process.py
@@ -270,6 +270,9 @@ class p4a_processor(object):
     # the list of fortran modules
     fortran_modules = set ()
 
+    # the list of kernels
+    kernels = []
+
     # some constant to be used for the pips generated files
     new_files_folder  = "p4a_new_files"
     new_files_include = new_files_folder + "_include.h"
@@ -736,7 +739,7 @@ class p4a_processor(object):
 
         for ph in apply_phases_kernel_after:
             # Apply requested phases before parallelization to generated kernels:
-			getattr(kernels, ph)(concurrent=True)		
+			getattr(kernels, ph)(concurrent=True)
 
         if not self.com_optimization :
             # Add communication around all the call site of the kernels. Since
@@ -747,7 +750,7 @@ class p4a_processor(object):
                                                )
         else :
             # Identify kernels first
-            kernels.flag_kernel()	
+            kernels.flag_kernel()
             #kernel for fftw3 runtime
             fftw3_kernel_filter_re = re.compile("^fftw.?_execute")
             fftw3_kernels = self.workspace.filter(lambda m: fftw3_kernel_filter_re.match(m.name))
@@ -807,7 +810,7 @@ class p4a_processor(object):
                              CROUGH_ARRAY_PARAMETER_AS_POINTER=True,
                              SET_RETURN_TYPE_AS_TYPEDEF_NEW_TYPE=self.kernel_return_type)
             wrappers.display ("c_printed_file",
-                             CROUGH_INCLUDE_FILE_LIST="p4a_accel.h",
+                              CROUGH_INCLUDE_FILE_LIST="p4a_accel.h",
                               DO_RETURN_TYPE_AS_TYPEDEF=True,
                               CROUGH_ARRAY_PARAMETER_AS_POINTER=True,
                               SET_RETURN_TYPE_AS_TYPEDEF_NEW_TYPE=self.wrapper_return_type)
@@ -836,11 +839,14 @@ class p4a_processor(object):
 
         for ph in apply_phases_after:
             # Apply requested phases to kernels, wrappers and launcher:
-			getattr(kernels, ph)(concurrent=True)		
-			getattr(wrappers, ph)(concurrent=True)		
-			getattr(kernel_launchers, ph)(concurrent=True)		
+			getattr(kernels, ph)(concurrent=True)
+			getattr(wrappers, ph)(concurrent=True)
+			getattr(kernel_launchers, ph)(concurrent=True)
 
         #self.workspace.all_functions.display()
+
+        # save the list of kernels for later work
+        self.kernels = kernels
 
         # To be able to inject Par4All accelerator run time initialization
         # later:
@@ -861,10 +867,10 @@ class p4a_processor(object):
             ''')
 
 
-    def ompify(self, 
-            filter_select = None, 
-            filter_exclude = None, 
-            apply_phases_before = [], 
+    def ompify(self,
+            filter_select = None,
+            filter_exclude = None,
+            apply_phases_before = [],
             apply_phases_after = []):
         """Add OpenMP #pragma from loop-parallel flag internal
         representation to generate... OpenMP code!"""
@@ -905,10 +911,32 @@ class p4a_processor(object):
         defines.append ("-D" + self.kernel_return_type + "=void")
         return defines
 
+    def kernel_to_wrapper_name (name):
+        return name.replace (self.get_kernel_prefix (), self.get_wrapper_prefix ())
+
+    def merge_wrappers_and_kernels (self):
+        for kernel in self.kernels:
+            # find the associated wrapper with the kernel
+            wrapper = self.kernel_to_wrapper_name (k)
+            # merge the file in the kernel
+            # Where the files do well in the .database workspace:
+            kernel_file = os.path.join(self.workspace.dirname(), "Src",
+                                       kernel + ".c")
+            wrapper_file = os.path.join(self.workspace.dirname(), "Src",
+                                        kernel + ".c")
+            k = open (kernel_file, 'a')
+            w = open (wrapper_file, 'r')
+            k.write (w.readlines ())
+            k.close ()
+            w.close ()
+            # remove the wrapper from the modules to be processed since already
+            #in the kernel
+            self.generated_modules.remove (wrapper)
+
     def save_header (self, output_dir, name):
         content = "/*All the generated includes are summarized here*/\n\n"
         for header in self.header_files:
-            content += '#inlude "' + header + '"\n'
+            content += '#include "' + header + '"\n'
         p4a_util.write_file (os.path.join (output_dir, name), content)
 
     def save_crough (self, output_dir):
@@ -962,7 +990,7 @@ class p4a_processor(object):
                 # of the .database
                 self.accel_post(pips_file,
                                 os.path.join(self.workspace.dirname(), "P4A"))
-                # update the pips file tpo the postprocess one
+                # update the pips file to the postprocess one
                 pips_file = os.path.join(self.workspace.dirname(), "P4A", name + extension_in)
 
             output_name = name + extension_out
@@ -1101,6 +1129,12 @@ class p4a_processor(object):
             output_dir = os.path.join(dest_dir,self.new_files_folder)
         if ((not (os.path.isdir(output_dir))) and (new_file_flag == True)):
             os.makedirs (output_dir)
+
+        # During the cuda generation process, kernels and wrappers might have
+        # been generated in different files. This is forbidden by cuda.
+        # Let's merge them in the same files
+        if ((self.c99 == True) and (self.cuda == True)):
+            self.merge_wrappers_and_kernels ()
 
         # save the user files
         output_files.extend (self.save_user_file (dest_dir, prefix, suffix))
