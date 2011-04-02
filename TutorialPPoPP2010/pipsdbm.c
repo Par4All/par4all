af9849204647843f1a470ec95165fa98cf4ab1c2
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-04-02 13:26:53 +0000

    Update some test cases for the tutorial.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3291 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ TutorialPPoPP2010/pipsdbm.c
@@ -0,0 +1,3 @@
+set_complexity_map( (statement_mapping) db_get_memory_resource(DBR_COMPLEXITIES, module_name, true));
+set_cumulated_rw_effects((statement_effects)db_get_memory_resource(DBR_REGIONS, module_name, true));
+
