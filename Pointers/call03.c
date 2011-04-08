06a8e4e5db845502b27225f6aa80e4c3954c79f9
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-04-08 07:51:32 +0000

    remove the compute of Effects With Points To
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3332 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pointers/call03.c
@@ -0,0 +1,21 @@
+/* #include<stdio.h> */
+
+typedef struct two_fields{int one; int two[10];} tf_t;
+
+int i;
+tf_t s;
+int * pi = &i;
+tf_t *q = &s;
+
+void call03()
+{
+  *pi = 1;
+  pi++;
+  q->one = 1;
+  q->two[4] = 2;
+}
+
+main()
+{
+  call03();
+}
