a35ea6197fd5c8ff430e52e2f3f634a6a08a40aa
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-04-19 13:32:12 +0000

    Try to fix scalarization29 : delete database if exists
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3378 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/scalarization29.py
+++ Transformations/scalarization29.py
@@ -1,6 +1,9 @@
-from __future__ import with_statement # this is to work with python2.5
+Je hu
 from pyps import workspace, module
 name="scalarization29"
+
+workspace.delete(name)
+
 with workspace(name+'.c',name=name,deleteOnClose=True) as ws:
 	ws.activate(module.must_regions)
 	ws.fun.scalarization29.display()
