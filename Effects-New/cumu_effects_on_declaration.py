690edeecea966fd87cea282d9b2b55b35f2ac415
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-04-20 17:57:54 +0000

    Add buggy case when cumulated effects should ignore "non store" (declaration) proper effects
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3385 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Effects-New/cumu_effects_on_declaration.py
@@ -0,0 +1,31 @@
+
+
+from pyps import workspace
+
+w = workspace("cumu_effects_on_declaration.c",deleteOnClose=True)
+
+cumu = w.fun.cumu
+
+print "\n****** Proper effects ******\n"
+cumu.display(activate="print_code_proper_effects")
+print "\n****** Cumulated effects ******\n"
+cumu.display(activate="print_code_cumulated_effects")
+
+
+print "\n\n****** With non memory effects ******\n\n"
+w.props.memory_effects_only = False
+
+
+# force recomputing
+cumu.proper_effects()
+
+
+print "\n****** Proper effects ******\n"
+cumu.display(activate="print_code_proper_effects")
+
+print "\n****** Cumulated effects ******"
+print "****** We expect them to be the same as before ******\n\n"
+cumu.display(activate="print_code_cumulated_effects")
+
+
+
