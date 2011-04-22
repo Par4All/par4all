4cf7c7b0c4bcb9ad03e6cb3ff7bc1ca14961ccae
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2011-04-22 15:34:37 +0000

    Improve Symbolic tiling.
    In fact, fix a bug and improve the pass. It is now possible to split
    tile walk-through in 2^n cases, so that the perfect tiles are separated
    from the border tiles.
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@19253 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/hyperplane/symbolic_tiling.c
+++ src/Libs/hyperplane/symbolic_tiling.c
@@ -21,134 +21,6 @@
 
 #include "transformations.h"
 
-static void do_symbolic_tiling(statement base, list vector)
-{
-    list effects = load_cumulated_rw_effects_list(base);
-    list tiled_loops_outer = NIL;
-    list tiled_loops_inner = NIL;
-    list prelude = NIL;
-    statement sloop=copy_statement(base);
-    FOREACH(EXPRESSION,tile_size,vector)
-    {
-        loop l = statement_loop(sloop);
-        list tile_expression_effects = proper_effects_of_expression(tile_size);
-        /* check if tile_size is modified by sloop and generate an a temporary variable if needed */
-        /* we should also check tile_size has no write effect
-         * but currently, string_to_expression asserts this */
-        FOREACH(EFFECT,teff,tile_expression_effects) {
-            FOREACH(EFFECT,eff,effects) {
-                if(effect_write_p(eff) && 
-                        references_may_conflict_p(effect_any_reference(teff),effect_any_reference(eff)))
-                {
-                    entity etemp = make_new_scalar_variable(get_current_module_entity(),make_basic_int(DEFAULT_INTEGER_TYPE_SIZE));
-                    AddEntityToCurrentModule(etemp);
-                    statement ass = make_assign_statement(entity_to_expression(etemp),copy_expression(tile_size));
-                    update_expression_syntax(tile_size,make_syntax_reference(make_reference(etemp,NIL)));
-                    prelude=CONS(STATEMENT,ass,prelude);
-                    goto generate_tile;
-                }
-            }
-        }
-generate_tile:;
-        /* outer loop new index */
-        entity index = make_new_index_entity(loop_index(l),"t");
-        /* inner loop */
-        statement inner = instruction_to_statement(
-                make_instruction_loop(
-                    make_loop(
-                        loop_index(l),
-                        make_range(
-                            entity_to_expression(index),
-                            binary_intrinsic_expression(MINUS_OPERATOR_NAME,
-                                binary_intrinsic_expression(MIN_OPERATOR_NAME,
-                                    binary_intrinsic_expression(PLUS_OPERATOR_NAME,
-                                        entity_to_expression(index),
-                                        copy_expression(tile_size)
-                                        ),
-                                    binary_intrinsic_expression(PLUS_OPERATOR_NAME,
-                                        copy_expression(range_upper(loop_range(l))),
-                                        copy_expression(range_increment(loop_range(l)))
-                                        )
-                                    ),
-                                int_to_expression(1)
-                                ),
-                            copy_expression(range_increment(loop_range(l)))
-                            ),
-                        statement_undefined,
-                        entity_empty_label(),
-                        make_execution_parallel(),
-                        NIL
-                        )
-                    )
-                );
-        /* outer loop */
-        statement outer = sloop;
-        loop_index(l)=index;
-        range_increment(loop_range(l))=copy_expression(tile_size);
-        /* will help partial_eval */
-        range_upper(loop_range(l))=
-            binary_intrinsic_expression(MINUS_OPERATOR_NAME,
-                    range_upper(loop_range(l)),
-                    binary_intrinsic_expression(MINUS_OPERATOR_NAME,
-                        copy_expression(tile_size),
-                        int_to_expression(1)
-                        )
-                    );
-        /* save */
-        tiled_loops_outer=CONS(STATEMENT,outer,tiled_loops_outer);
-        tiled_loops_inner=CONS(STATEMENT,inner,tiled_loops_inner);
-
-        /* go on for the next one */
-        sloop=loop_body(l);
-    }
-    /* once we are done, regenerate the whole thing */
-    tiled_loops_outer=gen_nreverse(tiled_loops_outer);
-    tiled_loops_inner=gen_nreverse(tiled_loops_inner);
-    list iter;
-    /* chain outer */
-    for(iter = tiled_loops_outer;!ENDP(CDR(iter));POP(iter)) {
-        statement curr = STATEMENT(CAR(iter));
-        loop_body(statement_loop(curr))=STATEMENT(CAR(CDR(iter)));
-    }
-    /* junction */
-    statement curr = STATEMENT(CAR(iter));
-    iter=tiled_loops_inner;
-    loop_body(statement_loop(curr))=STATEMENT(CAR((iter)));
-    /* chain inner */
-    for(;!ENDP(CDR(iter));POP(iter)) {
-        statement curr = STATEMENT(CAR(iter));
-        loop_body(statement_loop(curr))=STATEMENT(CAR(CDR(iter)));
-    }
-    /* tail */
-    loop_body(statement_loop(STATEMENT(CAR(iter))))=sloop;
-
-    /* update */
-    statement_label(base)=entity_empty_label();/*the label have been duplicated by copy_statement */
-    statement_instruction(base)=instruction_undefined;
-    update_statement_instruction(base,make_instruction_block(gen_nreverse(CONS(STATEMENT,STATEMENT(CAR(tiled_loops_outer)),prelude))));
-
-    /* and clean */
-    gen_free_list(tiled_loops_inner);
-    gen_free_list(tiled_loops_outer);
-}
-
-
-/* checks if sloop is a perfectly nested loop of depth @depth */
-static bool symbolic_tiling_valid_p(statement sloop, size_t depth)
-{
-    if(depth == 0 ) return true;
-    else {
-        if(statement_loop_p(sloop) && 
-                ( execution_parallel_p(loop_execution(statement_loop(sloop)))
-                                     || get_bool_property("SYMBOLIC_TILING_FORCE") )
-          ){
-            statement body = loop_body(statement_loop(sloop));
-            return symbolic_tiling_valid_p(body,depth-1);
-        }
-        else return false;
-    }
-}
-
 static void convert_min_max_to_tests_in_loop(loop l)
 {
     expression ub = range_upper(loop_range(l));
@@ -182,6 +54,172 @@ static void convert_min_max_to_tests(statement s)
     gen_recurse(s,loop_domain, gen_true, convert_min_max_to_tests_in_loop);
 }
 
+static statement do_symbolic_tiling_rec_junction(statement root, list tests) {
+    if(ENDP(tests)) return copy_statement(root);
+    else return instruction_to_statement(
+            make_instruction_test(
+                make_test(EXPRESSION(CAR(tests)),
+                    do_symbolic_tiling_rec_junction(root,CDR(tests)),
+                    do_symbolic_tiling_rec_junction(root,CDR(tests))
+                    )
+                )
+            );
+}
+
+void do_symbolic_tiling(statement base, list vector)
+{
+    list effects = load_cumulated_rw_effects_list(base);
+    list tiled_loops_outer = NIL;
+    list tiled_loops_inner = NIL;
+    list prelude = NIL;
+    list allmins = NIL;
+    statement sloop=copy_statement(base);
+    FOREACH(EXPRESSION,tile_size,vector)
+    {
+        loop l = statement_loop(sloop);
+        list tile_expression_effects = proper_effects_of_expression(tile_size);
+        /* check if tile_size is modified by sloop and generate an a temporary variable if needed */
+        /* we should also check tile_size has no write effect
+         * but currently, string_to_expression asserts this */
+        FOREACH(EFFECT,teff,tile_expression_effects) {
+            FOREACH(EFFECT,eff,effects) {
+                if(effect_write_p(eff) && 
+                        references_may_conflict_p(effect_any_reference(teff),effect_any_reference(eff)))
+                {
+                    entity etemp = make_new_scalar_variable(get_current_module_entity(),make_basic_int(DEFAULT_INTEGER_TYPE_SIZE));
+                    AddEntityToCurrentModule(etemp);
+                    statement ass = make_assign_statement(entity_to_expression(etemp),copy_expression(tile_size));
+                    update_expression_syntax(tile_size,make_syntax_reference(make_reference(etemp,NIL)));
+                    prelude=CONS(STATEMENT,ass,prelude);
+                    goto generate_tile;
+                }
+            }
+        }
+generate_tile:;
+        /* outer loop new index */
+        entity index = make_new_index_entity(loop_index(l),"t");
+        expression lower_bound = 
+            binary_intrinsic_expression(MULTIPLY_OPERATOR_NAME,
+                    entity_to_expression(index),
+                    copy_expression(tile_size)
+                    );
+        expression upperbound_lhs = copy_expression(range_upper(loop_range(l)));
+        expression upperbound_rhs = 
+            binary_intrinsic_expression(PLUS_OPERATOR_NAME,
+                    copy_expression(lower_bound),
+                    binary_intrinsic_expression(MINUS_OPERATOR_NAME,
+                        copy_expression(tile_size),
+                        int_to_expression(1)
+                        )
+                    );
+
+        expression upper_bound = 
+            binary_intrinsic_expression(MIN_OPERATOR_NAME,
+                    upperbound_lhs,
+                    upperbound_rhs
+                    );
+        allmins=CONS(EXPRESSION,binary_intrinsic_expression(GREATER_THAN_OPERATOR_NAME,
+                    copy_expression(upperbound_lhs),
+                    copy_expression(upperbound_rhs)
+                    ),allmins);
+
+        /* inner loop */
+        statement inner = instruction_to_statement(
+                make_instruction_loop(
+                    make_loop(
+                        loop_index(l),
+                        make_range(
+                            lower_bound,
+                            upper_bound,
+                            int_to_expression(1)
+                            ),
+                        statement_undefined,
+                        entity_empty_label(),
+                        make_execution_parallel(),
+                        NIL
+                        )
+                    )
+                );
+        /* outer loop */
+        statement outer = sloop;
+        loop_index(l)=index;
+        //range_increment(loop_range(l))=copy_expression(tile_size);
+        /* will help partial_eval */
+        range_upper(loop_range(l))=
+            binary_intrinsic_expression(DIVIDE_OPERATOR_NAME,
+                    range_upper(loop_range(l)),
+                    copy_expression(tile_size)
+                    );
+        /* save */
+        tiled_loops_outer=CONS(STATEMENT,outer,tiled_loops_outer);
+        tiled_loops_inner=CONS(STATEMENT,inner,tiled_loops_inner);
+
+        /* go on for the next one */
+        sloop=loop_body(l);
+    }
+    /* once we are done, regenerate the whole thing */
+    tiled_loops_outer=gen_nreverse(tiled_loops_outer);
+    tiled_loops_inner=gen_nreverse(tiled_loops_inner);
+    list iter;
+    /* chain outer */
+    for(iter = tiled_loops_outer;!ENDP(CDR(iter));POP(iter)) {
+        statement curr = STATEMENT(CAR(iter));
+        loop_body(statement_loop(curr))=STATEMENT(CAR(CDR(iter)));
+    }
+    /* prepare junction */
+    statement curr = STATEMENT(CAR(iter));
+    iter=tiled_loops_inner;
+    statement junction=STATEMENT(CAR(iter));
+
+    /* chain inner */
+    for(;!ENDP(CDR(iter));POP(iter)) {
+        statement curr = STATEMENT(CAR(iter));
+        loop_body(statement_loop(curr))=STATEMENT(CAR(CDR(iter)));
+    }
+    /* tail */
+    loop_body(statement_loop(STATEMENT(CAR(iter))))=sloop;
+
+    /* update */
+    statement_label(base)=entity_empty_label();/*the label have been duplicated by copy_statement */
+    statement_instruction(base)=instruction_undefined;
+    update_statement_instruction(base,make_instruction_block(gen_nreverse(CONS(STATEMENT,STATEMENT(CAR(tiled_loops_outer)),prelude))));
+
+    /* final junction */
+    if(get_bool_property("SYMBOLIC_TILING_PERFECT_TILES")) {
+        junction=do_symbolic_tiling_rec_junction(junction,allmins);
+        gen_free_list(allmins);
+    }
+    else {
+        gen_full_free_list(allmins);
+    }
+    loop_body(statement_loop(curr))=junction;
+
+    /* and clean */
+    gen_free_list(tiled_loops_inner);
+    gen_free_list(tiled_loops_outer);
+    if(get_bool_property("SYMBOLIC_TILING_NO_MIN"))
+        convert_min_max_to_tests(base);/* << this one is here to wait for better preconditions computations */
+}
+
+
+/* checks if sloop is a perfectly nested loop of depth @depth */
+static bool symbolic_tiling_valid_p(statement sloop, size_t depth)
+{
+    int l;
+    if(depth == 0 ) return true;
+    else {
+        if(statement_loop_p(sloop) && 
+                ( execution_parallel_p(loop_execution(statement_loop(sloop))) || get_bool_property("SYMBOLIC_TILING_FORCE") ) &&
+                ( expression_integer_value(range_increment(loop_range(statement_loop(sloop))),&l) && l == 1 )
+          ){
+            statement body = loop_body(statement_loop(sloop));
+            return symbolic_tiling_valid_p(body,depth-1);
+        }
+        else return false;
+    }
+}
+
+
 bool symbolic_tiling(const char *module_name)
 {
     /* prelude */
@@ -207,8 +245,6 @@ bool symbolic_tiling(const char *module_name)
             pips_user_warning("must provide a non empty array with expressions valid in current context\n");
         else if((result=symbolic_tiling_valid_p(sloop,gen_length(vector)))) {
             do_symbolic_tiling(sloop,vector);
-            if(get_bool_property("SYMBOLIC_TILING_NO_MIN"))
-                convert_min_max_to_tests(sloop);/* << this one is here to wait for better preconditions computations */
         }
         gen_full_free_list(vector);
     }
