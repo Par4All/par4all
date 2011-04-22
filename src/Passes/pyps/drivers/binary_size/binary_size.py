1d2c46bc17b53155338dd70b06b39dbd1908afbd
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2011-04-22 15:42:32 +0000

    Update binary_size workspace to be compatible with new API.
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@19354 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/pyps/drivers/binary_size/binary_size.py
+++ src/Passes/pyps/drivers/binary_size/binary_size.py
@@ -1,15 +1,16 @@
 import subprocess
-import re
+import re,os
 import pyps
 import new
 
-def binary_size(module, compiler = pyps.backendCompiler()):
+def binary_size(module, maker = pyps.Maker()):
 	"""Workspace extension to provide a binary_size function to each module.
 
 	A call to the binary_size function of a given module attempts to compile the program and then to extract the compiled size and the instruction count of the given module using objdump.  The compiled size and the instruction count are returned by the functon in a tuple. A ValueError exception is thrown if the function is not found in the binary (see below). A RuntimeError is thrown when the objdump call failed.
 
 	Be carefull. The symbol used by the compiler in the binary object for the given module must be guessed by the binary_size function. Given a function “foo”, the symbol can be “foo”, “foo.”, “_foo” or many others forms, thus we cannot ensure that this function will work in every situations. A wrong guess can lead to a ValueError exception or in a few cases to wrongs results."""
-	outfile = module._ws.compile(compiler)
+	module._ws.make(maker=maker)
+	(outfile,a,b,c) = module._ws.compile(CFLAGS="-O2")
 	
 	ret =__funcsize(module.name, outfile)
 	os.unlink(outfile)
