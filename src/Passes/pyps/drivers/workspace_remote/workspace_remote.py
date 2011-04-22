2ada647f756c03955ea487e9671712d5a7919287
guelton <guelton@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2011-04-22 15:39:30 +0000

    workspace remote is now working with new rules
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@19318 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/pyps/drivers/workspace_remote/workspace_remote.py
+++ src/Passes/pyps/drivers/workspace_remote/workspace_remote.py
@@ -68,6 +68,7 @@ class remoteExec:
 
 	def copyRec(self, path, to):
 		self.init_cm()
+		print ("scp -r %s %s %s:%s" % (self._ssh_opt, path, self._host, os.path.join(self._remoteDir, to)))
 		ret = os.system("scp -r %s %s %s:%s" % (self._ssh_opt, path, self._host, os.path.join(self._remoteDir, to)))
 		if ret != 0:
 			raise RuntimeError("SSH: error while copying %s to %s:%s : scp returned %d." % (path, self._host, to, ret))
@@ -76,154 +77,82 @@ class remoteExec:
 	def host(self): return self._host
 
 
-class remoteCompiler(pyps.backendCompiler):
-	def __init__(self,CC="cc",CFLAGS="",LDFLAGS="",compilemethod=None,rep=None,outfile="", args=[], remoteExec=None):
-		self.remoteExec = remoteExec
-		
-		#setting rep for remote and cleaning it
-		self.rdir = os.path.join(self.remoteExec.working_dir(), compiler.rep)
-		self.remoteExec.do("rm -rf \"%s\"" % rdir)
-		self.remoteExec.do("mkdir -p \"%s\"" % rdir)
-				
-		super(remoteCompiler,self).__init__(CC, CFLAGS,LDFLAGS,compilemethod,rep,outfile,args)
-
-	def compile(self, filename, extraCFLAGS="", verbose=False)
-		"""compiles one given .c file. Called by the workspace method compile"""
-		outfilename = os.path.basename(os.path.splitext(filename)[0]+".o")
-		outfilename = os.path.join(self.remoteExec.working_dir(),outfile)
-		command = [self.CC, extraCFLAGS, self.CFLAGS, "-c", filename, "-o", outfilename]
-		commandline = " ".join(command)
-		if verbose:
-			print >> sys.stderr, "Copying file %s to remote host %s..." % filename % self.remoteExec.host()
-		self.remoteExec.copy(f, rdir)
-
-		if verbose:
-			print >> sys.stderr , "Compiling a workspace file with", commandline
-		
-		p = self.remoteExec.doPopen(command)
-		(out,err) = p.communicate()
-		compiler.cc_stderr = err
-		ret = p.returncode
-		if ret != 0:
-			print >> sys.stderr, err
-			raise RuntimeError("%s failed with return code %d" % (commandline, ret))
-
-		self.cc_cmd = commandline
-		return [outfilename]
-
-	def link(self,files, verbose=False)
-		"""links a given list of .o files. Called by the workspace method compile"""
-		command = [self.CC]+files+[self.LDFLAGS]
-		if self.outfile:
-			command+=["-o", self.outfile]
-		commandline = " ".join(command)
-		if verbose:
-			print >> sys.stderr , "Linking the workspace with", commandline
-		p = Popen(commandline, shell=True, stdout = PIPE, stderr = PIPE)
-		(out,err) = p.communicate()
-		self.cc_stderr = err
-		ret = p.returncode
-		if ret != 0:
-			print >> sys.stderr, err
-			raise RuntimeError("%s failed with return code %d" % (commandline, ret))
-		self.cc_cmd = commandline
-		return self.outfile
-
-
-
-
-
-
-
-class workspace:
-	def __init__(self, ws, *args, **kwargs):
-		self.ws = ws
+class workspace(pyps.workspace):
+	def __init__(self, *sources, **kwargs):
 		self.remoteExec = kwargs["remoteExec"]
+		super(workspace,self).__init__(*sources,**kwargs)
 
-#	def compile(self,CC="gcc",CFLAGS="-O2 -g", LDFLAGS="", link=True, rep="d.out", outfile="",extrafiles=[],*args,**kwargs):
-	def compile(self,compiler,*args,**kwargs):
-		"""try to compile current workspace with compiler `CC', compilation flags `CFLAGS', linker flags `LDFLAGS' in directory `rep' as binary `outfile' and adding sources from `extrafiles'"""
-
-		#setting flags in workspace
-		CC = compiler.CC
-		CFLAGS = compiler.CFLAGS
-		LDFLAGS = compiler.LDFLAGS
-		link = kwargs.get("link", True)
-		outfile = compiler.outfile
-		#setting rep in workspace
-		if compiler.rep==None:
-			compiler.rep=self.ws.tmpdirname()+"d.out"
-		
-		#setting otmpfiles in workspace
-		otmpfiles=self.ws.save(rep=compiler.rep)
+	def save(self,rep=None):
+		if rep == None:
+			rep = self.tmpdirname()
+		files = super(workspace,self).save(rep)	
 		#setting otmpfiles for remote
 		otmpfilesrem = []
 		
 		#setting rep for remote and cleaning it
-		rdir = os.path.join(self.remoteExec.working_dir(), compiler.rep)
+		rdir = os.path.join(self.remoteExec.working_dir(), rep)
 		self.remoteExec.do("rm -rf \"%s\"" % rdir)
 		self.remoteExec.do("mkdir -p \"%s\"" % rdir)
-	
+
 		#copy all headers in remote
-		print >>sys.stderr, "Copying user headers to remote host %s..." % self.remoteExec.host()
-		user_headers = self.ws.user_headers()
-		for uh in user_headers:
+		#print >>sys.stderr, "Copying user headers to remote host %s..." % self.remoteExec.host()
+		#user_headers = self.user_headers()
+		#for uh in user_headers:
 			# TODO: try to find out in which directory the headers should go...
 			#rrep_inc = os.path.join(rdir, os.path.dirname(uh))
 			#self.remoteExec.do("mkdir -p \"%s\"" % rrep_inc)
-			self.remoteExec.copy(uh, rdir)
-	
-		#setting command 
-		command=[CC, self.ws.cppflags, CFLAGS]
-		if link:
-			if not outfile:
-				outfile=self.ws._name
-			outfile = os.path.join(self.remoteExec.working_dir(),outfile)
-			self.ws.goingToRunWith(otmpfiles, compiler.rep)
-			for f in otmpfiles:
-				dst = os.path.join(rdir, f[len(compiler.rep)+1:])
-				otmpfilesrem.append(dst)
-			command+=otmpfilesrem
-			command+=[LDFLAGS]
-			command+=["-o",outfile]
-		else:
-			for f in otmpfiles:
-				dst = os.path.join(rdir, f[len(compiler.rep)+1:])
-				otmpfilesrem.append(dst)
-			command+=["-c"]
-			command+=otmpfilesrem
-		commandline = " ".join(command)
-
-		# Copy files to remote host
-		if self.ws.verbose:
+		#	self.remoteExec.copy(uh, rdir)
+		
+		if self.verbose:
 			print >> sys.stderr, "Copying files to remote host %s..." % self.remoteExec.host()
-		rtmp = os.path.split(rdir)[0]
-		print >>sys.stderr, "Copy recursive %s to remote %s..." % (compiler.rep, rtmp)
-		self.remoteExec.copyRec(compiler.rep, rtmp)
+		#rtmp = os.path.split(rep)[0]
+		
+		print files
+		for f in files:
+			dst = os.path.join(rdir, os.path.basename(f))
+			otmpfilesrem.append(dst)
+			print >>sys.stderr, "Copy %s to remote %s..." % (f, rep)
+			self.remoteExec.copy(f, rep)
 
-		if self.ws.verbose:
-			print >> sys.stderr , "Compiling the workspace with", commandline
+		return otmpfilesrem
 
-		p = self.remoteExec.doPopen(command)
-		(out,err) = p.communicate()
-		compiler.cc_stderr = err
-		ret = p.returncode
-		if ret != 0:
-			if not link: map(os.remove,otmpfiles)
-			print >> sys.stderr, err
-			raise RuntimeError("%s failed with return code %d" % (commandline, ret))
-
-		compiler.outfile = outfile
-		compiler._compile_done = True
-		compiler.cc_cmd = commandline
-		return outfile
-
-	def run_output(self, compiler):
-		if not compiler._compile_done:
-			return self.ws.compile_and_run(compiler)
-		compiler.cmd = [compiler.outfile] + compiler.args
-		p = self.remoteExec.doPopen(compiler.cmd)
+	def make(self,rep=None, maker=pyps.Maker()):
+		if rep ==None:
+			rep = self.tmpdirname()
+		makefile = super(workspace,self).make(rep,maker)
+		#rtmp = os.path.split(rep)[0]
+		self.remoteExec.copy(os.path.join(rep,makefile),rep)
+		return makefile
+		
+	def compile(self,rep=None, makefile="Makefile", outfile="a.out"):
+		""" Uses makefiles on the remote host to compile the workspace"""
+		if rep == None:
+			rep = self.tmpdirname()
+		rep = os.path.join(self.remoteExec.working_dir(),rep)
+		commandline = ["make",]
+		commandline+=["-C",rep]
+		commandline+=["-f",makefile]
+		commandline.append("TARGET="+outfile)
+		
+		if self.verbose:
+			print >> sys.stderr , "Compiling the remote workspace with", commandline
+		#We need to set shell to False or it messes up with the make command
+		p = self.remoteExec.doPopen(commandline)
 		(out,err) = p.communicate()
 		rc = p.returncode
+		if rc != 0:
+			print >> sys.stderr, err
+			raise RuntimeError("%s failed with return code %d" % (commandline, rc))
+
+		return os.path.join(rep,outfile),rc,out,err
+
+	def run(self, binary, args=[]):
+		cmd = [os.path.join("./",binary)] + args
+		p = self.remoteExec.doPopen(cmd)
+		(out,err) = p.communicate()
+		rc = p.returncode
+		if rc != 0:
+			print >> sys.stderr, err
+			raise RuntimeError("%s failed with return code %d" % (cmd, rc))
 		return (rc,out,err)
 
