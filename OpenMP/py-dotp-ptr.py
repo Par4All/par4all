bf4440a8a2b52c827fe319f7c0e80306d71a8b01
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-04-27 16:33:08 +0000

    Update python openmp validation.
    Use the link trick as suggested by RK.
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3430 67b5017d-e62d-0410-a871-f72b1c62810f

--- OpenMP/py-dotp-ptr.py
+++ OpenMP/py-dotp-ptr.py
@@ -1,7 +1,1 @@
-from __future__ import with_statement
-from pyps import *
-import openmp
-
-with workspace("dotp-ptr.c") as w:
-	w.props.memory_effects_only=False
-	w.all_functions.openmp(verbose=True)
+default.py
