Par4All/par4all

commit d5f779fde45903e2fcbe5a20c74c1491ef94952e

Fix a bug at generic_eval_cell_with_points_to() : the function should return a list of effects when we do not have points-to relations, to be cheked by BC. Take into account the null location, a dirty solution waiting for another after the meeting with MA and BC. Fix a bug when storing points-to relations at each statement.
git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@19379 32a0c1e6-9308-0410-80dd-f9d2257a62c7

src/Libs/alias-classes/points_to_analysis_general_algorithm.c
@@ -311,9 +311,26 @@ void points_to_storage(set pts_to_set, statement s, bool store) {
   if(statement_unstructured_p(s) || statement_goto_p(s))
     store = false;
   if (!set_empty_p(pts_to_set) && store == true) {
+    instruction i = statement_instruction(s);
+    if(instruction_sequence_p(i)){
+      sequence seq = instruction_sequence(i);
+      FOREACH(statement, stm, sequence_statements(seq)){
+	pt_list = set_to_sorted_list(pts_to_set,
+				     (int(*)(const void*, const void*))
+				     points_to_compare_cells);
+	points_to_list new_pt_list = make_points_to_list(pt_list);
+	store_or_update_pt_to_list(stm, new_pt_list);
+      }
+    }
+    else{
     pt_list = set_to_sorted_list(pts_to_set,
 				 (int(*)(const void*, const void*))
 				 points_to_compare_cells);
+    points_to_list new_pt_list = make_points_to_list(pt_list);
+    store_or_update_pt_to_list(s, new_pt_list);
+    }
+  }
+  else if(set_empty_p(pts_to_set)){
     points_to_list new_pt_list = make_points_to_list(pt_list);
     store_pt_to_list(s, new_pt_list);
   }
@@ -790,26 +807,31 @@ set points_to_assignment(statement current, expression lhs, expression rhs,
   else if(expression_call_p(rhs)){
     if(ENTITY_MALLOC_SYSTEM_P(expression_to_entity(rhs)) ||
        ENTITY_CALLOC_SYSTEM_P(expression_to_entity(rhs))){
+      expression sizeof_exp = EXPRESSION (CAR(call_arguments(expression_call(rhs))));
       type t = expression_to_type(lhs);
-      reference nr = original_malloc_to_abstract_location(reference_undefined,
+      reference nr = original_malloc_to_abstract_location(lhs,
 							  t,
 							  type_undefined,
-							  expression_undefined,
+							  sizeof_exp,
 							  get_current_module_entity(),
-							  statement_number(current));
+							  current);
       cell nc = make_cell_reference(nr);
       R = CONS(CELL, nc, NIL);
       address_of_p = true;
     }
     else if(user_function_call_p(rhs)){
-      entity ne = entity_all_locations();
+      type t = entity_type(call_function(expression_call(rhs)));
+      entity ne = entity_all_xxx_locations_typed(ANYWHERE_LOCATION,t);
+     /*  entity ne = entity_all_locations(); */
       reference nr = make_reference(ne, NIL);
       cell nc = make_cell_reference(nr);
       R = CONS(CELL, nc, NIL);
       address_of_p = true;
     }
     else{
-      entity ne = entity_all_locations();
+     /*  entity ne = entity_all_locations(); */
+      type t = entity_type(call_function(expression_call(rhs)));
+      entity ne = entity_all_xxx_locations_typed(ANYWHERE_LOCATION,t);
       reference nr = make_reference(ne, NIL);
       cell nc = make_cell_reference(nr);
       R = CONS(CELL, nc, NIL);
@@ -817,7 +839,9 @@ set points_to_assignment(statement current, expression lhs, expression rhs,
     }
   }
   else{
-    entity ne = entity_all_locations();
+    type t = expression_to_type(rhs);
+    entity ne = entity_all_xxx_locations_typed(ANYWHERE_LOCATION,t);
+   /*  entity ne = entity_all_locations(); */
     reference nr = make_reference(ne, NIL);
     cell nc = make_cell_reference(nr);
     R = CONS(CELL, nc, NIL);
@@ -1045,52 +1069,7 @@ cell add_array_dimension(cell c)
   return c1;
 }
 
-set emami_ref_array(set lhs_set, set rhs_set, set pts_in)
-{
-/* lhs_path matches the kill set.*/
-  set kill = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set gen =  set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set res =  set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set input_kill_diff = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  approximation a = make_approximation_exact();
-  SET_FOREACH(points_to, p, pts_in)
-    {
-      SET_FOREACH(cell, c, lhs_set)
-	{
-	  if(cell_equal_p(points_to_source(p),c))
-	    set_add_element(kill,kill,(void*) p);
-	}
-    }
 
-  /* input - kill */
-  set_difference(input_kill_diff, pts_in, kill);
-
-  /* if the lhs_set or the rhs set contains more than an element, we
-     set the approximation to MAT. */
-  if(set_size(lhs_set) > 1 || set_size(rhs_set)>1)
-    a = make_approximation_may();
-
-  /* Computing the gen set*/
-  SET_FOREACH(cell, c, lhs_set)
-    {
-      SET_FOREACH(cell, c1, rhs_set)
-	{
-	  /* create a new points to with as source the current
-	     element of lhs_set and sink the current element of rhs_set.*/
-	  points_to pt_to = make_points_to(c, c1, a, make_descriptor_none());
-	  set_add_element(gen,gen,(void*) pt_to);
-	}
-    }
-  /* gen + input_kill_diff*/
-  set_union(res, gen, input_kill_diff);
-
-  return res;
-
-}
 
 
 /* iterate over the lhs_set, if it contains more than an element
@@ -1147,162 +1126,8 @@ set points_to_null_pointer(list lhs_list, set input)
 }
 
 
-/* iterate over the lhs_set, if it contains more than an element
-   approximations are set to MAY, otherwise it's set to EXACT
-   Set the sink to nowhere .*/
-set points_to_anywhere(list lhs_list, set input)
-{
-  /* lhs_path matches the kill set.*/
-  set kill = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set gen = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set res = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set input_kill_diff = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  approximation a = make_approximation_exact();
-
-  SET_FOREACH(points_to, p, input)
-    {
-      FOREACH(cell, c, lhs_list)
-	{
-	  if(cell_equal_p(points_to_source(p),c))
-	    set_add_element(kill,kill,(void*) p);
-	}
-    }
-
-  /* input - kill */
-  set_difference(input_kill_diff, input, kill);
-
-  /* if the lhs_set or the rhs set contains more than an element, we
-     set the approximation to MAY. */
-  if(gen_length(lhs_list) > 1 )//|| set_size(rhs_set)>1)
-    a = make_approximation_may();
-
-  /* Computing the gen set*/
-  FOREACH(cell, c, lhs_list){
-      /* we test if lhs is an array, so we change a[i] into a[*] */
-      /* if(array_type_p(cell_to_type(c))) */
-/* 	c = array_to_store_independent(c); */
-
-      /* create a new points to with as source the current
-	 element of lhs_set and sink the null value .*/
-      //entity e =entity_all_xxx_locations_typed(ANYWHERE_LOCATION,cell_to_type(c));
-    entity e =entity_all_xxx_locations_typed(ANYWHERE_LOCATION,make_type_unknown());
-      reference r = make_reference(e, NIL);
-      cell sink = make_cell_reference(r);
-      points_to pt_to = make_points_to(c, sink, a, make_descriptor_none());
-      set_add_element(gen, gen, (void*) pt_to);
-
-    }
-  /* gen + input_kill_diff*/
-  set_union(res, gen, input_kill_diff);
-
-  return res;
-}
 
 
-
-/* iterate over the lhs_set, if it contains more than an element
-   approximations are set to MAY, otherwise it's set to EXACT.*/
-set emami_ref_addr_case(set lhs_set, set rhs_set, set input)
-{
-  /* lhs_path matches the kill set.*/
-  set kill = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set gen = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set res = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set input_kill_diff = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  approximation a = make_approximation_exact();
-
-  SET_FOREACH(points_to, p, input) {
-      SET_FOREACH(cell, c, lhs_set) {
-	  if(cell_equal_p(points_to_source(p), c))
-	    set_add_element(kill,kill, (void*) p);
-	}
-    }
-
-  /* input - kill */
-  set_difference(input_kill_diff, input, kill);
-
-  /* if the lhs_set or the rhs set contains more than an element, we
-     set the approximation to MAT. */
-  if(set_size(lhs_set) > 1 )//|| set_size(rhs_set)>1)
-    a = make_approximation_may();
-
-  /* Computing the gen set*/
-  SET_FOREACH(cell, l, lhs_set) {
-      SET_FOREACH(cell, r, rhs_set) {
-	  /* create a new points to with as source the current
-	     element of lhs_set and sink the current element of rhs_set.*/
-	  points_to pt_to = make_points_to(l, r, a, make_descriptor_none());
-	  set_add_element(gen, gen, (void*) pt_to);
-	}
-    }
-  /* gen + input_kill_diff*/
-  set_union(res, gen, input_kill_diff);
-
-  return res;
-}
-
-/* iterate over the lhs_set, if it contains more than an element
-   approximations are set to MAY, otherwise it's set to EXACT.*/
-set emami_ref_ref_case(set lhs_set, set rhs_set, set input)
-{
-  /* lhs_path matches the kill set.*/
-  set kill = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set gen =  set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set res = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set input_kill_diff = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  set sink_set = set_generic_make(set_private, points_to_equal_p,
-				    points_to_rank);
-  approximation a = make_approximation_exact();
-
-  SET_FOREACH(points_to, p, input) {
-      SET_FOREACH(cell, c, lhs_set) {
-	  if(cell_equal_p(points_to_source(p), c))
-	    set_add_element(kill,kill,(void*) p);
-	}
-    }
-
-  /* input - kill */
-  set_difference(input_kill_diff, input, kill);
-
-  /* if the lhs_set or the rhs set contains more than an element, we
-     set the approximation to MAT. */
-  if(set_size(lhs_set) > 1 || set_size(rhs_set)>1)
-    a = make_approximation_may();
-
-  /* Computing the gen set*/
-  SET_FOREACH(points_to, pt, input) {
-     SET_FOREACH(cell, s, rhs_set ) {
-       if(cell_equal_p(points_to_source(pt), s))
-	 set_add_element(sink_set, sink_set,(void*)points_to_sink(pt));
-	 }
-  }
-  
-  SET_FOREACH(cell, l, lhs_set){
-      SET_FOREACH(cell, r, sink_set){
-	  /* create a new points to with as source the current
-	     element of lhs_set and sink the current element of rhs_set.*/
-	  points_to pt_to = make_points_to(l, r, a, make_descriptor_none());
-	  set_add_element(gen, gen, (void*) pt_to);
-	}
-    }
-
-  /* gen + input_kill_diff*/
-  set_union(res, gen, input_kill_diff);
-
-  return res;
-}
 
 set points_to_general_assignment(__attribute__ ((__unused__)) statement s,
 				 expression lhs,
@@ -2228,7 +2053,8 @@ list points_to_init_typedef(entity e){
 					    ef);
 	    cell c = get_memory_path(ex1, &eval);
 	    l = gen_nconc(CONS(CELL, c, NIL),l);
-	  }
+	  }else if(array_argument_p(ef))
+	    print_expression(ef);
 	}
       }else if(type_enum_p(t))
 	pips_user_warning("enum case not handled yet \n");
@@ -2287,6 +2113,27 @@ list  points_to_init_struct(entity e, entity ee){
 					  ef);
 	  cell c = get_memory_path(ex1, &eval);
 	  l = gen_nconc(CONS(CELL, c, NIL),l);
+	}
+	else if(array_argument_p(ef)){
+	  basic b = variable_basic(type_variable(entity_type(i)));
+	  /* arrays of pointers are changed into independent store arrays
+	     and initialized to nowhere_b0 */
+	  if(basic_pointer_p(b)){
+	    effect eff = effect_undefined;
+	    expression ind = make_unbounded_expression();
+	    expression ex1 = MakeBinaryCall(entity_intrinsic(FIELD_OPERATOR_NAME),
+					    ex,
+					    ef);
+	    set_methods_for_proper_simple_effects();
+	    list l1 = generic_proper_effects_of_complex_address_expression(ex1, &eff,
+								       true);
+	    reference r2 = effect_any_reference(eff);
+	    effects_free(l1);
+	    generic_effects_reset_all_methods();
+	    reference_indices(r2)= gen_nconc(reference_indices(r2), CONS(EXPRESSION, ind, NIL));
+	    cell c = make_cell_reference(r2);
+	    l = gen_nconc(CONS(CELL, c, NIL),l);
+	  }
 	}
       }
     }

src/Libs/effects-util/anywhere_abstract_locations.c
@@ -481,6 +481,10 @@ bool entity_abstract_location_p(entity al)
       found = strstr(ln, HEAP_AREA_LOCAL_NAME);
       abstract_p = (found!=NULL);
     }
+    /* if(!abstract_p) { */
+/*       found = strstr(ln,NULL_POINTER_NAME); */
+/*       abstract_p = (found!=NULL); */
+/*     } */
   }
 
   return abstract_p;
@@ -505,6 +509,9 @@ entity variable_to_abstract_location(entity v)
 
   if(entity_abstract_location_p(v))
     al = v;
+
+  else if(entity_null_locations_p(v))
+    al = entity_all_locations();
   else if(entity_variable_p(v)
      && !dummy_parameter_entity_p(v)
      && !variable_return_p(v)) {
@@ -520,6 +527,9 @@ entity variable_to_abstract_location(entity v)
 	al = entity_all_locations();
       else
 	al = entity_all_locations();
+
+    
+
     else { // must be a standard variable
       storage s = entity_storage(v);
       ram r = storage_ram(s);

src/Libs/alias-classes/constant-path-utils.c
@@ -175,16 +175,19 @@ set points_to_anywhere_typed(list lhs_list, set input)
 
       /* create a new points to with as source the current
 	 element of lhs_set and sink the null value .*/
-      entity e =entity_all_xxx_locations_typed(ANYWHERE_LOCATION,
-					       cell_reference_to_type(cell_reference(c)));
-      reference r = make_reference(e, NIL);
-      cell sink = make_cell_reference(r);
-      points_to pt_to = make_points_to(c, sink, a, make_descriptor_none());
-      set_add_element(gen, gen, (void*)pt_to);
+    reference cr = cell_to_reference(c);
+    entity er = reference_variable(cr);
+    type t = entity_type(er);
+    entity e = entity_all_xxx_locations_typed(ANYWHERE_LOCATION,t);
+					     /* cell_reference_to_type(cell_reference(c) )); */
+    reference r = make_reference(e, NIL);
+    cell sink = make_cell_reference(r);
+    points_to pt_to = make_points_to(c, sink, a, make_descriptor_none());
+    set_add_element(gen, gen, (void*)pt_to);
     }
   /* gen + input_kill_diff*/
   set_union(res, gen, input_kill_diff);
-
+  
   return res;
 }
 
@@ -753,8 +756,14 @@ bool opkill_must_constant_path(cell c1, cell c2)
   bool type_equal_p = true;
 
   if(! type_area_p(entity_type(v1)) && !type_area_p(entity_type(v2))){
-    t1 = simple_effect_reference_type(r1);
-    t2 = simple_effect_reference_type(r2);
+    if(entity_abstract_location_p(v1))
+      t1 = entity_type(v1);
+    else
+      t1 = simple_effect_reference_type(r1);
+    if(entity_abstract_location_p(v2))
+      t2 = entity_type(v2);
+    else
+      t2 = simple_effect_reference_type(r2);
     type_equal_p = opkill_must_type(t1,t2);
   }
   kill_must_p =opkill_must_module(c1,c2) && opkill_must_name(c1,c2) &&

src/Libs/alias-classes/points_to_interprocedural_analysis.c
@@ -93,30 +93,32 @@ set formal_points_to_parameter(cell c)
 
 /* To create the points-to stub assiciated to the formal parameter,
  * the sink name is a concatenation of the formal parmater and the POINTS_TO_MODULE_NAME */
-points_to create_stub_points_to(cell c, type t)
+points_to create_stub_points_to(cell c, type t, basic b)
 {
-	cell sink = cell_undefined;
-	reference r = reference_undefined;
-	entity e = entity_undefined;
-	reference sink_ref = reference_undefined;
-	approximation rel = approximation_undefined;
-	points_to pt_to = points_to_undefined;
-	r = cell_reference(copy_cell(c));
-	e = reference_variable(r);
-	string s = strdup(concatenate("_", entity_user_name(e),"_", i2a(pointer_index), NULL));
-	string formal_name = strdup(concatenate(POINTS_TO_MODULE_NAME,MODULE_SEP_STRING, s, NULL));
-	entity formal_parameter = gen_find_entity(formal_name);
-	if(entity_undefined_p(formal_parameter)) {
-	  formal_parameter = make_entity(formal_name,
-					 t, make_storage_rom(), make_value_unknown());
-	}
-	sink_ref = copy_reference(make_reference(formal_parameter, NIL));
-	sink = make_cell_reference(sink_ref);
-	rel = make_approximation_exact();
-	pt_to = make_points_to(copy_cell(c), sink, rel,
-			       make_descriptor_none());
-	pointer_index ++;
-	return pt_to;
+  expression ex = make_unbounded_expression();
+  reference r = cell_to_reference(copy_cell(c));
+  entity e = reference_variable(r);
+  string s = strdup(concatenate("_", entity_user_name(e),"_", i2a(pointer_index), NULL));
+  string formal_name = strdup(concatenate(POINTS_TO_MODULE_NAME,MODULE_SEP_STRING, s, NULL));
+  entity formal_parameter = gen_find_entity(formal_name);
+  type tt = make_type_variable(
+			       make_variable(copy_basic(b),
+					     CONS(DIMENSION,
+						  make_dimension(int_to_expression(0),ex),NIL),
+					     NIL));
+  if(entity_undefined_p(formal_parameter)) {
+    formal_parameter = make_entity(formal_name,
+				   tt, make_storage_formal(make_formal(get_current_module_entity(),pointer_index)), make_value_unknown());
+  }
+  reference sink_ref = copy_reference(make_reference(formal_parameter, NIL));
+/*   expression ind = make_unbounded_expression(); */
+  reference_indices_(sink_ref)= CONS(EXPRESSION, int_to_expression(0), NIL); //;CONS(EXPRESSION, ind, NIL);
+  cell sink = make_cell_reference(sink_ref);
+  approximation rel = make_approximation_exact();
+  points_to pt_to = make_points_to(copy_cell(c), sink, rel,
+				   make_descriptor_none());
+  pointer_index ++;
+  return pt_to;
 }
 
 
@@ -152,13 +154,13 @@ set  pointer_formal_parameter_to_stub_points_to(type pt, cell c)
       basic fpb = variable_basic(type_variable(upt));
       switch(basic_tag(fpb)){
       case is_basic_int:{
-	pt_to = create_stub_points_to(c, upt);
+	pt_to = create_stub_points_to(c, upt, fpb);
 	pt_in = set_add_element(pt_in, pt_in,
 				(void*) pt_to );
 	break;
       }
       case is_basic_float:{
-	pt_to = create_stub_points_to(c, upt);
+	pt_to = create_stub_points_to(c, upt, fpb);
 	pt_in = set_add_element(pt_in, pt_in,
 				(void*) pt_to );
 	break;
@@ -171,7 +173,7 @@ set  pointer_formal_parameter_to_stub_points_to(type pt, cell c)
 	break;
       case is_basic_pointer:{
 	//pt = type_to_pointed_type(pt);
-	pt_to = create_stub_points_to(c, upt);
+	pt_to = create_stub_points_to(c, upt, fpb);
 	pt_in = set_add_element(pt_in, pt_in,
 				(void*) pt_to );
 	cell sink = points_to_sink(pt_to);
@@ -180,20 +182,20 @@ set  pointer_formal_parameter_to_stub_points_to(type pt, cell c)
 	break;
       }
       case is_basic_derived:
-	pt_to = create_stub_points_to(c, upt);
+	pt_to = create_stub_points_to(c, upt, fpb);
 	pt_in = set_add_element(pt_in, pt_in,
 				(void*) pt_to );
 	break;
       case is_basic_bit:
 	break;
       case is_basic_string:{
-	pt_to = create_stub_points_to(c, upt);
+	pt_to = create_stub_points_to(c, upt, fpb);
 	pt_in = set_add_element(pt_in, pt_in,
 				(void*) pt_to );
 	break;
       }
       case is_basic_typedef:{
-	pt_to = create_stub_points_to(c, upt);
+	pt_to = create_stub_points_to(c, upt, fpb);
 	pt_in = set_add_element(pt_in, pt_in,
 				(void*) pt_to );
 	break;
@@ -208,7 +210,7 @@ set  pointer_formal_parameter_to_stub_points_to(type pt, cell c)
        be done for points-to analysis. */
   else if(type_void_p(upt)) {
     /* Create a target of unknown type */
-    pt_to = create_stub_points_to(c, make_type_unknown() /*memory leak?*/);
+    pt_to = create_stub_points_to(c, make_type_unknown(), basic_undefined /*memory leak?*/);
     pt_in = set_add_element(pt_in, pt_in, (void*) pt_to );
   }
   else

src/Libs/effects-generic/eval.c
@@ -150,8 +150,14 @@ list generic_eval_cell_with_points_to(cell input_cell, descriptor input_desc, li
 
   if (entity_abstract_location_p(input_ent))
     {
+      descriptor build_desc = descriptor_undefined;
       /* If the input path is an abstract location, the result is the same abstract location */
-      l = CONS(CELL, make_cell(is_cell_reference, make_reference(input_ent, NIL)), NIL);
+     /*  l = CONS(CELL, make_cell(is_cell_reference, make_reference(input_ent, NIL)), NIL); */
+      l = CONS(EFFECT, make_effect( make_cell(is_cell_reference, make_reference(input_ent, NIL)),
+				  make_action_write_memory(),
+				  make_approximation(*exact_p? is_approximation_exact : is_approximation_may, UU),
+				  build_desc),
+	       NIL);
       *exact_p = false;
     }
   else

src/Libs/effects-util/abstract_location.c
@@ -379,73 +379,81 @@ entity calloc_to_abstract_location(expression n, expression size,
 /* the list of callers will be added to ensure the context sensitive
    property. We should keep in mind that context and sensitive
    properties are orthogonal and we should modify them in pipsmake.*/
-reference original_malloc_to_abstract_location(reference __attribute__ ((unused)) lhs,
-					       type var_t,
+reference original_malloc_to_abstract_location(expression lhs,
+					       type  __attribute__ ((unused)) var_t,
 					       type __attribute__ ((unused)) cast_t,
-					       expression __attribute__ ((unused)) sizeof_exp,
+					       expression sizeof_exp,
 					       entity f,
-					       int stmt_number)
+					       statement stmt)
 {
   reference r = reference_undefined;
   entity e = entity_undefined;
-  //string st, s;
+  sensitivity_information si =
+    make_sensitivity_information(stmt,
+				 f,
+				 NIL);
   string opt = get_string_property("ABSTRACT_HEAP_LOCATIONS");
   bool type_sensitive_p = !get_bool_property("ALIASING_ACROSS_TYPES");
 
-  /* in case we want an anywhere abstract heap location : the property
-     ABSTRACT_HEAP_LOCATIONS is set to "unique" and a unique abstract
-     location is used for all heap buckets. */
-  if(strcmp(opt, "unique")==0){
-    if(type_sensitive_p) {
-      e = entity_all_heap_locations_typed(var_t);
-      r = make_reference(e , NIL);
-    }
-    else {
-      e = entity_all_heap_locations();
-      r = make_reference(e , NIL);
-    }
-  }
-
-  /* in case the property ABSTRACT_HEAP_LOCATIONS is set to
-     "insensitive": an abstract location is used for each function. */
-  else if(strcmp(opt, "insensitive")==0){
-    if(type_sensitive_p) {
-      e = entity_all_module_heap_locations_typed(get_current_module_entity(),
-						 var_t);
-      r = make_reference(e , NIL);
-    }
-    else {
-      e = entity_all_module_heap_locations(f);
-      r = make_reference(e , NIL);
-    }
-  }
-
-  /* in case the property ABSTRACT_HEAP_LOCATIONS is set to
-     "flow-sensitive" or "context-sensitive".
-
-     No difference here between the two values. The diffferent
-     behavior will show when points-to and effects are translated at
-     call sites
-
-     At this level, we want to use the statement number or the
-     statement ordering. The statement ordering is safer because two
-     statements or more can be put on the same line. The statement
-     number is more user-friendly.
-
-     There is no need to distinguish between types since the statement
-     ordering is at least as effective.
-  */
-  else if(strcmp(opt, "flow-sensitive")==0
-	  || strcmp(opt, "context-sensitive")==0 ){
-    e = entity_flow_or_context_sentitive_heap_location(stmt_number, var_t);
+  e = malloc_to_abstract_location(sizeof_exp, &si);
+  if(!entity_array_p(e))
     r = make_reference(e , NIL);
-  }
-  else {
-    pips_user_error("Unrecognized value for property ABSTRACT_HEAP_LOCATION:"
-		    " \"%s\"", opt);
-  }
+  else
+  r = make_reference(e , CONS(EXPRESSION, int_to_expression(0), NIL));
+  /* /\* in case we want an anywhere abstract heap location : the property */
+/*      ABSTRACT_HEAP_LOCATIONS is set to "unique" and a unique abstract */
+/*      location is used for all heap buckets. *\/ */
+/*   if(strcmp(opt, "unique")==0){ */
+/*     if(type_sensitive_p) { */
+/*       e = entity_all_heap_locations_typed(var_t); */
+/*       r = make_reference(e , NIL); */
+/*     } */
+/*     else { */
+/*       e = entity_all_heap_locations(); */
+/*       r = make_reference(e , NIL); */
+/*     } */
+/*   } */
 
-  pips_debug(8, "Reference to ");
+/*   /\* in case the property ABSTRACT_HEAP_LOCATIONS is set to */
+/*      "insensitive": an abstract location is used for each function. *\/ */
+/*   else if(strcmp(opt, "insensitive")==0){ */
+/*     if(type_sensitive_p) { */
+/*       e = entity_all_module_heap_locations_typed(get_current_module_entity(), */
+/* 						 var_t); */
+/*       r = make_reference(e , NIL); */
+/*     } */
+/*     else { */
+/*       e = entity_all_module_heap_locations(f); */
+/*       r = make_reference(e , NIL); */
+/*     } */
+/*   } */
+
+/*   /\* in case the property ABSTRACT_HEAP_LOCATIONS is set to */
+/*      "flow-sensitive" or "context-sensitive". */
+
+/*      No difference here between the two values. The diffferent */
+/*      behavior will show when points-to and effects are translated at */
+/*      call sites */
+
+/*      At this level, we want to use the statement number or the */
+/*      statement ordering. The statement ordering is safer because two */
+/*      statements or more can be put on the same line. The statement */
+/*      number is more user-friendly. */
+
+/*      There is no need to distinguish between types since the statement */
+/*      ordering is at least as effective. */
+/*   *\/ */
+/*   else if(strcmp(opt, "flow-sensitive")==0 */
+/* 	  || strcmp(opt, "context-sensitive")==0 ){ */
+/*     e = entity_flow_or_context_sentitive_heap_location(stmt_number, var_t); */
+/*     r = make_reference(e , NIL); */
+/*   } */
+/*   else { */
+/*     pips_user_error("Unrecognized value for property ABSTRACT_HEAP_LOCATION:" */
+/* 		    " \"%s\"", opt); */
+/*   } */
+
+/*   pips_debug(8, "Reference to "); */
 
   return r;
 }

src/Libs/effects-util/conflicts.c
@@ -377,7 +377,7 @@ bool references_may_conflict_p( reference r1, reference r2 ) {
 
     if ( v1 != v2 ) {
       /* We do not bother with the offset and the array types in case
-       of static aliasing */
+	 of static aliasing */
       /* We do not bother with the abstract locations */
       conflict_p = TRUE;
     } else {
@@ -400,10 +400,18 @@ bool references_may_conflict_p( reference r1, reference r2 ) {
        */
       if ( !entity_abstract_location_p( reference_variable(r1) )
 	   && !entity_abstract_location_p( reference_variable(r2) ) ) {
-	type t1 = cell_reference_to_type( r1 );
-	type t2 = cell_reference_to_type( r2 );
 
-	conflict_p = type_equal_p( t1, t2 );
+	/* Are we dealing with NULL POINTER ? */
+	if ( entity_null_locations_p(v1) && entity_null_locations_p(v2) )
+	  conflict_p = TRUE;
+	else if(entity_null_locations_p(v1) || entity_null_locations_p(v2) )
+	  conflict_p = FALSE;
+	else{
+	  type t1 = cell_reference_to_type( r1 );
+	  type t2 = cell_reference_to_type( r2 );
+	  
+	  conflict_p = type_equal_p( t1, t2 );
+	}
       }
     }
 
@@ -417,6 +425,12 @@ bool references_may_conflict_p( reference r1, reference r2 ) {
 	conflict_p = FALSE;
       }
     }
+
+    /* Are we dealing with NULL POINTER ? */
+    if ( entity_null_locations_p(v1) && entity_null_locations_p(v2) )
+      conflict_p = TRUE;
+    else if(entity_null_locations_p(v1) || entity_null_locations_p(v2) )
+      conflict_p = FALSE;
 
     /* There still could be a conflict in C because the two references
        might point to the same memory location. It might be more
@@ -560,6 +574,9 @@ bool entities_maymust_conflict_p( entity e1, entity e2, bool must_p )
   if ( entity_abstract_location_p( e1 ) )
     if ( entity_abstract_location_p( e2 ) )
       conflict_p = abstract_locations_conflict_p( e1, e2 );
+    else if ( entity_null_locations_p(e2) ) {
+      conflict_p = entity_all_locations_p(e1);
+    }
     else if ( entity_variable_p(e2) ) {
       if ( variable_return_p( e2 ) )
 	conflict_p = FALSE;
@@ -579,7 +596,10 @@ bool entities_maymust_conflict_p( entity e1, entity e2, bool must_p )
     }
   else {
     if ( entity_abstract_location_p( e2 ) ) {
-      if ( entity_variable_p(e1) ) {
+      if ( entity_null_locations_p(e1)) {
+	conflict_p = entity_all_locations_p(e2);
+	}
+      else if ( entity_variable_p(e1) ) {
 	if ( variable_return_p( e1 ) )
 	  conflict_p = FALSE;
 	else if ( entity_formal_p( e1 ) ) {
@@ -599,6 +619,8 @@ bool entities_maymust_conflict_p( entity e1, entity e2, bool must_p )
 	return e1 == e2;
       }
       else if ( entity_formal_p( e1 ) && entity_formal_p( e2 ) ) {
+	return e1 == e2;
+      }else if( entity_null_locations_p(e1) || entity_null_locations_p(e2) ){
 	return e1 == e2;
       }
       else if ( entity_variable_p(e1) && entity_variable_p(e2) ) {

commit 2a638c2e9ec77dd9308594b85bce7b59463ebb23

fix a bug at points_to_storage() introduced by r19379
git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@19402 32a0c1e6-9308-0410-80dd-f9d2257a62c7

src/Libs/alias-classes/points_to_analysis_general_algorithm.c
@@ -332,7 +332,7 @@ void points_to_storage(set pts_to_set, statement s, bool store) {
   }
   else if(set_empty_p(pts_to_set)){
     points_to_list new_pt_list = make_points_to_list(pt_list);
-    store_pt_to_list(s, new_pt_list);
+    store_or_update_pt_to_list(s, new_pt_list);
   }
 }
 

src/Libs/effects-util/anywhere_abstract_locations.c
@@ -481,10 +481,10 @@ bool entity_abstract_location_p(entity al)
       found = strstr(ln, HEAP_AREA_LOCAL_NAME);
       abstract_p = (found!=NULL);
     }
-    /* if(!abstract_p) { */
-/*       found = strstr(ln,NULL_POINTER_NAME); */
-/*       abstract_p = (found!=NULL); */
-/*     } */
+    if(!abstract_p) {
+      found = strstr(ln,NULL_POINTER_NAME);
+      abstract_p = (found!=NULL);
+    }
   }
 
   return abstract_p;
@@ -509,9 +509,9 @@ entity variable_to_abstract_location(entity v)
 
   if(entity_abstract_location_p(v))
     al = v;
-
+  /* NULL is an abstract location  */
   else if(entity_null_locations_p(v))
-    al = entity_all_locations();
+    al = v;
   else if(entity_variable_p(v)
      && !dummy_parameter_entity_p(v)
      && !variable_return_p(v)) {