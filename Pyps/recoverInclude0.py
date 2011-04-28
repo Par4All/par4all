25babea778ae63d0a1fd8c3e9e02ec7c6b9a6d13
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-04-28 09:25:12 +0000

    Fix test case when PAGER env variable is undefined
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3440 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pyps/recoverInclude0.py
+++ Pyps/recoverInclude0.py
@@ -1,9 +1,11 @@
 import os
 from pyps import workspace
+
 w=workspace("recoverInclude0.c",cppflags="-Iinclude",deleteOnClose=True,name="recoverInclude0")
 (sources, headers)=w.save()
 print sources
 print headers
-for i in sources:os.system(os.environ["PAGER"]+" " + i)
+for i in sources:
+	os.system(os.getenv("PAGER","cat")+" " + i)
 
 w.close()
