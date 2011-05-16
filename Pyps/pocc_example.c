15ddf1ad64b57727cf21d9c20e901ce8838b1215
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-16 18:36:30 +0000

    Add an example on how to use PoCC with pyps ; won't run the validation...
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3538 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pyps/pocc_example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int a[10][10],l,i;
+
+  for( i = 0; i<10; i++) {
+    for( l = 0; l<10; l++) {
+      a[i][l]=0;
+    }  
+  } 
+
+  return;
+}
---
+++ Pyps/pocc_example.py
@@ -0,0 +1,13 @@
+from __future__ import with_statement # this is to work with python2.5
+
+# Here we add an example for interacting with PoCC
+# it requires that polycc is in the PATH
+# we can't run it in the validation because PoCC isn't installed
+
+from pocc import workspace
+
+with workspace("pocc_example.c",name="poly",deleteOnCreate=True) as w:
+	w.all_functions.poccify()
+	w.fun.main.display()
+
+
