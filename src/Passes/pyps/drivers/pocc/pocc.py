5d82e6a01ea1428b8f295e9b85a6d37eca308dff
amini <amini@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2011-05-16 18:26:26 +0000

    Add a workspace for interacting easily with PoCC (polycc has to be in the PATH)
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@19480 32a0c1e6-9308-0410-80dd-f9d2257a62c7

---
+++ src/Passes/pyps/drivers/pocc/pocc.py
@@ -0,0 +1,54 @@
+
+import pyps,os,subprocess,shutil
+from re import match
+
+
+def poccify(self,**props):
+    ''' This method will try to find out static control parts, then outline
+    them in separated functions, pass pocc (polycc) on it, and get it back in 
+    PIPS'''
+
+    # Find scop and print pragma
+    self.pocc_prettyprinter()
+
+    # outline scop in new modules
+    self.scop_outliner(SCOP_PREFIX="PYPS_SCOP")
+    
+    # FIXME should only poccify previously outlined modules !! 
+    # need work on pips side
+    for m in self.workspace.filter(lambda m:match("PYPS_SCOP",m.name)):        
+        # Get source file for this module
+        code_rc=os.path.join(m.workspace.dirname,m.workspace.cpypips.show("C_SOURCE_FILE",m.name))
+        
+        # Run polycc !
+        # FIXME : args should be user defined (but it affect the output filename :-(
+        p = subprocess.Popen("polycc --parallel --tile %s " % (code_rc), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (sout,serr) = p.communicate()
+        if p.returncode != 0:
+            raise RuntimeError("Error while trying to call polycc %d.\nstdout:\n%s\nstderr:\n%s\n" % (p.returncode, sout, serr))
+
+        # Get back polycc result with some filtering !
+        basename, extension = os.path.splitext(code_rc)
+        pocc_file = basename + ".par" + extension
+        p = subprocess.call("grep -v '^#include' %s | grep -v '^#define' | grep -v '^/\*' > %s" % (pocc_file,code_rc),shell=True)
+        p = subprocess.call("cat %s" % (code_rc),shell=True)
+        
+        # Inline back the result
+        m.inlining()
+
+pyps.module.poccify = poccify
+
+def poccify(self, concurrent=False, **props):
+    """  
+"""
+    for m in self:
+        m.poccify()
+        
+
+pyps.modules.poccify = poccify
+
+
+
+class workspace(pyps.workspace):
+    ''' This is a workspace for interacting with pocc '''
+    
