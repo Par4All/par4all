73597ed1d7372ee575d636fa44eee0f791870175
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-18 11:28:57 +0000

    Hardened loop_fusion07 using scope and different entities as loop_indices
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3549 67b5017d-e62d-0410-a871-f72b1c62810f

--- Loop_fusion/loop_fusion07.c
+++ Loop_fusion/loop_fusion07.c
@@ -2,22 +2,26 @@
 
 
 int main() {
-  int a[10];
-
    double A[1][10000];
+   {
+      int lv1, lv2;
+      for(lv1 = 0; lv1 <= 0; lv1 += 1)
+         for(lv2 = 0; lv2 <= 9999; lv2 += 1)
+            A[lv1][lv2] = (double) 1.0;
+   }
    double B[1][10000];
+   {
+      int lv1, lv2;
+      for(lv1 = 0; lv1 <= 0; lv1 += 1)
+         for(lv2 = 0; lv2 <= 9999; lv2 += 1)
+            B[lv1][lv2] = (double) 1.0;
+   }
    double C[1][10000];
-   int lv1, lv2;
-
-// All these loops are fusable
-   for(lv1 = 0; lv1 <= 0; lv1 += 1)
-      for(lv2 = 0; lv2 <= 9999; lv2 += 1)
-         A[lv1][lv2] = (double) 1.0;
-   for(lv1 = 0; lv1 <= 0; lv1 += 1)
-      for(lv2 = 0; lv2 <= 9999; lv2 += 1)
-         B[lv1][lv2] = (double) 1.0;
-   for(lv1 = 0; lv1 <= 0; lv1 += 1)
-      for(lv2 = 0; lv2 <= 9999; lv2 += 1)
-         C[lv1][lv2] = A[lv1][lv2]+B[lv1][lv2];
+   {
+      int lv1, lv2;
+      for(lv1 = 0; lv1 <= 0; lv1 += 1)
+         for(lv2 = 0; lv2 <= 9999; lv2 += 1)
+            C[lv1][lv2] = A[lv1][lv2]+B[lv1][lv2];
+   }
 
 }
