1d56a3fa92c46dc06166173014e8ec4f04094d3e
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-19 20:24:59 +0000

    Go for pyps based validation for Dead_code_elimination, work in progress !
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3605 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/Dead-code-elimination.sub/use_def_elimination.py
@@ -0,0 +1,17 @@
+from __future__ import with_statement # this is to work with python2.5
+
+from validation import vworkspace
+import validate_dead_code_elimination
+import os
+
+
+with vworkspace() as w:
+    w.props.PRETTYPRINT_FINAL_RETURN = True
+    w.props.PRETTYPRINT_ALL_LABELS  = True
+    w.props.PRETTYPRINT_EMPTY_BLOCKS  = True
+    w.props.PRETTYPRINT_UNSTRUCTURED  = True
+    w.props.memory_effects_only = False
+
+    w.all_functions.validate_phases("unspaghettify","suppress_dead_code","dead_code_elimination",display_initial=True)
+
+
