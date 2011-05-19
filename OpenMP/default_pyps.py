a9cca020364836384892aea3f30cc84978504e17
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-19 22:56:54 +0000

    Switch python part of Openmp validation to default_pyps.py and common pyps validation infrastructure.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3611 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ OpenMP/default_pyps.py
@@ -0,0 +1,7 @@
+from __future__ import with_statement
+import openmp
+from validation import vworkspace
+
+with vworkspace() as w:
+    w.all_functions.validate_phases("openmp")
+
---
+++ OpenMP/py-nested_c.c
@@ -0,0 +1,1 @@
+nested_c.c
