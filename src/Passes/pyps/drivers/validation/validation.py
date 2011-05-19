08493ac132960102979d74c6997b5c629c9571ec
amini <amini@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2011-05-19 17:18:32 +0000

    Add more automatisation for validation stuff using pyps
    No need for environment variable : it'll use the script filename as a fallback
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@19521 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/pyps/drivers/validation/validation.py
+++ src/Passes/pyps/drivers/validation/validation.py
@@ -1,23 +1,37 @@
-import pyps,os
+import pyps,os,sys
 
 class vworkspace(pyps.workspace): 
     ''' This workspace is intended to handle some special PIPS validation 
     suff'''
     
     def __init__(self, *sources, **kwargs):
-        """init a workspace from (optionnal) sources for validation
+        """init a workspace from (optional) sources for validation
            name will be gather from WSPACE and sources from FILE environment variables 
         """
 
+        file = os.getenv('FILE')
+        if file != None and not os.path.isfile(file):
+            sys.stderr.write("Error, file " + file + " doesn't exists !!")
+            file = None
+            
+        if file == None :
+            # Try to recover source corresponding to script filename
+           basename = os.path.splitext(sys.argv[0])[0]
+           file = basename + '.c'
+           if not os.path.isfile(file):
+               file = basename + '.f'
+           if not os.path.isfile(file):
+               file = basename + '.f95'
+           if not os.path.isfile(file):
+               raise RuntimeError('''No source files ! Please define FILE environment 
+                                    variable or provide %s.{c,f,f95}''' % (file) )
+            
         # this workspace is intended to be run with WSPACE and FILE 
         # environment variable defined
         wspace = os.getenv("WSPACE")
         if wspace == None :
-            raise RuntimeError("WSPACE environment variable has to be defined")
-
-        file = os.getenv('FILE')
-        if file == None :
-            raise RuntimeError("FILE environment variable has to be defined")
+           sys.stderr.write("WSPACE environment variable has to be defined\n")
+           wspace = os.path.splitext(sys.argv[0])[0]
 
         super(vworkspace, self).__init__(file,
                                          *sources,
@@ -26,4 +40,37 @@ class vworkspace(pyps.workspace):
                                          deleteOnCreate=True,
                                          **kwargs)
 
+
+def validate_phases(self,*phases,**kwargs):
+    display_after= kwargs.setdefault("display_after",  True)
+    display_initial= kwargs.setdefault("display_initial",  True)
+    if display_initial:
+        print "//"
+        print "// Initial code for module " + self.name
+        print "//"
+        self.display()
+    for phase in list(phases):
+        getattr(self,phase)()
+        if display_after:
+            print "//"
+            print "// Code after " + phase + " for module " + self.name
+            print "//"
+            self.display()
         
+pyps.module.validate_phases = validate_phases
+        
+        
+def validate_phases(self, *phases,**kwargs):
+    display_initial= kwargs.setdefault("display_initial",  False)
+    display_after= kwargs.setdefault("display_after",  True)
+    if display_initial:
+        for m in self:
+            print "//"
+            print "// Initial code for module " + m.name
+            print "//"
+            m.display()
+    for phase in list(phases):
+        for m in self:
+            m.validate_phases(phase,display_after=display_after,display_initial=False)
+
+pyps.modules.validate_phases = validate_phases
