5bcaa9ed1c5e437fafb9b4b31e94518d018f61e6
coelho <coelho@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2011-05-20 09:02:57 +0000

    update polylib version.
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@1126 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

--- makes/setup_pips.sh
+++ makes/setup_pips.sh
@@ -33,11 +33,9 @@
 SVN_CRI='https://svn.cri.ensmp.fr/svn'
 PIPS_SVN=$SVN_CRI/pips
 
-#POLYLIB_SITE='http://icps.u-strasbg.fr/polylib/polylib_src'
 #POLYLIB_SITE='http://www.cri.ensmp.fr/pips'
-#POLYLIB='polylib-5.22.1'
 POLYLIB_SITE='http://icps.u-strasbg.fr/polylib/polylib_src'
-POLYLIB='polylib-5.22.3'
+POLYLIB='polylib-5.22.5'
 
 # help
 command=${0/*\//}
@@ -177,14 +175,14 @@ export NEWGEN_SVN=$SVN_CRI/newgen
 export LINEAR_SVN=$SVN_CRI/linear
 export PIPS_SVN=$SVN_CRI/pips
 
-# production directory
-prod=$prod
-
 # software roots are not needed
 #export EXTERN_ROOT=\$prod/extern
 #export NEWGEN_ROOT=\$prod/newgen
 #export LINEAR_ROOT=\$prod/linear
 #export PIPS_ROOT=\$prod/pips
+
+# production directory
+prod=$prod
 
 # fix path
 PATH=\$prod/pips/bin:\$prod/pips/utils:\$prod/newgen/bin:\$PATH
