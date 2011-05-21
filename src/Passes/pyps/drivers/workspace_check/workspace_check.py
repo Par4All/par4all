bd19953d376d2b012f2da73d08e7449822297307
amini <amini@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2011-05-21 20:04:57 +0000

    Add possibility to disable workspace_check and assert that main is prevent before trying to run
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@19530 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/pyps/drivers/workspace_check/workspace_check.py
+++ src/Passes/pyps/drivers/workspace_check/workspace_check.py
@@ -8,22 +8,25 @@ class workspace(pyps.workspace):
         self.__ref_maker = kwargs.get('ref_maker',pyps.Maker())
         self.__ref_argv = kwargs.get('ref_argv',[])
         self.__ref_output=None
+        self.__enable_check = True
 
     def __compile_and_run(self):
+        if "main" in self.fun:
             a_out = self.compile(self.__ref_maker)
             (_,cout,_) = self.run(a_out,self.__ref_argv)
             return cout
 
     def pre_phase(self,phase,module):
         """ generate reference if needed """
-        if self.__ref_output == None:
+        if self.__enable_check and self.__ref_output == None:
             self.__ref_output=self.__compile_and_run()
         super(workspace,self).pre_phase(phase,module)
 
     def post_phase(self,phase,module):
         """ check resulting code after the apply """
         super(workspace,self).post_phase(phase,module)
-        output=self.__compile_and_run()
-        if output != self.__ref_output:
-            raise RuntimeError("check by workspace check failed after %s on %s\n" % (phase , module ))
+        if self.__enable_check:
+            output=self.__compile_and_run()
+            if output != self.__ref_output:
+                raise RuntimeError("check by workspace check failed after %s on %s\n" % (phase , module ))
 
