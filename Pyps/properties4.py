611e0bdbd654596503373bfb07c07fc9293aae83
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-23 12:46:28 +0000

    Some more pyps valdiation.
    * gather pragma from loop
    * use [] to set props
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3630 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pyps/properties4.py
@@ -0,0 +1,6 @@
+from __future__ import with_statement # this is to work with python2.5
+from pyps import workspace
+with workspace("properties0.c",verbose=False,deleteOnClose=True) as w:
+	print w.props.MUST_REGIONS
+	w.props["MUST_REGIONS"]=not  w.props["MUST_REGIONS"]
+	print w.props.must_regions
