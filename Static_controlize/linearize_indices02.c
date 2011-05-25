1b9d5d5ea34d65d34ffdb7f2a691631e1286f57a
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-25 16:32:30 +0000

    Fix this test case so that it pass GCC
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3662 67b5017d-e62d-0410-a871-f72b1c62810f

--- Static_controlize/linearize_indices02.c
+++ Static_controlize/linearize_indices02.c
@@ -3,6 +3,6 @@
 void not_linearized_array_indices(int n, float fx[n], float pot[n])
 {
    for(int i = 0; i <= n; i += 1)
-     fx[i] = pot[k+1&128-1]/(2.*(float) 6.f/128);
+     fx[i] = pot[((i+1)&128)-1]/(2.*(float) 6.f/128);
 }
 
