1ed70a7c8d9ce169ec23fb16cf994eeafd220943
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-26 19:15:51 +0000

    Update this test case so that it's more representative and is less a particular case
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3672 67b5017d-e62d-0410-a871-f72b1c62810f

--- OpenMP/privatize_local_variable.c
+++ OpenMP/privatize_local_variable.c
@@ -3,15 +3,15 @@ int main()
 {
   int k;
   int n = 10;
-  int a[n][n];
+  int a[n][n][n];
+  int b[n][n][n];
 
 // Use to generate a pragma openmp with private(j) ; but j is declared inside the loop !
   for (k = 0; k < 8; ++k) {
     int i,j;
-    int b[n][n];
     for (i = 0; i < n; ++i) {
       for (j = 0; j < n; ++j) {
-        b[i][j] = a[i][j] + 42;
+        b[k][i][j] = a[k][i][j] + 42;
       }
     }
   }
