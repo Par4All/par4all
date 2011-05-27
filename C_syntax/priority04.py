cdbbf7e9e64f75a582d5da063cdae336359f90c2
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-27 17:24:37 +0000

    Add a parser bug : priority of unary and cast over binary operators.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3678 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ C_syntax/priority04.py
@@ -0,0 +1,8 @@
+from __future__ import with_statement # this is to work with python2.5
+
+from validation import vworkspace
+
+with vworkspace() as w:
+    w.fun.main.display()
+    print "Result of the execution : " + w.compile_and_run()
+
