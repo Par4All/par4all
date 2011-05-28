c03a4c97aab7934d66565a9bf7c0e8ce135c5a8f
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-28 09:22:22 +0000

    Improve test cases and reference
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3679 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/priority04.py
+++ C_syntax/priority04.py
@@ -4,5 +4,6 @@ from validation import vworkspace
 
 with vworkspace() as w:
     w.fun.main.display()
+    # The following line is just for design purpose, should be removed before validating the test case
     print "Result of the execution : " + w.compile_and_run()
 
