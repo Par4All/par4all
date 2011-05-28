d77fb57008fefce744de8f04fad6c9a7e31568ab
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-28 16:06:11 +0000

    Move test case to corresponding feature subdir
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3697 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/Loop-fusion.sub/conv.py
+++ Transformations/Loop-fusion.sub/conv.py
@@ -5,7 +5,7 @@ with workspace("conv.c") as w:
 	w.activate(module.region_chains)
 	w.activate(module.rice_regions_dependence_graph)
 	c.loops(0).unroll(rate=4)
-	c.delay_communications(accel_load="=")
+	#c.delay_communications(accel_load="=")
 	c.partial_eval()
 	c.loop_fusion()
 	c.display()
