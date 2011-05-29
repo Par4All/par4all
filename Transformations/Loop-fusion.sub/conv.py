62325229eed9819eb0778a1fa8c65861b97d4ff8
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-29 11:37:44 +0000

    Add conv test case for loop_fusion
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3699 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/Loop-fusion.sub/conv.py
+++ Transformations/Loop-fusion.sub/conv.py
@@ -1,12 +1,8 @@
-from pyps import workspace, module
-with workspace("conv.c") as w:
-	c=w.fun.conv
-	w.activate(module.must_regions)
-	w.activate(module.region_chains)
-	w.activate(module.rice_regions_dependence_graph)
-	c.loops(0).unroll(rate=4)
-	#c.delay_communications(accel_load="=")
-	c.partial_eval()
-	c.loop_fusion()
-	c.display()
+from validation import vworkspace
+
+with vworkspace() as w:
+    c=w.fun.conv
+    w.props.loop_fusion_greedy = True
+    c.loops(0).unroll(rate=4)
+    c.validate_phases("partial_eval","flatten_code","loop_fusion")
 
