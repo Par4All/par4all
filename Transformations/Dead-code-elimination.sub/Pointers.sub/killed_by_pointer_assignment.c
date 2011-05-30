0e835e03c772c54c4f27965fe5e6e50332bda86d
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-30 15:41:56 +0000

    Comment out the test case
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3722 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/Dead-code-elimination.sub/Pointers.sub/killed_by_pointer_assignment.c
+++ Transformations/Dead-code-elimination.sub/Pointers.sub/killed_by_pointer_assignment.c
@@ -3,11 +3,16 @@ int dead_code_elim01()
 {
   int i, *x, *y;
 
+// i is firstly initialized
   i = 2;
+
+//  make y points to i
   x = &i;
   y = x;
-  *y = 1;
+
+// Here we kill first i assignment
+  *y = 1; 
   
-  return *y;
+  return i;
 }
 
