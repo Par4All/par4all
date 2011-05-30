818be7656a9ca8c8f5b294c7a42c410ee43bdea5
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-05-30 08:30:54 +0000

    add examples presented at the May 19 meeting
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3713 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Chains/dead_code_elim01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/* test case for use-def elimination suggested by François for Mehdi.*/
+int dead_code_elim01()
+{
+  int i, *x, *y;
+
+  i = 2;
+  x = &i;
+  y = x;
+  *y = 1;
+  
+  return *y;
+}
+
