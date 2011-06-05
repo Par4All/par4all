0fc6624f073c0952b00f728a2d3fffb333450284
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-06-05 12:56:34 +0000

    maisonneuve07-4: same as maisonneuve07-1 but b is not numerically known
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3764 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics-New/maisonneuve07-4.c
+++ Semantics-New/maisonneuve07-4.c
@@ -1,5 +1,9 @@
 // This program has an invariant of kind -b<=x<=b which cannot be
 // derived directly from T'(dx)
+//
+// This is a copy of maisonneuve07-1.c, but b is no longer a known
+// constant as in maisonneuve07-3.c; however, x is still initialized
+// to 0
 
 // $Id$
 
@@ -8,7 +12,7 @@
 void run(void) {
   int x = 0;
   int t = 0;
-  int b = 10; // b > 0 should be enough
+  int b = rand(); // b > 0 should be enough
   while(1) {
     if(rand() % 2 && x >= -b) x--;
     if(rand() % 2 && x<=b) x++;
