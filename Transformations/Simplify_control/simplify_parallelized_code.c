970c6bfad0ff51d64d32b24e05993476bbbe52b3
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-06-14 21:34:10 +0000

    Simplify control failed when transformers_in_context is activated
    Two fails in one :
    
    - If directly applied on the internalized parallel code computed with transformers in context, then I get a "Newgen User Error -- gen_check: Type clash (expecting range, getting ???)"
    - If I first apply the internalize_parallel_code without transformers_in_context, and then do the same sequence with transformers_in_context then I get : pips error in find_final_statement_label: (../../../../../pips/src/Libs/transformations/loop_unroll.c:87) Useless empty sequence. Unexpected in controlized code.Abandon
    
    
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3854 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/Simplify_control/simplify_parallelized_code.c
@@ -0,0 +1,27 @@
+
+
+
+int simplifailed(int argc, char** argv) {
+  int t, i, j, cnt;
+  int length = 6;
+  int maxgrid = 64;
+  int niter = 1000;
+  int A[maxgrid];
+
+
+  for (j = 0; j <= maxgrid - 1; j++) {
+    A[j] = 0;
+    for (i = j + 1; i <= maxgrid - 1; i++)
+      int sum = 0;
+  }
+
+  for (t = 0; t < niter; t++) {
+    int sum2 = 0;
+    for (i = 0; i <= maxgrid - 1; i++)
+      int sum3 = 0;
+  }
+
+
+  return 0;
+}
+
