2e63e036070a10b6dbe796f50300f30e848fdd5d
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-06-19 18:07:55 +0000

    enum02, boolean06-11: more test cases for booleans implemented as int or enum
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3893 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Semantics/boolean07.c
@@ -0,0 +1,18 @@
+// Check boolean transformer
+
+#include <stdbool.h>
+
+int main(void)
+{
+  bool b;
+  int i, j, k;
+
+  if(k)
+    i = j;
+  else
+    i = j-1;
+
+  b = b && i <= j;
+
+  return 0;
+}
