2a4b5b9c553b8add534203e07d72cfc36bb574ed
Mehdi Amini <mehdi.amini@hpc-project.com>
2011-06-23 11:26:35 +0200

    Compile all benchmarks in C99 mode, and accept cmdline run arguments

--- examples/Benchmarks/bench.mk
+++ examples/Benchmarks/bench.mk
@@ -10,8 +10,9 @@ NRUNS := `seq 1 3`
 # source file for the different version of the code
 SOURCE := $(TARGET).c
 OMP_SOURCE := $(TARGET).openmp.c
-CUDA_SOURCE := $(TARGET).naive.cu
-CUDA_OPT_SOURCE := $(TARGET).opt.cu
+CUDA_SOURCE := $(TARGET).naive.c
+CUDA_OPT_SOURCE := $(TARGET).opt.c
+GENERATED_KERNELS = $(wildcard p4a_new_files/*.cu)
 
 SEQ_TARGET := $(TARGET)_seq
 OMP_TARGET := $(TARGET)_openmp
@@ -24,7 +25,7 @@ CC := gcc
 COMMON_FLAGS:= -I../../common/ -DPOLYBENCH_TIME
 CFLAGS:= -O3 -fno-strict-aliasing -fPIC -std=c99  
 LDFLAGS:= -lm
-P4A_FLAGS = -p $(TARGET)_p4a -r
+P4A_FLAGS+= --c99 -p $(TARGET)_p4a -r
 
 ifdef debug
 CFLAGS+=-W -Wall -DP4A_DEBUG
@@ -47,11 +48,11 @@ $(OMP_SOURCE): $(SOURCE) $(COMMON)
 
 $(CUDA_SOURCE): $(SOURCE) $(COMMON)
 	p4a $^ $(COMMON_FLAGS)  $(P4A_FLAGS) --cuda
-	mv $(<:%.c=%.p4a.cu) $@
+	mv $(<:%.c=%.p4a.c) $@
 
 $(CUDA_OPT_SOURCE): $(SOURCE) $(COMMON)
 	p4a $^ $(COMMON_FLAGS) $(P4A_FLAGS) --cuda --com-optimization
-	mv $(<:%.c=%.p4a.cu) $@
+	mv $(<:%.c=%.p4a.c) $@
 
 
 # build binary for the 3 different versions
@@ -64,33 +65,33 @@ $(OMP_TARGET): $(OMP_SOURCE) $(COMMON)
 openmp: $(OMP_TARGET)
 
 $(CUDA_TARGET): $(CUDA_SOURCE) $(COMMON)
-	nvcc $^ -o $@ $(COMMON_FLAGS) $(LDFLAGS) $(ACCEL_FLAGS) $(ACCEL_SRC)
+	nvcc $^ -o $@ $(COMMON_FLAGS) $(LDFLAGS) $(ACCEL_FLAGS) $(ACCEL_SRC) $(GENERATED_KERNELS)
 cuda: $(CUDA_TARGET)
 $(CUDA_OPT_TARGET): $(CUDA_OPT_SOURCE) $(COMMON)
-	nvcc $^ -o $@ $(COMMON_FLAGS) $(LDFLAGS) $(ACCEL_FLAGS) $(ACCEL_SRC)
+	nvcc $^ -o $@ $(COMMON_FLAGS) $(LDFLAGS) $(ACCEL_FLAGS) $(ACCEL_SRC) $(GENERATED_KERNELS)
 cuda_opt: $(CUDA_OPT_TARGET)
 
 # Run target
 run_seq: $(SEQ_TARGET)
 	for run in $(NRUNS); do \
-		$(TOP)/scripts/record_measure.sh $(TARGET) $@ `./$< | tee -a $(TARGET)_$@.time`; \
+		$(TOP)/scripts/record_measure.sh $(TARGET) $@ `./$< $(RUN_ARGS) | tee -a $(TARGET)_$@.time`; \
 	done
 run_openmp: $(OMP_TARGET)
 	for run in $(NRUNS); do \
-		$(TOP)/scripts/record_measure.sh $(TARGET) $@ `./$< | tee -a $(TARGET)_$@.time`; \
+		$(TOP)/scripts/record_measure.sh $(TARGET) $@ `./$< $(RUN_ARGS) | tee -a $(TARGET)_$@.time`; \
 	done
 run_cuda: $(CUDA_TARGET)
 	for run in $(NRUNS); do \
-		$(TOP)/scripts/record_measure.sh $(TARGET) $@ `./$< | tee -a $(TARGET)_$@.time`; \
+		$(TOP)/scripts/record_measure.sh $(TARGET) $@ `./$< $(RUN_ARGS) | tee -a $(TARGET)_$@.time`; \
 	done
 run_cuda_opt: $(CUDA_OPT_TARGET)
 	for run in $(NRUNS); do \
-		$(TOP)/scripts/record_measure.sh $(TARGET) $@ `./$< | tee -a $(TARGET)_$@.time`; \
+		$(TOP)/scripts/record_measure.sh $(TARGET) $@ `./$< $(RUN_ARGS) | tee -a $(TARGET)_$@.time`; \
 	done
 
 # Clean targets
 clean: 
-	rm -f $(OMP_SOURCE) $(CUDA_SOURCE) $(CUDA_OPT_SOURCE) *.database *.build .*.tmp
+	rm -Rf $(OMP_SOURCE) $(CUDA_SOURCE) $(CUDA_OPT_SOURCE) *.database *.build .*.tmp p4a_new_files
 dist-clean: clean 
 	rm -f $(TARGET) $(CUDA_TARGET) $(CUDA_OPT_TARGET) $(OMP_TARGET) $(SEQ_TARGET) $(TARGET)_run_seq.time $(TARGET)_run_openmp.time $(TARGET)_run_cuda.time $(TARGET)_run_cuda_opt.time
 
