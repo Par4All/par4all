0678fa7edead985d9fc21dde0bff34d49e5e8ec7
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-06-27 08:38:27 +0000

    one new buggy test case, and 2 ok
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3939 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Reductions/reduction02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  int n = 100;
+  float a[n];
+  float tmp1 = 0.0;
+
+  for(i = 0; i < n; i++)
+    a[i] = i+100;
+
+  for(i = 0; i < n; i++) {
+    tmp1 += a[i]*10;
+    tmp1 -= a[i]*5;
+  }
+
+  printf("%f %f %f %f", tmp1);
+  for(i = 0; i < n; i++)
+    printf("%f ", a[i]);
+
+  return 0;
+}
