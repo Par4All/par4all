555a1942d51f1257bda4e89c9e26ea0243e85613
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-06-28 15:54:11 +0000

    Two kinds of fixes: desugaring of all kinds of loops, and for to do loop conversion
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@3951 67b5017d-e62d-0410-a871-f72b1c62810f

--- Control/for_loop09.c
+++ Control/for_loop09.c
@@ -7,11 +7,17 @@ int for_loop09()
   int i, j = 0;
   for(i=0;i!=5;i++) {
     if (i == 3) {
-      printf("%d",i);
+      printf("i=%d\n",i);
       return i;
     }
     j++;
   }
-  printf("Exit with %d",j);
+  printf("Exit with j=%d\n",j);
   return i;
+}
+
+main()
+{
+  for_loop09();
+  return 0;
 }
