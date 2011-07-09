d33e929d08ac09c7684923aef410b0b8a8badbbb
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-07-09 18:32:46 +0000

    Add default script for privatization : always validate localize_declarations and clean_declarations output
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4000 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/Privatization.sub/default_pyps.py
@@ -0,0 +1,7 @@
+from validation import vworkspace
+
+with vworkspace() as w:
+    w.props.memory_effects_only = False
+    w.props.prettyprint_all_private_variables = True
+    w.all_functions.validate_phases("privatize_module","localize_declaration","clean_declarations")
+
