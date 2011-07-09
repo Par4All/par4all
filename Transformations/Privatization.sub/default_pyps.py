9ab522ac06f22e07b2e16b0a410b239725424027
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-07-09 19:09:58 +0000

    Differentiate phases according to the language used
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4003 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/Privatization.sub/default_pyps.py
+++ Transformations/Privatization.sub/default_pyps.py
@@ -3,5 +3,8 @@ from validation import vworkspace
 with vworkspace() as w:
     w.props.memory_effects_only = False
     w.props.prettyprint_all_private_variables = True
-    w.all_functions.validate_phases("privatize_module","localize_declaration","clean_declarations")
+    w.all_functions.validate_phases("privatize_module");
+
+    c_modules = w.filter(lambda m: not m.compilation_unit_p() and m.language=="c")
+    c_modules.validate_phases("localize_declaration","clean_declarations")
 
