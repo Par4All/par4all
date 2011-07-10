cd8fcc05cf1261581e9f7c427b903d5b5e1ae0ba
Mehdi Amini <mehdi.amini@hpc-project.com>
2011-07-10 22:13:16 +0200

    Fix post_processor : accept triangular loop nest (bound expression can contain parentheses)

--- src/p4a_accel/p4a_post_processor.py
+++ src/p4a_accel/p4a_post_processor.py
@@ -74,7 +74,7 @@ def p4a_launcher_clean_up(match_object):
         print 'vb', variables_before, 'end'
 
     # Inside loop nest, just remove the for loops:
-    variables_in_loop_nest = re.sub("(?s)\n\\s*for\\([^\n]*", "", loop_nest)
+    variables_in_loop_nest = re.sub("(?s)\\s*for\\([^\n]*", "", loop_nest)
     # Remove also the '// To be assigned to a call to P4A_vp_...' that
     # should no be stay here:
     variables_in_loop_nest = re.sub("(?s)\\s*// To be assigned to a call to P4A_vp_[^\n]*", "", variables_in_loop_nest)
@@ -186,7 +186,7 @@ def patch_to_use_p4a_methods(file_name, dir_name, includes):
     # with a regular expression without it
     )\n{)(.*?)(?#
     # First capture any declaration at the begining up to the loop label:
-    )// Loop nest P4A begin,(\\d+?)D\\(([^)]+?)\\)(?#
+    )// Loop nest P4A begin,(\\d+?)D\\((.+?)\\)\\n(?#
     # So now we have captured kernel iteration space.
     # Next capture up to following 'Loop nest P4A end', not the last one...
     )(.*?)// Loop nest P4A end.+?(?#
