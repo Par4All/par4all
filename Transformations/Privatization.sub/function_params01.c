449e7bac3e2243365e1d52a597e82743b4a03f43
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-07-18 19:46:00 +0000

    Rename test case more accurately
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4057 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/Privatization.sub/function_params01.c
@@ -0,0 +1,23 @@
+
+void privatize_local_variable()
+{
+   int j;
+   float sum;
+   float a[10];
+   for (j=0; j<size; j++) {
+     sum+=a[10];
+   }
+}
+
+
+
+void privatize_function_params(float sum )
+{
+   int j;
+   float a[10];
+   for (j=0; j<size; j++) {
+     sum+=a[10];
+   }
+}
+
+
