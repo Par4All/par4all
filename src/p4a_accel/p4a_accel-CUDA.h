89e7a0932f7993404d3d88779fc4fec6cbffc4b4
Mehdi Amini <mehdi.amini@hpc-project.com>
2011-07-19 21:37:52 +0200

    Fix adaptative threads per block in Cuda for 1D kernel

--- src/p4a_accel/p4a_accel-CUDA.h
+++ src/p4a_accel/p4a_accel-CUDA.h
@@ -446,7 +446,7 @@ void P4A_copy_to_accel_3d(size_t element_size,
 					 block_descriptor_name,		\
 					 size)				\
   /* Define the number of thread per block: */				\
-  int tpb = P4A_min(P4A_CUDA_THREAD_PER_BLOCK_IN_1D,size/P4A_CUDA_MIN_BLOCKS); \
+  int tpb = P4A_min(P4A_CUDA_THREAD_PER_BLOCK_IN_1D,(size)/P4A_CUDA_MIN_BLOCKS); \
   tpb = tpb & ~31; /* Truncate so that we have a 32 multiple */ \
   tpb = P4A_max(tpb,32); \
   dim3 block_descriptor_name(P4A_min((int) size,			\
@@ -476,6 +476,7 @@ void P4A_copy_to_accel_3d(size_t element_size,
     /* Allocate a maximum of threads alog X axis (the warp dimension) for \
        better average efficiency: */					\
     int tpb = P4A_min(P4A_CUDA_THREAD_MAX,(n_x_iter)*(n_y_iter)/P4A_CUDA_MIN_BLOCKS); \
+    tpb = tpb & ~31; /* Truncate so that we have a 32 multiple */ \
     tpb = P4A_max(tpb,32); \
     p4a_block_x = P4A_min((int) n_x_iter,				\
                           (int) tpb);			\
