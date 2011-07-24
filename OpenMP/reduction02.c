7ec20ecb243c3c940ed4a3d7e0da268b78751ac4
villalon <villalon@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-07-24 13:55:03 +0000

    two different test cases instead of one
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4087 67b5017d-e62d-0410-a871-f72b1c62810f

--- OpenMP/reduction02.c
+++ OpenMP/reduction02.c
@@ -6,8 +6,6 @@ int main() {
   float a[n];
   float tmp1 = 0.0;
   float tmp2 = 0.0;
-   float tmp3 = 0.0;
-   float tmp4 = 0.0;
 
   for(i = 0; i < n; i++)
     a[i] = i+100;
@@ -17,18 +15,7 @@ int main() {
     tmp2 += a[i]*5;
   }
 
-  for(i = 0; i <= n/2-1; i += 1) {
-    tmp1 += a[i]*15;
-    tmp2 += a[i]*20;
-
-    tmp3 += tmp1;
-    tmp4 += tmp2;
-
-    tmp1 -= a[i]*25;
-    tmp2 -= a[i]*30;
-  }
-
-  printf("%f %f %f %f", tmp1, tmp2,tmp3, tmp4);
+  printf("%f %f", tmp1, tmp2);
   for(i = 0; i < n; i++)
     printf("%f ", a[i]);
 
