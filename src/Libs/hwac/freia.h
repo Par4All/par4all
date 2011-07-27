ae312648dc2787c56ebad74cf853004a46608fd7
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2011-07-27 18:12:57 +0000

    more macros for freia common functions
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@20035 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/hwac/freia.h
+++ src/Libs/hwac/freia.h
@@ -38,6 +38,9 @@
 
 // macros for freia stuff
 #define FREIA_IMAGE_TYPE "freia_data2d"
+#define FREIA_ALLOC "freia_common_create_data"
+#define FREIA_FREE  "freia_common_destruct_data"
+
 #define AIPO "freia_aipo_"
 #define FREIA_IMAGE FREIA_IMAGE_TYPE " * "
 #define FREIA_DEFAULT_BPP get_int_property("FREIA_PIXEL_SIZE")
