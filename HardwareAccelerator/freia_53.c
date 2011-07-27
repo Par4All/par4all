60caf3bd0715ff5b1ad42ebe88cddf53caab521f
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-07-27 08:24:59 +0000

    add bug simplified from freia_49
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4133 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ HardwareAccelerator/freia_53.c
@@ -0,0 +1,19 @@
+#include "freia.h"
+
+freia_status freia_53(freia_data2d * out, freia_data2d * in)
+{
+  freia_data2d * tb = freia_common_create_data(16, 128, 128);
+  freia_data2d * tc = freia_common_create_data(16, 128, 128);
+  freia_data2d * td = freia_common_create_data(16, 128, 128);
+
+  freia_aipo_dilate_8c(tb, in, freia_morpho_kernel_8c);
+  freia_aipo_inf_const(tc, tb, 255);
+  freia_aipo_erode_8c(tb, tb, freia_morpho_kernel_8c);
+  freia_aipo_inf(td, tc, tb);
+  freia_aipo_sub(out, in, td);
+
+  freia_common_destruct_data(tb);
+  freia_common_destruct_data(tc);
+  freia_common_destruct_data(td);
+  return FREIA_OK;
+}
