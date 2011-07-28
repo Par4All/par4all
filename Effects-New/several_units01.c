c8762e0fd65be1789fcb967d6385fd35344ae37f
creusillet <creusillet@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-07-28 07:19:28 +0000

    add a comment
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4151 67b5017d-e62d-0410-a871-f72b1c62810f

--- Effects-New/several_units01.c
+++ Effects-New/several_units01.c
@@ -1,3 +1,7 @@
+// this is to test that the interprocedural translation type checking
+// works even with functions declared in different files and using
+// a type declared in a common header file.
+
 #include "types.h"
 
 int main()
