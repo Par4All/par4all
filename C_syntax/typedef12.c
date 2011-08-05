d243ddc7f6862ff6396adb23d9877c4bfe6e862a
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-08-05 21:23:56 +0000

    bangs removed from label names
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4239 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/typedef12.c
+++ C_syntax/typedef12.c
@@ -1,15 +1,15 @@
+// ROW used twice as typdef, for a dependent type!
 
-
-void local_typedef1(int n) {
+void local_typedef12a(int n) {
   // Create alternate buffer: m rows of n.
-  typedef int ROW[n];
+  typedef int ROW[10];
   ROW x;
   x[1] = 0;
 }
 
 
-void local_typedef2(int n) {
-  typedef int ROW[n];
+void local_typedef12b(int n) {
+  typedef int ROW[10];
   ROW x;
   x[1] = 0;
 }
