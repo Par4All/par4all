5cfa452bcfb5bb9850515e7b233058e6e105b49d
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-08-08 10:01:34 +0000

    catch 1 statement alone
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4279 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia_61.c
+++ HardwareAccelerator/freia_61.c
@@ -7,7 +7,7 @@ freia_61(freia_data2d * o, const freia_data2d * i0, const freia_data2d * i1)
   freia_aipo_add(t1, i0, i1);
   freia_aipo_threshold(o, t1, 100, 200, 0);
   if (1)
-  freia_aipo_sub(o, t1, o);
+    freia_aipo_sub(o, t1, o);
 
   freia_common_destruct_data(t1);
   return FREIA_OK;
