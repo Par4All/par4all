a7ad35ad9813571249be691f11567e0a062c8175
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-08-08 11:14:18 +0000

    fix test case, which seems to work properly, issue was a typo
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4280 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia_63.c
+++ HardwareAccelerator/freia_63.c
@@ -5,7 +5,7 @@ freia_63(freia_data2d * o, const freia_data2d * i)
 {
   freia_data2d * t = freia_common_create_data(16, 128, 128);
   freia_aipo_cast(t, i); 
-  freia_aipo_mul_const(o, i, 128);
+  freia_aipo_mul_const(o, t, 128);
   freia_common_destruct_data(t);
   return FREIA_OK;
 }
