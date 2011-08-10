141ff83479a5611e651da126ab77bac272c84605
Mehdi Amini <mehdi.amini@hpc-project.com>
2011-08-10 15:57:49 +0200

    Use as much L1 cache as possible in Cuda

--- src/p4a_accel/p4a_accel-CUDA.h
+++ src/p4a_accel/p4a_accel-CUDA.h
@@ -424,6 +424,8 @@ void P4A_copy_to_accel_3d(size_t element_size,
     Generate something like "kernel<<<block_dimension,thread_dimension>>>"
 */
 #define P4A_call_accel_kernel_context(kernel, ...)	\
+  cudaFuncSetCacheConfig( kernel, cudaFuncCachePreferL1 ); \
+  toolTestExecMessage("P4A CUDA cache config failed");      \
   kernel<<<__VA_ARGS__>>>
 
 
