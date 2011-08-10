bd779823433c00fb22206ebb25315444d44c2a4e
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-08-10 16:49:25 +0000

    bug in flatten code wrt "register" qualifiers.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4311 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/Flatten_code.sub/flatten_code17.c
@@ -0,0 +1,20 @@
+void flatten_code17(void)
+{
+  int i0;
+  register int i1;
+  {
+    int i2;
+    {
+      register int i3;
+      {
+        int i4;
+      }
+    }
+  }
+}
+
+
+int main(int argc, char **argv)
+{
+  flatten_code17();
+}
