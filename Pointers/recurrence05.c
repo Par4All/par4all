673dd73ed8bfa512c9879afe1865095db95e64f0
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-08-12 08:16:02 +0000

    Pointers/recurrence05: list + malloc
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4318 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pointers/recurrence05.c
@@ -0,0 +1,17 @@
+/* Check points-to analysis on a simple list data structure
+ *
+ * Discussed 11 August 2011
+ */
+
+#include "malloc.h"
+
+typedef struct list { int * content; struct list * next;} * list;
+
+void recurrence05(void)
+{
+  int i = 0;
+  list l = (list) malloc(sizeof(list *));
+  l->content = &i;
+  l->next = NULL;
+   return;
+}
