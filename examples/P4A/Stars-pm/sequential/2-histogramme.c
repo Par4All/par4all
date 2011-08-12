3dbec04cb1372b989cee4b3b47eac8039ad739ad
Mehdi Amini <mehdi.amini@hpc-project.com>
2011-08-12 16:41:25 +0200

    Modify Stars-pm to ease parallelization

--- examples/P4A/Stars-pm/sequential/2-histogramme.c
+++ examples/P4A/Stars-pm/sequential/2-histogramme.c
@@ -5,12 +5,24 @@
 
 
 
+
 static void _histogram(int data[NP][NP][NP],
               int histo[NP][NP][NP]) {
+#ifdef P4A_CUDA_CHEAT
   int i,j,k;
+  for (i = 0; i < NP; i++) {
+    for (k = 0; k < NP; k++) {
+      for (j = 0; j < NP; j++) {
+        histo[i][j][k] = 0;
+      }
+    }
+  }
+#else
   memset(histo, 0, NPART * sizeof(int));
+#endif
 #ifndef P4A
   /* Les casts ne passent pas dans PIPS :-( */
+  int i;
   for (i = 0; i < NPART; i++) {
     ++(((int *)histo)[((int*)data)[i]]);
   }
