1062eb218572c739d39ad84f7f3a7fd63243bcd1
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-08-14 15:42:42 +0000

    Preprocessor/C-syntax_check01 to 04: a few test cases to make sure the preliminary C syntax check is not complety wrong
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4334 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Preprocessor/c_syntax_check02.c
@@ -0,0 +1,11 @@
+// Make sure the syntactic check works.
+
+// Similar to c_syntax_check01, but here, the code is correct
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World!\n");
+  return 0;
+}
