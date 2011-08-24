d14973548e6e5da859eb03179d97bd7b83169b59
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-08-24 11:45:53 +0000

    update with a simpler default_tpips
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4512 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/Freia.sub/median_2.c
+++ Transformations/Freia.sub/median_2.c
@@ -1,10 +1,6 @@
-#include <freiaDebug.h>
-#include <freiaCommon.h>
-#include <freiaAtomicOp.h>
-#include <freiaComplexOp.h>
+#include "freia.h"
 
-freia_status
-median_2(freia_data2d *o, freia_data2d *i)
+freia_status median(freia_data2d *o, freia_data2d *i)
 {
   int32_t c = 8;
   freia_status ret;
@@ -22,4 +18,18 @@ median_2(freia_data2d *o, freia_data2d *i)
   ret |= freia_aipo_sup(o, o, t);
   ret |= freia_common_destruct_data(t);
   return ret;
+}
+
+int main(void)
+{
+  freia_dataio fdin, fdout;
+  freia_data2d * out, *in;
+  in = freia_common_create_data(16, 1024, 720);
+  out = freia_common_create_data(16, 1024, 720);
+  freia_common_rx_image(in, &fdin);
+  median(out, in);
+  freia_common_tx_image(out, &fdout);
+  freia_commen_destruct_data(out);
+  freia_commen_destruct_data(in);
+  return 0;
 }
