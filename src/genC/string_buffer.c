a92b8b4a1f562afe76ee19125446a1d70ee2621e
coelho <coelho@80f2d23d-f6f1-0310-8ea7-b0e6515bd809>
2011-08-29 18:37:12 +0000

    add convenient function to embed a buffer as a C string.
    git-svn-id: http://svn.cri.ensmp.fr/svn/newgen/trunk@1288 80f2d23d-f6f1-0310-8ea7-b0e6515bd809

--- src/genC/string_buffer.c
+++ src/genC/string_buffer.c
@@ -157,6 +157,54 @@ void string_buffer_to_file(const string_buffer sb, FILE * out)
   STACK_MAP_X(s, string, fputs(s, out), sb->ins, 0);
 }
 
+#define BUFFER_SIZE 512
+
+/* put string buffer as a C-string definition of the string buffer...
+ */
+void string_buffer_append_c_string_buffer(
+  string_buffer sb, const string_buffer src, int indent)
+{
+  message_assert("distinct string buffers", sb!=src);
+  // internal buffer...
+  char buffer[512];
+  int i = 0, j;
+  buffer[i++] = '"';
+  STACK_MAP_X(s, string,
+              for (char * c = s; *c; c++)
+              {
+                if (i>BUFFER_SIZE-10-indent)
+                {
+                  buffer[i++] = '\0';
+                  string_buffer_append(sb, buffer);
+                  i = 0;
+                }
+                if (*c=='\n')
+                {
+                  buffer[i++] = '"';
+                  buffer[i++] = '\n';
+                  for (j=0; j<indent; j++)
+                    buffer[i++] = ' ';
+                  buffer[i++] = '"';
+                }
+                else if (*c=='\\')
+                {
+                  buffer[i++] = '\\';
+                  buffer[i++] = '\\';
+                }
+                else if (*c=='"')
+                {
+                  buffer[i++] = '\\';
+                  buffer[i++] = '"';
+                }
+                else
+                  buffer[i++] = *c;
+              },
+              src->ins, 0);
+  buffer[i++] = '"';
+  buffer[i++] = '\0';
+  string_buffer_append(sb, buffer);
+}
+
 /* append string s (if non empty) to string buffer sb, the duplication
  * is done if needed according to the dup field.
  */
