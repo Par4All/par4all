e4a5b92cbbd185a4163b7f5f60a849d714c94aca
creusillet <creusillet@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-08-30 15:46:04 +0000

    edn99 is no longer buggy (see ticket #560)
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4614 67b5017d-e62d-0410-a871-f72b1c62810f

--- Effects-New/edn99.py
+++ Effects-New/edn99.py
@@ -4,5 +4,5 @@ from validation import vworkspace
 with vworkspace("edn99_ppm.c",cppflags="-I.") as w:
   w.props.constant_path_effects = False
   w.all_functions.display()
-  w.all_functions.privatize_module()
+  w.all_functions.display("print_code_proper_effects")
 
