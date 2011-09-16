e7d9faf0eae8b22677ccba65b37b5ea09d449adc
coelho <coelho@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2011-09-16 21:02:04 +0000

    forgotten path fix
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@20382 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/step/stepstuff.mk
+++ src/Libs/step/stepstuff.mk
@@ -7,7 +7,7 @@ step_api.tmp: step_api.h
 STEP_name.h: STEP_name_variable.h step_api.tmp step_common.h
 	cp $(srcdir)/STEP_name_variable.h $@ && \
 	echo "/* Runtime MACRO (generated from step_common.h ) */" >> $@ && \
-	grep "^#define[ \t]*STEP_" step_common.h |grep -v STEP_COMMON_H_ | sed 's/^#define[ \t]*\([^ \t]*\).*/#define \1_NAME "\1"/g' |sort >> $@ && \
+	grep "^#define[ \t]*STEP_" $(srcdir)/step_common.h |grep -v STEP_COMMON_H_ | sed 's/^#define[ \t]*\([^ \t]*\).*/#define \1_NAME "\1"/g' |sort >> $@ && \
 	echo "/* Runtime MACRO (end) */" >> $@ && \
 	awk 'BEGIN{FS=","; printf ("/* Runtime API intrinsic name (generated from step_api.h ) */\n") >> "$@"}\
 	{\
