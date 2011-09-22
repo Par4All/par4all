2aac33daee63fd493ad34089006b9b80d3b5eed4
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-09-22 15:55:55 +0000

    typo in the PyPS source
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4714 67b5017d-e62d-0410-a871-f72b1c62810f

--- Chains/chainsRR.py
+++ Chains/chainsRR.py
@@ -1,10 +1,8 @@
-from __future__ import with_statement # this is to work with python2.5
-
 from validation import vworkspace
 import pypsex 
 
 with vworkspace() as w:
     w.props.keep_read_read_dependence=True
     w.props.print_dependence_graph_using_sru_format=True
-    print w.all_functions.dump_chains_or_dg("chains")
+    w.all_functions.dump_chains_or_dg("chains")
 
