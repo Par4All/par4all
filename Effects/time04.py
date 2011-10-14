4eb3b373966d8cdecafa18575f644267dc319c32
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-10-14 04:10:05 +0000

    Add validation for time intrinsics effects.
    Plus a validation of parallelization in presence of tic / toc.
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4772 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Effects/time04.py
@@ -0,0 +1,9 @@
+from __future__ import with_statement
+from pyps import workspace, module
+name="time04"
+with workspace(name+".c",name=name,deleteOnCLose=True,deleteOnCreate=True) as w:
+    w.fun.main.display(activate=module.print_code_proper_effects)
+    w.fun.main.coarse_grain_parallelization()
+    w.fun.main.display()
+
+
