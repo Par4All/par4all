4acb4f4e529f86ea6959236fa79c122b9ba27607
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-10-15 19:52:01 +0000

    transformer_list: fixed_bug05 added; a loop that contains an empty transition
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4780 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics-New/Maisonneuve_misc.sub/fixed_bug_05.c
+++ Semantics-New/Maisonneuve_misc.sub/fixed_bug_05.c
@@ -1,5 +1,8 @@
 // Segfaults while computing preconditions with PIPS r20506,
 // using transformer lists.
+//
+// The transition transformer was not expected to be empty as here
+// because of call to exit()
 
 // $Id$
 
@@ -9,7 +12,7 @@ void run(void) {
 lbl:
 	exit(1);
 	goto lbl;
-}	
+}
 
 int main(void) {
 	run();
