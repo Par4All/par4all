ef31f8eab0c75e7ed0dd28b2e4dde0f6e4168dd9
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-10-28 14:32:09 +0000

    I assume the issue with this test case comes from a different environment.
    So try to be more careful...
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4839 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pyps/recoverInclude1.py
+++ Pyps/recoverInclude1.py
@@ -4,4 +4,4 @@ import os
 with vworkspace() as w:
     w.all_functions.display()
     w.save()
-    os.system(" ".join([os.environ["PAGER"],os.path.join(w.tmpdirname,w.name+".c")]))
+    os.system(" ".join([os.environ.get("PAGER","cat"),os.path.join(w.tmpdirname,w.name+".c")]))
