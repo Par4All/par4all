01c6fb3e38ab6bf753bc5e41fdab5680bd4a3f8c
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-10-28 13:45:46 +0000

    Include recovery bug fixed, added test case.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4838 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pyps/recoverInclude1.py
+++ Pyps/recoverInclude1.py
@@ -1,5 +1,7 @@
 from __future__ import with_statement
 from validation import vworkspace
+import os
 with vworkspace() as w:
     w.all_functions.display()
-    print "seems ok"
+    w.save()
+    os.system(" ".join([os.environ["PAGER"],os.path.join(w.tmpdirname,w.name+".c")]))
