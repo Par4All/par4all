0d0cd91bcb6ad8a343ae484b8849c5585aa188ad
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-03 16:13:09 +0000

    Add coarse_grain_parallelization_with_reduction test case on hyantes.
    Works pretty nicely!
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4860 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/Hyantes.sub/coarse_grain_parallelization_with_reduction.py
+++ Transformations/Hyantes.sub/coarse_grain_parallelization_with_reduction.py
@@ -4,5 +4,5 @@ with workspace("hyantes.c", "options.c") as w:
     w.activate(module.must_regions)
     w.props.constant_path_effects=False
     f=w["hyantes!do_run_AMORTIZED_DISK"]
-    f.coarse_grain_parallelization_with_reduction()
+    f.flag_parallel_reduced_loops_with_openmp_directives()
     f.display()
