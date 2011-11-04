644d97f39c4a2ec3f9aec768ad37f1c8f1e0a7d7
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-04 10:31:26 +0000

    Move hyantes.sub to a better place.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4862 67b5017d-e62d-0410-a871-f72b1c62810f

--- Benchmarks/Hyantes.sub/coarse_grain_parallelization_with_reduction.py
+++ Benchmarks/Hyantes.sub/coarse_grain_parallelization_with_reduction.py
@@ -3,6 +3,5 @@ from pyps import workspace, module
 with workspace("hyantes.c", "options.c") as w:
     w.activate(module.must_regions)
     w.props.constant_path_effects=False
-    f=w["hyantes!do_run_AMORTIZED_DISK"]
-    f.flag_parallel_reduced_loops_with_openmp_directives()
-    f.display()
+    w.all_functions.flag_parallel_reduced_loops_with_openmp_directives()
+    w.all_functions.display()
