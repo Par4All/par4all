af896ab22b29865fc1a3cb93cce3de25fff32078
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-07 09:50:03 +0000

    Add a comment from FI
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4873 67b5017d-e62d-0410-a871-f72b1c62810f

--- Regions/tile01.c
+++ Regions/tile01.c
@@ -4,6 +4,8 @@ void tile01(int n, int m, int a[n][m])
    int i, j;
    int N;
    int I_0;
+   // FI : it the outer loops had the tile sizes as loop 
+   // increment it would help
    for(I_0 = 1; I_0 <= (n-1)/N+1; I_0 += 1) {
       for(i = 1; i <= MIN(n-1, (I_0-1)*N+(N-1))-(I_0-1)*N+1; i += 1)
          for(j = 1; j <= m; j += 1)
