887157cfcb4b3a2b028f34efc8113ed54469b07e
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-08 12:50:58 +0000

    Nicer validation using MA's great work.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4896 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/Linearize_loop_range.sub/default_pyps.py
+++ Transformations/Linearize_loop_range.sub/default_pyps.py
@@ -1,11 +1,11 @@
 from __future__ import with_statement # this is to work with python2.5
 from validation import vworkspace
-from pyps import module
 
 with vworkspace() as w:
-    w.all_functions.display(module.print_code_preconditions)
-    w.all_functions.linearize_loop_range()
-    w.all_functions.display(module.print_code_preconditions)
-    w.all_functions.loop_normalize(one_increment=True)
-    w.all_functions.display(module.print_code_regions)
+    w.props.loop_normalize_one_increment=True
+    w.all_functions.validate_phases("print_code_preconditions",
+            "linearize_loop_range",
+            "print_code_preconditions",
+            "loop_normalize",
+            "print_code_regions")
 
