443169213cf75e80cdac48d40b03eb678e6ae268
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-13 18:32:05 +0000

    Semantics/modulo01, 02 and 03: modifications linked to ticket 619
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4912 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics-New/modulo01.c
+++ Semantics-New/modulo01.c
@@ -1,12 +1,17 @@
 /* Bug: side effects ignored in modulo based expressions. see
    modulo_to_transformer(). */
 
-main()
+#include <stdio.h>
+
+int main()
 {
-  int i;
-  int j;
+  int i=4;
+  int j, k;
 
   j = (i++)%2;
+  k = (++i)%2;
+
+  printf("i=%d, j=%d, k=%d\n", i, j, k);
 
   return 0;
 }
