c0c70229fb22ded8fddd0836b258b86c6bbf9d5f
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-21 18:22:41 +0000

    Validate better comment and asm handling.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4973 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/asm03.c
+++ C_syntax/asm03.c
@@ -1,17 +1,8 @@
-/* gcc extension not supported by -std=c99, gcc -c does not work either on ibarron:
- *
- *francois@ibarron:~/MYPIPS/validation/C_syntax$ gcc -c asm03.c
- * asm03.c: In function ‘main’:
- * asm03.c:5:20: error: invalid register name for ‘p1’
- * asm03.c:6:20: error: invalid register name for ‘p2’
- * asm03.c:7:20: error: invalid register name for ‘result’
- */
-
+#include <stdio.h>
 int main() {
-  int t1 = 1;
-     register int *p1 asm ("r0") = &t1;
-     register int *p2 asm ("r1") = &t1;
-     register int *result asm ("r0");
-     asm ("sysint" : "=r" (result) : "0" (p1), "r" (p2));
-     return 0;
+    register int r1 asm("%eax") = 1;
+    register int r2 asm("%ecx");
+    asm("movl %eax, %ecx");
+    printf("%d\n",r1);
+    return 0;
 }
