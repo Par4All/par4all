bb6d91f3031c4e41e23b944172413d30b6d0ee04
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-22 15:40:38 +0000

    Should appear as a subdirectory of Pointers
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@4993 67b5017d-e62d-0410-a871-f72b1c62810f

--- MPEG-PointsTo/c_inc_transformer02.c
+++
@@ -1,9 +0,0 @@
-/* simplified version of c_inc_transformer01.c */
-
-
-int main(int j, char **unused) {
-  int A[1];
-
-  A[1] = j++;
-  return j;
-}
--- MPEG-PointsTo/rand01.c
+++
@@ -1,9 +0,0 @@
-// Returned value is not used.
-
-#include <stdlib.h>
-
-int rand01() {
-  // Used to make transformers aborting
-  rand();
-  return 0;
-}
