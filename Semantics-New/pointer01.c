24ea8ba9f63ca0025d82e3751ed9f8c87234da7b
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-23 17:14:39 +0000

    Semantics/pointer02: check for proprety aliasing across types; I expected an anywhere type, but effects seem wrong to me for the call to foo()
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5005 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics-New/pointer01.c
+++ Semantics-New/pointer01.c
@@ -1,15 +1,20 @@
+// Check the use and translation of anywhere effect
+
 int x;
 int y;
 int z;
+
 void foo(int *p)
 {
   (*p)++;
 }
-int main() {
-       x= 1, y=2, z=3;
-       int *p = (int *)malloc(sizeof(int));
-       foo(p);
-       p = &x;
-       foo(p);
-       return x+y+z; // no information left about x, y and z
+
+int main()
+{
+  x= 1, y=2, z=3;
+  int *p = (int *)malloc(sizeof(int));
+  foo(p);
+  p = &x;
+  foo(p);
+  return x+y+z; // no information left about x, y and z
 }
