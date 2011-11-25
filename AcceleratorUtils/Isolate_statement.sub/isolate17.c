3da5bf539985fedf9ec00ffc767dd15e4f243718
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-25 14:32:18 +0000

    Some more isolate statement validation.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5013 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ AcceleratorUtils/Isolate_statement.sub/isolate17.c
@@ -0,0 +1,7 @@
+void sain(char *t) {
+    t[2]=0;
+}
+void pain() {
+    char *t=malloc(10);
+holy:sain(t);
+}
---
+++ AcceleratorUtils/Isolate_statement.sub/isolate17.py
@@ -0,0 +1,7 @@
+from validation import vworkspace
+
+w=vworkspace()
+w.props.constant_path_effects=False
+w.props.isolate_statement_label="holy"
+w.fun.pain.validate_phases("isolate_statement")
+w.close()
