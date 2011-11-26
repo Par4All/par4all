4e8bacb7972e70cdee811a928ba201f1ab6ab08f
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-26 10:54:27 +0000

    variant not to reuse imIn
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5022 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia_79.c
+++ HardwareAccelerator/freia_79.c
@@ -7,13 +7,13 @@ void freia_79(freia_data2d *imOut, const freia_data2d * imIn)
   w1 = freia_common_create_data(imIn->bpp, imIn->widthWa, imIn->heightWa);
   w2 = freia_common_create_data(imIn->bpp, imIn->widthWa, imIn->heightWa);
 
-  freia_aipo_dilate_8c(imIn, imIn, freia_morpho_kernel_8c);
-  freia_aipo_add(w1, imIn, imIn);
+  freia_aipo_dilate_8c(w2, imIn, freia_morpho_kernel_8c);
+  freia_aipo_sub(w1, imIn, w2);
   freia_aipo_mul_const(w2, w1, 2);
   freia_aipo_copy(imOut, w2);
 
   if (true)
-    freia_aipo_inf(imOut, imOut, w2);
+    freia_aipo_add(imOut, imOut, w2);
 
   freia_common_destruct_data(w1);
   freia_common_destruct_data(w2);
