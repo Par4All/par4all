d8445eaf1c23c1eb4ffdbd2ff9265e26d5ee09f1
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-28 17:15:34 +0000

    Add a test case for "imprecision" in kernel_load_store
    The region surrounding the kernel call contain only one equality.
    But isolate_statement check only inequalities in the regions...
    
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5039 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Gpu/kernel_load_store05.py
@@ -0,0 +1,15 @@
+from validation import vworkspace
+
+
+with vworkspace("include/p4a_stubs.c") as w:
+  w.activate("MUST_REGIONS")
+  w.activate("TRANSFORMERS_INTER_FULL")
+  w.activate("INTERPROCEDURAL_SUMMARY_PRECONDITION")
+  w.activate("PRECONDITIONS_INTER_FULL")
+
+  w.fun.main.display("print_code_regions")
+  w.fun.kernel.kernel_load_store()
+
+  w.fun.main.display()
+  w.fun.kernel.display()
+
