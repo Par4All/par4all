08e303f91f52752024ab879f0d10991408f14366
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-29 14:52:09 +0000

    Accept pips output of linearize09.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5049 67b5017d-e62d-0410-a871-f72b1c62810f

--- Transformations/Linearize_array.sub/default_pyps.py
+++ Transformations/Linearize_array.sub/default_pyps.py
@@ -1,6 +1,7 @@
 from validation import vworkspace
 
 with vworkspace() as w:
-  w.all_functions.linearize_array(use_pointers=True)
-  w.all_functions.display()
+    w.props.linearize_array_use_pointers=True
+    w.all_functions.display()
+    w.all_functions.validate_phases("linearize_array")
 
