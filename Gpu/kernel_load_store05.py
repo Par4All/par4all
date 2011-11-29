273448ae2fcf1ad7ebb964959c815c20c7b7daa1
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-11-29 08:48:24 +0000

    Fix MA's bug.
    Not a real bug in fact, one must trust the array accesses to have the code work properly, which is always the case during kernel-load-store.
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5043 67b5017d-e62d-0410-a871-f72b1c62810f

--- Gpu/kernel_load_store05.py
+++ Gpu/kernel_load_store05.py
@@ -6,6 +6,7 @@ with vworkspace("include/p4a_stubs.c") as w:
   w.activate("TRANSFORMERS_INTER_FULL")
   w.activate("INTERPROCEDURAL_SUMMARY_PRECONDITION")
   w.activate("PRECONDITIONS_INTER_FULL")
+  w.props.semantics_trust_array_references = True
 
   w.fun.main.display("print_code_regions")
   w.fun.kernel.kernel_load_store()
