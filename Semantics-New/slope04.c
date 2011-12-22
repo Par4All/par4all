58de1c22b5b4632175e13dd96eab6234d2d5a3c1
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-12-22 23:01:09 +0000

    Semantics-New/linked_regions02 and 03: the redundancy elimination is not satisfactory but the results are correct
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5161 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics-New/slope04.c
+++ Semantics-New/slope04.c
@@ -12,7 +12,9 @@ void slope04()
 
   /* Define a horizontal constraint that is slightly increasing in
    * rationals.  It links point (-3, 0) to point(7,3), 10y<=3x+9. It
-   * should be simplified into y<=x, y<=5, and 3*y<=x+2.
+   * should be simplified into y<=x, y<=2, and 3*y<=x+2, using
+   * intermediates integer points (1,1) and (4,2) and extreme points
+   * (0,0) and (5, 2).
    */
   if(-3*x+10*y-9<=0)
     k = x + y;
