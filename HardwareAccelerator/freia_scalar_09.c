6671c160b28cbf72bf7af78d36fc363f6b2ade93
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-12-26 15:13:42 +0000

    new cases, updated results & one bug with i+i -> 2*i
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5170 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ HardwareAccelerator/freia_scalar_09.c
@@ -0,0 +1,42 @@
+#include "freia.h"
+
+freia_status
+freia_scalar_09(freia_data2d * o, const freia_data2d * i, uint32_t * k)
+{
+  freia_data2d
+    * X = freia_common_create_data(16, 128, 128),
+    * tB = freia_common_create_data(16, 128, 128),
+    * tB2 = freia_common_create_data(16, 128, 128),
+    * tC = freia_common_create_data(16, 128, 128),
+    * tC2 = freia_common_create_data(16, 128, 128),
+    * tD = freia_common_create_data(16, 128, 128);
+  int sA, sB, sC;
+
+  // BUG: issue with X reuse...
+  // A
+  freia_aipo_global_max(i, &sA);
+  // B
+  freia_aipo_threshold(tB, i, sA-10, sA-1, false);
+  // A
+  freia_aipo_erode_8c(X, i, k);
+  // A
+  freia_aipo_global_min(X, &sB);
+  // C with X from A
+  freia_aipo_threshold(tC, X, sB-10, sB-1, false);
+  freia_aipo_add(tC2, tC, tC);
+  // B
+  freia_aipo_dilate_8c(tB2, tB, k);
+  freia_aipo_mul(X, tB2, tB);
+  freia_aipo_global_vol(X, &sC);
+  // D with X from B
+  freia_aipo_threshold(tD, X, sC-10, sC-1, false);
+  freia_aipo_add(o, tC2, tD);
+
+  freia_common_destruct_data(X);
+  freia_common_destruct_data(tB);
+  freia_common_destruct_data(tB2);
+  freia_common_destruct_data(tC);
+  freia_common_destruct_data(tC2);
+  freia_common_destruct_data(tD);
+  return FREIA_OK;
+}
