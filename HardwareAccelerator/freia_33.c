46680ba8e5d70a66dc47483efa12aec6faf92585
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2011-12-26 14:41:12 +0000

    change op to avoid algebraic optimisation
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5168 67b5017d-e62d-0410-a871-f72b1c62810f

--- HardwareAccelerator/freia_33.c
+++ HardwareAccelerator/freia_33.c
@@ -24,10 +24,10 @@ freia_33(freia_data2d * o, freia_data2d * i, uint32_t * k)
   freia_aipo_global_min(X0, &sB);
   // C
   freia_aipo_threshold(tC, X0, sB-10, sB-1, false);
-  freia_aipo_add(tC2, tC, tC);
+  freia_aipo_mul(tC2, tC, tC);
   // B
   freia_aipo_dilate_8c(tB2, tB, k);
-  freia_aipo_mul(X1, tB2, tB);
+  freia_aipo_div(X1, tB2, tB);
   freia_aipo_global_vol(X1, &sC);
   // D
   freia_aipo_threshold(tD, X1, sC-10, sC-1, false);
