0a8fea7e605c2e76794d55c4639343721006154a
Mehdi Amini <mehdi.amini@hpc-project.com>
2012-01-18 21:05:00 +0100

    Par4All examples: Fix benchmarks compilation
    LOCAL_ was added in some makefile, but not everywhere.
    
    
    Former-commit-id: 8ed2a23e457e671860375ec7fad0f197d21ed059

--- examples/Benchmarks/main.mk
+++ examples/Benchmarks/main.mk
@@ -6,98 +6,98 @@ default:
 all: seq openmp cuda cuda_opt pgi hmpp
 
 clean: 
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target clean ; \
 	done
 
 dist-clean: 
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target dist-clean ; \
 	done
 
 
 run_seq:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target run_seq ; \
 	done
 
 seq:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 run_openmp:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target run_openmp ; \
 	done
 
 openmp:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 run_cuda:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target run_cuda ; \
 	done
 
 cuda:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 run_cuda_opt:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target run_cuda_opt ; \
 	done
 
 cuda_opt:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 openmp_src:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 cuda_src:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 cuda_opt_src:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 pgi:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 run_pgi:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 hmpp:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 run_hmpp:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
 
 ppcg:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 run_ppcg:
-	for target in $(TARGETS) ; do \
+	for target in $(LOCAL_TARGETS) ; do \
 		make -C $$target $@ ; \
 	done
 
