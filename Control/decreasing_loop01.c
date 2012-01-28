636b960e1956e056763704451794697486c71ec5
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-01-28 23:09:37 +0000

    Control: add a few more test cases to check the conversion of for loops into do loops; many more test cases would be required to test all the code in control/for_to_while_loops.c and ri-util/eval.c
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5322 67b5017d-e62d-0410-a871-f72b1c62810f

--- Control/decreasing_loop01.c
+++ Control/decreasing_loop01.c
@@ -1,7 +1,12 @@
-void decreasing(int T, int NJ) {
+// Check the conversion of for loops into do loops
+
+// Here the sign ot T is unknown: the for loop must be transformed
+// into a while loop or preserved as a for loop
+
+void decreasing_loop01(int T, int NJ) {
   int tj;
   for(tj=NJ-1; tj>=T-1; tj-=T) 
-  ;
+    ;
 
 }
 
