bb5be3dfaa27000f2cf5a20d78f6e1122cc48e0c
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-01-29 18:37:01 +0000

    Control/increasing_loop03: 6 loops with different incrementation expressions
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5325 67b5017d-e62d-0410-a871-f72b1c62810f

--- Control/increasing_loop02.c
+++ Control/increasing_loop02.c
@@ -1,7 +1,8 @@
 // Check the conversion of for loops into do loops
 
 // Here the sign ot T is known: the for loop must be transformed
-// into a do loop
+// into a do loop (difference with increasing_loop01: the
+// incrementation formulae)
 
 void increasing_loop02(unsigned int T, int NJ) {
   int tj;
