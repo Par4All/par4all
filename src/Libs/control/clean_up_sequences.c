354f43e969d4ed8a899502970ac6cc2c95739732
creusillet <creusillet@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2012-02-07 17:53:39 +0000

    add new property CLEAN_UP_SEQUENCES_WITH_DECLARATIONS and update clean_up_sequences accordingly
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@20993 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/control/clean_up_sequences.c
+++ src/Libs/control/clean_up_sequences.c
@@ -43,6 +43,8 @@
 #include "text-util.h"
 #include "properties.h"
 #include "misc.h"
+#include "pipsdbm.h"
+#include "resources.h"
 
 
 /* To store the statistics: */
@@ -86,6 +88,99 @@ display_clean_up_sequences_statistics()
     }
 }
 
+/* Clean up sequences context */
+
+typedef struct {
+  list l_ref; // references of the outermost statement
+  list l_loops; // loops of the outermost statement
+  list l_decl; // declarations of the outermost statement
+  bool merge_sequences_with_declarations;
+} cusq_context;
+
+static bool
+store_reference(reference ref, cusq_context * p_ctxt)
+{
+  p_ctxt->l_ref = CONS(REFERENCE, ref, p_ctxt->l_ref);
+  return true;
+}
+
+static bool
+store_loops(loop l, cusq_context * p_ctxt)
+{
+  p_ctxt->l_loops = CONS(LOOP, l,  p_ctxt->l_loops);
+  return true;
+}
+
+static void
+cusq_ctxt_init(statement s, cusq_context * p_ctxt)
+{
+  p_ctxt->merge_sequences_with_declarations = get_bool_property("CLEAN_UP_SEQUENCES_WITH_DECLARATIONS");
+  p_ctxt->l_ref = NIL;
+  p_ctxt->l_loops = NIL;
+  p_ctxt->l_decl = NIL;
+
+  if (p_ctxt->merge_sequences_with_declarations)
+    {
+      gen_context_multi_recurse(s, p_ctxt,
+				reference_domain, store_reference, gen_null,
+				loop_domain, store_loops, gen_null,
+				NULL);
+      p_ctxt->l_decl = statement_to_declarations(s);// gen_copy_seq(statement_declarations(s));
+    }
+
+  ifdebug(1)
+    {
+      pips_debug(1, "outermost declarations:");
+      print_entities(p_ctxt->l_decl);
+      fprintf(stderr, "\n");
+      pips_debug(1, "%s merge sequences with declarations\n", p_ctxt->merge_sequences_with_declarations? "":"do not");
+    }
+}
+
+static void
+cusq_ctxt_reset(cusq_context * p_ctxt)
+{
+  gen_free_list(p_ctxt->l_loops);
+  p_ctxt->l_loops = NIL;
+
+  gen_free_list(p_ctxt->l_ref);
+  p_ctxt->l_ref = NIL;
+
+  gen_free_list(p_ctxt->l_decl);
+  p_ctxt->l_decl = NIL;
+}
+
+/**
+ *  replace entities in the module references and loop indices and loop locals
+ *  stored in the input cusq_context using the input old/new entities mapping
+ */
+static void
+replace_entities_in_cusq_context(cusq_context * p_ctxt, hash_table ht)
+{
+  FOREACH(REFERENCE, ref, p_ctxt->l_ref)
+    {
+      entity new_ent = hash_get(ht, reference_variable(ref));
+      if (new_ent !=  HASH_UNDEFINED_VALUE)
+	reference_variable(ref) = new_ent;
+    }
+
+  FOREACH(LOOP, l, p_ctxt->l_loops)
+    {
+      entity new_index = hash_get(ht, loop_index(l));
+      if (new_index != entity_undefined)
+	loop_index(l) = new_index;
+
+      for(list l_locals = loop_locals(l); !ENDP(l_locals); POP(l_locals))
+	{
+	  entity new_local = hash_get(ht, ENTITY(CAR(l_locals)));
+	  if (new_local != entity_undefined)
+	    CAR(l_locals).p = (gen_chunkp) new_local;
+	}
+    }
+}
+
+
+/* End: clean up sequences context */
 
 static bool __attribute__ ((unused))
 clean_up_sequences_filter(statement s)
@@ -193,8 +288,47 @@ deal_with_pending_comment(statement s,
     }
 }
 
+/**
+ * change the declarations of declaration statements directly
+ * contained in input sequence according to the input old/new entity mapping
+ *
+ * @param seq is the input sequence
+ * @param renamings is an old/new entity mapping
+ *
+ */
+static void
+sequence_proper_declarations_rename_in_place(sequence seq, hash_table renamings)
+{
+  FOREACH(STATEMENT, stmt, sequence_statements(seq))
+    {
+      if (declaration_statement_p(stmt))
+	{
+	  ifdebug(4) {
+	    pips_debug(4,
+		       "current declaration statement:\n");
+	    print_statement(stmt);
+	  }
 
-void clean_up_sequences_rewrite(statement s)
+	  list l_decl = statement_declarations(stmt);
+	  while (!ENDP(l_decl))
+	    {
+	      entity decl = ENTITY(CAR(l_decl));
+	      entity new_decl = hash_get(renamings, decl);
+	      if(new_decl != HASH_UNDEFINED_VALUE) // we have found the old declaration
+		{
+		  pips_debug(5, "replacing entity %s by %s\n", entity_name(decl), entity_name(new_decl));
+		  CAR(l_decl).p = (gen_chunkp) new_decl;
+		  hash_del(renamings, decl);
+		}
+	      POP(l_decl);
+	    }
+	}
+    }
+}
+
+
+static void
+clean_up_sequences_rewrite(statement s, cusq_context * p_ctxt)
 {
   instruction i = statement_instruction(s);
   tag t = instruction_tag(i);
@@ -204,7 +338,6 @@ void clean_up_sequences_rewrite(statement s)
       /* Delete empty instruction and fuse blocks of instructions : */
       list useful_sts, delete_sts;
       string the_comments = NULL;
-      list cst = list_undefined;
 
       useful_sts = delete_sts = NIL;
 
@@ -237,16 +370,18 @@ void clean_up_sequences_rewrite(statement s)
 	print_statement(s);
       }
 
-      for(cst = sequence_statements(instruction_sequence(i)); !ENDP(cst); POP(cst)) {
-	statement st = STATEMENT(CAR(cst));
+      list l_new_decls = NIL; // newly declared entities if need be.
+      entity module = get_current_module_entity();
 
-	ifdebug(9) {
+      FOREACH(STATEMENT, st, sequence_statements(instruction_sequence(i))) {
+
+	ifdebug(8) {
 	  fprintf(stderr, "[ The current statement in the sequence: ]\n");
 	  print_statement(st);
 	}
 
 	if (empty_statement_or_labelless_continue_p(st)) {
-	  ifdebug(9)
+	  ifdebug(8)
 	    fprintf(stderr, "[ Empty statement or continue ]\n");
 
 	  if (!statement_with_empty_comment_p(st)) {
@@ -273,31 +408,137 @@ void clean_up_sequences_rewrite(statement s)
 	  clean_up_empty_block_removed++;
 	}
 	else {
-	  list dl = statement_to_direct_declarations(st);
-	  if (instruction_sequence_p(statement_instruction(st))
+	  // take care of declarations from the statements directly in the internal sequence
+	  list l_internal_proper_decls = statement_to_direct_declarations(st);
+
+	  ifdebug(4){
+	    pips_debug(4, "l_internal_proper_decls = \n");
+	    print_entities(l_internal_proper_decls);
+	    fprintf(stderr, "\n");
+	    pips_debug(4, "internal statement declarations");
+	    print_entities(statement_declarations(st));
+	    fprintf(stderr, "\n");
+	  }
+
+	  if (statement_sequence_p(st)
 	      && !empty_statement_p(st)
 	      // The next two tests should be useless
 	      // The sequence can be cleaned up even when declarations
 	      // and extensions are present
 	      // FI: the second part of this test is awful but the
 	      // controlizer seems to rely on it... See flatten_code01
-	      && (ENDP(dl) || ENDP(statement_declarations(st)))
-	      // there can be statements with only extensions
+	      && ( p_ctxt->merge_sequences_with_declarations || ENDP(l_internal_proper_decls) || ENDP(statement_declarations(st)))
 	      && ENDP(extensions_extension(statement_extensions(st)))) {
-	    /* A sequence without declarations in a sequence: they can be fused: */
-	    list statements = sequence_statements(instruction_sequence(statement_instruction(st)));
-	    statement first = STATEMENT(CAR(statements));
+
+	    /* A sequence with or without declarations in a sequence: they can be fused: */
+	    sequence internal_sequence = statement_sequence(st);
+	    list l_stmt_to_be_merged = sequence_statements(internal_sequence);
+
+	    if (p_ctxt->merge_sequences_with_declarations && !ENDP(l_internal_proper_decls))
+	      {
+		hash_table renamings = hash_table_make(hash_pointer, HASH_DEFAULT_SIZE);
+		FOREACH(ENTITY, decl, l_internal_proper_decls)
+		  {
+		    pips_debug(3, "dealing with declaration: %s\n", entity_name(decl));
+
+		    // check if there is already a variable with the same user name
+		    // used in the outermost sequence
+		    const char * decl_user_name = entity_user_name(decl);
+		    bool parent_decl_with_same_name = false;
+		    FOREACH(ENTITY, parent_decl, p_ctxt->l_decl)
+		      {
+			// arghh, this is costly, we should at least memoize user_names...
+			if (parent_decl != decl
+			    && strcmp(entity_user_name(parent_decl), decl_user_name) == 0)
+			  {
+			    parent_decl_with_same_name = true;
+			    break;
+			  }
+		      }
+
+		    // find another non-conflicting name and take care of scope
+		    string decl_name = entity_name(decl);
+		    string decl_without_module_and_scope = strrchr(decl_name, BLOCK_SEP_CHAR);
+		    string new_name = string_undefined;
+
+		    pips_debug(6, "decl name without module+scope: %s\n", decl_without_module_and_scope);
+		    if (decl_without_module_and_scope ==NULL)
+		      {
+			// this should generate an error; however, names are sometimes
+			// generated by PIPS pĥases without scope
+			pips_debug(6, "non consistant variable scope: keep original name\n");
+		      }
+		    else if (decl_without_module_and_scope[-1]=='0' && decl_without_module_and_scope[-2]== MODULE_SEP_STRING[0])
+		      {
+			pips_debug(6, "Entity already at the uppermost scope level: keep it\n");
+		      }
+		    else
+		      {
+			entity new_decl = entity_undefined;
+
+			// duplicate the scope, minus the last scope
+			string module_and_new_scope = string_undefined;
+
+			int i = 2;
+			while (decl_without_module_and_scope[-i] != BLOCK_SEP_CHAR)
+			  {
+			    if (decl_without_module_and_scope[-i] == MODULE_SEP_STRING[0])
+			       pips_internal_error("unexpected MODULE_SEP_STRING, no BLOCK_SEP_CHAR FOUND\n");
+			    i++;
+			  }
+			if (decl_without_module_and_scope[-i] == BLOCK_SEP_CHAR)
+			   module_and_new_scope = strndup(decl_name, decl_without_module_and_scope-decl_name-i+1);
+
+			pips_debug(6, "module+scope of new decl: %s\n", module_and_new_scope);
+			new_name = strdup(concatenate(module_and_new_scope, decl_user_name, NULL));
+			free(module_and_new_scope);
+
+			pips_debug(6, "new decl name: %s\n", new_name);
+
+			if (parent_decl_with_same_name)
+			  new_decl = make_entity_copy_with_new_name_and_suffix(decl, new_name, true);
+			else
+			  new_decl = make_entity_copy_with_new_name(decl, new_name, true);
+
+			pips_debug(3, "new declaration: %s\n", entity_name(new_decl));
+			hash_put(renamings, decl, new_decl); // add the correspondance in renamings table for further actual renaming
+			l_new_decls = CONS(ENTITY, new_decl, l_new_decls);
+
+			// after the merge, the new entity is used in the outermost statement:
+			p_ctxt->l_decl = CONS(ENTITY, new_decl, p_ctxt->l_decl);
+			// the old entity is not used anymore in the module,  in the current statement and in the outermost statement
+			gen_remove(&entity_declarations(module), decl);
+			gen_remove(&statement_declarations(s), decl);
+			gen_remove(&p_ctxt->l_decl, decl);
+		      }
+		  } // FOREACH(ENTITY, decl, l_internal_proper_decls)
+
+		// replace the old entity by the new one in the references of the outermost statement
+		// as well as the loop indices and loop locals
+		replace_entities_in_cusq_context(p_ctxt, renamings); // ! does not change the declarations themselves
+		// change the declarations in place with new names
+		sequence_proper_declarations_rename_in_place(internal_sequence, renamings);
+		ifdebug(4) {
+		  pips_debug(4, "stmt after declaration changes:\n");
+		  print_statement(st);
+		}
+		hash_table_free(renamings);
+	      } // if (p_ctxt->merge_sequences_with_declarations && !ENDP(l_internal_proper_decls))
+
+	    statement first = STATEMENT(CAR(l_stmt_to_be_merged));
 	    /* Unlink the innermost sequence: */
-	    sequence_statements(instruction_sequence(statement_instruction(st))) = NIL;
+	    sequence_statements(internal_sequence) = NIL;
 	    /* Keep the sequence: */
-	    useful_sts = gen_nconc(useful_sts, statements);
+	    useful_sts = gen_nconc(useful_sts, l_stmt_to_be_merged);
+
 	    pips_debug(3, "2 nested sequences fused...\n");
 	    clean_up_fused_sequences++;
 	    /* Think to delete the sequence parent: */
 	    delete_sts = CONS(STATEMENT, st, delete_sts);
 	    /* To deal with a pending comment: */
 	    st = first;
-	  }
+
+	  } // if (statement_sequence_p(st) && !empty_statement_p(st)...)
 	  else {
 	    /* By default, it should be useful: */
 	    useful_sts = gen_nconc(useful_sts,
@@ -305,13 +546,14 @@ void clean_up_sequences_rewrite(statement s)
 	    pips_debug(4, "Statement useful... %zd\n",
 		       gen_length(useful_sts));
 	  }
-	  gen_free_list(dl);
 	  deal_with_pending_comment(st, &the_comments);
+	  // free the spine of the list of internal declarations
+	  gen_free_list(l_internal_proper_decls);
 	}
-      }
+      } // FOREACH(STATEMENT, st, sequence_statements(instruction_sequence(i)))
 
       /* Remove the list of unused statements with the
-	 statemenents them-self: */
+	 statements themselves: */
       gen_full_free_list(delete_sts);
 
       if (the_comments != NULL /* || !ENDP(statement_declarations(s))*/) {
@@ -336,6 +578,16 @@ void clean_up_sequences_rewrite(statement s)
 	 statements: */
       gen_free_list(sequence_statements(instruction_sequence(i)));
       sequence_statements(instruction_sequence(i)) = useful_sts;
+
+      // for consistency: add new entities to module and current_statement declarations
+      if (p_ctxt->merge_sequences_with_declarations)
+	{
+	  FOREACH(entity, decl, l_new_decls)
+	    {
+	      pips_debug(3, "adding new declaration for %s\n", entity_name(decl));
+	      AddLocalEntityToDeclarationsOnly(decl, module, s);
+	    }
+	}
 
       if (gen_length(useful_sts) == 1
 	  && ENDP(statement_declarations(s))
@@ -384,13 +636,19 @@ clean_up_sequences_internal(statement s)
       print_statement(s);
     }
     compute_statement_to_goto_table(s);
-    gen_recurse(s, statement_domain,
-		gen_true,
-		clean_up_sequences_rewrite);
+
+    cusq_context ctxt;
+    cusq_ctxt_init(s, &ctxt);
+
+    gen_context_multi_recurse(s, &ctxt,
+			      statement_domain, gen_true, clean_up_sequences_rewrite,
+			      NULL);
+    cusq_ctxt_reset(&ctxt);
     discard_statement_to_goto_table();
     ifdebug(1) {
       pips_debug(1, "Statement at exit:\n");
       print_statement(s);
+      statement_consistent_p(s);
     }
     debug_off();
 }
@@ -402,9 +660,12 @@ clean_up_sequences_internal(statement s)
 void
 clean_up_sequences(statement s)
 {
+
   debug_on("CLEAN_UP_SEQUENCES_DEBUG_LEVEL");
   initialize_clean_up_sequences_statistics();
+
   clean_up_sequences_internal(s);
+
   display_clean_up_sequences_statistics();
   debug_off();
 }
