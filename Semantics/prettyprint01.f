d0717bcc3455ba302b593ed13413e888cf4a1ab9
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-02-08 13:00:53 +0000

    Semantics/prettyprint01: check ordering of constraints in preconditions
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5367 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Semantics/prettyprint01.f
@@ -0,0 +1,14 @@
+! Check constraint sort (bug spotted in Transformations/costi)
+
+      program prettyprint01
+
+      if(2*ns2.le.n.and.4.le.n) then
+         i = 1
+      endif
+
+      if(4.le.n.and.2*ns2.le.n) then
+         i = 2
+      endif
+
+      end
+
