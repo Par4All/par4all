1d8794aa0f48326ba67b2b5da854155b9646fda2
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2012-02-09 15:40:15 +0000

    Passes/tpips/tpips.c: cast added for -Werror
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@21024 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Passes/tpips/tpips.c
+++ src/Passes/tpips/tpips.c
@@ -1243,7 +1243,7 @@ static void parse_arguments(int argc, char * argv[])
  */
 int tpips_main(int argc, char * argv[])
 {
-  variable_debug_name=entity_local_name;
+  variable_debug_name = (char * (*)(Variable)) entity_local_name;
   debug_on("TPIPS_DEBUG_LEVEL");
   pips_log_handler = smart_log_handler;
   initialize_signal_catcher();
