4060ddeb26029dc46a2f42430b0ba79fbc8f93b2
guelton <guelton@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-02-13 10:13:45 +0000

    Update validation after outlining change.
    Mostly reorderings.
    
    
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5395 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Outlining/outline29.py
@@ -0,0 +1,18 @@
+from __future__ import with_statement
+from validation import vworkspace
+with vworkspace() as w:
+    w.fun.ing.display()
+    w.fun.ing.outline(module_name="ding",label="here",independent_compilation_unit=True, callees_prefix="moulinette")
+    [ f.display() for f in w.fun ]
+    [ f.print_code_regions() for f in w.fun ]
+#    print """
+#***************
+#Generated files
+#***************
+#"""
+#    for fname in w.save()[0]:
+#        print "".join(file(fname).readlines())
+#        print """
+#***************
+#"""
+#
