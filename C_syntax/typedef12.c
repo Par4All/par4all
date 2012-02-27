93b74c7a1ee8071ad34e35b6daf9a442ab921ecb
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-02-27 16:46:24 +0000

    C_syntax: elimination of empty test files that are not bugs
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5452 67b5017d-e62d-0410-a871-f72b1c62810f

--- C_syntax/typedef12.c
+++ C_syntax/typedef12.c
@@ -1,7 +1,9 @@
-// ROW used twice as typdef, for a dependent type!
+/* ROW used twice as typdef, for a dependent type! */
+
+/* ROW is no longer a dependent type and PIPS preprocessor still fails... */
 
 void local_typedef12a(int n) {
-  // Create alternate buffer: m rows of n.
+  /* Create alternate buffer: m rows of n. */
   typedef int ROW[10];
   ROW x;
   x[1] = 0;
