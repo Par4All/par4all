1f5ff8a48cfc3c870adc71a2a1eadd04d1b52d2b
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-03-21 09:24:03 +0000

    Add new buggy and later tests: the recursive desecent is not done correctly
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5506 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ EffectsWithPointsTo/array10.c
@@ -0,0 +1,27 @@
+/* FI looking for recursive calls */
+#include <stdio.h>
+double a[100];
+int foo(int *p) {
+  int b[100];
+  p = &b[0];
+  a[(*p)+1]= 2.;
+  return 0;
+}
+
+int bar(int *p) {
+ int b[100];
+  p = &b[0];
+  a[*p++]= 2.;
+  return 0;
+}
+
+int toto(int *p) {
+  int *q;  
+  int b[100];
+  p = &b[0];
+
+  a[*(q=p++)]= 2.;
+  return 0;
+}
+ 
+
