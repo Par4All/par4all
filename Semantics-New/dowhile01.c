cd48d65c526e85db22e40da0c14fcf39915c76cb
creusillet <creusillet@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-03-22 11:04:45 +0000

    add a buggy test case for preconditions when dealing with englobing while loops.
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5507 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Semantics-New/dowhile01.c
@@ -0,0 +1,16 @@
+int main()
+{
+  int i, a[10], b[10];
+
+  do {
+    for (i=0; i<10; i++)
+      a[i] = i;
+  } while(0);
+
+  do {
+    for (i=0; i<10; i++)
+      b[i] = a[i];
+  } while (0);
+
+  return 0;
+}
