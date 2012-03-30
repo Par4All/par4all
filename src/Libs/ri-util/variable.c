abb9d59f221edc13f30ced22b24e8e88e40ea0e4
irigoin <irigoin@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2012-03-30 21:33:21 +0000

    ri-util: predicate static_global_variable_p() added
    git-svn-id: http://svn.cri.ensmp.fr/svn/pips/trunk@21193 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/variable.c
+++ src/Libs/ri-util/variable.c
@@ -1366,6 +1366,15 @@ bool formal_parameter_p(entity v)
     return formal_p;
 }
 
+/* Is v a global variable declared local to a C file such "static int i;" */
+bool static_global_variable_p(entity v)
+{
+  // static global variables are decared in a compilation unit
+  bool static_global_variable_p = compilation_unit_p(entity_module_name(v));
+
+  return static_global_variable_p;
+}
+
 
 /* True if a variable is the pseudo-variable used to store value
    returned by a function: */
