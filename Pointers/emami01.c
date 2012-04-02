4d01edb51af1d49aceb6ae02f95971232cae0acd
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-04-02 16:14:11 +0000

    Add Emami basic  cases
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5529 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pointers/emami01.c
@@ -0,0 +1,7 @@
+/* x -> a */
+int main() {
+  int *x, a =2;
+  x = &a;
+ 
+  return 0;
+}
---
+++ Pointers/emami02.c
@@ -0,0 +1,10 @@
+/* y -> a
+   x -> a
+
+ */
+int main() {
+  int a = 2, *y = &a, *x;
+  x = y;
+ 
+  return 0;
+}
