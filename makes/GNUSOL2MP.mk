c121cd0fea9161386ee669b0e8008f95400022bc
maisonneuve <maisonneuve@0884f79e-42f2-0310-bc61-a3b21e0d3bdb>
2012-04-24 07:31:26 +0000

    Adding optional support support for GMP through new compilation targets.
    git-svn-id: http://svn.cri.ensmp.fr/svn/nlpmake/trunk@1161 0884f79e-42f2-0310-bc61-a3b21e0d3bdb

---
+++ makes/GNUSOL2MP.mk
@@ -0,0 +1,28 @@
+# $Id$
+#
+# Copyright 1989-2010 MINES ParisTech
+#
+# This file is part of PIPS.
+#
+# PIPS is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# PIPS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PIPS.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+include $(ROOT)/makes/GNUMP.mk
+
+AR	= ar
+RANLIB	= ranlib
+CFLAGS  += -msupersparc
+CANSI	=
+INSTALL	= INSTALL=cp $(ROOT)/makes/install.sh
