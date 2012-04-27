8bbe1fd5bc09e1abe5ee9e8f87dae671fe04ff9a
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-04-27 12:52:27 +0000

    Pointers/global01,02,..07: mostly an issue with the use of the empty_points-to as sign of unreachability
    git-svn-id: http://svn.cri.ensmp.fr/svn/validation/trunk@5598 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/global01.c
+++ Pointers/global01.c
@@ -8,6 +8,10 @@ int k;
 
 int global01()
 {
+  // To avoid a problem with the semantics of the empty points-to set
+  // The solution might be to add always an arc ANYWHERE->ANYWHERE
+  // when entering a module statement
+  int * p = &k;
   i = &k;
   j = i;
   return 0;
