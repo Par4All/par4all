14dcc40a4abe1fa3da48692b09d0deca144d07e5
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-05-01 22:31:24 +0000

    Pointers: new tpips scripts, updated test results using information indirectly provided by dereferencing operations
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5612 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/global08.c
+++ Pointers/global08.c
@@ -1,26 +1,19 @@
+/* Derived from global03.c to understand a bug with q->one */
+
 /* #include<stdio.h> */
 
 typedef struct two_fields{int one; int two[10];} tf_t;
 
-int i[10];
 tf_t s;
-int *pi = &i[0];
 tf_t *q = &s;
+int i;
 
-void call03()
+void global08()
 {
   // To avoid a problem with the semantics of the empty points-to set
   // The solution might be to add always an arc ANYWHERE->ANYWHERE
   // when entering a module statement
-  int * p = i;
-  *pi = 1;
-  pi++;
+  int * p = &i;
   q->one = 1;
-  q->two[4] = 2;
   return;
-}
-
-main()
-{
-  call03();
 }
