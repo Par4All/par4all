cc9e1e552b0f2a209bd85a0822c87aabbbf5f477
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-05-06 07:14:10 +0000

    Semantics-New/pointerxy: points-to modified
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5640 67b5017d-e62d-0410-a871-f72b1c62810f

--- Semantics-New/pointer02.c
+++ Semantics-New/pointer02.c
@@ -2,12 +2,15 @@
 // pointer01.c, but the anywhere effect should be typed and not impact x,
 // y and z.
 
+#include <assert.h>
+
 int x;
 int y;
 int z;
 
 void foo(double *p)
 {
+  assert(p!=0);
   (*p)++;
 }
 
