2d304361d4dfe0523e6cc7d04a7437b312f21d0a
maisonneuve <maisonneuve@8716bff9-3cf5-0310-8878-b27a67f92df8>
2012-05-09 09:00:37 +0000

    Code documentation for fixed- and multiple-precision arithmetic.
    git-svn-id: https://scm.cri.ensmp.fr/svn/linear/trunk@1529 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/arithmetique/arith_fixprec.h
+++ src/arithmetique/arith_fixprec.h
@@ -22,6 +22,18 @@
 
 */
 
+/**
+ * This file provides functions for performing fixed-precision arithmetic on
+ * integer or rational numbers.
+ *
+ * Integer numbers are stored in objects of type \c zval_t;
+ * rational numbers are stored in objects of type \c qval_t.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #ifdef LINEAR_ARITH_MULPREC_H
 #error "arith_fixprec.h and arith_mulprec.h are both loaded"
 #endif
@@ -39,56 +51,143 @@
 
 // Integers
 
+/**
+ * Type of integer numbers.
+ */
 typedef long int zval_t;
 
+/**
+ * Initialize \a z and set its value to 0.
+ */
 #define zval_init(z) ((z) = 0)
 
+/**
+ * Free the space occupied by \a z.
+ */
 #define zval_clear(z)
 
+/**
+ * Set the value of \a z1 from \a z2.
+ */
 #define zval_set(z1, z2) ((z1) = (z2))
 
+/**
+ * Set the value of \a z from the <tt>signed long</tt> \a n.
+ */
 #define zval_set_i(z, n) ((z) = (n))
 
+/**
+ * Initialize \a z1 and set its value from \a z2.
+ */
 #define zval_init_set(z1, z2) ((z1) = (z2))
 
+/**
+ * Initialize \a z and set its value from the <tt>signed long</tt> \a n.
+ */
 #define zval_init_set_i(z, n) ((z) = (n))
 
+/**
+ * Return the value of \a z as a <tt>signed long</tt>.
+ */
 #define zval_get_i(z) (z)
 
+/**
+ * Set \a z1 to \a z2 + \a z3.
+ */
 #define zval_add(z1, z2, z3) ((z1) = (z2) + (z3))
 
+/**
+ * Set \a z1 to \a z2 - \a z3.
+ */
 #define zval_sub(z1, z2, z3) ((z1) = (z2) - (z3))
 
+/**
+ * Set \a z1 to \a z2 times \a z3.
+ */
 #define zval_mul(z1, z2, z3) ((z1) = value_protected_mult(z2, z3))
 
+/**
+ * Set \a z1 to \a z2/\a z3.
+ */
 #define zval_div(z1, z2, z3) ((z1) = (z2) / (z3))
 
+/**
+ * Set \a z1 to \a z1 + \a z2 times \a z3.
+ */
 #define zval_addmul(z1, z2, z3) ((z1) += value_protected_mult(z2, z3))
 
+/**
+ * Set \a z1 to \a z1 - \a z2 times \a z3.
+ */
 #define zval_submul(z1, z2, z3) ((z1) -= value_protected_mult(z2, z3))
 
+/**
+ * Set \a z1 to \a -\a z2.
+ */
 #define zval_neg(z1, z2) ((z1) = -(z2))
 
+/**
+ * Set \a z1 to the absolute value of \a z2.
+ */
 #define zval_abs(z1, z2) ((z1) = ABS(z2))
 
+/**
+ * Set \a z1 to \a z2 \c mod \a z3.
+ */
 #define zval_mod(z1, z2, z3) ((z1) = (z2) % (z3))
 
+/**
+ * Set \a z1 to the greatest common divisor of \a z2 and \a z3.
+ * The result is always positive, irrespective of the signs of \a z2 and \a z3.
+ * Except if both inputs are zero; then it is undefined.
+ */
 #define zval_gcd(z1, z2, z3) ((z1) = pgcd(z2, z3))
 
+/**
+ * Set \a z1 to the least common multiple of \a z2 and \a z3.
+ * The result is always positive, irrespective of the signs of \a z2 and \a z3.
+ * \a z1 will be zero if either \a z2 or \a z3 is zero.
+ */
 #define zval_lcm(z1, z2, z3) ((z1) = ppcm(z2, z3))
 
+/**
+ * Compare \a z1 and \a z2.
+ * Return a positive value if \a z1 > \a z2, zero if \a z1 = \a z2, or a
+ * negative value if \a z1 < \a z2.
+ */
 #define zval_cmp(z1, z2) ((z1) - (z2))
 
+/**
+ * Compare \a z with a <tt>signed long</tt> \a n.
+ * Return a positive value if \a z > \a n, zero if \a z = \a n, or a
+ * negative value if \a z < \a n.
+ */
 #define zval_cmp_i(z, n) ((z) - (n))
 
+/**
+ * Return +1 if \a z > 0, 0 if \a z = 0, and -1 if \a z < 0.
+ */
 #define zval_sgn(z) (value_sign(z))
 
+/**
+ * Return non-zero if \a z1 and \a z2 are equal, zero if they are non-equal.
+ */
 #define zval_equal(z1, z2) ((z1) == (z2))
 
+/**
+ * Return non-zero if \a z and the <tt>unsigned long</tt> \a n are equal,
+ * zero if they are non-equal.
+ */
 #define zval_equal_i(z, n) ((z) == (n))
 
+/**
+ * Output \a z on stdio stream \a stream.
+ */
 #define zval_fprint(stream, z) (fprintf(stream, "%li", z))
 
+/**
+ * Output \a z on <tt>stdout</tt>.
+ */
 #define zval_print(z) (printf("%li", z))
 
 // Rationals
@@ -98,6 +197,9 @@ typedef struct {
 	zval_t den;
 } qval_s, *qval_p;
 
+/**
+ * Type of rational numbers.
+ */
 typedef qval_s qval_t[1];
 
 static void qval_canonicalize_unsafe(qval_p q) {
@@ -115,6 +217,10 @@ static void qval_canonicalize_unsafe(qval_p q) {
 	}
 }
 
+/**
+ * Remove any factors that are common to the numerator and denominator of \a q,
+ * and make the denominator positive.
+ */
 static void NOWUNUSED qval_canonicalize(qval_p q) {
 	if (zval_cmp_i(q->den, 0) < 0) {
 		zval_neg(q->num, q->num);
@@ -123,18 +229,30 @@ static void NOWUNUSED qval_canonicalize(qval_p q) {
 	qval_canonicalize_unsafe(q);
 }
 
+/**
+ * Initialize \a q and set its value to 0/1.
+ */
 static void NOWUNUSED qval_init(qval_p q) {
 	q->num = 0;
 	q->den = 1;
 }
 
+/**
+ * Free the space occupied by \a q.
+ */
 #define qval_clear(q)
 
+/**
+ * Set the value of \a q1 from \a q2.
+ */
 static void NOWUNUSED qval_set(qval_p q1, qval_p q2) {
 	zval_set(q1->num, q2->num);
 	zval_set(q1->den, q2->den);
 }
 
+/**
+ * Set the value of \a q to \a q2num/\a q2den.
+ */
 static void NOWUNUSED qval_set_i(qval_p q1, Value q2num, Value q2den) {
 	assert(zval_cmp_i(q2den, 0) != 0);
 	zval_set_i(q1->num, q2num);
@@ -142,6 +260,9 @@ static void NOWUNUSED qval_set_i(qval_p q1, Value q2num, Value q2den) {
 	qval_canonicalize(q1);
 }
 
+/**
+ * Set \a q1 to \a q2 + \a q3.
+ */
 static void NOWUNUSED qval_add(qval_p q1, qval_p q2, qval_p q3) {
 	zval_t q3num; zval_init(q3num); zval_set(q3num, q3->num);
 	zval_t lcm; zval_init(lcm); zval_lcm(lcm, q2->den, q3->den);
@@ -155,6 +276,9 @@ static void NOWUNUSED qval_add(qval_p q1, qval_p q2, qval_p q3) {
 	qval_canonicalize_unsafe(q1);
 }
 
+/**
+ * Set \a q1 to \a q2 - \a q3.
+ */
 static void NOWUNUSED qval_sub(qval_t q1, qval_t q2, qval_t q3) {
 	zval_t q3num; zval_init(q3num); zval_set(q3num, q3->num);
 	zval_t lcm; zval_init(lcm); zval_lcm(lcm, q2->den, q3->den);
@@ -168,12 +292,18 @@ static void NOWUNUSED qval_sub(qval_t q1, qval_t q2, qval_t q3) {
 	qval_canonicalize_unsafe(q1);
 }
 
+/**
+ * Set \a q1 to \a q2 times \a q3.
+ */
 static void NOWUNUSED qval_mul(qval_t q1, qval_t q2, qval_t q3) {
 	zval_mul(q1->num, q2->num, q3->num);
 	zval_mul(q1->den, q2->den, q3->den);
 	qval_canonicalize_unsafe(q1);
 }
 
+/**
+ * Set \a q1 to \a q2/\a q3.
+ */
 static void NOWUNUSED qval_div(qval_t q1, qval_t q2, qval_t q3) {
 	zval_t q3num; zval_init(q3num); zval_set(q3num, q3->num);
 	assert(zval_cmp_i(q3num, 0) != 0);
@@ -183,16 +313,25 @@ static void NOWUNUSED qval_div(qval_t q1, qval_t q2, qval_t q3) {
 	qval_canonicalize(q1);
 }
 
+/**
+ * Set \a q1 to \a -\a q2.
+ */
 static void NOWUNUSED qval_neg(qval_t q1, qval_t q2) {
 	zval_neg(q1->num, q2->num);
 	zval_set(q1->den, q2->den);
 }
 
+/**
+ * Set \a q1 to the absolute value of \a q2.
+ */
 static void NOWUNUSED qval_abs(qval_t q1, qval_t q2) {
 	zval_abs(q1->num, q2->num);
 	zval_set(q1->den, q2->den);
 }
 
+/**
+ * Set \a q1 to 1/\a q2.
+ */
 static void NOWUNUSED qval_inv(qval_t q1, qval_t q2) {
 	zval_t q2num; zval_init(q2num); zval_set(q2num, q2->num);
 	assert(zval_cmp_i(q2num, 0) != 0);
@@ -202,6 +341,13 @@ static void NOWUNUSED qval_inv(qval_t q1, qval_t q2) {
 	qval_canonicalize(q1);
 }
 
+/**
+ * Compare \a q1 and \a q2.
+ * Return a positive value if \a q1 > \a q2, qero if \a q1 = \a q2, or a
+ * negative value if \a q1 < \a q2.
+ * To determine if two rationals are equal, \c qval_equal is faster than
+ * \c qval_cmp.
+ */
 static int NOWUNUSED qval_cmp(qval_t q1, qval_t q2) {
 	zval_t lcm; zval_init(lcm); zval_lcm(lcm, q1->den, q2->den);
 	zval_t z1; zval_init(z1);
@@ -216,6 +362,12 @@ static int NOWUNUSED qval_cmp(qval_t q1, qval_t q2) {
 	return res;
 }
 
+/**
+ * Compare \a q1 and \a q2num/\a q2den.
+ * Return a positive value if \a q1 > \a q2num/\a q2den,
+ * zero if \a q1 = \a q2num/\a q2den,
+ * or a negative value if \a q1 < \a q2num/\a q2den.
+ */
 static int NOWUNUSED qval_cmp_i(qval_t q1, Value q2num, Value q2den) {
 	zval_t lcm; zval_init(lcm); zval_lcm(lcm, q1->den, q2den);
 	zval_t z1; zval_init(z1);
@@ -230,16 +382,31 @@ static int NOWUNUSED qval_cmp_i(qval_t q1, Value q2num, Value q2den) {
 	return res;
 }
 
+/**
+ * Return +1 if \a q > 0, 0 if \a q = 0, and -1 if \a q < 0.
+ */
 static int NOWUNUSED qval_sgn(qval_t q) {
 	return zval_sgn(q->num);
 }
 
+/**
+ * Return non-zero if \a q1 and \a q2 are equal, zero if they are non-equal.
+ * Although \c qval_cmp can be used for the same purpose, this function is
+ * faster.
+ */
 static int NOWUNUSED qval_equal(qval_t q1, qval_t q2) {
 	return zval_cmp(q1->den, q2->den) == 0 && zval_cmp(q1->num, q2->num) == 0;
 }
 
+/**
+ * Return non-zero if \a q and \a q2num/\a q2den are equal,
+ * zero if they are non-equal.
+ */
 #define qval_equal_i(q1, q2num, q2den) (qval_cmp_i(q1, q2num, q2den) == 0)
 
+/**
+ * Output \a q on stdio stream \a stream.
+ */
 static int NOWUNUSED qval_fprint(FILE* stream, qval_t q) {
 	int c;
 	c = zval_fprint(stream, q->num);
@@ -250,6 +417,9 @@ static int NOWUNUSED qval_fprint(FILE* stream, qval_t q) {
 	return c;
 }
 
+/**
+ * Output \a q on <tt>stdout</tt>.
+ */
 #define qval_print(q) (qval_fprint(stdout, q))
 
 #endif
--- src/arithmetique/arith_mulprec.h
+++ src/arithmetique/arith_mulprec.h
@@ -21,6 +21,15 @@
   along with PIPS.  If not, see <http://www.gnu.org/licenses/>.
 
 */
+
+/**
+ * This file provides functions for performing multiple-precision arithmetic on
+ * integer or rational numbers.
+ *
+ * Integer numbers are stored in objects of type \c zval_t;
+ * rational numbers are stored in objects of type \c qval_t.
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -48,56 +57,143 @@
 
 // Integers
 
+/**
+ * Type of integer numbers.
+ */
 typedef mpz_t zval_t;
 
-#define zval_init mpz_init
+/**
+ * Initialize \a z and set its value to 0.
+ */
+#define zval_init(z) (mpz_init(z))
 
-#define zval_clear mpz_clear
+/**
+ * Free the space occupied by \a z.
+ */
+#define zval_clear(z) (mpz_clear(z))
 
-#define zval_set mpz_set
+/**
+ * Set the value of \a z1 from \a z2.
+ */
+#define zval_set(z1, z2) (mpz_set(z1, z2))
 
-#define zval_set_i mpz_set_si
+/**
+ * Set the value of \a z from the <tt>signed long</tt> \a n.
+ */
+#define zval_set_i(z, n) (mpz_set_si(z, n))
 
-#define zval_init_set mpz_init_set
+/**
+ * Initialize \a z1 and set its value from \a z2.
+ */
+#define zval_init_set(z1, z2) (mpz_init_set(z1, z2))
 
-#define zval_init_set_i mpz_init_set_si
+/**
+ * Initialize \a z and set its value from the <tt>signed long</tt> \a n.
+ */
+#define zval_init_set_i(z, n) (mpz_init_set_si(z, n))
 
-#define zval_get_i mpz_get_si
+/**
+ * Return the value of \a z as a <tt>signed long</tt>.
+ */
+#define zval_get_i(z) (mpz_get_si(z))
 
-#define zval_add mpz_add
+/**
+ * Set \a z1 to \a z2 + \a z3.
+ */
+#define zval_add(z1, z2, z3) (mpz_add(z1, z2, z3))
 
-#define zval_sub mpz_sub
+/**
+ * Set \a z1 to \a z2 - \a z3.
+ */
+#define zval_sub(z1, z2, z3) (mpz_sub(z1, z2, z3))
 
-#define zval_mul mpz_mul
+/**
+ * Set \a z1 to \a z2 times \a z3.
+ */
+#define zval_mul(z1, z2, z3) (mpz_mul(z1, z2, z3))
 
-#define zval_div mpz_fdiv_q
+/**
+ * Set \a z1 to \a z2/\a z3.
+ */
+#define zval_div(z1, z2, z3) (mpz_fdiv_q(z1, z2, z3))
 
-#define zval_addmul mpz_addmul
+/**
+ * Set \a z1 to \a z1 + \a z2 times \a z3.
+ */
+#define zval_addmul(z1, z2, z3) (mpz_addmul(z1, z2, z3))
 
-#define zval_submul mpz_submul
+/**
+ * Set \a z1 to \a z1 - \a z2 times \a z3.
+ */
+#define zval_submul(z1, z2, z3) (mpz_submul(z1, z2, z3))
 
-#define zval_neg mpz_neg
+/**
+ * Set \a z1 to \a -\a z2.
+ */
+#define zval_neg(z1, z2) (mpz_neg(z1, z2))
 
-#define zval_abs mpz_abs
+/**
+ * Set \a z1 to the absolute value of \a z2.
+ */
+#define zval_abs(z1, z2) (mpz_abs(z1, z2))
 
-#define zval_mod mpz_mod
+/**
+ * Set \a z1 to \a z2 \c mod \a z3.
+ */
+#define zval_mod(z1, z2, z3) (mpz_mod(z1, z2, z3))
 
-#define zval_gcd mpz_gcd
+/**
+ * Set \a z1 to the greatest common divisor of \a z2 and \a z3.
+ * The result is always positive, irrespective of the signs of \a z2 and \a z3.
+ * Except if both inputs are zero; then it is undefined.
+ */
+#define zval_gcd(z1, z2, z3) (mpz_gcd(z1, z2, z3))
 
-#define zval_lcm mpz_lcm
+/**
+ * Set \a z1 to the least common multiple of \a z2 and \a z3.
+ * The result is always positive, irrespective of the signs of \a z2 and \a z3.
+ * \a z1 will be zero if either \a z2 or \a z3 is zero.
+ */
+#define zval_lcm(z1, z2, z3) (mpz_lcm(z1, z2, z3))
 
-#define zval_cmp mpz_cmp
+/**
+ * Compare \a z1 and \a z2.
+ * Return a positive value if \a z1 > \a z2, zero if \a z1 = \a z2, or a
+ * negative value if \a z1 < \a z2.
+ */
+#define zval_cmp(z1, z2) (mpz_cmp(z1, z2))
 
-#define zval_cmp_i mpz_cmp_si
+/**
+ * Compare \a z with a <tt>signed long</tt> \a n.
+ * Return a positive value if \a z > \a n, zero if \a z = \a n, or a
+ * negative value if \a z < \a n.
+ */
+#define zval_cmp_i(z, n) (mpz_cmp_si(z, n))
 
-#define zval_sgn mpz_sgn
+/**
+ * Return +1 if \a z > 0, 0 if \a z = 0, and -1 if \a z < 0.
+ */
+#define zval_sgn(z) (mpz_sgn(z))
 
+/**
+ * Return non-zero if \a z1 and \a z2 are equal, zero if they are non-equal.
+ */
 #define zval_equal(z1, z2) (zval_cmp(z1, z2) == 0)
 
-#define zval_equal_i(z, v) (zval_cmp_i(z, v) == 0)
+/**
+ * Return non-zero if \a z and the <tt>unsigned long</tt> \a n are equal,
+ * zero if they are non-equal.
+ */
+#define zval_equal_i(z, n) (zval_cmp_i(z, n) == 0)
 
+/**
+ * Output \a z on stdio stream \a stream.
+ */
 #define zval_fprint(stream, z) (mpz_out_str(stream, 10, z))
 
+/**
+ * Output \a z on <tt>stdout</tt>.
+ */
 #define zval_print(z) (mpz_out_str(stdout, 10, z))
 
 // Rationals
@@ -106,44 +202,115 @@ typedef __mpq_struct qval_s;
 
 typedef mpq_ptr qval_p;
 
+/**
+ * Type of rational numbers.
+ */
 typedef qval_s qval_t[1];
 
-#define qval_canonicalize mpq_canonicalize
+/**
+ * Remove any factors that are common to the numerator and denominator of \a q,
+ * and make the denominator positive.
+ */
+#define qval_canonicalize(q) (mpq_canonicalize(q))
 
-#define qval_init mpq_init
+/**
+ * Initialize \a q and set its value to 0/1.
+ */
+#define qval_init(q) (mpq_init(q))
 
-#define qval_clear mpq_clear
+/**
+ * Free the space occupied by \a q.
+ */
+#define qval_clear(q) (mpq_clear(q))
 
-#define qval_set mpq_set
+/**
+ * Set the value of \a q1 from \a q2.
+ */
+#define qval_set(q1, q2) (mpq_set(q1, q2))
 
-#define qval_set_i mpq_set_si
+/**
+ * Set the value of \a q to \a q2num/\a q2den.
+ */
+#define qval_set_i(q1, q2num, q2den) (mpq_set_si(q1, q2num, q2den))
 
-#define qval_add mpq_add
+/**
+ * Set \a q1 to \a q2 + \a q3.
+ */
+#define qval_add(q1, q2, q3) (mpq_add(q1, q2, q3))
 
-#define qval_sub mpq_sub
+/**
+ * Set \a q1 to \a q2 - \a q3.
+ */
+#define qval_sub(q1, q2, q3) (mpq_sub(q1, q2, q3)(
 
-#define qval_mul mpq_mul
+/**
+ * Set \a q1 to \a q2 times \a q3.
+ */
+#define qval_mul(q1, q2, q3) (mpq_mul(q1, q2, q3))
 
-#define qval_div mpq_div
+/**
+ * Set \a q1 to \a q2/\a q3.
+ */
+#define qval_div(q1, q2, q3) (mpq_div(q1, q2, q3))
 
-#define qval_neg mpq_neg
+/**
+ * Set \a q1 to \a -\a q2.
+ */
+#define qval_neg(q1, q2) (mpq_neg(q1, q2))
 
-#define qval_abs mpq_abs
+/**
+ * Set \a q1 to the absolute value of \a q2.
+ */
+#define qval_abs(q1, q2) (mpq_abs(q1, q2))
 
-#define qval_inv mpq_inv
+/**
+ * Set \a q1 to 1/\a q2.
+ */
+#define qval_inv(q1, q2) (mpq_inv(q1, q2))
 
-#define qval_cmp mpq_cmp
+/**
+ * Compare \a q1 and \a q2.
+ * Return a positive value if \a q1 > \a q2, qero if \a q1 = \a q2, or a
+ * negative value if \a q1 < \a q2.
+ * To determine if two rationals are equal, \c qval_equal is faster than
+ * \c qval_cmp.
+ */
+#define qval_cmp(q1, q2) (mpq_cmp(q1, q2))
 
-#define qval_cmp_i mpq_cmp_si
+/**
+ * Compare \a q1 and \a q2num/\a q2den.
+ * Return a positive value if \a q1 > \a q2num/\a q2den,
+ * zero if \a q1 = \a q2num/\a q2den,
+ * or a negative value if \a q1 < \a q2num/\a q2den.
+ */
+#define qval_cmp_i(q1, q2num, q2den) (mpq_cmp_si(q1, q2num, q2den))
 
-#define qval_sgn mpq_sgn
+/**
+ * Return +1 if \a q > 0, 0 if \a q = 0, and -1 if \a q < 0.
+ */
+#define qval_sgn(q) (mpq_sgn(q))
 
-#define qval_equal mpq_equal
+/**
+ * Return non-zero if \a q1 and \a q2 are equal, zero if they are non-equal.
+ * Although \c qval_cmp can be used for the same purpose, this function is
+ * faster.
+ */
+#define qval_equal(q1, q2) (mpq_equal(q1, q2))
 
+/**
+ * Return non-zero if \a q and \a q2num/\a q2den are equal,
+ * zero if they are non-equal.
+ */
 #define qval_equal_i(q1, q2num, q2den) (qval_cmp_i(q1, q2num, q2den) == 0)
 
+/**
+ * Output \a q on stdio stream \a stream.
+ */
 #define qval_fprint(stream, q) (mpq_out_str(stream, 10, q))
 
+/**
+ * Output \a q on <tt>stdout</tt>.
+ */
 #define qval_print(q) (mpq_out_str(stdout, 10, q))
 
 #endif
