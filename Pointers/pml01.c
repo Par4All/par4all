71085a1ad4f1c042f8f870914b5ce0a70c4b7ffd
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-05-14 16:35:09 +0000

    Pointers/pml03: correct test case for pml01.c; comments added in pml01.c to explain the bug; the bug is not detected by gcc -Wall
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5656 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/pml01.c
+++ Pointers/pml01.c
@@ -1,3 +1,10 @@
+/* Bug: the assignments to root and root->next are buggy: lists are
+ * expected but pointers to lists are returned.
+ *
+ * Please do not fix this code as the type mismatch should be
+ * detected. A correct version is available as pml03.c.
+ */
+
 #include<stdlib.h>
 typedef struct {
   struct list* next;
---
+++ Pointers/pml03.c
@@ -0,0 +1,16 @@
+/* Correct version of pml01.c */
+
+#include<stdlib.h>
+
+typedef struct {
+  struct list* next;
+} list;
+
+int main()
+{
+  list *root;
+  root = malloc(sizeof(list));
+  root->next = malloc(sizeof(list));
+  free(root);
+  return 0;
+}
