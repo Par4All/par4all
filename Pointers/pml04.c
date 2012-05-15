517f229249d280010db6e4062f4e037b6dc4a005
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-05-15 12:37:39 +0000

    Pointers/plm04: two variables added to show the bug in the equations or the implementation of the modle of free(); Amira is in charge
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5660 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/pml04.c
+++ Pointers/pml04.c
@@ -1,9 +1,14 @@
+/* Bug: free should not work when a numerical non-zero offset appears*/
+
 #include<stdlib.h>
+
 int main()
 {
-  int *p;
+  int *p, *q, *r;
   p = malloc(10*sizeof(int));
   p++;
+  q = p-1;
+  r = p;
   free(p);
   return 0;
 }
