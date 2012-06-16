9d253e0c54cd1b080c737903cbe4b27e0b7521e1
amini <amini@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-06-16 14:55:34 +0000

    Add buggy linearize_array when the array is used in an entity_initial field
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5704 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Transformations/Linearize_array.sub/linearize_in_declarations01.c
@@ -0,0 +1,12 @@
+void p4a_kernel_lud_99(int size, int j, float (*a)[size][size], int i)
+{
+   //PIPS generated variable
+   int k;
+   // Loop nest P4A end
+   if (j+i<=size-1) {
+      float sum = (*a)[i][j+i];
+      sum = (*a)[i][j+i];
+   }
+   ;
+}
+
