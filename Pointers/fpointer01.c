ced10ce10d6181a419c7006b8a44b2fcf5270340
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-06-27 09:55:05 +0000

    Pointers: updates of test files due to C source updates
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5708 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/fpointer01.c
+++ Pointers/fpointer01.c
@@ -1,5 +1,7 @@
 // Check pointers to functions
 
+// FI: this is not supported by PIPS infrastructure
+
 void fpointer01(char         c,
 		void       * _stream,
 		void      (* my_fputc)(const char c,void * _stream),
