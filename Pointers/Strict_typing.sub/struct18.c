6f854fbe38ef1e35076e51be3a96b71e8e1663ac
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-08-17 15:48:44 +0000

    Pointers/struct18: a simplified version of struct08; two kinds of things are wrong: the stubs in the heap do not seem to be typed like usual stubs; somehow their types should evolve with the control context; also non-constant path are generated such as p[*]; this is not detected by the points-to graph consistency check
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5745 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pointers/Strict_typing.sub/struct18.c
@@ -0,0 +1,43 @@
+/* FI: excerpt of struct08, easier to debug */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 5
+
+typedef struct {
+  int num;
+  int tab1[N];
+  int *tab2;
+} mys;
+
+void init3(mys **p)
+{
+  int i;
+  p = malloc(N*sizeof(mys *));
+  for (i=0; i<N; i++) 
+    {
+      p[i] = malloc(sizeof(mys));
+      p[i]->tab2 = malloc(N*sizeof(int));
+    }
+  
+  p[0]->num=11;
+  p[0]->tab1[0]=20;
+  p[0]->tab2[0]=30;
+  
+  for (i=1; i<N; i++) 
+    {
+      p[i]->num = 10;
+      p[i]->tab1[0] = 21;
+      p[i]->tab2[0]= p[i]->tab1[0];
+    }  
+}
+
+int main() 
+{
+  mys *p, *q, **r, *s[N];
+
+  init3(r);
+
+  return 1;
+}
