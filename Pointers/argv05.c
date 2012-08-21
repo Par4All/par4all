0531a78c3aa6655477cb02fd24e72cf1798719d7
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-08-21 16:13:59 +0000

    Pointers: a few more updates of results
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5755 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/argv05.c
+++ Pointers/argv05.c
@@ -6,7 +6,10 @@ int main(int argc, char * argv[])
 {
   int i = 0;
   char * s0 = *argv;
-  for(i=0;i<argc;i++, argv++)
-    printf("%s\n", *argv);
+  for(i=0;i<argc;i++) {
+    printf("%s\n", s0);
+    argv++;
+    s0 = *argv; // to obtain the result of argv++
+  }
   return 0;
 }
