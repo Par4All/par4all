3f9973d5c313a9ddec5c74cd6a691dc4a9fb32e1
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-08-21 13:26:08 +0000

    Pointers: lots of .bug removed, results updated; 385 test cases, 1 core dump, 88 differences and still 12 known bugs, mostly subscripts and loop convergence
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5753 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/formal_parameter03.c
+++ Pointers/formal_parameter03.c
@@ -2,13 +2,9 @@
 
 int formal_parameter03(int ppp[10][10][10])
 {
-	int *q = ppp[0][0];
-	// Type mismatch
-	// int **qq = ppp[0];
-	int (*qq)[10][10] = ppp[0];
-	// int qq[10][10] = ppp[0];
-	// Type mismatch
-	// int ***qqq = ppp;
-	int (*qqq)[10][10][10] = ppp;
-	return 0;
+  int *q = ppp[0][0];
+  int (*qq)[10] = ppp[0];
+  int (*qqq)[10][10] = ppp;
+
+  return 0;
 }
