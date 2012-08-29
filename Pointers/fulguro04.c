e5aa78beb81e5e7def382a1ef0fdd3f6ed3b9ff5
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-08-29 09:32:31 +0000

    Pointers: Mensi.sub is used for all examples used in her dissertation; Interprocedural.sub is used only for interprocedural test cases (some redundance may occur with Mensi.sub); test case struct21 added to debug the analysis of the subscript construct
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5783 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/fulguro04.c
+++ Pointers/fulguro04.c
@@ -6,13 +6,7 @@ typedef struct {
     void **array;
 }FLGR_Data2D;
 
-typedef struct {
-    int spp; 
-    int bps; 
-    void *array;
-} FLGR_Vector;
-
-void flgr2d_set_data_vector_fgUINT16(FLGR_Data2D *dat, int row, int col, FLGR_Vector *vct)
+void flgr2d_set_data_vector_fgUINT16(FLGR_Data2D *dat, int row)
 {
    fgUINT16 *array_d = (fgUINT16 *) (dat->array)[row];
    return;
