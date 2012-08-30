c4c077ade4857c777494b5521f133405c6278ac4
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-08-30 16:48:02 +0000

    Pointers: a few bugs removed, a new bug added
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5793 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pointers/list07.c
@@ -0,0 +1,59 @@
+/* Derived from list00, with code expansion and no list_free()*/
+
+#include <stdlib.h>
+
+struct cons_t;
+typedef struct cons_t * list;
+
+// empty list
+const list nil = ((list) 0);
+
+struct cons_t {
+  double value;
+  list next;
+};
+
+// alloc/free
+list list_cons(double v, list l)
+{
+  list n = (list) malloc(sizeof(struct cons_t));
+  n->value = v;
+  n->next = l;
+  return n;
+}
+
+list list_next(list l)
+{
+  list n;
+  if (l==nil)
+    n = nil;
+  else
+    n = l->next;
+  return n;
+}
+
+// observer
+int list_len(list l)
+{
+  int n = 0;
+  list c = l;
+  while (c!=nil)
+  {
+    // we know that c!=nil...
+    c = (c==nil)? nil: c->next;
+    n++;
+  }
+  return n;
+}
+
+int main(void)
+{
+  list l;
+  l = nil;
+  l = list_cons(1.0, nil);
+  l = list_cons(2.0, l);
+  l = list_cons(3.0, l);
+  int n = list_len(l);
+  //list_free(l);
+  return n; // to please gcc
+}
