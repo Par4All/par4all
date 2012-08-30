ec017f8d7ec1c828336aeb7207d0ec341b32e901
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-08-30 15:50:59 +0000

    Pointers: argv06 used again; Strict_typing.sub/malloc03 and 06
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5792 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/Strict_typing.sub/malloc03.c
+++ Pointers/Strict_typing.sub/malloc03.c
@@ -1,5 +1,11 @@
-/* Check type sensitivity. Extension of malloc02.c */
+/* Check type sensitivity. Extension of malloc02.c
+ *
+ * FI: I changed the initialization and the type of pt
+ *
+ * The initialization was wrong: the allocated 
+ */
 
+#include <stdio.h>
 #include <malloc.h>
 
 int main(int argc, char *argv[])
@@ -13,8 +19,14 @@ int main(int argc, char *argv[])
   px = (float *) malloc(sizeof(float));
   pz = (float *) malloc(sizeof(float));
   pt = (int (*)[30]) malloc(sizeof(*pt));
+  // FI: a correct memory allocation to support pt++
+  //pt = (int (*)[30]) malloc(10*sizeof(*pt));
+
+  printf("%p\n", pt);
 
   pt++;
+
+  printf("%p\n", pt);
 
   return 0;
 }
