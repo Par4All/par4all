9c8be051de8d9ab83715a8fb886db5d17b39716c
coelho <coelho@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-09-01 06:00:32 +0000

    add some more help comments
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5801 67b5017d-e62d-0410-a871-f72b1c62810f

--- validate.mk
+++ validate.mk
@@ -17,12 +17,16 @@
 # - DO_PYPS: pyps must be available
 # - DO_F95: gfc2pips must be available
 # - D.sub: subdirectories in which to possibly recurse, defaults to *.sub
+# - OK: actions taken after validation, set to empty to keep err file
 #
 # example to do only later cases:
 #   sh> make DO_DEFAULT= DO_SLOW= DO_LATER=1 validate-test
 # special useful targets include:
 #   sh> make later-validate-test
 #   sh> make bug-validate-out
+# validate one test case:
+#   sh> make test-case.validate
+#
 
 # what special cases are included
 DO_BUG	=
