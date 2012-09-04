90f36d4afc061e8fea4c39aef1122e7e3f92480f
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-09-04 13:30:45 +0000

    Pointers: a few updates because the fix point algorithm used has been changed; also new issues detected with the C conditional operator; the fix point algorithm is the initial fix point algorithm, not the iterative one, and a normalization operator has been added to enforce convergence; however, too much information seems to be lost: this appears with Strict_typing.sub/linked_list01 and 02
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5807 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/call06.c
+++ Pointers/call06.c
@@ -10,12 +10,12 @@ void call06(int r, int m, int *x)
   return;
 }
 
-main()
+int main()
 {
   int d1 = 4;
   int d2 = 4;
   int y[d1][d2];
-  int i, j;
+  int i;
 
   for(i=0;i<d1;i++)
     call06(i, d2, &(y[i][0]));
