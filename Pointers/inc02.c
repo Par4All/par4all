262e85bb2bb536c28d5c29831f4362b7416731f2
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-09-14 17:00:14 +0000

    Pointers: updates due to some handling of casts and to arithmetic error dectection
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5823 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/inc02.c
+++ Pointers/inc02.c
@@ -5,13 +5,13 @@
 
 int main()
 {
-  int i = 0;
-  int * q = &i;
-  int * p = &i;
-  // Increment i
+  int i[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int * q = &i[0];
+  int * p = &i[0];
+  // Increment i[0]
   (*q)++;
   // increment q
-  *q++; // The loaded value is not used
-  printf("i=%d, p=%p, q=%p\n", i, p, q);
-  return 0;
+  int j = *q++; // The loaded value is not used
+  printf("i=%d, p=%p, q=%p\n", i[0], p, q);
+  return j;
 }
--- Pointers/pointer15.c
+++ Pointers/pointer15.c
@@ -1,9 +1,24 @@
 // array of pointers towards arrays
 
+#include <stdio.h>
+
 void pointer15(double * (*t[3][4])[5][6][7])
 {
   double z;
-  (*(t[0][0]))[1][2][3] = &z;
-  *(*(t[0][0]))[1][2][3] = 2.5;
+  (*(t[1][2]))[3][4][5] = &z;
+  *(*(t[1][2]))[3][4][5] = 2.5;
   return;
+}
+
+int main()
+{
+  double * d[3][4][5][6][7];
+  double * (*pd[3][4])[5][6][7];
+  int i, j;
+  for(i=0;i<3;i++)
+    for(j=0;j<4;j++)
+      pd[i][j] = &d[i][j];
+  pointer15(pd);
+  printf("%f\n", *d[1][2][3][4][5]);
+  return 0;
 }
