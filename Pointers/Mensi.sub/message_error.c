46791941cc7f6f8910ac95891e2fc68ebda65e69
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-11-19 16:35:53 +0000

    Adding a new example to the inerprocedural chapter
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5885 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ Pointers/Mensi.sub/message_error.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void msg_error(char* msg)
+{
+  printf("%s", msg);
+  exit(1);
+}
+
+int main()
+{
+  char *msg1 = "hello";
+  char *msg2 = msg1 ;
+  msg_error( msg2);
+
+  return 0;
+}
