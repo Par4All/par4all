3557dc6bed0efef1adab961e5fa8edddf22605ca
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2012-11-29 14:08:51 +0000

    Pointers: updates according linked to the last source update
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5910 67b5017d-e62d-0410-a871-f72b1c62810f

---
+++ EffectsWithPointsTo/struct13.c
@@ -0,0 +1,39 @@
+/* Excerpt from struct12.c, only init() is preserved
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 5
+
+typedef struct {
+  int num;
+  int tab1[N];
+  int *tab2;
+} mys;
+
+void init(mys *m) 
+{
+  int i;
+  m->num = N;
+  m->tab2 = malloc(N*sizeof(int));
+  m->tab1[0] = 10;
+  m->tab2[0] = 20;
+  for (i=0; i<N; i++)
+    { 
+      m->tab1[i] = 1;
+      m->tab2[i] = m->tab1[i];
+    }
+  
+}
+
+int main() 
+{
+  mys s1;
+  mys *p=&s1;
+
+  init(p);
+
+  return 1;
+}
