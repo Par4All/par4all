eaa4cb29c4e8110b522160af852da800b7d722ad
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2013-01-30 10:57:19 +0000

    Pointers: struct_inter03_null added to show impact of NULL_INITIALIZATION property
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@5990 67b5017d-e62d-0410-a871-f72b1c62810f

--- Pointers/Mensi.sub/struct_inter03_null.c
+++ Pointers/Mensi.sub/struct_inter03_null.c
@@ -1,12 +1,5 @@
-/* The pointer assignment is not translated fully acurately in the
- * main function. When "assignment" is analyzed, the value assigned
- * can be either a defined value or a NULL pointer.
- *
- * When the call site is analyzed, it is clear that "(**t2).ip2" is
- * not NULL.
- *
- * However, the alias analysis is not performed and there is no way to
- * induce that, hence, (**t1).ip1 is also different from NULL.
+/* Same as struct_inter03, but analyzed with NULL initialization
+   turned to false. Exact results should be obtained.
  */
 
 #include <stdio.h>
