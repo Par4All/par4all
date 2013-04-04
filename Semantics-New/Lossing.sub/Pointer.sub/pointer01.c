9090436eb87df374ec2477772b6f15aad3721e16
lossing <lossing@67b5017d-e62d-0410-a871-f72b1c62810f>
2013-04-04 10:45:02 +0000

    add exemple for semantic pointer analysis
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@6031 67b5017d-e62d-0410-a871-f72b1c62810f
    
    Former-commit-id: 5de84acfd4630e4c44726933231ea4b7213cb74e

---
+++ Semantics-New/Lossing.sub/Pointer.sub/pointer01.c
@@ -0,0 +1,50 @@
+
+
+int mainChar()
+{
+  char a[10];
+  char *p, *q;
+  
+  q=&a[0];
+  p=q+1;
+  
+  *q = 0;
+  *p = 0;
+}
+/*
+int mainInt()
+{
+  int a[10];
+  int *p, *q;
+  
+  q=&a[0];
+  p=q+1;
+  
+  *q = 0;
+  *p = 0;
+}
+
+int mainFloat()
+{
+  float a[10];
+  float *p, *q;
+  
+  q=&a[0];
+  p=q+1;
+  
+  *q = 0;
+  *p = 0;
+}
+
+int mainDouble()
+{
+  double a[10];
+  double *p, *q;
+  
+  q=&a[0];
+  p=q+1;
+  
+  *q = 0;
+  *p = 0;
+}
+*/
