bdce30bd9821864f8e7fdbe359246ea1fbbc3356
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2013-04-15 14:54:33 +0000

    changing quake02.c
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@6035 67b5017d-e62d-0410-a871-f72b1c62810f
    
    Former-commit-id: f3dc940a37277bed91fcca31b660a09506bf4779

--- RegionsWithPointsTo/quake02.c
+++ RegionsWithPointsTo/quake02.c
@@ -14,18 +14,14 @@ typedef struct smallarray_s {
   double pad;
 } smallarray_t;
 
-void smvp(int nodes, double (*A)[3][3], int *Acol, int *Aindex,
-	  double (*v)[3], double (*w)[3])
+void smvp(int nodes)
 {
   int i,j;
-  int Anext, Alast, col;
-  double sum0, sum1, sum2;
-  smallarray_t **w1 = (smallarray_t **) malloc(2* sizeof(smallarray_t *));
   int **w2 = (int **) malloc(2 * sizeof(int *));
   for (j = 0; j < 2; j++) {
-    //    for (i = 0; i < nodes; i++) {
-      w2[j] = &i;
-      //}
+      for (i = 0; i < nodes; i++) {
+      w2[j][i] = i;
+    }
   }
 
   
