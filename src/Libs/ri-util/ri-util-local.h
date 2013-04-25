1ab3f281f00e8b2428a0d7e77aca6c9fa2b03ed8
maisonneuve <maisonneuve@32a0c1e6-9308-0410-80dd-f9d2257a62c7>
2013-04-25 08:27:55 +0000

    Revert an involontary character change (raised by encoding issues).
    git-svn-id: https://scm.cri.ensmp.fr/svn/pips/trunk@21761 32a0c1e6-9308-0410-80dd-f9d2257a62c7

--- src/Libs/ri-util/ri-util-local.h
+++ src/Libs/ri-util/ri-util-local.h
@@ -68,7 +68,7 @@
 #define MAIN_PREFIX                      "%"
 #define COMMON_PREFIX                    "~"
 #define BLOCKDATA_PREFIX                 "&"
-#define F95MODULE_PREFIX                 "€"
+#define F95MODULE_PREFIX                 "¤"
 
 #define FILE_SEP_CHAR                    '!'
 #define FILE_SEP                         FILE_SEP_CHAR
