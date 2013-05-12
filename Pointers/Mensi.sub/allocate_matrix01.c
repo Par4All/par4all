b837066cb1f45d469c0991664a18abe8a08394bf
mensi <mensi@67b5017d-e62d-0410-a871-f72b1c62810f>
2013-05-12 07:41:41 +0000

    Adding a new example of matrix allocation
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@6066 67b5017d-e62d-0410-a871-f72b1c62810f
    
    Former-commit-id: dea0fed13964e607d08351178b7745e8fa7d91c6

---
+++ Pointers/Mensi.sub/allocate_matrix01.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include <stdio.h>
+#define NR_END 1
+#define FREE_ARG char*
+
+float **matrix(nrl,nrh,ncl,nch)
+long nch,ncl,nrh,nrl;
+/* allocate a float matrix with subscript range m[nrl..nrh][ncl..nch] */
+{
+	long i, nrow=nrh-nrl+1,ncol=nch-ncl+1;
+	float **m;
+
+	/* allocate pointers to rows */
+	m=(float **) malloc((unsigned int)((nrow+NR_END)*sizeof(float*)));
+	//if (!m) nrerror("allocation failure 1 in matrix()");
+	m += NR_END;
+	m -= nrl;
+
+	/* allocate rows and set pointers to them */
+	m[nrl]=(float *) malloc((unsigned int)((nrow*ncol+NR_END)*sizeof(float)));
+	//if (!m[nrl]) nrerror("allocation failure 2 in matrix()");
+	m[nrl] += NR_END;
+	m[nrl] -= ncl;
+
+	for(i=nrl+1;i<=nrh;i++) m[i]=m[i-1]+ncol;
+
+	/* return pointer to array of pointers to rows */
+	return m;
+}
