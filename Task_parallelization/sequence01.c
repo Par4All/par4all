de49ac306cb7407d004a7067c1c9bcd1ffd8857a
khaldi <khaldi@67b5017d-e62d-0410-a871-f72b1c62810f>
2013-08-17 13:54:08 +0000

    add a simple test for the parallelization of a sequence of tasks
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@6126 67b5017d-e62d-0410-a871-f72b1c62810f
    
    Former-commit-id: 6efe8932eb16dc6ec9a59f17db3db1b485aea66a

---
+++ Task_parallelization/sequence01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+void main(int argc, char *argv[]){
+  
+  unsigned int i, N;
+  double A[10], B[10], C[10];
+  double a,b;
+  N = 10;
+  for(i = 0 ; i < N ; i++){
+    A[i] = 5; 
+  }
+  for(i = 0 ; i < N ; i++){
+    B[i] = 3; 
+  }
+  for(i = 0 ; i < N ; i++)
+    C[i]+= A[i] +  B[i]; 
+}
+     
+      
