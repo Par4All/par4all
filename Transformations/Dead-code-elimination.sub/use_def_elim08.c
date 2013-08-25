5688b316d943dbb2221c71ae00a4136e21653178
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2013-08-25 06:11:58 +0000

    Transformations/Dead_code_elimination.sub: three bugs removed
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@6136 67b5017d-e62d-0410-a871-f72b1c62810f
    
    Former-commit-id: 14d1ae3e1d6666d470bb8a42fdc4863649a85641

--- Transformations/Dead-code-elimination.sub/use_def_elim08.c
+++ Transformations/Dead-code-elimination.sub/use_def_elim08.c
@@ -4,23 +4,31 @@
 // when they escape
 //
 // If the line j = j + c is removed, c = c + 1 becomes useless...
-int count(int J) {
+int COUNT(int J) {
   static int C = 0;
   J = J + C;
   C = C + 1;
   return J;
 }
 
-int lost_count(int J) {
+int LOST_COUNT(int J) {
   static int C = 0;
   C = C + 1;
   return J;
 }
 
+int LOST_COUNT_2(int J) {
+  static int C = 0;
+  J = J + C;
+  C = C + 1;
+  return 0;
+}
+
 int main() {
   int J=0;
-  J=count(J);
-  J=lost_count(J);
+  J=COUNT(J);
+  J=LOST_COUNT(J);
+  J=LOST_COUNT_2(J);
   printf("%d\n",J);
 }
 
