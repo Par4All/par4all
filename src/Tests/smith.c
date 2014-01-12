c761e9eb7024a023d68b839966d4c8d6c576bd57
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
2014-01-12 12:16:22 +0000

    resolution moved.
    git-svn-id: https://scm.cri.ensmp.fr/svn/linear/trunk@1568 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/Tests/smith.c
+++ src/Tests/smith.c
@@ -45,37 +45,5 @@ int main(void)
   matrix_fprint(stdout, D);
   fprintf(stdout, "# Q =\n");
   matrix_fprint(stdout, Q);
-
-  // discuss A x = 0 positive solutions
-  int min_mn = m>n? n: m;
-  int max_mn = m>n? m: n;
-  int i;
-  for (i = 1; i <= min_mn && MATRIX_ELEM(D, i, i) != VALUE_ZERO; i++);
-  int nfree = max_mn-i+1;
-  fprintf(stdout,
-          "\n"
-          "# A x = 0 strictly positive solutions:\n");
-  if (nfree == 0) {
-    fprintf(stdout, "# no free variable\n" "# no solution!\n" "# x* = 0\n");
-  }
-  else {
-    fprintf(stdout, "# %d free variable%s\n", nfree, nfree>1? "s": "");
-    for (int i=1; i<=n; i++) {
-      fprintf(stdout, "# x%d = ", i);
-      bool first = true;
-      for (int j=0; j<nfree; j++) {
-        Value v = matrix_elem(Q, i, n-j);
-        if (v != VALUE_ZERO) {
-          // should never be the case if no coupling
-          if (!first) fprintf(stdout, "+ ");
-          // value should be positive? if so, proof??
-          fprint_Value(stdout, v);
-          fprintf(stdout, " n%d", j);
-          first = false;
-        }
-      }
-      fprintf(stdout, "\n");
-    }
-  }
   return 0;
 }
