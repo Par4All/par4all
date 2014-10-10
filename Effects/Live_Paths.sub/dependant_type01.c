f2ed37a8cc45c58ad0ebaa5f1cbc17b744393f1f
lossing <lossing@67b5017d-e62d-0410-a871-f72b1c62810f>
2014-10-10 09:55:18 +0000

    add example for live path with dependant type
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@6269 67b5017d-e62d-0410-a871-f72b1c62810f
    
    Former-commit-id: 32ae1f51282bdb4e50eb7a385360168946dabb46

---
+++ Effects/Live_Paths.sub/dependant_type01.c
@@ -0,0 +1,10 @@
+
+int main()
+{
+  int i;
+  int size = 10;
+  int a[size];
+  
+  for (i=0; i<10; i++)
+    a[i] = i;
+}
---
+++ Effects/Live_Paths.sub/dependant_type02.c
@@ -0,0 +1,10 @@
+
+int main()
+{
+  int i;
+  int size = 10;
+  int a[size];
+  
+  for (i=0; i<size; i++)
+    a[i] = i;
+}
