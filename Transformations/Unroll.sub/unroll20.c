9237506c96bae331faf79bd6875ad98c989efdbe
irigoin <irigoin@67b5017d-e62d-0410-a871-f72b1c62810f>
2014-11-06 09:18:57 +0000

    Transformations/Unroll.sub: derived entities now supported in unrolling (unroll20 and 21), but not their initializations (unroll22 and 23)
    git-svn-id: https://scm.cri.ensmp.fr/svn/validation/trunk@6290 67b5017d-e62d-0410-a871-f72b1c62810f
    
    Former-commit-id: c788897c050b7292bbb248e60402bcc30e087f22

--- Transformations/Unroll.sub/unroll20.c
+++ Transformations/Unroll.sub/unroll20.c
@@ -4,7 +4,9 @@ void unroll20()
 bar:for(i=0;i<2;i++)
     {
       struct s {int f;};
-      struct s j = {i+2};
-        a[i]=j.f;
+      //struct s j = {i+2};
+      struct s j;
+      j.f = i+2;
+      a[i]=j.f;
     }
 }
--- Transformations/Unroll.sub/unroll22.c
+++ Transformations/Unroll.sub/unroll22.c
@@ -1,11 +1,10 @@
-void unroll21()
+void unroll22()
 {
   int i, a[2];
 bar:for(i=0;i<2;i++)
     {
       struct s {int f;};
-      struct s j;
-      j.f = i+2;
+      struct s j = {i+2};
       a[i]=j.f;
     }
 }
