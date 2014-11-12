00e4490d64afd4253142d6433e4cdc8d4c9b051d
coelho <coelho@8716bff9-3cf5-0310-8878-b27a67f92df8>
2014-11-12 13:20:40 +0000

    add bound option
    git-svn-id: https://scm.cri.ensmp.fr/svn/linear/trunk@1589 8716bff9-3cf5-0310-8878-b27a67f92df8

--- src/Tests/sc_proj.c
+++ src/Tests/sc_proj.c
@@ -37,46 +37,70 @@ extern char * sc_internal_symbol_table(char *);
 int main(int argc, char * const argv[])
 {
   // option management
-  bool reverse = false;
-  int debug = 0;
-  int opt;
-  while ((opt = getopt(argc, argv, "rD")) != -1) {
+  bool reverse = false, bound = false;
+  int debug = 0, opt;
+  while ((opt = getopt(argc, argv, "rbD")) != -1) {
     switch (opt) {
     case 'r': reverse = true; break;
+    case 'b': bound = true; break;
     case 'D': debug++; break;
     default: exit(1);
     }
   }
-  // get system
+  // only one of -r and -b
+  assert(!(reverse && bound));
+
+  // get system from stdin
   Psysteme s;
   bool ok = sc_fscan(stdin, &s);
   assert(ok);
   if (debug >= 2) sc_fprint(stderr, s, *variable_default_name);
-  // project & simplify
-  if (reverse) {
-    Pbase bnext;
-    for (Pbase b = sc_base(s); b && s; b = bnext) {
-      // Oops: the system may be nullifed by sc_projection...
-      // so the next iteration must not rely on it
-      bnext = vecteur_succ(b);
-      bool keep = false;
-      for (int i = optind; i < argc; i++) {
-        if (strcmp(argv[i], vecteur_var(b)) == 0) {
-          keep = true;
-          break;
-        }
-      }
-      if (!keep)
-        s = sc_projection(s, vecteur_var(b));
-    }
+
+  // get command arguments as a base
+  Pbase arg_base = BASE_NULLE;
+  for (int i = optind; i < argc; i++)
+    arg_base = base_add_variable(arg_base, sc_internal_symbol_table(argv[i]));
+
+  Pbase proj_base;
+  if (reverse || bound) {
+    proj_base = vect_copy(sc_base(s));
+    for (Pbase b = arg_base; b != BASE_NULLE; b = vecteur_succ(b))
+      proj_base = base_remove_variable(proj_base, vecteur_var(b));
   }
   else {
-    for(int i = optind; i < argc && s; i++)
-      s = sc_projection(s, sc_internal_symbol_table(argv[i]));
+    proj_base = vect_copy(arg_base);
   }
-  if (debug) sc_fprint(stderr, s, *variable_default_name);
+
+  // project all "proj_base" variables
+  for (Pbase b = proj_base; b != BASE_NULLE; b = vecteur_succ(b))
+      s = sc_projection(s, vecteur_var(b));
+
+  // cleanup
   sc_nredund(&s);
+
+  if (debug) sc_fprint(stderr, s, *variable_default_name);
+
   // print out result
-  sc_fprint(stdout, s, *variable_default_name);
+  if (bound) {
+    // we must bound each remaining variables
+    for (Pbase b = arg_base; b != BASE_NULLE; b = vecteur_succ(b)) {
+      Variable keep = vecteur_var(b);
+      Psysteme sb = sc_copy(s);
+      // YES, we use *s* base
+      for (Pbase c = sc_base(s); c != BASE_NULLE; c = vecteur_succ(c)) {
+        if (vecteur_var(c) != keep)
+          sb = sc_projection(sb, vecteur_var(c));
+      }
+      sc_nredund(&sb);
+      sc_fprint(stdout, sb, *variable_default_name);
+      sc_rm(sb);
+    }
+  }
+  else
+    sc_fprint(stdout, s, *variable_default_name);
+
+  base_rm(arg_base);
+  base_rm(proj_base);
+  sc_rm(s);
   return 0;
 }
